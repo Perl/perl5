db90f03e70f9eb8974742b6bfd3d48844372f8db
Perl 5 Porters <perl5-porters@africa.nicoh.com>
1996-02-08 02:10:33 +0000

    perl 5.002gamma: hints/machten.sh
    This version is specific to machten 4.X.  2.x.x has moved to its
    own hint file.

--- hints/machten.sh
+++ hints/machten.sh
@@ -1,50 +1,31 @@
 # machten.sh
-# This file has been put together by Mark Pease <peasem@primenet.com>
+# This is for MachTen 4.0.2.  It might work on other versions too.
+#
+# MachTen users might need a fixed tr from ftp.tenon.com.  This should
+# be described in the MachTen release notes.
+#
+# MachTen 2.x has its own hint file.
+#
+# This file has been put together by Andy Dougherty
+# <doughera@lafcol.lafayette.edu> based on comments from lots of
+# folks, especially 
+# 	Mark Pease <peasem@primenet.com>
+#	Martijn Koster <m.koster@webcrawler.com>
+#	Richard Yeh <rcyeh@cco.caltech.edu>
+
+#
 # Comments, questions, and improvements welcome!
 #
-# MachTen does not support dynamic loading. If you wish to, you
-# can get <ftp://tsx-11.mit.edu/pub/linux/sources/libs/dld-src-3.2.4.tar.gz>
-# compile and install. This is the version of DLD that works with the
-# ext/DynaLoader/dl_dld.xs in the perl5 package. Have fun!
+# MachTen 4.X does support dynamic loading, but perl doesn't
+# know how to use it yet.
 #
-#  Original version was for MachTen 2.1.1.
 #  Last modified by Andy Dougherty   <doughera@lafcol.lafayette.edu>
-#  Wed Mar  8 15:58:05 EST 1995
+#  Thu Feb  8 15:07:52 EST 1996
 
-# I don't know why this is needed.  It might be similar to NeXT's
-# problem.  See hints/next_3_2.sh.
-usemymalloc='n'
+# Configure doesn't know how to parse the nm output.
+usenm=undef
 
-so='none'
-# These are useful only if you have DLD, but harmless otherwise.
-lddlflags='-r'
-dlext='o'
-
-# MachTen does not support POSIX enough to compile the POSIX module.
-useposix=false
-
-#MachTen might have an incomplete Berkeley DB implementation.
-i_db=$undef
-
-#MachTen versions 2.X have no hard links.  This variable is used
-# by File::Find.
-# This will generate a harmless message:
-# Hmm...You had some extra variables I don't know about...I'll try to keep 'em.
-#	Propagating recommended variable dont_use_nlink
-case "$osver" in
-2*) dont_use_nlink=define ;;
-*) ;;
-esac
-
-case "$osvers" in
-2*)
-	cat <<'EOM' >&4
-
-Tests
-	io/fs test 4  and
-	op/stat test 3
-may fail since MachTen versions 2.X have no hard links.
-
-EOM
-    ;;
-esac
+# At least on PowerMac, doubles must be aligned on 8 byte boundaries.
+# I don't know if this is true for all MachTen systems, or how to
+# determine this automatically.
+alignbytes=8
