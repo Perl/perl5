9a5ea77892c142073969a964667e310fa1a1a18c
Perl 5 Porters <perl5-porters@africa.nicoh.com>
1996-02-09 00:52:11 +0000

    perl 5.002gamma: hints/os2.sh
    Updated.  I've deleted many of the apparently-no-longer-necessary
    commented-out items.

--- hints/os2.sh
+++ hints/os2.sh
@@ -1,47 +1,11 @@
-#! /bin/sh
-# Problems:
-# a) warning from fcntl: Third argument is int in emx - patched
-# b) gr_password is not a structure in struct group - patched
-# c) (gone)
-# d) Makefile needs sh before ./makedir
-# e) (gone)
-# f) (gone)
-# g) (gone)
-# h) (gone)
-# i) (gone)
-# j) the rule true in Makefile should become sh -c true
-# k) Cwd does not work. ===> the extensions cannot be compiled - patched
-# l) TEST expects to get -e 'perl' - patched
-# m) (gone)
-
-# Tests failing with .o compile (this is probably obsolete, but now it is .obj):
-
-# comp/cpp (because of sed above)
-# io/fs.t : (patched) 2..5 7..11 18 (why 11?)
-# io/inplace.t ???? (ak works)
-# io/tell.t 10 ????
-# op/exec.t 1 ???? 4 ????
-# op/glob.t 1 (bug in sh)
-# op/magic.t 4 1/2 (????) adding sleep 5 does not help
-# op/readdir.t 3 (same bug in ksh)
-# op/stat.t 3 4 9 10 20 >34
-
-# Newer results with .obj without i/o optimization, fail:
-
-# io/fs.t	: 2+
-# io/pipe.t	: 1+
-# io/tell.t	: 8, 10
-# op/exec.t	: 4, 6 (ok 1 comes as "ok \1")
-# op/fork.t	: 1+
-# op/misc.t	: 9
-# op/pack.t	: 8
-# op/stat.t	: 3 4 9 10 20 >34
-# lib/sdbm.t	: sdbm store returned -1, errno 0, key "21" at lib/sdbm.t line 112.
-# lib/posix.t	: coredump on 3
-
-# If compiled with i/o optimization, then 15% speedup on input, and
-# io/tell.t	: 11 only
-# no coredump in posix.t
+# hints/os2.sh
+# This file reflects the tireless work of
+# Ilya Zakharevich <ilya@math.ohio-state.edu>
+#
+# Trimmed and comments added by 
+#     Andy Dougherty  <doughera@lafcol.lafayette.edu>
+#     Exactly what is required beyond a standard OS/2 installation?
+#     There are notes about "patched pdksh" I don't understand.
 
 # Note that symbol extraction code gives wrong answers (sometimes?) on
 # gethostent and setsid.
@@ -75,7 +39,7 @@ if [ "$emxaout" != "" ]; then
     d_fork='define'
     lddlflags='-Zdll'
     ldflags='-Zexe'
-    ccflags='-DDOSISH -DOS2=2 -DEMBED -I.'
+    ccflags='-DDOSISH -DNO_SYS_ALLOC -DOS2=2 -DEMBED -I.'
     use_clib='c'
 else
     d_shrplib='define'
@@ -91,6 +55,7 @@ else
 fi
 
 # To get into config.sh (should start at the beginning of line)
+# or you can put it into config.over.
 plibext="$plibext"
 
 #libc="/emx/lib/st/c_import$lib_ext"
@@ -99,13 +64,15 @@ libc="$libemx/st/$use_clib$lib_ext"
 if test -r "$libemx/c_alias$lib_ext"; then 
     libnames="$libemx/c_alias$lib_ext"
 fi
-
 # otherwise puts -lc ???
+
+# [Maybe we should just remove c from $libswanted ?]
 
 libs='-lsocket -lm'
 archobjs="os2$obj_ext"
 
 # Run files without extension with sh - feature of patched ksh
+# [???]
 NOHASHBANG=sh
 # Same with newer ksh
 EXECSHELL=sh
@@ -118,6 +85,7 @@ usedl='define'
 #cppflags='-DDOSISH -DOS2=2 -DEMBED -I.'
 
 # This variables taken from recommended config.sh
+# [Does Configure get it wrong?]
 alignbytes='8'
 
 # for speedup: (some patches to ungetc are also needed):
@@ -165,70 +133,6 @@ nm_opt='-p'
 #dynamic_ext='Fcntl POSIX Socket SDBM_File Devel/DProf'
 #extensions='Fcntl GDBM_File SDBM_File POSIX Socket UPM REXXCALL'
 #extensions='Fcntl SDBM_File POSIX Socket Devel/DProf'
-
-# Unknown reasons for:
-#cpio='cpio'
-#csh=''
-#date=''
-#byacc=''
-#d_charsprf='undef'
-#d_drem='undef'
-#d_fmod='define'
-#d_linuxstd='undef'
-#d_socket='define'
-#gcc='gcc'
-#gidtype='gid_t'
-#glibpth='c:/usr/lib/emx h:/emx/lib /emx/lib'
-#groupstype='gid_t'
-#h_fcntl='true'
-#i_time='define'
-#line=''
-#lseektype='off_t'
-#man1ext='1'
-#man3ext='3'
-#modetype='mode_t'
-#more='more'
-#mv='mv'
-#sleep='sleep'
-#socketlib='-lsocket'
-#ssizetype='ssize_t'
-#tar='tar'
-#timetype='time_t'
-#uidtype='uid_t'
-#uname=''
-#uniq=''
-#xlibpth=''
-#yacc='yacc'
-#yaccflags=''
-#zcat='zcat'
-#orderlib='false'
-#pg='pg'
-#pr='pr'
-#ranlib=':'
-
-# Misfound by configure:
-
-#gcc='gcc'
-#more='more'
-#mv='mv'
-#pr='pr'
-#sleep='sleep'
-#tar='tar'
-
-#xlibpth=''
-
-# I cannot stand it, but did not test with:
-# d_dirnamlen='undef'
-
-# I try to do without these:
-
-#d_pwage='undef'
-#d_pwcomment='undef'
-
-# ????
-#mallocobj=''
-#mallocsrc=''
-#usemymalloc='false'
 
 # The next two are commented. pdksh handles #!
 # sharpbang='extproc '
