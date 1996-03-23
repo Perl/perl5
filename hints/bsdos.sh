0e9bad51c446e0ded7be04fd0b1db485fe11408b
Perl 5 Porters <perl5-porters@africa.nicoh.com>
1996-03-23 10:55:33 +0000

    perl 5.002_01: hints/bsdos.sh
    Update BSD/OS hints file to newer version shipped with Perl 5.001m,
    but comment out sig_name assignment, since it doesn't have a parallel
    sig_num.  Configure should do ok now, though, according to Andy Dougherty.

--- hints/bsdos.sh
+++ hints/bsdos.sh
@@ -1,32 +1,42 @@
-# hints/bsdi_bsdos.sh
+# hints/bsdos.sh
 #
 # hints file for BSD/OS 2.x (adapted from bsd386.sh)
 # Original by Neil Bowers <neilb@khoros.unm.edu>
 #     Tue Oct  4 12:01:34 EDT 1994
 # Updated by Tony Sanders <sanders@bsdi.com>
-#     Mon Mar 13 12:17:24 CST 1995
+#     Mon Nov 27 17:25:51 CST 1995
 #
 # You can override the compiler and loader on the Configure command line:
-#     ./Configure -Dcc=gcc -Dld=ld
+#     ./Configure -Dcc=shlicc2 -Dld=shlicc2
 
 # filename extension for shared library objects
 so='o'
 
-d_voidsig='define'
+# Don't use this for Perl 5.002, which needs parallel sig_name and sig_num lists
+#sig_name='ZERO HUP INT QUIT ILL TRAP IOT EMT FPE KILL BUS SEGV SYS PIPE ALRM TERM URG STOP TSTP CONT CHLD TTIN TTOU IO XCPU XFSZ VTALRM PROF WINCH INFO USR1 USR2 '
 signal_t='void'
-
-# If Configure's signal detection fails, uncomment this line.
-# sig_name='ZERO HUP INT QUIT ILL TRAP IOT EMT FPE KILL BUS SEGV SYS PIPE ALRM TERM URG STOP TSTP CONT CHLD TTIN TTOU IO XCPU XFSZ VTALRM PROF WINCH INFO USR1 USR2 '
-
+d_voidsig='define'
 d_dosuid='define'
 
-# we don't want to use -lnm, since exp() is busted in there (in 1.1 anyway)
+# we don't want to use -lnm, since exp() is busted (in 1.1 anyway)
 set `echo X "$libswanted "| sed -e 's/ nm / /'`
 shift
 libswanted="$*"
 
-# Avoid telldir prototype conflict in pp_sys.c  (BSD/386 uses const DIR *)
+# BSD/OS X libraries are in their own tree
+glibpth="$glibpth /usr/X11/lib"
+ldflags="$ldflags -L/usr/X11/lib"
+
+# Avoid telldir prototype conflict in pp_sys.c
 pp_sys_cflags='ccflags="$ccflags -DHAS_TELLDIR_PROTOTYPE"'
+
+case "$bsdos_distribution" in
+defined)
+	d_portable='no'
+	prefix='/usr/contrib'
+	man3dir='/usr/contrib/man/man3'
+	;;
+esac
 
 case "$osvers" in
 1.0*)
@@ -34,20 +44,40 @@ case "$osvers" in
 	POSIX_cflags='ccflags="$ccflags -UHUGE_VAL"' 
 	;;
 1.1*)
-	# Use gcc2 (2.5.8) if available in 1.1.
+	# Use gcc2
 	case "$cc" in
-	'')	cc=gcc2 ;;
+	'')	cc='gcc2' ;;
 	esac
 	;;
 2.*)
-	# Use 2.X's gcc2
-	case "$cc" in
-	'')	cc=gcc2 ;;
+	case "$osvers" in
+	2.1*)	# dlopen() is supported in 2.1
+		usedl='true'
+		d_dlopen='define'
+		cccdlflags='none'
+		# pre-link against the shared C library
+		lddlflags='-r -lc_s.2.1.0'
+
+		# BSD/OS 2.1 doesn't (yet) support `true' dynamic linking
+		# so we `preload' the shared libraries by linking
+		# against them; even though we don't pull in any symbols.
+		libswanted="Xpm Xaw Xmu Xt SM ICE Xext X11 $libswanted"
+		libswanted="rpc curses termcap $libswanted"
+
+		# Use the system malloc or else you'll have dualing mallocs!
+		d_mymalloc='undef'
+		usemymalloc='n'
+		;;
 	esac
 
-	# Link with shared libraries in 2.X
+	# default to GCC 2.X w/shared libraries
+	case "$cc" in
+	'')	cc='shlicc2' ;;
+	esac
+
+	# default ld to shared library linker
 	case "$ld" in
-	'')	ld='shlicc' ;;
+	'')	ld='shlicc2' ;;
 	esac
 
 	# setre?[ug]id() have been replaced by the _POSIX_SAVED_IDS stuff
