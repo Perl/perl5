3432c71bbc822f6d0b5d96962313d374b438fa64
Perl 5 Porters <perl5-porters@africa.nicoh.com>
1996-06-18 07:42:27 +0000

    Prototypes for OS/2-specific emulation of dlopen routines

---
+++ os2/dlfcn.h
@@ -0,0 +1,6 @@
+void *dlopen(char *path, int mode);
+void *dlsym(void *handle, char *symbol);
+char *dlerror(void);
+void *dlopen(char *path, int mode);
+void *dlsym(void *handle, char *symbol);
+char *dlerror(void);
