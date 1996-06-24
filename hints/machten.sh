baf67fda52a40289c9f17a029b77d57ec7ee99fb
Perl 5 Porters <perl5-porters@africa.nicoh.com>
1996-06-24 03:07:54 +0000

    perl 5.003: hints/machten.sh
    Don't use buggy sigsetjmp
    Don't use nlinks returned by stat to count subdirectories

--- hints/machten.sh
+++ hints/machten.sh
@@ -12,15 +12,17 @@
 # 	Mark Pease <peasem@primenet.com>
 #	Martijn Koster <m.koster@webcrawler.com>
 #	Richard Yeh <rcyeh@cco.caltech.edu>
-
+#
+# File::Find's use of link count disabled by Dominic Dunlop 950528
+# Perl's use of sigsetjmp etc. disabled by Dominic Dunlop 950521
 #
 # Comments, questions, and improvements welcome!
 #
 # MachTen 4.X does support dynamic loading, but perl doesn't
 # know how to use it yet.
 #
-#  Last modified by Andy Dougherty   <doughera@lafcol.lafayette.edu>
-#  Thu Feb  8 15:07:52 EST 1996
+#  Updated by Dominic Dunlop <domo@tcp.ip.lu>
+#  Tue May 28 11:20:08 WET DST 1996
 
 # Configure doesn't know how to parse the nm output.
 usenm=undef
@@ -29,3 +31,32 @@ usenm=undef
 # I don't know if this is true for all MachTen systems, or how to
 # determine this automatically.
 alignbytes=8
+
+# There appears to be a problem with perl's use of sigsetjmp and
+# friends.  Use setjmp and friends instead.
+d_sigsetjmp='undef' 
+
+# MachTen always reports ony two links to directories, even if they
+# contain subdirectories.  Consequently, we use this variable to stop
+# File::Find using the link count to determine whether there are
+# subdirectories to be searched.  This will generate a harmless message:
+# Hmm...You had some extra variables I don't know about...I'll try to keep 'em.
+#	Propagating recommended variable dont_use_nlink
+dont_use_nlink=define
+
+cat <<'EOM' >&4
+
+Tests
+	io/fs test 4  and
+	op/stat test 3
+may fail since MachTen does not return a useful nlinks field to stat
+on directories.
+
+At the end of Configure, you will see a harmless message
+
+Hmm...You had some extra variables I don't know about...I'll try to keep 'em.
+	Propagating recommended variable dont_use_nlink
+
+Read the File::Find documentation for more information.
+
+EOM
