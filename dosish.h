e6aa316494fe1e1bc199bd7d6e4d5ebbf7488889
Perl 5 Porters <perl5-porters@africa.nicoh.com>
1996-07-25 03:12:47 +0000

    Supply OS-specific values for macros added as part of Plan9 and VMS support

--- dosish.h
+++ dosish.h
@@ -6,6 +6,27 @@
 #define dXSUB_SYS int dummy
 #define TMPPATH "plXXXXXX"
 
+/* USEMYBINMODE
+ *	This symbol, if defined, indicates that the program should
+ *	use the routine my_binmode(FILE *fp, char iotype) to insure
+ *	that a file is in "binary" mode -- that is, that no translation
+ *	of bytes occurs on read or write operations.
+ */
+#undef USEMYBINMODE
+
+/* USE_STAT_RDEV:
+ *	This symbol is defined if this system has a stat structure declaring
+ *	st_rdev
+ */
+#define USE_STAT_RDEV 	/**/
+
+/* ACME_MESS:
+ *	This symbol, if defined, indicates that error messages should be 
+ *	should be generated in a format that allows the use of the Acme
+ *	GUI/editor's autofind feature.
+ */
+#undef ACME_MESS	/**/
+
 /*
  * fwrite1() should be a routine with the same calling sequence as fwrite(),
  * but which outputs all of the bytes requested as a single stream (unlike
