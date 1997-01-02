1b33cabaaf2fbe2e84c6a13a9b0a7fce45959c4f
Chip Salzenberg <chip@perl.com>
1997-01-03 11:18:00 +1200

    Add entry for prototype() in Pod::Functions

--- lib/Pod/Functions.pm
+++ lib/Pod/Functions.pm
@@ -193,6 +193,7 @@ my	Misc,Namespace	declare and assign a local variable (lexical scoping)
 next	Flow	iterate a block prematurely
 no	Modules	unimport some module symbols or semantics at compile time
 package	Modules,Objects,Namespace	declare a separate global namespace
+prototype	Flow,Misc	get the prototype (if any) of a subroutine
 oct	String,Math	convert a string to an octal number
 open	File	open a file, pipe, or descriptor
 opendir	File	open a directory
