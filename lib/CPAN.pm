1eb24ff5c6624250a59bc16c5ad8fb57f923a9a0
Chip Salzenberg <chip@atlantic.net>
1997-04-03 10:03:25 +1200

    CPAN & TRL-Gnu
    (this is the same change as commit 8fe3829d7944eb627ee5360e65ca8f7afe76886d, but as applied)

--- lib/CPAN.pm
+++ lib/CPAN.pm
@@ -1,7 +1,7 @@
 package CPAN;
 use vars qw{$META $Signal $Cwd $End $Suppress_readline};
 
-$VERSION = '1.24';
+$VERSION = '1.2401';
 
 # $Id: CPAN.pm,v 1.139 1997/03/31 22:43:23 k Exp $
 
@@ -308,10 +308,10 @@ sub shell {
     my $getcwd;
     $getcwd = $CPAN::Config->{'getcwd'} || 'cwd';
     my $cwd = Cwd->$getcwd();
-    # How should we determine if we have more than stub ReadLine enabled?
     my $rl_avail = $Suppress_readline ? "suppressed" :
-	defined &Term::ReadLine::Perl::readline ? "enabled" :
-	    "available (get Term::ReadKey and Term::ReadLine::Perl)";
+	($term->ReadLine ne "Term::ReadLine::Stub") ? "enabled" :
+	    "available (get Term::ReadKey and Term::ReadLine::Perl ".
+	    "or get Term::ReadLine::Gnu)";
 
     print qq{
 cpan shell -- CPAN exploration and modules installation (v$CPAN::VERSION)
