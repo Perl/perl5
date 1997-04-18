e8c10df3928ec185d4ddb049bbc636ff5fd03360
Scott Henry <scotth@sgi.com>
1997-04-18 12:37:24 -0700

    Irix update
    >>>>> "A" == Andy Dougherty <doughera@fractal.phys.lafayette.edu> writes:
    
    A> On 17 Apr 1997, Scott Henry wrote:
    >> My recommendation is that irix_6_3.sh, irix_6_4.sh should be
    >> duplicates of (the recently changed) irix_6_2.sh.  As best as I can
    >> determine this far ahead of release, irix_6_5.sh will be the same,
    >> too.
    
    A> In which case, they should all just be irix_6.sh.  If 6.0 and 6.1 need
    A> separate hints files, then we can have irix_6_0.sh and irix_6_1.sh.
    
    A> If we use the generic irix_6.sh, then 
    A> 1.  We don't have pointless, error-prone duplication of hint files.
    A> 2.  We magically get covered even when 6.5 and 6.6 are released and we're
    A> still stuck at 5.004. :-)
    
    EXCELLENT idea!  I'll make up my patch that way.
    
    [editor's note: the patch never hit the mailing list, but this message
    seems relevant]
    
    p5p-msgid: yd8afmwnu80.fsf@hoshi.engr.sgi.com
    private-msgid: yd8d8rsi0ln.fsf@hoshi.engr.sgi.com

--- hints/irix_6.sh
+++ hints/irix_6.sh
@@ -1,43 +1,67 @@
-# irix_6.sh
-# from Krishna Sethuraman, krishna@sgi.com
-# Date: Wed Jan 18 11:40:08 EST 1995
-# added `-32' to force compilation in 32-bit mode.
-# otherwise, copied from irix_5.sh.
+# hints/irix_6_2.sh
+#
+# original from Krishna Sethuraman, krishna@sgi.com
+#
+# Updated Mon Jul 22 14:52:25 EDT 1996
+# 	Andy Dougherty <doughera@lafcol.lafayette.edu>
+# 	with help from Dean Roehrich <roehrich@cray.com>.
+#   cc -n32 update info from Krishna Sethuraman, krishna@sgi.com.
+#       additional update from Scott Henry, scotth@sgi.com
 
-# Perl built with this hints file under IRIX 6.0.1 passes 
-# all tests (`make test').
+# Use   sh Configure -Dcc='cc -n32' to try compiling with -n32.
+#     or -Dcc='cc -n32 -mips3' (or -mips4) to force (non)portability
+# Don't bother with -n32 unless you have the 7.1 or later compilers.
+#     But there's no quick and light-weight way to check in 6.2.
 
-# Tue Jan  2 14:52:36 EST 1996
-# Apparently, there's a stdio bug that can lead to memory
-# corruption using perl's malloc, but not SGI's malloc.
-usemymalloc='n'
+case "$cc" in
+*"cc -n32"*)
+	ld=ld
+	ccflags="$ccflags -D_BSD_TYPES -D_BSD_TIME -woff 1009,1110,1184 -OPT:Olimit=0"
+#	optimize='none'	# for pre-7.1 compilers. Miniperl core dumps with -O
+	optimize='-O3'	# This works with the 7.1 and later compilers
+	ldflags=' -L/usr/local/lib -L/usr/lib32 -L/lib32'
+	cccdlflags=' '
+	lddlflags="-n32 -shared"
+	libc='/usr/lib32/libc.so'
+	plibpth='/usr/lib32 /lib32 /usr/ccs/lib'
+	nm_opt='-p'
+	nm_so_opt='-p'
+	;;
+*)
+	# this is needed to force the old-32 paths
+	#  since the system default can be changed.
+	ccflags="$ccflags -32 -D_BSD_TYPES -D_BSD_TIME -Olimit 3000"
+	;;
+esac
 
-ld=ld
-i_time='define'
-cc="cc -32"
-ccflags="$ccflags -D_POSIX_SOURCE -ansiposix -D_BSD_TYPES -Olimit 3000"
-lddlflags="-32 -shared"
+pp_sys_cflags='ccflags="$ccflags -DHAS_TELLDIR_PROTOTYPE"'
 
 # We don't want these libraries.  Anyone know why?
 set `echo X "$libswanted "|sed -e 's/ socket / /' -e 's/ nsl / /' -e 's/ dl / /'`
 shift
 libswanted="$*"
+
+# I have conflicting reports about the sun, crypt, bsd, and PW
+# libraries on Irix 6.2.
 #
-# The following might be of interest if you wish to try 64-bit mode:
-# irix_6_64bit.sh
-# Krishna Sethuraman, krishna@sgi.com
-# taken from irix_5.sh .  Changes from irix_5.sh:
-# Olimit and nested comments (warning 1009) no longer accepted
-# -OPT:fold_arith_limit so POSIX module will optimize
-# no 64bit versions of sun, crypt, nsl, socket, dl dso's available
-# as of IRIX 6.0.1 so omit those from libswanted line via `sed'.
+# One user rerports:
+# Don't need sun crypt bsd PW under 6.2.  You *may* need to link
+# with these if you want to run perl built under 6.2 on a 5.3 machine
+# (I haven't checked)
+#
+# Another user reported that if he included those libraries, a large number
+# of the tests failed (approx. 20-25) and he would get a core dump. To
+# make things worse, test results were inconsistent, i.e., some of the
+# tests would pass some times and fail at other times.
+# The safest thing to do seems to be to eliminate them.
+#
+#  Actually, the only libs that you want are '-lm'.  Everything else
+# you need is in libc.  You do also need '-lbsd' if you choose not
+# to use the -D_BSD_* defines.  Note that as of 6.2 the only
+# difference between '-lmalloc' and '-lc' malloc is the debugging
+# and control calls. -- scotth@sgi.com
 
-# perl 5 built with this hints file passes most tests (`make test').
-# Fails on op/subst test only. (built and tested under IRIX 6.0.1).
+set `echo X "$libswanted "|sed -e 's/ sun / /' -e 's/ crypt / /' -e 's/ bsd / /' -e 's/ PW / /'`
+shift
+libswanted="$*"
 
-# i_time='define'
-# ccflags="$ccflags -D_POSIX_SOURCE -ansiposix -D_BSD_TYPES -woff 1009 -OPT:fold_arith_limit=1046"
-# lddlflags="-shared"
-# set `echo X "$libswanted "|sed -e 's/ socket / /' -e 's/ sun / /' -e 's/ crypt / /' -e 's/ nsl / /' -e 's/ dl / /'`
-# shift
-# libswanted="$*"
