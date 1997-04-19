2ea6c7ce0488e1089687862028446bf8024c6a88
Roderick Schertler <roderick@argon.org>
1997-04-19 10:22:43 -0400

    Silence warnings on simple C<use ops>
    private-msgid: pzybafum6k.fsf@eeyore.ibcinc.com

--- ext/Opcode/ops.pm
+++ ext/Opcode/ops.pm
@@ -7,12 +7,12 @@ sub import {
     # Not that unimport is the prefered form since import's don't
 	# accumulate well owing to the 'only ever add opmask' rule.
 	# E.g., perl -Mops=:set1 -Mops=:setb is unlikely to do as expected.
-    opmask_add(invert_opset opset(@_));
+    opmask_add(invert_opset opset(@_)) if @_;
 }
 
 sub unimport {
     shift;
-    opmask_add(opset(@_));
+    opmask_add(opset(@_)) if @_;
 }
 
 1;
