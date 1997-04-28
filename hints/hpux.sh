a28b458ac6445fb6d53a53bab0f347e9b6860ec9
Chuck D. Phillips <cdp@hpescdp.fc.hp.com>
1997-04-27 23:35:07 -0600

    HP-UX hint update
    Subject: suggested changes to hpux hints file
    
    NOTE: It *might* not work under 11.x, but at least it's not guaranteed
    to fail.  :-)
    
    p5p-msgid: 199704280535.WAA22441@palrel1.hp.com

--- hints/hpux.sh
+++ hints/hpux.sh
@@ -1,29 +1,63 @@
-#! /local/gnu/bin/bash
+#! /bin/sh
+
 # hints/hpux.sh
 # Perl Configure hints file for Hewlett Packard HP-UX 9.x and 10.x
-# This file is based on 
-# hints/hpux_9.sh, Perl Configure hints file for Hewlett Packard HP-UX 9.x
+# (Hopefully, 7.x through 11.x.)
+#
+# This file is based on hints/hpux_9.sh, Perl Configure hints file for
+# Hewlett Packard HP-UX 9.x
+#
 # Use Configure -Dcc=gcc to use gcc.
+#
 # From: Jeff Okamoto <okamoto@corp.hp.com>
 # and
 # hints/hpux_10.sh, Perl Configure hints file for Hewlett Packard HP-UX 10.x
 # From: Giles Lean <giles@nemeton.com.au>
+# and
+# Use #define CPU_* instead of comments for >= 10.x.
+# Support PA1.2 under 10.x.
+# Distinguish between PA2.0, PA2.1, etc.
+# Distinguish between MC68020, MC68030, MC68040
+# Don't assume every OS != 10 is < 10, (e.g., 11).
+# From: Chuck Phillips <cdp@fc.hp.com>
 
-# This version: March 21, 1997
+# This version: April 27, 1997
 # Current maintainer: Jeff Okamoto <okamoto@corp.hp.com>
 
 #--------------------------------------------------------------------
 # Use Configure -Dcc=gcc to use gcc.
 # Use Configure -Dprefix=/usr/local to install in /usr/local.
 #
-# Some users have reported problems with dynamic loading if the 
-# environment variable LDOPTS='-a archive' .
+# You may have dynamic loading problems if the environment variable
+# LDOPTS='-a archive'.  Under >= 10.x, you can instead LDOPTS='-a
+# archive_shared' to prefer archive libraries without requiring them.
+# Regardless of HPUX release, in the "libs" variable or the ext.libs
+# file, you can always give explicit path names to archive libraries
+# that may not exist on the target machine.  E.g., /usr/lib/libndbm.a
+# instead of -lndbm.  See also note below on ndbm.
+#
+# ALSO, bear in mind that gdbm and Berkely DB contain incompatible
+# replacements for ndbm (and dbm) routines.  If you want concurrent
+# access to ndbm files, you need to make sure libndbm is linked in
+# *before* gdbm and Berkely DB.  Lastly, remember to check the
+# "ext.libs" file which is *probably* messing up the order.  Often,
+# you can replace ext.libs with an empty file to fix the problem.
 #
 # If you get a message about "too much defining", you might have to
 # add the following to your ccflags: '-Wp,-H256000'
 #--------------------------------------------------------------------
 
 # Turn on the _HPUX_SOURCE flag to get many of the HP add-ons
+# regardless of compiler.  For the HP ANSI C compiler, you may also
+# want to include +e to enable "long long" and "long double".
+#
+# HP compiler flags to include (if at all) *both* as part of ccflags
+# and cc itself so Configure finds (and builds) everything
+# consistently:
+#	-Aa -D_HPUX_SOURCE +e
+#
+# Lastly, you may want to include the "-z" HP linker flag so that
+# reading from a NULL pointer causes a SEGV.
 ccflags="$ccflags -D_HPUX_SOURCE"
 
 # If you plan to use gcc, then you should uncomment the following line
@@ -49,51 +83,46 @@ EOM
     else
 	ccflags="$ccflags -Aa"	# The add-on compiler supports ANSI C
     fi
+    # For HP's ANSI C compiler, up to "+O3" is safe for everything
+    # except shared libraries (PIC code).  Max safe for PIC is "+O2".
+    # Setting both causes innocuous warnings.
+    #optimize='+O3'
+    #cccdlflags='+z +O2'
     optimize='-O'
     ;;
 esac
 
 # Determine the architecture type of this system.
 # Keep leading tab below -- Configure Black Magic -- RAM, 03/02/97
-	xxuname=`uname -r`
-if echo $xxuname | $contains '10' >/dev/null 2>&1
+	xxOsRevMajor=`uname -r | sed -e 's/^[^0-9]*//' | cut -d. -f1`;
+	#xxOsRevMinor=`uname -r | sed -e 's/^[^0-9]*//' | cut -d. -f2`;
+if [ "$xxOsRevMajor" -ge 10 ]
 then
-	# This system is running 10.0
-	xxcpu1=`getconf CPU_VERSION`
-	xxcpu2=`printf %#x ${xxcpu1}`
-	xxcontext=`grep "$xxcpu2" /usr/include/sys/unistd.h`
-	if echo "$xxcontext" | $contains 'PA-RISC1.1' >/dev/null 2>&1
-	then
-		archname='PA-RISC1.1'
-	elif echo "$xxcontext" | $contains 'PA-RISC1.0' >/dev/null 2>&1
-	then
-		archname='PA-RISC1.0'
-	elif echo "$xxcontext" | $contains 'PA-RISC2' >/dev/null 2>&1
-	then
-		archname='PA-RISC2'
-	else
-		echo "This 10.0 system is of a PA-RISC type I don't recognize." >&2
-		echo "Debugging output: $xxcontext"
-		archname=''
-	fi
+	# This system is running >= 10.x
+
+	# Tested on 10.01 PA1.x and 10.20 PA[12].x.  Idea: Scan
+	# /usr/include/sys/unistd.h for matches with "#define CPU_* `getconf
+	# CPU_VERSION`" to determine CPU type.  Note the part following
+	# "CPU_" is used, *NOT* the comment.
+	#
+	# ASSUMPTIONS: Numbers will continue to be defined in hex -- and in
+	# /usr/include/sys/unistd.h -- and the CPU_* #defines will be kept
+	# up to date with new CPU/OS releases.
+	xxcpu=`getconf CPU_VERSION`; # Get the number.
+	xxcpu=`printf '0x%x' $xxcpu`; # convert to hex
+	archname=`sed -n -e "s/^#[ \t]*define[ \t]*CPU_//p" /usr/include/sys/unistd.h |
+	    sed -n -e "s/[ \t]*$xxcpu[ \t].*//p" |
+	    sed -e s/_RISC/-RISC/ -e s/HP_// -e s/_/./`;
 else
-	# This system is not running 10.0
-	xxcontext=`/bin/getcontext`
-	if echo "$xxcontext" | $contains 'PA-RISC1.1' >/dev/null 2>&1
-	then
-		archname='PA-RISC1.1'
-	elif echo "$xxcontext" | $contains 'PA-RISC1.0' >/dev/null 2>&1
-	then
-		archname='PA-RISC1.0'
-	elif echo "$xxcontext" | $contains 'HP-MC' >/dev/null 2>&1
-	then
-		archname='HP-MC68K'
-	else
-		echo "I cannot recognize what chip set this system is using." >&2
-		echo "Debugging output: $xxcontext"
-		archname=''
-	fi
+	# This system is running <= 9.x
+	# Tested on 9.0[57] PA and [78].0 MC680[23]0.  Idea: After removing
+	# MC6888[12] from context string, use first CPU identifier.
+	#
+	# ASSUMPTION: Only CPU identifiers contain no lowercase letters.
+	archname=`getcontext | tr ' ' '\012' | grep -v '[a-z]' | grep -v MC688 |
+	    sed -e 's/HP-//' -e 1q`;
 fi
+
 
 # Remove bad libraries that will cause problems
 # (This doesn't remove libraries that don't actually exist)
@@ -104,21 +133,27 @@ fi
 # The libraries crypt, malloc, ndir, and net are empty.
 # Although -lndbm should be included, it will make perl blow up if you should
 # copy the binary to a system without libndbm.sl.  See ccdlflags below.
-set `echo " $libswanted " | sed  -e 's@ ld @ @' -e 's@ dbm @ @' -e 's@ BSD @ @' -e 's@ PW @ @'`
+set `echo " $libswanted " | sed -e 's@ ld @ @' -e 's@ dbm @ @' -e 's@ BSD @ @' -e 's@ PW @ @'`
 libswanted="$*"
 
-# By setting the deferred flag below, this means that if you run perl on a
-# system that does not have the required shared library that you linked it
-# with, it will die when you try to access a symbol in the (missing) shared
-# library.  If you would rather know at perl startup time that you are
-# missing an important shared library, switch the comments so that immediate,
-# rather than deferred loading is performed.
-# ccdlflags="-Wl,-E $ccdlflags"
+# By setting the deferred flag below, this means that if you run perl
+# on a system that does not have the required shared library that you
+# linked it with, it will die when you try to access a symbol in the
+# (missing) shared library.  If you would rather know at perl startup
+# time that you are missing an important shared library, switch the
+# comments so that immediate, rather than deferred loading is
+# performed.  Even with immediate loading, you can postpone errors for
+# undefined (or multiply defined) routines until actual access by
+# adding the "nonfatal" option.
+# ccdlflags="-Wl,-E -Wl,-B,immediate $ccdlflags"
+# ccdlflags="-Wl,-E -Wl,-B,immediate,-B,nonfatal $ccdlflags"
 ccdlflags="-Wl,-E -Wl,-B,deferred $ccdlflags"
 
 usemymalloc='y'
 alignbytes=8
 selecttype='int *'
+# For native nm, you need "-p" to produce BSD format output.
+nm_opt='-p'
 
 # When HP-UX runs a script with "#!", it sets argv[0] to the script name.
 toke_cflags='ccflags="$ccflags -DARG_ZERO_IS_SCRIPT"'
