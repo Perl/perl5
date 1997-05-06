85f483a204da05f35df75385bcd8490f3f5fed89
Scott Henry <scotth@sgi.com>
1997-05-06 11:09:56 -0700

    Irix hint update
    Hopefully, this is the final set of patches for the IRIX hints file
    hints/irix_6.sh.  This is rather a multi-person effort, Jarkko and
    John Stoffel wrote the major changes (and changed the default to
    -n32), I made the last tweaks so that -o32 still works without
    errors on the various compiler releases.  I've tested on IRIX-6.2,
    IRIX64-6.2, IRIX64-6.4 and IRIX-6.5alpha.
    
    p5p-msgid: yd83es0fp57.fsf@hoshi.engr.sgi.com

--- hints/irix_6.sh
+++ hints/irix_6.sh
@@ -1,4 +1,4 @@
-# hints/irix_6_2.sh
+# hints/irix_6.sh
 #
 # original from Krishna Sethuraman, krishna@sgi.com
 #
@@ -8,17 +8,53 @@
 #   cc -n32 update info from Krishna Sethuraman, krishna@sgi.com.
 #       additional update from Scott Henry, scotth@sgi.com
 
+#       Futzed with by John Stoffel <jfs@fluent.com> on 4/24/1997
+#         - assumes 'cc -n32' by default
+#         - tries to check for various compiler versions and do the right 
+#           thing when it can
+#         - warnings turned off (-n32 messages):
+#            1116 - non-void function should return a value
+#            1048 - cast between pointer-to-object and pointer-to-function
+#            1042 - operand types are incompatible
+
 # Use   sh Configure -Dcc='cc -n32' to try compiling with -n32.
 #     or -Dcc='cc -n32 -mips3' (or -mips4) to force (non)portability
 # Don't bother with -n32 unless you have the 7.1 or later compilers.
 #     But there's no quick and light-weight way to check in 6.2.
 
+# Let's assume we want to use 'cc -n32' by default
+case "$cc" in
+'')
+    cc='cc -n32'
+    ;;
+esac
+
+# Check for which compiler we're using
+
 case "$cc" in
 *"cc -n32"*)
-	ld=ld
+
+	# Check for which version of the compiler we're running
+	case "`$cc -version 2>&1`" in
+	*7.0*)                        # Mongoose 7.0
+	     ccflags="$ccflags -D_BSD_TYPES -D_BSD_TIME -woff 1009,1042,1048,1110,1116,1184 -OPT:Olimit=0"
+	     optimize='none'	  
+	     ;;
+	*7.*)                         # Mongoose 7.1+
+	     ccflags="$ccflags -D_BSD_TYPES -D_BSD_TIME -woff 1009,1110,1184 -OPT:Olimit=0"
+	     optimize='-O3'	  
+	     ;;
+	*6.2*)                        # Ragnarok 6.2
+	     ccflags="$ccflags -D_BSD_TYPES -D_BSD_TIME -woff 1009,1110,1184"
+	     optimize='none'	  
+	     ;;
+	*)                            # Be safe and not optimize
 	ccflags="$ccflags -D_BSD_TYPES -D_BSD_TIME -woff 1009,1110,1184 -OPT:Olimit=0"
-#	optimize='none'	# for pre-7.1 compilers. Miniperl core dumps with -O
-	optimize='-O3'	# This works with the 7.1 and later compilers
+	     optimize='none'
+	     ;;
+	esac
+
+	ld=ld
 	ldflags=' -L/usr/local/lib -L/usr/lib32 -L/lib32'
 	cccdlflags=' '
 	lddlflags="-n32 -shared"
@@ -30,10 +66,12 @@ case "$cc" in
 *)
 	# this is needed to force the old-32 paths
 	#  since the system default can be changed.
-	ccflags="$ccflags -32 -D_BSD_TYPES -D_BSD_TIME -Olimit 3000"
+	ccflags="$ccflags -32 -D_BSD_TYPES -D_BSD_TIME -Olimit 3100"
+	optimize='-O'	  
 	;;
 esac
 
+# This should be a Configure thing, but not for now...
 pp_sys_cflags='ccflags="$ccflags -DHAS_TELLDIR_PROTOTYPE"'
 
 # We don't want these libraries.  Anyone know why?
@@ -64,4 +102,3 @@ libswanted="$*"
 set `echo X "$libswanted "|sed -e 's/ sun / /' -e 's/ crypt / /' -e 's/ bsd / /' -e 's/ PW / /'`
 shift
 libswanted="$*"
-
