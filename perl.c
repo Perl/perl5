e1c148c28bf3335bbd0ad9a2070b0917265c00c3
Malcolm Beattie <mbeattie@sable.ox.ac.uk>
1998-02-03 16:00:07 +0000

    Replaced two occurrences of THREADSV(find_thread_sv(...)) (order of execution causes core dump if threadsvp is moved). Replaced lvalue occurrence of AvARRAY(av) with SvPVX(av) (former does cast).
    p4raw-id: //depot/perl@459

--- perl.c
+++ perl.c
@@ -935,11 +935,7 @@ print \"  \\@INC:\\n    @INC\\n\";");
     /* now that script is parsed, we can modify record separator */
     SvREFCNT_dec(rs);
     rs = SvREFCNT_inc(nrs);
-#ifdef USE_THREADS
-    sv_setsv(THREADSV(find_threadsv("/")), rs); 
-#else
-    sv_setsv(GvSV(gv_fetchpv("/", TRUE, SVt_PV)), rs);
-#endif /* USE_THREADS */
+    sv_setsv(perl_get_sv("/", TRUE), rs);
     if (do_undump)
 	my_unexec();
 
@@ -2509,12 +2505,7 @@ init_predump_symbols(void)
     GV *tmpgv;
     GV *othergv;
 
-#ifdef USE_THREADS
-    sv_setpvn(THREADSV(find_threadsv("\"")), " ", 1);
-#else
-    sv_setpvn(GvSV(gv_fetchpv("\"", TRUE, SVt_PV)), " ", 1);
-#endif /* USE_THREADS */
-
+    sv_setpvn(perl_get_sv("\"", TRUE), " ", 1);
     stdingv = gv_fetchpv("STDIN",TRUE, SVt_PVIO);
     GvMULTI_on(stdingv);
     IoIFP(GvIOp(stdingv)) = PerlIO_stdin();
