d516a115cc96334486070e15a6babcd1278d8de9
Jarkko Hietaniemi <jhi@iki.fi>
1998-02-19 19:54:52 +0200

    retry [PATCH] 5.004_59: the perlhist.pod etc
    p4raw-id: //depot/perl@557

--- ext/Thread/Thread/Queue.pm
+++ ext/Thread/Thread/Queue.pm
@@ -1,6 +1,19 @@
 package Thread::Queue;
 use Thread qw(cond_wait cond_broadcast);
 
+=head1 NAME
+
+Thread::Queue - thread-safe queues
+
+=head1 SYNOPSIS
+
+    use Thread::Queue;
+    my $q = new Thread::Queue;
+    $q->enqueue("foo", "bar");
+    my $foo = $q->dequeue;	# The "bar" is still in the queue.
+
+=cut
+
 sub new {
     my $class = shift;
     return bless [@_], $class;
