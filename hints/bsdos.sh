b760be090b8160caf516192fda912a7defe1247a
Tim Bunce <TimBunce@ig.ac.uk>
1998-03-04 16:31:29 +0000

    Updated hints/bsdos.sh for BSD/OS 3.1 Fixed typo in pod/perlsyn.pod Added workaround for old gmake in ext/SDBM_File/sdbm/Makefile.PL Fixed typo in ext/GDBM_File/GDBM_File.pm
    p4raw-id: //depot/maint-5.004/perl@753

--- hints/bsdos.sh
+++ hints/bsdos.sh
@@ -69,7 +69,7 @@ case "$osvers" in
 	'')	cc='gcc2' ;;
 	esac
 	;;
-2.0*|2.1*|3.0*)
+2.0*|2.1*|3.0*|3.1*)
 	so='o'
 
 	# default to GCC 2.X w/shared libraries
@@ -88,8 +88,8 @@ case "$osvers" in
 	libswanted="Xpm Xaw Xmu Xt SM ICE Xext X11 $libswanted"
 	libswanted="rpc curses termcap $libswanted"
 	;;
-3.1*)
-	# ELF dynamic link libraries starting in 3.1
+3.2*)
+	# ELF dynamic link libraries starting in 3.2
         useshrplib='true'
 	so='so'
 	dlext='so'
