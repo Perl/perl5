12a95bd5bba5bc6762110760322a54758e529ee1
Albert Dvornik <bert@genscan.com>
1998-07-06 21:51:05 +0000

    "Fix for broken goto &xsub" Msg-ID:  <tq4sxawf2h.fsf@puma.genscan.com>  Files:  MANIFEST pp_ctl.c t/op/goto_xs.t
    p4raw-id: //depot/maint-5.004/perl@1344

--- pp_ctl.c
+++ pp_ctl.c
@@ -1770,6 +1770,16 @@ PP(pp_goto)
 		AvREAL_off(av);
 		av_clear(av);
 	    }
+	    else if (CvXSUB(cv)) {	/* put GvAV(defgv) back onto stack */
+		AV* av = GvAV(defgv);
+		int i;
+
+		items = AvFILLp(av) + 1;
+		stack_sp++;
+		EXTEND(stack_sp, items); /* @_ could have been extended. */
+		Copy(AvARRAY(av), stack_sp, items, SV*);
+		stack_sp += items;
+	    }
 	    if (cx->cx_type == CXt_SUB &&
 		!(CvDEPTH(cx->blk_sub.cv) = cx->blk_sub.olddepth))
 		SvREFCNT_dec(cx->blk_sub.cv);
@@ -1792,8 +1802,16 @@ PP(pp_goto)
 		    SP = stack_base + items;
 		}
 		else {
+		    SV **newsp;
+		    I32 gimme;
+
 		    stack_sp--;		/* There is no cv arg. */
+		    /* Push a mark for the start of arglist */
+		    PUSHMARK(mark); 
 		    (void)(*CvXSUB(cv))(cv);
+		    /* Pop the current context like a decent sub should */
+		    POPBLOCK(cx, curpm);
+		    /* Do _not_ use PUTBACK, keep the XSUB's return stack! */
 		}
 		LEAVE;
 		return pop_return();
