86457961b8c4aa6a16679a090e8384f792b52baa
Hugo van der Sanden <hv@crypt.org>
1998-10-02 12:01:14 +0100

    squelch undef warnings 	Message-Id: <199810021001.LAA19214@crypt.compulink.co.uk> 	Subject: [PATCH] Re: Apparent bug in Math::BigInt
    p4raw-id: //depot/maint-5.005/perl@2045
    p4raw-integrated: from //depot/perl@2044 'copy in' lib/Math/BigInt.pm
    	(@1659..)

--- lib/Math/BigInt.pm
+++ lib/Math/BigInt.pm
@@ -258,9 +258,9 @@ sub bdiv { #(dividend: num_str, divisor: num_str) return num_str
     else {
 	push(@x, 0);
     }
-    @q = (); ($v2,$v1) = @y[-2,-1];
+    @q = (); ($v2,$v1) = ($y[-2] || 0, $y[-1]);
     while ($#x > $#y) {
-	($u2,$u1,$u0) = @x[-3..-1];
+	($u2,$u1,$u0) = ($x[-3] || 0, $x[-2] || 0, $x[-1]);
 	$q = (($u0 == $v1) ? 99999 : int(($u0*1e5+$u1)/$v1));
 	--$q while ($v2*$q > ($u0*1e5+$u1-$q*$v1)*1e5+$u2);
 	if ($q) {
