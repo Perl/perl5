7e41cbf587d3939982dff81b53af233a1ae2406d
Martin J. Bligh <mbligh@us.ibm.com>
1998-11-10 08:24:26 -0800

    Re: Making Perl work on DYNIX/ptx
    To: jhi@iki.fi
    cc: gbarr@ti.com, gbarr@pobox.com, gsar@umich.edu
    Message-ID: <181999655.910715066@w-186d219.rhe.sequent.com>
    
    p4raw-id: //depot/cfgperl@2229

--- hints/dynixptx.sh
+++ hints/dynixptx.sh
@@ -1,5 +1,9 @@
 # Sequent Dynix/Ptx v. 4 hints
 # Created 1996/03/15 by Brad Howerter, bhower@wgc.woodward.com
+
+# Modified 1998/11/10 by Martin J. Bligh, mbligh@sequent.com
+# to incorporate work done by Kurtis D. Rader & myself.
+
 # Use Configure -Dcc=gcc to use gcc.
 
 # cc wants -G for dynamic loading
@@ -15,10 +19,23 @@ libswanted=`echo $libswanted | sed -e 's/ inet / /'`
 # Configure defaults to usenm='y', which doesn't work very well
 usenm='n'
 
-# Reported by bruce@aps.org ("Bruce P. Schuck") as needed for
-# DYNIX/ptx 4.0 V4.2.1 to get socket i/o to work
-# Not defined by default in case they break other versions.
-# These probably need to be worked into a piece of code that
-# checks for the need for this setting.
-# cppflags='-Wc,+abi-socket -I/usr/local/include'
-# ccflags='-Wc,+abi-socket -I/usr/local/include'
+# for performance, apparently this makes a huge difference (~krader)
+
+d_vfork='define'
+optimize='-Wc,-O3 -W0,-xstring'
+
+case "$osvers" in
+4.4*) # configure doesn't find sockets, as they're in libsocket, not libc
+        d_socket='define'
+        d_oldsock='undef'
+        d_sockpair='define'
+        ;;
+4.2*) # on ptx/TCP 4.2, we can use BSD sockets, but they're not the default.
+        cppflags='-Wc,+bsd-socket'
+        ccflags='-Wc,+bsd-socket'
+        ldflags='-Wc,+bsd-socket'
+        d_socket='define'
+        d_oldsock='undef'
+        d_sockpair='define'
+    ;;
+esac
