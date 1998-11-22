8b285049778d0339755514e2238f7c4dcd647370
Tim Bunce <Tim.Bunce@pobox.com>
1998-11-22 22:28:06 +0000

    Updated Changes file and patchlevel.h for 5.004_05 trial 7.
    p4raw-id: //depot/maint-5.004/perl@2292

--- patchlevel.h
+++ patchlevel.h
@@ -39,7 +39,7 @@
 /* The following line and terminating '};' are read by perlbug.PL. Don't alter. */ 
 static	char	*local_patches[] = {
 	NULL
-	,"MAINT_TRIAL_5 - 5.004_05 maintenance trial 5"
+	,"MAINT_TRIAL_7 - 5.004_05 maintenance trial 7"
 	,NULL
 };
 
