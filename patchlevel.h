c7863474e0508a98a4724f24496c12b71448e1e1
Tim Bunce <Tim.Bunce@pobox.com>
1998-12-03 15:49:32 +0000

    Ship dna.small.gif uuencoded. Update Changes and patchlevel.h for maint trial 8.
    p4raw-id: //depot/maint-5.004/perl@2445

--- patchlevel.h
+++ patchlevel.h
@@ -39,7 +39,7 @@
 /* The following line and terminating '};' are read by perlbug.PL. Don't alter. */ 
 static	char	*local_patches[] = {
 	NULL
-	,"MAINT_TRIAL_7 - 5.004_05 maintenance trial 7"
+	,"MAINT_TRIAL_8 - 5.004_05 maintenance trial 8"
 	,NULL
 };
 
