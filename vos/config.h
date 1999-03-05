2a60582f6f39590675ba35b604f79784dc0ce025
Paul Green <Paul.Green@stratus.com>
1999-03-05 13:08:49 -0500

    : platform: vos -- updates to VOS port of Perl5
    To: perl5-porters@perl.org
    Cc: jhi@iki.fi, Paul_Green@stratus.com
    Message-ID: <1D1A4EF7AD4DD211A80D00A0C9D7DB665A035A@exna1.stratus.com>
    
    p4raw-id: //depot/maint-5.005/perl@3092

--- vos/config.h
+++ vos/config.h
@@ -1,7 +1,7 @@
 /* This is config.h for Stratus VOS.  It was created by hand
    from the distribution copy of config_h.SH.  */
 
-/* Configuration time: February 3, 1999
+/* Configuration time: March 5, 1999
  * Configured by: Paul Green
  * Target system: Stratus VOS
  */
@@ -1192,7 +1192,7 @@
  *	This symbol is defined if the C compiler can cast negative
  *	or large floating point numbers to 32-bit ints.
  */
-#define	CASTI32		/**/
+/*#define	CASTI32		/**/
 
 /* CASTNEGFLOAT:
  *	This symbol is defined if the C compiler can cast negative
@@ -2046,6 +2046,18 @@
  *	instance.
  */
 #define ARCHNAME "vos"	/**/
+
+/* I_MACH_CTHREADS:
+ *    This symbol, if defined, indicates to the C program that it should
+ *    include <mach/cthreads.h>.
+ */
+/*#define	I_MACH_CTHREADS         /**/
+
+/* I_PTHREAD:
+ *    This symbol, if defined, indicates to the C program that it should
+ *    include <pthread.h>.
+ */
+/*#define   I_PTHREAD               /**/
 
 /* HAS_PTHREAD_YIELD:
  *	This symbol, if defined, indicates that the pthread_yield 
