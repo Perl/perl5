fa9667c9ce64fb4ff6898e82914e5ebb1b144a2e
Jarkko Hietaniemi <jhi@iki.fi>
1999-05-25 22:13:39 +0000

    The change #3479 wasn't perfect.
    p4raw-link: @3479 on //depot/perl: 366e399a4ddea371332acd3b46db7692b92ac76con //depot/cfgperl: 015b361d14b10a254511e7c77a5c4078d4a207cd
    
    p4raw-id: //depot/perl@3480

--- hints/aix.sh
+++ hints/aix.sh
@@ -104,10 +104,10 @@ esac
 #                           symbol: boot_$(EXP)  can it be auto-generated?
 case "$osvers" in
 3*) 
-    lddlflags="$lddlflags -H512 -T512 -bhalt:4 -bM:SRE -bI:$(PERL_INC)/perl.exp -bE:$(BASEEXT).exp -e _nostart -c"
+    lddlflags="$lddlflags -H512 -T512 -bhalt:4 -bM:SRE -bI:$(PERL_INC)/perl.exp -bE:$(BASEEXT).exp -e _nostart"
     ;;
 *) 
-    lddlflags="$lddlflags -bhalt:4 -bM:SRE -bI:$(PERL_INC)/perl.exp -bE:$(BASEEXT).exp -b noentry -c"
+    lddlflags="$lddlflags -bhalt:4 -bM:SRE -bI:$(PERL_INC)/perl.exp -bE:$(BASEEXT).exp -b noentry"
     ;;
 esac
 
@@ -224,6 +224,6 @@ EOM
 	;;
 esac
 
-lddlflags="$lddlflags $lddllibc"
-
 EOCBU
+
+lddlflags="$lddlflags $lddllibc"
