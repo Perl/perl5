e69117c59adec2885476b787a53e5ad8435809d0
Jarkko Hietaniemi <jhi@iki.fi>
1999-07-25 11:15:06 +0000

    Change #3732 mistakenly clobbered Glossary.
    p4raw-id: //depot/cfgperl@3733

--- Porting/config.sh
+++ Porting/config.sh
@@ -8,7 +8,7 @@
 
 # Package name      : perl5
 # Source directory  : .
-# Configuration time: Sun Jul 25 13:40:51 EET DST 1999
+# Configuration time: Sun Jul 25 14:08:44 EET DST 1999
 # Configured by     : jhi
 # Target system     : osf1 alpha.hut.fi v4.0 878 alpha 
 
@@ -55,7 +55,7 @@ ccflags='-pthread -std -DLANGUAGE_C'
 ccsymbols='__LANGUAGE_C__=1 _LONGLONG=1 LANGUAGE_C=1 SYSTYPE_BSD=1'
 cf_by='jhi'
 cf_email='yourname@yourhost.yourplace.com'
-cf_time='Sun Jul 25 13:40:51 EET DST 1999'
+cf_time='Sun Jul 25 14:08:44 EET DST 1999'
 chgrp=''
 chmod=''
 chown=''
