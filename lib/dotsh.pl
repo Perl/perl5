a6d7165678aed89f828954d0fcb2e714844ad7d6
Gurusamy Sarathy <gsar@cpan.org>
1999-07-26 05:12:24 +0000

    add disclaimer about perl 4 libraries (modified version of patch suggested by Clinton Pierce <cpierce1@ford.com>)
    p4raw-id: //depot/perl@3759

--- lib/dotsh.pl
+++ lib/dotsh.pl
@@ -1,6 +1,13 @@
 #
 #   @(#)dotsh.pl                                               03/19/94
 #
+# This library is no longer being maintained, and is included for backward
+# compatibility with Perl 4 programs which may require it.
+#
+# In particular, this should not be used as an example of modern Perl
+# programming techniques.
+#
+#
 #   Author: Charles Collins
 #
 #   Description:
