33c0e3ec3c9b4103fd0146ace1e9048f01c2013d
Spider Boardman <spider@orb.nashua.nh.us>
1999-09-23 13:54:53 -0400

    normalize time for strftime() (without the isdst effects of mktime()) using a custom mini_mktime() 	Message-Id: <199909232154.RAA25151@leggy.zk3.dec.com> 	Subject: Re: [ID 19990913.003] Possible bug using POSIX::strftime Digital UNIX Perl 5.005_03
    p4raw-id: //depot/perl@4223

--- ext/POSIX/hints/linux.pl
+++ ext/POSIX/hints/linux.pl
@@ -2,6 +2,4 @@
 # Thanks to Bart Schuller <schuller@Lunatech.com>
 # See Message-ID: <19971009002636.50729@tanglefoot>
 #  XXX A Configure test is needed.
-$self->{CCFLAGS} = $Config{ccflags}
-		 . ' -DHINT_STRFTIME_NEEDS_MKTIME'
-		 . ' -DSTRUCT_TM_HASZONE -DHINT_SC_EXIST' ;
+$self->{CCFLAGS} = $Config{ccflags} . ' -DSTRUCT_TM_HASZONE -DHINT_SC_EXIST' ;
