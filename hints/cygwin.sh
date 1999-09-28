c22e42be2432273165175c9218ee7b6238bfca24
Andy Dougherty <doughera@lafayette.edu>
1999-09-28 08:20:50 -0400

    To: Perl Porters <perl5-porters@perl.org> Subject: [PATCH 5.005_xx] Re: [Config 5.005_03] -DDEBUGGING Date: Tue, 28 Sep 1999 12:20:50 -0400 (EDT) Message-ID: <Pine.SOL.4.10.9909281019360.1890-100000@maxwell.phys.lafayette.edu>
    From: Andy Dougherty <doughera@lafayette.edu>
    To: Perl Porters <perl5-porters@perl.org>
    Subject: [ANOTHER PATCH 5.005_61] Re: [Config 5.005_03] -DDEBUGGING
    Date: Tue, 28 Sep 1999 13:39:49 -0400 (EDT)
    Message-ID: <Pine.SOL.4.10.9909281338180.2012-100000@maxwell.phys.lafayette.edu>
    
    p4raw-id: //depot/cfgperl@4248

--- hints/cygwin.sh
+++ hints/cygwin.sh
@@ -1,6 +1,11 @@
 #! /bin/sh
 # cygwin.sh - hints for building perl using the Cygwin environment for Win32
 #
+# Many of these inflexible settings should be changed to allow command-
+# line overrides and allow for variations in local set-ups.
+# I have made first guesses at some of these, but would welcome 
+# corrections from someone actually using Cygwin.
+#  Andy Dougherty  <doughera@lafayette.edu> Tue Sep 28 12:39:38 EDT 1999
 
 _exe='.exe'
 exe_ext='.exe'
@@ -10,25 +15,31 @@ sharpbang='#!'
 startsh='#!/bin/sh'
 
 archname='cygwin'
-cc='gcc'
+test -z "$cc" && cc='gcc'
 libpth='/usr/i586-cygwin32/lib /usr/lib /usr/local/lib'
 so='dll'
 libs='-lcygwin -lm -lkernel32'
 #optimize='-g'
-ccflags='-DCYGWIN -I/usr/include -I/usr/local/include'
-ldflags='-L/usr/i586-cygwin32/lib -L/usr/lib -L/usr/local/lib'
-usemymalloc='n'
+# Is -I/usr/include *really* needed?
+# Is -I/usr/local/include *really* needed?  I thought gcc always looked there.
+ccflags="$ccflags -DCYGWIN -I/usr/include -I/usr/local/include"
+# Is -L/usr/lib *really* needed?
+ldflags="$ldflags -L/usr/i586-cygwin32/lib -L/usr/lib -L/usr/local/lib"
+test -z "$usemymalloc" && usemymalloc='n'
 dlsrc='dl_cygwin.xs'
 cccdlflags=' '
 ld='ld2'
-lddlflags='-L/usr/local/lib'
+# Is -L/usr/local/lib *really* needed?
+lddlflags="$lddlflags -L/usr/local/lib"
 useshrplib='true'
 libperl='libperl.a'
 dlext='dll'
 dynamic_ext=' '
 
-man1dir=/usr/local/man/man1
-man3dir=/usr/local/man/man3
+# What if they aren't using $prefix=/usr/local ??
+# Why is this needed at all?  Doesn't Configure suggest this?
+test -z "$man1dir" && man1dir=/usr/local/man/man1
+test -z "$man3dir" && man3dir=/usr/local/man/man3
 
 case "$ldlibpthname" in
 '') ldlibpthname=PATH ;;
