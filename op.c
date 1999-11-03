4978d6d9ce16dffa585865d4af8c887a4f77ab53
Ilya Zakharevich <ilya@math.berkeley.edu>
1999-11-02 22:52:48 -0500

    ref to non-lvalue method
    To: perl5-porters@perl.org (Mailing list Perl5)
    Message-Id: <199911030852.DAA06563@monk.mps.ohio-state.edu>
    
    p4raw-id: //depot/cfgperl@4519

--- op.c
+++ op.c
@@ -1281,7 +1281,7 @@ Perl_mod(pTHX_ OP *o, I32 type)
 	}
 	else {				/* lvalue subroutine call */
 	    o->op_private |= OPpLVAL_INTRO;
-	    if (type == OP_GREPSTART || type == OP_ENTERSUB) {
+	    if (type == OP_GREPSTART || type == OP_ENTERSUB || type == OP_REFGEN) {
 		/* Backward compatibility mode: */
 		o->op_private |= OPpENTERSUB_INARGS;
 		break;
