1d7cb664dc49858d069c4ad7e05c059b1003a3cd
Gurusamy Sarathy <gsar@cpan.org>
2000-03-03 02:58:49 +0000

    File::Spec bugs (spotted by Hack Kampbjorn <hack@hackdata.com>)
    p4raw-id: //depot/perl@5472

--- lib/File/Spec/Win32.pm
+++ lib/File/Spec/Win32.pm
@@ -341,8 +341,8 @@ sub abs2rel {
 
 Converts a relative path to an absolute path. 
 
-    $abs_path = $File::Spec->rel2abs( $destination ) ;
-    $abs_path = $File::Spec->rel2abs( $destination, $base ) ;
+    $abs_path = File::Spec->rel2abs( $destination ) ;
+    $abs_path = File::Spec->rel2abs( $destination, $base ) ;
 
 If $base is not present or '', then L<cwd()> is used. If $base is relative, 
 then it is converted to absolute form using L</rel2abs()>. This means that it
@@ -368,11 +368,11 @@ sub rel2abs($;$;) {
 
     if ( ! $self->file_name_is_absolute( $path ) ) {
 
-        if ( ! $self->file_name_is_absolute( $base ) ) {
-            $base = $self->rel2abs( $base ) ;
-        }
-        elsif ( !defined( $base ) || $base eq '' ) {
+        if ( !defined( $base ) || $base eq '' ) {
             $base = cwd() ;
+        }
+        elsif ( ! $self->file_name_is_absolute( $base ) ) {
+            $base = $self->rel2abs( $base ) ;
         }
         else {
             $base = $self->canonpath( $base ) ;
