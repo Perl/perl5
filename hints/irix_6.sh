5ec7303013a96b8a72e717d9424dfb663b238cd2
Jarkko Hietaniemi <jhi@iki.fi>
2000-03-10 14:22:24 +0000

    Do not use Perl's malloc in IRIX.
    p4raw-id: //depot/cfgperl@5637

--- hints/irix_6.sh
+++ hints/irix_6.sh
@@ -142,8 +142,17 @@ case "$cc" in
 	ld=$cc
 
 	# perl's malloc can return improperly aligned buffer
-	# usemymalloc='undef'
-malloc_cflags='ccflags="-DSTRICT_ALIGNMENT $ccflags"'
+	# which (under 5.6.0RC1) leads into really bizarre bus errors
+	# and freak test failures (lib/safe1 #18, for example),
+	# even more so with -Duse64bitall: for example lib/io_linenumtb.
+	# fails under the harness but succeeds when run separately,
+	# under make test pragma/warnings #98 fails, and lib/io_dir
+	# apparently coredumps (the last two don't happen under
+    	# the harness.  Helmut Jarausch is seeing bus errors from
+        # miniperl, as was Scott Henry with snapshots from just before
+	# the RC1. --jhi
+	usemymalloc='undef'
+#malloc_cflags='ccflags="-DSTRICT_ALIGNMENT $ccflags"'
 
 	nm_opt='-p'
 	nm_so_opt='-p'
