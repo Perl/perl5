43bf06b71cccf28eee4a8747add26d531e3cf2e6
Jarkko Hietaniemi <jhi@iki.fi>
2000-03-13 05:03:37 +0000

    hints nibbling continues
    p4raw-id: //depot/cfgperl@5698

--- hints/hpux.sh
+++ hints/hpux.sh
@@ -179,30 +179,26 @@ EOM
 *LP64*|*PA-RISC2.0*) ;;
 *) xxx=/no/64-bit$xxx ;;
 esac'
-    case "$ccisgcc" in
-    "$define") ld=$cc ;;
-    *) ld=/usr/bin/ld ;;
-    esac
+    if test -n "$ccisgcc" -o -n "$gccversion"; then
+	ld="$cc"
+    else	
+	ld=/usr/bin/ld
+    fi
     ar=/usr/bin/ar
     full_ar=$ar
 
-    case "$ccisgcc" in
-    "$define") ;;
-    *) # The strict ANSI mode (-Aa) doesn't like the LL suffixes.
+    if test -z "$ccisgcc" -a -z "$gccversion"; then
+       # The strict ANSI mode (-Aa) doesn't like the LL suffixes.
        ccflags=`echo " $ccflags "|sed 's@ -Aa @ @g'`
        case "$ccflags" in
        *-Ae*) ;;
        *) ccflags="$ccflags -Ae" ;;
        esac
-       ;;
-    esac
+    fi
 
     set `echo " $libswanted " | sed -e 's@ dl @ @'`
     libswanted="$*"
 
-    case "$ccisgcc" in
-    "$define") ;;
-    esac
     ;;
 esac
 
@@ -211,7 +207,6 @@ case "$ccisgcc" in
 "$define") test -d /lib/pa1.1 && ccflags="$ccflags -L/lib/pa1.1" ;;
 esac
     
-
 case "$ccisgcc" in
 "$define") ;;
 *)  case "`getconf KERNEL_BITS 2>/dev/null`" in
@@ -392,16 +387,14 @@ case "$uselargefiles" in
 	# but we cheat for now.
 	ccflags="$ccflags -D_LARGEFILE_SOURCE -D_FILE_OFFSET_BITS=64"
 
-        case "$ccisgcc" in
-        "$define") ;;
-        *) # The strict ANSI mode (-Aa) doesn't like large files.
+        if test -z "$ccisgcc" -a -z "$gccversion"; then
+           # The strict ANSI mode (-Aa) doesn't like large files.
            ccflags=`echo " $ccflags "|sed 's@ -Aa @ @g'`
            case "$ccflags" in
            *-Ae*) ;;
            *) ccflags="$ccflags -Ae" ;;
            esac
-	   ;;
-	esac
+	fi
 
 	;;
 esac
