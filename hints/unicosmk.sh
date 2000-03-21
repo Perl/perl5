2220a07647482cb163ce6787c70ef1e59ae78241
Jarkko Hietaniemi <jhi@iki.fi>
2000-03-21 21:38:20 +0000

    The SysV shm*() are in libc but unimplemented.
    p4raw-id: //depot/cfgperl@5865

--- hints/unicosmk.sh
+++ hints/unicosmk.sh
@@ -16,3 +16,12 @@ case ":$PATH:" in
     esac
     ;;
 esac
+# As of UNICOS/mk 2.0.5.24 the shm* are in libc but unimplemented
+# (an attempt to use them causes a runtime error)
+# XXX Configure probe for really functional shm*() is needed XXX
+if test "$d_shm" = ""; then
+    test "$d_shmctl" = "" && d_shmctl="$undef"
+    test "$d_shmget" = "" && d_shmget="$undef"
+    d_shm="$undef"
+fi
+
