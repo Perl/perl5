a4f5cc89eed636ba46185bdb30ee0a36fa506dd8
Jarkko Hietaniemi <jhi@iki.fi>
2000-07-28 04:15:39 +0000

    Back out #6454, doesn't seem to work. (Reason: that patch was for perl 5.6.0, not perl-current) (A version of the patch for perl-current came in later as #6466)
    p4raw-id: //depot/perl@6458

--- toke.c
+++ toke.c
@@ -5146,6 +5146,7 @@ Perl_keyword(pTHX_ register char *d, I32 len)
 	}
 	break;
     case 'E':
+	if (strEQ(d,"EQ")) { deprecate(d);	return -KEY_eq;}
 	if (strEQ(d,"END"))			return KEY_END;
 	break;
     case 'e':
@@ -5209,6 +5210,12 @@ Perl_keyword(pTHX_ register char *d, I32 len)
 	case 8:
 	    if (strEQ(d,"formline"))		return -KEY_formline;
 	    break;
+	}
+	break;
+    case 'G':
+	if (len == 2) {
+	    if (strEQ(d,"GT")) { deprecate(d);	return -KEY_gt;}
+	    if (strEQ(d,"GE")) { deprecate(d);	return -KEY_ge;}
 	}
 	break;
     case 'g':
@@ -5310,6 +5317,12 @@ Perl_keyword(pTHX_ register char *d, I32 len)
 	    if (strEQ(d,"kill"))		return -KEY_kill;
 	}
 	break;
+    case 'L':
+	if (len == 2) {
+	    if (strEQ(d,"LT")) { deprecate(d);	return -KEY_lt;}
+	    if (strEQ(d,"LE")) { deprecate(d);	return -KEY_le;}
+	}
+	break;
     case 'l':
 	switch (len) {
 	case 2:
@@ -5360,6 +5373,9 @@ Perl_keyword(pTHX_ register char *d, I32 len)
 	    if (strEQ(d,"msgsnd"))		return -KEY_msgsnd;
 	    break;
 	}
+	break;
+    case 'N':
+	if (strEQ(d,"NE")) { deprecate(d);	return -KEY_ne;}
 	break;
     case 'n':
 	if (strEQ(d,"next"))			return KEY_next;
