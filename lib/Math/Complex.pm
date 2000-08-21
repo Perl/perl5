ffb4440d02d1e8964757828cffb3de1e6f18ad0b
Jarkko Hietaniemi <jhi@iki.fi>
2000-08-21 17:51:29 +0000

    Unicos/mk requires elaborate paranoia.
    p4raw-id: //depot/perl@6750

--- lib/Math/Complex.pm
+++ lib/Math/Complex.pm
@@ -12,10 +12,13 @@ $VERSION = "1.30";
 our($VERSION, @ISA, @EXPORT, %EXPORT_TAGS, $Inf);
 
 BEGIN {
-    my $e = $!;
-    $Inf = CORE::exp(CORE::exp(30)); # We do want an arithmetic overflow.
-    $! = $e; # Clear ERANGE.
-    undef $Inf unless $Inf =~ /^inf(?:inity)?$/i; # Inf INF inf Infinity
+    unless ($^O eq 'unicosmk') {
+        my $e = $!;
+	# We do want an arithmetic overflow.
+        eval '$Inf = CORE::exp(CORE::exp(30))';
+        $! = $e; # Clear ERANGE.
+        undef $Inf unless $Inf =~ /^inf(?:inity)?$/i; # Inf INF inf Infinity
+    }
     $Inf = "Inf" if !defined $Inf || !($Inf > 0); # Desperation.
 }
 
