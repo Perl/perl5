50162ea74bd1f162944c12724f6737c44fef61d3
Gurusamy Sarathy <gsar@cpan.org>
2000-10-16 02:49:36 +0000

    integrate change#5924 from mainline
    avoid using uninitialized memory in require version check
    
    p4raw-link: @5924 on //depot/perl: 4305d8abee2315e02cb395dec9deede4e7f9f179
    
    p4raw-id: //depot/maint-5.6/perl@7256
    p4raw-integrated: from //depot/perl@5924 'copy in' universal.c
    	(@5912..) pp_ctl.c (@5914..)

--- universal.c
+++ universal.c
@@ -262,7 +262,7 @@ XS(XS_UNIVERSAL_VERSION)
 		    break;
 	    }
 	    if (len) {
-		if (SvNIOKp(req) && SvPOK(req)) {
+		if (SvNOK(req) && SvPOK(req)) {
 		    /* they said C<use Foo v1.2.3> and $Foo::VERSION
 		     * doesn't look like a float: do string compare */
 		    if (sv_cmp(req,sv) == 1) {
@@ -283,7 +283,7 @@ XS(XS_UNIVERSAL_VERSION)
 	/* if we get here, we're looking for a numeric comparison,
 	 * so force the required version into a float, even if they
 	 * said C<use Foo v1.2.3> */
-	if (SvNIOKp(req) && SvPOK(req)) {
+	if (SvNOK(req) && SvPOK(req)) {
 	    NV n = SvNV(req);
 	    req = sv_newmortal();
 	    sv_setnv(req, n);
