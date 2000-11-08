8a4e5b40dba3387613897ea9c275824694f125bc
David Dyck <david.dyck@fluke.com>
2000-11-08 10:36:26 -0800

    patch to detect stat followed by lstat(_): [ID 20001106.006] find2perl generated scripts issue new lstat()    on filehandle _ warning Message-ID: <Pine.LNX.4.30.0011081817490.177-100000@dd.tc.fluke.com>
    p4raw-id: //depot/perl@7620

--- pp_sys.c
+++ pp_sys.c
@@ -2557,9 +2557,15 @@ PP(pp_stat)
 
     if (PL_op->op_flags & OPf_REF) {
 	gv = cGVOP_gv;
-	if (PL_op->op_type == OP_LSTAT && ckWARN(WARN_IO) && gv != PL_defgv)
-	    Perl_warner(aTHX_ WARN_IO,
+	if (PL_op->op_type == OP_LSTAT) {
+	    if (PL_laststype != OP_LSTAT)
+		Perl_croak(aTHX_ "The stat preceding lstat() wasn't an lstat");
+	    if (ckWARN(WARN_IO) && gv != PL_defgv)
+		Perl_warner(aTHX_ WARN_IO,
 			"lstat() on filehandle %s", GvENAME(gv));
+		/* Perl_my_lstat (-l) croak's on filehandle, why warn here? */
+	}
+
       do_fstat:
 	if (gv != PL_defgv) {
 	    PL_laststype = OP_STAT;
