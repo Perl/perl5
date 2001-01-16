f6e681c80d37390847118575795f2cc6a2e69c87
Mark-Jason Dominus <mjd@plover.com>
2001-01-16 14:43:18 +0000

    regcomp.c old feature removal Message-ID: <20010116144318.7140.qmail@plover.com>
    p4raw-id: //depot/perl@8455

--- regcomp.c
+++ regcomp.c
@@ -154,11 +154,6 @@ typedef struct RExC_state_t {
 #define	ISMULT1(c)	((c) == '*' || (c) == '+' || (c) == '?')
 #define	ISMULT2(s)	((*s) == '*' || (*s) == '+' || (*s) == '?' || \
 	((*s) == '{' && regcurly(s)))
-#ifdef atarist
-#define	PERL_META	"^$.[()|?+*\\"
-#else
-#define	META	"^$.[()|?+*\\"
-#endif
 
 #ifdef SPSTART
 #undef SPSTART		/* dratted cpp namespace... */
