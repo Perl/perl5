5aa723418959f04967a963333655305248442a6e
Andy Dougherty <doughera@lafayette.edu>
2001-02-05 05:19:29 -0500

    Re: [PATCH embed.pl] Forgot to add ./lib to @INC for File::Glob Message-ID: <Pine.SOL.4.10.10102051013380.11242-100000@maxwell.phys.lafayette.edu>
    p4raw-id: //depot/perl@8742

--- embed.pl
+++ embed.pl
@@ -1,5 +1,9 @@
 #!/usr/bin/perl -w
 
+BEGIN {
+    unshift @INC, "./lib";
+}
+
 require 5.003;	# keep this compatible, an old perl is all we may have before
                 # we build the new one
 
