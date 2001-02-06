aa832f4288d752b7d495a7b95b97bfa9a50532a8
Vadim Konovalov <vkonovalov@lucent.com>
2001-02-06 12:23:20 +0300

    RE: some win32 fixes Message-ID: <402099F49BEED211999700805FC7359F9CAF49@ru0028exch01.spb.lucent.com>
    p4raw-id: //depot/perl@8706

--- toke.c
+++ toke.c
@@ -4170,10 +4170,6 @@ Perl_yylex(pTHX)
 			(void)PerlIO_seek(PL_rsfp, 0L, 0);
 		    }
 		    if (PerlLIO_setmode(PerlIO_fileno(PL_rsfp), O_TEXT) != -1) {
-#if defined(__BORLANDC__)
-			/* XXX see note in do_binmode() */
-			((FILE*)PL_rsfp)->flags |= _F_BIN;
-#endif
 			if (loc > 0)
 			    PerlIO_seek(PL_rsfp, loc, 0);
 		    }
