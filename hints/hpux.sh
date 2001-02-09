c7d9b0963c05530262614fc3d2388e2658739e34
Jarkko Hietaniemi <jhi@iki.fi>
2001-02-09 16:46:54 +0000

    Integrate changes #8687,8736 from maintperl.
    HP-UX thread patches from Merijn H. Brand.
    
    Latin nit from Philip Newton.
    
    p4raw-link: @8736 on //depot/maint-5.6/perl: 46c0e3f74b24cd7d4cacf5a7af8403364b68a206
    p4raw-link: @8687 on //depot/maint-5.6/perl: 55551d482634089ae7d12e15564ce5358a4701f4
    
    p4raw-id: //depot/perl@8737
    p4raw-integrated: from //depot/maint-5.6/perl@8735 'copy in'
    	hints/hpux.sh (@8609..) README.hpux (@8687..)

--- hints/hpux.sh
+++ hints/hpux.sh
@@ -23,6 +23,11 @@
 # HP-UX 10 pthreads hints: Matthew T Harden <mthard@mthard1.monsanto.com>
 # From: Dominic Dunlop <domo@computer.org>
 # Abort and offer advice if bundled (non-ANSI) C compiler selected
+# From: H.Merijn Brand <h.m.brand@hccnet.nl>
+# ccversion detection
+# perl/64/HP-UX wants libdb-3.0 to be shared ELF 64
+# generic pthread support detection for PTH package
+
 
 # This version: March 8, 2000
 # Current maintainer: Jeff Okamoto <okamoto@corp.hp.com>
@@ -334,31 +339,50 @@ EOM
         fi
         case "$xxOsRevMajor" in
         10)
-            # Under 10.X, a threaded perl can be built, but it needs
-            # libcma and OLD_PTHREADS_API.  Also <pthread.h> needs to
-            # be #included before any other includes (in perl.h)
-            if [ ! -f /usr/include/pthread.h -o ! -f /usr/lib/libcma.sl ]; then
+            # Under 10.X, a threaded perl can be built
+            if [ -f /usr/include/pthread.h ]; then
+		if [ -f /usr/lib/libcma.sl ]; then
+		    # DCE (from Core OS CD) is installed
+
+		    # It needs # libcma and OLD_PTHREADS_API. Also <pthread.h>
+		    # needs to be #included before any other includes
+		    # (in perl.h)
+
+		    # HP-UX 10.X uses the old pthreads API
+		    d_oldpthreads="$define"
+
+		    # include libcma before all the others
+		    libswanted="cma $libswanted"
+
+		    # tell perl.h to include <pthread.h> before other include files
+		    ccflags="$ccflags -DPTHREAD_H_FIRST"
+
+		    # CMA redefines select to cma_select, and cma_select expects int *
+		    # instead of fd_set * (just like 9.X)
+		    selecttype='int *'
+
+		elif [ -f /usr/lib/libpthread.sl ]; then
+		    # PTH package is installed
+		    libswanted="pthread $libswanted"
+		else
+		    libswanted="no_threads_available"
+		    fi
+	    else
+		libswanted="no_threads_available"
+		fi
+
+            if [ $libswanted = "no_threads_available" ]; then
                 cat <<EOM >&4
 In HP-UX 10.X for POSIX threads you need both of the files
-/usr/include/pthread.h and /usr/lib/libcma.sl.
-Either you must install the CMA package or you must upgrade to HP-UX 11.
+/usr/include/pthread.h and either /usr/lib/libcma.sl or /usr/lib/libpthread.sl.
+Either you must upgrade to HP-UX 11 or install a posix thread library:
+ DCE-CoreTools from HP-UX 10.20 Hardware Extensions 3.0 CD (B3920-13941) or
+ PTH package from i.e. http://hpux.tn.tudelft.nl/hppd/hpux/alpha.html
 Cannot continue, aborting.
 EOM
      	        exit 1
-            fi
+		fi
 
-            # HP-UX 10.X uses the old pthreads API
-            d_oldpthreads="$define"
-
-            # include libcma before all the others
-            libswanted="cma $libswanted"
-
-            # tell perl.h to include <pthread.h> before other include files
-            ccflags="$ccflags -DPTHREAD_H_FIRST"
-
-            # CMA redefines select to cma_select, and cma_select expects int *
-            # instead of fd_set * (just like 9.X)
-            selecttype='int *'
             ;;
         11 | 12) # 12 may want upping the _POSIX_C_SOURCE datestamp...
             ccflags=" -D_POSIX_C_SOURCE=199506L $ccflags"
