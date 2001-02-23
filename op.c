98b9765ed69ac4b9971aa26bf6cc11d849cb68be
Jarkko Hietaniemi <jhi@iki.fi>
2001-02-23 19:31:38 +0000

    Retract #8902 for now, requested by Charles Lane, the correct fix will be something else.
    p4raw-id: //depot/perl@8914

--- op.c
+++ op.c
@@ -4154,6 +4154,7 @@ Perl_cv_undef(pTHX_ CV *cv)
 	LEAVE;
     }
     SvPOK_off((SV*)cv);		/* forget prototype */
+    CvFLAGS(cv) = 0;
     CvGV(cv) = Nullgv;
     SvREFCNT_dec(CvOUTSIDE(cv));
     CvOUTSIDE(cv) = Nullcv;
@@ -4182,7 +4183,6 @@ Perl_cv_undef(pTHX_ CV *cv)
 	}
 	CvPADLIST(cv) = Nullav;
     }
-    CvFLAGS(cv) = 0;
 }
 
 #ifdef DEBUG_CLOSURES
