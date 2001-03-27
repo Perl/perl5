b3d30bf7e271096b5b02c85f1129c4e49c36a709
Nick Ing-Simmons <nik@tiuk.ti.com>
2001-03-27 21:18:58 +0000

    Outline docs for PerlIO and PerlIO::Scalar
    p4raw-id: //depot/perlio@9395

--- ext/PerlIO/Scalar/Scalar.pm
+++ ext/PerlIO/Scalar/Scalar.pm
@@ -4,3 +4,23 @@ use XSLoader ();
 XSLoader::load 'PerlIO::Scalar';
 1;
 __END__
+
+=head1 NAME
+
+PerlIO::Scalar - support module for in-memory IO.
+
+=head1 SYNOPSIS
+
+   open($fh,"<...",\$scalar);
+
+=head1 DESCRIPTION
+
+C<PerlIO::Scalar> only exists to use XSLoader to load C code that provides
+support for treating a scalar as an "in memory" file.
+
+All normal file operations can be performed on the handle. The scalar is considered
+a stream of bytes. Currently fileno($fh) returns C<undef>.
+
+=cut
+
+
--- lib/PerlIO.pm
+++ lib/PerlIO.pm
@@ -24,3 +24,30 @@ sub import
 
 1;
 __END__
+
+=head1 NAME
+
+PerlIO - On demand loader for PerlIO::* name space
+
+=head1 SYNOPSIS
+
+  open($fh,">:foo",...)
+
+=head1 DESCRIPTION
+
+When an undefined layer 'foo' is encountered in an C<open> or C<binmode> layer
+specification then C code performs the equivalent of:
+
+  use PerlIO 'foo';
+
+The perl code in PerlIO.pm then attempts to locate a layer by doing
+
+  require PerlIO::foo;
+
+Otherwise the C<PerlIO> package is a place holder for additional PerLIO related
+functions.
+
+
+=cut
+
+
