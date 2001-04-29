f4315cfa2587f64466f29dc6226481e910d50166
Abigail <abigail@abigail.be>
2001-04-29 05:14:25 +0200

    Doc fixes. Message-ID: <20010429011425.24503.qmail@foad.org>
    p4raw-id: //depot/perl@9902

--- ext/IO/lib/IO/Seekable.pm
+++ ext/IO/lib/IO/Seekable.pm
@@ -41,7 +41,7 @@ corresponding built-in functions:
 
 =over 4
 
-=item $io->setpos ( POS, WHENCE )
+=item $io->seek ( POS, WHENCE )
 
 Seek the IO::File to position POS, relative to WHENCE:
 
@@ -55,7 +55,7 @@ POS is absolute position. (Seek relative to the start of the file)
 
 POS is an offset from the current position. (Seek relative to current)
 
-=item WHENCE=1 (SEEK_END)
+=item WHENCE=2 (SEEK_END)
 
 POS is an offset from the end of the file. (Seek relative to end)
 
