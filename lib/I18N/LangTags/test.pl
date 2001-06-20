21aeefd57a5747f2eae308d425d77bf17cddfc0c
Jarkko Hietaniemi <jhi@iki.fi>
2001-06-20 18:02:18 +0000

    Update to I18N::LangTags 0.24, from Sean Burke.
    p4raw-id: //depot/perl@10759

--- lib/I18N/LangTags/test.pl
+++ lib/I18N/LangTags/test.pl
@@ -1,21 +1,14 @@
-BEGIN {
-    chdir 't' if -d 't';
-    @INC = '../lib';
-}
+# Before `make install' is performed this script should be runnable with
+# `make test'. After `make install' it should work as `perl test.pl'
 
 ######################### We start with some black magic to print on failure.
 require 5;
-
+ # Time-stamp: "2001-06-20 01:43:31 MDT"
 use strict;
 use Test;
-BEGIN { plan tests => 23 };
+BEGIN { plan tests => 46 };
 BEGIN { ok 1 }
-use I18N::LangTags qw(is_language_tag same_language_tag
-		      extract_language_tags super_languages
-		      similarity_language_tag is_dialect_of
-		      locale2language_tag alternate_language_tags
-		      encode_language_tag
-		     );
+use I18N::LangTags (':ALL');
 
 ok !is_language_tag('');
 ok  is_language_tag('fr');
@@ -41,5 +34,44 @@ ok 1 == similarity_language_tag('en-ca', 'en-us');
 ok 2 == similarity_language_tag('en-us-southern', 'en-us-western');
 ok 2 == similarity_language_tag('en-us-southern', 'en-us');
 
-# print "So there!\n";
+ok grep $_ eq 'hi', panic_languages('kok');
+ok grep $_ eq 'en', panic_languages('x-woozle-wuzzle');
+ok ! grep $_ eq 'mr', panic_languages('it');
+ok grep $_ eq 'es', panic_languages('it');
+ok grep $_ eq 'it', panic_languages('es');
+
+
+print "# Now the ::List tests...\n";
+use I18N::LangTags::List;
+foreach my $lt (qw(
+ en
+ en-us
+ en-kr
+ el
+ elx
+ i-mingo
+ i-mingo-tom
+ x-mingo-tom
+ it
+ it-it
+ it-IT
+ it-FR
+ yi
+ ji
+ cre-syllabic
+ cre-syllabic-western
+ cre-western
+ cre-latin
+)) {
+  my $name = I18N::LangTags::List::name($lt);
+  if($name) {
+    ok(1);
+    print "#        $lt -> $name\n";
+  } else {
+    ok(0);
+    print "#        Failed lookup on $lt\n";
+  }
+}
+
+print "# So there!\n";
 
