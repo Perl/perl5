9219c8ded06600040fa6d862a60325b5afc73df4
Jarkko Hietaniemi <jhi@iki.fi>
2001-07-02 13:36:58 +0000

    Netware tweaks from Guruprasad.
    p4raw-id: //depot/perl@11090

--- NetWare/iperlhost.h
+++ NetWare/iperlhost.h
@@ -8,11 +8,11 @@
  */
 
 /*
- * FILENAME		:	iperlhost.h
- * DESCRIPTION	:	IPerlHost class file.
- * Author		:	SGP
- * Date			:	January 2001.
- *
+ * FILENAME     :   iperlhost.h
+ * DESCRIPTION  :   IPerlHost class file.
+ * Author       :   SGP
+ * Date Created :   January 2001.
+ * Date Modified:   July 2nd 2001.
  */
 
 
@@ -34,7 +34,8 @@ public:
 	virtual int PerlParse(PerlInterpreter *my_perl,int argc, char** argv, char** env) = 0;
 	virtual int PerlRun(PerlInterpreter *my_perl) = 0;
 	virtual void PerlDestroy(PerlInterpreter *my_perl) = 0;
-
+	virtual bool RegisterWithThreadTable(void)=0;
+	virtual bool UnregisterWithThreadTable(void)=0;
 };
 
 extern "C" IPerlHost* AllocStdPerl();
--- perlsdio.h
+++ perlsdio.h
@@ -1,4 +1,9 @@
 #ifdef PERLIO_IS_STDIO
+
+#ifdef NETWARE
+	#include "nwstdio.h"
+#else
+
 /*
  * This file #define-s the PerlIO_xxx abstraction onto stdio functions.
  * Make this as close to original stdio as possible.
@@ -136,4 +141,5 @@
 #define PerlIO_get_bufsiz(f)		(abort(),0)
 #endif
 
+#endif	/* NETWARE */
 #endif /* PERLIO_IS_STDIO */
