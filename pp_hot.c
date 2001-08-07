f5f1d18ea865e75add13fe3664f46a1d86cc28c5
Abhijit Menon-Sen <ams@wiw.org>
2001-08-07 14:40:54 +0530

    Re: [ID 20010806.006] Can't read coderef from tied variable Message-ID: <20010807091054.H526@lustre.dyn.wiw.org>
    p4raw-id: //depot/perl@11602

--- pp_hot.c
+++ pp_hot.c
@@ -2444,6 +2444,8 @@ PP(pp_entersub)
 	    }
 	    if (SvGMAGICAL(sv)) {
 		mg_get(sv);
+		if (SvROK(sv))
+		    goto got_rv;
 		sym = SvPOKp(sv) ? SvPVX(sv) : Nullch;
 	    }
 	    else
@@ -2455,6 +2457,7 @@ PP(pp_entersub)
 	    cv = get_cv(sym, TRUE);
 	    break;
 	}
+  got_rv:
 	{
 	    SV **sp = &sv;		/* Used in tryAMAGICunDEREF macro. */
 	    tryAMAGICunDEREF(to_cv);
