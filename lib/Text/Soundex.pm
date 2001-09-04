7e6e257ff69fdd63bc59933314de6190d0b80d46
Ian Phillipps <Ian.Phillipps@iname.com>
2001-09-04 15:16:59 +0100

    AUTHORS updates and
    Subject: [PATCH] Just some preening :-)        
    Message-ID: <20010904141659.A10129@homer.diplex.co.uk>
    
    p4raw-id: //depot/perl@11857

--- lib/Text/Soundex.pm
+++ lib/Text/Soundex.pm
@@ -11,7 +11,7 @@ $VERSION = '1.01';
 #
 # Implementation of soundex algorithm as described by Knuth in volume
 # 3 of The Art of Computer Programming, with ideas stolen from Ian
-# Phillips <ian@pipex.net>.
+# Phillipps <ian@pipex.net>.
 #
 # Mike Stok <Mike.Stok@meiko.concord.ma.us>, 2 March 1994.
 #
@@ -146,5 +146,5 @@ of C<H416>.
 =head1 AUTHOR
 
 This code was implemented by Mike Stok (C<stok@cybercom.net>) from the 
-description given by Knuth.  Ian Phillips (C<ian@pipex.net>) and Rich Pinder 
+description given by Knuth.  Ian Phillipps (C<ian@pipex.net>) and Rich Pinder 
 (C<rpinder@hsc.usc.edu>) supplied ideas and spotted mistakes.
