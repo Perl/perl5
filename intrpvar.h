913e641e6ea0cf4a7d46aec1372ac6f83e382dbb
Jarkko Hietaniemi <jhi@iki.fi>
2001-09-20 12:16:50 +0000

    The sharedsv_space* are variables, not code.  Duh.
    p4raw-id: //depot/perl@12091

--- intrpvar.h
+++ intrpvar.h
@@ -483,10 +483,12 @@ PERLVAR(Iregex_padav,   AV*)		/* All regex objects */
 PERLVAR(Ireentrant_buffer, REBUF*)	/* here we store the _r buffers */
 #endif
 
+PERLVAR(sharedsv_space,		PerlInterpreter*)
+PERLVAR(sharesv_space_mutex,	perl_mutex)
+
 #endif
 
 PERLVAR(Isavebegin,     bool)	/* save BEGINs for compiler	*/
-
 
 PERLVAR(Icustom_op_names, HV*)  /* Names of user defined ops */
 PERLVAR(Icustom_op_descs, HV*)  /* Descriptions of user defined ops */
