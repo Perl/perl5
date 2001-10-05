276fd468aa555e31b01c2ef79370cbc782cc03cf
David Dyck <david.dyck@fluke.com>
2001-10-05 09:04:43 -0700

    patch to ext/IO/lib/IO/Socket/INET.pm fixes Bug ID (20010803.022) Date: Fri, 5 Oct 2001 16:04:43 -0700 (PDT) Message-ID: <Pine.LNX.4.33.0110051548340.9856-100000@dd.tc.fluke.com>
    Subject: Re: patch to ext/IO/lib/IO/Socket/INET.pm fixes Bug ID (20010803.022)
    From: David Dyck <dcd@tc.fluke.com>
    Date: Fri, 5 Oct 2001 17:27:46 -0700 (PDT)
    Message-ID: <Pine.LNX.4.33.0110051714350.14248-100000@dd.tc.fluke.com>
    
    p4raw-id: //depot/perl@12343

--- ext/IO/lib/IO/Socket/INET.pm
+++ ext/IO/lib/IO/Socket/INET.pm
@@ -52,9 +52,8 @@ sub _sock_info {
   }
 
   if(defined $port) {
-    $port =~ s,\((\d+)\)$,,;
 
-    my $defport = $1 || undef;
+    my $defport = ($port =~ s,\((\d+)\)$,,)[0];
     my $pnum = ($port =~ m,^(\d+)$,)[0];
 
     @serv = getservbyname($port, $proto[0] || "")
