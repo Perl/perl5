d02ee443a95f37e809897c7b362de209cac4b522
Jarkko Hietaniemi <jhi@iki.fi>
2001-11-02 23:20:03 +0000

    Definitions for the Unicode Newline Guidelines.
    p4raw-id: //depot/perl@12825

--- perl.h
+++ perl.h
@@ -4012,4 +4012,24 @@ extern void moncontrol(int);
 #include "wince.h"
 #endif
 
+/* ISO 6429 NEL - C1 control NExt Line */
+/* See http://www.unicode.org/unicode/reports/tr13/ */
+#ifdef EBCDIC	/* In EBCDIC NEL is just an alias for LF */
+#   if '^' == 95	/* CP 1047: MVS OpenEdition - OS/390 - z/OS */
+#       define NEXT_LINE_CHAR	0x15
+#   else		/* CDRA */
+#       define NEXT_LINE_CHAR	0x25
+#   endif
+#else
+#   define NEXT_LINE_CHAR	0x85
+#endif
+
+/* The UTF-8 bytes of the Unicode LS and PS, U+2028 and U+2029 */
+#define UNICODE_LINE_SEPA_0	0xE2
+#define UNICODE_LINE_SEPA_1	0x80
+#define UNICODE_LINE_SEPA_2	0xA8
+#define UNICODE_PARA_SEPA_0	0xE2
+#define UNICODE_PARA_SEPA_1	0x80
+#define UNICODE_PARA_SEPA_2	0xA9
+
 #endif /* Include guard */
