9c5e059618c67712016740089dc5dbbbd3724712
Michael G. Schwern <schwern@pobox.com>
2001-12-11 18:17:32 -0500

    Old RCS $Date$ tag Message-ID: <20011212041732.GS17250@blackrider>
    p4raw-id: //depot/perl@13675

--- lib/ExtUtils/Install.pm
+++ lib/ExtUtils/Install.pm
@@ -2,8 +2,7 @@ package ExtUtils::Install;
 
 use 5.006_001;
 our(@ISA, @EXPORT, $VERSION);
-$VERSION = substr q$Revision: 1.29 $, 10;
-# $Date: 1998/01/25 07:08:24 $
+$VERSION = 1.29
 
 use Exporter;
 use Carp ();
