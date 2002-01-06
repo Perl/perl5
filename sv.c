25596c822ec76c369036d51b5e91fcdfb4acdb05
Jarkko Hietaniemi <jhi@iki.fi>
2002-01-06 20:38:34 +0000

    PL_debug_pad wasn't initialized to zeroes which broke ithreads + DEBUGGING.
    p4raw-id: //depot/perl@14108

--- sv.c
+++ sv.c
@@ -9682,7 +9682,7 @@ perl_clone_using(PerlInterpreter *proto_perl, UV flags,
     PL_savestack = 0;
     PL_retstack = 0;
     PL_sig_pending = 0;
-    Zero(&PL_debug_pad, 1, struct perl_debug_pad); 
+    Zero(&PL_debug_pad, 1, struct perl_debug_pad);
 #  else	/* !DEBUGGING */
     Zero(my_perl, 1, PerlInterpreter);
 #  endif	/* DEBUGGING */
@@ -9712,6 +9712,7 @@ perl_clone_using(PerlInterpreter *proto_perl, UV flags,
     PL_savestack = 0;
     PL_retstack = 0;
     PL_sig_pending = 0;
+    Zero(&PL_debug_pad, 1, struct perl_debug_pad);
 #    else	/* !DEBUGGING */
     Zero(my_perl, 1, PerlInterpreter);
 #    endif	/* DEBUGGING */
