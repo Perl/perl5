0656a6931f660a4c8c0682d39550d07b7083e8f7
Chris Nandor <pudge@pobox.com>
2002-01-23 21:00:29 +0000

    Don't crash on Help for long text (bug #506672)
    p4raw-id: //depot/maint-5.6/macperl@14399

--- macos/macperl/MPHelp.c
+++ macos/macperl/MPHelp.c
@@ -5,6 +5,9 @@ Author	:	Matthias Neeracher
 Language	:	MPW C
 
 $Log: MPHelp.c,v $
+Revision 1.3  2002/01/23 21:56:53  pudge
+Don't crash if very long word selected for help (bug #506672)
+
 Revision 1.2  2002/01/04 03:34:45  pudge
 Modifications for universal headers 3.4
 
@@ -316,6 +319,10 @@ symbolic_variable:
 	} else 
 		pos = 0;
 		
+	// Maximum is 255 characters
+	if (pos > 255)
+		pos = 255;
+
 	if (!pos) {
 		MetaHelp("\pYou didn't select any text to look up.",
 					(StringPtr) "\p", (StringPtr) "\p", (StringPtr) "\p");
