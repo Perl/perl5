8d481ee5bb3b5edbc9e7e7ba50aa3437fea792a6
Ville Skyttä <scop@cs132170.pp.htv.fi>
2002-02-15 12:53:06 +0200

    Re: [ID 20020213.015] Pod::Html XHTML update for 5.7.2    Message-Id: <1013763186.28457.6.camel@cs78130147.pp.htv.fi>
    p4raw-id: //depot/perl@14706

--- lib/Pod/Html.pm
+++ lib/Pod/Html.pm
@@ -435,10 +435,9 @@ sub pod2html {
 END_OF_BLOCK
 
     print HTML <<END_OF_HEAD;
-<?xml version="1.0" encoding="UTF-8"?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
-head>
+<head>
 <title>$title</title>$csslink
 <link rev="made" href="mailto:$Config{perladmin}" />
 </head>
