8a996ce8d00364c0bbcf12b34586652aab085a22
Jarkko Hietaniemi <jhi@iki.fi>
2002-03-23 00:48:23 +0000

    Purify: Array bounds read: o->op_last was accessed for unops (via newUNOP() and ck_eof()). (analysis okay, patch bad: see #15439)
    p4raw-id: //depot/perl@15434

--- op.c
+++ op.c
@@ -5971,7 +5971,8 @@ Perl_ck_fun(pTHX_ OP *o)
 			OP *newop = newGVOP(OP_GV, 0,
 			    gv_fetchpv(SvPVx(((SVOP*)kid)->op_sv, n_a), TRUE,
 					SVt_PVIO) );
-			if (kid == cLISTOPo->op_last)
+			if ((o->op_flags & OPf_KIDS) &&
+			    kid == cLISTOPo->op_last)
 			    cLISTOPo->op_last = newop;
 			op_free(kid);
 			kid = newop;
