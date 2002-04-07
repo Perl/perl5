ba1658c4079eb2cd76a8b742ef49644335a0fe06
Jarkko Hietaniemi <jhi@iki.fi>
2002-04-07 23:31:28 +0000

    Retract #15793 since #15775 should fix the problem.
    p4raw-id: //depot/perl@15794

--- hv.c
+++ hv.c
@@ -195,7 +195,6 @@ S_hv_fetch_flags(pTHX_ HV *hv, const char *key, I32 klen, I32 lval, int flags)
     register XPVHV* xhv;
     register U32 hash;
     register HE *entry;
-    const char* keysave = key;
     SV *sv;
 
     if (!hv)
