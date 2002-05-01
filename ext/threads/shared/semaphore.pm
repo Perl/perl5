8fc378b1af7a738f51216909699f057338fee03a
Artur Bergman <sky@nanisky.com>
2002-05-01 19:15:46 +0000

    Now passes old test case.
    p4raw-id: //depot/perl@16315

--- ext/threads/shared/semaphore.pm
+++ ext/threads/shared/semaphore.pm
@@ -74,17 +74,17 @@ sub new {
 
 sub down {
     my $s = shift;
-    lock($s);
+    lock($$s);
     my $inc = @_ ? shift : 1;
-    cond_wait $s until $$s >= $inc;
+    cond_wait $$s until $$s >= $inc;
     $$s -= $inc;
 }
 
 sub up {
     my $s = shift;
-    lock($s);
+    lock($$s);
     my $inc = @_ ? shift : 1;
-    ($$s += $inc) > 0 and cond_broadcast $s;
+    ($$s += $inc) > 0 and cond_broadcast $$s;
 }
 
 1;
