b412ba13de1aba05d1c6879f79a05afb87caff24
Dave Mitchell <davem@fdisolutions.com>
2002-05-04 00:51:10 +0100

    Re: [PATCH scope.c] Re: local($tied->{foo}) leaks Message-ID: <20020503235110.E22026@fdgroup.com>
    p4raw-id: //depot/perl@16386

--- pp_hot.c
+++ pp_hot.c
@@ -1682,8 +1682,17 @@ PP(pp_helem)
 		    STRLEN keylen;
 		    char *key = SvPV(keysv, keylen);
 		    SAVEDELETE(hv, savepvn(key,keylen), keylen);
-		} else
+		} else {
+		    SV *sv;
 		    save_helem(hv, keysv, svp);
+		    sv = *svp;
+		    /* If we're localizing a tied hash element, this new
+		     * sv won't actually be stored in the hash - so it
+		     * won't get reaped when the localize ends. Ensure it
+		     * gets reaped by mortifying it instead. DAPM */
+		    if (SvTIED_mg(sv, PERL_MAGIC_tiedelem))
+			sv_2mortal(sv);
+		}
             }
 	}
 	else if (PL_op->op_private & OPpDEREF)
@@ -2938,8 +2947,17 @@ PP(pp_aelem)
 	    PUSHs(lv);
 	    RETURN;
 	}
-	if (PL_op->op_private & OPpLVAL_INTRO)
+	if (PL_op->op_private & OPpLVAL_INTRO) {
+	    SV *sv;
 	    save_aelem(av, elem, svp);
+	    sv = *svp;
+	    /* If we're localizing a tied array element, this new sv
+	     * won't actually be stored in the array - so it won't get
+	     * reaped when the localize ends. Ensure it gets reaped by
+	     * mortifying it instead. DAPM */
+	    if (SvTIED_mg(sv, PERL_MAGIC_tiedelem))
+		sv_2mortal(sv);
+	}
 	else if (PL_op->op_private & OPpDEREF)
 	    vivify_ref(*svp, PL_op->op_private & OPpDEREF);
     }
