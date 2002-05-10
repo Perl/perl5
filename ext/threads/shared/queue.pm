78f7020ac7bebedf21cc49b06704a43770868392
Jarkko Hietaniemi <jhi@iki.fi>
2002-05-10 15:06:52 +0000

    Move pod, add $VERSION.
    p4raw-id: //depot/perl@16537

--- ext/threads/shared/queue.pm
+++ ext/threads/shared/queue.pm
@@ -1,48 +1,9 @@
-
 package threads::shared::queue;
 
 use threads::shared;
 use strict;
 
-sub new {
-    my $class = shift;
-    my @q : shared = @_;
-    my $q = \@q;
-    return bless $q, $class;
-}
-
-sub dequeue  {
-    my $q = shift;
-    lock(@$q);
-    until(@$q) {
-	cond_wait(@$q);
-    }
-    return shift @$q;
-}
-
-sub dequeue_nb {
-  my $q = shift;
-  lock(@$q);
-  if (@$q) {
-    return shift @$q;
-  } else {
-    return undef;
-  }
-}
-
-sub enqueue {
-    my $q = shift;
-    lock(@$q);
-    push(@$q, @_) and cond_broadcast @$q;
-}
-
-sub pending  {
-  my $q = shift;
-  lock(@$q);
-  return scalar(@$q);
-}
-
-1;
+our $VERSION = '1.00';
 
 =head1 NAME
 
@@ -103,4 +64,45 @@ The C<pending> method returns the number of items still in the queue.
 L<threads>, L<threads::shared>
 
 =cut
+
+sub new {
+    my $class = shift;
+    my @q : shared = @_;
+    my $q = \@q;
+    return bless $q, $class;
+}
+
+sub dequeue  {
+    my $q = shift;
+    lock(@$q);
+    until(@$q) {
+	cond_wait(@$q);
+    }
+    return shift @$q;
+}
+
+sub dequeue_nb {
+  my $q = shift;
+  lock(@$q);
+  if (@$q) {
+    return shift @$q;
+  } else {
+    return undef;
+  }
+}
+
+sub enqueue {
+    my $q = shift;
+    lock(@$q);
+    push(@$q, @_) and cond_broadcast @$q;
+}
+
+sub pending  {
+  my $q = shift;
+  lock(@$q);
+  return scalar(@$q);
+}
+
+1;
+
 
