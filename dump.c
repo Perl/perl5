662a8415e06f51848864e67b8bf26a899274884e
Tim Jenness <tjenness@cpan.org>
2002-05-23 00:00:00 -1000

    dump.c recognizes CvLOCKED Message-ID: <Pine.LNX.4.44.0205230959040.3222-100000@lapaki>
    p4raw-id: //depot/perl@16754

--- dump.c
+++ dump.c
@@ -979,6 +979,7 @@ Perl_do_sv_dump(pTHX_ I32 level, PerlIO *file, SV *sv, I32 nest, I32 maxnest, bo
 	if (SvCOMPILED(sv))	sv_catpv(d, "COMPILED,");
 	if (CvLVALUE(sv))	sv_catpv(d, "LVALUE,");
 	if (CvMETHOD(sv))	sv_catpv(d, "METHOD,");
+	if (CvLOCKED(sv))	sv_catpv(d, "LOCKED,");
 	break;
     case SVt_PVHV:
 	if (HvSHAREKEYS(sv))	sv_catpv(d, "SHAREKEYS,");
