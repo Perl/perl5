bb7af5ca4c2c314cbc4d8bf6a2c966e012f3faa5
Nicholas Clark <nick@ccl4.org>
2002-06-02 21:26:22 +0100

    [Encode PATCH] spurious warning Message-ID: <20020602192619.GA320@Bagpuss.unfortu.net>
    p4raw-id: //depot/perl@17009

--- ext/Encode/lib/Encode/Alias.pm
+++ ext/Encode/lib/Encode/Alias.pm
@@ -158,7 +158,7 @@ sub init_aliases
     define_alias( qr/^(.+)\@euro$/i => '"$1"' );
 
     define_alias( qr/\b(?:iso[-_]?)?latin[-_]?(\d+)$/i 
-		  => '"iso-8859-$Encode::Alias::Latin2iso[$1]"' );
+		  => 'defined $Encode::Alias::Latin2iso[$1] ? "iso-8859-$Encode::Alias::Latin2iso[$1]" : undef' );
 
     define_alias( qr/\bwin(latin[12]|cyrillic|baltic|greek|turkish|
 			 hebrew|arabic|baltic|vietnamese)$/ix => 
