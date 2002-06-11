deec275f317b7ba568cf64f11f21be48566283ed
Jarkko Hietaniemi <jhi@iki.fi>
2002-06-11 13:55:12 +0000

    Trying to printf Perl-specific datatypes using printf (with perlio or stdio) is folly, says Hugo.
    p4raw-id: //depot/perl@17189

--- ext/XS/APItest/APItest.pm
+++ ext/XS/APItest/APItest.pm
@@ -12,9 +12,8 @@ use base qw/ DynaLoader Exporter /;
 # Do not simply export all your public functions/methods/constants.
 
 # Export everything since these functions are only used by a test script
-our @EXPORT = qw( print_double print_nv print_iv print_int
+our @EXPORT = qw( print_double print_int print_long
 		  print_float print_long_double have_long_double
-		  print_uv print_long
 );
 
 our $VERSION = '0.01';
