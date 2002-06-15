07b0e447e5dffb77ce06dc459eeb25d4d36b8d06
Jarkko Hietaniemi <jhi@iki.fi>
2002-06-15 20:09:47 +0000

    Collect the known AmigaOS wisdom.
    p4raw-id: //depot/perl@17254

--- hints/amigaos.sh
+++ hints/amigaos.sh
@@ -11,7 +11,6 @@ usenm='true'
 usemymalloc='n'
 useperlio='true'
 d_eofnblk='define'
-d_fork='define'
 groupstype='int'
 
 # libs
@@ -24,11 +23,12 @@ xlibpth="$libpth"
 # to just these few.  E.g. what about Berkeley DB?
 libswanted='gdbm m dld'
 so=' '
+libs='-lm'
 
 # compiler & linker flags
 # Respect command-line values.
 
-ccflags="$ccflags -DAMIGAOS -mstackextend -Dfork=ix_vfork_resume"
+ccflags="$ccflags -DAMIGAOS"
 case "$optimize" in
 '') optimize='-O2 -fomit-frame-pointer';;
 esac
