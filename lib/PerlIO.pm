d5563ed7fd46e9485baaa3b43c79a6a71c7b4740
Jarkko Hietaniemi <jhi@iki.fi>
2002-06-17 13:56:33 +0000

    Further discouraging of :raw.
    p4raw-id: //depot/perl@17271

--- lib/PerlIO.pm
+++ lib/PerlIO.pm
@@ -33,8 +33,8 @@ PerlIO - On demand loader for PerlIO layers and root of PerlIO::* name space
 
 =head1 SYNOPSIS
 
-  open($fh,">:crlf","my.txt")
-  open($fh,">:raw","his.jpg")
+  open($fh,">:crlf", "my.txt")
+  open($fh,">:bytes","his.jpg")
 
   Shell:
     PERLIO=perlio perl ....
@@ -107,6 +107,8 @@ on output perl will warn if a "wide" character is written
 to a such a stream.
 
 =item raw
+
+B<Note that the explicit use of the C<raw> layer is deprecated.>
 
 A pseudo-layer which performs two functions (which is messy, but
 necessary to maintain compatibility with non-PerlIO builds of Perl
