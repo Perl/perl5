51997bc3cf799cd7796ef40df40dca19c40bb71a
Nicholas Clark <nick@ccl4.org>
2002-08-11 21:05:49 +0100

    better modfl detection Message-id: <20020811190548.GK300@Bagpuss.unfortu.net>
    p4raw-id: //depot/perl@17715

--- perl.h
+++ perl.h
@@ -1241,6 +1241,11 @@ typedef NVTYPE NV;
 /* e.g. libsunmath doesn't have modfl and frexpl as of mid-March 2000 */
 #   ifdef HAS_MODFL
 #       define Perl_modf(x,y) modfl(x,y)
+/* eg glibc 2.2 series seems to provide modfl on ppc and arm, but has no
+   prototype in <math.h> */
+#       ifndef HAS_MODFL_PROTO
+long double modfl(long double, long double *);
+#	endif
 #   else
 #       define Perl_modf(x,y) ((long double)modf((double)(x),(double*)(y)))
 #   endif
