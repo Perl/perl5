8fa6084d352991e38e07764ee38446b012daba0d
Jarkko Hietaniemi <jhi@iki.fi>
2002-11-15 04:37:24 +0000

    Add pseudo-hashes deprecation warning (at each pseudo-hash access).
    p4raw-id: //depot/maint-5.8/perl@18143

--- av.c
+++ av.c
@@ -949,6 +949,9 @@ HV*
 Perl_avhv_keys(pTHX_ AV *av)
 {
     SV **keysp = av_fetch(av, 0, FALSE);
+    if (ckWARN(WARN_DEPRECATED))	
+	 Perl_warner(aTHX_ packWARN(WARN_DEPRECATED),
+		     "Pseudo-hashes are deprecated");
     if (keysp) {
 	SV *sv = *keysp;
 	if (SvGMAGICAL(sv))
