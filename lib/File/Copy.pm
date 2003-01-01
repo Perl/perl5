5ce10329cdf928fb6a8fbddb793336dc3ff530e6
Nicholas Clark <nick@ccl4.org>
2003-01-01 21:09:30 +0000

    [DOCPATCH] File::Copy's synopsis Message-ID: <20030101210930.GE296@Bagpuss.unfortu.net>
    p4raw-id: //depot/perl@18441

--- lib/File/Copy.pm
+++ lib/File/Copy.pm
@@ -269,13 +269,13 @@ File::Copy - Copy files or filehandles
 
 =head1 SYNOPSIS
 
-  	use File::Copy;
+	use File::Copy;
 
-	copy("file1","file2");
-  	copy("Copy.pm",\*STDOUT);'
+	copy("file1","file2") or die "Copy failed: $!";
+	copy("Copy.pm",\*STDOUT);
 	move("/dev1/fileA","/dev2/fileB");
 
-  	use POSIX;
+	use POSIX;
 	use File::Copy cp;
 
 	$n = FileHandle->new("/a/file","r");
