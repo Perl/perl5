bd5cf8491554ab1313db72afbf9e7bc1debe967c
Hugo van der Sanden <hv@crypt.org>
2003-02-26 01:11:14 +0000

    regen headers
    p4raw-id: //depot/perl@18781

--- perlapi.h
+++ perlapi.h
@@ -496,6 +496,8 @@ END_EXTERN_C
 #define PL_sig_pending		(*Perl_Isig_pending_ptr(aTHX))
 #undef  PL_sighandlerp
 #define PL_sighandlerp		(*Perl_Isighandlerp_ptr(aTHX))
+#undef  PL_signals
+#define PL_signals		(*Perl_Isignals_ptr(aTHX))
 #undef  PL_sort_RealCmp
 #define PL_sort_RealCmp		(*Perl_Isort_RealCmp_ptr(aTHX))
 #undef  PL_splitstr
