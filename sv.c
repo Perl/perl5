a73e8557ffb104ab62198e9838714843724d84e5
Jarkko Hietaniemi <jhi@iki.fi>
2003-03-12 10:59:03 +0000

    Another finding by IRIX cc: non-void function sv_cat_decode() should return a value.
    p4raw-id: //depot/perl@18954

--- sv.c
+++ sv.c
@@ -11499,8 +11499,8 @@ bool
 Perl_sv_cat_decode(pTHX_ SV *dsv, SV *encoding,
 		   SV *ssv, int *offset, char *tstr, int tlen)
 {
+    bool ret = FALSE;
     if (SvPOK(ssv) && SvPOK(dsv) && SvROK(encoding) && offset) {
-        bool ret = FALSE;
 	SV *offsv;
 	dSP;
 	ENTER;
@@ -11521,8 +11521,9 @@ Perl_sv_cat_decode(pTHX_ SV *dsv, SV *encoding,
 	PUTBACK;
 	FREETMPS;
 	LEAVE;
-	return ret;
     }
-    Perl_croak(aTHX_ "Invalid argument to sv_cat_decode.");
+    else
+        Perl_croak(aTHX_ "Invalid argument to sv_cat_decode");
+    return ret;
 }
 
