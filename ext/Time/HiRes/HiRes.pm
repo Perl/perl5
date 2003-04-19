36d6c396be0fa6cbceda4d703f6121eaeceaf5d8
Mark-Jason Dominus <mjd@plover.com>
2003-04-19 15:13:44 +0000

    typo in Time::HiRes docs Message-ID: <20030419151344.28237.qmail@plover.com>
    p4raw-id: //depot/perl@19328

--- ext/Time/HiRes/HiRes.pm
+++ ext/Time/HiRes/HiRes.pm
@@ -288,7 +288,7 @@ The interval is always what you put in using setitimer().
 
   use Time::HiRes qw ( setitimer ITIMER_VIRTUAL time );
 
-  $SIG{VTLARM} = sub { print time, "\n" };
+  $SIG{VTALRM} = sub { print time, "\n" };
   setitimer(ITIMER_VIRTUAL, 10, 2.5);
 
 =head1 C API
