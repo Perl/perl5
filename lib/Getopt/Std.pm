68f6d3d226605b973491663dc291d00e54b14e22
Ilya Zakharevich <ilya@math.berkeley.edu>
2003-06-03 17:45:23 -0700

    Integrate: [ 21245] Subject: Re: [PATCH 5.8.1 @19053] Getopt::Std Message-ID: <20030604074523.GA7148@math.berkeley.edu> p4raw-link: @21245 on //depot/perl: 669ecdbcc9859cccd0b304b919aa0725f0097a2a
    p4raw-id: //depot/maint-5.8/perl@21246
    p4raw-integrated: from //depot/perl@21244 'copy in' lib/Getopt/Std.pm
    	(@19171..)

--- lib/Getopt/Std.pm
+++ lib/Getopt/Std.pm
@@ -55,6 +55,9 @@ the output file handle, the name of option-processing package, its version,
 and the switches string.  If the subroutines are not defined, an attempt is
 made to generate intelligent messages; for best results, define $main::VERSION.
 
+If embedded documentation (in pod format, see L<perlpod>) is detected
+in the script, C<--help> will also show how to access the documentation.
+
 Note that due to excessive paranoia, if $Getopt::Std::STANDARD_HELP_VERSION
 isn't true (the default is false), then the messages are printed on STDERR,
 and the processing continues after the messages are printed.  This being
@@ -186,11 +189,26 @@ sub help_mess ($;$) {
 	}
 	my ($scr) = ($0 =~ m,([^/\\]+)$,);
 	print $h <<EOH if @_;			# Let the script override this
+
 Usage: $scr [-OPTIONS [-MORE_OPTIONS]] [--] [PROGRAM_ARG1 ...]
 EOH
 	print $h <<EOH;
+
 The following single-character options are accepted:$help
+
 Options may be merged together.  -- stops processing of options.$arg
+EOH
+	my $has_pod;
+	if ( defined $0 and $0 ne '-e' and -f $0 and -r $0
+	     and open my $script, '<', $0 ) {
+	    while (<$script>) {
+		$has_pod = 1, last if /^=(pod|head1)/;
+	    }
+	}
+	print $h <<EOH if $has_pod;
+
+For more details run
+	perldoc -F $0
 EOH
     }
 }
