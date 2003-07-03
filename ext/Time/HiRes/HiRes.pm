98718de32ba9128d7c985a9043d61c025725d7fa
Jarkko Hietaniemi <jhi@iki.fi>
2003-07-03 21:34:03 +0000

    Bump Time::HiRes version to 1.50.
    p4raw-id: //depot/perl@19973

--- ext/Time/HiRes/HiRes.pm
+++ ext/Time/HiRes/HiRes.pm
@@ -15,7 +15,7 @@ require DynaLoader;
 		 d_usleep d_ualarm d_gettimeofday d_getitimer d_setitimer
 		 d_nanosleep);
 	
-$VERSION = '1.49';
+$VERSION = '1.50';
 $XS_VERSION = $VERSION;
 $VERSION = eval $VERSION;
 
