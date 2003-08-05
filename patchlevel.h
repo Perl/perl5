777f2ba2a8f91c3f1baf8643bb3087a9174355c5
Jarkko Hietaniemi <jhi@iki.fi>
2003-08-05 16:16:28 +0000

    Update Changes.
    p4raw-id: //depot/maint-5.8/perl@20505

--- patchlevel.h
+++ patchlevel.h
@@ -119,7 +119,7 @@ hunk.
 #if !defined(PERL_PATCHLEVEL_H_IMPLICIT) && !defined(LOCAL_PATCH_COUNT)
 static	char	*local_patches[] = {
         NULL
-        ,"RC4"
+        ,"MAINT20504"
         ,NULL
 };
 
