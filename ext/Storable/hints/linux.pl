b5967c96bba0911f2cc6162f459bbf8cacc1c608
Jarkko Hietaniemi <jhi@iki.fi>
2003-08-24 06:13:13 +0000

    gcc -O3 and Storable in Linux do not like each other.
    p4raw-id: //depot/perl@20861

--- ext/Storable/hints/linux.pl
+++ ext/Storable/hints/linux.pl
@@ -1,13 +1,10 @@
-# gcc -O3 (and higher) can cause Storable.xs to produce code that
+# gcc -O3 (and higher) can cause code produced from Storable.xs that
 # dumps core immediately in recurse.t and retrieve.t, in is_storing()
 # and last_op_in_netorder(), respectively.  In both cases the cxt is
 # full of junk (and according to valgrind the cxt was never stack'd,
-# malloc'd or free'd).  Observed in Debian 3.0 x86, both with gccs
-# 2.95.4 20011002 and 3.3.  The failures are seen only for unthreaded
-# builds, threaded builds work okay.
+# malloc'd or free'd).  Observed in Debian 3.0 x86, with gccs 2.95.4
+# 20011002 and 3.3, and in Redhat 7.1 with gcc 3.3.1. The failures
+# happen only for unthreaded builds, threaded builds work okay.
 use Config;
-$self->{OPTIMIZE} = '-O2'
-    if -f '/etc/debian_version' &&
-       ($Config{gccversion} =~ /^2\.95\.4 20011002 / ||
-        $Config{gccversion} eq '3.3');
+$self->{OPTIMIZE} = '-O2';
 
