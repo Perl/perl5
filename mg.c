39bd0f18b4bccd2fa3176747640b8ecf99930080
Paul Johnson <paul@pjcj.net>
2003-12-05 01:57:21 +0100

    $^P, eval and caller Message-ID: <20031204235721.GJ26355@pjcj.net>
    p4raw-id: //depot/perl@21842

--- mg.c
+++ mg.c
@@ -2072,7 +2072,8 @@ Perl_magic_set(pTHX_ SV *sv, MAGIC *mg)
 	break;
     case '\020':	/* ^P */
 	PL_perldb = SvIOK(sv) ? SvIVX(sv) : sv_2iv(sv);
-	if (PL_perldb && !PL_DBsingle)
+	if ((PERLDB_SUB || PERLDB_LINE || PERLDB_SUBLINE || PERLDB_ASSERTION)
+		&& !PL_DBsingle)
 	    init_debugger();
 	break;
     case '\024':	/* ^T */
