908fffd27681b4621cbe99d1973324d75e559cbb
Nicholas Clark <nick@ccl4.org>
2004-01-26 22:26:53 +0000

    Integrate: [ 22071] [perl #24674] stop $^O getting tainted on read access, and disallow tainted assignment to it
    [ 22181]
    change #22071 (taint bug in $^0) introduced a potential double
    free(), because PL_osname may get freed but not nulled if a taint
    test fails in between. Mea cupla and all that...
    p4raw-link: @22181 on //depot/perl: 1bf2966364b6356e9050b17d8920dd4a8ce27d97
    p4raw-link: @22071 on //depot/perl: 3511154c18a0900e8873e8e72a4b74931525e718
    
    p4raw-id: //depot/maint-5.8/perl@22225
    p4raw-integrated: from //depot/perl@22222 'edit in' mg.c (@22071..)
    	'merge in' t/op/taint.t (@21902..)

--- mg.c
+++ mg.c
@@ -643,8 +643,10 @@ Perl_magic_get(pTHX_ SV *sv, MAGIC *mg)
 	    sv_setsv(sv, &PL_sv_undef);
 	break;
     case '\017':		/* ^O & ^OPEN */
-	if (*(mg->mg_ptr+1) == '\0')
+	if (*(mg->mg_ptr+1) == '\0') {
 	    sv_setpv(sv, PL_osname);
+	    SvTAINTED_off(sv);
+	}
 	else if (strEQ(mg->mg_ptr, "\017PEN")) {
 	    if (!PL_compiling.cop_io)
 		sv_setsv(sv, &PL_sv_undef);
@@ -2096,12 +2098,14 @@ Perl_magic_set(pTHX_ SV *sv, MAGIC *mg)
 	break;
     case '\017':	/* ^O */
 	if (*(mg->mg_ptr+1) == '\0') {
-	    if (PL_osname)
+	    if (PL_osname) {
 		Safefree(PL_osname);
-	    if (SvOK(sv))
-		PL_osname = savepv(SvPV(sv,len));
-	    else
 		PL_osname = Nullch;
+	    }
+	    if (SvOK(sv)) {
+		TAINT_PROPER("assigning to $^O");
+		PL_osname = savepv(SvPV(sv,len));
+	    }
 	}
 	else if (strEQ(mg->mg_ptr, "\017PEN")) {
 	    if (!PL_compiling.cop_io)
