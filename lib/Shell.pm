bf0e938bbcf9ca8acf915645a43d4861b6dc412a
Manuel Valente <mvalente@idealx.com>
2004-01-27 20:18:26 +0100

    Integrate: [ 22254] Subject:  Patch for Shell.pm Message-Id:  <4016AB72.1080503@idealx.com>
    New option for Shell.pm to discard stderr instead of capturing it
    
    [ 22259]
    update fix to Shell.pm in change #22254: make /dev/null portable
    
    [ 22272]
    Fix a non-standard NAME pod section
    (by Smylers and Casey West)
    p4raw-link: @22272 on //depot/perl: 4952958fb8c38233c6dd3f5829a95308029f2bee
    p4raw-link: @22259 on //depot/perl: d0b4fbd9e2c9ee9a42f5c0de27ae9f61e9e33074
    p4raw-link: @22254 on //depot/perl: c4a2e7a54bf494cff88f7c7c033f6b017d36f653
    
    p4raw-id: //depot/maint-5.8/perl@22283
    p4raw-integrated: from //depot/perl@22277 'copy in' lib/Carp/Heavy.pm
    	(@22109..) 'edit in' lib/Shell.pm (@22258..)

--- lib/Shell.pm
+++ lib/Shell.pm
@@ -2,9 +2,11 @@ package Shell;
 use 5.006_001;
 use strict;
 use warnings;
+use File::Spec::Functions;
+
 our($capture_stderr, $VERSION, $AUTOLOAD);
 
-$VERSION = '0.5';
+$VERSION = '0.5.1';
 
 sub new { bless \my $foo, shift }
 sub DESTROY { }
@@ -28,11 +30,15 @@ sub AUTOLOAD {
     shift if ref $_[0] && $_[0]->isa( 'Shell' );
     my $cmd = $AUTOLOAD;
     $cmd =~ s/^.*:://;
+    my $null = File::Spec::Functions::devnull();
+    $Shell::capture_stderr ||= 0;
     eval <<"*END*";
 	sub $AUTOLOAD {
 	    shift if ref \$_[0] && \$_[0]->isa( 'Shell' );
 	    if (\@_ < 1) {
-		\$Shell::capture_stderr ? `$cmd 2>&1` : `$cmd`;
+		\$Shell::capture_stderr ==  1 ? `$cmd 2>&1` : 
+		\$Shell::capture_stderr == -1 ? `$cmd 2>$null` : 
+		`$cmd`;
 	    } elsif ('$^O' eq 'os2') {
 		local(\*SAVEOUT, \*READ, \*WRITE);
 
@@ -84,7 +90,8 @@ sub AUTOLOAD {
 			\$_ = \$_;
 		    }
 		}
-		push \@arr, '2>&1' if \$Shell::capture_stderr;
+		push \@arr, '2>&1'        if \$Shell::capture_stderr ==  1;
+		push \@arr, '2>$null' if \$Shell::capture_stderr == -1;
 		open(SUBPROC, join(' ', '$cmd', \@arr, '|'))
 		    or die "Can't exec $cmd: \$!\\n";
 		if (wantarray) {
@@ -153,6 +160,9 @@ Larry
 
 If you set $Shell::capture_stderr to 1, the module will attempt to
 capture the STDERR of the process as well.
+
+If you set $Shell::capture_stderr to -1, the module will discard the 
+STDERR of the process.
 
 The module now should work on Win32.
 
