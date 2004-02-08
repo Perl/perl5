bf2b43ffd0997b89e460287fefa9f059ed6ae7ce
Rafael Garcia-Suarez <rgarciasuarez@gmail.com>
2004-02-08 21:34:27 +0000

    Add the new private flag OPpGREP_LEX in B::Concise.
    p4raw-id: //depot/perl@22290

--- ext/B/B/Concise.pm
+++ ext/B/B/Concise.pm
@@ -14,7 +14,7 @@ use warnings; # uses #3 and #4, since warnings uses Carp
 
 use Exporter (); # use #5
 
-our $VERSION   = "0.57";
+our $VERSION   = "0.58";
 our @ISA       = qw(Exporter);
 our @EXPORT_OK = qw(set_style set_style_standard add_callback
 		    concise_subref concise_cv concise_main);
@@ -423,6 +423,8 @@ $priv{"threadsv"}{64} = "SVREFd";
 $priv{"exit"}{128} = "VMS";
 $priv{$_}{2} = "FTACCESS"
   for ("ftrread", "ftrwrite", "ftrexec", "fteread", "ftewrite", "fteexec");
+$priv{$_}{2} = "GREPLEX"
+  for ("mapwhile", "mapstart", "grepwhile", "grepstart");
 
 sub private_flags {
     my($name, $x) = @_;
