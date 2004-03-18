5ab053b06800167d2bc083dd4186b23d7cb95fce
Dave Mitchell <davem@fdisolutions.com>
2004-03-18 19:02:32 +0000

    add code comment for change 22525
    p4raw-id: //depot/perl@22531

--- pp.c
+++ pp.c
@@ -2377,7 +2377,7 @@ PP(pp_complement)
 	register I32 anum;
 	STRLEN len;
 
-	(void)SvPV_nomg(sv,len);
+	(void)SvPV_nomg(sv,len); /* force check for uninit var */
 	sv_setsv_nomg(TARG, sv);
 	tmps = (U8*)SvPV_force(TARG, len);
 	anum = len;
