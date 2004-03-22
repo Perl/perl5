45a2796c78934ddcc83dbc97c4c2f291dc19cfd1
Rafael Garcia-Suarez <rgarciasuarez@gmail.com>
2004-03-22 21:29:51 +0000

    Update the -v copyright notice.
    p4raw-id: //depot/perl@22560

--- perl.c
+++ perl.c
@@ -2638,7 +2638,7 @@ Perl_moreswitches(pTHX_ char *s)
 #endif
 
 	PerlIO_printf(PerlIO_stdout(),
-		      "\n\nCopyright 1987-2003, Larry Wall\n");
+		      "\n\nCopyright 1987-2004, Larry Wall\n");
 #ifdef MACOS_TRADITIONAL
 	PerlIO_printf(PerlIO_stdout(),
 		      "\nMac OS port Copyright 1991-2002, Matthias Neeracher;\n"
