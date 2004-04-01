893f4f4cb0eda9631b4c0c3abc008fc21ccd340f
Nicholas Clark <nick@ccl4.org>
2004-04-01 13:09:08 +0000

    Change 22555 left a perforce conflict in the copyright message. D'oh. Silly maint pumpking.
    p4raw-id: //depot/maint-5.8/perl@22627

--- pad.c
+++ pad.c
@@ -1,12 +1,6 @@
 /*    pad.c
  *
->>>> ORIGINAL pad.c#29
- *    Copyright (C) 2002,2003 by Larry Wall and others
-==== THEIRS pad.c#30
  *    Copyright (C) 2002, 2003, 2004, by Larry Wall and others
-==== YOURS pad.c
- *    Copyright (C) 2002, by Larry Wall and others
-<<<<
  *
  *    You may distribute under the terms of either the GNU General Public
  *    License or the Artistic License, as specified in the README file.
