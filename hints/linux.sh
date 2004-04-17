33fae54deded526e94a53b7d5a5e86c27b5996cb
Nicholas Clark <nick@ccl4.org>
2004-04-17 14:28:57 +0000

    Integrate: [ 22706] On Linux-PPC, using gcc, downgrade to the -O1 optimisation level (with -O2 miniperl behaves really badly.) p4raw-link: @22706 on //depot/perl: be92ff21b1b31bf647f96c308fe1a246f1974bde
    p4raw-id: //depot/maint-5.8/perl@22711
    p4raw-integrated: from //depot/perl@22710 'copy in' hints/linux.sh
    	(@22585..)

--- hints/linux.sh
+++ hints/linux.sh
@@ -86,7 +86,16 @@ esac
 
 case "$optimize" in
 # use -O2 by default ; -O3 doesn't seem to bring significant benefits with gcc
-'') optimize='-O2' ;;
+'')
+    optimize='-O2'
+    case "`uname -m`" in
+        ppc*)
+            # on ppc, it seems that gcc (at least gcc 3.3.2) isn't happy
+	    # with -O2 ; so downgrade to -O1.
+            optimize='-O1'
+        ;;
+    esac
+    ;;
 esac
 
 # Are we using ELF?  Thanks to Kenneth Albanowski <kjahds@kjahds.com>
