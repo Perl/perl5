56da48f7e00cdb289542091917287b81e0078360
Dave Mitchell <davem@fdisolutions.com>
2004-08-09 17:21:27 +0000

    fix minor nit in file description, to keep Jarkko happy
    p4raw-id: //depot/perl@23207

--- utf8.c
+++ utf8.c
@@ -32,7 +32,8 @@ static char unees[] = "Malformed UTF-8 character (unexpected end of string)";
 This file contains various utility functions for manipulating UTF8-encoded
 strings. For the uninitiated, this is a method of representing arbitrary
 Unicode characters as a variable number of bytes, in such a way that
-characters in the ASCII range are unmodified, and a zero byte never appears.
+characters in the ASCII range are unmodified, and a zero byte never appears
+within non-zero characters.
 
 =for apidoc A|U8 *|uvuni_to_utf8_flags|U8 *d|UV uv|UV flags
 
