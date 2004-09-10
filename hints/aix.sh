066079d9dfc8875c5116292c5abd46d98a6e4b93
Nicholas Clark <nick@ccl4.org>
2004-09-10 06:25:00 +0000

    Integrate: [ 23122] oslevel can fail on AIX, but the output generated would confuse Configure
    [ 23124]
    Be sure HP-UX' ANSI C compiler's PATH is found *before*
    the path to the bundled braindead C compiler. This might
    influence ccache's behaviour in finding the correct path
    
    [ 23174]
    First steps towards an explicit perl.exp-less AIX build
    Previous method can still be used by undeffing usenativedlopen
    If that is ever tested at all on AIX
    
    [ 23188]
    gcc on AIX doesn't like -G on the commandline
    
    [ 23189]
    gcc on AIX 4 doesn't like -G on the commandline too
    p4raw-link: @23189 on //depot/perl: 1be326de458e100f1527bf44371fc6d90f6f67fe
    p4raw-link: @23188 on //depot/perl: ad5c63c6cecbd2f3619070c0fd8ea7f3132c0eae
    p4raw-link: @23174 on //depot/perl: 142c15906ae08e559b1337c4816a6c2a0c341e24
    p4raw-link: @23124 on //depot/perl: 87bdd940c6fd08108ec93f5976066dfb8645f9af
    p4raw-link: @23122 on //depot/perl: e81c5c2a4504e8ed2983ba7b4e9c704c6ea45a05
    
    p4raw-id: //depot/maint-5.8/perl@23305
    p4raw-integrated: from //depot/perl@23304 'copy in' Configure
    	(@23122..) hints/aix.sh (@23174..)
    p4raw-integrated: from //depot/perl@23174 'ignore' hints/aix_4.sh
    	(@22743..)

--- hints/aix.sh
+++ hints/aix.sh
@@ -157,7 +157,11 @@ case "$cc" in
 # -bI:$(PERL_INC)/perl.exp  Read the exported symbols from the perl binary
 # -bE:$(BASEEXT).exp	    Export these symbols.  This file contains only one
 #			    symbol: boot_$(EXP)	 can it be auto-generated?
-lddlflags="$lddlflags -bhalt:4 -bM:SRE -bI:\$(PERL_INC)/perl.exp -bE:\$(BASEEXT).exp -bnoentry -lc"
+if test $usenativedlopen = 'true' ; then
+    lddlflags="$lddlflags -bhalt:4 -bexpall -G -bnoentry -lc"
+else
+    lddlflags="$lddlflags -bhalt:4 -bM:SRE -bI:\$(PERL_INC)/perl.exp -bE:\$(BASEEXT).exp -bnoentry -lc"
+    fi
 
 case "$use64bitall" in
     $define|true|[yY]*) use64bitint="$define" ;;
@@ -314,6 +318,7 @@ libswanted_uselargefiles="`getconf XBS5_ILP32_OFFBIG_LIBS 2>/dev/null|sed -e 's@
 		ccflags="`echo $ccflags | sed -e 's@ -b@ -Wl,-b@g'`"
 		ldflags="`echo ' '$ldflags | sed -e 's@ -b@ -Wl,-b@g'`"
 		lddlflags="`echo ' '$lddlflags | sed -e 's@ -b@ -Wl,-b@g'`"
+		lddlflags="`echo ' '$lddlflags | sed -e 's@ -G @ -Wl,-G @g'`"
 		ld='gcc'
 		echo >&4 "(using ccflags   $ccflags)"
 		echo >&4 "(using ldflags   $ldflags)"
@@ -415,9 +420,19 @@ EOCBU
 
 if test $usenativedlopen = 'true' ; then
     ccflags="$ccflags -DUSE_NATIVE_DLOPEN"
+    # -brtl		    Enables a binary to use run time linking
+    # -bdynamic		    When used with -brtl, tells linker to search for
+    #			    ".so"-suffix libraries as well as ".a" suffix
+    #			    libraries. AIX allows both .so and .a libraries to
+    #			    contain dynamic shared objects.
+    # -bmaxdata:0x80000000  This increases the size of heap memory available
+    #			    to perl. Default is 256 MB, which sounds large but
+    #			    caused a software vendor problems. So this sets
+    #			    heap to 2 GB maximum. Anything higher and you'd
+    #			    want to consider 64 bit perl.
     case "$cc" in
-	*gcc*) ldflags="$ldflags -Wl,-brtl" ;;
-	*)     ldflags="$ldflags -brtl" ;;
+	*gcc*) ldflags="$ldflags -Wl,-brtl -Wl,-bdynamic -Wl,-bmaxdata:0x80000000" ;;
+	*)     ldflags="$ldflags -brtl -bdynamic -bmaxdata:0x80000000" ;;
 	esac
 elif test -f /lib/libC.a -a X"`$cc -v 2>&1 | grep gcc`" = X; then
     # If the C++ libraries, libC and libC_r, are available we will
