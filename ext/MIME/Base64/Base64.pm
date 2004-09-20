2a99bba51541367c1fa77cddaf4838ae3213738a
Rafael Garcia-Suarez <rgarciasuarez@gmail.com>
2004-09-20 17:20:38 +0000

    Upgrade to MIME::Base64 3.05.
    p4raw-id: //depot/perl@23330

--- ext/MIME/Base64/Base64.pm
+++ ext/MIME/Base64/Base64.pm
@@ -1,6 +1,6 @@
 package MIME::Base64;
 
-# $Id: Base64.pm,v 3.3 2004/08/25 09:33:45 gisle Exp $
+# $Id: Base64.pm,v 3.5 2004/09/20 09:23:23 gisle Exp $
 
 use strict;
 use vars qw(@ISA @EXPORT $VERSION);
@@ -10,7 +10,7 @@ require DynaLoader;
 @ISA = qw(Exporter DynaLoader);
 @EXPORT = qw(encode_base64 decode_base64);
 
-$VERSION = '3.03';
+$VERSION = '3.05';
 
 MIME::Base64->bootstrap($VERSION);
 
