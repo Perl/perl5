6dc978865f77828857b2e2f15447be440d0ac6a3
Nicholas Clark <nick@ccl4.org>
2004-10-21 11:43:53 +0000

    Integrate: [ 23351] Make the perl interpreter more tolerant of UTF-16-encoded script (patch by Jarkko Hietaniemi)
    [ 23352]
    Briefly document the test.utf16 make target.
    
    [ 23362]
    Minor tweaks for the test.utf16 target, by Jarkko
    p4raw-link: @23362 on //depot/perl: 90f6ca78c85ab94ac108f3fd8202d56948c025be
    p4raw-link: @23352 on //depot/perl: cc0710ff8fd43a4bc2497be73d2dd00477cc8990
    p4raw-link: @23351 on //depot/perl: 1de9afcdf18cf98bbdecaa782da93e907be6fe4e
    
    p4raw-id: //depot/maint-5.8/perl@23400
    p4raw-integrated: from //depot/perl@23399 'copy in' utf8.c (@23207..)
    	pod/perlhack.pod (@23240..)
    p4raw-integrated: from //depot/perl@23351 'edit in' t/TEST (@22932..)
    	'merge in' Makefile.SH (@23155..) toke.c (@23339..)

--- utf8.c
+++ utf8.c
@@ -868,8 +868,14 @@ Perl_utf16_to_utf8(pTHX_ U8* p, U8* d, I32 bytelen, I32 *newlen)
     U8* pend;
     U8* dstart = d;
 
+    if (bytelen == 1 && p[0] == 0) { /* Be understanding. */
+	 d[0] = 0;
+	 *newlen = 1;
+	 return d;
+    }
+
     if (bytelen & 1)
-	Perl_croak(aTHX_ "panic: utf16_to_utf8: odd bytelen");
+	Perl_croak(aTHX_ "panic: utf16_to_utf8: odd bytelen %d", bytelen);
 
     pend = p + bytelen;
 
