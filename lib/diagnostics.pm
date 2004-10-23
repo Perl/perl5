c411974dd51c7d7c0ee08434cc1bb9b77909385d
Steve Peters <steve@fisharerojo.org>
2004-10-23 13:55:41 +0000

    [perl #30227] [PATCH]splain vs. -w From: "Steve Peters via RT" <perlbug-followup@perl.org> Message-ID: <rt-3.0.11-30227-98375.1.28465791111211@perl.org>
    p4raw-id: //depot/perl@23422

--- lib/diagnostics.pm
+++ lib/diagnostics.pm
@@ -28,7 +28,8 @@ Using diagnostics to get stack traces from a misbehaving script:
 =head2 The C<diagnostics> Pragma
 
 This module extends the terse diagnostics normally emitted by both the
-perl compiler and the perl interpreter, augmenting them with the more
+perl compiler and the perl interpreter (from running perl with a -w 
+switch or C<use warnings>), augmenting them with the more
 explicative and endearing descriptions found in L<perldiag>.  Like the
 other pragmata, it affects the compilation phase of your program rather
 than merely the execution phase.
