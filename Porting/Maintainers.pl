56e79fa5da7d447ac2f9e86aee2ead2c9c65ab53
Rafael Garcia-Suarez <rgarciasuarez@gmail.com>
2004-12-16 15:43:02 +0000

    Pod::Html is not part of the podlators distribution
    p4raw-id: //depot/perl@23657

--- Porting/Maintainers.pl
+++ Porting/Maintainers.pl
@@ -399,7 +399,7 @@ package Maintainers;
 	'podlators' =>
 		{
 		'MAINTAINER'	=> 'rra',
-		'FILES'		=> q[lib/Pod/{Html,Man,ParseLink,Text,Text/{Color,Overstrike,Termcap}}.pm pod/pod2man.PL pod/pod2text.PL lib/Pod/t/{basic.*,{man,parselink,text*}.t}],
+		'FILES'		=> q[lib/Pod/{Man,ParseLink,Text,Text/{Color,Overstrike,Termcap}}.pm pod/pod2man.PL pod/pod2text.PL lib/Pod/t/{basic.*,{man,parselink,text*}.t}],
 		'CPAN'		=> 1,
 		},
 
