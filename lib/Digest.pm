7f7034cae74adb1d244baf919e9e470495aa0659
Nicholas Clark <nick@ccl4.org>
2005-02-05 16:59:25 +0000

    Integrate: [ 23928] Upgrade to Digest 1.10 p4raw-link: @23928 on //depot/perl: a9acc3567ea7c266e6edabba11e4855b4a1676ed
    p4raw-id: //depot/maint-5.8/perl@23939
    p4raw-branched: from //depot/perl@23938 'branch in' lib/Digest/Changes
    	lib/Digest/file.pm lib/Digest/t/file.t
    p4raw-integrated: from //depot/perl@23938 'copy in' lib/Digest/base.pm
    	(@21809..) lib/Digest.pm (@22915..) 'merge in' MANIFEST
    	(@23925..)

--- lib/Digest.pm
+++ lib/Digest.pm
@@ -3,7 +3,7 @@ package Digest;
 use strict;
 use vars qw($VERSION %MMAP $AUTOLOAD);
 
-$VERSION = "1.08";
+$VERSION = "1.10";
 
 %MMAP = (
   "SHA-1"      => ["Digest::SHA1", ["Digest::SHA", 1], ["Digest::SHA2", 1]],
