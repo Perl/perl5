fbbafa6d4d20579293b51de798077b9e8c394e94
Rafael Garcia-Suarez <rgarciasuarez@gmail.com>
2005-02-06 22:18:48 +0000

    Avoid evaluating a strlen twice due the new implementation of New() with PERL_MALLOC_WRAP
    p4raw-id: //depot/perl@23943

--- util.c
+++ util.c
@@ -758,10 +758,18 @@ char *
 Perl_savepv(pTHX_ const char *pv)
 {
     register char *newaddr;
+#ifdef PERL_MALLOC_WRAP
+    STRLEN pvlen;
+#endif
     if (!pv)
 	return Nullch;
 
+#ifdef PERL_MALLOC_WRAP
+    pvlen = strlen(pv)+1;
+    New(902,newaddr,pvlen,char);
+#else
     New(902,newaddr,strlen(pv)+1,char);
+#endif
     return strcpy(newaddr,pv);
 }
 
