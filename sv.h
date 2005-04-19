7ae8ee9e2dc5b5e2978d561e17a946dd34408c4f
Nicholas Clark <nick@ccl4.org>
2005-04-19 20:55:24 +0000

    Restore SvEND_set
    p4raw-id: //depot/perl@24253

--- sv.h
+++ sv.h
@@ -818,6 +818,9 @@ in gv.h: */
 #define SvLEN_set(sv, val) \
 	STMT_START { assert(SvTYPE(sv) >= SVt_PV); \
 		(((XPV*)  SvANY(sv))->xpv_len = (val)); } STMT_END
+#define SvEND_set(sv, val) \
+	STMT_START { assert(SvTYPE(sv) >= SVt_PV); \
+		(SvCUR(sv) = (val) - SvPVX(sv)); } STMT_END
 
 #define SvPV_renew(sv,n) \
 	STMT_START { SvLEN_set(sv, n); \
