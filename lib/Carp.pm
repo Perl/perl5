8c3d97219390037dbdf177595955b394d8b3290e
Dave Mitchell <davem@fdisolutions.com>
2005-07-01 14:34:52 +0000

    undo some excessive Carp.pm minimalisation
    p4raw-id: //depot/perl@25039

--- lib/Carp.pm
+++ lib/Carp.pm
@@ -1,21 +1,21 @@
 package Carp;
-our $VERSION = '1.04';
+
+our $VERSION = '1.05';
 # this file is an utra-lightweight stub. The first time a function is
 # called, Carp::Heavy is loaded, and the real short/longmessmess_jmp
 # subs are installed
 
-# $MaxEvalLen, $Verbose
-# are supposed to default to 0, but undef should be close enough
-
-$CarpLevel = 0;
-$MaxArgLen = 64;    # How much of each argument to print. 0 = all.
-$MaxArgNums = 8;    # How many arguments to print. 0 = all.
+our $MaxEvalLen = 0;
+our $Verbose    = 0;
+our $CarpLevel  = 0;
+our $MaxArgLen  = 64;   # How much of each argument to print. 0 = all.
+our $MaxArgNums = 8;    # How many arguments to print. 0 = all.
 
 require Exporter;
-@ISA = ('Exporter');
-@EXPORT = qw(confess croak carp);
-@EXPORT_OK = qw(cluck verbose longmess shortmess);
-@EXPORT_FAIL = qw(verbose);	# hook to enable verbose mode
+our @ISA = ('Exporter');
+our @EXPORT = qw(confess croak carp);
+our @EXPORT_OK = qw(cluck verbose longmess shortmess);
+our @EXPORT_FAIL = qw(verbose);	# hook to enable verbose mode
 
 # if the caller specifies verbose usage ("perl -MCarp=verbose script.pl")
 # then the following method will be called by the Exporter which knows
@@ -187,7 +187,6 @@ all calls to C<croak> become C<confess>.
 Note, this is analogous to using C<use Carp 'verbose'>.
 
 Defaults to C<0>.
-
 
 =head1 BUGS
 
