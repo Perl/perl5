dcddf0a3de6867dcbe63ebab15b50da587648ebb
Steve Hay <SteveHay@planit.com>
2005-07-22 10:32:02 +0000

    Tidy up options / defines output from makedef.pl and change Win32-specific comments to say "On Win32 ..." now that AIX is using the same code too
    p4raw-id: //depot/perl@25212

--- makedef.pl
+++ makedef.pl
@@ -38,15 +38,16 @@ exists $PLATFORM{$PLATFORM} || die "PLATFORM must be one of: @PLATFORM\n";
 
 if ($PLATFORM eq 'win32' or $PLATFORM eq "aix") {
 	# Add the compile-time options that miniperl was built with to %define.
-	# These are not the same options as perl itself will be built with since
-	# miniperl is built with a canned config (one of the win32/config_H.*)
-	# and none of the BUILDOPT's that are set in the makefiles, but they do
-	# include some #define's that are hard-coded in various source files and
-	# header files and don't include any BUILDOPT's that the user might have
-	# chosen to disable because the canned configs are minimal configs that
-	# don't include any of those options.
+	# On Win32 these are not the same options as perl itself will be built
+	# with since miniperl is built with a canned config (one of the win32/
+	# config_H.*) and none of the BUILDOPT's that are set in the makefiles,
+	# but they do include some #define's that are hard-coded in various
+	# source files and header files and don't include any BUILDOPT's that
+	# the user might have chosen to disable because the canned configs are
+	# minimal configs that don't include any of those options.
 	my $config = `$^X -Ilib -V`;
-	my($options) = $config =~ /^  Compile-time options: (.*?)^  \S/ms;
+	my($options) = $config =~ /^  Compile-time options: (.*?)\n^  \S/ms;
+	$options =~ s/\s+/ /g;
 	print STDERR "Options: ($options)\n";
 	foreach (split /\s+/, $options) {
 		$define{$_} = 1;
@@ -150,8 +151,9 @@ if ($define{USE_ITHREADS} && $PLATFORM ne 'win32' && $^O ne 'darwin') {
 
 my $sym_ord = 0;
 
+print STDERR "Defines: (" . join(' ', sort keys %define) . ")\n";
+
 if ($PLATFORM =~ /^win(?:32|ce)$/) {
-    warn join(' ',keys %define)."\n";
     ($dll = ($define{PERL_DLL} || "perl59")) =~ s/\.dll$//i;
     print "LIBRARY $dll\n";
     print "DESCRIPTION 'Perl interpreter'\n";
