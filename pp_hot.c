906679c7ad5e5ea58a1e4bf1de62471e4fa4a045
Rafael Garcia-Suarez <rgarciasuarez@gmail.com>
2005-10-31 10:44:32 +0000

    is_utf8_string_loc() is now a macro, don't use its Perl_-prefixed form
    p4raw-id: //depot/perl@25916

--- pp_hot.c
+++ pp_hot.c
@@ -1610,7 +1610,7 @@ Perl_do_readline(pTHX)
 	     const U8 *f;
 	     
 	     if (ckWARN(WARN_UTF8) &&
-		    !Perl_is_utf8_string_loc(aTHX_ s, len, &f))
+		    !is_utf8_string_loc(aTHX_ s, len, &f))
 		  /* Emulate :encoding(utf8) warning in the same case. */
 		  Perl_warner(aTHX_ packWARN(WARN_UTF8),
 			      "utf8 \"\\x%02X\" does not map to Unicode",
