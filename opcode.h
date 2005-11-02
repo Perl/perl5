cd299c6e57209410a1331dd3e21dad7fec4cce55
Rafael Garcia-Suarez <rgarciasuarez@gmail.com>
2005-11-02 13:14:19 +0000

    Regen headers
    p4raw-id: //depot/perl@25954

--- opcode.h
+++ opcode.h
@@ -1646,7 +1646,7 @@ EXT const U32 PL_opargs[] = {
 	0x00017604,	/* pop */
 	0x00017604,	/* shift */
 	0x0004691d,	/* unshift */
-	0x0005a801,	/* sort */
+	0x0005a841,	/* sort */
 	0x00004809,	/* reverse */
 	0x0004a841,	/* grepstart */
 	0x00000648,	/* grepwhile */
