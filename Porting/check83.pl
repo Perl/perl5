a9b610e983e597edf8d9f9d6eeb62f1e3a3db482
Yitzchak Scott-Thoennes <sthoenna@efn.org>
2005-12-10 23:19:58 -0800

    Re: check83.pl Message-ID: <20051211151958.GA6188@efn.org>
    p4raw-id: //depot/perl@26338

--- Porting/check83.pl
+++ Porting/check83.pl
@@ -9,25 +9,22 @@
 # "no filename shall be longer than eight and a suffix if present
 # not longer than three".
 
-# TODO: this doesn't actually check for *directory entries*, what this
-# does is to check for *MANIFEST entries*, which are only files, not
-# directories.  In other words, a 8.3 conflict between a directory
-# "abcdefghx" and a file "abcdefghy" wouldn't be noticed-- or even for
-# a directory "abcdefgh" and a file "abcdefghy".
+my %seen;
 
 sub eight_dot_three {
+    next if $seen{$_[0]}++;
     my ($dir, $base, $ext) = ($_[0] =~ m!^(?:(.+)/)?([^/.]+)(?:\.([^/.]+))?$!);
-    my $file = $base . defined $ext ? ".$ext" : "";
+    my $file = $base . ( defined $ext ? ".$ext" : "" );
     $base = substr($base, 0, 8);
     $ext  = substr($ext,  0, 3) if defined $ext;
     if ($dir =~ /\./)  {
-	warn "$dir: directory name contains '.'\n";
+	print "directory name contains '.': $dir\n";
     }
     if ($file =~ /[^A-Za-z0-9\._-]/) {
-	warn "$file: filename contains non-portable characters\n";
+	print "filename contains non-portable characters: $_[0]\n";
     }
     if (length $file > 30) {
-	warn "$file: filename longer than 30 characters\n"; # make up a limit
+	print "filename longer than 30 characters: $_[0]\n"; # make up a limit
     }
     if (defined $dir) {
 	return ($dir, defined $ext ? "$dir/$base.$ext" : "$dir/$base");
@@ -43,16 +40,18 @@ if (open(MANIFEST, "MANIFEST")) {
 	chomp;
 	s/\s.+//;
 	unless (-f) {
-	    warn "$_: missing\n";
+	    print "missing: $_\n";
 	    next;
 	}
 	if (tr/././ > 1) {
-	    print "$_: more than one dot\n";
+	    print "more than one dot: $_\n";
 	    next;
 	}
-	my ($dir, $edt) = eight_dot_three($_);
-	($dir, $edt) = map { lc } ($dir, $edt);
-	push @{$dir{$dir}->{$edt}}, $_;
+	while (m!/|\z!g) {
+	    my ($dir, $edt) = eight_dot_three($`);
+	    ($dir, $edt) = map { lc } ($dir, $edt);
+	    push @{$dir{$dir}->{$edt}}, $_;
+	}
     }
 } else {
     die "$0: MANIFEST: $!\n";
@@ -62,7 +61,7 @@ for my $dir (sort keys %dir) {
     for my $edt (keys %{$dir{$dir}}) {
 	my @files = @{$dir{$dir}->{$edt}};
 	if (@files > 1) {
-	    print "@files: directory $dir conflict $edt\n";
+	    print "directory $dir conflict $edt: @files\n";
 	}
     }
 }
