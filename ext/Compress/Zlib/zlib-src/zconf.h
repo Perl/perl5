b70307b60b7dcc706d4c6a9deaa669f6a0a9a0d9
Rafael Garcia-Suarez <rgarciasuarez@gmail.com>
2005-12-23 11:06:34 +0000

    Fix build of Compress::Zlib on MVS (by Paul Marquess)
    p4raw-id: //depot/perl@26468

--- ext/Compress/Zlib/zlib-src/zconf.h
+++ ext/Compress/Zlib/zlib-src/zconf.h
@@ -307,9 +307,6 @@ typedef uLong FAR uLongf;
 
 #if defined(__MVS__)
 #  define NO_vsnprintf
-#  ifdef FAR
-#    undef FAR
-#  endif
 #endif
 
 /* MVS linker does not support external names larger than 8 bytes */
