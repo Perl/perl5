8e6b4db615e712f1ce0853278b06e6a52c1c0df2
Philippe M. Chiasson <gozer@ActiveState.com>
2006-01-26 05:39:59 -0800

    Change 26781/26830 (SvRTRIM should also write a new '\0' at the end) can cause segfaults From: "Philippe M. Chiasson" <gozer@ActiveState.com> Message-ID: <43D941AF.1060503@ActiveState.com> Date: Thu, 26 Jan 2006 13:39:59 -0800
    p4raw-id: //depot/perl@26952

--- mg.c
+++ mg.c
@@ -643,10 +643,12 @@ Perl_magic_len(pTHX_ SV *sv, MAGIC *mg)
 #define SvRTRIM(sv) STMT_START { \
     STRLEN len = SvCUR(sv); \
     char * const p = SvPVX(sv); \
-    while (len > 0 && isSPACE(p[len-1])) \
-	--len; \
-    SvCUR_set(sv, len); \
-    p[len] = '\0'; \
+    if (p) { \
+	while (len > 0 && isSPACE(p[len-1])) \
+	   --len; \
+	SvCUR_set(sv, len); \
+	p[len] = '\0'; \
+    } \
 } STMT_END
 
 int
