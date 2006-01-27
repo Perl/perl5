eae92ea03dd0fc112e5aeae640f2e5f76eddcd7e
Gisle Aas <gisle@activestate.com>
2006-01-27 08:49:34 +0000

    Safer implementation of RTRIM(). Don't rely on SvCUR to be sane unless SvPOK.
    p4raw-id: //depot/perl@26953

--- mg.c
+++ mg.c
@@ -641,9 +641,9 @@ Perl_magic_len(pTHX_ SV *sv, MAGIC *mg)
 }
 
 #define SvRTRIM(sv) STMT_START { \
-    STRLEN len = SvCUR(sv); \
-    char * const p = SvPVX(sv); \
-    if (p) { \
+    if (SvPOK(sv)) { \
+        STRLEN len = SvCUR(sv); \
+        char * const p = SvPVX(sv); \
 	while (len > 0 && isSPACE(p[len-1])) \
 	   --len; \
 	SvCUR_set(sv, len); \
