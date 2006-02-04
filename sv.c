85dec29a83aced8b747bed98a9d9b9fada93229a
Nicholas Clark <nick@ccl4.org>
2006-02-04 22:51:17 +0000

    bcc was issuing a warning about this line in Perl_sv_2cv, so tweak it to keep it happy.
    p4raw-id: //depot/perl@27086

--- sv.c
+++ sv.c
@@ -6992,8 +6992,11 @@ Perl_sv_2cv(pTHX_ SV *sv, HV **st, GV **gvp, I32 lref)
     GV *gv = NULL;
     CV *cv = NULL;
 
-    if (!sv)
-	return *st = NULL, *gvp = NULL, NULL;
+    if (!sv) {
+	*st = NULL;
+	*gvp = NULL;
+	return NULL;
+    }
     switch (SvTYPE(sv)) {
     case SVt_PVCV:
 	*st = CvSTASH(sv);
