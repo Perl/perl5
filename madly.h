d6ae750195600e01c65b55bcabc0deaf4194d8b9
Nicholas Clark <nick@ccl4.org>
2006-03-10 18:53:01 +0000

    All the autogenerated files from change 27462.
    p4raw-id: //depot/perl@27463

--- madly.h
+++ madly.h
@@ -53,47 +53,51 @@
      ELSIF = 278,
      CONTINUE = 279,
      FOR = 280,
-     LOOPEX = 281,
-     DOTDOT = 282,
-     FUNC0 = 283,
-     FUNC1 = 284,
-     FUNC = 285,
-     UNIOP = 286,
-     LSTOP = 287,
-     RELOP = 288,
-     EQOP = 289,
-     MULOP = 290,
-     ADDOP = 291,
-     DOLSHARP = 292,
-     DO = 293,
-     HASHBRACK = 294,
-     NOAMP = 295,
-     LOCAL = 296,
-     MY = 297,
-     MYSUB = 298,
-     COLONATTR = 299,
-     PREC_LOW = 300,
-     DOROP = 301,
-     OROP = 302,
-     ANDOP = 303,
-     NOTOP = 304,
-     ASSIGNOP = 305,
-     DORDOR = 306,
-     OROR = 307,
-     ANDAND = 308,
-     BITOROP = 309,
-     BITANDOP = 310,
-     SHIFTOP = 311,
-     MATCHOP = 312,
-     REFGEN = 313,
-     UMINUS = 314,
-     POWOP = 315,
-     POSTDEC = 316,
-     POSTINC = 317,
-     PREDEC = 318,
-     PREINC = 319,
-     ARROW = 320,
-     PEG = 321
+     GIVEN = 281,
+     WHEN = 282,
+     DEFAULT = 283,
+     LOOPEX = 284,
+     DOTDOT = 285,
+     FUNC0 = 286,
+     FUNC1 = 287,
+     FUNC = 288,
+     UNIOP = 289,
+     LSTOP = 290,
+     RELOP = 291,
+     EQOP = 292,
+     MULOP = 293,
+     ADDOP = 294,
+     DOLSHARP = 295,
+     DO = 296,
+     HASHBRACK = 297,
+     NOAMP = 298,
+     LOCAL = 299,
+     MY = 300,
+     MYSUB = 301,
+     REQUIRE = 302,
+     COLONATTR = 303,
+     PREC_LOW = 304,
+     DOROP = 305,
+     OROP = 306,
+     ANDOP = 307,
+     NOTOP = 308,
+     ASSIGNOP = 309,
+     DORDOR = 310,
+     OROR = 311,
+     ANDAND = 312,
+     BITOROP = 313,
+     BITANDOP = 314,
+     SHIFTOP = 315,
+     MATCHOP = 316,
+     REFGEN = 317,
+     UMINUS = 318,
+     POWOP = 319,
+     POSTDEC = 320,
+     POSTINC = 321,
+     PREDEC = 322,
+     PREINC = 323,
+     ARROW = 324,
+     PEG = 325
    };
 #endif
 /* Tokens.  */
@@ -120,54 +124,58 @@
 #define ELSIF 278
 #define CONTINUE 279
 #define FOR 280
-#define LOOPEX 281
-#define DOTDOT 282
-#define FUNC0 283
-#define FUNC1 284
-#define FUNC 285
-#define UNIOP 286
-#define LSTOP 287
-#define RELOP 288
-#define EQOP 289
-#define MULOP 290
-#define ADDOP 291
-#define DOLSHARP 292
-#define DO 293
-#define HASHBRACK 294
-#define NOAMP 295
-#define LOCAL 296
-#define MY 297
-#define MYSUB 298
-#define COLONATTR 299
-#define PREC_LOW 300
-#define DOROP 301
-#define OROP 302
-#define ANDOP 303
-#define NOTOP 304
-#define ASSIGNOP 305
-#define DORDOR 306
-#define OROR 307
-#define ANDAND 308
-#define BITOROP 309
-#define BITANDOP 310
-#define SHIFTOP 311
-#define MATCHOP 312
-#define REFGEN 313
-#define UMINUS 314
-#define POWOP 315
-#define POSTDEC 316
-#define POSTINC 317
-#define PREDEC 318
-#define PREINC 319
-#define ARROW 320
-#define PEG 321
+#define GIVEN 281
+#define WHEN 282
+#define DEFAULT 283
+#define LOOPEX 284
+#define DOTDOT 285
+#define FUNC0 286
+#define FUNC1 287
+#define FUNC 288
+#define UNIOP 289
+#define LSTOP 290
+#define RELOP 291
+#define EQOP 292
+#define MULOP 293
+#define ADDOP 294
+#define DOLSHARP 295
+#define DO 296
+#define HASHBRACK 297
+#define NOAMP 298
+#define LOCAL 299
+#define MY 300
+#define MYSUB 301
+#define REQUIRE 302
+#define COLONATTR 303
+#define PREC_LOW 304
+#define DOROP 305
+#define OROP 306
+#define ANDOP 307
+#define NOTOP 308
+#define ASSIGNOP 309
+#define DORDOR 310
+#define OROR 311
+#define ANDAND 312
+#define BITOROP 313
+#define BITANDOP 314
+#define SHIFTOP 315
+#define MATCHOP 316
+#define REFGEN 317
+#define UMINUS 318
+#define POWOP 319
+#define POSTDEC 320
+#define POSTINC 321
+#define PREDEC 322
+#define PREINC 323
+#define ARROW 324
+#define PEG 325
 
 
 
 
 #endif /* PERL_CORE */
 #if ! defined (YYSTYPE) && ! defined (YYSTYPE_IS_DECLARED)
-#line 21 "madly.y"
+#line 22 "madly.y"
 typedef union YYSTYPE {
     I32	ival;
     char *pval;
@@ -176,7 +184,7 @@ typedef union YYSTYPE {
     GV *gvval;
 } YYSTYPE;
 /* Line 1447 of yacc.c.  */
-#line 178 "madly.h"
+#line 186 "madly.h"
 # define yystype YYSTYPE /* obsolescent; will be withdrawn */
 # define YYSTYPE_IS_DECLARED 1
 # define YYSTYPE_IS_TRIVIAL 1
