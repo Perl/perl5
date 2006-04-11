a51caccf222faf8588ed061240391957b9a0e70d
Nicholas Clark <nick@ccl4.org>
2006-04-11 16:17:13 +0000

    Stop Perl_newSVpvn_share() potentially leaking the return result from bytes_from_utf8().
    p4raw-id: //depot/perl@27766

--- sv.c
+++ sv.c
@@ -6990,6 +6990,8 @@ Perl_newSVpvn_share(pTHX_ const char *src, I32 len, U32 hash)
     dVAR;
     register SV *sv;
     bool is_utf8 = FALSE;
+    const char *const orig_src = src;
+
     if (len < 0) {
 	STRLEN tmplen = -len;
         is_utf8 = TRUE;
@@ -7009,6 +7011,8 @@ Perl_newSVpvn_share(pTHX_ const char *src, I32 len, U32 hash)
     SvPOK_on(sv);
     if (is_utf8)
         SvUTF8_on(sv);
+    if (src != orig_src)
+	Safefree(src);
     return sv;
 }
 
