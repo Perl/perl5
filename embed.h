0b0c55ab0a3e8da50e366c0d5292769452b6fc73
Steve Hay <SteveHay@planit.com>
2006-05-03 08:25:27 +0000

    Make Perl_unshare_hek visible to core extensions without being public
    See the comments in:
    http://www.xray.mpe.mpg.de/mailing-lists/perl5-porters/2006-04/msg00480.html
    (which were put into practice in changes #27800 and #27848)
    p4raw-link: @27800 on //depot/perl: 1242dfaee525d4d0ab20ac8ae62b3f0de03fa88e
    
    p4raw-id: //depot/perl@28073

--- embed.h
+++ embed.h
@@ -903,7 +903,9 @@
 #define unpack_str		Perl_unpack_str
 #define unpackstring		Perl_unpackstring
 #define unsharepvn		Perl_unsharepvn
+#if defined(PERL_CORE) || defined(PERL_EXT)
 #define unshare_hek		Perl_unshare_hek
+#endif
 #ifdef PERL_MAD
 #ifdef PERL_CORE
 #define utilize			Perl_utilize
@@ -3063,7 +3065,9 @@
 #define unpack_str(a,b,c,d,e,f,g,h)	Perl_unpack_str(aTHX_ a,b,c,d,e,f,g,h)
 #define unpackstring(a,b,c,d,e)	Perl_unpackstring(aTHX_ a,b,c,d,e)
 #define unsharepvn(a,b,c)	Perl_unsharepvn(aTHX_ a,b,c)
+#if defined(PERL_CORE) || defined(PERL_EXT)
 #define unshare_hek(a)		Perl_unshare_hek(aTHX_ a)
+#endif
 #ifdef PERL_MAD
 #ifdef PERL_CORE
 #define utilize(a,b,c,d,e)	Perl_utilize(aTHX_ a,b,c,d,e)
