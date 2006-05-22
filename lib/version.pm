4c0d88fa74047e2136ab0d51a21a0f536d0d1bcb
Rafael Garcia-Suarez <rgarciasuarez@gmail.com>
2006-05-22 16:51:36 +0000

    Amend version.pm so it correctly loads the XS version of qv()
    p4raw-id: //depot/perl@28280

--- lib/version.pm
+++ lib/version.pm
@@ -6,22 +6,10 @@ use strict;
 
 use vars qw(@ISA $VERSION $CLASS *qv);
 
-$VERSION = "0.60";
+$VERSION = "0.60_01";
 $VERSION = eval($VERSION);
 
 $CLASS = 'version';
-
-eval "use version::vxs $VERSION";
-if ( $@ ) { # don't have the XS version installed
-    eval "use version::vpp $VERSION"; # don't tempt fate
-    die "$@" if ( $@ );
-    push @ISA, "version::vpp";
-    *version::qv = \&version::vpp::qv;
-}
-else { # use XS module
-    push @ISA, "version::vxs";
-    *version::qv = \&version::vxs::qv;
-}
 
 # Preloaded methods go here.
 sub import {
