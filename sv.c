147bc37422bb9ff10b7db69f9d76b4e4e70636d7
Nicholas Clark <nick@ccl4.org>
2006-10-22 22:51:59 +0000

    Fix the other bug introduced by change 29079
    p4raw-id: //depot/perl@29086

--- sv.c
+++ sv.c
@@ -10668,7 +10668,8 @@ Perl_ss_dup(pTHX_ PerlInterpreter *proto_perl, CLONE_PARAMS* param)
 	    TOPPTR(nss,ix) = DUP_WARNINGS((STRLEN*)ptr);
 	    break;
 	default:
-	    Perl_croak(aTHX_ "panic: ss_dup inconsistency (%"IVdf")", (IV) i);
+	    Perl_croak(aTHX_
+		       "panic: ss_dup inconsistency (%"IVdf")", (IV) type);
 	}
     }
 
