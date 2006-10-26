cdbcf450a6724e04d0f4998b4e10f0f280d99586
Jerry D. Hedden <jdhedden@cpan.org>
2006-10-26 01:35:46 -0700

    Integrate: [ 26522] add tests for MY_CXT API and improve its documentation
    [ 29124]
    Subject: [PATCH] Change MY_CXT ref in perl.h
    From: "Jerry D. Hedden" <jdhedden@yahoo.com>
    Message-ID: <20061026153546.71547.qmail@web30214.mail.mud.yahoo.com>
    p4raw-link: @29124 on //depot/perl: 7588b095b4fd2549b6878a1928233682c0412566
    p4raw-link: @26522 on //depot/perl: 85ce96a160e902929b94338ada20cf46b265d595
    
    p4raw-id: //depot/maint-5.8/perl@30262
    p4raw-branched: from //depot/perl@30261 'branch in'
    	ext/XS/APItest/t/my_cxt.t
    p4raw-integrated: from //depot/perl@29124 'merge in' perl.h (@29075..)
    p4raw-integrated: from //depot/perl@26522 'merge in'
    	ext/XS/APItest/APItest.pm (@25261..) pod/perlxs.pod (@26073..)
    	ext/XS/APItest/APItest.xs (@26171..) MANIFEST (@26498..)

--- ext/XS/APItest/APItest.pm
+++ ext/XS/APItest/APItest.pm
@@ -21,6 +21,7 @@ our @EXPORT = qw( print_double print_int print_long
 		  G_KEEPERR G_NODEBUG G_METHOD
 		  exception
 		  mycroak strtab
+		  my_cxt_getint my_cxt_getsv my_cxt_setint my_cxt_setsv
 );
 
 # from cop.h 
@@ -34,7 +35,7 @@ sub G_KEEPERR()	{  16 }
 sub G_NODEBUG()	{  32 }
 sub G_METHOD()	{  64 }
 
-our $VERSION = '0.08';
+our $VERSION = '0.09';
 
 bootstrap XS::APItest $VERSION;
 
