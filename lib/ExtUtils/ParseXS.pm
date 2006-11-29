70414f6c616c3d07a0332e24a018679b54402963
Steve Peters <steve@fisharerojo.org>
2006-11-29 15:17:59 +0000

    Upgrade to ExtUtils-ParseXS-2.17.  Actuall just caught up with most of the patches to bleadperl.  The remaining change missing is #29252, so, the version has been bumped to 2.17_01.
    p4raw-id: //depot/perl@29418

--- lib/ExtUtils/ParseXS.pm
+++ lib/ExtUtils/ParseXS.pm
@@ -18,7 +18,7 @@ my(@XSStack);	# Stack of conditionals and INCLUDEs
 my($XSS_work_idx, $cpp_next_tmp);
 
 use vars qw($VERSION);
-$VERSION = '2.16_02';
+$VERSION = '2.17_01';
 
 use vars qw(%input_expr %output_expr $ProtoUsed @InitFileCode $FH $proto_re $Overload $errors $Fallback
 	    $cplusplus $hiertype $WantPrototypes $WantVersionChk $except $WantLineNumbers
