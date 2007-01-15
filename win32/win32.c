29e13651528c7c44e8f5d0007c94bec0a96fe2d1
Jan Dubois <jand@activestate.com>
2007-01-15 11:25:11 -0800

    Re: [PATCH] win32_async_check() can still loop indefinitely Message-ID: <rigoq25vda3sg4i7ppdb5o2jhvuiq15kqj@4ax.com>
    p4raw-id: //depot/perl@29868

--- win32/win32.c
+++ win32/win32.c
@@ -2143,7 +2143,7 @@ win32_msgwait(pTHX_ DWORD count, LPHANDLE handles, DWORD timeout, LPDWORD result
 	timeout += ticks;
     }
     while (1) {
-	DWORD result = MsgWaitForMultipleObjects(count,handles,FALSE,timeout-ticks, QS_ALLEVENTS);
+	DWORD result = MsgWaitForMultipleObjects(count,handles,FALSE,timeout-ticks, QS_POSTMESSAGE|QS_TIMER);
 	if (resultp)
 	   *resultp = result;
 	if (result == WAIT_TIMEOUT) {
