1fef4616ad97a72dc45194bd8ac11e63e518aba2
Rafael Garcia-Suarez <rgarciasuarez@gmail.com>
2007-02-07 09:04:56 +0000

    Wrap the SvTYPE macro definition in parens.
    p4raw-id: //depot/perl@30153

--- sv.h
+++ sv.h
@@ -274,7 +274,7 @@ perform the upgrade if necessary.  See C<svtype>.
 #endif
 
 #define SVTYPEMASK	0xff
-#define SvTYPE(sv)	(svtype)((sv)->sv_flags & SVTYPEMASK)
+#define SvTYPE(sv)	((svtype)((sv)->sv_flags & SVTYPEMASK))
 
 /* Sadly there are some parts of the core that have pointers to already-freed
    SV heads, and rely on being able to tell that they are now free. So mark
