7f759b241dafc9badf8e99e6e77db8c8b85f2939
Nicholas Clark <nick@ccl4.org>
2007-02-14 22:33:56 +0000

    Integrate: [ 27346] Upgrade to CPAN 1.87 p4raw-link: @27346 on //depot/perl: 87892b7316b5db4861dda5a8422f3d25156801f5
    p4raw-id: //depot/maint-5.8/perl@30305
    p4raw-integrated: from //depot/perl@27346 'copy in'
    	lib/CPAN/FirstTime.pm lib/CPAN/HandleConfig.pm
    	lib/CPAN/Tarzip.pm (@27192..) lib/CPAN.pm lib/CPAN/SIGNATURE
    	(@27311..)

--- lib/CPAN/FirstTime.pm
+++ lib/CPAN/FirstTime.pm
@@ -2,7 +2,7 @@
 package CPAN::Mirrored::By;
 use strict;
 use vars qw($VERSION);
-$VERSION = sprintf "%.6f", substr(q$Rev: 561 $,4)/1000000 + 5.4;
+$VERSION = sprintf "%.6f", substr(q$Rev: 657 $,4)/1000000 + 5.4;
 
 sub new { 
     my($self,@arg) = @_;
@@ -21,7 +21,7 @@ use File::Basename ();
 use File::Path ();
 use File::Spec;
 use vars qw($VERSION);
-$VERSION = sprintf "%.6f", substr(q$Rev: 561 $,4)/1000000 + 5.4;
+$VERSION = sprintf "%.6f", substr(q$Rev: 657 $,4)/1000000 + 5.4;
 
 =head1 NAME
 
@@ -51,7 +51,8 @@ sub init {
     unless ($CPAN::VERSION) {
 	require CPAN::Nox;
     }
-    eval {require CPAN::Config;};
+    require CPAN::HandleConfig;
+    CPAN::HandleConfig::require_myconfig_or_config();
     $CPAN::Config ||= {};
     local($/) = "\n";
     local($\) = "";
