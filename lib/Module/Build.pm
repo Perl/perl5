6e3f52c94c501ef5a8c239dec80b253b2f172b68
Rafael Garcia-Suarez <rgarciasuarez@gmail.com>
2007-03-01 09:19:07 +0000

    Teach Module::Build about DragonflyBSD
    p4raw-id: //depot/perl@30432

--- lib/Module/Build.pm
+++ lib/Module/Build.pm
@@ -15,7 +15,7 @@ use Module::Build::Base;
 
 use vars qw($VERSION @ISA);
 @ISA = qw(Module::Build::Base);
-$VERSION = '0.2806';
+$VERSION = '0.2806_01';
 $VERSION = eval $VERSION;
 
 # Okay, this is the brute-force method of finding out what kind of
@@ -26,6 +26,7 @@ my %OSTYPES = qw(
 		 aix       Unix
 		 bsdos     Unix
 		 dgux      Unix
+		 dragonfly Unix
 		 dynixptx  Unix
 		 freebsd   Unix
 		 linux     Unix
