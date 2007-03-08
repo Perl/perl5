2440f57eb96c4329967f5d7bc390ffabf1449b9c
Anton Berezin <tobez@tobez.org>
2007-03-08 13:08:45 +0100

    Default to ELF on FreeBSD when objformat is not there Message-ID: <20070308110845.GA24332@heechee.tobez.org>
    p4raw-id: //depot/perl@30512

--- hints/freebsd.sh
+++ hints/freebsd.sh
@@ -116,17 +116,17 @@ case "$osvers" in
 
 *)
         objformat=`/usr/bin/objformat`
-        if [ x$objformat = xelf ]; then
-            libpth="/usr/lib /usr/local/lib"
-            glibpth="/usr/lib /usr/local/lib"
-            ldflags="-Wl,-E "
-            lddlflags="-shared "
-        else
+        if [ x$objformat = xaout ]; then
             if [ -e /usr/lib/aout ]; then
                 libpth="/usr/lib/aout /usr/local/lib /usr/lib"
                 glibpth="/usr/lib/aout /usr/local/lib /usr/lib"
             fi
             lddlflags='-Bshareable'
+        else
+            libpth="/usr/lib /usr/local/lib"
+            glibpth="/usr/lib /usr/local/lib"
+            ldflags="-Wl,-E "
+            lddlflags="-shared "
         fi
         cccdlflags='-DPIC -fPIC'
         ;;
