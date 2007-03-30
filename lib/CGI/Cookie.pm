ff405b7394b66365d9c93cd5e3fb79977d8f5321
Nicholas Clark <nick@ccl4.org>
2007-03-30 19:02:24 +0000

    Integrate: [ 30778] Upgrade to CGI.pm-3.28 p4raw-link: @30778 on //depot/perl: 2c454cbd693e2f6a6c12d8a4c7f82f006f7cc257
    p4raw-id: //depot/maint-5.8/perl@30795
    p4raw-integrated: from //depot/perl@30794 'copy in' lib/CGI.pm
    	lib/CGI/Changes (@30486..) 'merge in' lib/CGI/Cookie.pm
    	(@28746..)

--- lib/CGI/Cookie.pm
+++ lib/CGI/Cookie.pm
@@ -13,7 +13,7 @@ package CGI::Cookie;
 # wish, but if you redistribute a modified version, please attach a note
 # listing the modifications you have made.
 
-$CGI::Cookie::VERSION='1.27';
+$CGI::Cookie::VERSION='1.28';
 
 use CGI::Util qw(rearrange unescape escape);
 use CGI;
@@ -51,7 +51,7 @@ sub fetch {
    my %results;
    my($key,$value);
    
-   my(@pairs) = split("; ?",$raw_cookie);
+   my(@pairs) = split("[;,] ?",$raw_cookie);
    foreach (@pairs) {
      s/\s*(.*?)\s*/$1/;
      if (/^([^=]+)=(.*)/) {
