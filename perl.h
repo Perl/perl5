944c4fdaac207ce2839ee03a49666f3fe72b9802
Steve Peters <steve@fisharerojo.org>
2007-04-05 16:32:19 +0000

    g++ on NetBSD needs to have environ declared as well.
    p4raw-id: //depot/perl@30848

--- perl.h
+++ perl.h
@@ -1201,7 +1201,7 @@ EXTERN_C char **environ;
 #endif
 
 #if defined(__cplusplus)
-#  if defined(__OpenBSD__) || defined(__FreeBSD__)
+#  if defined(__OpenBSD__) || defined(__FreeBSD__) || defined(__NetBSD__)
 EXTERN_C char **environ;
 #  elif defined(__CYGWIN__)
 EXTERN_C char *crypt(const char *, const char *);
