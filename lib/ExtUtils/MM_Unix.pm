2679043380ccf1423491dfb0147e322b03f679e3
Steve Peters <steve@fisharerojo.org>
2007-04-10 16:36:08 +0000

    Version change to ExtUtils::MM_Unix missed in change #30380. p4raw-link: @30380 on //depot/perl: 1e65eb70bcb514e61cdd8408bdbe4e5a20b7fd95
    p4raw-id: //depot/perl@30896

--- lib/ExtUtils/MM_Unix.pm
+++ lib/ExtUtils/MM_Unix.pm
@@ -18,7 +18,7 @@ use vars qw($VERSION @ISA
 
 use ExtUtils::MakeMaker qw($Verbose neatvalue);
 
-$VERSION = '1.51_02';
+$VERSION = '1.52_02';
 
 require ExtUtils::MM_Any;
 @ISA = qw(ExtUtils::MM_Any);
