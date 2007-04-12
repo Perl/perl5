1abe45988edc1c4df0edabb952007ef1b6fe20e5
Nicholas Clark <nick@ccl4.org>
2007-04-12 21:03:08 +0000

    Bump the VERSION of ExtUtils::Constant::Base.
    p4raw-id: //depot/perl@30940

--- lib/ExtUtils/Constant/Base.pm
+++ lib/ExtUtils/Constant/Base.pm
@@ -5,7 +5,7 @@ use vars qw($VERSION);
 use Carp;
 use Text::Wrap;
 use ExtUtils::Constant::Utils qw(C_stringify perl_stringify);
-$VERSION = '0.03_01';
+$VERSION = '0.04';
 
 use constant is_perl56 => ($] < 5.007 && $] > 5.005_50);
 
