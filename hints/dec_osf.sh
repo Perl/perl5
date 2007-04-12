ee34af25fd0d43d821b583560e4abf3a14edc5d1
Jarkko Hietaniemi <jhi@iki.fi>
2007-04-12 08:19:30 +0300

    g++ in Tru64: align sockaddr and sockaddr_in defs Message-Id: <200704120219.l3C2JUGZ294947@kosh.hut.fi>
    p4raw-id: //depot/perl@30918

--- hints/dec_osf.sh
+++ hints/dec_osf.sh
@@ -167,9 +167,10 @@ ccflags="$ccflags $_ccflags_strict_ansi"
 # its moral equivalent, _XOPEN_SOURCE=500 (which would
 # define a lot of the required prototypes for us), because
 # the gcc-processed version of <sys/wait.h> contains fatally
-# conflicting prototypes for wait3().
+# conflicting prototypes for wait3().  The _SOCKADDR_LEN is
+# needed to get struct sockaddr and struct sockaddr_in to align.
 case "$cc" in
-*g++*) ccflags="$ccflags -D_XOPEN_SOURCE -D_OSF_SOURCE -D_AES_SOURCE -D_BSD -D_POSIX_C_SOURCE=199309L -D_POSIX_PII_SOCKET" ;;
+*g++*) ccflags="$ccflags -D_XOPEN_SOURCE -D_OSF_SOURCE -D_AES_SOURCE -D_BSD -D_POSIX_C_SOURCE=199309L -D_POSIX_PII_SOCKET -D_SOCKADDR_LEN" ;;
 esac
 
 # for gcc the Configure knows about the -fpic:
