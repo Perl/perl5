31db09eff5a7858ca0bfee00128a8a84579b2f52
Jerry D. Hedden <jdhedden@cpan.org>
2007-05-14 04:17:51 -0400

    threads::shared 1.11 From: "Jerry D. Hedden" <jdhedden@cpan.org> Message-ID: <1ff86f510705140517n1c140d96nca36667bcbbc27f@mail.gmail.com> Date: Mon, 14 May 2007 08:17:51 -0400
    p4raw-id: //depot/perl@31216

--- ext/threads/shared/shared.pm
+++ ext/threads/shared/shared.pm
@@ -5,7 +5,7 @@ use 5.008;
 use strict;
 use warnings;
 
-our $VERSION = '1.09_02';
+our $VERSION = '1.11';
 my $XS_VERSION = $VERSION;
 $VERSION = eval $VERSION;
 
@@ -73,7 +73,7 @@ threads::shared - Perl extension for sharing data structures between threads
 
 =head1 VERSION
 
-This document describes threads::shared version 1.09
+This document describes threads::shared version 1.11
 
 =head1 SYNOPSIS
 
@@ -368,7 +368,7 @@ L<threads::shared> Discussion Forum on CPAN:
 L<http://www.cpanforum.com/dist/threads-shared>
 
 Annotated POD for L<threads::shared>:
-L<http://annocpan.org/~JDHEDDEN/threads-shared-1.09/shared.pm>
+L<http://annocpan.org/~JDHEDDEN/threads-shared-1.11/shared.pm>
 
 Source repository:
 L<http://code.google.com/p/threads-shared/>
