56b27c9aa7f7f437454756acd9a09e4d194e45fe
Dave Mitchell <davem@fdisolutions.com>
2007-05-15 21:51:03 +0000

    change 31200 broke src filters used within an eval eg BEGIN { eval q(use MyFilter) } would ignore the filter
    p4raw-id: //depot/perl@31225

--- toke.c
+++ toke.c
@@ -686,7 +686,7 @@ Perl_lex_start(pTHX_ SV *line, PerlIO *rsfp, bool new_filter)
     parser->lex_state = LEX_NORMAL;
     parser->expect = XSTATE;
     parser->rsfp = rsfp;
-    parser->rsfp_filters = (new_filter || !oparser) ? NULL
+    parser->rsfp_filters = (new_filter || !oparser) ? newAV()
 		: (AV*)SvREFCNT_inc(oparser->rsfp_filters);
 
     Newx(parser->lex_brackstack, 120, char);
