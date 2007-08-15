3b9a98565e2d390457228bfe912ab7ca457a1c60
Reini Urban <rurban@x-ray.at>
2007-08-15 11:44:29 +0200

    Re: Test failure @recent bleadperls (31712,31711) Message-Id: <46C2AEDD.80400@x-ray.at>
    p4raw-id: //depot/perl@31715

--- lib/File/Spec/Cygwin.pm
+++ lib/File/Spec/Cygwin.pm
@@ -109,7 +109,8 @@ Default: 1
 =cut
 
 sub case_tolerant () {
-  my $drive = shift || "C:";
+  if ($^O ne 'cygwin') return 1;
+  my $drive = shift || "/cygdrive/c";
   my $mntopts = Cygwin::mount_flags($drive);
   if ($mntopts and ($mntopts =~ /,managed/)) {
     return 0;
