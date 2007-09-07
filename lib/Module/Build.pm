e9858669ba375a588d8365a06b81eec34cca6518
Rafael Garcia-Suarez <rgarciasuarez@gmail.com>
2007-09-07 14:03:52 +0000

    Bump version of Module::Build after change #31619 p4raw-link: @31619 on //depot/perl: f82d2ab4f80792a36288c2270b44dbb7387cc379
    p4raw-id: //depot/perl@31813

--- lib/Module/Build.pm
+++ lib/Module/Build.pm
@@ -15,7 +15,7 @@ use Module::Build::Base;
 
 use vars qw($VERSION @ISA);
 @ISA = qw(Module::Build::Base);
-$VERSION = '0.2808';
+$VERSION = '0.2808_01';
 $VERSION = eval $VERSION;
 
 # Okay, this is the brute-force method of finding out what kind of
