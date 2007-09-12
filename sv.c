5edc65b39b6317e0ab48cabbe2834afad4b21a9a
Rafael Garcia-Suarez <rgarciasuarez@gmail.com>
2007-09-12 11:48:32 +0000

    Amend change 31854 for when NAN_COMPARE_BROKEN
    p4raw-id: //depot/perl@31855

--- sv.c
+++ sv.c
@@ -9194,7 +9194,12 @@ Perl_sv_vcatpvfn(pTHX_ SV *sv, const char *pat, STRLEN patlen, va_list *args, SV
 		: SvNV(argsv);
 
 	    need = 0;
-	    if (c != 'e' && c != 'E' && nv == nv) {
+#if defined(NAN_COMPARE_BROKEN) && defined(Perl_isnan)
+	    if (c != 'e' && c != 'E' && !Perl_isnan(nv))
+#else
+	    if (c != 'e' && c != 'E' && nv == nv)
+#endif
+	    {
 		i = PERL_INT_MIN;
 		/* FIXME: if HAS_LONG_DOUBLE but not USE_LONG_DOUBLE this
 		   will cast our (long double) to (double) */
