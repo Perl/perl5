8265e3d1a493335506aedfbdd58318bf524d0b39
Nicholas Clark <nick@ccl4.org>
2007-09-20 10:20:47 +0000

    assert that what is passed into the hash functions is really an HV. (MRO code is calling hash functions during global destruction, hence the check on SVTYPEMASK.)
    p4raw-id: //depot/perl@31922

--- hv.c
+++ hv.c
@@ -422,6 +422,10 @@ Perl_hv_common(pTHX_ HV *hv, SV *keysv, const char *key, STRLEN klen,
 
     if (!hv)
 	return NULL;
+    if (SvTYPE(hv) == SVTYPEMASK)
+	return NULL;
+
+    assert(SvTYPE(hv) == SVt_PVHV);
 
     if (SvSMAGICAL(hv) && SvGMAGICAL(hv) && !(action & HV_DISABLE_UVAR_XKEY)) {
 	MAGIC* mg;
