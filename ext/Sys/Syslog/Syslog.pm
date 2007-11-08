46eb16f1dff3be6ceb73471f0435a4411a87318d
Sébastien Aperghis-Tramoni <sebastien@aperghis.net>
2007-11-08 03:05:45 +0100

    Fwd: CPAN Upload: S/SA/SAPER/Sys-Syslog-0.22.tar.gz Message-Id: <BA3BE7CB-BBCC-47EF-A8EF-569BE97133E6@free.fr>
    p4raw-id: //depot/perl@32286

--- ext/Sys/Syslog/Syslog.pm
+++ ext/Sys/Syslog/Syslog.pm
@@ -10,7 +10,7 @@ require 5.005;
 require Exporter;
 
 {   no strict 'vars';
-    $VERSION = '0.21';
+    $VERSION = '0.22';
     @ISA = qw(Exporter);
 
     %EXPORT_TAGS = (
@@ -771,7 +771,7 @@ Sys::Syslog - Perl interface to the UNIX syslog(3) calls
 
 =head1 VERSION
 
-Version 0.21
+Version 0.22
 
 =head1 SYNOPSIS
 
