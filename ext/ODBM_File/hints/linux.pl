f6fbbb567ece06280787cefc318ef0b2e5fe31e1
Rafael Garcia-Suarez <rgarciasuarez@gmail.com>
2007-11-15 07:18:02 +0000

    -e is better than -f (in case of symbolic links)
    p4raw-id: //depot/perl@32322

--- ext/ODBM_File/hints/linux.pl
+++ ext/ODBM_File/hints/linux.pl
@@ -3,6 +3,6 @@ $self->{LIBS} = ['-lgdbm'];
 
 # Debian/Ubuntu have /usr/lib/libgdbm_compat.so.3* but not this file,
 # so linking may fail
-if (-f '/usr/lib/libgdbm_compat.so') {
+if (-e '/usr/lib/libgdbm_compat.so') {
     $self->{LIBS}->[0] .= ' -lgdbm_compat';
 }
