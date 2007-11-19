96f78345cd759f05ac656cec907458f36a38aae8
Nicholas Clark <nick@ccl4.org>
2007-11-19 09:41:28 +0000

    Integrate: [ 32213] [perl #47045] CLONE_SKIP doesn't result in undef copies clarify the documentation on CLONE_SKIP, and ensure that the undef value has all its flags cleared p4raw-link: @32213 on //depot/perl: 33de8e4aff58f134481d1a594f7a9734709d8ea5
    p4raw-id: //depot/maint-5.8/perl@32398
    p4raw-integrated: from //depot/perl@32397 'merge in' pod/perlmod.pod
    	(@30685..)
    p4raw-integrated: from //depot/perl@32213 'merge in' sv.c (@32152..)

--- sv.c
+++ sv.c
@@ -9516,8 +9516,7 @@ Perl_sv_dup(pTHX_ SV *sstr, CLONE_PARAMS* param)
 
     /* don't clone objects whose class has asked us not to */
     if (SvOBJECT(sstr) && ! (SvFLAGS(SvSTASH(sstr)) & SVphv_CLONEABLE)) {
-	SvFLAGS(dstr) &= ~SVTYPEMASK;
-	SvOBJECT_off(dstr);
+	SvFLAGS(dstr) = 0;
 	return dstr;
     }
 
