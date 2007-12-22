2886f0bef4296d0a3086101fb428f8c81c532ad9
Nicholas Clark <nick@ccl4.org>
2007-12-22 16:05:50 +0000

    By moving the "can't upgrade downwards" croak() in Perl_sv_upgrade() after the type changing logic, we allow sv_upgrade() from SVt_NV to SVt_IV to "work" (SVt_NV beomes SVt_PVNV in the type changing logic) which simplifies the code for Perl_sv_setiv() slightly.
    p4raw-id: //depot/perl@32702
    
    (cherry picked from commit 3376de987afecc3e3aea14b3dea087f4b50fd3f4)

--- sv.c
+++ sv.c
@@ -1179,11 +1179,6 @@ Perl_sv_upgrade(pTHX_ register SV *sv, svtype new_type)
     if (old_type == new_type)
 	return;
 
-    if (old_type > new_type)
-	Perl_croak(aTHX_ "sv_upgrade from type %d down to type %d",
-		(int)old_type, (int)new_type);
-
-
     old_body = SvANY(sv);
 
     /* Copying structures onto other structures that have been neatly zeroed
@@ -1264,6 +1259,11 @@ Perl_sv_upgrade(pTHX_ register SV *sv, svtype new_type)
 	    Perl_croak(aTHX_ "Can't upgrade %s (%" UVuf ") to %" UVuf,
 		       sv_reftype(sv, 0), (UV) old_type, (UV) new_type);
     }
+
+    if (old_type > new_type)
+	Perl_croak(aTHX_ "sv_upgrade from type %d down to type %d",
+		(int)old_type, (int)new_type);
+
     new_type_details = bodies_by_type + new_type;
 
     SvFLAGS(sv) &= ~SVTYPEMASK;
@@ -1543,10 +1543,8 @@ Perl_sv_setiv(pTHX_ register SV *sv, IV i)
     SV_CHECK_THINKFIRST_COW_DROP(sv);
     switch (SvTYPE(sv)) {
     case SVt_NULL:
-	sv_upgrade(sv, SVt_IV);
-	break;
     case SVt_NV:
-	sv_upgrade(sv, SVt_PVNV);
+	sv_upgrade(sv, SVt_IV);
 	break;
     case SVt_RV:
     case SVt_PV:
