99710fe38e53c8e763d4758979c48cc5bc8503cf
Nicholas Clark <nick@ccl4.org>
2008-01-21 16:50:59 +0000

    In pp_subst, rxtainted is not a boolean, as it stores 2 bits of values.
    p4raw-id: //depot/perl@33033

--- pp_hot.c
+++ pp_hot.c
@@ -2051,7 +2051,7 @@ PP(pp_subst)
     I32 maxiters;
     register I32 i;
     bool once;
-    bool rxtainted;
+    U8 rxtainted;
     char *orig;
     I32 r_flags;
     register REGEXP *rx = PM_GETRE(pm);
