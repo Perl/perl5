7e8f054fecaf3261238dc3eb396d7a3e8a3cdbef
Jerry D. Hedden <jdhedden@cpan.org>
2008-01-30 04:15:25 -0500

    Integrate: [ 33154] Subject: [PATCH] threads::shared TODO test for outstanding bug From: "Jerry D. Hedden" <jdhedden@cpan.org> Date: Wed, 30 Jan 2008 09:15:25 -0500 Message-ID: <1ff86f510801300615t1f9e8c68u5ff8caaeffb1a835@mail.gmail.com>
    [ 33387]
    Subject: [PATCH] threads::shared 1.18
    From: "Jerry D. Hedden" <jdhedden@cpan.org>
    Date: Tue, 26 Feb 2008 11:29:54 -0500
    Message-ID: <1ff86f510802260829h306ea16by990af232dac22a82@mail.gmail.com>
    p4raw-link: @33387 on //depot/perl: 291f766ec79ed2f8f09e116381515c07bad45726
    p4raw-link: @33154 on //depot/perl: ea6a7c5888642f2a40f4c7124bc00d68d8d40278
    
    p4raw-id: //depot/maint-5.10/perl@33521
    p4raw-integrated: from //depot/perl@33520 'copy in'
    	ext/threads/shared/t/stress.t (@31233..)
    	ext/threads/shared/t/object.t (@32658..)
    	ext/threads/shared/Makefile.PL ext/threads/shared/shared.pm
    	(@33360..)

--- ext/threads/shared/shared.pm
+++ ext/threads/shared/shared.pm
@@ -5,7 +5,7 @@ use 5.008;
 use strict;
 use warnings;
 
-our $VERSION = '1.17';
+our $VERSION = '1.18';
 my $XS_VERSION = $VERSION;
 $VERSION = eval $VERSION;
 
@@ -73,7 +73,7 @@ threads::shared - Perl extension for sharing data structures between threads
 
 =head1 VERSION
 
-This document describes threads::shared version 1.17
+This document describes threads::shared version 1.18
 
 =head1 SYNOPSIS
 
@@ -371,7 +371,7 @@ L<threads::shared> Discussion Forum on CPAN:
 L<http://www.cpanforum.com/dist/threads-shared>
 
 Annotated POD for L<threads::shared>:
-L<http://annocpan.org/~JDHEDDEN/threads-shared-1.17/shared.pm>
+L<http://annocpan.org/~JDHEDDEN/threads-shared-1.18/shared.pm>
 
 Source repository:
 L<http://code.google.com/p/threads-shared/>
