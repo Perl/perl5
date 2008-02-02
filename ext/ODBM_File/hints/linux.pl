4856be169323dea57bf0fd2d3b540fd606d57639
Nicholas Clark <nick@ccl4.org>
2008-02-02 17:17:29 +0000

    Integrate: [ 32607] Subject: [PATCH] Escape $ENV in comments in config_h.SH From: "Jerry D. Hedden" <jdhedden@cpan.org> Date: Tue, 11 Dec 2007 09:12:32 -0500 Message-ID: <1ff86f510712110612o7bc5fa2ax9e575c0396cb2a64@mail.gmail.com>
    [ 32623]
    Subject: [PATCH cflags.SH] typo?
    From: "Robin Barker" <Robin.Barker@npl.co.uk>
    Date: Mon, 17 Dec 2007 10:27:28 -0000
    Message-ID: <46A0F33545E63740BC7563DE59CA9C6D09398B@exchsvr2.npl.ad.local>
    
    [ 32632]
    Make the hint file work as well on 64-bit linuxes,
    noticed by Wolfgang Friebel
    p4raw-link: @32632 on //depot/perl: acaeff27db8b7093f6a594fca14c6f48eaadb74a
    p4raw-link: @32623 on //depot/perl: a5105380309f5a172ff19aaf0d8159eaa0e0ada9
    p4raw-link: @32607 on //depot/perl: 47d847ba998f71d4ef4a613aea7635fe9d1cd6e8
    
    p4raw-id: //depot/maint-5.8/perl@33195
    p4raw-integrated: from //depot/perl@33192 'copy in'
    	ext/ODBM_File/hints/linux.pl (@32322..)
    p4raw-integrated: from //depot/perl@32623 'merge in' cflags.SH
    	(@32181..)
    p4raw-integrated: from //depot/perl@32607 'merge in' config_h.SH
    	(@30977..)

--- ext/ODBM_File/hints/linux.pl
+++ ext/ODBM_File/hints/linux.pl
@@ -3,6 +3,6 @@ $self->{LIBS} = ['-lgdbm'];
 
 # Debian/Ubuntu have /usr/lib/libgdbm_compat.so.3* but not this file,
 # so linking may fail
-if (-e '/usr/lib/libgdbm_compat.so') {
+if (-e '/usr/lib/libgdbm_compat.so' or -e '/usr/lib64/libgdbm_compat.so') {
     $self->{LIBS}->[0] .= ' -lgdbm_compat';
 }
