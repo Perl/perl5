3fc8c156ce3b393896131e0cffcfaf901576e0d2
Nicholas Clark <nick@ccl4.org>
2008-02-10 19:17:13 +0000

    The sv argument to gv_stashsv() is not NULL.
    p4raw-id: //depot/perl@33273
    
    (cherry-picked from commit 8798655dec8bb39ddfc6da720c750a733c8d3662)

--- proto.h
+++ proto.h
@@ -677,7 +677,9 @@ PERL_CALLCONV HV*	Perl_gv_stashpv(pTHX_ const char* name, I32 flags)
 PERL_CALLCONV HV*	Perl_gv_stashpvn(pTHX_ const char* name, U32 namelen, I32 flags)
 			__attribute__nonnull__(pTHX_1);
 
-PERL_CALLCONV HV*	Perl_gv_stashsv(pTHX_ SV* sv, I32 flags);
+PERL_CALLCONV HV*	Perl_gv_stashsv(pTHX_ SV* sv, I32 flags)
+			__attribute__nonnull__(pTHX_1);
+
 PERL_CALLCONV void	Perl_hv_clear(pTHX_ HV* hv);
 PERL_CALLCONV HV *	Perl_hv_copy_hints_hv(pTHX_ HV *const ohv);
 PERL_CALLCONV void	Perl_hv_delayfree_ent(pTHX_ HV *hv, HE *entry)
