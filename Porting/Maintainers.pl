898471883c344a0ca91f3181256a88fe8cd50057
Jerry D. Hedden <jdhedden@cpan.org>
2008-02-15 06:12:07 -0500

    Thread::Semaphore 2.04 From: "Jerry D. Hedden" <jdhedden@cpan.org> Message-ID: <1ff86f510802150812r3facd53cs1913dd82c3070ac0@mail.gmail.com>
    p4raw-id: //depot/perl@33329

--- Porting/Maintainers.pl
+++ Porting/Maintainers.pl
@@ -882,6 +882,13 @@ package Maintainers;
 		'CPAN'		=> 1,
 		},
 
+	'Thread::Semaphore' =>
+		{
+		'MAINTAINER'	=> 'jdhedden',
+		'FILES'		=> q[lib/Thread/Semaphore.pm lib/Thread/Semaphore],
+		'CPAN'		=> 1,
+		},
+
 	'threads' =>
 		{
 		'MAINTAINER'	=> 'jdhedden',
@@ -929,13 +936,6 @@ package Maintainers;
                 {
                 'MAINTAINER'    => 'msergeant',
                 'FILES'         => q[ext/Time/Piece],
-                'CPAN'          => 1,
-                },
-
-        'Thread::Semaphore' =>
-                {
-                'MAINTAINER'    => 'jdhedden',
-                'FILES'         => q[lib/Thread/Semaphore.pm lib/Thread/Semaphore.t],
                 'CPAN'          => 1,
                 },
 
