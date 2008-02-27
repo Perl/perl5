5fa45a31a1cc49254291a0192eac8a082942f306
Steve Hay <SteveHay@planit.com>
2008-02-27 12:54:52 +0000

    Fix another Win32/VC6 compiler warning caused by #33231 (missed by #33336--this one only occurs in debug builds)
    p4raw-id: //depot/perl@33385

--- sv.c
+++ sv.c
@@ -407,7 +407,7 @@ S_visit(pTHX_ SVFUNC_t f, const U32 flags, const U32 mask)
 /* called by sv_report_used() for each live SV */
 
 static void
-do_report_used(pTHX_ SV *sv)
+do_report_used(pTHX_ SV *const sv)
 {
     if (SvTYPE(sv) != SVTYPEMASK) {
 	PerlIO_printf(Perl_debug_log, "****\n");
