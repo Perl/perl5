52a5bfab8876f302d269f1bfa46eae1998f0d3ca
Steve Peters <steve@fisharerojo.org>
2008-03-08 15:13:26 +0000

    Perl_croak_nocontext() is called with NULL indirectly from some XS-based modules.
    p4raw-id: //depot/perl@33453

--- util.c
+++ util.c
@@ -1432,7 +1432,6 @@ Perl_croak_nocontext(const char *pat, ...)
 {
     dTHX;
     va_list args;
-    PERL_ARGS_ASSERT_CROAK_NOCONTEXT;
     va_start(args, pat);
     vcroak(pat, &args);
     /* NOTREACHED */
