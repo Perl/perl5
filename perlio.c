5cc43b324abf978b4a69b36cf45a58d3f7bbc21e
Nicholas Clark <nick@ccl4.org>
2008-03-12 20:05:05 +0000

    Integrate: [ 33491] Correct logic error in PerlIOStdio_close() - 0 is an acceptable value from dup(), so it can't also be the "don't do anything later" value.
    [ 33492]
    We need mutex protection in PerlIOStdio_close() for the duration of
    holding our true love file handle open, to stop anything else
    temporarily using it for a quick dup() fling, and then closing the
    file handle underneath us.
    I suspect that the lack of this protection was the cause of the threads
    free.t and blocks.t failures on OS X on 5.8.x, where usefaststdio is
    the default, and PerlIO is unable to "invalidate" the FILE *.
    
    [ 33498]
    Change 33492 did not spread the protection wide enough. There were
    still two more races to be lost.
    1: The close() could still happen after the (premature) mutex release
       allowed another thread to dup() to that file descriptor.
    2: The initial dup() could happen whilst another thread was in the
       mutex protected region, and had temporarily closed the file
       descriptor.
    Race conditions remain with any other thread that actually does I/O
    during the execution of the mutex protected region (as noted in a
    comment), and dup() failure is not handled gracefully (also noted).
    p4raw-link: @33498 on //depot/perl: 9bab90c0f28bfa909019e7c256fe9c7dfb6ca308
    p4raw-link: @33492 on //depot/perl: 711e8db2defd635d78bc9d3e168dbd0a8a3b89fa
    p4raw-link: @33491 on //depot/perl: 1d791a44a46c93b2808774b83d9ca1f346372145
    
    p4raw-id: //depot/maint-5.10/perl@33501
    p4raw-integrated: from //depot/perl@33496 'edit in' perlio.c (@33492..)

--- perlio.c
+++ perlio.c
@@ -3119,7 +3119,7 @@ PerlIOStdio_close(pTHX_ PerlIO *f)
 	int invalidate = 0;
 	IV result = 0;
 	int saveerr = 0;
-	int dupfd = 0;
+	int dupfd = -1;
 #ifdef SOCKS5_VERSION_NAME
     	/* Socks lib overrides close() but stdio isn't linked to
 	   that library (though we are) - so we must call close()
@@ -3145,8 +3145,37 @@ PerlIOStdio_close(pTHX_ PerlIO *f)
 	    result = PerlIO_flush(f);
 	    saveerr = errno;
 	    invalidate = PerlIOStdio_invalidate_fileno(aTHX_ stdio);
-	    if (!invalidate)
+	    if (!invalidate) {
+#ifdef USE_ITHREADS
+		MUTEX_LOCK(&PL_perlio_mutex);
+		/* Right. We need a mutex here because for a brief while we will
+		   have the situation that fd is actually closed. Hence if a
+		   second thread were to get into this block, its dup() would
+		   likely return our fd as its dupfd. (after all, it is closed).
+		   Then if we get to the dup2() first, we blat the fd back
+		   (messing up its temporary as a side effect) only for it to
+		   then close its dupfd (== our fd) in its close(dupfd) */
+
+		/* There is, of course, a race condition, that any other thread
+		   trying to input/output/whatever on this fd will be stuffed
+		   for the duraction of this little manoeuver. Perhaps we should
+		   hold an IO mutex for the duration of every IO operation if
+		   we know that invalidate doesn't work on this platform, but
+		   that would suck, and could kill performance.
+
+		   Except that correctness trumps speed.
+		   Advice from klortho #11912. */
+#endif
 		dupfd = PerlLIO_dup(fd);
+#ifdef USE_ITHREADS
+		if (dupfd < 0) {
+		    MUTEX_UNLOCK(&PL_perlio_mutex);
+		    /* Oh cXap. This isn't going to go well. Not sure if we can
+		       recover from here, or if closing this particular FILE *
+		       is a good idea now.  */
+		}
+#endif
+	    }
 	}
         result = PerlSIO_fclose(stdio);
 	/* We treat error from stdio as success if we invalidated
@@ -3160,9 +3189,12 @@ PerlIOStdio_close(pTHX_ PerlIO *f)
 	/* in SOCKS' case, let close() determine return value */
 	result = close(fd);
 #endif
-	if (dupfd) {
+	if (dupfd >= 0) {
 	    PerlLIO_dup2(dupfd,fd);
 	    PerlLIO_close(dupfd);
+#ifdef USE_ITHREADS
+	    MUTEX_UNLOCK(&PL_perlio_mutex);
+#endif
 	}
 	return result;
     }
