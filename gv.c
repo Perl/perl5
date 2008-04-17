c49b597d07430509a57aa1eb8401de4245277a3a
Rafael Garcia-Suarez <rgarciasuarez@gmail.com>
2008-04-17 10:03:04 +0000

    %.*s mandates an "int", says the C standard
    p4raw-id: //depot/perl@33704

--- gv.c
+++ gv.c
@@ -671,7 +671,7 @@ Perl_gv_fetchmethod_flags(pTHX_ HV *stash, const char *name, U32 flags)
 	    if (stash) {
 		Perl_croak(aTHX_
 			   "Can't locate object method \"%s\" via package \"%.*s\"",
-			   name, (STRLEN)HvNAMELEN_get(stash), HvNAME_get(stash));
+			   name, (int)HvNAMELEN_get(stash), HvNAME_get(stash));
 	    }
 	    else {
 		STRLEN packlen;
