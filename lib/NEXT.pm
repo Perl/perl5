962a05baec1f7f7e0c5c065f41bb68bdf777e297
Nicholas Clark <nick@ccl4.org>
2008-06-07 12:11:34 +0000

    Integrate: [ 23075] Subject: Bug with NEXT when called from an eval block From: Dave Rolsky <autarch@urth.org> Date: Fri, 9 Jul 2004 17:06:07 -0500 (CDT) Message-ID: <Pine.LNX.4.58.0407091703010.4578@urth.org>
    [ 25261]
    Subject: [PATCH] Typos in *.p[lm]
    From: "Piotr Fusik" <pfusik@op.pl>
    Date: Sun, 31 Jul 2005 12:50:04 +0200
    Message-ID: <001401c595bd$dccb5d80$0bd34dd5@piec>
    
    [ 33943]
    Integrate:
    ...
    
    [ 33416]
    Subject: Re: [PATCH] NEXT.pm bug within overloaded stringification
    From: Marcel GrÃ¼nauer <gr@univie.ac.at>
    Message-Id: <511B0A54-AB2D-4A65-A02E-E2E07C043EAB@univie.ac.at>
    Date: Mon, 3 Mar 2008 16:08:48 +0100
    
    (a resend of <7BBCFD93-91CF-4656-A97F-ED5E749F7B2E@univie.ac.at>)
    
    ...
    
    [ 33439]
    Bump VERSION after change #33416
    (so we don't end up with different code but same version in
    5.10.0 and, say, 5.10.1)
    
    ...
    
    [ 34006]
    Integrate:
    [ 33991]
    Update to NEXT by Damian.
    This fixes the working of NEXT with AUTOLOAD.
    
    [ 33995]
    Bump NEXT version to 0.61 for CPAN release
    p4raw-link: @34006 on //depot/maint-5.10/perl: 97834c389fb6e11328ef109533cb4c07b71f0a38
    p4raw-link: @33995 on //depot/perl: ccea220c5378568154094be403a96de17f611d80
    p4raw-link: @33991 on //depot/perl: 597fc7a09a53eb11962ad7d817b9904ed154d0ed
    p4raw-link: @33943 on //depot/maint-5.10/perl: 06f85ae8bee7544faa1071d29038257e671be30a
    p4raw-link: @33439 on //depot/perl: 9a35f1ea0522ed94d65ce910c1f49334b567bf08
    p4raw-link: @33416 on //depot/perl: 5dd54fb4257bccaa223fa7d57413e664e9bb3c2c
    p4raw-link: @25261 on //depot/perl: 3c4b39bee8832007b7e91bfce8701d34cacab411
    p4raw-link: @23075 on //depot/perl: 874ad44d852a3d0f27f985a25ac70c3cde24a259
    
    p4raw-id: //depot/maint-5.8/perl@34007
    p4raw-branched: from //depot/maint-5.10/perl@33994 'branch in'
    	lib/NEXT/t/stringify.t
    p4raw-integrated: from //depot/maint-5.10/perl@33994 'copy in'
    	lib/NEXT.pm (@33943..)
    p4raw-integrated: from //depot/perl@33994 'copy in' lib/NEXT/t/next.t
    	(@13117..)
    p4raw-integrated: from //depot/perl@25261 'ignore' lib/NEXT.pm
    	(@23075..)

--- lib/NEXT.pm
+++ lib/NEXT.pm
@@ -1,7 +1,8 @@
 package NEXT;
-$VERSION = '0.60';
+$VERSION = '0.61';
 use Carp;
 use strict;
+use overload ();
 
 sub NEXT::ELSEWHERE::ancestors
 {
@@ -29,130 +30,151 @@ sub NEXT::ELSEWHERE::ordered_ancestors
 	            :                0 } @outlist;
 }
 
-sub AUTOLOAD
+sub NEXT::ELSEWHERE::buildAUTOLOAD
 {
-	my ($self) = @_;
-	my $caller = (caller(1))[3]; 
-	my $wanted = $NEXT::AUTOLOAD || 'NEXT::AUTOLOAD';
-	undef $NEXT::AUTOLOAD;
-	my ($caller_class, $caller_method) = $caller =~ m{(.*)::(.*)}g;
-	my ($wanted_class, $wanted_method) = $wanted =~ m{(.*)::(.*)}g;
-	croak "Can't call $wanted from $caller"
-		unless $caller_method eq $wanted_method;
+    my $autoload_name = caller() . '::AUTOLOAD';
 
-	local ($NEXT::NEXT{$self,$wanted_method}, $NEXT::SEEN) =
-	      ($NEXT::NEXT{$self,$wanted_method}, $NEXT::SEEN);
+    no strict 'refs';
+    *{$autoload_name} = sub {
+        my ($self) = @_;
+        my $depth = 1;
+        until (((caller($depth))[3]||q{}) !~ /^\(eval\)$/) { $depth++ }
+        my $caller = (caller($depth))[3];
+        my $wanted = $NEXT::AUTOLOAD || $autoload_name;
+        undef $NEXT::AUTOLOAD;
+        my ($caller_class, $caller_method) = $caller =~ m{(.*)::(.*)}g;
+        my ($wanted_class, $wanted_method) = $wanted =~ m{(.*)::(.*)}g;
+        croak "Can't call $wanted from $caller"
+            unless $caller_method eq $wanted_method;
 
+        my $key = ref $self && overload::Overloaded($self)
+            ? overload::StrVal($self) : $self;
 
-	unless ($NEXT::NEXT{$self,$wanted_method}) {
-		my @forebears =
-			NEXT::ELSEWHERE::ancestors ref $self || $self,
-						   $wanted_class;
-		while (@forebears) {
-			last if shift @forebears eq $caller_class
-		}
-		no strict 'refs';
-		@{$NEXT::NEXT{$self,$wanted_method}} = 
-			map { *{"${_}::$caller_method"}{CODE}||() } @forebears
-				unless $wanted_method eq 'AUTOLOAD';
-		@{$NEXT::NEXT{$self,$wanted_method}} = 
-			map { (*{"${_}::AUTOLOAD"}{CODE}) ? "${_}::AUTOLOAD" : ()} @forebears
-				unless @{$NEXT::NEXT{$self,$wanted_method}||[]};
-		$NEXT::SEEN->{$self,*{$caller}{CODE}}++;
-	}
-	my $call_method = shift @{$NEXT::NEXT{$self,$wanted_method}};
-	while ($wanted_class =~ /^NEXT\b.*\b(UNSEEN|DISTINCT)\b/
-	       && defined $call_method
-	       && $NEXT::SEEN->{$self,$call_method}++) {
-		$call_method = shift @{$NEXT::NEXT{$self,$wanted_method}};
-	}
-	unless (defined $call_method) {
-		return unless $wanted_class =~ /^NEXT:.*:ACTUAL/;
-		(local $Carp::CarpLevel)++;
-		croak qq(Can't locate object method "$wanted_method" ),
-		      qq(via package "$caller_class");
-	};
-	return $self->$call_method(@_[1..$#_]) if ref $call_method eq 'CODE';
-	no strict 'refs';
-	($wanted_method=${$caller_class."::AUTOLOAD"}) =~ s/.*:://
-		if $wanted_method eq 'AUTOLOAD';
-	$$call_method = $caller_class."::NEXT::".$wanted_method;
-	return $call_method->(@_);
+        local ($NEXT::NEXT{$key,$wanted_method}, $NEXT::SEEN) =
+            ($NEXT::NEXT{$key,$wanted_method}, $NEXT::SEEN);
+
+        unless ($NEXT::NEXT{$key,$wanted_method}) {
+            my @forebears =
+                NEXT::ELSEWHERE::ancestors ref $self || $self,
+                            $wanted_class;
+            while (@forebears) {
+                last if shift @forebears eq $caller_class
+            }
+            no strict 'refs';
+            @{$NEXT::NEXT{$key,$wanted_method}} = 
+                map { *{"${_}::$caller_method"}{CODE}||() } @forebears
+                    unless $wanted_method eq 'AUTOLOAD';
+            @{$NEXT::NEXT{$key,$wanted_method}} = 
+                map { (*{"${_}::AUTOLOAD"}{CODE}) ? "${_}::AUTOLOAD" : ()} @forebears
+                    unless @{$NEXT::NEXT{$key,$wanted_method}||[]};
+            $NEXT::SEEN->{$key,*{$caller}{CODE}}++;
+        }
+        my $call_method = shift @{$NEXT::NEXT{$key,$wanted_method}};
+        while ($wanted_class =~ /^NEXT\b.*\b(UNSEEN|DISTINCT)\b/
+            && defined $call_method
+            && $NEXT::SEEN->{$key,$call_method}++) {
+            $call_method = shift @{$NEXT::NEXT{$key,$wanted_method}};
+        }
+        unless (defined $call_method) {
+            return unless $wanted_class =~ /^NEXT:.*:ACTUAL/;
+            (local $Carp::CarpLevel)++;
+            croak qq(Can't locate object method "$wanted_method" ),
+                qq(via package "$caller_class");
+        };
+        return $self->$call_method(@_[1..$#_]) if ref $call_method eq 'CODE';
+        no strict 'refs';
+        ($wanted_method=${$caller_class."::AUTOLOAD"}) =~ s/.*:://
+            if $wanted_method eq 'AUTOLOAD';
+        $$call_method = $caller_class."::NEXT::".$wanted_method;
+        return $call_method->(@_);
+    };
 }
 
 no strict 'vars';
-package NEXT::UNSEEN;		@ISA = 'NEXT';
-package NEXT::DISTINCT;		@ISA = 'NEXT';
-package NEXT::ACTUAL;		@ISA = 'NEXT';
-package NEXT::ACTUAL::UNSEEN;	@ISA = 'NEXT';
-package NEXT::ACTUAL::DISTINCT;	@ISA = 'NEXT';
-package NEXT::UNSEEN::ACTUAL;	@ISA = 'NEXT';
-package NEXT::DISTINCT::ACTUAL;	@ISA = 'NEXT';
+package NEXT;                                  NEXT::ELSEWHERE::buildAUTOLOAD();
+package NEXT::UNSEEN;		@ISA = 'NEXT';     NEXT::ELSEWHERE::buildAUTOLOAD();
+package NEXT::DISTINCT;		@ISA = 'NEXT';     NEXT::ELSEWHERE::buildAUTOLOAD();
+package NEXT::ACTUAL;		@ISA = 'NEXT';     NEXT::ELSEWHERE::buildAUTOLOAD();
+package NEXT::ACTUAL::UNSEEN;	@ISA = 'NEXT'; NEXT::ELSEWHERE::buildAUTOLOAD();
+package NEXT::ACTUAL::DISTINCT;	@ISA = 'NEXT'; NEXT::ELSEWHERE::buildAUTOLOAD();
+package NEXT::UNSEEN::ACTUAL;	@ISA = 'NEXT'; NEXT::ELSEWHERE::buildAUTOLOAD();
+package NEXT::DISTINCT::ACTUAL;	@ISA = 'NEXT'; NEXT::ELSEWHERE::buildAUTOLOAD();
 
-package EVERY::LAST;		@ISA = 'EVERY';
-package EVERY;			@ISA = 'NEXT';
-sub AUTOLOAD
-{
-	my ($self) = @_;
-	my $caller = (caller(1))[3]; 
-	my $wanted = $EVERY::AUTOLOAD || 'EVERY::AUTOLOAD';
-	undef $EVERY::AUTOLOAD;
-	my ($wanted_class, $wanted_method) = $wanted =~ m{(.*)::(.*)}g;
+package EVERY;
 
-	local $NEXT::ALREADY_IN_EVERY{$self,$wanted_method} =
-	      $NEXT::ALREADY_IN_EVERY{$self,$wanted_method};
+sub EVERY::ELSEWHERE::buildAUTOLOAD {
+    my $autoload_name = caller() . '::AUTOLOAD';
 
-	return if $NEXT::ALREADY_IN_EVERY{$self,$wanted_method}++;
-	
-	my @forebears = NEXT::ELSEWHERE::ordered_ancestors ref $self || $self,
-					                   $wanted_class;
-	@forebears = reverse @forebears if $wanted_class =~ /\bLAST\b/;
-	no strict 'refs';
-	my %seen;
-	my @every = map { my $sub = "${_}::$wanted_method";
-		          !*{$sub}{CODE} || $seen{$sub}++ ? () : $sub
-		        } @forebears
-				unless $wanted_method eq 'AUTOLOAD';
+    no strict 'refs';
+    *{$autoload_name} = sub {
+        my ($self) = @_;
+        my $depth = 1;
+        until (((caller($depth))[3]||q{}) !~ /^\(eval\)$/) { $depth++ }
+        my $caller = (caller($depth))[3];
+        my $wanted = $EVERY::AUTOLOAD || $autoload_name;
+        undef $EVERY::AUTOLOAD;
+        my ($wanted_class, $wanted_method) = $wanted =~ m{(.*)::(.*)}g;
 
-	my $want = wantarray;
-	if (@every) {
-		if ($want) {
-			return map {($_, [$self->$_(@_[1..$#_])])} @every;
-		}
-		elsif (defined $want) {
-			return { map {($_, scalar($self->$_(@_[1..$#_])))}
-				     @every
-			       };
-		}
-		else {
-			$self->$_(@_[1..$#_]) for @every;
-			return;
-		}
-	}
+        my $key = ref($self) && overload::Overloaded($self)
+            ? overload::StrVal($self) : $self;
 
-	@every = map { my $sub = "${_}::AUTOLOAD";
-		       !*{$sub}{CODE} || $seen{$sub}++ ? () : "${_}::AUTOLOAD"
-		     } @forebears;
-	if ($want) {
-		return map { $$_ = ref($self)."::EVERY::".$wanted_method;
-			     ($_, [$self->$_(@_[1..$#_])]);
-			   } @every;
-	}
-	elsif (defined $want) {
-		return { map { $$_ = ref($self)."::EVERY::".$wanted_method;
-			       ($_, scalar($self->$_(@_[1..$#_])))
-			     } @every
-		       };
-	}
-	else {
-		for (@every) {
-			$$_ = ref($self)."::EVERY::".$wanted_method;
-			$self->$_(@_[1..$#_]);
-		}
-		return;
-	}
+        local $NEXT::ALREADY_IN_EVERY{$key,$wanted_method} =
+            $NEXT::ALREADY_IN_EVERY{$key,$wanted_method};
+
+        return if $NEXT::ALREADY_IN_EVERY{$key,$wanted_method}++;
+
+        my @forebears = NEXT::ELSEWHERE::ordered_ancestors ref $self || $self,
+                                        $wanted_class;
+        @forebears = reverse @forebears if $wanted_class =~ /\bLAST\b/;
+        no strict 'refs';
+        my %seen;
+        my @every = map { my $sub = "${_}::$wanted_method";
+                    !*{$sub}{CODE} || $seen{$sub}++ ? () : $sub
+                    } @forebears
+                    unless $wanted_method eq 'AUTOLOAD';
+
+        my $want = wantarray;
+        if (@every) {
+            if ($want) {
+                return map {($_, [$self->$_(@_[1..$#_])])} @every;
+            }
+            elsif (defined $want) {
+                return { map {($_, scalar($self->$_(@_[1..$#_])))}
+                        @every
+                    };
+            }
+            else {
+                $self->$_(@_[1..$#_]) for @every;
+                return;
+            }
+        }
+
+        @every = map { my $sub = "${_}::AUTOLOAD";
+                !*{$sub}{CODE} || $seen{$sub}++ ? () : "${_}::AUTOLOAD"
+                } @forebears;
+        if ($want) {
+            return map { $$_ = ref($self)."::EVERY::".$wanted_method;
+                    ($_, [$self->$_(@_[1..$#_])]);
+                } @every;
+        }
+        elsif (defined $want) {
+            return { map { $$_ = ref($self)."::EVERY::".$wanted_method;
+                    ($_, scalar($self->$_(@_[1..$#_])))
+                    } @every
+                };
+        }
+        else {
+            for (@every) {
+                $$_ = ref($self)."::EVERY::".$wanted_method;
+                $self->$_(@_[1..$#_]);
+            }
+            return;
+        }
+    };
 }
 
+package EVERY::LAST;   @ISA = 'EVERY';   EVERY::ELSEWHERE::buildAUTOLOAD();
+package EVERY;         @ISA = 'NEXT';    EVERY::ELSEWHERE::buildAUTOLOAD();
 
 1;
 
@@ -229,7 +251,7 @@ do better.
 
 By default, if a redispatch attempt fails to find another method
 elsewhere in the objects class hierarchy, it quietly gives up and does
-nothing (but see L<"Enforcing redispatch">). This gracious acquiesence
+nothing (but see L<"Enforcing redispatch">). This gracious acquiescence
 is also unlike the (generally annoying) behaviour of C<SUPER>, which
 throws an exception if it cannot redispatch.
 
@@ -416,7 +438,7 @@ order. Instead, they are called "breadth-first-dependency-wise".
 That means that the inheritance tree of the object is traversed breadth-first
 and the resulting order of classes is used as the sequence in which methods
 are called. However, that sequence is modified by imposing a rule that the
-appropritae method of a derived class must be called before the same method of
+appropriate method of a derived class must be called before the same method of
 any ancestral class. That's why, in the above example, C<X::foo> is called
 before C<D::foo>, even though C<D> comes before C<X> in C<@B::ISA>.
 
