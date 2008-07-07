930a82cc50a6cf19c7af5e3f2ce73bd0fb646910
Jan Dubois <jand@activestate.com>
2008-07-07 08:58:48 -0700

    Integrate: [ 34064] Warnings-free for metalint-3.5-27. Woot! Removed all files that are now identical to dist-3.5
    [ 34105]
    Part one of y2038 changes for Schwern
    
    [ 34107]
    Probe for timegm
    
    [ 34112]
    Be sure to use the correct type, so bits do not get wrapped
    
    [ 34114]
    Fine-grained sGMTIME_min/max detection
    
    [ 34115]
    Update Win32 canned configs
    
    Adds new values introduced by #34105/34107, incorporating this patch:
    
    Subject: RE: New probes
    From: "Jan Dubois" <jand@activestate.com>
    Message-ID: <072601c8e085$04694860$0d3bd920$@com>
    
    The appropriate values for the other compilers were determined by
    running the probe functions added to Configure by #34105/34112/34114.
    (Note that VC8 requires an invalid parameter handler to run those
    functions without crashing, and both VC8 and VC9 need %I64d rather
    than %ld to output the appropriate values.)
    
    Also applies the changes to the config_H.* files, together with changes
    for #33597/33598/33949 which never got added to them.
    p4raw-link: @34115 on //depot/perl: 4b7e285e7ac0f212412ed301bb6824593baf29e9
    p4raw-link: @34114 on //depot/perl: 92a3ac59c93b6fba629d9177ad303a51fcb0d765
    p4raw-link: @34112 on //depot/perl: 5bcaa86b3b74d7972c51a495c7a8c358e9515fd8
    p4raw-link: @34107 on //depot/perl: cbb9e8a748b5e5e3976f6136bbf786e27be09152
    p4raw-link: @34105 on //depot/perl: 81c4fd9ed76693652365ade5cfe1811926de24d8
    p4raw-link: @34064 on //depot/perl: 1d8eaf8cf59b440c0fa63ff8dfca4ae76cf04fd0
    
    p4raw-id: //depot/maint-5.10/perl@35019
    p4raw-integrated: from //depot/perl@34115 'copy in' win32/config_sh.PL
    	(@34075..) 'merge in' win32/config_H.bc win32/config_H.gc
    	win32/config_H.vc win32/config_H.vc64 (@33350..)
    	win32/config.bc win32/config.gc win32/config.vc
    	win32/config.vc64 (@33949..)
    p4raw-integrated: from //depot/perl@34112 'copy in' Porting/config_H
    	(@33049..) 'edit in' Configure (@34107..) 'merge in'
    	Porting/config.sh (@33949..)
    p4raw-integrated: from //depot/perl@34105 'edit in' handy.h (@33379..)
    	Porting/Glossary (@33845..) 'ignore' config_h.SH (@34064..)

--- handy.h
+++ handy.h
@@ -175,7 +175,7 @@ typedef U64TYPE U64;
 #endif
 
 /* HMB H.Merijn Brand - a placeholder for preparing Configure patches */
-#if defined(LOCALTIME_R_NEEDS_TZSET) && defined(HAS_PSEUDOFORK) && defined(USE_DTRACE)
+#if defined(LOCALTIME_R_NEEDS_TZSET) && defined(HAS_PSEUDOFORK) && defined(USE_DTRACE) && defined(GMTIME_MAX) && defined(GMTIME_MIN) && defined(HAS_TIMEGM)
 /* Not (yet) used at top level, but mention them for metaconfig */
 #endif
 
