477acd91b77777975279707e892853aef237706b
Steve Hay <SteveHay@planit.com>
2008-09-11 16:50:06 +0000

    Skip another file in the VERSION comparison program
    p4raw-id: //depot/perl@34348

--- Porting/cmpVERSION.pl
+++ Porting/cmpVERSION.pl
@@ -25,7 +25,10 @@ chdir $ARGV[0] or die "$0: chdir '$ARGV[0]' failed: $!\n";
 # Files to skip from the check for one reason or another,
 # usually because they pull in their version from some other file.
 my %skip;
-@skip{'./lib/Exporter/Heavy.pm'} = ();
+@skip{
+    './lib/Carp/Heavy.pm',
+    './lib/Exporter/Heavy.pm'
+} = ();
 
 my @wanted;
 find(
