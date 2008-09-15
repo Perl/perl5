64febd972f4111aade8c42b8fba46068566e6cb3
Nicholas Clark <nick@ccl4.org>
2008-09-15 19:10:23 +0000

    Revert change 28337 for B::Bytecode, because it's not correct for 5.8.x.
    p4raw-id: //depot/maint-5.8/perl@34367

--- ext/B/B/Bytecode.pm
+++ ext/B/B/Bytecode.pm
@@ -631,6 +631,7 @@ sub B::LOOP::bsave {
 sub B::COP::bsave {
     my ($cop,$ix) = @_;
     my $warnix = $cop->warnings->ix;
+    my $ioix = $cop->io->ix;
     if (ITHREADS) {
 	$cop->B::OP::bsave($ix);
 	asm "cop_stashpv", pvix $cop->stashpv;
@@ -647,6 +648,7 @@ sub B::COP::bsave {
     asm "cop_arybase", $cop->arybase;
     asm "cop_line", $cop->line;
     asm "cop_warnings", $warnix;
+    asm "cop_io", $ioix;
 }
 
 sub B::OP::opwalk {
