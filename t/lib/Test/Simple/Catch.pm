40e14bdfc526d918ec2286ef9d3b2cce41b37e3f
Steve Peters <steve@fisharerojo.org>
2008-10-15 13:20:02 +0000

    Some additional files changes for Test-Simple-0.82
    p4raw-id: //depot/perl@34484
    
    (cherry picked from commit 717da8a4201bbbe7be187b87e3604a793556e80a)

--- t/lib/Test/Simple/Catch.pm
+++ t/lib/Test/Simple/Catch.pm
@@ -1,9 +1,10 @@
 # For testing Test::Simple;
+# $Id: /mirror/googlecode/test-more-trunk/t/lib/Test/Simple/Catch.pm 67132 2008-10-01T01:11:04.501643Z schwern  $
 package Test::Simple::Catch;
 
 use Symbol;
 use TieOut;
-my($out_fh, $err_fh) = (gensym, gensym);
+my( $out_fh, $err_fh ) = ( gensym, gensym );
 my $out = tie *$out_fh, 'TieOut';
 my $err = tie *$err_fh, 'TieOut';
 
@@ -13,6 +14,6 @@ $t->output($out_fh);
 $t->failure_output($err_fh);
 $t->todo_output($err_fh);
 
-sub caught { return($out, $err) }
+sub caught { return( $out, $err ) }
 
 1;
