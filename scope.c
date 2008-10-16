c5047247a3524ec2f5c5cb9b27e15e15abf49de9
Nicholas Clark <nick@ccl4.org>
2008-10-16 21:21:51 +0000

    Integrate: [ 34494] Clarify which save types are in mathoms, which aren't used, and which still are in use. p4raw-link: @34494 on //depot/perl: 95e06916561ca1cbaa0ca21057644bd664ebf2dc
    p4raw-id: //depot/maint-5.10/perl@34496
    p4raw-integrated: from //depot/perl@34495 'merge in' scope.c (@33376..)

--- scope.c
+++ scope.c
@@ -961,16 +961,29 @@ Perl_leave_scope(pTHX_ I32 base)
 		SvFLAGS(sv) |= val;
 	    }
 	    break;
-	    /* These are only saved in mathoms.c */
+
+	    /* This would be a mathom, but Perl_save_svref() calls a static
+	       function, S_save_scalar_at(), so has to stay in this file.  */
 	case SAVEt_SVREF:			/* scalar reference */
 	    value = (SV*)SSPOPPTR;
 	    ptr = SSPOPPTR;
 	    av = NULL; /* what to refcnt_dec */
 	    goto restore_sv;
+
+	    /* These are only saved in mathoms.c */
+	case SAVEt_NSTAB:
+	    gv = (GV*)SSPOPPTR;
+	    (void)sv_clear((SV*)gv);
+	    break;
 	case SAVEt_LONG:			/* long reference */
 	    ptr = SSPOPPTR;
 	    *(long*)ptr = (long)SSPOPLONG;
 	    break;
+	case SAVEt_IV:				/* IV reference */
+	    ptr = SSPOPPTR;
+	    *(IV*)ptr = (IV)SSPOPIV;
+	    break;
+
 	    /* This case is rendered redundant by the integration of change
 	       33078. See the comment near Perl_save_padsv().  */
 	case SAVEt_PADSV:
@@ -992,14 +1005,6 @@ Perl_leave_scope(pTHX_ I32 base)
 	case SAVEt_I8:				/* I8 reference */
 	    ptr = SSPOPPTR;
 	    *(I8*)ptr = (I8)SSPOPINT;
-	    break;
-	case SAVEt_IV:				/* IV reference */
-	    ptr = SSPOPPTR;
-	    *(IV*)ptr = (IV)SSPOPIV;
-	    break;
-	case SAVEt_NSTAB:
-	    gv = (GV*)SSPOPPTR;
-	    (void)sv_clear((SV*)gv);
 	    break;
 	case SAVEt_DESTRUCTOR:
 	    ptr = SSPOPPTR;
