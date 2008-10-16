c84a25c741881ec8b62eb5c2c67b2bd4fe4c977c
Nicholas Clark <nick@ccl4.org>
2008-10-16 18:08:52 +0000

    Integrate: [ 33074] In POPLOOP, if CxITERVAR(cx) is non-NULL, then so is itersave, and itersave is a less complex expression for the C compiler.
    [ 33075]
    Restore the else block accidently eaten by change 33074.
    p4raw-link: @33075 on //depot/perl: 9a98d8a1ee5dd7f1caa95f2ec5ab0341e0b4f8d1
    p4raw-link: @33074 on //depot/perl: b99874c7926b2f5919256cf4bcee8eb7bf3dff22
    
    p4raw-id: //depot/maint-5.10/perl@34492
    p4raw-integrated: from //depot/perl@33075 'edit in' cop.h (@33074..)

--- cop.h
+++ cop.h
@@ -501,7 +501,7 @@ struct block_loop {
 
 #define POPLOOP(cx)							\
 	SvREFCNT_dec(cx->blk_loop.iterlval);				\
-	if (CxITERVAR(cx)) {						\
+	if (cx->blk_loop.itersave) {					\
             if (SvPADMY(cx->blk_loop.itersave)) {			\
 		SV ** const s_v_p = CxITERVAR(cx);			\
 		sv_2mortal(*s_v_p);					\
