2cb0746837a53e31204f9cf8cd544bfb5171a1a7
Nicholas Clark <nick@ccl4.org>
2008-10-24 20:43:36 +0000

    Add PL_sv_serial to makedef.pl, so that it is skipped approriately on AIX, OS/2, Win32 etc
    p4raw-id: //depot/perl@34571
    
    (cherry picked from commit dfc72f41b0aaceda3480a2bf23c0aba6675f43e9)

--- makedef.pl
+++ makedef.pl
@@ -831,6 +831,12 @@ unless ($define{'PERL_NEED_TIMESBASE'}) {
 		    )];
 }
 
+unless ($define{'DEBUG_LEAKING_SCALARS'}) {
+    skip_symbols [qw(
+		    PL_sv_serial
+		    )];
+}
+
 unless ($define{'DEBUG_LEAKING_SCALARS_FORK_DUMP'}) {
     skip_symbols [qw(
 		    PL_dumper_fd
