1129b882ced9d5881a47214405219a2e6e332a92
Nicholas Clark <nick@ccl4.org>
2008-10-25 12:23:01 +0000

    Update copyright years.
    p4raw-id: //depot/perl@34585

--- perlsdio.h
+++ perlsdio.h
@@ -1,7 +1,7 @@
 /*    perlsdio.h
  *
  *    Copyright (C) 1996, 1997, 1998, 1999, 2000, 2001,
- *    2002, 2003, 2006, 2007, by Larry Wall and others
+ *    2002, 2003, 2006, 2007, 2008 by Larry Wall and others
  *
  *    You may distribute under the terms of either the GNU General Public
  *    License or the Artistic License, as specified in the README file.
