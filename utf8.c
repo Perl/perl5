285be2d09253ada039f97d71887a35722209960b
Nicholas Clark <nick@ccl4.org>
2008-10-26 21:44:48 +0000

    Integrate: [ 34585] Update copyright years.
    [ 34586]
    Update copyright year in embed.pl, and everything that it builds.
    p4raw-link: @34586 on //depot/perl: 67edeb9aab7e64ec46e81a9ccb767449a20cad5f
    p4raw-link: @34585 on //depot/perl: 1129b882ced9d5881a47214405219a2e6e332a92
    
    p4raw-id: //depot/maint-5.10/perl@34599
    p4raw-integrated: from //depot/perl@34593 'copy in' perlapi.c
    	(@31245..) perlsdio.h (@32793..) XSUB.h (@32820..) perly.c
    	(@33370..) cc_runtime.h (@34002..) pp.h (@34055..) 'merge in'
    	av.h (@33051..) mg.h (@33256..) deb.c locale.c numeric.c pad.c
    	pp_pack.c regexec.c taint.c (@33291..) embed.pl (@33539..)
    	mathoms.c (@33627..) scope.h (@33654..) cop.h (@33656..) gv.c
    	(@33814..) doop.c (@33831..) perly.y (@33858..) global.sym
    	(@33901..) pp_sort.c (@33937..) gv.h (@34029..) op.h pad.h
    	regexp.h (@34030..) perl.h (@34069..) doio.c mg.c pp.c pp_sys.c
    	xsutils.c (@34092..) pp_ctl.c (@34140..) cv.h dump.c (@34167..)
    	toke.c (@34224..) universal.c (@34354..) mro.c (@34357..) op.c
    	(@34358..) av.c (@34381..) hv.c (@34383..) utf8.c (@34416..)
    	scope.c (@34494..) pp_hot.c (@34506..) regcomp.c (@34507..)
    	intrpvar.h sv.h (@34568..) embed.h embedvar.h perlapi.h
    	(@34569..) util.c (@34574..) sv.c (@34576..)
    p4raw-integrated: from //depot/perl@34585 'ignore' proto.h (@34574..)
    	'merge in' perlio.c (@33978..) handy.h (@34577..)

--- utf8.c
+++ utf8.c
@@ -1,6 +1,6 @@
 /*    utf8.c
  *
- *    Copyright (C) 2000, 2001, 2002, 2003, 2004, 2005, 2006, 2007
+ *    Copyright (C) 2000, 2001, 2002, 2003, 2004, 2005, 2006, 2007, 2008
  *    by Larry Wall and others
  *
  *    You may distribute under the terms of either the GNU General Public
