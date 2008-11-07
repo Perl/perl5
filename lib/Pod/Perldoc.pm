9b411def1b80bf112b838a109987f0c9ba0eba60
Steve Hay <SteveHay@planit.com>
2008-11-07 11:32:36 +0000

    Upgrade to Pod-Perldoc-3.15
    p4raw-id: //depot/perl@34765

--- lib/Pod/Perldoc.pm
+++ lib/Pod/Perldoc.pm
@@ -12,7 +12,7 @@ use File::Spec::Functions qw(catfile catdir splitdir);
 use vars qw($VERSION @Pagers $Bindir $Pod2man
   $Temp_Files_Created $Temp_File_Lifetime
 );
-$VERSION = '3.14_07';
+$VERSION = '3.15';
 #..........................................................................
 
 BEGIN {  # Make a DEBUG constant very first thing...
