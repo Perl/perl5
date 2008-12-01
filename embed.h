dfcd3de5defffbf214d60b8322b79ded8e367fd3
Nicholas Clark <nick@ccl4.org>
2008-12-01 00:02:17 +0000

    Expose save_pushptrptr() and implement SAVESWITCHSTACK() with it.
    p4raw-id: //depot/perl@34960

--- embed.h
+++ embed.h
@@ -809,10 +809,10 @@
 #if defined(PERL_IN_SCOPE_C)
 #ifdef PERL_CORE
 #define save_pushi32ptr		S_save_pushi32ptr
-#define save_pushptrptr		S_save_pushptrptr
 #endif
 #endif
 #ifdef PERL_CORE
+#define save_pushptrptr		Perl_save_pushptrptr
 #define sawparens		Perl_sawparens
 #define scalar			Perl_scalar
 #endif
@@ -3159,10 +3159,10 @@
 #if defined(PERL_IN_SCOPE_C)
 #ifdef PERL_CORE
 #define save_pushi32ptr(a,b,c)	S_save_pushi32ptr(aTHX_ a,b,c)
-#define save_pushptrptr(a,b,c)	S_save_pushptrptr(aTHX_ a,b,c)
 #endif
 #endif
 #ifdef PERL_CORE
+#define save_pushptrptr(a,b,c)	Perl_save_pushptrptr(aTHX_ a,b,c)
 #define sawparens(a)		Perl_sawparens(aTHX_ a)
 #define scalar(a)		Perl_scalar(aTHX_ a)
 #endif
