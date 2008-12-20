ee64842bc790a93f4f81821591246c72ec1bb8d9
Leon Brocard <acme@astray.com>
2008-12-20 08:42:06 +0000

    Add a program to switch to a Perforce id
    (cherry picked from commit 734abad9c35353b7df4e009f63d8b7c5c845141c)

---
+++ Porting/switch_to_perforce_id.pl
@@ -0,0 +1,22 @@
+#!perl
+use strict;
+use warnings;
+use English;
+
+my $perforce_id = shift;
+die "Usage: switch_to_perforce_id.pl 34440" unless $perforce_id;
+
+open my $fh, 'git log -z --pretty=raw|' || die $!;
+local $INPUT_RECORD_SEPARATOR = "\0";
+
+my $re = qr/p4raw-id:.+\@$perforce_id/;
+
+while ( my $log = <$fh> ) {
+    next unless $log =~ /$re/;
+    my ($commit) = $log =~ /commit ([a-z0-9]+)/;
+    system "git checkout $commit";
+    print "(use git checkout blead to go back)\n";
+    exit;
+}
+
+die "No log found for $perforce_id";
