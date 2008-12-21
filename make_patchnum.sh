22c2721efb14ccccec2cc0562807e99ea7d2f881
Yves Orton <demerphq@dromedary.booking.com>
2008-12-21 19:40:18 +0100

    slightly smarter make_patchnum logic, detect if there are changes to the working directory so we dont lie about our version
    (cherry picked from commit 10275c29786c111a7eacbb751ea7e0ba257dc869)

--- make_patchnum.sh
+++ make_patchnum.sh
@@ -4,7 +4,12 @@ Existing=`cat .patchnum 2>/dev/null`
 if [ -e ".patch" ]; then
 	Current=`awk '{print $4}' .patch`
 else
+	# we should do something better here
 	Current=`git describe`
+	Changed=`git diff-index --name-only HEAD`
+	if [ -n "$Changed" ]; then
+		Current="$Current-with-uncommitted-changes"
+	fi
 fi
 
 if [ "$Existing" != "$Current" ]; then
