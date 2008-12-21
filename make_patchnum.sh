ba4bb01ebc0497918a1b1428e4135e7b0e9c40b6
Yves Orton <demerphq@dromedary.booking.com>
2008-12-21 18:49:27 +0100

    smarter make_patchnum.sh, get the .patchnum from the .patch file if it exists

--- make_patchnum.sh
+++ make_patchnum.sh
@@ -1,7 +1,11 @@
 #!/bin/sh
 
 Existing=`cat .patchnum 2>/dev/null`
-Current=`git describe`
+if [ -e ".patch" ]; then
+	Current=`awk '{print $4}' .patch`
+else
+	Current=`git describe`
+fi
 
 if [ "$Existing" != "$Current" ]; then
 	echo "Updating .patchnum"
