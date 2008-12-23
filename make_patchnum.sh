531a2f446e4c4ff48354dfe55b2d17a7719ab97c
H.M. Brand <merijn@a5.(none)>
2008-12-23 13:48:33 +0100

    Generate .patchnum from .patch or .git

--- make_patchnum.sh
+++ make_patchnum.sh
@@ -3,13 +3,11 @@
 Existing=`cat .patchnum 2>/dev/null`
 if [ -e ".patch" ]; then
 	Current=`awk '{print $4}' .patch`
-else
+elif [ -d ".git" ]; then
 	# we should do something better here
 	Current=`git describe`
 	Changed=`git diff-index --name-only HEAD`
-	if [ -n "$Changed" ]; then
-		Current="$Current-with-uncommitted-changes"
-	fi
+	[ -n "$Changed" ] && Current="$Current-with-uncommitted-changes"
 fi
 
 if [ "$Existing" != "$Current" ]; then
