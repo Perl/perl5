b6f66bd1313e64c8dc4f1244c8dc820baafcbbdc
kevin brintnall <kbrint@rufus.net>
2008-12-24 20:59:15 -0600

    sv_dup(): avoid cloning empty arrays
    Testing the ARRAY pointer is insufficent.  Arrays emptied by 'shift' or
    'pop' may still have non-NULL 'ARRAY' pointers.  Check more carefully to
    determine whether the array has anything worth duplicating.  If not, reset
    the FILL and MAX offsets to -1 just as 'undef @ARRAY' would.
    
    This avoids potential corruption in the PL_ptr_table during perl_clone().
    
    (cherry-picked from 2779b694b3fbb69a13c300a6e239e050151abf6d)

--- sv.c
+++ sv.c
@@ -10421,7 +10421,8 @@ Perl_sv_dup(pTHX_ const SV *sstr, CLONE_PARAMS* param)
 		IoBOTTOM_NAME(dstr)	= SAVEPV(IoBOTTOM_NAME(dstr));
 		break;
 	    case SVt_PVAV:
-		if (AvARRAY((AV*)sstr)) {
+		/* avoid cloning an empty array */
+		if (AvARRAY((AV *)sstr) && AvFILLp((AV *)sstr) >= 0) {
 		    SV **dst_ary, **src_ary;
 		    SSize_t items = AvFILLp((AV*)sstr) + 1;
 
@@ -10446,6 +10447,8 @@ Perl_sv_dup(pTHX_ const SV *sstr, CLONE_PARAMS* param)
 		else {
 		    AvARRAY((AV*)dstr)	= NULL;
 		    AvALLOC((AV*)dstr)	= (SV**)NULL;
+		    AvMAX(  (AV *)dstr)	= -1;
+		    AvFILLp((AV *)dstr)	= -1;
 		}
 		break;
 	    case SVt_PVHV:
