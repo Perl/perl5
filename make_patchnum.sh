857f071675764ed717265a533ac348a37c51ac8b
Nicholas Clark <nick@ccl4.org>
2008-12-27 13:07:29 -0800

    Merge branch 'blead' of nicholas@perl5.git.perl.org:/gitroot/perl into blead
    (cherry picked from commit 4e7245b5373225aafe95c40cba1590f590d1c782)

--- make_patchnum.sh
+++ make_patchnum.sh
@@ -5,18 +5,18 @@
 Existing=`cat .patchnum 2>/dev/null`
 Existing_Sha1=`cat .sha1 2>/dev/null`
 
-if [ -e ".patch" ]; then
+if test -s ".patch" ; then
 	Current=`awk '{print $4}' .patch`
 	Sha1=`awk '{print $3}' .patch`
-elif [ -d ".git" ]; then
+elif test -d ".git" ; then
 	# we should do something better here
 	Current=`git describe`
 	Sha1=`git rev-parse HEAD`
 	Changed=`git diff-index --name-only HEAD`
-	[ -n "$Changed" ] && Current="$Current-with-uncommitted-changes"
+	test -n "$Changed"  && Current="$Current-with-uncommitted-changes"
 fi
 
-if [ "$Existing" != "$Current" -o "$Existing_Sha1" != "$Sha1" ]; then
+if test "$Existing" != "$Current" -o "$Existing_Sha1" != "$Sha1" ; then
 	(echo "hi there\c" ; echo " ") >.echotmp
 	if $contains c .echotmp >/dev/null 2>&1 ; then
 		n='-n'
