cd3d9d47255d3080961ba7b58c9a145c7b45b905
Rafael Garcia-Suarez <rgarciasuarez@gmail.com>
2008-12-28 23:03:27 +0100

    Switch.pm doesn't appear to support plain arrays and hashes in case().

--- lib/Switch.pm
+++ lib/Switch.pm
@@ -524,10 +524,9 @@ released Nov 22, 2006.
 	case 1		{ print "number 1" }
 	case "a"	{ print "string a" }
 	case [1..10,42]	{ print "number in list" }
-	case (@array)	{ print "number in list" }
+	case (\@array)	{ print "number in list" }
 	case /\w+/	{ print "pattern" }
 	case qr/\w+/	{ print "pattern" }
-	case (%hash)	{ print "entry in hash" }
 	case (\%hash)	{ print "entry in hash" }
 	case (\&sub)	{ print "arg to subroutine" }
 	else		{ print "previous case not true" }
