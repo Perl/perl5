050a6864ccd9ebde9d8e59e8575c3ba4ac7097d4
Nicholas Clark <nick@ccl4.org>
2008-12-31 07:42:55 +0000

    Switching from $() to `` requires two more backticks in unpushed_commits=`...`

--- make_patchnum.sh
+++ make_patchnum.sh
@@ -29,7 +29,7 @@ elif [ -d ".git" ]; then
 	extra_info="git_commit_date='$commit_created'"
 	if [ -n "$branch" ] && [ -n "$remote" ]; then
 		unpushed_commit_list=`git cherry $remote/$branch | awk 'BEGIN{ORS=","} /\+/ {print $2}' | sed -e 's/,$//'`
-		unpushed_commits=`git cherry $remote/$branch | awk 'BEGIN{ORS="\t\\\n"} /\+/ {print ",\"" $2 "\""}'`
+		unpushed_commits=`git cherry $remote/$branch | awk 'BEGIN{ORS="\t\\\\\n"} /\+/ {print ",\"" $2 "\""}'`
 
 		if [ -n "$unpushed_commits" ]; then
 			commit_title="Local Commit:"
