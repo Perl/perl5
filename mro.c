c17d15b9600d43b60db1020b490414769aa26059
Nicholas Clark <nick@ccl4.org>
2009-01-03 10:23:16 +0000

    Following change 34356, which decoupled get_isa_hash() from S_mro_get_linear_isa_dfs(), we can move get_isa_hash() to any other source file.
    (cherry picked from commit 00bf72ff155f3450b111622b688bd0036a18326c)

--- mro.c
+++ mro.c
@@ -184,41 +184,6 @@ Perl_mro_meta_dup(pTHX_ struct mro_meta* smeta, CLONE_PARAMS* param)
 
 #endif /* USE_ITHREADS */
 
-HV *
-Perl_get_isa_hash(pTHX_ HV *const stash)
-{
-    dVAR;
-    struct mro_meta *const meta = HvMROMETA(stash);
-
-    PERL_ARGS_ASSERT_GET_ISA_HASH;
-
-    if (!meta->isa) {
-	AV *const isa = mro_get_linear_isa(stash);
-	if (!meta->isa) {
-	    HV *const isa_hash = newHV();
-	    /* Linearisation didn't build it for us, so do it here.  */
-	    SV *const *svp = AvARRAY(isa);
-	    SV *const *const svp_end = svp + AvFILLp(isa) + 1;
-	    const HEK *const canon_name = HvNAME_HEK(stash);
-
-	    while (svp < svp_end) {
-		(void) hv_store_ent(isa_hash, *svp++, &PL_sv_undef, 0);
-	    }
-
-	    (void) hv_common(isa_hash, NULL, HEK_KEY(canon_name),
-			     HEK_LEN(canon_name), HEK_FLAGS(canon_name),
-			     HV_FETCH_ISSTORE, &PL_sv_undef,
-			     HEK_HASH(canon_name));
-	    (void) hv_store(isa_hash, "UNIVERSAL", 9, &PL_sv_undef, 0);
-
-	    SvREADONLY_on(isa_hash);
-
-	    meta->isa = isa_hash;
-	}
-    }
-    return meta->isa;
-}
-
 /*
 =for apidoc mro_get_linear_isa_dfs
 
