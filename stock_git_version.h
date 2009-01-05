7c568aade2b9ffd485fd458736e5289bd2ce082e
Rafael Garcia-Suarez <rgarciasuarez@gmail.com>
2009-01-05 08:06:16 +0100

    Typo fix in stock PERL_PATCHNUM definition
    (cherry picked from commit 20231e4a0b934f359913ecaf2bb6aa0d11a2ac3c)

--- stock_git_version.h
+++ stock_git_version.h
@@ -1,9 +1,9 @@
 /***************************************************************************
 This is: stock_git_config.h - temporary git_config.h file.
-This file is used at the very start of the build process when we dont have
+This file is used at the very start of the build process when we don't have
 a miniperl available to make the real thing.  It is copied in place during
 the build process, and then later on replaced.
 ****************************************************************************/
-#define PERL_PATCHNUM "UNKOWN"
+#define PERL_PATCHNUM "UNKNOWN"
 #define PERL_GIT_UNCOMMITTED_CHANGES ,"UNKNOWN"
 #define PERL_GIT_UNPUSHED_COMMITS /*leave-this-comment*/
