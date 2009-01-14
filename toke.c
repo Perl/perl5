Perl/perl5

commit 26b6dc3ff01d5987ade8e7bb4d8844b01828ff83

Require a space or a newline after a "#line XXX" directive
This fixes bug [perl #59170] Typo: bad regex for #line directive in perlsyn.

toke.c
@@ -824,6 +824,8 @@ S_incline(pTHX_ const char *s)
     n = s;
     while (isDIGIT(*s))
 	s++;
+    if (!SPACE_OR_TAB(*s) && *s != '\n' && *s != '\0')
+	return;
     while (SPACE_OR_TAB(*s))
 	s++;
     if (*s == '"' && (t = strchr(s+1, '"'))) {

t/comp/parser.t
@@ -9,7 +9,7 @@ BEGIN {
 }
 
 BEGIN { require "./test.pl"; }
-plan( tests => 110 );
+plan( tests => 112 );
 
 eval '%@x=0;';
 like( $@, qr/^Can't modify hash dereference in repeat \(x\)/, '%@x=0' );
@@ -329,6 +329,9 @@ check(qr/^"BBFRPRAFPGHPP$/, 43, "actually missing a quote is still valid");
 
 #line 47 bang eth
 check(qr/^"BBFRPRAFPGHPP$/, 46, "but spaces aren't allowed without quotes");
+
+#line 77sevenseven
+check(qr/^"BBFRPRAFPGHPP$/, 49, "need a space after the line number");
 
 eval <<'EOSTANZA'; die $@ if $@;
 #line 51 "With wonderful deathless ditties|We build up the world's great cities,|And out of a fabulous story|We fashion an empire's glory:|One man with a dream, at pleasure,|Shall go forth and conquer a crown;|And three with a new song's measure|Can trample a kingdom down."

commit 07714eb4889156c33187b02961d4af49685702e0

Allow '\r' after "#line XXX" directives.
Fixes lib/feature.t and lib/warnings.t tests that have been failing on Win32 since commit 26b6dc3.

toke.c
@@ -824,7 +824,7 @@ S_incline(pTHX_ const char *s)
     n = s;
     while (isDIGIT(*s))
 	s++;
-    if (!SPACE_OR_TAB(*s) && *s != '\n' && *s != '\0')
+    if (!SPACE_OR_TAB(*s) && *s != '\r' && *s != '\n' && *s != '\0')
 	return;
     while (SPACE_OR_TAB(*s))
 	s++;