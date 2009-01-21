076a2a80a09d79b056f73d9ef04bd4d977712fce
Jerry D. Hedden <jdhedden@cpan.org>
2009-01-21 11:03:38 -0500

    Code refs aren't lockable
    Remove the check for code refs in pp_lock.
    For debugging, assert that the ref is not a code ref
    (per Dave Mitchell's suggestion).

--- pp.c
+++ pp.c
@@ -5062,9 +5062,9 @@ PP(pp_lock)
     dSP;
     dTOPss;
     SV *retsv = sv;
+    assert(SvTYPE(retsv) != SVt_PVCV);
     SvLOCK(sv);
-    if (SvTYPE(retsv) == SVt_PVAV || SvTYPE(retsv) == SVt_PVHV
-	|| SvTYPE(retsv) == SVt_PVCV) {
+    if (SvTYPE(retsv) == SVt_PVAV || SvTYPE(retsv) == SVt_PVHV) {
 	retsv = refto(retsv);
     }
     SETs(retsv);
