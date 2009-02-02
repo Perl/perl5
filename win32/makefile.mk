421926c2f211043ade7c80ffab00d5afd74d01e0
Steve Hay <SteveHay@planit.com>
2009-02-02 13:18:34 +0000

    Restore broken clean target in win32/makefile.mk
    Commit aaaf44d72fce5980564533b9072da6e594d91d81 accidentally missed a --all
    argument for buildext.pl, which resulted in the extensions not getting
    "dmake realclean" run in them.

--- win32/makefile.mk
+++ win32/makefile.mk
@@ -1404,7 +1404,7 @@ Extensions_clean :
 	-if exist $(MINIPERL) $(MINIPERL) -I..\lib buildext.pl "MAKE=$(MAKE)" --dir=$(EXTDIR) --all --target=clean
 
 Extensions_realclean :
-	-if exist $(MINIPERL) $(MINIPERL) -I..\lib buildext.pl "MAKE=$(MAKE)" --dir=$(EXTDIR) --target=realclean
+	-if exist $(MINIPERL) $(MINIPERL) -I..\lib buildext.pl "MAKE=$(MAKE)" --dir=$(EXTDIR) --all --target=realclean
 
 #-------------------------------------------------------------------------------
 
