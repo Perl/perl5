55043f18e345806d858a3b85770254be7e8a6dfe
Nicholas Clark <nick@ccl4.org>
2009-02-10 06:19:42 +0000

    Sys-Syslog I18N-Langinfo have long names now.
    (cherry picked from commit a0373bf6e165c060691e3ff83083a704b0006e27)

--- win32/FindExt.pm
+++ win32/FindExt.pm
@@ -6,7 +6,7 @@ use strict;
 use warnings;
 
 my $no = join('|',qw(GDBM_File ODBM_File NDBM_File DB_File
-                     VMS Syslog IPC-SysV Langinfo));
+                     VMS Sys-Syslog IPC-SysV I18N-Langinfo));
 $no = qr/^(?:$no)$/i;
 
 my %ext;
