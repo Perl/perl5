8fa43ce7928bf33b9db1f23d9af1a2400915e05a
Rafael Garcia-Suarez <rgarciasuarez@gmail.com>
2009-03-02 10:44:54 +0100

    Bump versions to 0.10 for CPAN sync
    (cherry picked from commit b6be863a9cf5bbc527bcd449f58168fbf06e381a)

--- lib/CPANPLUS/Dist/Build/Constants.pm
+++ lib/CPANPLUS/Dist/Build/Constants.pm
@@ -9,7 +9,7 @@ BEGIN {
     require Exporter;
     use vars    qw[$VERSION @ISA @EXPORT];
   
-    $VERSION    = 0.01;
+    $VERSION    = 0.10;
     @ISA        = qw[Exporter];
     @EXPORT     = qw[ BUILD_DIR BUILD ];
 }
