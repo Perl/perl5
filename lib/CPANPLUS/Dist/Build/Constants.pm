4ec613fc9ea24805f53c42982fd74a869cd3674a
Rafael Garcia-Suarez <rgarciasuarez@gmail.com>
2009-03-02 15:05:40 +0100

    Bump versions to 0.12 to sync with CPAN version

--- lib/CPANPLUS/Dist/Build/Constants.pm
+++ lib/CPANPLUS/Dist/Build/Constants.pm
@@ -9,7 +9,7 @@ BEGIN {
     require Exporter;
     use vars    qw[$VERSION @ISA @EXPORT];
   
-    $VERSION    = 0.10;
+    $VERSION    = '0.12';
     @ISA        = qw[Exporter];
     @EXPORT     = qw[ BUILD_DIR BUILD ];
 }
