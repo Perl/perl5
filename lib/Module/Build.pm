2645075aa05e530ac7f2ed965bbfaef588edc22b
Steve Hay <SteveHay@planit.com>
2009-03-06 11:54:02 +0000

    Upgrade to Module-Build-0.32
    But keep cd5cc49dbc0e5ee748252c2da8b435855908e6d2 (Module::Build on VMS, #42724)
    and the MBTest.pm tweak from 66e531b6b23122113bed8498baac1ef25a958d57.

--- lib/Module/Build.pm
+++ lib/Module/Build.pm
@@ -15,7 +15,7 @@ use Module::Build::Base;
 
 use vars qw($VERSION @ISA);
 @ISA = qw(Module::Build::Base);
-$VERSION = '0.31_0401';
+$VERSION = '0.32_01';
 $VERSION = eval $VERSION;
 
 # Okay, this is the brute-force method of finding out what kind of
--- lib/Module/Build/Base.pm
+++ lib/Module/Build/Base.pm
@@ -4,7 +4,7 @@ package Module::Build::Base;
 
 use strict;
 use vars qw($VERSION);
-$VERSION = '0.31_04';
+$VERSION = '0.32_01';
 $VERSION = eval $VERSION;
 BEGIN { require 5.00503 }
 
