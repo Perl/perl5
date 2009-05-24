e21a6cc6e23cb05573794c3438f5a45564a045d6
Andreas J Koenig <andk@cpan.org>
2009-05-24 21:57:49 +0200

    Update CPAN.pm to 1.94

--- lib/CPAN.pm
+++ lib/CPAN.pm
@@ -2,7 +2,7 @@
 # vim: ts=4 sts=4 sw=4:
 use strict;
 package CPAN;
-$CPAN::VERSION = '1.93_54';
+$CPAN::VERSION = '1.94';
 $CPAN::VERSION =~ s/_//;
 
 # we need to run chdir all over and we would get at wrong libraries
