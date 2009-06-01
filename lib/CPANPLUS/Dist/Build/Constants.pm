e42df61ffa4b532a113a57b2965d347ce4da44b3
Chris Williams <chris@bingosnet.co.uk>
2009-06-01 23:28:45 +0100

    Update CPANPLUS::Dist::Build to CPAN version 0.32

--- lib/CPANPLUS/Dist/Build/Constants.pm
+++ lib/CPANPLUS/Dist/Build/Constants.pm
@@ -9,7 +9,7 @@ BEGIN {
     require Exporter;
     use vars    qw[$VERSION @ISA @EXPORT];
   
-    $VERSION    = '0.30';
+    $VERSION    = '0.32';
     @ISA        = qw[Exporter];
     @EXPORT     = qw[ BUILD_DIR BUILD ];
 }
