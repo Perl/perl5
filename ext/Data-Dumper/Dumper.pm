20fc10417a2e591867f81f2d35a64a646135e115
Steffen Mueller <smueller@cpan.org>
2009-06-13 17:23:57 +0200

    Update to Data::Dumper 2.124
    ... which includes all test files that blead had and CPAN did not.

--- ext/Data-Dumper/Dumper.pm
+++ ext/Data-Dumper/Dumper.pm
@@ -9,7 +9,7 @@
 
 package Data::Dumper;
 
-$VERSION = '2.123'; # Don't forget to set version and release date in POD!
+$VERSION = '2.124'; # Don't forget to set version and release date in POD!
 
 #$| = 1;
 
@@ -1297,7 +1297,7 @@ modify it under the same terms as Perl itself.
 
 =head1 VERSION
 
-Version 2.123  (Jun 11 2009)
+Version 2.124  (Jun 13 2009)
 
 =head1 SEE ALSO
 
