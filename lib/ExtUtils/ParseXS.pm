55ec0dff636c2a8ee5225314d7d46f928ab7f6da
David Golden <dagolden@cpan.org>
2009-07-01 13:54:43 -0400

    Updating ExtUtils-ParseXS to 2.20
    2.20 - Wed Jul  1 13:42:11 EDT 2009
    
     - No changes from 2.19_04
    
    Signed-off-by: H.Merijn Brand <h.m.brand@xs4all.nl>

--- lib/ExtUtils/ParseXS.pm
+++ lib/ExtUtils/ParseXS.pm
@@ -18,7 +18,7 @@ my(@XSStack);	# Stack of conditionals and INCLUDEs
 my($XSS_work_idx, $cpp_next_tmp);
 
 use vars qw($VERSION);
-$VERSION = '2.19_04';
+$VERSION = '2.20';
 
 use vars qw(%input_expr %output_expr $ProtoUsed @InitFileCode $FH $proto_re $Overload $errors $Fallback
 	    $cplusplus $hiertype $WantPrototypes $WantVersionChk $except $WantLineNumbers
