27395e5492e01aab57182cb258f6f1da1948dda9
David Mitchell <davem@iabyn.com>
2009-07-03 22:09:59 +0100

    make cmpVERSION.pl ignore Config.pm
    (cherry picked from commit 7536d879835346ee8671a43b3f9e0bf5a70ad7c3)

--- Porting/cmpVERSION.pl
+++ Porting/cmpVERSION.pl
@@ -58,8 +58,9 @@ chdir $ARGV[0] or die "$0: chdir '$ARGV[0]' failed: $!\n";
 my %skip;
 @skip{
     './lib/Carp/Heavy.pm',
+    './lib/Config.pm',		# no version number but contents will vary
     './lib/Exporter/Heavy.pm',
-    './win32/FindExt.pm'
+    './win32/FindExt.pm',
 } = ();
 my $skip_dirs = qr|^\./t/lib|;
 
