2f175318a4bf1a5a0d3c3dfabb9a049ffc16c4e9
Vincent Pit <vince@profvince.com>
2009-07-15 22:32:36 +0200

    Document PL_opfreehook

--- intrpvar.h
+++ intrpvar.h
@@ -170,6 +170,18 @@ PERLVARA(Icolors,6,	char *)		/* from regcomp.c */
 PERLVARI(Ipeepp,	peep_t, MEMBER_TO_FPTR(Perl_peep))
 					/* Pointer to peephole optimizer */
 
+/*
+=for apidoc Amn|Perl_ophook_t|PL_opfreehook
+
+When non-C<NULL>, the function pointed by this variable will be called each time an OP is freed with the corresponding OP as the argument.
+This allows extensions to free any extra attribute they have locally attached to an OP.
+It is also assured to first fire for the parent OP and then for its kids.
+
+When you replace this variable, it is considered a good practice to store the possibly previously installed hook and that you recall it inside your own.
+
+=cut
+*/
+
 PERLVARI(Iopfreehook,	Perl_ophook_t, 0) /* op_free() hook */
 
 PERLVARI(Imaxscream,	I32,	-1)
