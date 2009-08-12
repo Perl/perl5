Perl/perl5

commit 984f9f66477bc722578262ae8520584e44e881af

Re: [PATCH: TODO Tests] Re: [perl #53806] No complain about bareword From: "Rafael Garcia-Suarez" <rgarciasuarez@gmail.com> Message-ID: <b77c1dce0805200045i1000598ci13c9cb3a29f64b6d@mail.gmail.com>
p4raw-id: //depot/perl@33874

toke.c
@@ -5667,10 +5667,10 @@ Perl_yylex(pTHX)
 
 		/* Call it a bare word */
 
+		bareword:
 		if (PL_hints & HINT_STRICT_SUBS)
 		    pl_yylval.opval->op_private |= OPpCONST_STRICT;
 		else {
-		bareword:
 		    if (lastchar != '-') {
 			if (ckWARN(WARN_RESERVED)) {
 			    d = PL_tokenbuf;

commit cb4bdfcf966b22445c291b778e785e035f399f9d

Added a failing test for a bareword parsing regression created in 984f9f66
Date: Tue, 11 Aug 2009 23:20:05 +0200
From: Slaven Rezic <slaven@rezic.de>
To: perl5-porters@perl.org
Subject: Re: 5.10.1: regression regarding bareword in readline()
Reply-to: slaven@rezic.de

On Tue 11.Aug'09 at 23:20:05 +0200, Slaven Rezic wrote:
> > git bisect says that the breakage occurred in
> > 109c3d50375e96ee005ac98a3d457c7da2f5cb08 in the maint-5.10 branch,
> > respective in 984f9f66477bc722578262ae8520584e44e881af in the blead branch.
> >
> > This breaks at least the CPAN distribution MySQL-Backup-0.04.
> >
> > /usr/perl-5.10.0-3220-gf9961a0/bin/perl -c -e 'use strict; readline(STDIN)'
> I found another CPAN distribution with the same problem: Tie-Simple-1.03
>
> And there's another bareword problem, found in Games-Go-GoPair-1.001.>     use strict;
>     my @players;
>     sub _rankCompare { }
>     @players = sort(_rankCompare @players);

t/lib/strict/subs
@@ -408,3 +408,17 @@ system(FOO . "\n");
 EXPECT
 Bareword "FOO" not allowed while "strict subs" in use at - line 4.
 Execution of - aborted due to compilation errors.
+########
+# commit 984f9f66477bc722578262ae8520584e44e881af broke this parsing
+use strict 'subs';
+my @players;
+sub _rankCompare { }
+@players = sort(_rankCompare @players);
+EXPECT
+
+########
+# commit 984f9f66477bc722578262ae8520584e44e881af broke this parsing
+use strict;
+readline(STDIN);
+EXPECT
+