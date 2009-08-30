4f87fa576f174a31e6d8fdb08cae76e644395db0
Steffen Mueller <smueller@cpan.org>
2009-08-30 20:21:31 +0200

    Move Text::Tabs/Text::Wrap from lib to ext
    It'll live in ext/Text-Tabs for now to make the Makefile.PL generation
    happy.

--- Porting/Maintainers.pl
+++ Porting/Maintainers.pl
@@ -1750,12 +1750,8 @@ package Maintainers;
 	{
 	'MAINTAINER'	=> 'muir',
 	'DISTRIBUTION'	=> 'MUIR/modules/Text-Tabs+Wrap-2009.0305.tar.gz',
-	'FILES'		=> q[lib/Text/{Tabs,Wrap}.pm lib/Text/TabsWrap],
+	'FILES'		=> q[ext/Text-Tabs],
 	'EXCLUDED'	=> [ qw( t/dnsparks.t ) ], # see af6492bf9e
-	'MAP'		=> {
-			     ''    => 'lib/Text/TabsWrap/',
-			     'lib/'=> 'lib/',
-			   },
 	'CPAN'		=> 1,
 	'UPSTREAM'	=> 'cpan',
 	},
