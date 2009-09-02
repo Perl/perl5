e9a927b58cfb908fb66a58427a00c931c84d0d87
Steffen Mueller <smueller@cpan.org>
2009-09-02 23:04:31 +0200

    Deprecate tainted.pl with a warning

--- lib/tainted.pl
+++ lib/tainted.pl
@@ -1,4 +1,10 @@
+# This legacy library is deprecated and will be removed in a future
+# release of perl.
 # This subroutine returns true if its argument is tainted, false otherwise.
+#
+warn( "The 'tainted.pl' legacy library is deprecated and will be"
+      . " removed in the next major release of perl. Please use the"
+      . " Scalar::Util module ('tainted' function) instead." );
 
 sub tainted {
     local($@);
