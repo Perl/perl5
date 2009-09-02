155f9b97008e73be0bb5f1feb882805e21da3999
Steffen Mueller <smueller@cpan.org>
2009-09-02 23:03:53 +0200

    Deprecate open3.pl with a warning

--- lib/open3.pl
+++ lib/open3.pl
@@ -1,3 +1,6 @@
+# This legacy library is deprecated and will be removed in a future
+# release of perl.
+#
 # This is a compatibility interface to IPC::Open3.  New programs should
 # do
 #
@@ -6,6 +9,10 @@
 # instead of
 #
 #     require 'open3.pl';
+
+warn( "The 'open3.pl' legacy library is deprecated and will be"
+      . " removed in the next major release of perl. Please use the"
+      . " IPC::Open3 module instead." );
 
 package main;
 use IPC::Open3 'open3';
