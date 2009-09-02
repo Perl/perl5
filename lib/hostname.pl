d9591f87ff50af3a58bed017b7ac5c2a5c448219
Steffen Mueller <smueller@cpan.org>
2009-09-02 20:14:40 +0200

    Deprecate hostname.pl with a warning

--- lib/hostname.pl
+++ lib/hostname.pl
@@ -2,12 +2,18 @@
 #
 # This library is no longer being maintained, and is included for backward
 # compatibility with Perl 4 programs which may require it.
+# This legacy library is deprecated and will be removed in a future
+# release of perl.
 #
 # In particular, this should not be used as an example of modern Perl
 # programming techniques.
 #
 # Suggested alternative: Sys::Hostname
-#
+
+warn( "The 'hostname.pl' legacy library is deprecated and will be"
+      . " removed in the next major release of perl. Please use the"
+      . " Sys::Hostname module instead." );
+
 sub hostname
 {
 	local(*P,@tmp,$hostname,$_);
