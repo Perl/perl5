42cb3b6d01c213ff7daf09d23f012bd467bea3aa
Steffen Mueller <smueller@cpan.org>
2009-09-02 20:14:52 +0200

    Deprecate importenv.pl with a warning

--- lib/importenv.pl
+++ lib/importenv.pl
@@ -1,3 +1,12 @@
+# This library is no longer being maintained, and is included for backward
+# compatibility with Perl 4 programs which may require it.
+# This legacy library is deprecated and will be removed in a future
+# release of perl.
+
+warn( "The 'importenv.pl' legacy library is deprecated and will be"
+      . " removed in the next major release of perl. Please use the"
+      . " Env::Export module (or similar) from CPAN instead." );
+
 ;# This file, when interpreted, pulls the environment into normal variables.
 ;# Usage:
 ;#	require 'importenv.pl';
