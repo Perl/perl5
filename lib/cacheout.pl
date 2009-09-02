e879c7c674245b9a876e1c4ba5181f88494ec4c8
Steffen Mueller <smueller@cpan.org>
2009-09-02 18:39:36 +0200

    Deprecate cacheout.pl with a warning

--- lib/cacheout.pl
+++ lib/cacheout.pl
@@ -1,11 +1,17 @@
 #
 # This library is no longer being maintained, and is included for backward
 # compatibility with Perl 4 programs which may require it.
+# This legacy library is deprecated and will be removed in a future
+# release of perl.
 #
 # In particular, this should not be used as an example of modern Perl
 # programming techniques.
 #
 # Suggested alternative: FileCache
+
+warn( "The 'cacheout.pl' legacy library is deprecated and will be"
+      . " removed in the next major release of perl. Please use the"
+      . " FileCache module instead." );
 
 # Open in their package.
 
