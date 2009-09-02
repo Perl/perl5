a06bfbf46ad8e21060b59ed409ba2f87fbfcdc35
Steffen Mueller <smueller@cpan.org>
2009-09-02 23:03:48 +0200

    Deprecate open2.pl with a warning

--- lib/open2.pl
+++ lib/open2.pl
@@ -1,3 +1,6 @@
+# This legacy library is deprecated and will be removed in a future
+# release of perl.
+#
 # This is a compatibility interface to IPC::Open2.  New programs should
 # do
 #
@@ -6,6 +9,10 @@
 # instead of
 #
 #     require 'open2.pl';
+
+warn( "The 'open2.pl' legacy library is deprecated and will be"
+      . " removed in the next major release of perl. Please use the"
+      . " IPC::Open2 module instead." );
 
 package main;
 use IPC::Open2 'open2';
