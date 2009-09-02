19aca64933c801470dbac7c3262f00a8f0d0310b
Abhijit Menon-Sen <ams@toroid.org>
2009-09-03 00:09:44 +0530

    Deprecate lib/find{,depth}.pl with a warning

--- lib/find.pl
+++ lib/find.pl
@@ -1,3 +1,12 @@
+# This library is deprecated and unmaintained. It is included for
+# compatibility with Perl 4 scripts which may use it, but it will be
+# removed in a future version of Perl. Please use the File::Find module
+# instead.
+
+warn( "Please use the File::Find module instead of the deprecated"
+     ." 'find.pl' library, which will be removed in the next major"
+     ." release of perl" );
+
 # Usage:
 #	require "find.pl";
 #
@@ -10,11 +19,11 @@
 #		to $dir when the function is called.  The function may
 #		set $prune to prune the tree.
 #
-# This library is primarily for find2perl, which, when fed
+# For example,
 #
-#   find2perl / -name .nfs\* -mtime +7 -exec rm -f {} \; -o -fstype nfs -prune
+#   find / -name .nfs\* -mtime +7 -exec rm -f {} \; -o -fstype nfs -prune
 #
-# spits out something like this
+# corresponds to this
 #
 #	sub wanted {
 #	    /^\.nfs.*$/ &&
