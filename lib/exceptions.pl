25209816f53a8d9986b7b64f9d3cf1004452aacc
Steffen Mueller <smueller@cpan.org>
2009-09-02 18:45:23 +0200

    Deprecate exceptions.pl with a warning

--- lib/exceptions.pl
+++ lib/exceptions.pl
@@ -3,11 +3,15 @@
 #
 # This library is no longer being maintained, and is included for backward
 # compatibility with Perl 4 programs which may require it.
+# This legacy library is deprecated and will be removed in a future
+# release of perl.
 #
 # In particular, this should not be used as an example of modern Perl
 # programming techniques.
-#
-# 
+
+warn( "The 'exceptions.pl' legacy library is deprecated and will be"
+      . " removed in the next major release of perl." );
+
 # Here's a little code I use for exception handling.  It's really just
 # glorfied eval/die.  The way to use use it is when you might otherwise
 # exit, use &throw to raise an exception.  The first enclosing &catch
