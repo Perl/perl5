Perl/perl5

commit 4677aef710c292706460adb42ae4c5704ccecc56

Moved AutoLoader from lib/ to ext/

Porting/Maintainers.pl
@@ -236,7 +236,7 @@ use File::Glob qw(:case);
 	{
 	'MAINTAINER'	=> 'smueller',
 	'DISTRIBUTION'	=> 'SMUELLER/AutoLoader-5.69.tar.gz',
-	'FILES'		=> q[lib/AutoLoader.pm lib/AutoSplit.pm lib/AutoLoader],
+	'FILES'		=> q[ext/AutoLoader.pm],
 	'EXCLUDED'	=> [ qw( t/00pod.t ) ],
 	'CPAN'		=> 1,
 	'UPSTREAM'	=> "cpan",

MANIFEST
@@ -191,6 +191,10 @@ ext/autodie/t/user-context.t		autodie - Context changes for usersubs
 ext/autodie/t/usersub.t			autodie - user subroutine tests
 ext/autodie/t/version.t			autodie - versioning tests
 ext/autodie/t/version_tag.t
+ext/AutoLoader/lib/AutoLoader.pm		Autoloader base class
+ext/AutoLoader/lib/AutoSplit.pm		Split up autoload functions
+ext/AutoLoader/t/01AutoLoader.t	See if AutoLoader works
+ext/AutoLoader/t/02AutoSplit.t	See if AutoSplit works
 ext/autouse/lib/autouse.pm	Load and call a function only when it's used
 ext/autouse/t/autouse.t		See if autouse works
 ext/base/Changes		base.pm changelog
@@ -2765,10 +2769,6 @@ lib/abbrev.pl			An abbreviation table builder
 lib/AnyDBM_File.pm		Perl module to emulate dbmopen
 lib/AnyDBM_File.t		See if AnyDBM_File works
 lib/assert.pl			assertion and panic with stack trace
-lib/AutoLoader.pm		Autoloader base class
-lib/AutoLoader/t/01AutoLoader.t	See if AutoLoader works
-lib/AutoLoader/t/02AutoSplit.t	See if AutoSplit works
-lib/AutoSplit.pm		Split up autoload functions
 lib/Benchmark.pm		Measure execution time
 lib/Benchmark.t			See if Benchmark works
 lib/bigfloat.pl			An arbitrary precision floating point package

ext/.gitignore
@@ -16,6 +16,7 @@ ppport.h
 /Attribute-Handlers/Makefile.PL
 /attributes/Makefile.PL
 /autodie/Makefile.PL
+/AutoLoader/Makefile.PL
 /autouse/Makefile.PL
 /base/Makefile.PL
 /bignum/Makefile.PL

ext/AutoLoader/t/01AutoLoader.t
@@ -3,7 +3,7 @@
 BEGIN {
   if ($ENV{PERL_CORE}) {
     chdir 't' if -d 't';
-    @INC = '../lib';
+    #@INC = '../lib';
   }
 }
 

lib/.gitignore
@@ -3,6 +3,8 @@
 /Archive/Tar/Constant.pm
 /Archive/Tar/File.pm
 /Attribute
+/AutoLoader.pm
+/AutoSplit.pl
 /autodie
 /autodie.pm
 /autouse.pm

commit 961b49424657d49b2fbc2121bae68b67175886d8

Fix typo for the FILES for AutoLoader, introduced in 4677aef710c2.

Porting/Maintainers.pl
@@ -236,7 +236,7 @@ use File::Glob qw(:case);
 	{
 	'MAINTAINER'	=> 'smueller',
 	'DISTRIBUTION'	=> 'SMUELLER/AutoLoader-5.69.tar.gz',
-	'FILES'		=> q[ext/AutoLoader.pm],
+	'FILES'		=> q[ext/AutoLoader],
 	'EXCLUDED'	=> [ qw( t/00pod.t ) ],
 	'CPAN'		=> 1,
 	'UPSTREAM'	=> "cpan",