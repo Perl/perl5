9373cb24bc4ca30048542b574f8448aca481be99
Steffen Mueller <smueller@cpan.org>
2009-09-21 16:11:25 +0200

    Revert "Upgrade to Attribute::Handlers 0.87 (which is just a core sync)"
    This reverts commit 248f30289f80d02b83f3d80d7ff575cb3e758ced because I
    accidentally broke everything.

--- ext/Attribute-Handlers/lib/Attribute/Handlers.pm
+++ ext/Attribute-Handlers/lib/Attribute/Handlers.pm
@@ -4,7 +4,7 @@ use Carp;
 use warnings;
 use strict;
 use vars qw($VERSION $AUTOLOAD);
-$VERSION = '0.87'; # remember to update version in POD!
+$VERSION = '0.86'; # remember to update version in POD!
 # $DB::single=1;
 
 my %symcache;
@@ -257,8 +257,8 @@ Attribute::Handlers - Simpler definition of attribute handlers
 
 =head1 VERSION
 
-This document describes version 0.87 of Attribute::Handlers,
-released September 21, 2009.
+This document describes version 0.86 of Attribute::Handlers,
+released August  8, 2009.
 
 =head1 SYNOPSIS
 
