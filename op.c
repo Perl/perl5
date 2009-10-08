e92f586bd3fd45336a351e251027401d8d4a7135
Rafael Garcia-Suarez <rgs@consttype.org>
2009-10-08 14:56:56 +0200

    Simplify code that sets $VERSION on the "package" line

--- op.c
+++ op.c
@@ -3821,10 +3821,7 @@ Perl_package_version( pTHX_ OP *v )
 {
     dVAR;
     PERL_ARGS_ASSERT_PACKAGE_VERSION;
-    SV *const version = cSVOPx(v)->op_sv;
-    SV *const pkgname = sv_mortalcopy(PL_curstname);
-    sv_catpv(pkgname, "::VERSION");
-    sv_setsv( get_sv(SvPV_nolen(pkgname),TRUE), version );
+    sv_setsv( GvSV(gv_fetchpvs("VERSION", GV_ADDMULTI, SVt_PV)), cSVOPx(v)->op_sv );
     op_free(v);
 }
 
