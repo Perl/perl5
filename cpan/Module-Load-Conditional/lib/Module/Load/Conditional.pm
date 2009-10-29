482aa6ffeb230a1cea9d05e9eb425b3d7fc09217
Chris Williams <chris@bingosnet.co.uk>
2009-10-29 09:30:42 +0000

    Update Module::Load::Conditional to cpan version 0.34
      Changes for 0.34    Thu Oct 29 09:22:48 GMT 2009
      =================================================
      * Remove DOS line endings from test files RT#50926
        reported by Steve Hay

--- cpan/Module-Load-Conditional/lib/Module/Load/Conditional.pm
+++ cpan/Module-Load-Conditional/lib/Module/Load/Conditional.pm
@@ -18,7 +18,7 @@ BEGIN {
                         $FIND_VERSION $ERROR $CHECK_INC_HASH];
     use Exporter;
     @ISA            = qw[Exporter];
-    $VERSION        = '0.32';
+    $VERSION        = '0.34';
     $VERBOSE        = 0;
     $DEPRECATED     = 0;
     $FIND_VERSION   = 1;
