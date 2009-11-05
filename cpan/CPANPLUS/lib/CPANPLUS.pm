f395eedc84e42f36a84c71a79c9224f84ec41177
Chris Williams <chris@bingosnet.co.uk>
2009-11-05 14:38:29 +0000

    Updated CPANPLUS to cpan version 0.89_07

--- cpan/CPANPLUS/lib/CPANPLUS.pm
+++ cpan/CPANPLUS/lib/CPANPLUS.pm
@@ -13,7 +13,7 @@ BEGIN {
     use vars        qw( @EXPORT @ISA $VERSION );
     @EXPORT     =   qw( shell fetch get install );
     @ISA        =   qw( Exporter );
-    $VERSION = "0.89_06";     #have to hardcode or cpan.org gets unhappy
+    $VERSION = "0.89_07";     #have to hardcode or cpan.org gets unhappy
 }
 
 ### purely for backward compatibility, so we can call it from the commandline:
