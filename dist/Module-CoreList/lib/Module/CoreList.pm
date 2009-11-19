75c8032f4f01d336ac0b5386a2838286d362c030
Chris Williams <chris@bingosnet.co.uk>
2009-11-19 10:03:53 +0000

    Bump version of Module::CoreList to 2.23

--- dist/Module-CoreList/lib/Module/CoreList.pm
+++ dist/Module-CoreList/lib/Module/CoreList.pm
@@ -2,7 +2,7 @@ package Module::CoreList;
 use strict;
 use vars qw/$VERSION %released %version %families %upstream
 	    %bug_tracker %deprecated/;
-$VERSION = '2.22';
+$VERSION = '2.23';
 
 =head1 NAME
 
