dcae3e365f028d9009bb2a2707ea4f42a56f9f69
Reini Urban <rurban@x-ray.at>
2009-12-03 20:47:55 +0100

    pp_sort.c typo: stabilty
    I'm now working on the sort code in th perl compiler as you can see :)
    --
    Reini Urban
    http://phpwiki.org/  http://murbreak.at/
    
    From b8c749be70f51499fe1ffd9e483ee3a0a8305d9b Mon Sep 17 00:00:00 2001
    From: Reini Urban <rurban@cpan.org>
    Date: Thu, 3 Dec 2009 19:44:57 +0000
    Subject: [PATCH] fix typo: stabilty
    
    Signed-off-by: H.Merijn Brand <h.m.brand@xs4all.nl>

--- pp_sort.c
+++ pp_sort.c
@@ -1291,7 +1291,7 @@ S_qsortsvu(pTHX_ SV ** array, size_t num_elts, SVCOMPARE_t compare)
  * by the original comparison routine on the elements pointed to.
  * Because we don't move the elements of list1 around through
  * this phase, we can break ties on elements that compare equal
- * using their address in the list1 array, ensuring stabilty.
+ * using their address in the list1 array, ensuring stability.
  * This leaves us with something looking like
  *
  *  indir                  list1
