Perl/perl5

commit 37e2e78edfe0a224b8a615820f46db879584f523

qr/\X/ expansion

regexec.c
@@ -120,11 +120,31 @@
 /* these are unrolled below in the CCC_TRY_XXX defined */
 #define LOAD_UTF8_CHARCLASS(class,str) STMT_START { \
     if (!CAT2(PL_utf8_,class)) { bool ok; ENTER; save_re_context(); ok=CAT2(is_utf8_,class)((const U8*)str); assert(ok); LEAVE; } } STMT_END
+
+/* Doesn't do an assert to verify that is correct */
+#define LOAD_UTF8_CHARCLASS_NO_CHECK(class) STMT_START { \
+    if (!CAT2(PL_utf8_,class)) { bool ok; ENTER; save_re_context(); ok=CAT2(is_utf8_,class)((const U8*)" "); LEAVE; } } STMT_END
+
 #define LOAD_UTF8_CHARCLASS_ALNUM() LOAD_UTF8_CHARCLASS(alnum,"a")
 #define LOAD_UTF8_CHARCLASS_DIGIT() LOAD_UTF8_CHARCLASS(digit,"0")
 #define LOAD_UTF8_CHARCLASS_SPACE() LOAD_UTF8_CHARCLASS(space," ")
-#define LOAD_UTF8_CHARCLASS_MARK()  LOAD_UTF8_CHARCLASS(mark, "\xcd\x86")
 
+#define LOAD_UTF8_CHARCLASS_GCB()  /* Grapheme cluster boundaries */        \
+	LOAD_UTF8_CHARCLASS(X_begin, " "),                                  \
+	LOAD_UTF8_CHARCLASS(X_non_hangul, "A"),                             \
+	/* These are utf8 constants, and not utf-ebcdic constants, so the   \
+	    * assert should likely and hopefully fail on an EBCDIC machine */ \
+	LOAD_UTF8_CHARCLASS(X_extend, "\xcc\x80"), /* U+0300 */             \
+									    \
+	/* No asserts are done for these, in case called on an early        \
+	    * Unicode version in which they map to nothing */               \
+	LOAD_UTF8_CHARCLASS_NO_CHECK(X_prepend),/* U+0E40 "\xe0\xb9\x80" */ \
+	LOAD_UTF8_CHARCLASS_NO_CHECK(X_L),	    /* U+1100 "\xe1\x84\x80" */ \
+	LOAD_UTF8_CHARCLASS_NO_CHECK(X_LV),     /* U+AC00 "\xea\xb0\x80" */ \
+	LOAD_UTF8_CHARCLASS_NO_CHECK(X_LVT),    /* U+AC01 "\xea\xb0\x81" */ \
+	LOAD_UTF8_CHARCLASS_NO_CHECK(X_LV_LVT_V),/* U+AC01 "\xea\xb0\x81" */\
+	LOAD_UTF8_CHARCLASS_NO_CHECK(X_T),      /* U+11A8 "\xe1\x86\xa8" */ \
+	LOAD_UTF8_CHARCLASS_NO_CHECK(X_V)       /* U+1160 "\xe1\x85\xa0" */  
 
 /* 
    We dont use PERL_LEGACY_UNICODE_CHARCLASS_MAPPINGS as the direct test
@@ -3521,22 +3541,216 @@ S_regmatch(pTHX_ regmatch_info *reginfo, regnode *prog)
 	CCC_TRY_AFF( DIGIT,  DIGITL, posix_digit, "0", isDIGIT_LC_utf8, isDIGIT, isDIGIT_LC);
 	CCC_TRY_NEG(NDIGIT, NDIGITL, posix_digit, "0", isDIGIT_LC_utf8, isDIGIT, isDIGIT_LC);
 
-	case CLUMP:
+	case CLUMP: /* Match \X: logical Unicode character.  This is defined as
+		       a Unicode extended Grapheme Cluster */
+	    /* From http://www.unicode.org/reports/tr29 (5.2 version).  An
+	      extended Grapheme Cluster is:
+
+	       CR LF
+	       | Prepend* Begin Extend*
+	       | .
+
+	       Begin is (Hangul-syllable | ! Control)
+	       Extend is (Grapheme_Extend | Spacing_Mark)
+	       Control is [ GCB_Control CR LF ]
+
+	       The discussion below shows how the code for CLUMP is derived
+	       from this regex.  Note that most of these concepts are from
+	       property values of the Grapheme Cluster Boundary (GCB) property.
+	       No code point can have multiple property values for a given
+	       property.  Thus a code point in Prepend can't be in Control, but
+	       it must be in !Control.  This is why Control above includes
+	       GCB_Control plus CR plus LF.  The latter two are used in the GCB
+	       property separately, and so can't be in GCB_Control, even though
+	       they logically are controls.  Control is not the same as gc=cc,
+	       but includes format and other characters as well.
+
+	       The Unicode definition of Hangul-syllable is:
+		   L+
+		   | (L* ( ( V | LV ) V* | LVT ) T*)
+		   | T+ 
+		  )
+	       Each of these is a value for the GCB property, and hence must be
+	       disjoint, so the order they are tested is immaterial, so the
+	       above can safely be changed to
+		   T+
+		   | L+
+		   | (L* ( LVT | ( V | LV ) V*) T*)
+
+	       The last two terms can be combined like this:
+		   L* ( L
+		        | (( LVT | ( V | LV ) V*) T*))
+
+	       And refactored into this:
+		   L* (L | LVT T* | V  V* T* | LV  V* T*)
+
+	       That means that if we have seen any L's at all we can quit
+	       there, but if the next character is a LVT, a V or and LV we
+	       should keep going.
+
+	       There is a subtlety with Prepend* which showed up in testing.
+	       Note that the Begin, and only the Begin is required in:
+	        | Prepend* Begin Extend*
+	       Also, Begin contains '! Control'.  A Prepend must be a '!
+	       Control', which means it must be a Begin.  What it comes down to
+	       is that if we match Prepend* and then find no suitable Begin
+	       afterwards, that if we backtrack the last Prepend, that one will
+	       be a suitable Begin.
+	    */
+
 	    if (locinput >= PL_regeol)
 		sayNO;
-	    if  (do_utf8) {
-		LOAD_UTF8_CHARCLASS_MARK();
-		if (swash_fetch(PL_utf8_mark,(U8*)locinput, do_utf8))
-		    sayNO;
-		locinput += PL_utf8skip[nextchr];
-		while (locinput < PL_regeol &&
-		       swash_fetch(PL_utf8_mark,(U8*)locinput, do_utf8))
-		    locinput += UTF8SKIP(locinput);
-		if (locinput > PL_regeol)
-		    sayNO;
-	    } 
-	    else
-	       locinput++;
+	    if  (! do_utf8) {
+
+		/* Match either CR LF  or '.', as all the other possibilities
+		 * require utf8 */
+		locinput++;	    /* Match the . or CR */
+		if (nextchr == '\r'
+		    && locinput < PL_regeol
+		    && UCHARAT(locinput) == '\n') locinput++;
+	    }
+	    else {
+
+		/* Utf8: See if is ( CR LF ); already know that locinput <
+		 * PL_regeol, so locinput+1 is in bounds */
+		if (nextchr == '\r' && UCHARAT(locinput + 1) == '\n') {
+		    locinput += 2;
+		}
+		else {
+		    /* In case have to backtrack to beginning, then match '.' */
+		    char *starting = locinput;
+
+		    /* In case have to backtrack the last prepend */
+		    char *previous_prepend = 0;
+
+		    LOAD_UTF8_CHARCLASS_GCB();
+
+		    /* Match (prepend)* */
+		    while (locinput < PL_regeol
+			   && swash_fetch(PL_utf8_X_prepend,
+					  (U8*)locinput, do_utf8))
+		    {
+			previous_prepend = locinput;
+			locinput += UTF8SKIP(locinput);
+		    }
+
+		    /* As noted above, if we matched a prepend character, but
+		     * the next thing won't match, back off the last prepend we
+		     * matched, as it is guaranteed to match the begin */
+		    if (previous_prepend
+			&& (locinput >=  PL_regeol
+			    || ! swash_fetch(PL_utf8_X_begin,
+					     (U8*)locinput, do_utf8)))
+		    {
+			locinput = previous_prepend;
+		    }
+
+		    /* Note that here we know PL_regeol > locinput, as we
+		     * tested that upon input to this switch case, and if we
+		     * moved locinput forward, we tested the result just above
+		     * and it either passed, or we backed off so that it will
+		     * now pass */
+		    if (! swash_fetch(PL_utf8_X_begin, (U8*)locinput, do_utf8)) {
+
+			/* Here did not match the required 'Begin' in the
+			 * second term.  So just match the very first
+			 * character, the '.' of the final term of the regex */
+			locinput = starting + UTF8SKIP(starting);
+		    } else {
+
+			/* Here is the beginning of a character that can have
+			 * an extender.  It is either a hangul syllable, or a
+			 * non-control */
+			if (swash_fetch(PL_utf8_X_non_hangul,
+					(U8*)locinput, do_utf8))
+			{
+
+			    /* Here not a Hangul syllable, must be a
+			     * ('!  * Control') */
+			    locinput += UTF8SKIP(locinput);
+			} else {
+
+			    /* Here is a Hangul syllable.  It can be composed
+			     * of several individual characters.  One
+			     * possibility is T+ */
+			    if (swash_fetch(PL_utf8_X_T,
+					    (U8*)locinput, do_utf8))
+			    {
+				while (locinput < PL_regeol
+					&& swash_fetch(PL_utf8_X_T,
+							(U8*)locinput, do_utf8))
+				{
+				    locinput += UTF8SKIP(locinput);
+				}
+			    } else {
+
+				/* Here, not T+, but is a Hangul.  That means
+				 * it is one of the others: L, LV, LVT or V,
+				 * and matches:
+				 * L* (L | LVT T* | V  V* T* | LV  V* T*) */
+
+				/* Match L*           */
+				while (locinput < PL_regeol
+					&& swash_fetch(PL_utf8_X_L,
+							(U8*)locinput, do_utf8))
+				{
+				    locinput += UTF8SKIP(locinput);
+				}
+
+				/* Here, have exhausted L*.  If the next
+				 * character is not an LV, LVT nor V, it means
+				 * we had to have at least one L, so matches L+
+				 * in the original equation, we have a complete
+				 * hangul syllable.  Are done. */
+
+				if (locinput < PL_regeol
+				    && swash_fetch(PL_utf8_X_LV_LVT_V,
+						    (U8*)locinput, do_utf8))
+				{
+
+				    /* Otherwise keep going.  Must be LV, LVT
+				     * or V.  See if LVT */
+				    if (swash_fetch(PL_utf8_X_LVT,
+						    (U8*)locinput, do_utf8))
+				    {
+					locinput += UTF8SKIP(locinput);
+				    } else {
+
+					/* Must be  V or LV.  Take it, then
+					 * match V*     */
+					locinput += UTF8SKIP(locinput);
+					while (locinput < PL_regeol
+						&& swash_fetch(PL_utf8_X_V,
+							 (U8*)locinput, do_utf8))
+					{
+					    locinput += UTF8SKIP(locinput);
+					}
+				    }
+
+				    /* And any of LV, LVT, or V can be followed
+				     * by T*            */
+				    while (locinput < PL_regeol
+					   && swash_fetch(PL_utf8_X_T,
+							   (U8*)locinput,
+							   do_utf8))
+				    {
+					locinput += UTF8SKIP(locinput);
+				    }
+				}
+			    }
+			}
+
+			/* Match any extender */
+			while (locinput < PL_regeol
+				&& swash_fetch(PL_utf8_X_extend,
+						(U8*)locinput, do_utf8))
+			{
+			    locinput += UTF8SKIP(locinput);
+			}
+		    }
+		}
+		if (locinput > PL_regeol) sayNO;
+	    }
 	    nextchr = UCHARAT(locinput);
 	    break;
             

MANIFEST
@@ -3593,6 +3593,7 @@ lib/Unicode/README		Explanation what happened to lib/unicode.
 lib/Unicode/UCD.pm		Unicode character database
 lib/Unicode/UCD.t		See if Unicode character database works
 lib/unicore/ArabicShaping.txt			Unicode character database
+lib/unicore/auxiliary/GCBTest.txt	Unicode character database
 lib/unicore/auxiliary/GraphemeBreakProperty.txt	Unicode character database
 lib/unicore/auxiliary/SentenceBreakProperty.txt	Unicode character database
 lib/unicore/auxiliary/WordBreakProperty.txt	Unicode character database

embed.fnc
@@ -502,6 +502,16 @@ ApR	|bool	|is_utf8_print	|NN const U8 *p
 ApR	|bool	|is_utf8_punct	|NN const U8 *p
 ApR	|bool	|is_utf8_xdigit	|NN const U8 *p
 ApR	|bool	|is_utf8_mark	|NN const U8 *p
+pR	|bool	|is_utf8_X_begin	|NN const U8 *p
+pR	|bool	|is_utf8_X_extend	|NN const U8 *p
+pR	|bool	|is_utf8_X_prepend	|NN const U8 *p
+pR	|bool	|is_utf8_X_non_hangul	|NN const U8 *p
+pR	|bool	|is_utf8_X_L		|NN const U8 *p
+pR	|bool	|is_utf8_X_LV		|NN const U8 *p
+pR	|bool	|is_utf8_X_LVT		|NN const U8 *p
+pR	|bool	|is_utf8_X_LV_LVT_V	|NN const U8 *p
+pR	|bool	|is_utf8_X_T		|NN const U8 *p
+pR	|bool	|is_utf8_X_V		|NN const U8 *p
 : Used in perly.y
 p	|OP*	|jmaybe		|NN OP *o
 : Used in pp.c 

embed.h
@@ -392,6 +392,16 @@
 #define is_utf8_xdigit		Perl_is_utf8_xdigit
 #define is_utf8_mark		Perl_is_utf8_mark
 #ifdef PERL_CORE
+#define is_utf8_X_begin		Perl_is_utf8_X_begin
+#define is_utf8_X_extend	Perl_is_utf8_X_extend
+#define is_utf8_X_prepend	Perl_is_utf8_X_prepend
+#define is_utf8_X_non_hangul	Perl_is_utf8_X_non_hangul
+#define is_utf8_X_L		Perl_is_utf8_X_L
+#define is_utf8_X_LV		Perl_is_utf8_X_LV
+#define is_utf8_X_LVT		Perl_is_utf8_X_LVT
+#define is_utf8_X_LV_LVT_V	Perl_is_utf8_X_LV_LVT_V
+#define is_utf8_X_T		Perl_is_utf8_X_T
+#define is_utf8_X_V		Perl_is_utf8_X_V
 #define jmaybe			Perl_jmaybe
 #define keyword			Perl_keyword
 #endif
@@ -2785,6 +2795,16 @@
 #define is_utf8_xdigit(a)	Perl_is_utf8_xdigit(aTHX_ a)
 #define is_utf8_mark(a)		Perl_is_utf8_mark(aTHX_ a)
 #ifdef PERL_CORE
+#define is_utf8_X_begin(a)	Perl_is_utf8_X_begin(aTHX_ a)
+#define is_utf8_X_extend(a)	Perl_is_utf8_X_extend(aTHX_ a)
+#define is_utf8_X_prepend(a)	Perl_is_utf8_X_prepend(aTHX_ a)
+#define is_utf8_X_non_hangul(a)	Perl_is_utf8_X_non_hangul(aTHX_ a)
+#define is_utf8_X_L(a)		Perl_is_utf8_X_L(aTHX_ a)
+#define is_utf8_X_LV(a)		Perl_is_utf8_X_LV(aTHX_ a)
+#define is_utf8_X_LVT(a)	Perl_is_utf8_X_LVT(aTHX_ a)
+#define is_utf8_X_LV_LVT_V(a)	Perl_is_utf8_X_LV_LVT_V(aTHX_ a)
+#define is_utf8_X_T(a)		Perl_is_utf8_X_T(aTHX_ a)
+#define is_utf8_X_V(a)		Perl_is_utf8_X_V(aTHX_ a)
 #define jmaybe(a)		Perl_jmaybe(aTHX_ a)
 #define keyword(a,b,c)		Perl_keyword(aTHX_ a,b,c)
 #endif

embedvar.h
@@ -324,6 +324,16 @@
 #define PL_unitcheckav_save	(vTHX->Iunitcheckav_save)
 #define PL_unlockhook		(vTHX->Iunlockhook)
 #define PL_unsafe		(vTHX->Iunsafe)
+#define PL_utf8_X_L		(vTHX->Iutf8_X_L)
+#define PL_utf8_X_LV		(vTHX->Iutf8_X_LV)
+#define PL_utf8_X_LVT		(vTHX->Iutf8_X_LVT)
+#define PL_utf8_X_LV_LVT_V	(vTHX->Iutf8_X_LV_LVT_V)
+#define PL_utf8_X_T		(vTHX->Iutf8_X_T)
+#define PL_utf8_X_V		(vTHX->Iutf8_X_V)
+#define PL_utf8_X_begin		(vTHX->Iutf8_X_begin)
+#define PL_utf8_X_extend	(vTHX->Iutf8_X_extend)
+#define PL_utf8_X_non_hangul	(vTHX->Iutf8_X_non_hangul)
+#define PL_utf8_X_prepend	(vTHX->Iutf8_X_prepend)
 #define PL_utf8_alnum		(vTHX->Iutf8_alnum)
 #define PL_utf8_alpha		(vTHX->Iutf8_alpha)
 #define PL_utf8_ascii		(vTHX->Iutf8_ascii)
@@ -641,6 +651,16 @@
 #define PL_Iunitcheckav_save	PL_unitcheckav_save
 #define PL_Iunlockhook		PL_unlockhook
 #define PL_Iunsafe		PL_unsafe
+#define PL_Iutf8_X_L		PL_utf8_X_L
+#define PL_Iutf8_X_LV		PL_utf8_X_LV
+#define PL_Iutf8_X_LVT		PL_utf8_X_LVT
+#define PL_Iutf8_X_LV_LVT_V	PL_utf8_X_LV_LVT_V
+#define PL_Iutf8_X_T		PL_utf8_X_T
+#define PL_Iutf8_X_V		PL_utf8_X_V
+#define PL_Iutf8_X_begin	PL_utf8_X_begin
+#define PL_Iutf8_X_extend	PL_utf8_X_extend
+#define PL_Iutf8_X_non_hangul	PL_utf8_X_non_hangul
+#define PL_Iutf8_X_prepend	PL_utf8_X_prepend
 #define PL_Iutf8_alnum		PL_utf8_alnum
 #define PL_Iutf8_alpha		PL_utf8_alpha
 #define PL_Iutf8_ascii		PL_utf8_ascii

intrpvar.h
@@ -531,6 +531,16 @@ PERLVAR(Iutf8_print,	SV *)
 PERLVAR(Iutf8_punct,	SV *)
 PERLVAR(Iutf8_xdigit,	SV *)
 PERLVAR(Iutf8_mark,	SV *)
+PERLVAR(Iutf8_X_begin,	SV *)
+PERLVAR(Iutf8_X_extend,	SV *)
+PERLVAR(Iutf8_X_prepend,	SV *)
+PERLVAR(Iutf8_X_non_hangul,	SV *)
+PERLVAR(Iutf8_X_L,	SV *)
+PERLVAR(Iutf8_X_LV,	SV *)
+PERLVAR(Iutf8_X_LVT,	SV *)
+PERLVAR(Iutf8_X_T,	SV *)
+PERLVAR(Iutf8_X_V,	SV *)
+PERLVAR(Iutf8_X_LV_LVT_V,	SV *)
 PERLVAR(Iutf8_toupper,	SV *)
 PERLVAR(Iutf8_totitle,	SV *)
 PERLVAR(Iutf8_tolower,	SV *)

lib/unicore/README.perl
@@ -5,16 +5,17 @@ The *.txt files were copied from
 with subdirectories 'extracted' and 'auxiliary'
 
 The Unihan files were not included due to space considerations.  Also NOT
-included were any *.html files and *Test.txt files.  It is possible to add the
-Unihan files, and edit mktables (see instructions near its beginning) to look
-at them.
+included were any *.html files.  It is possible to add the Unihan files, and
+edit mktables (see instructions near its beginning) to look at them.
 
 The file 'version' should exist and be a single line with the Unicode version,
 like:
 5.2.0
 
 To be 8.3 filesystem friendly, the names of some of the input files have been
-changed from the values that are in the Unicode DB:
+changed from the values that are in the Unicode DB.  Not all of the Test files
+are currently used, so may not be present, so some of the mv's can fail.  The
+.html Test files are not touched.
 
 mv PropertyValueAliases.txt PropValueAliases.txt
 mv NamedSequencesProv.txt NamedSqProv.txt
@@ -33,6 +34,11 @@ mv extracted/DerivedLineBreak.txt extracted/DLineBreak.txt
 mv extracted/DerivedNumericType.txt extracted/DNumType.txt
 mv extracted/DerivedNumericValues.txt extracted/DNumValues.txt
 
+mv auxiliary/GraphemeBreakTest.txt auxiliary/GCBTest.txt
+mv auxiliary/LineBreakTest.txt auxiliary/LBTest.txt
+mv auxiliary/SentenceBreakTest.txt auxiliary/SBTest.txt
+mv auxiliary/WordBreakTest.txt auxiliary/WBTest.txt
+
 If you have the Unihan database (5.2 and above), you should also do the
 following:
 
@@ -45,9 +51,9 @@ mv Unihan_RadicalStrokeCounts.txt UnihanRadicalStrokeCounts.txt
 mv Unihan_Readings.txt UnihanReadings.txt
 mv Unihan_Variants.txt UnihanVariants.txt
 
-If you download everything, the names of files, such as test files, that are
-not used by mktables are not changed by the above, and will not work correctly
-as-is on 8.3 filesystems.
+If you download everything, the names of files that are not used by mktables
+are not changed by the above, and will not work correctly as-is on 8.3
+filesystems.
 
 mktables is used to generate the tables used by the rest of Perl.  It will warn
 you about any *.txt files in the directory substructure that it doesn't know
@@ -58,17 +64,12 @@ its lists to process.  You can run
 
 to have it try to process these tables generically.
 
-If any files are added, deleted, or their names change, you must run
-
-    mktables -makelist
-
-to generate a new list of all the files.
-
 FOR PUMPKINS
 
 The files are inter-related.  If you take the latest UnicodeData.txt, for
 example, but leave the older versions of other files, there can be subtle
-problems.
+problems.  So get everything available from Unicode, and delete those which
+aren't needed.
 
 When moving to a new version of Unicode, you need to update 'version' by hand
 
@@ -85,27 +86,19 @@ mktables can continue to be used for earlier Unicode versions.
 When putting out a new Perl release, think about if any of the Deprecated
 properties should be moved to Suppressed.
 
-The *.pl files are generated from the *.txt files by the mktables script,
-more recently done during the Perl build process, but if you want to try
-the old manual way:
-	
-	cd lib/unicore
-	p4 edit *.pl */*.pl */*/*.pl
-	perl ./mktables -P ../../pod -T ../../t/re/uniprops.t -makelist
-	p4 revert -a
-	cd ../..
-	perl Porting/manicheck
-	
-If any new (or deleted, unlikely but not impossible) *.pl files are indicated:
+The code in regexec.c for the \X match construct is intimately tied to the
+regular expression in UAX #29 (http://www.unicode.org/reports/tr29/).  You
+should see if it has changed, and if so regexec.c should be modified.  The
+current one is
+( CRLF
+| Prepend* ( Hangul-syllable | !Control )
+  ( Grapheme_Extend | Spacing_Mark)*
+| . )
 
-	cd lib/unicore
-	p4 add ...
-	p4 delete ...
-	cd ../...
-	p4 edit MANIFEST
-	...
+mktables has many checks to warn you if there are unexpected or novel things
+that it doesn't know how to handle.
 
-And finally:
+Finally:
 
 	p4 submit
 

commit d275fa5ec19c41bfadd2caecf9152a6e9b995717

Fix compile failure introduced in 37e2e78edfe0a224b8a615820f46db879584f523.
Solaris, VMS, and Win32 all failed to build after this change.  In C99's
description of:

   do statement while ( expression ) ;

the trailing semicolon does not appear to be optional.  And at least
three compilers from three vendors agree.

regexec.c
@@ -130,20 +130,20 @@
 #define LOAD_UTF8_CHARCLASS_SPACE() LOAD_UTF8_CHARCLASS(space," ")
 
 #define LOAD_UTF8_CHARCLASS_GCB()  /* Grapheme cluster boundaries */        \
-	LOAD_UTF8_CHARCLASS(X_begin, " "),                                  \
-	LOAD_UTF8_CHARCLASS(X_non_hangul, "A"),                             \
+	LOAD_UTF8_CHARCLASS(X_begin, " ");                                  \
+	LOAD_UTF8_CHARCLASS(X_non_hangul, "A");                             \
 	/* These are utf8 constants, and not utf-ebcdic constants, so the   \
 	    * assert should likely and hopefully fail on an EBCDIC machine */ \
-	LOAD_UTF8_CHARCLASS(X_extend, "\xcc\x80"), /* U+0300 */             \
+	LOAD_UTF8_CHARCLASS(X_extend, "\xcc\x80"); /* U+0300 */             \
 									    \
 	/* No asserts are done for these, in case called on an early        \
 	    * Unicode version in which they map to nothing */               \
-	LOAD_UTF8_CHARCLASS_NO_CHECK(X_prepend),/* U+0E40 "\xe0\xb9\x80" */ \
-	LOAD_UTF8_CHARCLASS_NO_CHECK(X_L),	    /* U+1100 "\xe1\x84\x80" */ \
-	LOAD_UTF8_CHARCLASS_NO_CHECK(X_LV),     /* U+AC00 "\xea\xb0\x80" */ \
-	LOAD_UTF8_CHARCLASS_NO_CHECK(X_LVT),    /* U+AC01 "\xea\xb0\x81" */ \
-	LOAD_UTF8_CHARCLASS_NO_CHECK(X_LV_LVT_V),/* U+AC01 "\xea\xb0\x81" */\
-	LOAD_UTF8_CHARCLASS_NO_CHECK(X_T),      /* U+11A8 "\xe1\x86\xa8" */ \
+	LOAD_UTF8_CHARCLASS_NO_CHECK(X_prepend);/* U+0E40 "\xe0\xb9\x80" */ \
+	LOAD_UTF8_CHARCLASS_NO_CHECK(X_L);	    /* U+1100 "\xe1\x84\x80" */ \
+	LOAD_UTF8_CHARCLASS_NO_CHECK(X_LV);     /* U+AC00 "\xea\xb0\x80" */ \
+	LOAD_UTF8_CHARCLASS_NO_CHECK(X_LVT);    /* U+AC01 "\xea\xb0\x81" */ \
+	LOAD_UTF8_CHARCLASS_NO_CHECK(X_LV_LVT_V);/* U+AC01 "\xea\xb0\x81" */\
+	LOAD_UTF8_CHARCLASS_NO_CHECK(X_T);      /* U+11A8 "\xe1\x86\xa8" */ \
 	LOAD_UTF8_CHARCLASS_NO_CHECK(X_V)       /* U+1160 "\xe1\x85\xa0" */  
 
 /* 