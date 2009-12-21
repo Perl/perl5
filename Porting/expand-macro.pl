Perl/perl5

commit 4784c5e80ccedd3734ce162e02ed1c7e25e60c01

Allow expand-macro.pl to expand macros in perl.h without pre-processor warnings.

Porting/expand-macro.pl
@@ -57,7 +57,12 @@ my $sentinel = "$macro expands to";
 print $out <<"EOF";
 #include "EXTERN.h"
 #include "perl.h"
-#include "$header"
+EOF
+
+print qq{#include "$header"\n}
+    unless $header eq 'perl.h' or $header eq 'EXTERN.h';
+
+print $out <<"EOF";
 #line 4 "$sentinel"
 $macro$args
 EOF

commit c3c0aa283b73660f84ae7e190dcbbd607facb512

expand-macro.pl was printing the header include to STDOUT by mistake.
This stopped it expanding macros in header files not directly or indirectly
included via perl.h or EXTERN.h

The bug was introduced with change 4784c5e80ccedd3734ce162e02ed1c7e25e60c01.

Porting/expand-macro.pl
@@ -59,7 +59,7 @@ print $out <<"EOF";
 #include "perl.h"
 EOF
 
-print qq{#include "$header"\n}
+print $out qq{#include "$header"\n}
     unless $header eq 'perl.h' or $header eq 'EXTERN.h';
 
 print $out <<"EOF";