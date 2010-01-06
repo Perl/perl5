bb1773de14d1367a85531986b90d3ce2d9da9dcb
Abigail <abigail@abigail.be>
2010-01-06 18:24:14 +0100

    Fix typo in reference

--- ext/re/re.pm
+++ ext/re/re.pm
@@ -209,7 +209,7 @@ variable interpolation.  That is normally disallowed, since it is a
 potential security risk.  Note that this pragma is ignored when the regular
 expression is obtained from tainted data, i.e.  evaluation is always
 disallowed with tainted regular expressions.  See L<perlre/(?{ code })> 
-and L<perlre/(?{ code })>.
+and L<perlre/(??{ code })>.
 
 For the purpose of this pragma, interpolation of precompiled regular
 expressions (i.e., the result of C<qr//>) is I<not> considered variable
