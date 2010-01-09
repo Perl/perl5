Perl/perl5

commit 4149198fba64273f3fea8fc073ccb5d080059f4a

reset sv_serial for each new interpreter
to a value of 0 + 1000000*interpreter_number
where interpreter_number is currently just a dirty hash of my_perl

sv.c
@@ -11836,6 +11836,7 @@ perl_clone_using(PerlInterpreter *proto_perl, UV flags,
     PL_sig_pending = 0;
     PL_parser = NULL;
     Zero(&PL_debug_pad, 1, struct perl_debug_pad);
+    PL_sv_serial = (((U32)my_perl >> 2) & 0xfff) * 1000000;
 #else	/* !DEBUGGING */
     Zero(my_perl, 1, PerlInterpreter);
 #endif	/* DEBUGGING */

commit 02d9cd5ef82ff73ba121632a5e98252e4d298ed5

PL_sv_serial only exists ifdef DEBUG_LEAKING_SCALARS
Fix for 4149198fba64273f3fea8fc073ccb5d080059f4a which broke
debugging build. Whoops!

sv.c
@@ -11836,7 +11836,9 @@ perl_clone_using(PerlInterpreter *proto_perl, UV flags,
     PL_sig_pending = 0;
     PL_parser = NULL;
     Zero(&PL_debug_pad, 1, struct perl_debug_pad);
+#  ifdef DEBUG_LEAKING_SCALARS
     PL_sv_serial = (((U32)my_perl >> 2) & 0xfff) * 1000000;
+#  endif
 #else	/* !DEBUGGING */
     Zero(my_perl, 1, PerlInterpreter);
 #endif	/* DEBUGGING */