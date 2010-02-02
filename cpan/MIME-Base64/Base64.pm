9f1a4ec8341de9d773560197e1271866342ebdcb
Gisle Aas <gisle@aas.no>
2010-02-02 23:22:15 +0100

    Update to MIME-Base64 3.09
    Fixes issue where the Quoted-Printable encoder would sometimes output
    lines that were 77 characters long.  The max line length should be 76.
    
    This reverts revert 9ef40646096384d9f65acffab1b9a2df07d03a0c.

--- cpan/MIME-Base64/Base64.pm
+++ cpan/MIME-Base64/Base64.pm
@@ -7,7 +7,7 @@ require Exporter;
 @ISA = qw(Exporter);
 @EXPORT = qw(encode_base64 decode_base64);
 
-$VERSION = '3.08';
+$VERSION = '3.09';
 
 require XSLoader;
 XSLoader::load('MIME::Base64', $VERSION);
--- cpan/MIME-Base64/QuotedPrint.pm
+++ cpan/MIME-Base64/QuotedPrint.pm
@@ -7,7 +7,7 @@ require Exporter;
 @ISA = qw(Exporter);
 @EXPORT = qw(encode_qp decode_qp);
 
-$VERSION = "3.08";
+$VERSION = "3.09";
 
 use MIME::Base64;  # will load XS version of {en,de}code_qp()
 
