677a29545230804411babf7eefdbced18ffdd71d
David Mitchell <davem@iabyn.com>
2010-02-24 22:41:09 +0000

    more prose fixups to bytes.pm deprecation warning
    (suggested by Dr.Rudd)

--- lib/bytes.pm
+++ lib/bytes.pm
@@ -36,14 +36,14 @@ bytes - Perl pragma to force byte semantics rather than character semantics
 =head1 DEPRECATED
 
 This pragma reflects early attempts to incorporate Unicode into perl and
-has since been superceded. It breaks encapulation (i.e. it exposes the
-innards of how perl happens to be currently storing a string), and use of
-this module for anything other than debugging purposes is strongly
-discouraged. If you feel that the functions here within might be useful
-for your application, this possibly indicates a mismatch beteen your
-mental model of Perl Unicode and the current reality. In that case, you
-may wish to peruse some of the perl Unicode documentation: L<perluniintro>,
-L<perlunitut>, L<perlunifaq> and L<perlunicode>.
+has since been superseded. It breaks encapsulation (i.e. it exposes the
+innards of how the perl executable currently happens to store a string),
+and use of this module for anything other than debugging purposes is
+strongly discouraged. If you feel that the functions here within might be
+useful for your application, this possibly indicates a mismatch beteen
+your mental model of Perl Unicode and the current reality. In that case,
+you may wish to peruse some of the perl Unicode documentation:
+L<perluniintro>, L<perlunitut>, L<perlunifaq> and L<perlunicode>.
 
 =head1 SYNOPSIS
 
