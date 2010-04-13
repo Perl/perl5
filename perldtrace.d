6b5ec6e4fa6ef6732ba89d1614d3c98944138a22
Josh ben Jore <jjore@cpan.org>
2010-04-13 07:53:12 -0700

    Add emacs/vim hints

--- perldtrace.d
+++ perldtrace.d
@@ -7,3 +7,12 @@ provider perl {
     probe sub__entry(char *, char *, int);
     probe sub__return(char *, char *, int);
 };
+
+/*
+ * Local Variables:
+ * tab-width: 4
+ * indent-tabs-mode: nil
+ * End:
+ *
+ * ex: set ts=8 sts=4 sw=4 noet:
+ */
