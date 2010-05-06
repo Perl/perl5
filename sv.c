Perl/perl5

commit 5b98cd54dff3b16344eab33ce6b09fb6fb1b89c2

Make sv_vcatpvfn() complain when special formats "%s" and "%.0f" can't find their argument
This fixes [RT #62874] : printf does not print a warning when a lone %s conversion is used

sv.c
@@ -9365,6 +9365,8 @@ Perl_sv_vcatpvfn(pTHX_ SV *const sv, const char *const pat, const STRLEN patlen,
 	else if (svix < svmax) {
 	    sv_catsv(sv, *svargs);
 	}
+	else
+	    S_vcatpvfn_missing_argument(aTHX);
 	return;
     }
     if (args && patlen == 3 && pat[0] == '%' &&
@@ -9390,7 +9392,7 @@ Perl_sv_vcatpvfn(pTHX_ SV *const sv, const char *const pat, const STRLEN patlen,
 	    if (svix < svmax)
 		nv = SvNV(*svargs);
 	    else
-		return;
+		S_vcatpvfn_missing_argument(aTHX);
 	    if (*pp == 'g') {
 		/* Add check for digits != 0 because it seems that some
 		   gconverts are buggy in this case, and we don't yet have

t/op/sprintf.t
@@ -373,6 +373,8 @@ __END__
 >%+8.1f<    >-1234.875<   > -1234.9<
 >%*.*f<     >[5, 2, 12.3456]< >12.35<
 >%f<        >0<           >0.000000<
+>%.0f<      >[]<          >0 MISSING<
+> %.0f<     >[]<          > 0 MISSING<
 >%.0f<      >0<           >0<
 >%.0f<      >2**38<       >274877906944<   >Should have exact int'l rep'n<
 >%.0f<      >0.1<         >0<
@@ -494,6 +496,8 @@ __END__
 >%#p<       >''<          >%#p INVALID<
 >%q<        >''<          >%q INVALID<
 >%r<        >''<          >%r INVALID<
+>%s<        >[]<          > MISSING<
+> %s<       >[]<          >  MISSING<
 >%s<        >'string'<    >string<
 >%10s<      >'string'<    >    string<
 >%+10s<     >'string'<    >    string<

commit d347ad18ecf3da70504843223bc83fa49705ebaf

Stop returning 0 for sprintf("%.0g")
And also from throwing two "Missing argument" warnings for this construct.

This was a regression introduced by 5b98cd54dff3b16344eab33ce6b09fb6fb1b89c2.

sv.c
@@ -9391,8 +9391,10 @@ Perl_sv_vcatpvfn(pTHX_ SV *const sv, const char *const pat, const STRLEN patlen,
 
 	    if (svix < svmax)
 		nv = SvNV(*svargs);
-	    else
+	    else {
 		S_vcatpvfn_missing_argument(aTHX);
+		return;
+	    }
 	    if (*pp == 'g') {
 		/* Add check for digits != 0 because it seems that some
 		   gconverts are buggy in this case, and we don't yet have

t/op/sprintf.t
@@ -374,7 +374,7 @@ __END__
 >%+8.1f<    >-1234.875<   > -1234.9<
 >%*.*f<     >[5, 2, 12.3456]< >12.35<
 >%f<        >0<           >0.000000<
->%.0f<      >[]<          >0 MISSING<
+>%.0f<      >[]<          > MISSING<
 > %.0f<     >[]<          > 0 MISSING<
 >%.0f<      >0<           >0<
 >%.0f<      >2**38<       >274877906944<   >Should have exact int'l rep'n<
@@ -390,7 +390,9 @@ __END__
 >%g<        >12345.6789<  >12345.7<
 >%+g<       >12345.6789<  >+12345.7<
 >%#g<       >12345.6789<  >12345.7<
->%.0g<      >-0.0<	  >-0<		   >C99 standard mandates minus sign but C89 does not skip: MSWin32 VMS hpux:10.20 openbsd netbsd:1.5 irix darwin<
+>%.0g<      >[]<          > MISSING<
+> %.0g<     >[]<          > 0 MISSING<
+>%.0g<      >-0.0<        >-0<		   >C99 standard mandates minus sign but C89 does not skip: MSWin32 VMS hpux:10.20 openbsd netbsd:1.5 irix darwin<
 >%.0g<      >12345.6789<  >1e+04<
 >%#.0g<     >12345.6789<  >1.e+04<
 >%.2g<      >12345.6789<  >1.2e+04<