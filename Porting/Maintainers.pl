04febe174c342d1018822ab2c67fb381bb88c55f
Jerry D. Hedden <jdhedden@cpan.org>
2010-06-11 07:57:34 +0100

    [PATCH-revised] Upgrade to Thread-Semaphore 2.11
      Added new methods ->down_nb() and ->down_force() at the suggestion of
      Rick Garlick.
    
      Refactored methods to skip argument validation when no argument is supplied.
    
    Signed-off-by: Chris 'BinGOs' Williams <chris@bingosnet.co.uk>

--- Porting/Maintainers.pl
+++ Porting/Maintainers.pl
@@ -1406,7 +1406,7 @@ use File::Glob qw(:case);
     'Thread::Semaphore' =>
 	{
 	'MAINTAINER'	=> 'jdhedden',
-	'DISTRIBUTION'	=> 'JDHEDDEN/Thread-Semaphore-2.09.tar.gz',
+	'DISTRIBUTION'	=> 'JDHEDDEN/Thread-Semaphore-2.11.tar.gz',
 	'FILES'		=> q[dist/Thread-Semaphore],
 	'EXCLUDED'	=> [ qw(examples/semaphore.pl
 				t/00_load.t
