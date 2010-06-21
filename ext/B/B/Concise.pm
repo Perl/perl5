c6e505a8d0d6cd507b02a13babaff0238f7a24e2
Matt S Trout <mst@shadowcat.co.uk>
2010-06-21 22:04:40 +0100

    version bump for B::Concise and add change to delta

--- ext/B/B/Concise.pm
+++ ext/B/B/Concise.pm
@@ -14,7 +14,7 @@ use warnings; # uses #3 and #4, since warnings uses Carp
 
 use Exporter (); # use #5
 
-our $VERSION   = "0.78";
+our $VERSION   = "0.79";
 our @ISA       = qw(Exporter);
 our @EXPORT_OK = qw( set_style set_style_standard add_callback
 		     concise_subref concise_cv concise_main
