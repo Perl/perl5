9e09409a8eb5c132fdad60484f1ae39ebd8b4e32
Chris 'BinGOs' Williams <chris@bingosnet.co.uk>
2010-06-30 10:03:06 +0100

    Update Compress-Raw-Bzip2 to CPAN version 2.027
      [DELTA]
    
      2.027 24 April 2010
    
          * No Changes
    
      2.026 7 April 2010
    
          * No Changes
    
      2.025 27 March 2010
    
          * Documented the unused "verbosity" option in
            Compress::Raw::Bunzip2::new
            [RT# 54425]
    
      2.023 9 November 2009
    
          * Removed redundant bzip2 source files from the bzip2-src directory.
            [RT# 47225]
    
          * Fixed instance where $[ should have been $] in t/01bzip2.t
            Thanks to Robin Barker and zefram [RT #50764] for independantly
            spotting the issue.

--- Porting/Maintainers.pl
+++ Porting/Maintainers.pl
@@ -310,7 +310,7 @@ use File::Glob qw(:case);
     'Compress::Raw::Bzip2' =>
 	{
 	'MAINTAINER'	=> 'pmqs',
-	'DISTRIBUTION'	=> 'PMQS/Compress-Raw-Bzip2-2.021.tar.gz',
+	'DISTRIBUTION'	=> 'PMQS/Compress-Raw-Bzip2-2.027.tar.gz',
 	'FILES'		=> q[cpan/Compress-Raw-Bzip2],
 	'EXCLUDED'	=> [ qr{^t/Test/},
 			     qw( bzip2-src/bzip2-cpp.patch
