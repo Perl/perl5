Perl/perl5

commit 64345bb5cdba725a5e2af06c99aa36d8a1b8b873

make it an error to look for magic hv backref
Now that we only store a stash's backref AV in xhv_backreferences,
make it a panic if we don't find it there, rather than falling back to
look for backref magic.

sv.c
@@ -5373,14 +5373,16 @@ Perl_sv_del_backref(pTHX_ SV *const tsv, SV *const sv)
 
     PERL_ARGS_ASSERT_SV_DEL_BACKREF;
 
-    if (SvTYPE(tsv) == SVt_PVHV && SvOOK(tsv)) {
-	av = *Perl_hv_backreferences_p(aTHX_ MUTABLE_HV(tsv));
-	/* We mustn't attempt to "fix up" the hash here by moving the
-	   backreference array back to the hv_aux structure, as that is stored
-	   in the main HvARRAY(), and hfreentries assumes that no-one
-	   reallocates HvARRAY() while it is running.  */
+    if (SvTYPE(tsv) == SVt_PVHV) {
+	if (SvOOK(tsv)) {
+	    /* SvOOK: We must avoid creating the hv_aux structure if its
+	     * not already there, as that is stored in the main HvARRAY(),
+	     * and hfreentries assumes that no-one reallocates HvARRAY()
+	     * while it is running.  */
+	    av = *Perl_hv_backreferences_p(aTHX_ MUTABLE_HV(tsv));
+	}
     }
-    if (!av) {
+    else {
 	const MAGIC *const mg
 	    = SvMAGICAL(tsv) ? mg_find(tsv, PERL_MAGIC_backref) : NULL;
 	if (mg)

commit 1c480c903473b013658c29213ac3d1772ab6b63a

Hash assignment can zap weak references to the hash
[perl #76716]

commits 044d8c24f and 64345bb broke backrefs to hashes that are merely
cleared or undeffed, but not freed. Spotted by Father Chrysostomos.
Test for it here (fixes coming next)

t/op/hashassign.t
@@ -8,7 +8,7 @@ BEGIN {
 
 # use strict;
 
-plan tests => 215;
+plan tests => 217;
 
 my @comma = ("key", "value");
 
@@ -305,4 +305,15 @@ foreach my $chr (60, 200, 600, 6000, 60000) {
     @h{@refs} = @types;
     @expect{map "$_", @refs} = @types;
     ok (eq_hash(\%h, \%expect), 'blessed ref stringification');
+}
+
+# [perl #76716] Hash assignment should not zap weak refs.
+{
+ my %tb;
+ use Scalar::Util;
+ Scalar::Util::weaken(my $p = \%tb);
+ %tb = ();
+ is $p, \%tb, "hash assignment should not zap weak refs";
+ undef %tb;
+ is $p, \%tb, "hash undef should not zap weak refs";
 }