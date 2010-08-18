d98a0e0610723630e3a6cf6f1de20f87701b22bc
Chris 'BinGOs' Williams <chris@bingosnet.co.uk>
2010-08-18 22:57:24 +0100

    Version bump Module-CoreList to 2.37 for a CPAN release

--- Porting/Maintainers.pl
+++ Porting/Maintainers.pl
@@ -956,7 +956,7 @@ use File::Glob qw(:case);
     'Module::CoreList' =>
 	{
 	'MAINTAINER'	=> 'rgarcia',
-	'DISTRIBUTION'	=> 'BINGOS/Module-CoreList-2.36.tar.gz',
+	'DISTRIBUTION'	=> 'BINGOS/Module-CoreList-2.37.tar.gz',
 	'FILES'		=> q[dist/Module-CoreList],
 	'UPSTREAM'	=> 'blead',
 	},
--- dist/Module-CoreList/lib/Module/CoreList.pm
+++ dist/Module-CoreList/lib/Module/CoreList.pm
@@ -2,7 +2,7 @@ package Module::CoreList;
 use strict;
 use vars qw/$VERSION %released %version %families %upstream
 	    %bug_tracker %deprecated/;
-$VERSION = '2.36';
+$VERSION = '2.37';
 
 =head1 NAME
 
