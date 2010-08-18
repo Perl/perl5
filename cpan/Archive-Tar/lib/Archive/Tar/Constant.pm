ff7894f5de15c54bf9db4d72df358a9df7f78f2e
Chris 'BinGOs' Williams <chris@bingosnet.co.uk>
2010-08-18 11:20:06 +0100

    Update Archive-Tar to CPAN version 1.68
      [DELTA]
    
      * important changes in version 1.68 17/08/2010
      - Apply a patch from Colin Newell that checks whether long files can be created or
        not in 02_methods.t. Hopefully resolves RT #57312 and RT #56163
    
      Most importantly it should resolve test failures when building on encrypted
      filesystems.

--- cpan/Archive-Tar/lib/Archive/Tar/Constant.pm
+++ cpan/Archive-Tar/lib/Archive/Tar/Constant.pm
@@ -3,7 +3,7 @@ package Archive::Tar::Constant;
 BEGIN {
     require Exporter;
     
-    $VERSION    = '1.66';
+    $VERSION    = '1.68';
     @ISA        = qw[Exporter];
 
     require Time::Local if $^O eq "MacOS";
