Perl/perl5

commit ea25a9b2cf73948b1e8c5675de027e0ad13277bd

make qw(...) first-class syntax
This makes a qw(...) list literal a distinct token type for the
parser, where previously it was munged into a "(",THING,")" sequence.
The change means that qw(...) can't accidentally supply parens to parts
of the grammar that want real parens.  Due to many bits of code taking
advantage of that by "foreach my $x qw(...) {}", this patch also includes
a hack to coerce qw(...) to the old-style parenthesised THING, emitting
a deprecation warning along the way.

toke.c
@@ -7319,14 +7319,13 @@ Perl_yylex(pTHX)
 	case KEY_quotemeta:
 	    UNI(OP_QUOTEMETA);
 
-	case KEY_qw:
+	case KEY_qw: {
+	    OP *words = NULL;
 	    s = scan_str(s,!!PL_madskills,FALSE);
 	    if (!s)
 		missingterm(NULL);
 	    PL_expect = XOPERATOR;
-	    force_next(')');
 	    if (SvCUR(PL_lex_stuff)) {
-		OP *words = NULL;
 		int warned = 0;
 		d = SvPV_force(PL_lex_stuff, len);
 		while (len) {
@@ -7358,18 +7357,17 @@ Perl_yylex(pTHX)
 					    newSVOP(OP_CONST, 0, tokeq(sv)));
 		    }
 		}
-		if (words) {
-		    start_force(PL_curforce);
-		    NEXTVAL_NEXTTOKE.opval = words;
-		    force_next(THING);
-		}
 	    }
+	    if (!words)
+		words = newNULLLIST();
 	    if (PL_lex_stuff) {
 		SvREFCNT_dec(PL_lex_stuff);
 		PL_lex_stuff = NULL;
 	    }
-	    PL_expect = XTERM;
-	    TOKEN('(');
+	    PL_expect = XOPERATOR;
+	    pl_yylval.opval = sawparens(words);
+	    TOKEN(QWLIST);
+	}
 
 	case KEY_qq:
 	    s = scan_str(s,!!PL_madskills,FALSE);
@@ -13983,6 +13981,22 @@ Perl_parse_fullstmt(pTHX_ U32 flags)
     fullstmtop = PL_eval_root;
     LEAVE;
     return fullstmtop;
+}
+
+void
+Perl_coerce_qwlist_to_paren_list(pTHX_ OP *qwlist)
+{
+    PERL_ARGS_ASSERT_COERCE_QWLIST_TO_PAREN_LIST;
+    deprecate("qw(...) as parentheses");
+    force_next(')');
+    if (qwlist->op_type == OP_STUB) {
+	op_free(qwlist);
+    }
+    else {
+	NEXTVAL_NEXTTOKE.opval = qwlist;
+	force_next(THING);
+    }
+    force_next('(');
 }
 
 /*

embed.fnc
@@ -2398,6 +2398,8 @@ xpoM	|void|store_cop_label \
 		|NN COP *cop|NN const char *label|STRLEN len|U32 flags
 
 xpo	|int	|keyword_plugin_standard|NN char* keyword_ptr|STRLEN keyword_len|NN OP** op_ptr
+: Used in perly.y
+xp	|void	|coerce_qwlist_to_paren_list|NN OP* qwlist
 
 #if defined(USE_ITHREADS)
 #  if defined(PERL_IN_SV_C)

embed.h
@@ -2081,6 +2081,9 @@
 #ifdef PERL_CORE
 #define boot_core_mro		Perl_boot_core_mro
 #endif
+#ifdef PERL_CORE
+#define coerce_qwlist_to_paren_list	Perl_coerce_qwlist_to_paren_list
+#endif
 #if defined(USE_ITHREADS)
 #  if defined(PERL_IN_SV_C)
 #ifdef PERL_CORE
@@ -4556,6 +4559,7 @@
 #ifdef PERL_CORE
 #endif
 #ifdef PERL_CORE
+#define coerce_qwlist_to_paren_list(a)	Perl_coerce_qwlist_to_paren_list(aTHX_ a)
 #endif
 #if defined(USE_ITHREADS)
 #  if defined(PERL_IN_SV_C)

lib/Tie/Scalar.t
@@ -22,7 +22,7 @@ use Test::More tests => 16;
 use_ok( 'Tie::Scalar' );
 
 # these are "abstract virtual" parent methods
-for my $method qw( TIESCALAR FETCH STORE ) {
+for my $method (qw( TIESCALAR FETCH STORE )) {
 	eval { Tie::Scalar->$method() };
 	like( $@, qr/doesn't define a $method/, "croaks on inherited $method()" );
 }

lib/unicore/mktables
@@ -792,7 +792,7 @@ if ($v_version gt v3.2.0) {
 # unless explicitly added.
 if ($v_version ge v5.2.0) {
     my $unihan = 'Unihan; remove from list if using Unihan';
-    foreach my $table qw (
+    foreach my $table (qw (
                            kAccountingNumeric
                            kOtherNumeric
                            kPrimaryNumeric
@@ -808,7 +808,7 @@ if ($v_version ge v5.2.0) {
                            kIRG_USource
                            kIRG_VSource
                            kRSUnicode
-                        )
+                        ))
     {
         $why_suppress_if_empty_warn_if_not{$table} = $unihan;
     }
@@ -4314,10 +4314,10 @@ sub trace { return main::trace(@_); }
 
     # Here are the methods that are required to be defined by any derived
     # class
-    for my $sub qw(
+    for my $sub (qw(
                     append_to_body
                     pre_body
-                )
+                ))
                 # append_to_body and pre_body are called in the write() method
                 # to add stuff after the main body of the table, but before
                 # its close; and to prepend stuff before the beginning of the
@@ -4795,7 +4795,7 @@ sub trace { return main::trace(@_); }
 
     # Accessors for the range list stored in this table.  First for
     # unconditional
-    for my $sub qw(
+    for my $sub (qw(
                     contains
                     count
                     each_range
@@ -4806,7 +4806,7 @@ sub trace { return main::trace(@_); }
                     range_count
                     reset_each_range
                     value_of
-                )
+                ))
     {
         no strict "refs";
         *$sub = sub {
@@ -4818,9 +4818,9 @@ sub trace { return main::trace(@_); }
     }
 
     # Then for ones that should fail if locked
-    for my $sub qw(
+    for my $sub (qw(
                     delete_range
-                )
+                ))
     {
         no strict "refs";
         *$sub = sub {
@@ -5783,9 +5783,9 @@ END
     }
 
     # Accessors for the underlying list that should fail if locked.
-    for my $sub qw(
+    for my $sub (qw(
                     add_duplicate
-                )
+                ))
     {
         no strict "refs";
         *$sub = sub {
@@ -6553,10 +6553,10 @@ END
     }
 
     # Accessors for the underlying list
-    for my $sub qw(
+    for my $sub (qw(
                     get_valid_code_point
                     get_invalid_code_point
-                )
+                ))
     {
         no strict "refs";
         *$sub = sub {
@@ -7121,7 +7121,7 @@ sub trace { return main::trace(@_) if main::DEBUG && $to_trace }
 
     # Most of the accessors for a property actually apply to its map table.
     # Setup up accessor functions for those, referring to %map
-    for my $sub qw(
+    for my $sub (qw(
                     add_alias
                     add_anomalous_entry
                     add_comment
@@ -7166,7 +7166,7 @@ sub trace { return main::trace(@_) if main::DEBUG && $to_trace }
                     to_output_map
                     value_of
                     write
-                )
+                ))
                     # 'property' above is for symmetry, so that one can take
                     # the property of a property and get itself, and so don't
                     # have to distinguish between properties and tables in

perly.h
@@ -48,71 +48,72 @@
      THING = 263,
      PMFUNC = 264,
      PRIVATEREF = 265,
-     FUNC0SUB = 266,
-     UNIOPSUB = 267,
-     LSTOPSUB = 268,
-     PLUGEXPR = 269,
-     PLUGSTMT = 270,
-     LABEL = 271,
-     FORMAT = 272,
-     SUB = 273,
-     ANONSUB = 274,
-     PACKAGE = 275,
-     USE = 276,
-     WHILE = 277,
-     UNTIL = 278,
-     IF = 279,
-     UNLESS = 280,
-     ELSE = 281,
-     ELSIF = 282,
-     CONTINUE = 283,
-     FOR = 284,
-     GIVEN = 285,
-     WHEN = 286,
-     DEFAULT = 287,
-     LOOPEX = 288,
-     DOTDOT = 289,
-     YADAYADA = 290,
-     FUNC0 = 291,
-     FUNC1 = 292,
-     FUNC = 293,
-     UNIOP = 294,
-     LSTOP = 295,
-     RELOP = 296,
-     EQOP = 297,
-     MULOP = 298,
-     ADDOP = 299,
-     DOLSHARP = 300,
-     DO = 301,
-     HASHBRACK = 302,
-     NOAMP = 303,
-     LOCAL = 304,
-     MY = 305,
-     MYSUB = 306,
-     REQUIRE = 307,
-     COLONATTR = 308,
-     PREC_LOW = 309,
-     DOROP = 310,
-     OROP = 311,
-     ANDOP = 312,
-     NOTOP = 313,
-     ASSIGNOP = 314,
-     DORDOR = 315,
-     OROR = 316,
-     ANDAND = 317,
-     BITOROP = 318,
-     BITANDOP = 319,
-     SHIFTOP = 320,
-     MATCHOP = 321,
-     REFGEN = 322,
-     UMINUS = 323,
-     POWOP = 324,
-     POSTDEC = 325,
-     POSTINC = 326,
-     PREDEC = 327,
-     PREINC = 328,
-     ARROW = 329,
-     PEG = 330
+     QWLIST = 266,
+     FUNC0SUB = 267,
+     UNIOPSUB = 268,
+     LSTOPSUB = 269,
+     PLUGEXPR = 270,
+     PLUGSTMT = 271,
+     LABEL = 272,
+     FORMAT = 273,
+     SUB = 274,
+     ANONSUB = 275,
+     PACKAGE = 276,
+     USE = 277,
+     WHILE = 278,
+     UNTIL = 279,
+     IF = 280,
+     UNLESS = 281,
+     ELSE = 282,
+     ELSIF = 283,
+     CONTINUE = 284,
+     FOR = 285,
+     GIVEN = 286,
+     WHEN = 287,
+     DEFAULT = 288,
+     LOOPEX = 289,
+     DOTDOT = 290,
+     YADAYADA = 291,
+     FUNC0 = 292,
+     FUNC1 = 293,
+     FUNC = 294,
+     UNIOP = 295,
+     LSTOP = 296,
+     RELOP = 297,
+     EQOP = 298,
+     MULOP = 299,
+     ADDOP = 300,
+     DOLSHARP = 301,
+     DO = 302,
+     HASHBRACK = 303,
+     NOAMP = 304,
+     LOCAL = 305,
+     MY = 306,
+     MYSUB = 307,
+     REQUIRE = 308,
+     COLONATTR = 309,
+     PREC_LOW = 310,
+     DOROP = 311,
+     OROP = 312,
+     ANDOP = 313,
+     NOTOP = 314,
+     ASSIGNOP = 315,
+     DORDOR = 316,
+     OROR = 317,
+     ANDAND = 318,
+     BITOROP = 319,
+     BITANDOP = 320,
+     SHIFTOP = 321,
+     MATCHOP = 322,
+     REFGEN = 323,
+     UMINUS = 324,
+     POWOP = 325,
+     POSTDEC = 326,
+     POSTINC = 327,
+     PREDEC = 328,
+     PREINC = 329,
+     ARROW = 330,
+     PEG = 331
    };
 #endif
 /* Tokens.  */
@@ -124,71 +125,72 @@
 #define THING 263
 #define PMFUNC 264
 #define PRIVATEREF 265
-#define FUNC0SUB 266
-#define UNIOPSUB 267
-#define LSTOPSUB 268
-#define PLUGEXPR 269
-#define PLUGSTMT 270
-#define LABEL 271
-#define FORMAT 272
-#define SUB 273
-#define ANONSUB 274
-#define PACKAGE 275
-#define USE 276
-#define WHILE 277
-#define UNTIL 278
-#define IF 279
-#define UNLESS 280
-#define ELSE 281
-#define ELSIF 282
-#define CONTINUE 283
-#define FOR 284
-#define GIVEN 285
-#define WHEN 286
-#define DEFAULT 287
-#define LOOPEX 288
-#define DOTDOT 289
-#define YADAYADA 290
-#define FUNC0 291
-#define FUNC1 292
-#define FUNC 293
-#define UNIOP 294
-#define LSTOP 295
-#define RELOP 296
-#define EQOP 297
-#define MULOP 298
-#define ADDOP 299
-#define DOLSHARP 300
-#define DO 301
-#define HASHBRACK 302
-#define NOAMP 303
-#define LOCAL 304
-#define MY 305
-#define MYSUB 306
-#define REQUIRE 307
-#define COLONATTR 308
-#define PREC_LOW 309
-#define DOROP 310
-#define OROP 311
-#define ANDOP 312
-#define NOTOP 313
-#define ASSIGNOP 314
-#define DORDOR 315
-#define OROR 316
-#define ANDAND 317
-#define BITOROP 318
-#define BITANDOP 319
-#define SHIFTOP 320
-#define MATCHOP 321
-#define REFGEN 322
-#define UMINUS 323
-#define POWOP 324
-#define POSTDEC 325
-#define POSTINC 326
-#define PREDEC 327
-#define PREINC 328
-#define ARROW 329
-#define PEG 330
+#define QWLIST 266
+#define FUNC0SUB 267
+#define UNIOPSUB 268
+#define LSTOPSUB 269
+#define PLUGEXPR 270
+#define PLUGSTMT 271
+#define LABEL 272
+#define FORMAT 273
+#define SUB 274
+#define ANONSUB 275
+#define PACKAGE 276
+#define USE 277
+#define WHILE 278
+#define UNTIL 279
+#define IF 280
+#define UNLESS 281
+#define ELSE 282
+#define ELSIF 283
+#define CONTINUE 284
+#define FOR 285
+#define GIVEN 286
+#define WHEN 287
+#define DEFAULT 288
+#define LOOPEX 289
+#define DOTDOT 290
+#define YADAYADA 291
+#define FUNC0 292
+#define FUNC1 293
+#define FUNC 294
+#define UNIOP 295
+#define LSTOP 296
+#define RELOP 297
+#define EQOP 298
+#define MULOP 299
+#define ADDOP 300
+#define DOLSHARP 301
+#define DO 302
+#define HASHBRACK 303
+#define NOAMP 304
+#define LOCAL 305
+#define MY 306
+#define MYSUB 307
+#define REQUIRE 308
+#define COLONATTR 309
+#define PREC_LOW 310
+#define DOROP 311
+#define OROP 312
+#define ANDOP 313
+#define NOTOP 314
+#define ASSIGNOP 315
+#define DORDOR 316
+#define OROR 317
+#define ANDAND 318
+#define BITOROP 319
+#define BITANDOP 320
+#define SHIFTOP 321
+#define MATCHOP 322
+#define REFGEN 323
+#define UMINUS 324
+#define POWOP 325
+#define POSTDEC 326
+#define POSTINC 327
+#define PREDEC 328
+#define PREINC 329
+#define ARROW 330
+#define PEG 331
 
 
 

commit 3d8e05a034fc6625a503b87c8ac336d4d84fb338

fix MAD breakage caused by qw patch
ea25a9b2cf73948b1e8c5675de027e0ad13277bd broke MAD due to incorrect
usage of the token-forcing mechanism.

toke.c
@@ -13993,6 +13993,7 @@ Perl_coerce_qwlist_to_paren_list(pTHX_ OP *qwlist)
 	op_free(qwlist);
     }
     else {
+	start_force(PL_curforce);
 	NEXTVAL_NEXTTOKE.opval = qwlist;
 	force_next(THING);
     }