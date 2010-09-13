7650682f4377a73dc7c46a6c3b8113a5f987c902
Rafael Garcia-Suarez <rgs@consttype.org>
2010-09-13 15:11:27 +0200

    Avoid infinite loop in _find_code_refs.
    Patch by Yasushi Nakajima (rt.cpan.org #61262)

--- dist/Safe/Safe.pm
+++ dist/Safe/Safe.pm
@@ -2,7 +2,7 @@ package Safe;
 
 use 5.003_11;
 use strict;
-use Scalar::Util qw(reftype);
+use Scalar::Util qw(reftype refaddr);
 
 $Safe::VERSION = "2.27";
 
@@ -362,10 +362,12 @@ sub reval {
     return (wantarray) ? @subret : $subret[0];
 }
 
+my %OID;
 
 sub wrap_code_refs_within {
     my $obj = shift;
 
+    %OID = ();
     $obj->_find_code_refs('wrap_code_ref', @_);
 }
 
@@ -377,6 +379,10 @@ sub _find_code_refs {
     for my $item (@_) {
         my $reftype = $item && reftype $item
             or next;
+
+        # skip references already seen
+        next if ++$OID{refaddr $item} > 1;
+
         if ($reftype eq 'ARRAY') {
             $obj->_find_code_refs($visitor, @$item);
         }
