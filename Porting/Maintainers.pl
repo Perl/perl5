1e7868346284e83b3b2359a1ae43d11a29cb8f00
Florian Ragwitz <rafl@debian.org>
2010-09-15 04:17:03 +0200

    We now include Safe version 2.28

--- Porting/Maintainers.pl
+++ Porting/Maintainers.pl
@@ -1226,7 +1226,7 @@ use File::Glob qw(:case);
     'Safe' =>
 	{
 	'MAINTAINER'	=> 'rgarcia',
-	'DISTRIBUTION'	=> 'RGARCIA/Safe-2.27.tar.gz',
+	'DISTRIBUTION'	=> 'RGARCIA/Safe-2.28.tar.gz',
 	'FILES'		=> q[dist/Safe],
 	'UPSTREAM'	=> 'blead',
 	},
