daec24988e24c01ab264ee5b01da03f539b58741
Chris 'BinGOs' Williams <chris@bingosnet.co.uk>
2010-10-18 16:37:43 +0100

    Update Compress-Raw-Bzip2 to CPAN version 2.031
      [DELTA]
    
      2.031 21 September 2010
    
          * Updated to use bzip2 1.0.6

--- Porting/Maintainers.pl
+++ Porting/Maintainers.pl
@@ -316,7 +316,7 @@ use File::Glob qw(:case);
     'Compress::Raw::Bzip2' =>
 	{
 	'MAINTAINER'	=> 'pmqs',
-	'DISTRIBUTION'	=> 'PMQS/Compress-Raw-Bzip2-2.030.tar.gz',
+	'DISTRIBUTION'	=> 'PMQS/Compress-Raw-Bzip2-2.031.tar.gz',
 	'FILES'		=> q[cpan/Compress-Raw-Bzip2],
 	'EXCLUDED'	=> [ qr{^t/Test/},
 			     qw( bzip2-src/bzip2-cpp.patch
--- cpan/Compress-Raw-Bzip2/lib/Compress/Raw/Bzip2.pm
+++ cpan/Compress-Raw-Bzip2/lib/Compress/Raw/Bzip2.pm
@@ -12,7 +12,7 @@ use Carp ;
 use bytes ;
 our ($VERSION, $XS_VERSION, @ISA, @EXPORT, $AUTOLOAD);
 
-$VERSION = '2.030';
+$VERSION = '2.031';
 $XS_VERSION = $VERSION; 
 $VERSION = eval $VERSION;
 
