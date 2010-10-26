Perl/perl5

commit 304474c3f9eec6df709a20e0dc0f6e58bffecd76

Remove code added by c8bbf675 that turns out to be unnecessary

hv.c
@@ -692,32 +692,7 @@ Perl_hv_common(pTHX_ HV *hv, SV *keysv, const char *key, STRLEN klen,
 		}
 		HeVAL(entry) = val;
 	    } else if (action & HV_FETCH_ISSTORE) {
-		bool moving_package = FALSE;
-		SV *old_val = HeVAL(entry);
-
-		/* If this is a stash and the key ends with ::, then some-
-		   one is aliasing (or moving) a package. */
-		if (HvNAME(hv)) {
-		    if (keysv) key = SvPV(keysv, klen);
-		    if (klen > 1
-		     && key[klen-2] == ':' && key[klen-1] == ':') {
-			if(SvTYPE(old_val) == SVt_PVGV) {
-			    const HV * const old_stash
-			     = GvHV((GV *)old_val);
-			    if(old_stash && HvNAME(old_stash))
-				mro_package_moved(old_stash);
-			}
-			moving_package = TRUE;
-		    }
-		}
-
-		SvREFCNT_dec(old_val);
 		HeVAL(entry) = val;
-
-		if (moving_package && SvTYPE(val) == SVt_PVGV) {
-		    const HV * const stash = GvHV((GV *)val);
-		    if (stash && HvNAME(stash)) mro_package_moved(stash);
-		}
 	    }
 	} else if (HeVAL(entry) == &PL_sv_placeholder) {
 	    /* if we find a placeholder, we pretend we haven't found

commit a50d6ed0a23b68b97ff8aac166442b08a17d34fd

detect the failure introduced in 304474c3 and fixed in cefd5c7c

t/op/svleak.t
@@ -13,7 +13,7 @@ BEGIN {
 	or skip_all("XS::APItest not available");
 }
 
-plan tests => 17;
+plan tests => 18;
 
 # run some code N times. If the number of SVs at the end of loop N is
 # greater than (N-1)*delta at the end of loop 1, we've got a leak
@@ -112,4 +112,19 @@ leak_expr(5, 0, q{"YYYYYa" =~ /.+?(a(.+?)|b)/ }, "trie leak");
     @a = map { qr/1/ && ($count[$_] = sv_count()) && 99 }  0..3;
     is(@count[3] - @count[0], 3, "list   map block: one new tmp per iter");
 
+}
+
+SKIP:
+{ # broken by 304474c3, fixed by cefd5c7c, but didn't seem to cause
+  # any other test failures
+  # base test case from ribasushi (Peter Rabbitson)
+  eval { require Scalar::Util; Scalar::Util->import("weaken"); 1; }
+    or skip "no weaken", 1;
+  my $weak;
+  {
+    $weak = my $in = {};
+    weaken($weak);
+    my $out = { in => $in, in => undef }
+  }
+  ok(!$weak, "hash referenced weakened SV released");
 }