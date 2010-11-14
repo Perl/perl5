2bb1f15cf3c668a3c56c1bc9c3ce74475ad2d425
Karl Williamson <public@khwilliamson.com>
2010-11-13 20:51:08 -0700

    regcomp.h: Renumber ANYOF_EOS bit
    This is in preparation for adding a new bit which for debugging ease
    ought to be adjacent to another one.

--- regcomp.h
+++ regcomp.h
@@ -323,14 +323,14 @@ struct regnode_charclass_class {
 #define ANYOF_CLASS	 ANYOF_LOCALE
 #define ANYOF_LARGE      ANYOF_CLASS    /* Same; name retained for back compat */
 
-/* EOS used for regstclass only */
-#define ANYOF_EOS		0x10	/* Can match an empty string too */
-
 /* Set if the bitmap doesn't fully represent what this node can match */
 #define ANYOF_NONBITMAP		0x20
 #define ANYOF_UNICODE		ANYOF_NONBITMAP	/* old name, for back compat */
 
 #define ANYOF_UNICODE_ALL	0x40	/* Matches 0x100 - infinity */
+
+/* EOS used for regstclass only */
+#define ANYOF_EOS		0x80	/* Can match an empty string too */
 
 #define ANYOF_FLAGS_ALL		0xff
 
