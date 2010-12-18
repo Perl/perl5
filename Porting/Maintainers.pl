c3e6accbfa366773737da407c6d6f1574c1a057a
Chris 'BinGOs' Williams <chris@bingosnet.co.uk>
2010-12-18 22:36:50 +0000

    Update Archive-Tar to CPAN version 1.74
      [DELTA]
    
      * important changes in version 1.74 18/12/2010
      - Skip extracting pax extended headers, reported as
        RT #64038

--- Porting/Maintainers.pl
+++ Porting/Maintainers.pl
@@ -196,7 +196,7 @@ use File::Glob qw(:case);
     'Archive::Tar' =>
 	{
 	'MAINTAINER'	=> 'kane',
-	'DISTRIBUTION'	=> 'BINGOS/Archive-Tar-1.72.tar.gz',
+	'DISTRIBUTION'	=> 'BINGOS/Archive-Tar-1.74.tar.gz',
 	'FILES'		=> q[cpan/Archive-Tar],
 	'EXCLUDED'	=> [ qw(Makefile.PL) ],
 	'UPSTREAM'	=> 'cpan',
