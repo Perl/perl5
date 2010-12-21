Perl/perl5

commit 3b0fc154d4e77cfb1d426144cb362eb2fa6018f1

speed up is_utf8_char() Message-ID: <429F557E.3090007@gmail.com>
p4raw-id: //depot/perl@24687

utf8.h
@@ -257,3 +257,73 @@ encoded character.
 	 toLOWER((input)[1]) == 's')
 #endif
 #define SHARP_S_SKIP 2
+
+#define IS_UTF8_CHAR_1(p)	\
+	((p)[0] <= 0x7F)
+#define IS_UTF8_CHAR_2(p)	\
+	((p)[0] >= 0xC2 && (p)[0] <= 0xDF && \
+	 (p)[1] >= 0x80 && (p)[1] <= 0xBF)
+#define IS_UTF8_CHAR_3a(p)	\
+	((p)[0] == 0xE0 && \
+	 (p)[1] >= 0xA0 && (p)[1] <= 0xBF && \
+	 (p)[2] >= 0x80 && (p)[2] <= 0xBF)
+#define IS_UTF8_CHAR_3b(p)	\
+	((p)[0] >= 0xE1 && (p)[0] <= 0xEC && \
+	 (p)[1] >= 0x80 && (p)[1] <= 0xBF && \
+	 (p)[2] >= 0x80 && (p)[2] <= 0xBF)
+#define IS_UTF8_CHAR_3c(p)	\
+	((p)[0] == 0xED && \
+	 (p)[1] >= 0x80 && (p)[1] <= 0xBF && \
+	 (p)[2] >= 0x80 && (p)[2] <= 0xBF)
+/* In IS_UTF8_CHAR_3c(p) one could use
+ * (p)[1] >= 0x80 && (p)[1] <= 0x9F
+ * if one wanted to exclude surrogates. */
+#define IS_UTF8_CHAR_3d(p)	\
+	((p)[0] >= 0xEE && (p)[0] <= 0xEF && \
+	 (p)[1] >= 0x80 && (p)[1] <= 0xBF && \
+	 (p)[2] >= 0x80 && (p)[2] <= 0xBF)
+#define IS_UTF8_CHAR_4a(p)	\
+	((p)[0] == 0xF0 && \
+	 (p)[1] >= 0x90 && (p)[1] <= 0xBF && \
+	 (p)[2] >= 0x80 && (p)[2] <= 0xBF && \
+	 (p)[3] >= 0x80 && (p)[3] <= 0xBF)
+#define IS_UTF8_CHAR_4b(p)	\
+	((p)[0] >= 0xF1 && (p)[0] <= 0xF3 && \
+	 (p)[1] >= 0x80 && (p)[1] <= 0xBF && \
+	 (p)[2] >= 0x80 && (p)[2] <= 0xBF && \
+	 (p)[3] >= 0x80 && (p)[3] <= 0xBF)
+/* In IS_UTF8_CHAR_4c(p) one could use
+ * (p)[0] == 0xF4
+ * if one wanted to stop at the Unicode limit U+10FFFF.
+ * The 0xF7 allows us to go to 0x1fffff (0x200000 would
+ * require five bytes).  Not doing any further code points
+ * since that is not needed (and that would not be strict
+ * UTF-8, anyway).  The "slow path" in Perl_is_utf8_char()
+ * will take care of the "extended UTF-8". */
+#define IS_UTF8_CHAR_4c(p)	\
+	((p)[0] == 0xF4 && (p)[0] <= 0xF7 && \
+	 (p)[1] >= 0x80 && (p)[1] <= 0xBF && \
+	 (p)[2] >= 0x80 && (p)[2] <= 0xBF && \
+	 (p)[3] >= 0x80 && (p)[3] <= 0xBF)
+
+#define IS_UTF8_CHAR_3(p)	\
+	(IS_UTF8_CHAR_3a(p) || \
+	 IS_UTF8_CHAR_3b(p) || \
+	 IS_UTF8_CHAR_3c(p) || \
+	 IS_UTF8_CHAR_3d(p))
+#define IS_UTF8_CHAR_4(p)	\
+	(IS_UTF8_CHAR_4a(p) || \
+	 IS_UTF8_CHAR_4b(p) || \
+	 IS_UTF8_CHAR_4c(p))
+
+/* IS_UTF8_CHAR(p) is strictly speaking wrong (not UTF-8) because it
+ * (1) allows UTF-8 encoded UTF-16 surrogates
+ * (2) it allows code points past U+10FFFF.
+ * The Perl_is_utf8_char() full "slow" code will handle the Perl
+ * "extended UTF-8". */
+#define IS_UTF8_CHAR(p, n)	\
+	((n) == 1 ? IS_UTF8_CHAR_1(p) : \
+ 	 (n) == 2 ? IS_UTF8_CHAR_2(p) : \
+	 (n) == 3 ? IS_UTF8_CHAR_3(p) : \
+	 (n) == 4 ? IS_UTF8_CHAR_4(p) : 0)
+

utf8.c
@@ -186,40 +186,48 @@ will be returned if it is valid, otherwise 0.
 STRLEN
 Perl_is_utf8_char(pTHX_ const U8 *s)
 {
-    U8 u = *s;
-    STRLEN slen, len;
-    UV uv, ouv;
-
-    if (UTF8_IS_INVARIANT(u))
-	return 1;
-
-    if (!UTF8_IS_START(u))
-	return 0;
-
+    STRLEN len;
+#ifdef IS_UTF8_CHAR
     len = UTF8SKIP(s);
+    if (len <= 4)
+        return IS_UTF8_CHAR(s, len) ? len : 0;
+#endif /* #ifdef IS_UTF8_CHAR */
+    {
+	U8 u = *s;
+        STRLEN slen;
+        UV uv, ouv;
 
-    if (len < 2 || !UTF8_IS_CONTINUATION(s[1]))
-	return 0;
+        if (UTF8_IS_INVARIANT(u))
+            return 1;
 
-    slen = len - 1;
-    s++;
-    u &= UTF_START_MASK(len);
-    uv  = u;
-    ouv = uv;
-    while (slen--) {
-	if (!UTF8_IS_CONTINUATION(*s))
-	    return 0;
-	uv = UTF8_ACCUMULATE(uv, *s);
-	if (uv < ouv) 
-	    return 0;
-	ouv = uv;
-	s++;
+        if (!UTF8_IS_START(u))
+            return 0;
+
+        len = UTF8SKIP(s);
+
+        if (len < 2 || !UTF8_IS_CONTINUATION(s[1]))
+            return 0;
+
+        slen = len - 1;
+        s++;
+        u &= UTF_START_MASK(len);
+        uv  = u;
+        ouv = uv;
+        while (slen--) {
+            if (!UTF8_IS_CONTINUATION(*s))
+                return 0;
+            uv = UTF8_ACCUMULATE(uv, *s);
+            if (uv < ouv) 
+                return 0;
+            ouv = uv;
+            s++;
+        }
+
+        if ((STRLEN)UNISKIP(uv) < len)
+            return 0;
+
+        return len;
     }
-
-    if ((STRLEN)UNISKIP(uv) < len)
-	return 0;
-
-    return len;
 }
 
 /*

commit a18d6e6e4cf998a0ba9067ceac2d75f71aedef15

Fix IS_UTF8_CHAR() to recognise start bytes 0xF5, 0xF6, 0xF7.
The refactoring of 3b0fc154d4e77cfb inadvertently introduced a bug
in Perl_is_utf8_char() and its callers, such as Perl_is_utf8_string(),
whereby the beyond-Unicode characters 0x140000 to 0x1fffff were no longer
recognised as valid.

utf8.h
@@ -343,7 +343,7 @@ Perl's extended UTF-8 means we can have start bytes up to FF.
  * UTF-8, anyway).  The "slow path" in Perl_is_utf8_char()
  * will take care of the "extended UTF-8". */
 #define IS_UTF8_CHAR_4c(p)	\
-	((p)[0] == 0xF4 && (p)[0] <= 0xF7 && \
+	((p)[0] >= 0xF4 && (p)[0] <= 0xF7 && \
 	 (p)[1] >= 0x80 && (p)[1] <= 0xBF && \
 	 (p)[2] >= 0x80 && (p)[2] <= 0xBF && \
 	 (p)[3] >= 0x80 && (p)[3] <= 0xBF)

lib/utf8.t
@@ -39,8 +39,6 @@ no utf8; # Ironic, no?
 #
 #
 
-plan tests => 157;
-
 {
     # bug id 20001009.001
 
@@ -465,3 +463,25 @@ SKIP: {
    no strict 'subs';
    ok( !utf8::is_utf8( asd           ), "Wasteful format - bareword" );
 }
+
+{
+    my @highest =
+	(undef, 0x7F, 0x7FF, 0xFFFF, 0x1FFFFF, 0x3FFFFFF, 0x7FFFFFFF);
+    my @step =
+	(undef, undef, 0x40, 0x1000, 0x40000, 0x1000000, 0x40000000);
+
+    foreach my $length (6, 5, 4, 3, 2) {
+	my $high = $highest[$length];
+	while ($high > $highest[$length - 1]) {
+	    my $low = $high - $step[$length] + 1;
+	    $low = $highest[$length - 1] + 1 if $low <= $highest[$length - 1];
+	    ok(utf8::valid(do {no warnings 'utf8'; chr $low}),
+	       sprintf "chr %x, length $length is valid", $low);
+	    ok(utf8::valid(do {no warnings 'utf8'; chr $high}),
+	       sprintf "chr %x, length $length is valid", $high);
+	    $high -= $step[$length];
+	}
+    }
+}
+
+done_testing();