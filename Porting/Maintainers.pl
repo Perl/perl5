e2f1db54fc230d7e1ab6cd1298c19f127b9caa02
Chris 'BinGOs' Williams <chris@bingosnet.co.uk>
2011-01-11 19:23:52 +0000

    Update Compress-Raw-Zlib to CPAN version 2.033
      [DELTA]
    
      2.033 11 Jan 2011
          * Fixed typos & spelling errors.
            [perl# 81782]
    
      2.032 4 Jan 2011
    
          * Document inflateReset
            [RT #61082]

--- Porting/Maintainers.pl
+++ Porting/Maintainers.pl
@@ -336,7 +336,7 @@ use File::Glob qw(:case);
     'Compress::Raw::Zlib' =>
 	{
 	'MAINTAINER'	=> 'pmqs',
-	'DISTRIBUTION'	=> 'PMQS/Compress-Raw-Zlib-2.030.tar.gz',
+	'DISTRIBUTION'	=> 'PMQS/Compress-Raw-Zlib-2.033.tar.gz',
 
 	'FILES'		=> q[cpan/Compress-Raw-Zlib],
 	'EXCLUDED'	=> [ qr{^t/Test/},
