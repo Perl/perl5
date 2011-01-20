329170ddbd75297070c969d2fc75a74f29459e68
Jesse Vincent <jesse@bestpractical.com>
2011-01-20 14:29:39 -0500

    Bumping version of Pod::Perldoc per porting/cmpVERSION.pl

--- dist/Pod-Perldoc/lib/Pod/Perldoc.pm
+++ dist/Pod-Perldoc/lib/Pod/Perldoc.pm
@@ -12,7 +12,7 @@ use File::Spec::Functions qw(catfile catdir splitdir);
 use vars qw($VERSION @Pagers $Bindir $Pod2man
   $Temp_Files_Created $Temp_File_Lifetime
 );
-$VERSION = '3.15_02';
+$VERSION = '3.15_03';
 #..........................................................................
 
 BEGIN {  # Make a DEBUG constant very first thing...
