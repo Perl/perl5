19097ff56232337f4983ecd761ea97708ac0f5a2
Jesse Vincent <jesse@bestpractical.com>
2011-01-20 14:03:43 -0500

    Version bumps for re non-dual-life modules identified by
    ./perl -Ilib Porting/cmpVERSION.pl -xd . v5.13.8

--- ext/re/re.pm
+++ ext/re/re.pm
@@ -4,7 +4,7 @@ package re;
 use strict;
 use warnings;
 
-our $VERSION     = "0.15";
+our $VERSION     = "0.16";
 our @ISA         = qw(Exporter);
 our @EXPORT_OK   = ('regmust',
                     qw(is_regexp regexp_pattern
