6fb896962705a45ad6bcc30514cbc7e6cf163105
Jesse Vincent <jesse@bestpractical.com>
2011-01-20 14:05:27 -0500

    Version bump for UNIVERSAL.pm identified by
    ./perl -Ilib Porting/cmpVERSION.pl -xd . v5.13.8

--- lib/UNIVERSAL.pm
+++ lib/UNIVERSAL.pm
@@ -1,6 +1,6 @@
 package UNIVERSAL;
 
-our $VERSION = '1.07';
+our $VERSION = '1.08';
 
 # UNIVERSAL should not contain any extra subs/methods beyond those
 # that it exists to define. The use of Exporter below is a historical
