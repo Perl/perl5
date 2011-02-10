120f2c875ffb668661f9af38d6fe52ec435448a3
Steffen Mueller <smueller@cpan.org>
2011-02-10 16:51:53 +0100

    Bump ExtUtils::Typemaps version
    Just to make sure there really will never be a confusion or
    collision with the similarly named module that has been living
    on CPAN.

--- dist/ExtUtils-ParseXS/lib/ExtUtils/Typemaps.pm
+++ dist/ExtUtils-ParseXS/lib/ExtUtils/Typemaps.pm
@@ -2,7 +2,7 @@ package ExtUtils::Typemaps;
 use 5.006001;
 use strict;
 use warnings;
-our $VERSION = '0.05';
+our $VERSION = '1.00';
 use Carp qw(croak);
 
 our $Proto_Regexp = "[" . quotemeta('\$%&*@;[]') . "]";
