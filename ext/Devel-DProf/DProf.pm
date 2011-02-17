26dd057074f4b0c5511b05afb90d71292e1d5f93
Florian Ragwitz <rafl@debian.org>
2011-02-17 21:50:54 +0100

    Move dprofpp from utils/ to ext/Devel-DProf/bin
    This way the script can easily be part of the CPAN distribution, where
    ExtUtils::MakeMaker will do its shebang munging.
    
    In the core, we still have utils/dprofpp.PL, which also does shebang munging and
    takes care of creating `dprofpp.com' instead of just `dprofpp' on VMS, which
    ExtUtils::MakeMaker doesn't seem to be up to yet.
    
    To make this work, some things had to be moved from script-generation-time to
    run-time:
    
     * location of the stty binary
       This is probably better for binary perl distributions anyway
    
     * Devel::DProf version detection
       This is needed for `dprofpp -V'. The version number used to be copied into
       the script, but is now determined by actually loading Devel::DProf.
    
       In order for this to work it's necessary to be able to load DProf without
       causing profiling to start. For that, starting of the profiler has been moved
       to `import', causing
    
         require Devel::DProf;
    
       to not trigger it anymore, while
    
         use Devel::DProf;
    
       (and therefore perl -d:DProf) still does.

--- ext/Devel-DProf/DProf.pm
+++ ext/Devel-DProf/DProf.pm
@@ -213,26 +213,30 @@ sub NONESUCH_noxs {
 	return $Devel::DProf::VERSION;
 }
 
-package DB;
+{
+    package DB;
 
-#
-# As of perl5.003_20, &DB::sub stub is not needed (some versions
-# even had problems if stub was redefined with XS version).
-#
+    #
+    # As of perl5.003_20, &DB::sub stub is not needed (some versions
+    # even had problems if stub was redefined with XS version).
+    #
 
-# disable DB single-stepping
-BEGIN { $single = 0; }
+    # disable DB single-stepping
+    BEGIN { $single = 0; }
 
-# This sub is needed during startup.
-sub DB { 
-#	print "nonXS DBDB\n";
+    # This sub is needed during startup.
+    sub DB {
+	#	print "nonXS DBDB\n";
+    }
 }
 
 use XSLoader ();
 
-$Devel::DProf::VERSION = '20080331.00';  # this version not authorized by
+$Devel::DProf::VERSION = '20110217.00';  # this version not authorized by
 				         # Dean Roehrich. See "Changes" file.
 
-XSLoader::load 'Devel::DProf', $Devel::DProf::VERSION;
+sub import {
+    XSLoader::load 'Devel::DProf', $Devel::DProf::VERSION;
+}
 
 1;
