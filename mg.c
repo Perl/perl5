Perl/perl5

commit c22d665b55f24cd837419195a9ef11b09877c9cf

Unblock signal-mask on error for unsafe signals

mg.c
@@ -3106,15 +3106,27 @@ Perl_sighandler(int sig)
 
     POPSTACK;
     if (SvTRUE(ERRSV)) {
-#if !defined(PERL_MICRO) && !defined(HAS_SIGPROCMASK)
+#ifndef PERL_MICRO
 	/* Handler "died", for example to get out of a restart-able read().
 	 * Before we re-do that on its behalf re-enable the signal which was
 	 * blocked by the system when we entered.
 	 */
+#ifdef HAS_SIGPROCMASK
+#ifdef HAS_SIGACTION
+	if (sip)
+#endif
+	{
+	    sigset_t set;
+	    sigemptyset(&set);
+	    sigaddset(&set,sig);
+	    sigprocmask(SIG_UNBLOCK, &set, NULL);
+	}
+#else
 	/* Not clear if this will work */
 	(void)rsignal(sig, SIG_IGN);
 	(void)rsignal(sig, PL_csighandlerp);
-#endif /* !PERL_MICRO && !HAS_SIGPROCMASK*/
+#endif
+#endif /* !PERL_MICRO */
 	die_sv(ERRSV);
     }
 cleanup:

t/op/sigdispatch.t
@@ -9,7 +9,7 @@ BEGIN {
 use strict;
 use Config;
 
-plan tests => 12;
+plan tests => 13;
 
 watchdog(10);
 
@@ -70,4 +70,10 @@ SKIP: {
     POSIX::sigprocmask(&POSIX::SIG_UNBLOCK, $new, $old);
     ok $old->ismember(&POSIX::SIGUSR1), 'SIGUSR1 was still blocked';
     is $gotit, 2, 'Received fifth signal';
+
+    # test unsafe signal handlers in combination with exceptions
+    my $action = POSIX::SigAction->new(sub { $gotit--, die }, POSIX::SigSet->new, 0);
+    POSIX::sigaction(&POSIX::SIGUSR1, $action);
+    eval { kill SIGUSR1, $$ } for 1..2;
+    is $gotit, 0, 'Received both signals';
 }

commit d488af49a24e1d0cc601b18ecc8722114e5a97c8

No sip without SA_SIGINFO.  Broken in c22d665.

mg.c
@@ -3112,7 +3112,7 @@ Perl_sighandler(int sig)
 	 * blocked by the system when we entered.
 	 */
 #ifdef HAS_SIGPROCMASK
-#ifdef HAS_SIGACTION
+#if defined(HAS_SIGACTION) && defined(SA_SIGINFO)
 	if (sip)
 #endif
 	{