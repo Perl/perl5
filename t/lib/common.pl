ebf2da99b3c7f4f2c8cb1ba474ef47dd8a9deaff
Nicholas Clark <nick@ccl4.org>
2011-02-24 16:04:45 +0000

    Migrate the test running logic from t/lib/common.pl to t/test/pl
    This will make it available to several tests in t/op, which currently duplicate
    it.

--- t/lib/common.pl
+++ t/lib/common.pl
@@ -10,6 +10,7 @@ BEGIN {
 }
 
 use Config;
+use File::Path;
 use File::Spec::Functions qw(catfile curdir rel2abs);
 
 use strict;
@@ -62,9 +63,8 @@ my $cleanup = 1;
 
 END {
     if ($cleanup) {
-	require File::Path;
 	chdir '..' or die "Couldn't chdir .. for cleanup: $!";
-	File::Path::rmtree($tempdir);
+	rmtree($tempdir);
     }
 }
 
@@ -74,150 +74,6 @@ my $tests = $::local_tests || 0;
 $tests = scalar(@prgs)-$files + $tests if $tests !~ /\D/;
 plan $tests;    # If input is 'no_plan', pass it on unchanged
 
-my $tmpfile = tempfile();
-
-for (@prgs){
-    unless (/\n/)
-     {
-      print "# From $_\n";
-      next;
-     }
-    my $switch = "";
-    my @temps = () ;
-    my @temp_path = () ;
-    if (s/^\s*-\w+//){
-        $switch = $&;
-    }
-    my($prog,$expected) = split(/\nEXPECT(?:\n|$)/, $_, 2);
-
-    my %reason;
-    foreach my $what (qw(skip todo)) {
-	$prog =~ s/^#\s*\U$what\E\s*(.*)\n//m and $reason{$what} = $1;
-	# If the SKIP reason starts ? then it's taken as a code snippet to
-	# evaluate. This provides the flexibility to have conditional SKIPs
-	if ($reason{$what} && $reason{$what} =~ s/^\?//) {
-	    my $temp = eval $reason{$what};
-	    if ($@) {
-		die "# In \U$what\E code reason:\n# $reason{$what}\n$@";
-	    }
-	    $reason{$what} = $temp;
-	}
-    }
-
-    if ( $prog =~ /--FILE--/) {
-        my(@files) = split(/\n--FILE--\s*([^\s\n]*)\s*\n/, $prog) ;
-	shift @files ;
-	die "Internal error: test $_ didn't split into pairs, got " .
-		scalar(@files) . "[" . join("%%%%", @files) ."]\n"
-	    if @files % 2 ;
-	while (@files > 2) {
-	    my $filename = shift @files ;
-	    my $code = shift @files ;
-    	    push @temps, $filename ;
-    	    if ($filename =~ m#(.*)/# && $filename !~ m#^\.\./#) {
-		require File::Path;
-                File::Path::mkpath($1);
-                push(@temp_path, $1);
-	    }
-	    open my $fh, '>', $filename or die "Cannot open $filename: $!\n" ;
-	    print $fh $code;
-	    close $fh or die "Cannot close $filename: $!\n";
-	}
-	shift @files ;
-	$prog = shift @files ;
-    }
-
-    open my $fh, '>', $tmpfile or die "Cannot open >$tmpfile: $!";
-    print $fh q{
-        BEGIN {
-            open STDERR, '>&', STDOUT
-              or die "Can't dup STDOUT->STDERR: $!;";
-        }
-    };
-    print $fh "\n#line 1\n";  # So the line numbers don't get messed up.
-    print $fh $prog,"\n";
-    close $fh or die "Cannot close $tmpfile: $!";
-    my $results = runperl( switches => ["-I../../lib", $switch], nolib => 1,
-			   stderr => 1, progfile => $tmpfile );
-    my $status = $?;
-    $results =~ s/\n+$//;
-    # allow expected output to be written as if $prog is on STDIN
-    $results =~ s/$::tempfile_regexp/-/g;
-    if ($^O eq 'VMS') {
-        # some tests will trigger VMS messages that won't be expected
-        $results =~ s/\n?%[A-Z]+-[SIWEF]-[A-Z]+,.*//;
-
-        # pipes double these sometimes
-        $results =~ s/\n\n/\n/g;
-    }
-# bison says 'parse error' instead of 'syntax error',
-# various yaccs may or may not capitalize 'syntax'.
-    $results =~ s/^(syntax|parse) error/syntax error/mig;
-    # allow all tests to run when there are leaks
-    $results =~ s/Scalars leaked: \d+\n//g;
-
-    $expected =~ s/\n+$//;
-    my $prefix = ($results =~ s#^PREFIX(\n|$)##) ;
-    # any special options? (OPTIONS foo bar zap)
-    my $option_regex = 0;
-    my $option_random = 0;
-    if ($expected =~ s/^OPTIONS? (.+)\n//) {
-	foreach my $option (split(' ', $1)) {
-	    if ($option eq 'regex') { # allow regular expressions
-		$option_regex = 1;
-	    }
-	    elsif ($option eq 'random') { # all lines match, but in any order
-		$option_random = 1;
-	    }
-	    else {
-		die "$0: Unknown OPTION '$option'\n";
-	    }
-	}
-    }
-    die "$0: can't have OPTION regex and random\n"
-        if $option_regex + $option_random > 1;
-    my $ok = 0;
-    if ($results =~ s/^SKIPPED\n//) {
-	print "$results\n" ;
-	$ok = 1;
-    }
-    elsif ($option_random) {
-	my @got = sort split "\n", $results;
-	my @expected = sort split "\n", $expected;
-	
-	$ok = "@got" eq "@expected";
-    }
-    elsif ($option_regex) {
-	$ok = $results =~ /^$expected/;
-    }
-    elsif ($prefix) {
-	$ok = $results =~ /^\Q$expected/;
-    }
-    else {
-	$ok = $results eq $expected;
-    }
- 
-    local $::TODO = $reason{todo};
-
-    unless ($ok) {
-	my $err_line = "PROG: $switch\n$prog\n" .
-		       "EXPECTED:\n$expected\n" .
-		       "GOT:\n$results\n";
-	if ($::TODO) {
-	    $err_line =~ s/^/# /mg;
-	    print $err_line;  # Harness can't filter it out from STDERR.
-	}
-	else {
-	    print STDERR $err_line;
-	}
-    }
-
-    ok($ok);
-
-    foreach (@temps)
-	{ unlink $_ if $_ }
-    foreach (@temp_path)
-	{ File::Path::rmtree $_ if -d $_ }
-}
+run_multiple_progs(@prgs);
 
 1;
