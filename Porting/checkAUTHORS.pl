ed429a63c8cf378f6a991a808357b56d655dd1aa
Leon Brocard <acme@astray.com>
2011-03-06 16:57:11 +0000

    Add Danny Sadinoff's old email address to Porting/checkAUTHORS.pl

--- Porting/checkAUTHORS.pl
+++ Porting/checkAUTHORS.pl
@@ -784,6 +784,7 @@ rootbeer\100teleport.com                rootbeer\100redcat.com
 +                                       tomphoenix\100unknown
 rurban\100x-ray.at                      rurban\100cpan.org
 
+sadinoff\100olf.com                     danny-cpan\100sadinoff.com
 schubiger\100cpan.org                   steven\100accognoscere.org
 +                                       sts\100accognoscere.org
 +                                       schubiger\100gmail.com
