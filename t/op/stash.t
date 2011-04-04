Perl/perl5

commit 2d0d1eccfcfe45f2fac66080cd30117a608088f8

[perl #79208] %stash:: = () anonymises CVs
This keeps stash names visible during %foo:: = ().

This fixes @ISA assignment inside a DESTROY method triggered by
%foo:: = () and also lets existing CVs retain their pointers to
the stash.

So
  %foo:: = ()
is now equivalent to
  delete $foo::{$_} for keys %foo::

t/op/stash.t
@@ -7,7 +7,7 @@ BEGIN {
 
 BEGIN { require "./test.pl"; }
 
-plan( tests => 46 );
+plan( tests => 47 );
 
 # Used to segfault (bug #15479)
 fresh_perl_like(
@@ -96,7 +96,7 @@ SKIP: {
     isa_ok( $gv, "B::GV", "cleared stash leaves CV with valid GV");
     is( b($sub)->CvFLAGS & $CVf_ANON, $CVf_ANON, "...and CVf_ANON set");
     is( eval { $gv->NAME }, "__ANON__", "...and an __ANON__ name");
-    is( eval { $gv->STASH->NAME }, "__ANON__", "...and an __ANON__ stash");
+    is( eval { $gv->STASH->NAME }, "two", "...but leaves stash intact");
 
     $sub = do {
         package three;
@@ -250,4 +250,14 @@ SKIP: {
     }
 }
 
+# Setting the name during undef %stash:: should have no effect.
+{
+    my $glob = \*Phoo::glob;
+    sub o::DESTROY { eval '++$Phoo::bar' }
+    no strict 'refs';
+    ${"Phoo::thing1"} = bless [], "o";
+    undef %Phoo::;
+    is "$$glob", "*__ANON__::glob",
+      "setting stash name during undef has no effect";
+}
 

hv.c
@@ -1654,27 +1654,15 @@ S_hfreeentries(pTHX_ HV *hv)
 {
     /* This is the array that we're going to restore  */
     HE **const orig_array = HvARRAY(hv);
-    HEK *name;
-    I32 name_count;
+    HE **tmp_array = NULL;
+    const bool has_aux = SvOOK(hv);
+    struct xpvhv_aux * current_aux = NULL;
     int attempts = 100;
 
     PERL_ARGS_ASSERT_HFREEENTRIES;
 
     if (!orig_array)
 	return;
-
-    if (SvOOK(hv)) {
-	/* If the hash is actually a symbol table with a name, look after the
-	   name.  */
-	struct xpvhv_aux *iter = HvAUX(hv);
-
-	name = iter->xhv_name;
-	name_count = iter->xhv_name_count;
-	iter->xhv_name = NULL;
-    } else {
-	name = NULL;
-	name_count = 0;
-    }
 
     /* orig_array remains unchanged throughout the loop. If after freeing all
        the entries it turns out that one of the little blighters has triggered
@@ -1688,14 +1676,33 @@ S_hfreeentries(pTHX_ HV *hv)
 	HE ** const array = HvARRAY(hv);
 	I32 i = HvMAX(hv);
 
-	/* Because we have taken xhv_name out, the only allocated pointer
-	   in the aux structure that might exist is the backreference array.
-	*/
+	struct xpvhv_aux *iter = SvOOK(hv) ? HvAUX(hv) : NULL;
+
+	/* make everyone else think the array is empty, so that the destructors
+	 * called for freed entries can't recursively mess with us */
+	HvARRAY(hv) = NULL;
 
 	if (SvOOK(hv)) {
 	    HE *entry;
             struct mro_meta *meta;
-	    struct xpvhv_aux *iter = HvAUX(hv);
+
+	    SvFLAGS(hv) &= ~SVf_OOK; /* Goodbye, aux structure.  */
+	    /* What aux structure?  */
+	    /* (But we still have a pointer to it in iter.) */
+
+	    /* Copy the name to a new aux structure if there is a name. */
+	    if (iter->xhv_name) {
+		struct xpvhv_aux * const newaux = hv_auxinit(hv);
+		newaux->xhv_name = iter->xhv_name;
+		newaux->xhv_name_count = iter->xhv_name_count;
+		iter->xhv_name = NULL;
+	    }
+
+	    /* Because we have taken xhv_name out, the only allocated
+	       pointers in the aux structure that might exist are the back-
+	       reference array, xhv_eiter and the MRO stuff.
+	     */
+
 	    /* weak references: if called from sv_clear(), the backrefs
 	     * should already have been killed; if there are any left, its
 	     * because we're doing hv_clear() or hv_undef(), and the HV
@@ -1764,16 +1771,14 @@ S_hfreeentries(pTHX_ HV *hv)
             }
 
 	    /* There are now no allocated pointers in the aux structure.  */
-
-	    SvFLAGS(hv) &= ~SVf_OOK; /* Goodbye, aux structure.  */
-	    /* What aux structure?  */
 	}
 
-	/* make everyone else think the array is empty, so that the destructors
-	 * called for freed entries can't recursively mess with us */
-	HvARRAY(hv) = NULL;
-	((XPVHV*) SvANY(hv))->xhv_keys = 0;
+	/* If there are no keys, there is nothing left to free. */
+	if (!((XPVHV*) SvANY(hv))->xhv_keys) break;
 
+	/* Since we have removed the HvARRAY (and possibly replaced it by
+	   calling hv_auxinit), set the number of keys accordingly. */
+	((XPVHV*) SvANY(hv))->xhv_keys = 0;
 
 	do {
 	    /* Loop down the linked list heads  */
@@ -1798,42 +1803,32 @@ S_hfreeentries(pTHX_ HV *hv)
 	    break;
 	}
 
-	if (SvOOK(hv)) {
-	    /* Someone attempted to iterate or set the hash name while we had
-	       the array set to 0.  We'll catch backferences on the next time
-	       round the while loop.  */
-	    assert(HvARRAY(hv));
-
-	    if (HvAUX(hv)->xhv_name) {
-		if(HvAUX(hv)->xhv_name_count) {
-		    HEK ** const name = (HEK **)HvAUX(hv)->xhv_name;
-		    I32 const count = HvAUX(hv)->xhv_name_count;
-		    HEK **hekp = name + (count < 0 ? -count : count);
-		    while(hekp-- > name) 
-			unshare_hek_or_pvn(*hekp, 0, 0, 0);
-		    Safefree(name);
-		}
-		else unshare_hek_or_pvn(HvAUX(hv)->xhv_name, 0, 0, 0);
-	    }
-	}
-
 	if (--attempts == 0) {
 	    Perl_die(aTHX_ "panic: hfreeentries failed to free hash - something is repeatedly re-creating entries");
 	}
     }
 	
+    /* Set aside the current array for now, in case we still need it. */
+    if (SvOOK(hv)) current_aux = HvAUX(hv);
+    if (HvARRAY(hv) && HvARRAY(hv) != orig_array)
+	tmp_array = HvARRAY(hv);
+
     HvARRAY(hv) = orig_array;
 
-    /* If the hash was actually a symbol table, put the name back.  */
-    if (name) {
-	/* We have restored the original array.  If name is non-NULL, then
-	   the original array had an aux structure at the end. So this is
-	   valid:  */
-	struct xpvhv_aux * const aux = HvAUX(hv);
+    if (has_aux)
 	SvFLAGS(hv) |= SVf_OOK;
-	aux->xhv_name = name;
-	aux->xhv_name_count = name_count;
+    else
+	SvFLAGS(hv) &=~SVf_OOK;
+
+    /* If the hash was actually a symbol table, put the name back.  */
+    if (current_aux) {
+	struct xpvhv_aux * const aux
+	 = SvOOK(hv) ? HvAUX(hv) : hv_auxinit(hv);
+	aux->xhv_name = current_aux->xhv_name;
+	aux->xhv_name_count = current_aux->xhv_name_count;
     }
+
+    if (tmp_array) Safefree(tmp_array);
 }
 
 /*
@@ -1864,8 +1859,13 @@ Perl_hv_undef(pTHX_ HV *hv)
         mro_package_moved(NULL, hv, NULL, name, HvENAMELEN_get(hv));
     }
 
-    hfreeentries(hv);
     if (name || (name = HvNAME(hv))) {
+        if (PL_stashcache)
+	    (void)hv_delete(PL_stashcache, name, HvNAMELEN_get(hv), G_DISCARD);
+	hv_name_set(hv, NULL, 0, 0);
+    }
+    hfreeentries(hv);
+    if ((name = HvNAME(hv))) {
         if (PL_stashcache)
 	    (void)hv_delete(PL_stashcache, name, HvNAMELEN_get(hv), G_DISCARD);
 	hv_name_set(hv, NULL, 0, 0);

commit 57f45d7ba6658ede12e3850ae36f93319790c957

[perl #87664] Don’t autovivify stashes when anonymising CVs
This commit stops CV anonymisation from autovivifying stashes to point
to (unless the stash is %__ANON__::).

If a stash has been deleted from its original position in the symbol
table, then its HvNAME will no longer indicate where to find it.

S_anonymise_cv_maybe in sv.c was using the HvNAME to look up (and
autovivify) the *__ANON__ glob in the stash, without taking into
account that it might not actually be looking in the right spot.

So now, after checking that the stash still has a name (HvNAME), it
uses the HvENAME to find it. If the HvENAME is null, which indicates
that the stash has been detached altogether, then %__ANON__:: is used,
as happens when HvNAME is null.

This solves a Class::Monadic failure introduced by commit 2d0d1eccfc
([perl #79208] %stash:: = () anonymises CVs), which was included
in 5.13.7.

Basically, it can be reduced to this:

sv.c
@@ -5991,7 +5991,8 @@ S_anonymise_cv_maybe(pTHX_ GV *gv, CV* cv)
     }
 
     /* if not, anonymise: */
-    stash  = GvSTASH(gv) ? HvNAME(GvSTASH(gv)) : NULL;
+    stash  = GvSTASH(gv) && HvNAME(GvSTASH(gv))
+              ? HvENAME(GvSTASH(gv)) : NULL;
     gvname = Perl_newSVpvf(aTHX_ "%s::__ANON__",
 					stash ? stash : "__ANON__");
     anongv = gv_fetchsv(gvname, GV_ADDMULTI, SVt_PVCV);

t/op/stash.t
@@ -7,7 +7,7 @@ BEGIN {
 
 BEGIN { require "./test.pl"; }
 
-plan( tests => 51 );
+plan( tests => 53 );
 
 # Used to segfault (bug #15479)
 fresh_perl_like(
@@ -135,6 +135,19 @@ SKIP: {
     { local $TODO = 'STASHES not anonymized';
 	is($st, q/__ANON__/, "...and an __ANON__ stash");
     }
+
+    my $sub = do {
+	package six;
+	\&{"six"}
+    };
+    my $stash_glob = delete $::{"six::"};
+    # Now free the GV while the stash still exists (though detached)
+    delete $$stash_glob{"six"};
+    $gv = B::svref_2object($sub)->GV;
+    ok($gv->isa(q/B::GV/),
+       'anonymised CV whose stash is detached still has a GV');
+    is $gv->STASH->NAME, '__ANON__',
+     'CV anonymised when its stash is detached becomes __ANON__::__ANON__';
 
     # CvSTASH should be null on a named sub if the stash has been deleted
     {