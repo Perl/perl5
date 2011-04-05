7287d036f77a8e36b33684f04817b77934c30178
Nicholas Clark <nick@ccl4.org>
2011-04-05 09:52:04 +0100

    Bump ExtUtils::CBuilder version following a24b897525551a1d.

--- dist/ExtUtils-CBuilder/lib/ExtUtils/CBuilder/Platform/VMS.pm
+++ dist/ExtUtils-CBuilder/lib/ExtUtils/CBuilder/Platform/VMS.pm
@@ -4,7 +4,7 @@ use strict;
 use ExtUtils::CBuilder::Base;
 
 use vars qw($VERSION @ISA);
-$VERSION = '0.280202';
+$VERSION = '0.280203';
 @ISA = qw(ExtUtils::CBuilder::Base);
 
 use File::Spec::Functions qw(catfile catdir);
