e4561db65e35a795b75322bae6f48eec808474de
brian d foy <brian.d.foy@gmail.com>
2011-04-05 16:50:08 -0500

    Detabbed English.pm

--- lib/English.pm
+++ lib/English.pm
@@ -56,17 +56,17 @@ sub import {
     if ( @_ == @list ) {
         *EXPORT = \@COMPLETE_EXPORT ;
         $globbed_match ||= (
-	    eval q{
-		*MATCH				= *&	;
-		*PREMATCH			= *`	;
-		*POSTMATCH			= *'	;
-		1 ;
-	       }
-	    || do {
-		require Carp ;
-		Carp::croak("Can't create English for match leftovers: $@") ;
-	    }
-	) ;
+        eval q{
+        *MATCH              = *&    ;
+        *PREMATCH           = *`    ;
+        *POSTMATCH          = *'    ;
+        1 ;
+           }
+        || do {
+        require Carp ;
+        Carp::croak("Can't create English for match leftovers: $@") ;
+        }
+    ) ;
     }
     else {
         *EXPORT = \@MINIMAL_EXPORT ;
@@ -75,154 +75,154 @@ sub import {
 }
 
 @MINIMAL_EXPORT = qw(
-	*ARG
-	*LAST_PAREN_MATCH
-	*INPUT_LINE_NUMBER
-	*NR
-	*INPUT_RECORD_SEPARATOR
-	*RS
-	*OUTPUT_AUTOFLUSH
-	*OUTPUT_FIELD_SEPARATOR
-	*OFS
-	*OUTPUT_RECORD_SEPARATOR
-	*ORS
-	*LIST_SEPARATOR
-	*SUBSCRIPT_SEPARATOR
-	*SUBSEP
-	*FORMAT_PAGE_NUMBER
-	*FORMAT_LINES_PER_PAGE
-	*FORMAT_LINES_LEFT
-	*FORMAT_NAME
-	*FORMAT_TOP_NAME
-	*FORMAT_LINE_BREAK_CHARACTERS
-	*FORMAT_FORMFEED
-	*CHILD_ERROR
-	*OS_ERROR
-	*ERRNO
-	*EXTENDED_OS_ERROR
-	*EVAL_ERROR
-	*PROCESS_ID
-	*PID
-	*REAL_USER_ID
-	*UID
-	*EFFECTIVE_USER_ID
-	*EUID
-	*REAL_GROUP_ID
-	*GID
-	*EFFECTIVE_GROUP_ID
-	*EGID
-	*PROGRAM_NAME
-	*PERL_VERSION
-	*ACCUMULATOR
-	*COMPILING
-	*DEBUGGING
-	*SYSTEM_FD_MAX
-	*INPLACE_EDIT
-	*PERLDB
-	*BASETIME
-	*WARNING
-	*EXECUTABLE_NAME
-	*OSNAME
-	*LAST_REGEXP_CODE_RESULT
-	*EXCEPTIONS_BEING_CAUGHT
-	*LAST_SUBMATCH_RESULT
-	@LAST_MATCH_START
-	@LAST_MATCH_END
+    *ARG
+    *LAST_PAREN_MATCH
+    *INPUT_LINE_NUMBER
+    *NR
+    *INPUT_RECORD_SEPARATOR
+    *RS
+    *OUTPUT_AUTOFLUSH
+    *OUTPUT_FIELD_SEPARATOR
+    *OFS
+    *OUTPUT_RECORD_SEPARATOR
+    *ORS
+    *LIST_SEPARATOR
+    *SUBSCRIPT_SEPARATOR
+    *SUBSEP
+    *FORMAT_PAGE_NUMBER
+    *FORMAT_LINES_PER_PAGE
+    *FORMAT_LINES_LEFT
+    *FORMAT_NAME
+    *FORMAT_TOP_NAME
+    *FORMAT_LINE_BREAK_CHARACTERS
+    *FORMAT_FORMFEED
+    *CHILD_ERROR
+    *OS_ERROR
+    *ERRNO
+    *EXTENDED_OS_ERROR
+    *EVAL_ERROR
+    *PROCESS_ID
+    *PID
+    *REAL_USER_ID
+    *UID
+    *EFFECTIVE_USER_ID
+    *EUID
+    *REAL_GROUP_ID
+    *GID
+    *EFFECTIVE_GROUP_ID
+    *EGID
+    *PROGRAM_NAME
+    *PERL_VERSION
+    *ACCUMULATOR
+    *COMPILING
+    *DEBUGGING
+    *SYSTEM_FD_MAX
+    *INPLACE_EDIT
+    *PERLDB
+    *BASETIME
+    *WARNING
+    *EXECUTABLE_NAME
+    *OSNAME
+    *LAST_REGEXP_CODE_RESULT
+    *EXCEPTIONS_BEING_CAUGHT
+    *LAST_SUBMATCH_RESULT
+    @LAST_MATCH_START
+    @LAST_MATCH_END
 );
 
 
 @MATCH_EXPORT = qw(
-	*MATCH
-	*PREMATCH
-	*POSTMATCH
+    *MATCH
+    *PREMATCH
+    *POSTMATCH
 );
 
 @COMPLETE_EXPORT = ( @MINIMAL_EXPORT, @MATCH_EXPORT ) ;
 
 # The ground of all being. @ARG is deprecated (5.005 makes @_ lexical)
 
-	*ARG					= *_	;
+    *ARG                        = *_    ;
 
 # Matching.
 
-	*LAST_PAREN_MATCH			= *+	;
-	*LAST_SUBMATCH_RESULT			= *^N ;
-	*LAST_MATCH_START			= *-{ARRAY} ;
-	*LAST_MATCH_END				= *+{ARRAY} ;
+    *LAST_PAREN_MATCH           = *+    ;
+    *LAST_SUBMATCH_RESULT           = *^N ;
+    *LAST_MATCH_START           = *-{ARRAY} ;
+    *LAST_MATCH_END             = *+{ARRAY} ;
 
 # Input.
 
-	*INPUT_LINE_NUMBER			= *.	;
-	    *NR					= *.	;
-	*INPUT_RECORD_SEPARATOR			= */	;
-	    *RS					= */	;
+    *INPUT_LINE_NUMBER          = *.    ;
+        *NR                     = *.    ;
+    *INPUT_RECORD_SEPARATOR     = */    ;
+        *RS                     = */    ;
 
 # Output.
 
-	*OUTPUT_AUTOFLUSH			= *|	;
-	*OUTPUT_FIELD_SEPARATOR			= *,	;
-	    *OFS				= *,	;
-	*OUTPUT_RECORD_SEPARATOR		= *\	;
-	    *ORS				= *\	;
+    *OUTPUT_AUTOFLUSH           = *|    ;
+    *OUTPUT_FIELD_SEPARATOR     = *,    ;
+        *OFS                    = *,    ;
+    *OUTPUT_RECORD_SEPARATOR    = *\    ;
+        *ORS                    = *\    ;
 
 # Interpolation "constants".
 
-	*LIST_SEPARATOR				= *"	;
-	*SUBSCRIPT_SEPARATOR			= *;	;
-	    *SUBSEP				= *;	;
+    *LIST_SEPARATOR             = *"    ;
+    *SUBSCRIPT_SEPARATOR        = *;    ;
+        *SUBSEP                 = *;    ;
 
 # Formats
 
-	*FORMAT_PAGE_NUMBER			= *%	;
-	*FORMAT_LINES_PER_PAGE			= *=	;
-	*FORMAT_LINES_LEFT			= *-	;
-	*FORMAT_NAME				= *~	;
-	*FORMAT_TOP_NAME			= *^	;
-	*FORMAT_LINE_BREAK_CHARACTERS		= *:	;
-	*FORMAT_FORMFEED			= *^L	;
+    *FORMAT_PAGE_NUMBER         = *%    ;
+    *FORMAT_LINES_PER_PAGE      = *=    ;
+    *FORMAT_LINES_LEFT          = *-    ;
+    *FORMAT_NAME                = *~    ;
+    *FORMAT_TOP_NAME            = *^    ;
+    *FORMAT_LINE_BREAK_CHARACTERS       = *:    ;
+    *FORMAT_FORMFEED            = *^L   ;
 
 # Error status.
 
-	*CHILD_ERROR				= *?	;
-	*OS_ERROR				= *!	;
-	    *ERRNO				= *!	;
-	*EXTENDED_OS_ERROR			= *^E	;
-	*EVAL_ERROR				= *@	;
+    *CHILD_ERROR                = *?    ;
+    *OS_ERROR                   = *!    ;
+        *ERRNO                  = *!    ;
+    *EXTENDED_OS_ERROR          = *^E   ;
+    *EVAL_ERROR                 = *@    ;
 
 # Process info.
 
-	*PROCESS_ID				= *$	;
-	    *PID				= *$	;
-	*REAL_USER_ID				= *<	;
-	    *UID				= *<	;
-	*EFFECTIVE_USER_ID			= *>	;
-	    *EUID				= *>	;
-	*REAL_GROUP_ID				= *(	;
-	    *GID				= *(	;
-	*EFFECTIVE_GROUP_ID			= *)	;
-	    *EGID				= *)	;
-	*PROGRAM_NAME				= *0	;
+    *PROCESS_ID                 = *$    ;
+        *PID                    = *$    ;
+    *REAL_USER_ID               = *<    ;
+        *UID                    = *<    ;
+    *EFFECTIVE_USER_ID          = *>    ;
+        *EUID                   = *>    ;
+    *REAL_GROUP_ID              = *(    ;
+        *GID                    = *(    ;
+    *EFFECTIVE_GROUP_ID         = *)    ;
+        *EGID                   = *)    ;
+    *PROGRAM_NAME               = *0    ;
 
 # Internals.
 
-	*PERL_VERSION				= *^V	;
-	*ACCUMULATOR				= *^A	;
-	*COMPILING				= *^C	;
-	*DEBUGGING				= *^D	;
-	*SYSTEM_FD_MAX				= *^F	;
-	*INPLACE_EDIT				= *^I	;
-	*PERLDB					= *^P	;
-	*LAST_REGEXP_CODE_RESULT		= *^R	;
-	*EXCEPTIONS_BEING_CAUGHT		= *^S	;
-	*BASETIME				= *^T	;
-	*WARNING				= *^W	;
-	*EXECUTABLE_NAME			= *^X	;
-	*OSNAME					= *^O	;
+    *PERL_VERSION               = *^V   ;
+    *ACCUMULATOR                = *^A   ;
+    *COMPILING                  = *^C   ;
+    *DEBUGGING                  = *^D   ;
+    *SYSTEM_FD_MAX              = *^F   ;
+    *INPLACE_EDIT               = *^I   ;
+    *PERLDB                     = *^P   ;
+    *LAST_REGEXP_CODE_RESULT    = *^R   ;
+    *EXCEPTIONS_BEING_CAUGHT    = *^S   ;
+    *BASETIME                   = *^T   ;
+    *WARNING                    = *^W   ;
+    *EXECUTABLE_NAME            = *^X   ;
+    *OSNAME                     = *^O   ;
 
 # Deprecated.
 
-#	*ARRAY_BASE				= *[	;
-#	*OFMT					= *#	;
-#	*OLD_PERL_VERSION			= *]	;
+#   *ARRAY_BASE                 = *[    ;
+#   *OFMT                       = *#    ;
+#   *OLD_PERL_VERSION           = *]    ;
 
 1;
