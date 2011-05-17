e528b6072f9f5a668f9b01d9ab00c92a31d12e64
Chris 'BinGOs' Williams <chris@bingosnet.co.uk>
2011-05-17 22:40:34 +0100

    Update Object-Accessor to CPAN version 0.42
      [DELTA]
    
      Changes for 0.42    Fri May 13 12:21:50 BST  2011
      =================================================
      * Missed a test using exists, Ikegami RT #68154
    
      Changes for 0.40    Thu May 12 20:41:36 BST  2011
      =================================================
      * Apply patch from Ikegami RT #68154, which
        avoids use of deprecated exists on array elements.

--- Porting/Maintainers.pl
+++ Porting/Maintainers.pl
@@ -1352,7 +1352,7 @@ use File::Glob qw(:case);
     'Object::Accessor' =>
 	{
 	'MAINTAINER'	=> 'kane',
-	'DISTRIBUTION'	=> 'BINGOS/Object-Accessor-0.38.tar.gz',
+	'DISTRIBUTION'	=> 'BINGOS/Object-Accessor-0.42.tar.gz',
 	'FILES'		=> q[cpan/Object-Accessor],
 	'UPSTREAM'	=> 'cpan',
 	},
