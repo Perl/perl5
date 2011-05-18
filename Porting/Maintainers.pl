06fd9d7a579f00bca05b6ba920373565ea10c138
Chris 'BinGOs' Williams <chris@bingosnet.co.uk>
2011-05-19 00:31:23 +0100

    Updated Sys-Syslog to CPAN version 0.29
      [DELTA]
    
      0.29 -- 2011.04.18 -- Sebastien Aperghis-Tramoni (SAPER) #PerlQA2011
            [BUGFIX] CPAN-RT#55215: Sys::Syslog might call exit which triggers
            DESTROY (Alexander Berger).
            [BUGFIX] CPAN-RT#55151: Allow temporary facility with native mechanism
            (Tim Jenkins).
            [BUGFIX] CPAN-RT#50928: Convert Win32 files to Unix EOLs (Steve Hay).
            [BUGFIX] CPAN-RT#50534: Unwanted space at the end of syslog message
            (Eugene V. Lyubimkin).
            [BUGFIX] CPAN-RT#49877: Options not reset after closelog() (Herbert Brezina).
            [BUGFIX] CPAN-RT#48386: Add Win32::EventLog as a prerequisite (kmx and
            Laurent Dami).
            [FEATURE] CPAN-RT#50534: Added options noeol and nonul.
            [DIST] Set INSTALLDIRS to "site" when installed on Perl 5.11+
    
      0.28 -- 2011.04.16 -- Sebastien Aperghis-Tramoni (SAPER) #PerlQA2011
            [BUGFIX] CPAN-RT#56084: Reset connection tracking vars (Vladimir Marek).
            [BUGFIX] CPAN-RT#56826: Avoid memory corruption when closelog() is
            called twice (Andreas Jaekel).
            [BUGFIX] CPAN-RT#64287: Make strftime() Windows-compatible (Dave Stafford).
            [FEATURE] New API for setlogsock(), in order to allow the setting
            of new options. Thanks to Jake Scott for the idea.
            [CODE] Modify can_load() so it can print the warnings if desired.
            [CODE] CPAN-RT#44410: Provide fallback macros in Syslog.xs in
            order to compile on Novell Netware.
            [TESTS] In blead, the distribution was moved from ext/Sys/Syslog
            to ext/Sys-Syslog. t/constants.t had to be fixed (Vincent Pit).
            [TESTS] CPAN-RT#53317: In core, Sys-Syslog was moved to cpan/, thus
            making t/constants.t unable to find macros.all (David Mitchell).
            [TESTS] CPAN-RT#64716: Skip t/data-validation.t if the available
            version of POE::Component::Server::Syslog is too old.
            [DIST] Bleadperl no longer require an empty MAN3PODS (Nicholas Clark).
            [DIST] Removed unneeded modules in Makefile.PL (Nicholas Clark).
            [DOC] Recreated Sys::Syslog history from the unified git repository.
            [DOC] Added a section to list the matching Perl and Sys::Syslog version.
            [DOC] CPAN-RT#49859: Removed a dead link (Leon Brocard).

--- Porting/Maintainers.pl
+++ Porting/Maintainers.pl
@@ -1705,7 +1705,7 @@ use File::Glob qw(:case);
     'Sys::Syslog' =>
 	{
 	'MAINTAINER'	=> 'saper',
-	'DISTRIBUTION'	=> 'SAPER/Sys-Syslog-0.27.tar.gz',
+	'DISTRIBUTION'	=> 'SAPER/Sys-Syslog-0.29.tar.gz',
 	'FILES'		=> q[cpan/Sys-Syslog],
 	'EXCLUDED'	=> [ qr{^eg/},
 			     qw{t/data-validation.t
