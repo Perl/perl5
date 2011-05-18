882c7064de216e4687c6d4721e523ecd1f05c78a
Chris 'BinGOs' Williams <chris@bingosnet.co.uk>
2011-05-18 20:16:42 +0100

    Updated Compress-Raw-Zlib to CPAN version 2.035
      [DELTA]
    
      2.035 6 May 2011
    
          * No Changes

--- Porting/Maintainers.pl
+++ Porting/Maintainers.pl
@@ -381,7 +381,7 @@ use File::Glob qw(:case);
     'Compress::Raw::Zlib' =>
 	{
 	'MAINTAINER'	=> 'pmqs',
-	'DISTRIBUTION'	=> 'PMQS/Compress-Raw-Zlib-2.033.tar.gz',
+	'DISTRIBUTION'	=> 'PMQS/Compress-Raw-Zlib-2.035.tar.gz',
 
 	'FILES'		=> q[cpan/Compress-Raw-Zlib],
 	'EXCLUDED'	=> [ qr{^t/Test/},
--- cpan/Compress-Raw-Zlib/lib/Compress/Raw/Zlib.pm
+++ cpan/Compress-Raw-Zlib/lib/Compress/Raw/Zlib.pm
@@ -13,7 +13,7 @@ use warnings ;
 use bytes ;
 our ($VERSION, $XS_VERSION, @ISA, @EXPORT, $AUTOLOAD);
 
-$VERSION = '2.033';
+$VERSION = '2.035';
 $XS_VERSION = $VERSION; 
 $VERSION = eval $VERSION;
 
