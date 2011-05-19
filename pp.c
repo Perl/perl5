Perl/perl5

commit a4f4e9060b702ac8bd69a560b36e93c3c44a5c97

Avoid C<study>ing any strings that might change underneath us, such as tied scalars and scalars with overloaded stringification.
p4raw-id: //depot/perl@27273

pp.c
@@ -639,13 +639,22 @@ PP(pp_study)
 	if (SvSCREAM(sv))
 	    RETPUSHYES;
     }
-    else {
-	if (PL_lastscream) {
-	    SvSCREAM_off(PL_lastscream);
-	    SvREFCNT_dec(PL_lastscream);
-	}
-	PL_lastscream = SvREFCNT_inc(sv);
+    s = (unsigned char*)(SvPV(sv, len));
+    pos = len;
+    if (pos <= 0 || !SvPOK(sv)) {
+	/* No point in studying a zero length string, and not safe to study
+	   anything that doesn't appear to be a simple scalar (and hence might
+	   change between now and when the regexp engine runs without our set
+	   magic ever running, such as a reference to an object with overloaded
+	   stringification.  */
+	RETPUSHNO;
     }
+
+    if (PL_lastscream) {
+	SvSCREAM_off(PL_lastscream);
+	SvREFCNT_dec(PL_lastscream);
+    }
+    PL_lastscream = SvREFCNT_inc(sv);
 
     s = (unsigned char*)(SvPV(sv, len));
     pos = len;

t/op/studytied.t
@@ -41,16 +41,10 @@ for my $do_study qw( 0 1 ) {
     is( index( $x, 'f' ), -1,   qq{"next" doesn't contain "f"} );
 
     # Subsequent references to $x are "next", so should match /n/
-    TODO: {
-	local $TODO = $do_study ? 'not yet fixed' : 0;
-	ok( $x =~ /n/,              qq{"next" matches /n/} );
-    }
+    ok( $x =~ /n/,              qq{"next" matches /n/} );
     is( index( $x, 'n' ), 0,    qq{"next" contains "n" at pos 0} );
 
     # The letter "t" is in both, but in different positions
-    TODO: {
-	local $TODO = $do_study ? 'not yet fixed' : 0;
-	ok( $x =~ /t/,              qq{"next" matches /t/} );
-    }
-    is( index( $x, 't' ), 3,    qq{"next" contains "t" at pos 3} );
+    ok( $x =~ /t/,              qq{"next" matches /x/} );
+    is( index( $x, 't' ), 3,    qq{"next" contains "x" at pos 3} );
 }

commit 2913b40e5d98ac5a17648839dc32086276c19d8b

Remove duplicate code from pp_study(), caused by a4f4e9060b702ac8.
The duplicated code could have had no effect - for the case of pos <= 0
pp_study had already returned NO. The second call to SvPV() would have had no
side effects, because scalars with side effects don't have SvPOK() true, so
would also have already returned NO.

pp.c
@@ -728,10 +728,6 @@ PP(pp_study)
     }
     PL_lastscream = SvREFCNT_inc_simple(sv);
 
-    s = (unsigned char*)(SvPV(sv, len));
-    pos = len;
-    if (pos <= 0)
-	RETPUSHNO;
     if (pos > PL_maxscream) {
 	if (PL_maxscream < 0) {
 	    PL_maxscream = pos + 80;