daca484d698401314760ddb24f2669780bf9de12
Nicholas Clark <nick@ccl4.org>
2011-05-20 09:49:24 +0100

    Bump the versions of B and Storable following 1b95d04f713d9c56.
    1b95d04f713d9c56 changed HvKEYS() to HvUSEDKEYS() in the XS code.

--- dist/Storable/Storable.pm
+++ dist/Storable/Storable.pm
@@ -21,7 +21,7 @@ package Storable; @ISA = qw(Exporter);
 
 use vars qw($canonical $forgive_me $VERSION);
 
-$VERSION = '2.27';
+$VERSION = '2.28';
 
 BEGIN {
     if (eval { local $SIG{__DIE__}; require Log::Agent; 1 }) {
