040d2336f034a9225639af9abaec69a2e6da4301
Father Chrysostomos <sprout@cpan.org>
2011-06-30 19:19:14 -0700

    Add another address for Claes Jakobsson

--- Porting/checkAUTHORS.pl
+++ Porting/checkAUTHORS.pl
@@ -536,6 +536,7 @@ brian.d.foy\100gmail.com                bdfoy\100cpan.org
 BQW10602\100nifty.com                   sadahiro\100cpan.org
 
 chromatic\100wgz.org                    chromatic\100rmci.net
+claes\100surfar.nu                      claes\100versed.se
 clintp\100geeksalad.org                 cpierce1\100ford.com
 clkao\100clkao.org                      clkao\100bestpractical.com
 corion\100corion.net                    corion\100cpan.org
