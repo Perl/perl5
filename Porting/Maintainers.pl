65321c4afc7a61a5a2002db73c2b02781e61e892
Florian Ragwitz <rafl@debian.org>
2011-07-09 11:03:13 +0200

    Synchronise Dumpvalue to CPAN

--- Porting/Maintainers.pl
+++ Porting/Maintainers.pl
@@ -609,7 +609,7 @@ use File::Glob qw(:case);
     'Dumpvalue' =>
 	{
 	'MAINTAINER'	=> 'p5p',
-	'DISTRIBUTION'	=> 'FLORA/Dumpvalue-1.15.tar.gz',
+	'DISTRIBUTION'	=> 'FLORA/Dumpvalue-1.16.tar.gz',
 	'FILES'		=> q[dist/Dumpvalue],
 	'UPSTREAM'	=> 'blead',
 	},
