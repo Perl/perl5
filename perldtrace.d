344c1cf28158104009413cff3adfad9580fff885
Reini Urban <rurban@x-ray.at>
2011-07-24 20:07:37 +0200

    fix outdated url to dtrace blog post

--- perldtrace.d
+++ perldtrace.d
@@ -1,6 +1,6 @@
 /*
  * Written by Alan Burlinson -- taken from his blog post
- * at <http://blogs.sun.com/alanbur/date/20050909>.
+ * at <http://bleaklow.com/2005/09/09/dtrace_and_perl.html>.
  */
 
 provider perl {
