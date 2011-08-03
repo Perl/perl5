4eb81ef27984510df69d5b65e3f5cfa0bd218069
Chris 'BinGOs' Williams <chris@bingosnet.co.uk>
2011-08-03 09:28:11 +0100

    Update Module-Metadata to CPAN version 1.000005
      [DELTA]
    
      1.0.5 2011-08-02 09:45:00
    
      - Localize $package::VERSION during version discovery (MIYAGAWA)
      - Fix references to Module::Build::ModuleInfo [RT #66133] (DAGOLDEN)
      - Added 'new_from_handle()' method [RT #68875] (DAGOLDEN)
      - Improved documentation (SYNOPSIS, broke out class/object method, and
        other minor edits) (DAGOLDEN)

--- Porting/Maintainers.pl
+++ Porting/Maintainers.pl
@@ -1286,9 +1286,9 @@ use File::Glob qw(:case);
     'Module::Metadata' =>
 	{
 	'MAINTAINER'	=> 'dagolden',
-	'DISTRIBUTION'	=> 'DAGOLDEN/Module-Metadata-1.000004.tar.gz',
+	'DISTRIBUTION'	=> 'DAGOLDEN/Module-Metadata-1.000005.tar.gz',
 	'FILES'		=> q[cpan/Module-Metadata],
-	'EXCLUDED'	=> [ ],
+	'EXCLUDED'	=> [ qr{^maint}, qr{^xt} ],
 	'UPSTREAM'	=> 'cpan',
 	},
 
