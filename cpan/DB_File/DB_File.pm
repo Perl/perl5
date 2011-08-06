37adef70fb8ef1239bc8f880ba06268efdc67904
Chris 'BinGOs' Williams <chris@bingosnet.co.uk>
2011-08-06 22:38:29 +0100

    Update DB_File to CPAN version 1.824
      [DELTA]
    
      1.824 6 Aug 2011
    
       * Amendments to tests to work in blead
         [RT #70108]

--- cpan/DB_File/DB_File.pm
+++ cpan/DB_File/DB_File.pm
@@ -165,7 +165,7 @@ our ($db_version, $use_XSLoader, $splice_end_array_no_length, $splice_end_array,
 use Carp;
 
 
-$VERSION = "1.823" ;
+$VERSION = "1.824" ;
 $VERSION = eval $VERSION; # needed for dev releases
 
 {
