4df4e8c46d30f48994973aa13c4eb40ffb905021
Florian Ragwitz <rafl@debian.org>
2011-08-08 16:37:33 +0200

    Upgrade bignum to version 0.29

--- Porting/Maintainers.pl
+++ Porting/Maintainers.pl
@@ -334,7 +334,7 @@ use File::Glob qw(:case);
     'bignum' =>
 	{
 	'MAINTAINER'	=> 'rafl',
-	'DISTRIBUTION'	=> 'FLORA/bignum-0.28.tar.gz',
+	'DISTRIBUTION'	=> 'FLORA/bignum-0.29.tar.gz',
 	'FILES'		=> q[dist/bignum],
 	'EXCLUDED'	=> [ qr{^inc/Module/}, qw(t/pod.t t/pod_cov.t) ],
 	'UPSTREAM'	=> 'blead',
