Perl/perl5

commit 18c4b137c9980e718512bda9b795c3430bccd5f4

fix AIX and multiplicity problems
p4raw-id: //depot/cfgperl@5054

makedef.pl
@@ -100,6 +100,33 @@ while (<CFG>) {
 }
 close(CFG);
 
+# perl.h logic duplication begins
+
+if ($define{USE_ITHREADS}) {
+    if (!$define{MULTIPLICITY} && !defined{PERL_OBJECT}) {
+        $define{MULTIPLICITY} = 1;
+    }
+}
+
+$define{PERL_IMPLICIT_CONTEXT} ||=
+    $define{USE_ITHREADS} ||
+    $define{USE_THREADS}  ||
+    $define{MULTIPLICITY} ;
+
+if ($define{PERL_CAPI}) {
+    delete $define{PERL_OBJECT};
+    $define{MULTIPLICITY} = 1; 
+    $define{PERL_IMPLICIT_CONTEXT} = 1;
+    $define{PERL_IMPLICIT_SYS}     = 1;
+}
+
+if ($define{PERL_OBJECT}) {
+    $define{PERL_IMPLICIT_CONTEXT} = 1;
+    $define{PERL_IMPLICIT_SYS}     = 1;
+}
+
+# perl.h logic duplication ends
+
 if ($PLATFORM eq 'win32') {
     warn join(' ',keys %define)."\n";
     print "LIBRARY Perl56\n";
@@ -472,7 +499,13 @@ for my $syms (@syms) {
 # variables
 
 if ($define{'PERL_OBJECT'} || $define{'MULTIPLICITY'}) {
-    for my $f ($perlvars_h, $intrpvar_h, $thrdvar_h) {
+    for my $f ($perlvars_h) {
+	my $glob = readvar($f, sub { "Perl_" . $_[1] . $_[2] . "_ptr" });
+	emit_symbols $glob;
+	$glob = readvar($f);
+	emit_symbols $glob;
+    }
+    for my $f ($intrpvar_h, $thrdvar_h) {
 	my $glob = readvar($f, sub { "Perl_" . $_[1] . $_[2] . "_ptr" });
 	emit_symbols $glob;
     }

Makefile.SH
@@ -436,7 +436,7 @@ $(LIBPERL): $& perl$(OBJ_EXT) $(obj) $(LIBPERLEXPORT)
 !NO!SUBS!
 
 	case "${osname}${osvers}" in
-	next4*)
+	next4*|aix*)
 		$spitshell >>Makefile <<'!NO!SUBS!'
 miniperl: $& miniperlmain$(OBJ_EXT) $(LIBPERL) opmini$(OBJ_EXT)
 	$(CC) -o miniperl `echo $(obj) | sed 's/ op$(OBJ_EXT) / /'` \

commit f526348b03790a912fff80ad5e71b95decfac116

In makedef.pl, no longer any need to duplicate logic from perl.h
PL_OP_SLAB_ALLOC, MULTIPLICITY, PERL_IMPLICIT_CONTEXT and USE_REENTRANT_API
are all now correctly picked up from Config::bincompat_options().

This also removes a really subtle bug in the diagnostics, present since the
logic duplication was introduced in 18c4b137c9980e71 - this code:

$define{PERL_IMPLICIT_CONTEXT} ||=
    $define{USE_ITHREADS} ||
    $define{MULTIPLICITY} ;

will always add a key for PERL_IMPLICIT_CONTEXT in %define, hence the later
use of keys %define for output to STDERR previously always used to report
PERL_IMPLICIT_CONTEXT, even though the value was (usually) undef. The code
later in makedef.pl uses a truth test, so is not affected by this bug.

makedef.pl
@@ -114,28 +114,6 @@ while (<CFG>) {
 }
 close(CFG);
 
-# perl.h logic duplication begins
-
-if ($define{PERL_IMPLICIT_SYS}) {
-    $define{PL_OP_SLAB_ALLOC} = 1;
-}
-
-if ($define{USE_ITHREADS}) {
-    if (!$define{MULTIPLICITY}) {
-        $define{MULTIPLICITY} = 1;
-    }
-}
-
-$define{PERL_IMPLICIT_CONTEXT} ||=
-    $define{USE_ITHREADS} ||
-    $define{MULTIPLICITY} ;
-
-if ($define{USE_ITHREADS} && $ARGS{PLATFORM} ne 'win32' && $^O ne 'darwin') {
-    $define{USE_REENTRANT_API} = 1;
-}
-
-# perl.h logic duplication ends
-
 print STDERR "Defines: (" . join(' ', sort keys %define) . ")\n";
 
 my $sym_ord = 0;

perl.h
@@ -44,10 +44,6 @@
 /* See L<perlguts/"The Perl API"> for detailed notes on
  * PERL_IMPLICIT_CONTEXT and PERL_IMPLICIT_SYS */
 
-/* Note that from here --> to <-- the same logic is
- * repeated in makedef.pl, so be certain to update
- * both places when editing. */
-
 #ifdef PERL_IMPLICIT_SYS
 /* PERL_IMPLICIT_SYS implies PerlMemShared != PerlMem
    so use slab allocator to avoid lots of MUTEX overhead
@@ -113,8 +109,6 @@
 #if defined(USE_ITHREADS) && !defined(USE_REENTRANT_API) && !defined(NETWARE) && !defined(WIN32) && !defined(PERL_DARWIN)
 #   define USE_REENTRANT_API
 #endif
-
-/* <--- here ends the logic shared by perl.h and makedef.pl */
 
 /*
  * PERL_DARWIN for MacOSX (__APPLE__ exists but is not officially sanctioned)