0b19d81a098d6a9b895996d374ca0adc621f1590
Father Chrysostomos <sprout@cpan.org>
2011-08-21 17:27:58 -0700

    Add OPpCOREARGS_PUSHMARK flag
    This will be used to tell pp_coreargs when it needs to call
    pp_pushmark.
    
    For those functions that need a pushmark, it has to come between
    two things that pp_coreargs does; so the easiest way is to use
    this flag.

--- ext/B/B/Concise.pm
+++ ext/B/B/Concise.pm
@@ -661,7 +661,7 @@ if ($] >= 5.009) {
     for ("mapwhile", "mapstart", "grepwhile", "grepstart");
 }
 $priv{$_}{128} = '+1' for qw "caller wantarray";
-@{$priv{coreargs}}{1,2} = ('DREF1','DREF2');
+@{$priv{coreargs}}{1,2,128} = ('DREF1','DREF2','MARK');
 
 our %hints; # used to display each COP's op_hints values
 
