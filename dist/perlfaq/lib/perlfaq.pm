a57ee431fd743f8281a3ab1fd5671ed73c78c15b
Florian Ragwitz <rafl@debian.org>
2011-08-26 14:35:51 +0200

    Bump the perlfaq version for a new cpan release

--- dist/perlfaq/lib/perlfaq.pm
+++ dist/perlfaq/lib/perlfaq.pm
@@ -2,6 +2,6 @@ package # This is here merely for reasons related to our toolchain. It's not
         # supposed to be indexed.
     perlfaq;
 
-our $VERSION = '5.015000';
+our $VERSION = '5.015003';
 
 0; # not is it supposed to be loaded
