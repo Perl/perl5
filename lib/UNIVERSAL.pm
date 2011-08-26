e937c40d399bae93789feb66422b1547265ce0e0
Father Chrysostomos <sprout@cpan.org>
2011-08-26 10:00:13 -0700

    Update UNIVERSAL::VERSION docs following 9bf41c1

--- lib/UNIVERSAL.pm
+++ lib/UNIVERSAL.pm
@@ -1,6 +1,6 @@
 package UNIVERSAL;
 
-our $VERSION = '1.08';
+our $VERSION = '1.09';
 
 # UNIVERSAL should not contain any extra subs/methods beyond those
 # that it exists to define. The use of Exporter below is a historical
@@ -165,9 +165,8 @@ block or C<blessed> if you need to be extra paranoid.
 C<VERSION> will return the value of the variable C<$VERSION> in the
 package the object is blessed into. If C<REQUIRE> is given then
 it will do a comparison and die if the package version is not
-greater than or equal to C<REQUIRE>.  Both C<$VERSION> or C<REQUIRE>
-must be "lax" version numbers (as defined by the L<version> module)
-or C<VERSION> will die with an error.
+greater than or equal to C<REQUIRE>, or if either C<$VERSION> or C<REQUIRE>
+is not a "lax" version number (as defined by the L<version> module).
 
 C<VERSION> can be called as either a class (static) method or an object
 method.
