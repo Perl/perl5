6b51c551aece72c6ab4354f1cb334a36cb4b377b
Father Chrysostomos <sprout@cpan.org>
2011-09-01 13:52:05 -0700

    Bump Math::Big(In|Floa)t versions

--- dist/Math-BigInt/lib/Math/BigFloat.pm
+++ dist/Math-BigInt/lib/Math/BigFloat.pm
@@ -12,7 +12,7 @@ package Math::BigFloat;
 #   _a	: accuracy
 #   _p	: precision
 
-$VERSION = '1.995';
+$VERSION = '1.996';
 require 5.006002;
 
 require Exporter;
--- dist/Math-BigInt/lib/Math/BigInt.pm
+++ dist/Math-BigInt/lib/Math/BigInt.pm
@@ -18,7 +18,7 @@ package Math::BigInt;
 my $class = "Math::BigInt";
 use 5.006002;
 
-$VERSION = '1.996';
+$VERSION = '1.997';
 
 @ISA = qw(Exporter);
 @EXPORT_OK = qw(objectify bgcd blcm); 
