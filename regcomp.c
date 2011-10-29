11951bcbfcaf4c260b0da0421e72fc80b4654f17
Karl Williamson <public@khwilliamson.com>
2011-10-29 11:20:40 -0600

    PATCH: [perl #101940]: BBC Tk
    This commit that turned up this bug turns out merely exposes an
    underlying problem that could be generated via other means.
    
    regcomp.c was looking at the SvUTF8 flag on the input pattern before
    doing an SvPV on it.  Generally the flag is considered not reliable
    unless checked immediately after a SvPV.
    
    I haven't been able to come up with a simple test case that reproduces
    the bug.  I suspect that XS code is required to trigger it.

--- regcomp.c
+++ regcomp.c
@@ -4553,7 +4553,14 @@ Perl_re_compile(pTHX_ SV * const pattern, U32 orig_pm_flags)
 
     DEBUG_r(if (!PL_colorset) reginitcolors());
 
-    RExC_utf8 = RExC_orig_utf8 = SvUTF8(pattern);
+    exp = SvPV(pattern, plen);
+
+    if (plen == 0) { /* ignore the utf8ness if the pattern is 0 length */
+	RExC_utf8 = RExC_orig_utf8 = 0;
+    }
+    else {
+	RExC_utf8 = RExC_orig_utf8 = SvUTF8(pattern);
+    }
     RExC_uni_semantics = 0;
     RExC_contains_locale = 0;
 
@@ -4565,12 +4572,7 @@ Perl_re_compile(pTHX_ SV * const pattern, U32 orig_pm_flags)
     }
 
     if (jump_ret == 0) {    /* First time through */
-	exp = SvPV(pattern, plen);
 	xend = exp + plen;
-	/* ignore the utf8ness if the pattern is 0 length */
-	if (plen == 0) {
-	    RExC_utf8 = RExC_orig_utf8 = 0;
-	}
 
         DEBUG_COMPILE_r({
             SV *dsv= sv_newmortal();
