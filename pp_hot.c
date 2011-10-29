4e19c54bba2320807aca2f5aa19a4a0646e2a82c
Karl Williamson <public@khwilliamson.com>
2011-10-29 08:52:07 -0600

    Fix spelling in comment

--- pp_hot.c
+++ pp_hot.c
@@ -1964,7 +1964,7 @@ PP(pp_iter)
 /*
 A description of how taint works in pattern matching and substitution.
 
-While the pattern is being assembled/concatenated and them compiled,
+While the pattern is being assembled/concatenated and then compiled,
 PL_tainted will get set if any component of the pattern is tainted, e.g.
 /.*$tainted/.  At the end of pattern compilation, the RXf_TAINTED flag
 is set on the pattern if PL_tainted is set.
