324e9a83bcd8796d48e6577e4271e810e6096819
Steve Hay <steve.m.hay@googlemail.com>
2011-11-07 21:10:44 +0000

    Fix VC6 compilation of pp_sort.c
    Fixes copy & paste errors introduced by f3dab52a51.

--- pp_sort.c
+++ pp_sort.c
@@ -1881,7 +1881,7 @@ S_sv_ncmp(pTHX_ SV *const a, SV *const b)
     PERL_ARGS_ASSERT_SV_NCMP;
 
 #if defined(NAN_COMPARE_BROKEN) && defined(Perl_isnan)
-    if (Perl_isnan(right) || Perl_isnan(left)) {
+    if (Perl_isnan(nv1) || Perl_isnan(nv2)) {
 #else
     if (nv1 != nv1 || nv2 != nv2) {
 #endif
