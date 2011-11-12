b7576bcb309ba314f0b2e2ff3a1fc7243ef6abac
Karl Williamson <public@khwilliamson.com>
2011-11-12 08:45:40 -0700

    pp.c: Make sure variable is initialized
    A compiler generated a warning about this.  It is the degenerate case
    with an empty input, so isn't really a problem, but silence the warning

--- pp.c
+++ pp.c
@@ -3486,6 +3486,7 @@ PP(pp_ucfirst)
 
     if (! slen) {   /* If empty */
 	need = 1; /* still need a trailing NUL */
+	ulen = 0;
     }
     else if (DO_UTF8(source)) {	/* Is the source utf8? */
 	doing_utf8 = TRUE;
