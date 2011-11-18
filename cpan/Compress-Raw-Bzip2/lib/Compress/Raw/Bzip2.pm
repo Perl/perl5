a32349f5b8f0b889021135c1a0538678db52261b
Chris 'BinGOs' Williams <chris@bingosnet.co.uk>
2011-11-18 19:59:16 +0000

    Update Compress-Raw-Bzip2 to CPAN version 2.042
      [DELTA]
    
      2.042 17 November 2011
    
          * No Changes

--- cpan/Compress-Raw-Bzip2/lib/Compress/Raw/Bzip2.pm
+++ cpan/Compress-Raw-Bzip2/lib/Compress/Raw/Bzip2.pm
@@ -12,7 +12,7 @@ use Carp ;
 use bytes ;
 our ($VERSION, $XS_VERSION, @ISA, @EXPORT, $AUTOLOAD);
 
-$VERSION = '2.040';
+$VERSION = '2.042';
 $XS_VERSION = $VERSION; 
 $VERSION = eval $VERSION;
 
--- cpan/Compress-Raw-Bzip2/t/000prereq.t
+++ cpan/Compress-Raw-Bzip2/t/000prereq.t
@@ -19,7 +19,7 @@ BEGIN
         if eval { require Test::NoWarnings ;  import Test::NoWarnings; 1 };
 
 
-    my $VERSION = '2.040';
+    my $VERSION = '2.042';
     my @NAMES = qw(
 			
 			);
