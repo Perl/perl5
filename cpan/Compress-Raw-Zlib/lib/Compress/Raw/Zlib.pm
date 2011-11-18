54172f843dd1c7e46e2884f3ae1338dd49640448
Chris 'BinGOs' Williams <chris@bingosnet.co.uk>
2011-11-18 20:06:18 +0000

    Update Compress-Raw-Zlib to CPAN version 2.042
      [DELTA]
    
      2.042 17 November 2011
    
          * No Changes

--- cpan/Compress-Raw-Zlib/lib/Compress/Raw/Zlib.pm
+++ cpan/Compress-Raw-Zlib/lib/Compress/Raw/Zlib.pm
@@ -13,7 +13,7 @@ use warnings ;
 use bytes ;
 our ($VERSION, $XS_VERSION, @ISA, @EXPORT, $AUTOLOAD);
 
-$VERSION = '2.040';
+$VERSION = '2.042';
 $XS_VERSION = $VERSION; 
 $VERSION = eval $VERSION;
 
