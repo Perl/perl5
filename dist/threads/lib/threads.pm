47e6bc513676443472ed9b4e9d5a437163b0df6d
Father Chrysostomos <sprout@cpan.org>
2011-12-02 17:51:26 -0800

    Increase $threads::VERSION to 1.86

--- dist/threads/lib/threads.pm
+++ dist/threads/lib/threads.pm
@@ -5,7 +5,7 @@ use 5.008;
 use strict;
 use warnings;
 
-our $VERSION = '1.85';
+our $VERSION = '1.86';
 my $XS_VERSION = $VERSION;
 $VERSION = eval $VERSION;
 
@@ -134,7 +134,7 @@ threads - Perl interpreter-based threads
 
 =head1 VERSION
 
-This document describes threads version 1.85
+This document describes threads version 1.86
 
 =head1 SYNOPSIS
 
