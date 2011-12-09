9d3432a1d0f36442d541b38a7316790cbec2abf1
Father Chrysostomos <sprout@cpan.org>
2011-12-09 13:31:18 -0800

    Increase ExtUtils::Packlist::VERSION to 1.45

--- dist/ExtUtils-Install/lib/ExtUtils/Packlist.pm
+++ dist/ExtUtils-Install/lib/ExtUtils/Packlist.pm
@@ -5,7 +5,7 @@ use strict;
 use Carp qw();
 use Config;
 use vars qw($VERSION $Relocations);
-$VERSION = '1.44';
+$VERSION = '1.45';
 $VERSION = eval $VERSION;
 
 # Used for generating filehandle globs.  IO::File might not be available!
