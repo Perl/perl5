63a0dcf71fbcd47a260abe81c68ea3c2bdb493ba
Father Chrysostomos <sprout@cpan.org>
2011-12-16 23:27:49 -0800

    [rt.cpan.org #45885] File::Spec: Don’t use tainted tmpdir in 5.6
    Perl 5.6 doesn’t have ${^TAINT}, so the taint check in
    File::Spec::Unix->_tmpdir was skipped.  This commit adds a taint check
    using eval { eval $safe_substring } for 5.6.

--- dist/Cwd/lib/File/Spec/Unix.pm
+++ dist/Cwd/lib/File/Spec/Unix.pm
@@ -3,7 +3,7 @@ package File::Spec::Unix;
 use strict;
 use vars qw($VERSION);
 
-$VERSION = '3.34';
+$VERSION = '3.35';
 $VERSION = eval $VERSION;
 
 =head1 NAME
@@ -135,7 +135,7 @@ writable:
     $ENV{TMPDIR}
     /tmp
 
-Since perl 5.8.0, if running under taint mode, and if $ENV{TMPDIR}
+If running under taint mode, and if $ENV{TMPDIR}
 is tainted, it is not used.
 
 =cut
@@ -150,6 +150,9 @@ sub _tmpdir {
 	if (${"\cTAINT"}) { # Check for taint mode on perl >= 5.8.0
             require Scalar::Util;
 	    @dirlist = grep { ! Scalar::Util::tainted($_) } @dirlist;
+	}
+	elsif ($] < 5.007) { # No ${^TAINT} before 5.8
+	    @dirlist = grep { eval { eval('1'.substr $_,0,0) } } @dirlist;
 	}
     }
     foreach (@dirlist) {
