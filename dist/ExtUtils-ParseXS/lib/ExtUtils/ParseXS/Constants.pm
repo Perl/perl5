e4673b1bd6943b9247bc856c7dab0c0b9e948f5c
Steffen Mueller <smueller@cpan.org>
2012-01-20 08:05:30 +0100

    Bump ExtUtils::ParseXS version for doc references

--- dist/ExtUtils-ParseXS/lib/ExtUtils/ParseXS/Constants.pm
+++ dist/ExtUtils-ParseXS/lib/ExtUtils/ParseXS/Constants.pm
@@ -3,7 +3,7 @@ use strict;
 use warnings;
 use Symbol;
 
-our $VERSION = '3.11';
+our $VERSION = '3.12';
 
 =head1 NAME
 
--- dist/ExtUtils-ParseXS/lib/ExtUtils/ParseXS/CountLines.pm
+++ dist/ExtUtils-ParseXS/lib/ExtUtils/ParseXS/CountLines.pm
@@ -1,7 +1,7 @@
 package ExtUtils::ParseXS::CountLines;
 use strict;
 
-our $VERSION = '3.11';
+our $VERSION = '3.12';
 
 our $SECTION_END_MARKER;
 
