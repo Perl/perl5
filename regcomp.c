Perl/perl5

commit b6c46382308166d54090e3d8e385710664693ac0

regcomp.c: Compile inverted character classes with \p{}
This commit causes character classes of the form [^\p{...}] to have
their code points known at compile time instead of runtime.  This allows
for better optimization and runtime execution speed.

regcomp.c
@@ -10299,7 +10299,6 @@ parseit:
 
                     /* Look up the property name, and get its swash and
                      * inversion list, if the property is found  */
-                    if (! (ANYOF_FLAGS(ret) & ANYOF_INVERT)) {
                     if (swash) {
                         SvREFCNT_dec(swash);
                     }
@@ -10310,10 +10309,7 @@ parseit:
                                                       undefined properties */
                                              NULL, FALSE /* No inversion list */
                                             );
-                    }
-
-                    if (   ANYOF_FLAGS(ret) & ANYOF_INVERT
-                        || ! swash
+                    if (   ! swash
                         || ! SvROK(swash)
                         || ! SvTYPE(SvRV(swash)) == SVt_PVHV
                         || ! (invlistsvp =

commit 5d103fec94a383c3e6812c3cb735356fffe54e7c

perl #110648, single Unicode prop in inverted class not getting inverted
Commit b6c46382308166d54090e3d8e385710664693ac0 introduced a bug when
an inverted bracketed character class consists solely of a Unicode
property, that property wasn't getting inverted outside the Latin1
range.

This was due to an optimization that should have been disabled for this
case.

pod/perldelta.pod
@@ -717,6 +717,13 @@ C</[[:ascii:]]/> and C</[[:blank:]]/> now use locale rules under
 C<use locale> when the platform supports that.  Previously, they used
 the platform's native character set.
 
+=item *
+
+A regression introduced in 5.13.6 was fixed.  This involved an inverted
+bracketed character class in a regular expression that consisted solely
+of a Unicode property, that property wasn't getting inverted outside the
+Latin1 range.
+
 =back
 
 =head1 Known Problems

regcomp.c
@@ -11425,6 +11425,12 @@ parseit:
 	     * there should not be overlap unless is /d rules. */
 	    _invlist_invert(nonbitmap);
 
+	    /* Any swash can't be used as-is, because we've inverted things */
+	    if (swash) {
+		SvREFCNT_dec(swash);
+		swash = NULL;
+	    }
+
 	    for (i = 0; i < 256; ++i) {
 		if (ANYOF_BITMAP_TEST(ret, i)) {
 		    ANYOF_BITMAP_CLEAR(ret, i);

t/re/re_tests
@@ -1572,4 +1572,7 @@ abc\N{def	-	c	-	\\N{NAME} must be resolved by the lexer
 /[[:lower:]]/i	\N{U+3400}	n	-	-
 /[[:upper:]]/i	\N{U+01BB}	n	-	-
 
+# [perl #110648]
+[^\p{Alphabetic}]	\x{100}	n	-	-
+
 # vim: softtabstop=0 noexpandtab