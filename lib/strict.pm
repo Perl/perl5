1c389bff36c57eefe2a89bf883b80bbc7fd006a3
Father Chrysostomos <sprout@cpan.org>
2012-03-31 13:33:33 -0700

    Increase $strict::VERSION to 1.07

--- lib/strict.pm
+++ lib/strict.pm
@@ -1,6 +1,6 @@
 package strict;
 
-$strict::VERSION = "1.06";
+$strict::VERSION = "1.07";
 
 # Verify that we're called correctly so that strictures will work.
 unless ( __FILE__ =~ /(^|[\/\\])\Q${\__PACKAGE__}\E\.pmc?$/ ) {
