57ef7c84d0edb2d2e89bbab264bcdfaf37312a51
Father Chrysostomos <sprout@cpan.org>
2012-04-03 08:51:14 -0700

    Decrease $Safe::VERSION to 2.31_01
    There has been a release of 2.32 on CPAN with changes that are not in
    blead.  So what bleadperl has is 2.31 plus a tiny fix that does not
    affect older perl versions.

--- dist/Safe/Safe.pm
+++ dist/Safe/Safe.pm
@@ -3,7 +3,7 @@ package Safe;
 use 5.003_11;
 use Scalar::Util qw(reftype refaddr);
 
-$Safe::VERSION = "2.32";
+$Safe::VERSION = "2.31_01";
 
 # *** Don't declare any lexicals above this point ***
 #
