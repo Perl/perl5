0447e8df3db3f566f76a613f62c5f4cdd7262997
Karl Williamson <public@khwilliamson.com>
2012-04-18 15:25:28 -0600

    utf8.h: Use correct definition of start byte
    The previous definition allowed for (illegal) overlongs.  The uses of
    this macro in the core assume that it is accurate.  The inacurracy can
    cause such code to fail.

--- utf8.h
+++ utf8.h
@@ -139,9 +139,7 @@ Perl's extended UTF-8 means we can have start bytes up to FF.
 */
 
 #define UNI_IS_INVARIANT(c)		(((UV)c) <  0x80)
-/* Note that C0 and C1 are invalid in legal UTF8, so the lower bound of the
- * below might ought to be C2 */
-#define UTF8_IS_START(c)		(((U8)c) >= 0xc0)
+#define UTF8_IS_START(c)		(((U8)c) >= 0xc2)
 #define UTF8_IS_CONTINUATION(c)		(((U8)c) >= 0x80 && (((U8)c) <= 0xbf))
 #define UTF8_IS_CONTINUED(c) 		(((U8)c) &  0x80)
 
