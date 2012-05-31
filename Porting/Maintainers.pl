96e633ea56c1df47b3409393a1942f435cf9f6ad
Chris 'BinGOs' Williams <chris@bingosnet.co.uk>
2012-05-31 11:45:07 +0100

    Update Params-Check to CPAN version 0.36
      [DELTA]
    
      Changes for 0.36    Fri Apr 27 22:57:02 2012
      ============================================
      * More speed enhancements by Vincent Pit
    
      Changes for 0.34    Wed Apr 25 13:51:31 2012
      ============================================
      * check() now works fasteh thanks to
        Vincent Pit

--- Porting/Maintainers.pl
+++ Porting/Maintainers.pl
@@ -1350,7 +1350,7 @@ use File::Glob qw(:case);
 
     'Params::Check' => {
         'MAINTAINER'   => 'kane',
-        'DISTRIBUTION' => 'BINGOS/Params-Check-0.32.tar.gz',
+        'DISTRIBUTION' => 'BINGOS/Params-Check-0.36.tar.gz',
         'EXCLUDED'     => ['Params-Check-0.26.tar.gz'],
         'FILES'        => q[cpan/Params-Check],
         'UPSTREAM'     => 'cpan',
