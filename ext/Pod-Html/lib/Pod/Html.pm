995657526623d5b0b356d4ef56684807399b1c88
Nicholas Clark <nick@ccl4.org>
2012-06-04 21:27:58 +0200

    Bump $Pod::Html::VERSION to reflect the changes of commit 978af2c644ee9abf.

--- ext/Pod-Html/lib/Pod/Html.pm
+++ ext/Pod-Html/lib/Pod/Html.pm
@@ -3,7 +3,7 @@ use strict;
 require Exporter;
 
 use vars qw($VERSION @ISA @EXPORT @EXPORT_OK);
-$VERSION = 1.15_02;
+$VERSION = 1.16;
 @ISA = qw(Exporter);
 @EXPORT = qw(pod2html htmlify);
 @EXPORT_OK = qw(anchorify);
