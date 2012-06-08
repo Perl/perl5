c70a6f041b1579be443b8d34373e7fc4a4cbb6aa
Father Chrysostomos <sprout@cpan.org>
2012-06-08 14:50:35 -0700

    Increase $B::Deparse::VERSION to 1.15

--- dist/B-Deparse/Deparse.pm
+++ dist/B-Deparse/Deparse.pm
@@ -20,7 +20,7 @@ use B qw(class main_root main_start main_cv svref_2object opnumber perlstring
          CVf_METHOD CVf_LVALUE
 	 PMf_KEEP PMf_GLOBAL PMf_CONTINUE PMf_EVAL PMf_ONCE
 	 PMf_MULTILINE PMf_SINGLELINE PMf_FOLD PMf_EXTENDED);
-$VERSION = '1.14';
+$VERSION = '1.15';
 use strict;
 use vars qw/$AUTOLOAD/;
 use warnings ();
