e128ab2c0803fa45e7f26de2941696d173a9a293
David Mitchell <davem@iabyn.com>
2012-06-13 17:29:27 +0100

    update docs for (?{}) jumbo fix
    Update the docs and add perldelta entries summarising the changes and
    fixes related to (?{}) and (??{}) accumulated over the 120 or so commits
    in this branch.

--- ext/re/re.pm
+++ ext/re/re.pm
@@ -284,8 +284,9 @@ other transformations.
 
 When C<use re 'eval'> is in effect, a regexp is allowed to contain
 C<(?{ ... })> zero-width assertions and C<(??{ ... })> postponed
-subexpressions, even if the regular expression contains
-variable interpolation.  That is normally disallowed, since it is a
+subexpressions that are derived from variable interpolation, rather than
+appearing literally within the regexp.  That is normally disallowed, since
+it is a
 potential security risk.  Note that this pragma is ignored when the regular
 expression is obtained from tainted data, i.e.  evaluation is always
 disallowed with tainted regular expressions.  See L<perlre/(?{ code })> 
