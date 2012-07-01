8767b1ab5e2415da830be5abebbce9af2142fb86
Father Chrysostomos <sprout@cpan.org>
2012-06-30 17:31:32 -0700

    Don’t allow name after our/state sub
    It was a mistake that this was ever allowed.

--- toke.c
+++ toke.c
@@ -8214,6 +8214,7 @@ Perl_yylex(pTHX)
 		d = s;
 		s = SKIPSPACE2(s,tmpwhite);
 #else
+		d = s;
 		s = skipspace(s);
 #endif
 
@@ -8258,8 +8259,13 @@ Perl_yylex(pTHX)
 #endif
 		}
 		else {
-		    if (key == KEY_my)
-			Perl_croak(aTHX_ "Missing name in \"my sub\"");
+		    if (key == KEY_my || key == KEY_our || key==KEY_state)
+		    {
+			*d = '\0';
+			/* diag_listed_as: Missing name in "%s sub" */
+			Perl_croak(aTHX_
+				  "Missing name in \"%s\"", PL_bufptr);
+		    }
 		    PL_expect = XTERMBLOCK;
 		    attrful = XATTRTERM;
 		    sv_setpvs(PL_subname,"?");
