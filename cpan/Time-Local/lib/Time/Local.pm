fb4cbec857f8b483fa17554ee4c770c2bbc0466f
Dave Rolsky <autarch@urth.org>
2012-07-09 12:06:39 -0500

    Sync with Time::Local 1.2300 from cpan

--- cpan/Time-Local/lib/Time/Local.pm
+++ cpan/Time-Local/lib/Time/Local.pm
@@ -6,7 +6,7 @@ use Config;
 use strict;
 
 use vars qw( $VERSION @ISA @EXPORT @EXPORT_OK );
-$VERSION   = '1.2200';
+$VERSION   = '1.2300';
 
 @ISA       = qw( Exporter );
 @EXPORT    = qw( timegm timelocal );
