6b1793ddd4d5900c92472c56efaa98b20dd4f823
Nicholas Clark <nick@ccl4.org>
2012-07-23 13:39:05 +0200

    Bump B's VERSION and note the changes in perldelta.pod.

--- ext/B/B.pm
+++ ext/B/B.pm
@@ -15,7 +15,7 @@ require Exporter;
 # walkoptree comes from B.xs
 
 BEGIN {
-    $B::VERSION = '1.36';
+    $B::VERSION = '1.37';
     @B::EXPORT_OK = ();
 
     # Our BOOT code needs $VERSION set, and will append to @EXPORT_OK.
