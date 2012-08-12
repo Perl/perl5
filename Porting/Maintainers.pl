0a9c57c8f16d855c5f36768152cd5f37c0e05472
Chris 'BinGOs' Williams <chris@bingosnet.co.uk>
2012-08-12 09:09:29 +0100

    Sync Module-CoreList version with CPAN

--- Porting/Maintainers.pl
+++ Porting/Maintainers.pl
@@ -1258,7 +1258,7 @@ use File::Glob qw(:case);
 
     'Module::CoreList' => {
         'MAINTAINER'   => 'bingos',
-        'DISTRIBUTION' => 'BINGOS/Module-CoreList-2.69.tar.gz',
+        'DISTRIBUTION' => 'BINGOS/Module-CoreList-2.70.tar.gz',
         'FILES'        => q[dist/Module-CoreList],
         'UPSTREAM'     => 'blead',
     },
