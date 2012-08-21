e07d7e14bdcbce2e3fdcfb70ac0e637a7e39e2dd
Father Chrysostomos <sprout@cpan.org>
2012-08-21 08:25:13 -0700

    Increase $Module::CoreList::TieHashDelta::VERSION to 2.72

--- dist/Module-CoreList/lib/Module/CoreList/TieHashDelta.pm
+++ dist/Module-CoreList/lib/Module/CoreList/TieHashDelta.pm
@@ -3,7 +3,7 @@ package Module::CoreList::TieHashDelta;
 use strict;
 use vars qw($VERSION);
 
-$VERSION = "2.71";
+$VERSION = "2.72";
 
 sub TIEHASH {
     my ($class, $changed, $removed, $parent) = @_;
