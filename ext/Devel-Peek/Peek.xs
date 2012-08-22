Perl/perl5

commit c9795579db61c900bacee2790bdceb7bad3dd45d

Devel::Peek: Fix so compiles under C++
Commit 86b9d29366aea0e71ad75b61d04f56f1fe5b0d4d created a new PADLIST
type.  However, this broke the compilation of Devel::Peek with C++.
This commit gets it to compile again, and pass our regression test
suite.

[Modified by the committer to use the correct PADLIST_ macros; other-
 wise it will crash.]

ext/Devel-Peek/Peek.xs
@@ -31,7 +31,8 @@ DeadCode(pTHX)
 	for (sv = sva + 1; sv < svend; ++sv) {
 	    if (SvTYPE(sv) == SVt_PVCV) {
 		CV *cv = (CV*)sv;
-		AV* padlist = CvPADLIST(cv), *argav;
+		PADLIST* padlist = CvPADLIST(cv);
+                AV *argav;
 		SV** svp;
 		SV** pad;
 		int i = 0, j, levelm, totm = 0, levelref, totref = 0;
@@ -53,10 +54,11 @@ DeadCode(pTHX)
 		    PerlIO_printf(Perl_debug_log, "  busy\n");
 		    continue;
 		}
-		svp = AvARRAY(padlist);
-		while (++i <= AvFILL(padlist)) { /* Depth. */
+		svp = PADLIST_ARRAY(padlist);
+		while (++i <= PADLIST_MAX(padlist)) { /* Depth. */
 		    SV **args;
 		    
+		    if (!svp[i]) continue;
 		    pad = AvARRAY((AV*)svp[i]);
 		    argav = (AV*)pad[0];
 		    if (!argav || (SV*)argav == &PL_sv_undef) {
@@ -108,7 +110,7 @@ DeadCode(pTHX)
 		    if (dumpit)
 			do_sv_dump(0, Perl_debug_log, (SV*)cv, 0, 2, 0, 0);
 		}
-		if (AvFILL(padlist) > 1) {
+		if (PADLIST_MAX(padlist) > 1) {
 		    PerlIO_printf(Perl_debug_log, "  total: refs: %i, strings: %i in %i,\targsarrays: %i, argsstrings: %i\n", 
 			    totref, totm, tots, tota, totas);
 		}

ext/Devel-Peek/Peek.pm
@@ -3,7 +3,7 @@
 
 package Devel::Peek;
 
-$VERSION = '1.09';
+$VERSION = '1.10';
 $XS_VERSION = $VERSION;
 $VERSION = eval $VERSION;
 

commit 9dbc96cb5bce40f7c50cd0c8b770a804cca113ed

PATCH: Devel::Peek doesn't compile under C++
Commit c9795579db61c900bacee2790bdceb7bad3dd45d introduced
an error in C++: it's missing a cast.

ext/Devel-Peek/Peek.xs
@@ -54,7 +54,7 @@ DeadCode(pTHX)
 		    PerlIO_printf(Perl_debug_log, "  busy\n");
 		    continue;
 		}
-		svp = PADLIST_ARRAY(padlist);
+		svp = (SV**) PADLIST_ARRAY(padlist);
 		while (++i <= PADLIST_MAX(padlist)) { /* Depth. */
 		    SV **args;
 		    