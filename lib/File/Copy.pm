Perl/perl5

commit 43ddfa56142b05eb634e67f9eb458184251a389e

rt #111126 - don't empty a file with copy("foo/bar", "foo/");

lib/File/Copy.pm
@@ -22,7 +22,7 @@ sub syscopy;
 sub cp;
 sub mv;
 
-$VERSION = '2.23';
+$VERSION = '2.24';
 
 require Exporter;
 @ISA = qw(Exporter);
@@ -133,6 +133,10 @@ sub copy {
         return 1;
     }
 
+    if (!$from_a_handle && !$to_a_handle && -d $to && ! -d $from) {
+	$to = _catname($from, $to);
+    }
+
     if ((($Config{d_symlink} && $Config{d_readlink}) || $Config{d_link}) &&
 	!($^O eq 'MSWin32' || $^O eq 'os2')) {
 	my @fs = stat($from);
@@ -143,10 +147,6 @@ sub copy {
                 return 0;
 	    }
 	}
-    }
-
-    if (!$from_a_handle && !$to_a_handle && -d $to && ! -d $from) {
-	$to = _catname($from, $to);
     }
 
     if (defined &syscopy && !$Syscopy_is_copy

lib/File/Copy.t
@@ -476,7 +476,6 @@ use File::Temp qw(tempdir);
 use File::Spec;
 
 SKIP: {
-    local $TODO = "copy foo/file to foo/ overwrites, RT #111126";
     # RT #111126: File::Copy copy() zeros file when copying a file
     # into the same directory it is stored in
 

commit a0084943302ae84253561a08e28aecf602611ac4

Fix File::Copy test failure on Windows
Failure was introduced by 43ddfa5614 which looks for a warning message from
code that isn't run on Windows.

lib/File/Copy.pm
@@ -148,6 +148,10 @@ sub copy {
 	    }
 	}
     }
+    elsif (_eq($from, $to)) {
+	carp("'$from' and '$to' are identical (not copied)");
+	return 0;
+    }
 
     if (defined &syscopy && !$Syscopy_is_copy
 	&& !$to_a_handle