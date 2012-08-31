Perl/perl5

commit 6b00f562eddf90e215cb117d990dd4595e072f29

Break s//3}->{3/e
This should never have worked:

%_=(_,"Just another ");
$_="Perl hacker,\n";
s//_}->{_/e;print

toke.c
@@ -5932,7 +5932,10 @@ Perl_yylex(pTHX)
 #endif
 		    return yylex();	/* ignore fake brackets */
 		}
-		if (*s == '-' && s[1] == '>')
+		if (PL_lex_inwhat == OP_SUBST && PL_lex_repl
+		 && SvEVALED(PL_lex_repl))
+		    PL_lex_state = LEX_INTERPEND;
+		else if (*s == '-' && s[1] == '>')
 		    PL_lex_state = LEX_INTERPENDMAYBE;
 		else if (*s != '[' && *s != '{')
 		    PL_lex_state = LEX_INTERPEND;

t/base/lex.t
@@ -1,6 +1,6 @@
 #!./perl
 
-print "1..71\n";
+print "1..72\n";
 
 $x = 'x';
 
@@ -354,3 +354,7 @@ s//"#" . <<END/e;
 foo
 END
 print "ok 71 - s//'#' . <<END/e\n";
+
+eval "s//3}->{3/e";
+print "not " unless $@;
+print "ok 72 - s//3}->{3/e\n";

commit f777953f1a27fe4a456b4dc3acb1ea6332e12ced

Commit 6b00f562ed broke s/${\%x}{3}//e
It was meant to check whether it was inside the replacement part of
s///e, but it only checked that it was inside s///e.  PL_lex_repl is
set on both sides, but is only equal to PL_linestr on the rhs.

toke.c
@@ -5932,7 +5932,7 @@ Perl_yylex(pTHX)
 #endif
 		    return yylex();	/* ignore fake brackets */
 		}
-		if (PL_lex_inwhat == OP_SUBST && PL_lex_repl
+		if (PL_lex_inwhat == OP_SUBST && PL_lex_repl == PL_linestr
 		 && SvEVALED(PL_lex_repl))
 		    PL_lex_state = LEX_INTERPEND;
 		else if (*s == '-' && s[1] == '>')

t/base/lex.t
@@ -1,6 +1,6 @@
 #!./perl
 
-print "1..72\n";
+print "1..73\n";
 
 $x = 'x';
 
@@ -358,3 +358,8 @@ print "ok 71 - s//'#' . <<END/e\n";
 eval "s//3}->{3/e";
 print "not " unless $@;
 print "ok 72 - s//3}->{3/e\n";
+
+$_ = "not ok 73";
+$x{3} = "not ";
+eval 's/${\%x}{3}//e';
+print "$_ - s//\${\\%x}{3}/e\n";