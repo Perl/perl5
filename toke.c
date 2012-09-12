74736ae6be18c8156adf865e9641f1f6d230ede1
Father Chrysostomos <sprout@cpan.org>
2012-09-12 13:31:40 -0700

    toke.c: Under -DT, dump complement properly
    Before:
    
    $ ./miniperl -DT -e '~foo'
    ....
    ### 0:LEX_NORMAL/XSTATE ""
    ### <== ?? 126
    ....
    
    After:
    
    $ ./miniperl -DT -e '~foo'
    ....
    ### 0:LEX_NORMAL/XSTATE ""
    ### <== '~'
    ....

--- toke.c
+++ toke.c
@@ -433,7 +433,7 @@ S_tokereport(pTHX_ I32 rv, const YYSTYPE* lvalp)
 	}
 	if (name)
 	    Perl_sv_catpv(aTHX_ report, name);
-	else if ((char)rv > ' ' && (char)rv < '~')
+	else if ((char)rv > ' ' && (char)rv <= '~')
 	    Perl_sv_catpvf(aTHX_ report, "'%c'", (char)rv);
 	else if (!rv)
 	    sv_catpvs(report, "EOF");
