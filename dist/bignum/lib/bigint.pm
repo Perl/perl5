2b1c39ffe7d4f5945ee4ead637b6507a2d81fba4
Father Chrysostomos <sprout@cpan.org>
2012-10-04 00:49:37 -0700

    Increase bignum versions to 0.31 after the preceding change

--- dist/bignum/lib/bigint.pm
+++ dist/bignum/lib/bigint.pm
@@ -1,7 +1,7 @@
 package bigint;
 use 5.006;
 
-$VERSION = '0.30';
+$VERSION = '0.31';
 use Exporter;
 @ISA		= qw( Exporter );
 @EXPORT_OK	= qw( PI e bpi bexp hex oct );
--- dist/bignum/lib/bigrat.pm
+++ dist/bignum/lib/bigrat.pm
@@ -1,7 +1,7 @@
 package bigrat;
 use 5.006;
 
-$VERSION = '0.30';
+$VERSION = '0.31';
 require Exporter;
 @ISA		= qw( bigint );
 @EXPORT_OK 	= qw( PI e bpi bexp hex oct );
