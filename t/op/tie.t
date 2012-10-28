Perl/perl5

commit 54a4274e3c68e0e5bc9baaef00b55c43097929a7

[perl #45147] Issue with the exists function
Perl_av_exists tested to see if regdata magic was present,
but did not have any logic to fetch that data in the positive
key case.  Additionally, in the negative key case, if AvFILL
indicated the key existed, it wouldn't return, and would then
fall through to the logic that treated it like a real array.

av.c
@@ -901,7 +901,9 @@ Perl_av_exists(pTHX_ AV *av, I32 key)
     if (SvRMAGICAL(av)) {
         const MAGIC * const tied_magic
 	    = mg_find((const SV *)av, PERL_MAGIC_tied);
-        if (tied_magic || mg_find((const SV *)av, PERL_MAGIC_regdata)) {
+        const MAGIC * const regdata_magic
+            = mg_find((const SV *)av, PERL_MAGIC_regdata);
+        if (tied_magic || regdata_magic) {
 	    SV * const sv = sv_newmortal();
             MAGIC *mg;
             /* Handle negative array indices 20020222 MJD */
@@ -920,7 +922,16 @@ Perl_av_exists(pTHX_ AV *av, I32 key)
                     key += AvFILL(av) + 1;
                     if (key < 0)
                         return FALSE;
+                    else
+                        return TRUE;
                 }
+            }
+
+            if(key >= 0 && regdata_magic) {
+                if (key <= AvFILL(av))
+                    return TRUE;
+                else
+                    return FALSE;
             }
 
             mg_copy(MUTABLE_SV(av), sv, 0, key);

AUTHORS
@@ -821,6 +821,7 @@ Peter J. Farley III		<pjfarley@banet.net>
 Peter J. Holzer			<hjp@hjp.at>
 Peter Jaspers-Fayer
 Peter John Acklam		<pjacklam@online.no>
+Peter Martini                   <PeterCMartini@GMail.com>
 Peter O'Gorman			<peter@pogma.com>
 Peter Prymmer			<PPrymmer@factset.com>
 Peter Rabbitson			<rabbit@rabbit.us>

t/re/pat.t
@@ -23,7 +23,7 @@ BEGIN {
 }
 
 
-plan tests => 411;  # Update this when adding/deleting tests.
+plan tests => 423;  # Update this when adding/deleting tests.
 
 run_tests() unless caller;
 
@@ -640,6 +640,19 @@ sub run_tests {
         ok !defined $+ [3] && !defined $- [3] &&
            !defined $+ [4] && !defined $- [4];
 
+        # Exists has a special check for @-/@+ - bug 45147
+        ok exists $-[0];
+        ok exists $+[0];
+        ok exists $-[2];
+        ok exists $+[2];
+        ok !exists $-[3];
+        ok !exists $+[3];
+        ok exists $-[-1];
+        ok exists $+[-1];
+        ok exists $-[-3];
+        ok exists $+[-3];
+        ok !exists $-[-4];
+        ok !exists $+[-4];
 
         /.(a)(b)?(a)/;
         iseq $#+, 3;

commit 0960ff5ae77d081b538d8f0690367bb5144cf1c1

Don’t skip tied EXISTS for negative array indices
This was broken in 5.14.0 for those cases where $NEGATIVE_INDICES is
not true:

sub TIEARRAY{bless[]};
sub FETCHSIZE { 50 }
sub EXISTS { print "does $_[1] exist?\n" }
tie @a, "";
exists $a[1];
exists $a[-1];
$NEGATIVE_INDICES=1;
exists $a[-1];

$ pbpaste|perl5.12.0
does 1 exist?
does 49 exist?
does -1 exist?
$ pbpaste|perl5.14.0
does 1 exist?
does -1 exist?

This was broken by 54a4274e3c.

av.c
@@ -968,8 +968,6 @@ Perl_av_exists(pTHX_ AV *av, I32 key)
                     key += AvFILL(av) + 1;
                     if (key < 0)
                         return FALSE;
-                    else
-                        return TRUE;
                 }
             }
 

t/op/tie.t
@@ -1302,3 +1302,18 @@ each %$h;
 delete $$h{foo};
 tie %$h, 'l';
 EXPECT
+########
+
+# NAME EXISTS on arrays
+sub TIEARRAY{bless[]};
+sub FETCHSIZE { 50 }
+sub EXISTS { print "does $_[1] exist?\n" }
+tie @a, "";
+exists $a[1];
+exists $a[-1];
+$NEGATIVE_INDICES=1;
+exists $a[-1];
+EXPECT
+does 1 exist?
+does 49 exist?
+does -1 exist?