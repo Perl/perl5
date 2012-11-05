Perl/perl5

commit 5db1eb8d3ecc607380cd43267d08149b82822fe9

Label UTF8 cleanup
This meant changing LABEL's definition in perly.y, so most of this
commit is actually from the regened files.

perly.h
@@ -231,7 +231,7 @@ typedef union YYSTYPE
     TOKEN* tkval;
 #endif
 }
-/* Line 1489 of yacc.c.  */
+/* Line 1529 of yacc.c.  */
 	YYSTYPE;
 # define yystype YYSTYPE /* obsolescent; will be withdrawn */
 # define YYSTYPE_IS_DECLARED 1
@@ -242,6 +242,6 @@ typedef union YYSTYPE
 
 
 /* Generated from:
- * 653e5740260a91fc0511942c124de9498176ffc3862f4d9d4523d3bafbace9c6 perly.y
+ * 047d7d5048e78a17bc586b7bb9a0f0e9dedd5cd43b30e486482b1ff8f955ddcb perly.y
  * 53f57d7143a42b3c008841a14d158bcf9cab64b2904b07ef5e95051fe9a8a875 regen_perly.pl
  * ex: set ro: */

perly.tab
@@ -180,27 +180,27 @@ static const yytype_uint16 yyrline[] =
 {
        0,   142,   142,   141,   151,   150,   160,   159,   172,   171,
      184,   183,   196,   195,   207,   217,   221,   224,   234,   239,
-     240,   249,   257,   261,   267,   275,   277,   282,   300,   321,
-     333,   349,   348,   365,   374,   383,   389,   391,   393,   403,
-     413,   434,   443,   452,   461,   468,   467,   493,   499,   509,
-     511,   513,   517,   521,   525,   529,   534,   540,   541,   547,
-     561,   562,   571,   577,   578,   583,   586,   590,   595,   599,
-     603,   607,   608,   612,   618,   623,   628,   638,   639,   644,
-     645,   649,   659,   663,   673,   674,   684,   688,   692,   696,
-     700,   710,   719,   723,   728,   735,   744,   750,   756,   764,
-     768,   775,   774,   785,   786,   790,   799,   804,   812,   819,
-     826,   836,   845,   852,   861,   868,   874,   881,   886,   896,
-     900,   904,   910,   914,   918,   922,   926,   930,   934,   946,
-     950,   954,   958,   968,   972,   979,   983,   987,   992,   997,
-    1002,  1011,  1016,  1021,  1027,  1033,  1044,  1048,  1052,  1064,
-    1077,  1085,  1097,  1098,  1099,  1100,  1101,  1106,  1110,  1112,
-    1116,  1121,  1123,  1128,  1130,  1132,  1134,  1136,  1138,  1140,
-    1149,  1160,  1162,  1164,  1169,  1182,  1187,  1192,  1196,  1200,
-    1204,  1208,  1212,  1216,  1220,  1222,  1225,  1229,  1235,  1237,
-    1242,  1245,  1254,  1260,  1265,  1266,  1267,  1273,  1277,  1285,
-    1292,  1297,  1302,  1304,  1306,  1311,  1313,  1318,  1319,  1323,
-    1326,  1325,  1333,  1337,  1343,  1349,  1355,  1361,  1367,  1374,
-    1376,  1378,  1381
+     240,   249,   257,   261,   268,   277,   279,   284,   302,   323,
+     335,   351,   350,   367,   376,   385,   391,   393,   395,   405,
+     415,   436,   445,   454,   463,   470,   469,   495,   501,   511,
+     513,   515,   519,   523,   527,   531,   536,   542,   543,   549,
+     563,   564,   573,   579,   580,   585,   588,   592,   597,   601,
+     605,   609,   610,   614,   620,   625,   630,   640,   641,   646,
+     647,   651,   661,   665,   675,   676,   686,   690,   694,   698,
+     702,   712,   721,   725,   730,   737,   746,   752,   758,   766,
+     770,   777,   776,   787,   788,   792,   801,   806,   814,   821,
+     828,   838,   847,   854,   863,   870,   876,   883,   888,   898,
+     902,   906,   912,   916,   920,   924,   928,   932,   936,   948,
+     952,   956,   960,   970,   974,   981,   985,   989,   994,   999,
+    1004,  1013,  1018,  1023,  1029,  1035,  1046,  1050,  1054,  1066,
+    1079,  1087,  1099,  1100,  1101,  1102,  1103,  1108,  1112,  1114,
+    1118,  1123,  1125,  1130,  1132,  1134,  1136,  1138,  1140,  1142,
+    1151,  1162,  1164,  1166,  1171,  1184,  1189,  1194,  1198,  1202,
+    1206,  1210,  1214,  1218,  1222,  1224,  1227,  1231,  1237,  1239,
+    1244,  1247,  1256,  1262,  1267,  1268,  1269,  1275,  1279,  1287,
+    1294,  1299,  1304,  1306,  1308,  1313,  1315,  1320,  1321,  1325,
+    1328,  1327,  1335,  1339,  1345,  1351,  1357,  1363,  1369,  1376,
+    1378,  1380,  1383
 };
 #endif
 
@@ -1093,6 +1093,6 @@ static const toketypes yy_type_tab[] =
 };
 
 /* Generated from:
- * 653e5740260a91fc0511942c124de9498176ffc3862f4d9d4523d3bafbace9c6 perly.y
+ * 047d7d5048e78a17bc586b7bb9a0f0e9dedd5cd43b30e486482b1ff8f955ddcb perly.y
  * 53f57d7143a42b3c008841a14d158bcf9cab64b2904b07ef5e95051fe9a8a875 regen_perly.pl
  * ex: set ro: */

perly.y
@@ -260,13 +260,15 @@ fullstmt:	barestmt
 
 labfullstmt:	LABEL barestmt
 			{
-			  $$ = newSTATEOP(0, PVAL($1), $2);
+			  $$ = newSTATEOP(SvUTF8(((SVOP*)$1)->op_sv),
+                                        savepv(SvPVX(((SVOP*)$1)->op_sv)), $2);
 			  TOKEN_GETMAD($1,
 			      $2 ? cLISTOPx($$)->op_first : $$, 'L');
 			}
 	|	LABEL labfullstmt
 			{
-			  $$ = newSTATEOP(0, PVAL($1), $2);
+			  $$ = newSTATEOP(SvUTF8(((SVOP*)$1)->op_sv),
+                                        savepv(SvPVX(((SVOP*)$1)->op_sv)), $2);
 			  TOKEN_GETMAD($1, cLISTOPx($$)->op_first, 'L');
 			}
 	;

toke.c
@@ -359,7 +359,7 @@ static struct debug_tokens {
     { GIVEN,		TOKENTYPE_IVAL,		"GIVEN" },
     { HASHBRACK,	TOKENTYPE_NONE,		"HASHBRACK" },
     { IF,		TOKENTYPE_IVAL,		"IF" },
-    { LABEL,		TOKENTYPE_PVAL,		"LABEL" },
+    { LABEL,		TOKENTYPE_OPVAL,	"LABEL" },
     { LOCAL,		TOKENTYPE_IVAL,		"LOCAL" },
     { LOOPEX,		TOKENTYPE_OPNUM,	"LOOPEX" },
     { LSTOP,		TOKENTYPE_OPNUM,	"LSTOP" },
@@ -4231,6 +4231,7 @@ Perl_madlex(pTHX)
     case FUNC0SUB:
     case UNIOPSUB:
     case LSTOPSUB:
+    case LABEL:
 	if (pl_yylval.opval)
 	    append_madprops(PL_thismad, pl_yylval.opval, 0);
 	PL_thismad = 0;
@@ -4289,10 +4290,6 @@ Perl_madlex(pTHX)
 		PL_bufptr = s;
 	    }
 	}
-	break;
-
-    /* pval */
-    case LABEL:
 	break;
 
     /* ival */
@@ -6573,7 +6570,9 @@ Perl_yylex(pTHX)
 	if (!anydelim && PL_expect == XSTATE
 	      && d < PL_bufend && *d == ':' && *(d + 1) != ':') {
 	    s = d + 1;
-	    pl_yylval.pval = CopLABEL_alloc(PL_tokenbuf);
+	    pl_yylval.opval = (OP*)newSVOP(OP_CONST, 0,
+                                            newSVpvn_flags(PL_tokenbuf,
+                                                        len, UTF ? SVf_UTF8 : 0));
 	    CLINE;
 	    TOKEN(LABEL);
 	}
@@ -8797,7 +8796,7 @@ S_scan_word(pTHX_ register char *s, char *dest, STRLEN destlen, int allow_packag
     for (;;) {
 	if (d >= e)
 	    Perl_croak(aTHX_ ident_too_long);
-	if (isALNUM(*s))	/* UTF handled below */
+	if (isALNUM(*s) || (!UTF && isALNUMC_L1(*s)))	/* UTF handled below */
 	    *d++ = *s++;
 	else if (allow_package && (*s == '\'') && isIDFIRST_lazy_if(s+1,UTF)) {
 	    *d++ = ':';
@@ -11509,8 +11508,8 @@ Perl_parse_label(pTHX_ U32 flags)
     if (PL_lex_state == LEX_KNOWNEXT) {
 	PL_parser->yychar = yylex();
 	if (PL_parser->yychar == LABEL) {
-	    char *lpv = pl_yylval.pval;
-	    STRLEN llen = strlen(lpv);
+	    STRLEN llen;
+	    char *lpv = SvPV(cSVOPx(pl_yylval.opval)->op_sv, llen);
 	    SV *lsv;
 	    PL_parser->yychar = YYEMPTY;
 	    lsv = newSV_type(SVt_PV);
@@ -11518,6 +11517,8 @@ Perl_parse_label(pTHX_ U32 flags)
 	    SvCUR_set(lsv, llen);
 	    SvLEN_set(lsv, llen+1);
 	    SvPOK_on(lsv);
+            if (SvUTF8(cSVOPx(pl_yylval.opval)->op_sv))
+                SvUTF8_on(lsv);
 	    return lsv;
 	} else {
 	    yyunlex();
@@ -11525,17 +11526,12 @@ Perl_parse_label(pTHX_ U32 flags)
 	}
     } else {
 	char *s, *t;
-	U8 c;
 	STRLEN wlen, bufptr_pos;
 	lex_read_space(0);
 	t = s = PL_bufptr;
-	c = (U8)*s;
-	if (!isIDFIRST_A(c))
+        if (!isIDFIRST_lazy_if(s, UTF))
 	    goto no_label;
-	do {
-	    c = (U8)*++t;
-	} while(isWORDCHAR_A(c));
-	wlen = t - s;
+	t = scan_word(s, PL_tokenbuf, sizeof PL_tokenbuf, FALSE, &wlen);
 	if (word_takes_any_delimeter(s, wlen))
 	    goto no_label;
 	bufptr_pos = s - SvPVX(PL_linestr);
@@ -11547,7 +11543,7 @@ Perl_parse_label(pTHX_ U32 flags)
 	    PL_oldoldbufptr = PL_oldbufptr;
 	    PL_oldbufptr = s;
 	    PL_bufptr = t+1;
-	    return newSVpvn(s, wlen);
+	    return newSVpvn_flags(s, wlen, UTF ? SVf_UTF8 : 0);
 	} else {
 	    PL_bufptr = s;
 	    no_label:

MANIFEST
@@ -3996,6 +3996,7 @@ ext/XS-APItest/t/keyword_multiline.t	test keyword plugin parsing across lines
 ext/XS-APItest/t/keyword_plugin.t	test keyword plugin mechanism
 ext/XS-APItest/t/labelconst.aux	auxiliary file for label test
 ext/XS-APItest/t/labelconst.t	test recursive descent label parsing
+ext/XS-APItest/t/labelconst_utf8.aux	auxiliary file for label test in UTF-8
 ext/XS-APItest/t/loopblock.t	test recursive descent block parsing
 ext/XS-APItest/t/looprest.t	test recursive descent statement-sequence parsing
 ext/XS-APItest/t/lvalue.t	Test XS lvalue functions
@@ -5492,6 +5493,7 @@ t/uni/fold.t			See if Unicode folding works
 t/uni/goto.t			See if Unicode goto &sub works
 t/uni/greek.t			See if Unicode in greek works
 t/uni/gv.t			See if Unicode GVs work.
+t/uni/labels.t			See if Unicode labels work
 t/uni/latin2.t			See if Unicode in latin2 works
 t/uni/lex_utf8.t		See if Unicode in lexer works
 t/uni/lower.t			See if Unicode casing works

cop.h
@@ -555,6 +555,8 @@ be zero.
     cophh_2hv(CopHINTHASH_get(cop), flags)
 
 #define CopLABEL(c)  Perl_cop_fetch_label(aTHX_ (c), NULL, NULL)
+#define CopLABEL_len(c,len)  Perl_cop_fetch_label(aTHX_ (c), len, NULL)
+#define CopLABEL_len_flags(c,len,flags)  Perl_cop_fetch_label(aTHX_ (c), len, flags)
 #define CopLABEL_alloc(pv)	((pv)?savepv(pv):NULL)
 
 #define CopSTASH_ne(c,hv)	(!CopSTASH_eq(c,hv))
@@ -779,6 +781,8 @@ struct block_loop {
 	 : (SV**)NULL)
 
 #define CxLABEL(c)	(0 + CopLABEL((c)->blk_oldcop))
+#define CxLABEL_len(c,len)	(0 + CopLABEL_len((c)->blk_oldcop, len))
+#define CxLABEL_len_flags(c,len,flags)	(0 + CopLABEL_len_flags((c)->blk_oldcop, len, flags))
 #define CxHASARGS(c)	(((c)->cx_type & CXp_HASARGS) == CXp_HASARGS)
 #define CxLVAL(c)	(0 + (c)->blk_u16)
 

embed.fnc
@@ -1841,12 +1841,13 @@ snR	|char *	|bytes_to_uni	|NN const U8 *start|STRLEN len|NN char *dest
 
 #if defined(PERL_IN_PP_CTL_C)
 sR	|OP*	|docatch	|NULLOK OP *o
-sR	|OP*	|dofindlabel	|NN OP *o|NN const char *label|NN OP **opstack|NN OP **oplimit
+sR	|OP*	|dofindlabel	|NN OP *o|NN const char *label|STRLEN len \
+                                |U32 flags|NN OP **opstack|NN OP **oplimit
 s	|MAGIC *|doparseform	|NN SV *sv
 snR	|bool	|num_overflow	|NV value|I32 fldsize|I32 frcsize
 sR	|I32	|dopoptoeval	|I32 startingblock
 sR	|I32	|dopoptogiven	|I32 startingblock
-sR	|I32	|dopoptolabel	|NN const char *label
+sR	|I32	|dopoptolabel	|NN const char *label|STRLEN len|U32 flags
 sR	|I32	|dopoptoloop	|I32 startingblock
 sR	|I32	|dopoptosub_at	|NN const PERL_CONTEXT* cxstk|I32 startingblock
 sR	|I32	|dopoptowhen	|I32 startingblock

commit 5504e6cfd4036fd9f057da65d41cb31360de5221

Fix invalid token warning with PERL_XMLDUMP and label
Under mad builds, commit 5db1eb8 caused this warning:

$ PERL_XMLDUMP=/dev/null ./perl -Ilib -e 'foo:'
Invalid TOKEN object ignored at -e line 1.

Since I don’t understand the mad code so well, the easiest fix is to
revert back to using a PV, as we did before 5db1eb8.  To record the
utf8ness, we sneak it behind the trailing null.

perly.act
@@ -190,21 +190,21 @@ case 2:
   case 25:
 #line 278 "perly.y"
     {
-			  (yyval.opval) = newSTATEOP(SvUTF8(((SVOP*)(ps[(1) - (2)].val.p_tkval))->op_sv),
-                                        savepv(SvPVX(((SVOP*)(ps[(1) - (2)].val.p_tkval))->op_sv)), (ps[(2) - (2)].val.opval));
+			  (yyval.opval) = newSTATEOP(SVf_UTF8
+					   * PVAL((ps[(1) - (2)].val.p_tkval))[strlen(PVAL((ps[(1) - (2)].val.p_tkval)))+1],
+					  PVAL((ps[(1) - (2)].val.p_tkval)), (ps[(2) - (2)].val.opval));
 			  TOKEN_GETMAD((ps[(1) - (2)].val.p_tkval),
 			      (ps[(2) - (2)].val.opval) ? cLISTOPx((yyval.opval))->op_first : (yyval.opval), 'L');
-			  op_free((OP*)(ps[(1) - (2)].val.p_tkval));
 			;}
     break;
 
   case 26:
 #line 286 "perly.y"
     {
-			  (yyval.opval) = newSTATEOP(SvUTF8(((SVOP*)(ps[(1) - (2)].val.p_tkval))->op_sv),
-                                        savepv(SvPVX(((SVOP*)(ps[(1) - (2)].val.p_tkval))->op_sv)), (ps[(2) - (2)].val.opval));
+			  (yyval.opval) = newSTATEOP(SVf_UTF8
+					   * PVAL((ps[(1) - (2)].val.p_tkval))[strlen(PVAL((ps[(1) - (2)].val.p_tkval)))+1],
+					  PVAL((ps[(1) - (2)].val.p_tkval)), (ps[(2) - (2)].val.opval));
 			  TOKEN_GETMAD((ps[(1) - (2)].val.p_tkval), cLISTOPx((yyval.opval))->op_first, 'L');
-			  op_free((OP*)(ps[(1) - (2)].val.p_tkval));
 			;}
     break;
 
@@ -1781,6 +1781,6 @@ case 2:
     
 
 /* Generated from:
- * f140f4bf4565d188246d4b85b0baf052281eeed1824a5697fcb811727a34da08 perly.y
+ * 28c2c7738eefc00762e227af0363c729186c992e9127e0c935684cccbf3a3b4f perly.y
  * 73288b53f1f6dc0183252a5a8e4f39cfe36e357e77233704fad3021fee88eb5b regen_perly.pl
  * ex: set ro: */

perly.h
@@ -266,6 +266,6 @@ typedef union YYSTYPE
 
 
 /* Generated from:
- * f140f4bf4565d188246d4b85b0baf052281eeed1824a5697fcb811727a34da08 perly.y
+ * 28c2c7738eefc00762e227af0363c729186c992e9127e0c935684cccbf3a3b4f perly.y
  * 73288b53f1f6dc0183252a5a8e4f39cfe36e357e77233704fad3021fee88eb5b regen_perly.pl
  * ex: set ro: */

perly.tab
@@ -1127,6 +1127,6 @@ static const toketypes yy_type_tab[] =
 };
 
 /* Generated from:
- * f140f4bf4565d188246d4b85b0baf052281eeed1824a5697fcb811727a34da08 perly.y
+ * 28c2c7738eefc00762e227af0363c729186c992e9127e0c935684cccbf3a3b4f perly.y
  * 73288b53f1f6dc0183252a5a8e4f39cfe36e357e77233704fad3021fee88eb5b regen_perly.pl
  * ex: set ro: */

perly.y
@@ -276,18 +276,18 @@ fullstmt:	barestmt
 
 labfullstmt:	LABEL barestmt
 			{
-			  $$ = newSTATEOP(SvUTF8(((SVOP*)$1)->op_sv),
-                                        savepv(SvPVX(((SVOP*)$1)->op_sv)), $2);
+			  $$ = newSTATEOP(SVf_UTF8
+					   * PVAL($1)[strlen(PVAL($1))+1],
+					  PVAL($1), $2);
 			  TOKEN_GETMAD($1,
 			      $2 ? cLISTOPx($$)->op_first : $$, 'L');
-			  op_free((OP*)$1);
 			}
 	|	LABEL labfullstmt
 			{
-			  $$ = newSTATEOP(SvUTF8(((SVOP*)$1)->op_sv),
-                                        savepv(SvPVX(((SVOP*)$1)->op_sv)), $2);
+			  $$ = newSTATEOP(SVf_UTF8
+					   * PVAL($1)[strlen(PVAL($1))+1],
+					  PVAL($1), $2);
 			  TOKEN_GETMAD($1, cLISTOPx($$)->op_first, 'L');
-			  op_free((OP*)$1);
 			}
 	;
 

toke.c
@@ -358,7 +358,7 @@ static struct debug_tokens {
     { GIVEN,		TOKENTYPE_IVAL,		"GIVEN" },
     { HASHBRACK,	TOKENTYPE_NONE,		"HASHBRACK" },
     { IF,		TOKENTYPE_IVAL,		"IF" },
-    { LABEL,		TOKENTYPE_OPVAL,	"LABEL" },
+    { LABEL,		TOKENTYPE_PVAL,		"LABEL" },
     { LOCAL,		TOKENTYPE_IVAL,		"LOCAL" },
     { LOOPEX,		TOKENTYPE_OPNUM,	"LOOPEX" },
     { LSTOP,		TOKENTYPE_OPNUM,	"LSTOP" },
@@ -4278,7 +4278,6 @@ Perl_madlex(pTHX)
     case FUNC0SUB:
     case UNIOPSUB:
     case LSTOPSUB:
-    case LABEL:
 	if (pl_yylval.opval)
 	    append_madprops(PL_thismad, pl_yylval.opval, 0);
 	PL_thismad = 0;
@@ -4291,6 +4290,10 @@ Perl_madlex(pTHX)
 	    addmad(newMADsv('p', PL_endwhite), &PL_thismad, 0);
 	    PL_endwhite = 0;
 	}
+	break;
+
+    /* pval */
+    case LABEL:
 	break;
 
     case ']':
@@ -6712,9 +6715,9 @@ Perl_yylex(pTHX)
 	if (!anydelim && PL_expect == XSTATE
 	      && d < PL_bufend && *d == ':' && *(d + 1) != ':') {
 	    s = d + 1;
-	    pl_yylval.opval = (OP*)newSVOP(OP_CONST, 0,
-                                            newSVpvn_flags(PL_tokenbuf,
-                                                        len, UTF ? SVf_UTF8 : 0));
+	    pl_yylval.pval = savepvn(PL_tokenbuf, len+1);
+	    pl_yylval.pval[len] = '\0';
+	    pl_yylval.pval[len+1] = UTF ? 1 : 0;
 	    CLINE;
 	    TOKEN(LABEL);
 	}
@@ -11763,11 +11766,10 @@ Perl_parse_label(pTHX_ U32 flags)
     if (PL_lex_state == LEX_KNOWNEXT) {
 	PL_parser->yychar = yylex();
 	if (PL_parser->yychar == LABEL) {
-	    SV *lsv;
+	    char * const lpv = pl_yylval.pval;
+	    STRLEN llen = strlen(lpv);
 	    PL_parser->yychar = YYEMPTY;
-	    lsv = newSV_type(SVt_PV);
-	    sv_copypv(lsv, cSVOPx(pl_yylval.opval)->op_sv);
-	    return lsv;
+	    return newSVpvn_flags(lpv, llen, lpv[llen+1] ? SVf_UTF8 : 0);
 	} else {
 	    yyunlex();
 	    goto no_label;