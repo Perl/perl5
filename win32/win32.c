Perl/perl5

commit 3fcdbd32b23d57a14f2c17d16671c54ae6a034ca

Don't try to ansify the path if it is empty
Fixes a possible crash (manifested when running with the page heap enabled)
when running after clearing PATH, which at least one test in op/taint.t
does.

win32/win32.c
@@ -4242,6 +4242,8 @@ ansify_path(void)
     wide_path = (WCHAR*)win32_malloc(len*sizeof(WCHAR));
     while (wide_path) {
         size_t newlen = GetEnvironmentVariableW(L"PATH", wide_path, len);
+        if (newlen == 0)
+            return;
         if (newlen < len)
             break;
         len = newlen;

commit cfd4abf2bf035401d8184babc5d1aca80208cd5b

Oops, fix memory leak just introduced by 3fcdbd32b2

win32/win32.c
@@ -4242,8 +4242,10 @@ ansify_path(void)
     wide_path = (WCHAR*)win32_malloc(len*sizeof(WCHAR));
     while (wide_path) {
         size_t newlen = GetEnvironmentVariableW(L"PATH", wide_path, len);
-        if (newlen == 0)
+        if (newlen == 0) {
+            win32_free(wide_path);
             return;
+        }
         if (newlen < len)
             break;
         len = newlen;