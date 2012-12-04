a8e6f76f54394645f5d605301d81d4d0f7cd9422
Father Chrysostomos <sprout@cpan.org>
2012-12-04 06:57:02 -0800

    sigtrap.pm: Avoid lexical $_
    my $_ is about to be deprecated.
    
    It has been using it since commit 972fc2eb7a to avoid modifying a
    read-only argument.  Using @{[...]} instead works, too.

--- lib/sigtrap.pm
+++ lib/sigtrap.pm
@@ -95,8 +95,7 @@ sub handler_traceback {
     # Now go for broke.
     for ($i = 1; ($p,$f,$l,$s,$h,$w,$e,$r) = caller($i); $i++) {
         @a = ();
-	for my $fr (@args) {
-            my $_ = $fr;
+	for (@{[@args]}) {
 	    s/([\'\\])/\\$1/g;
 	    s/([^\0]*)/'$1'/
 	      unless /^(?: -?[\d.]+ | \*[\w:]* )$/x;
