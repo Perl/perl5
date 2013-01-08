3f1ec28328e8edb26ba0e89faa2450accdbe704e
Chris 'BinGOs' Williams <chris@bingosnet.co.uk>
2013-01-08 10:27:17 +0000

    Update Compress-Raw-Bzip2 to CPAN version 2.060
      [DELTA]
    
      2.060 7 January 2013
    
          * No Changes

--- Porting/Maintainers.pl
+++ Porting/Maintainers.pl
@@ -363,7 +363,7 @@ use File::Glob qw(:case);
 
     'Compress::Raw::Bzip2' => {
         'MAINTAINER'   => 'pmqs',
-        'DISTRIBUTION' => 'PMQS/Compress-Raw-Bzip2-2.059.tar.gz',
+        'DISTRIBUTION' => 'PMQS/Compress-Raw-Bzip2-2.060.tar.gz',
         'FILES'        => q[cpan/Compress-Raw-Bzip2],
         'EXCLUDED'     => [
             qr{^t/Test/},
