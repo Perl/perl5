804e686f69c78784d249726ceeca7c7d5ff65141
Chris 'BinGOs' Williams <chris@bingosnet.co.uk>
2013-01-25 16:37:59 +0000

    Update CPANPLUS-Dist-Build to CPAN version 0.70
      [DELTA]
    
      0.70 Fri Jan 25 15:58:27 GMT 2013
        - Add deprecation usage to warn if modules are loaded
          from corelib. CPANPLUS::Dist::Build will be leaving
          core with v5.20.0 and will still be available on
          CPAN.

--- cpan/CPANPLUS-Dist-Build/lib/CPANPLUS/Dist/Build.pm
+++ cpan/CPANPLUS-Dist-Build/lib/CPANPLUS/Dist/Build.pm
@@ -1,5 +1,7 @@
 package CPANPLUS::Dist::Build;
 
+use if $] > 5.017, 'deprecate';
+
 use strict;
 use warnings;
 use vars    qw[@ISA $STATUS $VERSION];
@@ -30,7 +32,7 @@ use Locale::Maketext::Simple    Class => 'CPANPLUS', Style => 'gettext';
 
 local $Params::Check::VERBOSE = 1;
 
-$VERSION = '0.68';
+$VERSION = '0.70';
 
 =pod
 
