d907e2e99312aeebe2e1355d660e3271a67c8221
James E Keenan <jkeenan@cpan.org>
2013-01-27 18:55:42 -0500

    Belated Happy New Year!

--- perl.c
+++ perl.c
@@ -3471,7 +3471,7 @@ S_minus_v(pTHX)
 #endif
 
 	PerlIO_printf(PIO_stdout,
-		      "\n\nCopyright 1987-2012, Larry Wall\n");
+		      "\n\nCopyright 1987-2013, Larry Wall\n");
 #ifdef MSDOS
 	PerlIO_printf(PIO_stdout,
 		      "\nMS-DOS port Copyright (c) 1989, 1990, Diomidis Spinellis\n");
