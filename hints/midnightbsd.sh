721fe537fc341f13be7c2677a2ddb9dfb4f38766
Lucas Holt <luke@foolishgames.com>
2013-02-16 13:18:58 -0800

    [perl #116785] libc_r was removed from recent versions of MidnightBSD and older versions work better with -pthread
    This corrects build errors with threading enabled on MidnightBSD 0.4-CURRENT
    
    Signed-off-by: Chris 'BinGOs' Williams <chris@bingosnet.co.uk>

--- hints/midnightbsd.sh
+++ hints/midnightbsd.sh
@@ -33,12 +33,8 @@ d_voidsig='define'
 cat > UU/usethreads.cbu <<'EOCBU'
 case "$usethreads" in
 $define|true|[yY]*)
-        lc_r=`/sbin/ldconfig -r|grep ':-lc_r'|awk '{print $NF}'|sed -n '$p'`
-	    	ldflags="-pthread $ldflags"
-	      d_gethostbyaddr_r="undef"
-	      d_gethostbyaddr_r_proto="0"
-
-	set `echo X "$libswanted "| sed -e 's/ c / c_r /'`
+	ldflags="-pthread $ldflags"
+	set `echo X "$libswanted "| sed -e 's/ c //'`
 	shift
 	libswanted="$*"
 	# Configure will probably pick the wrong libc to use for nm scan.
