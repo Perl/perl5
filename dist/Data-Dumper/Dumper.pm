ecac65218c6c89b4e9ef7b8657392213ee119959
Steffen Mueller <smueller@cpan.org>
2013-02-26 07:53:59 +0100

    Update Data::Dumper version and release date

--- dist/Data-Dumper/Dumper.pm
+++ dist/Data-Dumper/Dumper.pm
@@ -10,7 +10,7 @@
 package Data::Dumper;
 
 BEGIN {
-    $VERSION = '2.142'; # Don't forget to set version and release
+    $VERSION = '2.143'; # Don't forget to set version and release
 }               # date in POD below!
 
 #$| = 1;
@@ -1400,7 +1400,7 @@ modify it under the same terms as Perl itself.
 
 =head1 VERSION
 
-Version 2.142  (January 13 2013)
+Version 2.143  (February 26 2013)
 
 =head1 SEE ALSO
 
