e2e0809474df6127199e06d6ca1edcbbe64d78cc
Chris 'BinGOs' Williams <chris@bingosnet.co.uk>
2013-03-22 13:00:07 +0000

    Module-CoreList is 2.85 on CPAN

--- Porting/Maintainers.pl
+++ Porting/Maintainers.pl
@@ -1271,7 +1271,7 @@ use File::Glob qw(:case);
 
     'Module::CoreList' => {
         'MAINTAINER'   => 'bingos',
-        'DISTRIBUTION' => 'BINGOS/Module-CoreList-2.84.tar.gz',
+        'DISTRIBUTION' => 'BINGOS/Module-CoreList-2.85.tar.gz',
         'FILES'        => q[dist/Module-CoreList],
         'UPSTREAM'     => 'blead',
     },
