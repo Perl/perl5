44bda1357563f00c4856ad314c8cde43bba62433
Brian Fraser <fraserbn@gmail.com>
2013-03-23 17:40:54 -0300

    File::Glob: Drop dependency on given/when

--- ext/File-Glob/Glob.pm
+++ ext/File-Glob/Glob.pm
@@ -4,7 +4,6 @@ use strict;
 our($VERSION, @ISA, @EXPORT_OK, @EXPORT_FAIL, %EXPORT_TAGS, $DEFAULT_FLAGS);
 
 require XSLoader;
-use feature 'switch';
 
 @ISA = qw(Exporter);
 
@@ -38,26 +37,31 @@ pop @{$EXPORT_TAGS{bsd_glob}}; # no "glob"
 
 @EXPORT_OK   = (@{$EXPORT_TAGS{'glob'}}, 'csh_glob');
 
-$VERSION = '1.19';
+$VERSION = '1.20';
 
 sub import {
     require Exporter;
     local $Exporter::ExportLevel = $Exporter::ExportLevel + 1;
     Exporter::import(grep {
-	my $passthrough;
-	given ($_) {
-	    $DEFAULT_FLAGS &= ~GLOB_NOCASE() when ':case';
-	    $DEFAULT_FLAGS |= GLOB_NOCASE() when ':nocase';
-	    when (':globally') {
-		no warnings 'redefine';
-		*CORE::GLOBAL::glob = \&File::Glob::csh_glob;
-	    }
-	    if ($_ eq ':bsd_glob') {
-		no strict; *{caller."::glob"} = \&bsd_glob_override;
-	    }
-	    $passthrough = 1;
+        my $passthrough;
+        if ($_ eq ':case') {
+            $DEFAULT_FLAGS &= ~GLOB_NOCASE()
+        }
+        elsif ($_ eq ':nocase') {
+            $DEFAULT_FLAGS |= GLOB_NOCASE();
+        }
+        elsif ($_ eq ':globally') {
+	    no warnings 'redefine';
+	    *CORE::GLOBAL::glob = \&File::Glob::csh_glob;
 	}
-	$passthrough;
+        elsif ($_ eq ':bsd_glob') {
+	    no strict; *{caller."::glob"} = \&bsd_glob_override;
+            $passthrough = 1;
+	}
+	else {
+            $passthrough = 1;
+        }
+        $passthrough;
     } @_);
 }
 
