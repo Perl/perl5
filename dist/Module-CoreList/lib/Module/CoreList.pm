262731eedb57f5384b69ab925580eb75e90180b7
James E Keenan <jkeenan@cpan.org>
2013-04-13 17:45:10 +0100

    Subject: [PATCH] Update INSTALLDIRS to favor installation under 'site'.
    For: RT #116479

--- dist/Module-CoreList/lib/Module/CoreList.pm
+++ dist/Module-CoreList/lib/Module/CoreList.pm
@@ -3,7 +3,7 @@ use strict;
 use vars qw/$VERSION %released %version %families %upstream
 	    %bug_tracker %deprecated/;
 use Module::CoreList::TieHashDelta;
-$VERSION = '2.85';
+$VERSION = '2.86';
 
 my $dumpinc = 0;
 sub import {
