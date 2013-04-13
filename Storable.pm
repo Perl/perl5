ba729e8a9195143c20876a7c6d5bfce540887b9b
James E Keenan <jkeenan@cpan.org>
2013-04-13 17:45:10 +0100

    Subject: [PATCH] Update INSTALLDIRS to favor installation under 'site'.
    For: RT #116479

--- Storable.pm
+++ Storable.pm
@@ -21,7 +21,7 @@ package Storable; @ISA = qw(Exporter);
 
 use vars qw($canonical $forgive_me $VERSION);
 
-$VERSION = '2.40';
+$VERSION = '2.41';
 
 BEGIN {
     if (eval { local $SIG{__DIE__}; require Log::Agent; 1 }) {
