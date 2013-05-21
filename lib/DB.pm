c0b435603334620c1f3cdd7aedc09f29b4f07381
David Steinbrunner <dsteinbrunner@pobox.com>
2013-05-21 07:01:35 -0400

    typo fix for DB
    Bump $VERSION.

--- lib/DB.pm
+++ lib/DB.pm
@@ -38,10 +38,10 @@ BEGIN {
 
   $DB::package = '';    # current package space
   $DB::filename = '';   # current filename
-  $DB::subname = '';    # currently executing sub (fullly qualified name)
+  $DB::subname = '';    # currently executing sub (fully qualified name)
   $DB::lineno = '';     # current line number
 
-  $DB::VERSION = $DB::VERSION = '1.04';
+  $DB::VERSION = $DB::VERSION = '1.05';
 
   # initialize private globals to avoid warnings
 
