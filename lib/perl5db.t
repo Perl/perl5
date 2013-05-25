Perl/perl5

commit c7b0c61d36b2484174d0a6b943a35f4d9472d95b

Extract _handle_doc_command.

lib/perl5db.pl
@@ -2853,12 +2853,7 @@ Just calls C<runman()> to print the appropriate document.
 
 =cut
 
-                # man, perldoc, doc - show manual pages.
-                if (my ($man_page)
-                    = $cmd =~ /\A(?:man|(?:perl)?doc)\b(?:\s+([^(]*))?\z/) {
-                    runman($man_page);
-                    next CMD;
-                }
+                $obj->_handle_doc_command;
 
 =head4 C<p> - print
 
@@ -3833,6 +3828,19 @@ sub _handle_H_command {
 
         $self->i_cmd($i);
 
+        next CMD;
+    }
+
+    return;
+}
+
+sub _handle_doc_command {
+    my $self = shift;
+
+    # man, perldoc, doc - show manual pages.
+    if (my ($man_page)
+        = $DB::cmd =~ /\A(?:man|(?:perl)?doc)\b(?:\s+([^(]*))?\z/) {
+        runman($man_page);
         next CMD;
     }
 

commit b019bbd28397aba60a9673fa89dded4500c6c783

Fix the man/perldoc/doc command in the debugger.
This was accidentally broken by commit c7b0c61d36b24841.

lib/perl5db.pl
@@ -3695,7 +3695,7 @@ sub _handle_doc_command {
     # man, perldoc, doc - show manual pages.
     if (my ($man_page)
         = $DB::cmd =~ /\A(?:man|(?:perl)?doc)\b(?:\s+([^(]*))?\z/) {
-        runman($man_page);
+        DB::runman($man_page);
         next CMD;
     }
 

pod/perldelta.pod
@@ -362,6 +362,12 @@ for C<struct pmop>. Previously it could return memory only aligned to a
 on some 32 bit platforms. Notably, this caused the build to fail completely
 on sparc GNU/Linux. [RT #118055]
 
+=item *
+
+The debugger's C<man> command been fixed. It was broken in the v5.18.0
+release. The C<man> command is aliased to the names C<doc> and C<perldoc> -
+all now work again.
+
 =back
 
 =head1 Known Problems

lib/perl5db.t
@@ -28,7 +28,7 @@ BEGIN {
     }
 }
 
-plan(113);
+plan(114);
 
 my $rc_filename = '.perldb';
 
@@ -2632,6 +2632,26 @@ sub _calc_trace_wrapper
 \s+new\ value:\s+'2'\n
         /msx,
         "Test w for lexical values.",
+    );
+}
+
+# Test the perldoc command
+# We don't actually run the program, but we need to provide one to the wrapper.
+{
+    my $wrapper = DebugWrap->new(
+        {
+            cmds =>
+            [
+                'perldoc perlrules',
+                'q',
+            ],
+            prog => '../lib/perl5db/t/fact',
+        }
+    );
+
+    $wrapper->output_like(
+        qr/No manual entry for perlrules/,
+        'perldoc command works fine',
     );
 }
 