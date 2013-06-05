ca61997a4280794e35c294ceb92fb53efc2d9195
Nicholas Clark <nick@ccl4.org>
2013-06-05 20:15:12 +0200

    Correct a type in FindExt::apply_config in the "I18N-Langinfo" code.
    Commit 557ab4cb986767c7 (Feb 2011) which added this routine had
    $config->{i_nl_langinfo} not $config->{d_nl_langinfo} ('i' should be 'd').
    
    The logic for I18N::Langinfo should now be correct.

--- win32/FindExt.pm
+++ win32/FindExt.pm
@@ -18,7 +18,7 @@ sub apply_config {
     # duplicates logic from Configure (mostly)
     push @no, "DB_File" unless $config->{i_db};
     push @no, "GDBM_File" unless $config->{i_gdbm};
-    push @no, "I18N-Langinfo" unless $config->{i_langinfo} && $config->{i_nl_langinfo};
+    push @no, "I18N-Langinfo" unless $config->{i_langinfo} && $config->{d_nl_langinfo};
     push @no, "IPC-SysV" unless $config->{d_msg} || $config->{d_sem} || $config->{d_shm};
     push @no, "NDBM_File" unless $config->{d_ndbm};
     push @no, "ODBM_File"
