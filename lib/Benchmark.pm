eda74b07919680a4fe9672b9422bb40f8517d7df
Father Chrysostomos <sprout@cpan.org>
2013-06-22 21:52:11 -0700

    Increase $Benchmark::VERSION to 1.17

--- lib/Benchmark.pm
+++ lib/Benchmark.pm
@@ -442,7 +442,7 @@ our(@ISA, @EXPORT, @EXPORT_OK, %EXPORT_TAGS, $VERSION);
 	      clearcache clearallcache disablecache enablecache);
 %EXPORT_TAGS=( all => [ @EXPORT, @EXPORT_OK ] ) ;
 
-$VERSION = 1.16;
+$VERSION = 1.17;
 
 # --- ':hireswallclock' special handling
 
