c6c8952117b3f95f22ad700d84fdb39432aaa4dd
Nicholas Clark <nick@ccl4.org>
2013-07-08 15:23:48 +0200

    Use more semantic markup in the Pod for ExtUtils::Embed.
    Instead of B<> use F<> for filenames.
    Instead of B<> use C<> for code.
    Instead of B<> use I<> for parameter names.
    Instead of F<> use C<> for e-mail addresses.
    
    Remove semantically unnecessary blank lines, and trailing spaces from edited
    lines.

--- lib/ExtUtils/Embed.pm
+++ lib/ExtUtils/Embed.pm
@@ -285,21 +285,20 @@ ExtUtils::Embed - Utilities for embedding Perl in C/C++ applications
 
 =head1 SYNOPSIS
 
-
  perl -MExtUtils::Embed -e xsinit 
  perl -MExtUtils::Embed -e ccopts 
  perl -MExtUtils::Embed -e ldopts 
 
 =head1 DESCRIPTION
 
-ExtUtils::Embed provides utility functions for embedding a Perl interpreter
+C<ExtUtils::Embed> provides utility functions for embedding a Perl interpreter
 and extensions in your C/C++ applications.  
-Typically, an application B<Makefile> will invoke ExtUtils::Embed
+Typically, an application F<Makefile> will invoke C<ExtUtils::Embed>
 functions while building your application.  
 
 =head1 @EXPORT
 
-ExtUtils::Embed exports the following functions:
+C<ExtUtils::Embed> exports the following functions:
 
 xsinit(), ldopts(), ccopts(), perl_inc(), ccflags(), 
 ccdlflags(), xsi_header(), xsi_protos(), xsi_body()
@@ -338,30 +337,25 @@ B<[@modules]> is an array ref, same as additional arguments mentioned above.
 
 =item Examples
 
-
  perl -MExtUtils::Embed -e xsinit -- -o xsinit.c Socket
 
+This will generate code with an C<xs_init> function that glues the perl C<Socket::bootstrap> function 
+to the C C<boot_Socket> function and writes it to a file named F<xsinit.c>.
 
-This will generate code with an B<xs_init> function that glues the perl B<Socket::bootstrap> function 
-to the C B<boot_Socket> function and writes it to a file named F<xsinit.c>.
-
-Note that B<DynaLoader> is a special case where it must call B<boot_DynaLoader> directly.
+Note that L<DynaLoader> is a special case where it must call C<boot_DynaLoader> directly.
 
  perl -MExtUtils::Embed -e xsinit
 
-
-This will generate code for linking with B<DynaLoader> and 
-each static extension found in B<$Config{static_ext}>.
-The code is written to the default file name B<perlxsi.c>.
-
+This will generate code for linking with C<DynaLoader> and
+each static extension found in C<$Config{static_ext}>.
+The code is written to the default file name F<perlxsi.c>.
 
  perl -MExtUtils::Embed -e xsinit -- -o xsinit.c -std DBI DBD::Oracle
 
-
 Here, code is written for all the currently linked extensions along with code
-for B<DBI> and B<DBD::Oracle>.
+for C<DBI> and C<DBD::Oracle>.
 
-If you have a working B<DynaLoader> then there is rarely any need to statically link in any 
+If you have a working C<DynaLoader> then there is rarely any need to statically link in any 
 other extensions.
 
 =item ldopts()
@@ -380,16 +374,16 @@ with the current Perl.
 B<-I> E<lt>path1:path2E<gt>
 
 Search path for ModuleName.a archives.  
-Default path is B<@INC>.
+Default path is C<@INC>.
 Library archives are expected to be found as 
-B</some/path/auto/ModuleName/ModuleName.a>
-For example, when looking for B<Socket.a> relative to a search path, 
-we should find B<auto/Socket/Socket.a>  
+F</some/path/auto/ModuleName/ModuleName.a>
+For example, when looking for F<Socket.a> relative to a search path,
+we should find F<auto/Socket/Socket.a>
 
-When looking for B<DBD::Oracle> relative to a search path,
-we should find B<auto/DBD/Oracle/Oracle.a>
+When looking for C<DBD::Oracle> relative to a search path,
+we should find F<auto/DBD/Oracle/Oracle.a>
 
-Keep in mind that you can always supply B</my/own/path/ModuleName.a>
+Keep in mind that you can always supply F</my/own/path/ModuleName.a>
 as an additional linker argument.
 
 B<-->  E<lt>list of linker argsE<gt>
@@ -418,30 +412,25 @@ rather than print it to STDOUT.
 
 =item Examples
 
-
  perl -MExtUtils::Embed -e ldopts
 
-
-This will print arguments for linking with B<libperl> and
-extensions found in B<$Config{static_ext}>.  This includes libraries
-found in B<$Config{libs}> and the first ModuleName.a library
-for each extension that is found by searching B<@INC> or the path 
-specified by the B<-I> option.  
+This will print arguments for linking with C<libperl> and
+extensions found in C<$Config{static_ext}>.  This includes libraries
+found in C<$Config{libs}> and the first ModuleName.a library
+for each extension that is found by searching C<@INC> or the path
+specified by the B<-I> option.
 In addition, when ModuleName.a is found, additional linker arguments
-are picked up from the B<extralibs.ld> file in the same directory.
-
+are picked up from the F<extralibs.ld> file in the same directory.
 
  perl -MExtUtils::Embed -e ldopts -- -std Socket
 
-
-This will do the same as the above example, along with printing additional arguments for linking with the B<Socket> extension.
+This will do the same as the above example, along with printing additional arguments for linking with the C<Socket> extension.
 
  perl -MExtUtils::Embed -e ldopts -- -std Msql -- -L/usr/msql/lib -lmsql
 
 Any arguments after the second '--' token are additional linker
 arguments that will be examined for potential conflict.  If there is no
 conflict, the additional arguments will be part of the output.  
-
 
 =item perl_inc()
 
@@ -463,29 +452,29 @@ These functions simply print $Config{ccflags} and $Config{ccdlflags}
 
 =item ccopts()
 
-This function combines perl_inc(), ccflags() and ccdlflags() into one.
+This function combines C<perl_inc()>, C<ccflags()> and C<ccdlflags()> into one.
 
 =item xsi_header()
 
-This function simply returns a string defining the same B<EXTERN_C> macro as
-B<perlmain.c> along with #including B<perl.h> and B<EXTERN.h>.  
+This function simply returns a string defining the same C<EXTERN_C> macro as
+F<perlmain.c> along with #including F<perl.h> and F<EXTERN.h>.
 
 =item xsi_protos(@modules)
 
-This function returns a string of B<boot_$ModuleName> prototypes for each @modules.
+This function returns a string of C<boot_$ModuleName> prototypes for each @modules.
 
 =item xsi_body(@modules)
 
-This function returns a string of calls to B<newXS()> that glue the module B<bootstrap>
-function to B<boot_ModuleName> for each @modules.
+This function returns a string of calls to C<newXS()> that glue the module I<bootstrap>
+function to I<boot_ModuleName> for each @modules.
 
-B<xsinit()> uses the xsi_* functions to generate most of its code.
+C<xsinit()> uses the xsi_* functions to generate most of its code.
 
 =back
 
 =head1 EXAMPLES
 
-For examples on how to use B<ExtUtils::Embed> for building C/C++ applications
+For examples on how to use C<ExtUtils::Embed> for building C/C++ applications
 with embedded perl, see L<perlembed>.
 
 =head1 SEE ALSO
@@ -494,10 +483,9 @@ L<perlembed>
 
 =head1 AUTHOR
 
-Doug MacEachern E<lt>F<dougm@osf.org>E<gt>
+Doug MacEachern E<lt>C<dougm@osf.org>E<gt>
 
-Based on ideas from Tim Bunce E<lt>F<Tim.Bunce@ig.co.uk>E<gt> and
-B<minimod.pl> by Andreas Koenig E<lt>F<k@anna.in-berlin.de>E<gt> and Tim Bunce.
+Based on ideas from Tim Bunce E<lt>C<Tim.Bunce@ig.co.uk>E<gt> and
+F<minimod.pl> by Andreas Koenig E<lt>C<k@anna.in-berlin.de>E<gt> and Tim Bunce.
 
 =cut
-
