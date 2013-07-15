feb38e3b9dba8f9f75fe6c737d7c4d99ff1aca46
David Mitchell <davem@iabyn.com>
2013-07-15 21:57:34 +0100

    pp_subst: don't use REXEC_COPY_STR on 2nd match
    pp_subst() sets the REXEC_COPY_STR flag on the first match. On the second
    and subsequent matches, it doesn't set it in two out three of the branches
    (including pp_susbstcont) where it calls CALLREGEXEC().
    The one place where it *does* set it is a (harmless) mistake, since regexec
    ignores REXEC_COPY_STR if REXEC_NOT_FIRST is set (which is it is, on all 3
    brnanches).
    
    So unset REXEC_COPY_STR in the third branch too, for consistency

--- pp_hot.c
+++ pp_hot.c
@@ -2283,7 +2283,6 @@ PP(pp_subst)
 	    PUSHSUBST(cx);
 	    RETURNOP(cPMOP->op_pmreplrootu.op_pmreplroot);
 	}
-	r_flags |= REXEC_IGNOREPOS | REXEC_NOT_FIRST;
 	first = TRUE;
 	do {
 	    if (iters++ > maxiters)
@@ -2322,7 +2321,7 @@ PP(pp_subst)
 	    if (once)
 		break;
 	} while (CALLREGEXEC(rx, s, strend, orig, s == m,
-			     TARG, NULL, r_flags));
+			     TARG, NULL, REXEC_NOT_FIRST|REXEC_IGNOREPOS));
 	sv_catpvn_nomg_maybeutf8(dstr, s, strend - s, DO_UTF8(TARG));
 
 	if (rpm->op_pmflags & PMf_NONDESTRUCT) {
