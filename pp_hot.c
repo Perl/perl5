c947cd8d4017ec6273b59c3f119e59654b960f34
David Mitchell <davem@iabyn.com>
2013-07-15 21:10:47 +0100

    pp_subst: remove one use of 'm' local var

--- pp_hot.c
+++ pp_hot.c
@@ -2200,11 +2200,10 @@ PP(pp_subst)
 	    }
 	    else if ((i = m - s)) {	/* faster from front */
 		d -= clen;
-		m = d;
 		Move(s, d - i, i, char);
 		sv_chop(TARG, d-i);
 		if (clen)
-		    Copy(c, m, clen, char);
+		    Copy(c, d, clen, char);
 	    }
 	    else if (clen) {
 		d -= clen;
