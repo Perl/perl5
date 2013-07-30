Perl/perl5

commit 89d3fa0ee43d5c7489581a62b3d662c316bfcb43

regcomp.c: Remove redundant code
This code is redundant, attempting to output what isn't returned in 'lv'
by reglcass_swash(), but that function makes sure that 'lv' contains
everything it should, so we ended up processing (and outputting) the
same data twice.

regcomp.c
@@ -14780,40 +14780,6 @@ Perl_regprop(pTHX_ const regexp *prog, SV *sv, const regnode *o)
                                            output */
 
 	    if (lv && lv != &PL_sv_undef) {
-		if (sw) {
-                    int rangestart = -1;
-                    int i;
-		    U8 s[UTF8_MAXBYTES_CASE+1];
-
-		    for (i = 0; i <= 256; i++) { /* Look at chars in bitmap */
-			uvchr_to_utf8(s, i);
-
-			if (i < 256
-                            && ! ANYOF_BITMAP_TEST(o, i)    /* Don't duplicate
-                                                               things already
-                                                               output as part
-                                                               of the bitmap */
-                            && swash_fetch(sw, s, TRUE))
-                        {
-			    if (rangestart == -1)
-				rangestart = i;
-			} else if (rangestart != -1) {
-                            byte_output = TRUE;
-			    if (i <= rangestart + 3)
-				for (; rangestart < i; rangestart++) {
-				    put_byte(sv, rangestart);
-				}
-			    else {
-				put_byte(sv, rangestart);
-				sv_catpvs(sv, "-");
-				put_byte(sv, i-1);
-			    }
-			    rangestart = -1;
-			}
-		    }
-		}
-
-		{
 		    char *s = savesvpv(lv);
 		    char * const origs = s;
 
@@ -14854,7 +14820,6 @@ Perl_regprop(pTHX_ const regexp *prog, SV *sv, const regnode *o)
 		out_dump:
 
 		    Safefree(origs);
-		}
 		SvREFCNT_dec_NN(lv);
 	    }
 	}

commit a42823ac3c233f0a9b8aefaac74a3b1e1600e6f6

regcomp.c: Properly declare variable with C89
Commit 89d3fa0ee43d5c7489581a62b3d662c316bfcb43 introduced a syntax
error under C89 compilers, as it removed braces so a declaration wasn't
the first thing in a block.  This adds a declaration in the proper
place.

regcomp.c
@@ -14786,6 +14786,7 @@ Perl_regprop(pTHX_ const regexp *prog, SV *sv, const regnode *o)
 	    sv_catpvs(sv, "{unicode_all}");
 	else if (ANYOF_NONBITMAP(o)) {
             SV *lv; /* Set if there is something outside the bit map. */
+            SV * sw;
 
             if (flags & ANYOF_NONBITMAP_NON_UTF8) {
                 sv_catpvs(sv, "{outside bitmap}");
@@ -14795,7 +14796,7 @@ Perl_regprop(pTHX_ const regexp *prog, SV *sv, const regnode *o)
             }
 
             /* Get the stuff that wasn't in the bitmap */
-	    SV * const sw = regclass_swash(prog, o, FALSE, &lv, NULL);
+	    sw = regclass_swash(prog, o, FALSE, &lv, NULL);
             bool byte_output = FALSE;   /* If something in the bitmap has been
                                            output */
 	    if (lv && lv != &PL_sv_undef) {