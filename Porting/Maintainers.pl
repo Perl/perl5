89e01640071c224e0f44fd5235cfabb0d602b175
Steve Hay <steve.m.hay@googlemail.com>
2013-07-30 14:19:20 +0100

    Upgrade Config-Perl-V from 0.18 to 0.19

--- Porting/Maintainers.pl
+++ Porting/Maintainers.pl
@@ -376,7 +376,7 @@ use File::Glob qw(:case);
 
     'Config::Perl::V' => {
         'MAINTAINER'   => 'hmbrand',
-        'DISTRIBUTION' => 'HMBRAND/Config-Perl-V-0.18.tgz',
+        'DISTRIBUTION' => 'HMBRAND/Config-Perl-V-0.19.tgz',
         'FILES'        => q[cpan/Config-Perl-V],
         'EXCLUDED'     => ['examples/show-v.pl'],
         'UPSTREAM'     => 'cpan',
