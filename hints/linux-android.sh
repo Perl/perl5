2f3174d73f4dec5e3423771f3073ea2867dbde1d
Brian Fraser <fraserbn@gmail.com>
2013-08-11 18:38:05 -0300

    hints/linux-androideabi.sh: use $chmod instead of plain chmod

--- hints/linux-android.sh
+++ hints/linux-android.sh
@@ -202,13 +202,13 @@ fi
 exit \$result_status
 
 EOF
-chmod a+rx $run
+$chmod a+rx $run
 
 $cat >$targetmkdir <<EOF
 #!/bin/sh
 adb -s $targethost shell "mkdir -p \$@"
 EOF
-chmod a+rx $targetmkdir
+$chmod a+rx $targetmkdir
 
 $cat >$to <<EOF
 #!/bin/sh
@@ -225,7 +225,7 @@ do
 done
 exit 0
 EOF
-chmod a+rx $to
+$chmod a+rx $to
 
 $cat >$from <<EOF
 #!/bin/sh
@@ -236,5 +236,5 @@ do
 done
 exit 0
 EOF
-chmod a+rx $from
+$chmod a+rx $from
 
