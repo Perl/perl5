Perl/perl5

commit 19bad6733a83fa572b0d6a19b4693ea22c241ab0

Don't call strlen() on CopFILE() for the unthreaded case, because the length can be obtained via CopFILESV().
p4raw-id: //depot/perl@32129

gv.c
@@ -170,10 +170,24 @@ GP *
 Perl_newGP(pTHX_ GV *const gv)
 {
     GP *gp;
+    U32 hash;
+#ifdef USE_ITHREADS
     const char *const file
 	= (PL_curcop && CopFILE(PL_curcop)) ? CopFILE(PL_curcop) : "";
-    STRLEN len = strlen(file);
-    U32 hash;
+    const STRLEN len = strlen(file);
+#else
+    SV *const temp_sv = CopFILESV(PL_curcop);
+    const char *file;
+    STRLEN len;
+
+    if (temp_sv) {
+	file = SvPVX(temp_sv);
+	len = SvCUR(temp_sv);
+    } else {
+	file = "";
+	len = 0;
+    }
+#endif
 
     PERL_HASH(hash, file, len);
 

toke.c
@@ -824,8 +824,18 @@ S_incline(pTHX_ const char *s)
     if (t - s > 0) {
 	const STRLEN len = t - s;
 #ifndef USE_ITHREADS
-	const char * const cf = CopFILE(PL_curcop);
-	STRLEN tmplen = cf ? strlen(cf) : 0;
+	SV *const temp_sv = CopFILESV(PL_curcop);
+	const char *cf;
+	STRLEN tmplen;
+
+	if (temp_sv) {
+	    cf = SvPVX(temp_sv);
+	    tmplen = SvCUR(temp_sv);
+	} else {
+	    cf = NULL;
+	    tmplen = 0;
+	}
+
 	if (tmplen > 7 && strnEQ(cf, "(eval ", 6)) {
 	    /* must copy *{"::_<(eval N)[oldfilename:L]"}
 	     * to *{"::_<newfilename"} */

commit cb0d3385a7bb30bc0aaa2ada8f6f21fe3554ec32

Restore NULL check to gv.c:newGP, removed by 19bad673
1df5f7c195 added checks to newGP to account for PL_curcop being NULL.
19bad673 inadvertently removed the NULL check for non-threaded builds,
except in one spot.

Since S_cop_free now sets PL_curcop to NULL as of the previous commit
(something 1df5f7c195 was meant to do but didn’t), this check is a
good idea, even though PL_curcop is never NULL here.

See also <20130805200313.GS3729@plum.flirble.org>.

gv.c
@@ -191,13 +191,17 @@ Perl_newGP(pTHX_ GV *const gv)
 	len = 0;
     }
 #else
-    if(PL_curcop)
+    if(PL_curcop) {
 	gp->gp_line = CopLINE(PL_curcop); /* 0 otherwise Newxz */
-    filegv = CopFILEGV(PL_curcop);
-    if (filegv) {
-	file = GvNAME(filegv)+2;
-	len = GvNAMELEN(filegv)-2;
-    } else {
+	filegv = CopFILEGV(PL_curcop);
+	if (filegv) {
+	    file = GvNAME(filegv)+2;
+	    len = GvNAMELEN(filegv)-2;
+	}
+	else goto no_file;
+    }
+    else {
+	no_file:
 	file = "";
 	len = 0;
     }