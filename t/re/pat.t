Perl/perl5

commit c30fc27b4df65a43710b25dd1d2a57d78ee2fe33

Handle /[#]/ and /[(?#]/ with code blocks
This is a regression in 5.18.0.

In something like /[#](?{})/x, the perl toker incorrectly sees the '#' as a
comment and skips the code block without parsing it.

t/re/re_tests
@@ -1764,3 +1764,15 @@ A+?(*PRUNE)BC(?{})	AAABC	y	$&	ABC
 A+(*THEN)BC(?{})	AAABC	y	$&	AAABC
 A+(*PRUNE)BC(?{})	AAABC	y	$&	AAABC
 # vim: softtabstop=0 noexpandtab
+/[#]/	a#b	y	$&	#
+/[#]b/	a#b	y	$&	#b
+/[#]/x	a#b	y	$&	#
+/[#]b/x	a#b	y	$&	#b
+/[#](?{})/x	a#b	y	$&	#
+/[#](??{'b'})/x	a#b	y	$&	#b
+/(?#)(?{})b/	a#b	y	$&	b
+/(?#)(??{'b'})/	a#b	y	$&	b
+/[(?#](?{})b/	a#b	y	$&	#b
+/[(?#](??{'b'})/	a#b	y	$&	#b
+/(?#)(?{})b/x	a#b	y	$&	b
+/(?#)(??{'b'})/x	a#b	y	$&	b

toke.c
@@ -3285,12 +3285,12 @@ S_scan_const(pTHX_ char *start)
 	 * char, which will be done separately.
 	 * Stop on (?{..}) and friends */
 
-	else if (*s == '(' && PL_lex_inpat && s[1] == '?') {
+	else if (*s == '(' && PL_lex_inpat && s[1] == '?' && !in_charclass) {
 	    if (s[2] == '#') {
 		while (s+1 < send && *s != ')')
 		    *d++ = NATIVE_TO_NEED(has_utf8,*s++);
 	    }
-	    else if (!PL_lex_casemods && !in_charclass &&
+	    else if (!PL_lex_casemods &&
 		     (    s[2] == '{' /* This should match regcomp.c */
 		      || (s[2] == '?' && s[3] == '{')))
 	    {
@@ -3299,7 +3299,7 @@ S_scan_const(pTHX_ char *start)
 	}
 
 	/* likewise skip #-initiated comments in //x patterns */
-	else if (*s == '#' && PL_lex_inpat &&
+	else if (*s == '#' && PL_lex_inpat && !in_charclass &&
 	  ((PMOP*)PL_lex_inpat)->op_pmflags & RXf_PMf_EXTENDED) {
 	    while (s+1 < send && *s != '\n')
 		*d++ = NATIVE_TO_NEED(has_utf8,*s++);

commit 61965f1d7ce73bac298dbea9d5df1d787777d222

[perl #45667] Apply tests from #119125 in reverse
Ticket #119125 represents the breakage caused by c30fc27b4df65a, which
fixed #45667 (/[#]$not_interpolated/x; a regression from 5.000) as a
side-effect of fixing /[#](?{this is not a code block})/x (a regres-
sion from 5.16.0).

It was backported to maint-5.18, which broke several modules, because
bug #45667 is *old*, and many modules depend on it.

So 02682386fe3e on the maint branch modified the fix to apply only to
code blocks, and added tests to make sure that #45667 wasn’t fixed.

#45667 is fixed in blead, but is untested, so grab the tests from
02682386fe3e and reverse the logic of the second one.

t/re/pat.t
@@ -20,7 +20,7 @@ BEGIN {
     require './test.pl';
 }
 
-plan tests => 694;  # Update this when adding/deleting tests.
+plan tests => 696;  # Update this when adding/deleting tests.
 
 run_tests() unless caller;
 
@@ -1450,6 +1450,18 @@ EOP
 	is($res, "ACAD");
     }
 
+
+    {
+	# RT #45667
+	# /[#$x]/x didn't interpolate the var $x.
+	my $b = 'cd';
+	my $s = 'abcd$%#&';
+	$s =~ s/[a#$b%]/X/g;
+	is ($s, 'XbXX$XX&', 'RT #45667 without /x');
+	$s = 'abcd$%#&';
+	$s =~ s/[a#$b%]/X/gx;
+	is ($s, 'XbXX$XX&', 'RT #45667 with /x');
+    }
 
 } # End of sub run_tests
 