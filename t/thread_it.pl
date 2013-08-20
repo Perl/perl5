c534a167485cd439996d31212be053a2e31b41d7
Father Chrysostomos <sprout@cpan.org>
2013-08-20 00:38:57 -0700

    thread_it.pl: Tweak stack sizes
    The stack size appropriate to HP-UX is too small for some systems,
    where the default is fine.  (See <20130816182909.GA14081@iabyn.com>.)
    On Mac OS X, the size appropriate to HP-UX is too small, as is the
    default.

--- t/thread_it.pl
+++ t/thread_it.pl
@@ -29,8 +29,10 @@ note('running tests in a new thread');
 # Currently 59*4096 is the minimum stack size to just get t/re/pat_thr.t to
 # pass on HP-UX 64bit PA-RISC. The test for capture buffers (eg \87)
 # recurses heavily, and busts the default stack size (65536 on PA-RISC)
+# On Mac OS X under gcc and g++, the default stack size is also too small.
 my $curr = threads->create({
-                            stack_size => 524288,
+                            stack_size => $^O eq 'hpux'   ? 524288 :
+                                          $^O eq 'darwin' ? 847873 : 0,
                            }, sub {
 			       run_tests();
 			       return defined &curr_test ? curr_test() : ()
