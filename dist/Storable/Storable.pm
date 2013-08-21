00b91e958b80faed996e6b529c23c0ed8ead56c9
Father Chrysostomos <sprout@cpan.org>
2013-08-20 20:38:08 -0700

    Increase $Storable::VERSION to 2.47

--- dist/Storable/Storable.pm
+++ dist/Storable/Storable.pm
@@ -22,7 +22,7 @@ package Storable; @ISA = qw(Exporter);
 
 use vars qw($canonical $forgive_me $VERSION);
 
-$VERSION = '2.46';
+$VERSION = '2.47';
 
 BEGIN {
     if (eval { local $SIG{__DIE__}; require Log::Agent; 1 }) {
