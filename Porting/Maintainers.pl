014f01ad4261ed059a0141e4a741dcb192ec0d2b
Steve Hay <steve.m.hay@googlemail.com>
2013-08-21 12:39:09 +0100

    Module::CoreList 2.97 is now on CPAN

--- Porting/Maintainers.pl
+++ Porting/Maintainers.pl
@@ -1198,7 +1198,7 @@ use File::Glob qw(:case);
 
     'Module::CoreList' => {
         'MAINTAINER'   => 'bingos',
-        'DISTRIBUTION' => 'BINGOS/Module-CoreList-2.96.tar.gz',
+        'DISTRIBUTION' => 'BINGOS/Module-CoreList-2.97.tar.gz',
         'FILES'        => q[dist/Module-CoreList],
         'UPSTREAM'     => 'blead',
     },
