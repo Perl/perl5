bf4588d0c394f60786161c36506f799e7007b077
Zefram <zefram@fysh.org>
2013-08-22 22:57:01 +0100

    detect Carp/Carp::Heavy version mismatch
    [rt.cpan.org #79649]  If an old Carp, requiring old-style Carp::Heavy
    that provides subroutines, gets a newer stub-style Carp::Heavy, due to
    @INC having changed, the resulting error messages were not awesome.

--- dist/Carp/lib/Carp/Heavy.pm
+++ dist/Carp/lib/Carp/Heavy.pm
@@ -4,6 +4,11 @@ use Carp ();
 
 our $VERSION = '1.32';
 
+my $cv = defined($Carp::VERSION) ? $Carp::VERSION : "undef";
+if($cv ne $VERSION) {
+	die "Version mismatch between Carp $cv ($INC{q(Carp.pm)}) and Carp::Heavy $VERSION ($INC{q(Carp/Heavy.pm)}).  Did you alter \@INC after Carp was loaded?\n";
+}
+
 1;
 
 # Most of the machinery of Carp used to be here.
