xiaobohaha/perl

Stop pos() from being confused by changing utf8ness
The value of pos() is stored as a byte offset.  If it is stored on a
tied variable or a reference (or glob), then the stringification could
change, resulting in pos() now pointing to a different character off-
set or pointing to the middle of a character:

$ ./perl -Ilib -le '$x = bless [], chr 256; pos $x=1; bless $x, a; print pos $x'
2
$ ./perl -Ilib -le '$x = bless [], chr 256; pos $x=1; bless $x, "\x{1000}"; print pos $x'
Malformed UTF-8 character (unexpected end of string) in match position at -e line 1.
0

So pos() should be stored as a character offset.

The regular expression engine expects byte offsets always, so allow it
to store bytes when possible (a pure non-magical string) but use char-
acters otherwise.

This does result in more complexity than I should like, but the alter-
native (always storing a character offset) would slow down regular
expressions, which is a big no-no.

embed.fnc
@@ -2654,4 +2654,9 @@ op	|void	|populate_isa	|NN const char *name|STRLEN len|...
 Xop	|bool	|feature_is_enabled|NN const char *const name \
 		|STRLEN namelen
 
+: Some static inline functions need predeclaration because they are used
+: inside other static inline functions.
+i	|STRLEN	|sv_or_pv_pos_u2b|NN SV *sv|NN const char *pv|STRLEN pos \
+				 |NULLOK STRLEN *lenp
+
 : ex: set ts=8 sts=4 sw=4 noet:

Perl/perl5

embed.fnc
@@ -2665,4 +2665,9 @@ op	|void	|populate_isa	|NN const char *name|STRLEN len|...
 Xop	|bool	|feature_is_enabled|NN const char *const name \
 		|STRLEN namelen
 
+: Some static inline functions need predeclaration because they are used
+: inside other static inline functions.
+Ei	|STRLEN	|sv_or_pv_pos_u2b|NN SV *sv|NN const char *pv|STRLEN pos \
+				 |NULLOK STRLEN *lenp
+
 : ex: set ts=8 sts=4 sw=4 noet: