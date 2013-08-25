dd4728cdd134e435e22c1a20712eca96c65a3163
Father Chrysostomos <sprout@cpan.org>
2013-08-25 13:20:57 -0700

    Increase $re::VERSION to 0.26

--- ext/re/re.pm
+++ ext/re/re.pm
@@ -4,7 +4,7 @@ package re;
 use strict;
 use warnings;
 
-our $VERSION     = "0.25";
+our $VERSION     = "0.26";
 our @ISA         = qw(Exporter);
 our @EXPORT_OK   = ('regmust',
                     qw(is_regexp regexp_pattern
