1522d188b83c42df1e2b65084cfe4fb70415b67b
Father Chrysostomos <sprout@cpan.org>
2013-08-25 00:13:33 -0700

    Increase $mro::VERSION to 1.14

--- ext/mro/mro.pm
+++ ext/mro/mro.pm
@@ -12,7 +12,7 @@ use warnings;
 
 # mro.pm versions < 1.00 reserved for MRO::Compat
 #  for partial back-compat to 5.[68].x
-our $VERSION = '1.13';
+our $VERSION = '1.14';
 
 sub import {
     mro::set_mro(scalar(caller), $_[1]) if $_[1];
