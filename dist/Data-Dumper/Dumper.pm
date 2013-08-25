1d84dc32c2522c8186d0d97df004a5d43a93541b
Father Chrysostomos <sprout@cpan.org>
2013-08-25 00:10:16 -0700

    Increase $Data::Dumper::VERSION to 2.149

--- dist/Data-Dumper/Dumper.pm
+++ dist/Data-Dumper/Dumper.pm
@@ -10,7 +10,7 @@
 package Data::Dumper;
 
 BEGIN {
-    $VERSION = '2.148'; # Don't forget to set version and release
+    $VERSION = '2.149'; # Don't forget to set version and release
 }               # date in POD below!
 
 #$| = 1;
@@ -1400,7 +1400,7 @@ modify it under the same terms as Perl itself.
 
 =head1 VERSION
 
-Version 2.145  (March 15 2013))
+Version 2.149  (September 20 2013)
 
 =head1 SEE ALSO
 
