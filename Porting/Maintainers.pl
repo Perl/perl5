dd741cc9e27c317b52b61f1f20bb1d88e66be20c
Steve Hay <steve.m.hay@googlemail.com>
2013-09-12 21:57:09 +0100

    Upgrade perlfaq from version 5.0150043 to 5.0150044

--- Porting/Maintainers.pl
+++ Porting/Maintainers.pl
@@ -728,7 +728,7 @@ use File::Glob qw(:case);
 
     'perlfaq' => {
         'MAINTAINER'   => 'perlfaq',
-        'DISTRIBUTION' => 'LLAP/perlfaq-5.0150043.tar.gz',
+        'DISTRIBUTION' => 'LLAP/perlfaq-5.0150044.tar.gz',
         'FILES'        => q[cpan/perlfaq],
         'EXCLUDED'     => [
             qw( t/release-pod-syntax.t
--- cpan/perlfaq/lib/perlfaq.pm
+++ cpan/perlfaq/lib/perlfaq.pm
@@ -1,6 +1,6 @@
 package perlfaq;
 {
-  $perlfaq::VERSION = '5.0150043';
+  $perlfaq::VERSION = '5.0150044';
 }
 
 0; # not is it supposed to be loaded
