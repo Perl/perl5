f2a7d0fc9f4d388e4bb0628fba2476c2789721c8
Karl Williamson <public@khwilliamson.com>
2013-10-16 21:53:23 -0600

    utf8.c: White-space only
    This outdents code to the proper level given that the surrounding block
    has been removed.

--- utf8.c
+++ utf8.c
@@ -3428,15 +3428,15 @@ S_swash_scan_list_line(pTHX_ U8* l, U8* const lend, UV* min, UV* max, UV* val,
 	if (wants_value) {
 	    if (isBLANK(*l)) {
 		++l;
-		    flags = PERL_SCAN_SILENT_ILLDIGIT
-			| PERL_SCAN_DISALLOW_PREFIX
-			| PERL_SCAN_SILENT_NON_PORTABLE;
-		    numlen = lend - l;
-		    *val = grok_hex((char *)l, &numlen, &flags, NULL);
-		    if (numlen)
-			l += numlen;
-		    else
-			*val = 0;
+                flags = PERL_SCAN_SILENT_ILLDIGIT
+                    | PERL_SCAN_DISALLOW_PREFIX
+                    | PERL_SCAN_SILENT_NON_PORTABLE;
+                numlen = lend - l;
+                *val = grok_hex((char *)l, &numlen, &flags, NULL);
+                if (numlen)
+                    l += numlen;
+                else
+                    *val = 0;
 	    }
 	    else {
 		*val = 0;
