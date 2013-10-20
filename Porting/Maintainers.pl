bd99d29dcdb67c72f3474cdafe9b30d72907844c
Steve Hay <steve.m.hay@googlemail.com>
2013-10-20 16:39:13 +0100

    Module::CoreList 3.00 is now on CPAN

--- Porting/Maintainers.pl
+++ Porting/Maintainers.pl
@@ -814,7 +814,7 @@ use File::Glob qw(:case);
     },
 
     'Module::CoreList' => {
-        'DISTRIBUTION' => 'BINGOS/Module-CoreList-2.99.tar.gz',
+        'DISTRIBUTION' => 'BINGOS/Module-CoreList-3.00.tar.gz',
         'FILES'        => q[dist/Module-CoreList],
     },
 
