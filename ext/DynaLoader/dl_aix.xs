Perl/perl5

commit 860b3d937d9b004a7de4b684765ab75006c71f00

DynaLoader: silence some compiler warnings

ext/DynaLoader/dl_aix.xs
@@ -772,6 +772,7 @@ dl_error()
 void
 CLONE(...)
     CODE:
+    PERL_UNUSED_VAR(items);
     MY_CXT_CLONE;
 
     /* MY_CXT_CLONE just does a memcpy on the whole structure, so to avoid

ext/DynaLoader/dl_dllload.xs
@@ -197,6 +197,7 @@ dl_error()
 void
 CLONE(...)
     CODE:
+    PERL_UNUSED_VAR(items);
     MY_CXT_CLONE;
 
     /* MY_CXT_CLONE just does a memcpy on the whole structure, so to avoid

ext/DynaLoader/dl_dlopen.xs
@@ -271,6 +271,7 @@ dl_error()
 void
 CLONE(...)
     CODE:
+    PERL_UNUSED_VAR(items);
     MY_CXT_CLONE;
 
     /* MY_CXT_CLONE just does a memcpy on the whole structure, so to avoid

ext/DynaLoader/dl_dyld.xs
@@ -226,6 +226,7 @@ dl_error()
 void
 CLONE(...)
     CODE:
+    PERL_UNUSED_VAR(items);
     MY_CXT_CLONE;
 
     /* MY_CXT_CLONE just does a memcpy on the whole structure, so to avoid

ext/DynaLoader/dl_hpux.xs
@@ -182,6 +182,7 @@ dl_error()
 void
 CLONE(...)
     CODE:
+    PERL_UNUSED_VAR(items);
     MY_CXT_CLONE;
 
     /* MY_CXT_CLONE just does a memcpy on the whole structure, so to avoid

ext/DynaLoader/dl_next.xs
@@ -329,6 +329,7 @@ dl_error()
 void
 CLONE(...)
     CODE:
+    PERL_UNUSED_VAR(items);
     MY_CXT_CLONE;
 
     /* MY_CXT_CLONE just does a memcpy on the whole structure, so to avoid

ext/DynaLoader/dl_symbian.xs
@@ -226,6 +226,7 @@ dl_error()
 void
 CLONE(...)
     CODE:
+    PERL_UNUSED_VAR(items);
     MY_CXT_CLONE;
 
     /* MY_CXT_CLONE just does a memcpy on the whole structure, so to avoid

ext/DynaLoader/dl_vms.xs
@@ -359,6 +359,7 @@ dl_error()
 void
 CLONE(...)
     CODE:
+    PERL_UNUSED_VAR(items);
     MY_CXT_CLONE;
 
     /* MY_CXT_CLONE just does a memcpy on the whole structure, so to avoid

commit 3bd46979e0ad1e0a1f7feeac9dd3cc3c1bbcc75e

Dynaloader: fix build issue
My commit from yesterday, 860b3d937d9b004a7de4b684765ab75006c71f00,
inadvertently included c99-ish mixed declarations and code.

ext/DynaLoader/dl_aix.xs
@@ -772,8 +772,9 @@ dl_error()
 void
 CLONE(...)
     CODE:
-    PERL_UNUSED_VAR(items);
     MY_CXT_CLONE;
+
+    PERL_UNUSED_VAR(items);
 
     /* MY_CXT_CLONE just does a memcpy on the whole structure, so to avoid
      * using Perl variables that belong to another thread, we create our 

ext/DynaLoader/dl_dllload.xs
@@ -197,8 +197,9 @@ dl_error()
 void
 CLONE(...)
     CODE:
-    PERL_UNUSED_VAR(items);
     MY_CXT_CLONE;
+
+    PERL_UNUSED_VAR(items);
 
     /* MY_CXT_CLONE just does a memcpy on the whole structure, so to avoid
      * using Perl variables that belong to another thread, we create our 

ext/DynaLoader/dl_dlopen.xs
@@ -271,8 +271,9 @@ dl_error()
 void
 CLONE(...)
     CODE:
-    PERL_UNUSED_VAR(items);
     MY_CXT_CLONE;
+
+    PERL_UNUSED_VAR(items);
 
     /* MY_CXT_CLONE just does a memcpy on the whole structure, so to avoid
      * using Perl variables that belong to another thread, we create our 

ext/DynaLoader/dl_dyld.xs
@@ -226,8 +226,9 @@ dl_error()
 void
 CLONE(...)
     CODE:
-    PERL_UNUSED_VAR(items);
     MY_CXT_CLONE;
+
+    PERL_UNUSED_VAR(items);
 
     /* MY_CXT_CLONE just does a memcpy on the whole structure, so to avoid
      * using Perl variables that belong to another thread, we create our 

ext/DynaLoader/dl_hpux.xs
@@ -182,8 +182,9 @@ dl_error()
 void
 CLONE(...)
     CODE:
-    PERL_UNUSED_VAR(items);
     MY_CXT_CLONE;
+
+    PERL_UNUSED_VAR(items);
 
     /* MY_CXT_CLONE just does a memcpy on the whole structure, so to avoid
      * using Perl variables that belong to another thread, we create our 

ext/DynaLoader/dl_next.xs
@@ -329,8 +329,9 @@ dl_error()
 void
 CLONE(...)
     CODE:
-    PERL_UNUSED_VAR(items);
     MY_CXT_CLONE;
+
+    PERL_UNUSED_VAR(items);
 
     /* MY_CXT_CLONE just does a memcpy on the whole structure, so to avoid
      * using Perl variables that belong to another thread, we create our 

ext/DynaLoader/dl_symbian.xs
@@ -226,8 +226,9 @@ dl_error()
 void
 CLONE(...)
     CODE:
-    PERL_UNUSED_VAR(items);
     MY_CXT_CLONE;
+
+    PERL_UNUSED_VAR(items);
 
     /* MY_CXT_CLONE just does a memcpy on the whole structure, so to avoid
      * using Perl variables that belong to another thread, we create our 

ext/DynaLoader/dl_vms.xs
@@ -359,8 +359,9 @@ dl_error()
 void
 CLONE(...)
     CODE:
-    PERL_UNUSED_VAR(items);
     MY_CXT_CLONE;
+
+    PERL_UNUSED_VAR(items);
 
     /* MY_CXT_CLONE just does a memcpy on the whole structure, so to avoid
      * using Perl variables that belong to another thread, we create our 

ext/DynaLoader/dl_win32.xs
@@ -199,8 +199,9 @@ dl_error()
 void
 CLONE(...)
     CODE:
-    PERL_UNUSED_VAR(items);
     MY_CXT_CLONE;
+
+    PERL_UNUSED_VAR(items);
 
     /* MY_CXT_CLONE just does a memcpy on the whole structure, so to avoid
      * using Perl variables that belong to another thread, we create our 