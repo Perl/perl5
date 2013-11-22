5beb00b04461e0264795d17194866774957fba9b
Chris 'BinGOs' Williams <chris@bingosnet.co.uk>
2013-11-22 14:37:33 +0000

    Module-CoreList on CPAN is 3.01

--- Porting/Maintainers.pl
+++ Porting/Maintainers.pl
@@ -803,7 +803,7 @@ use File::Glob qw(:case);
     },
 
     'Module::CoreList' => {
-        'DISTRIBUTION' => 'BINGOS/Module-CoreList-3.00.tar.gz',
+        'DISTRIBUTION' => 'BINGOS/Module-CoreList-3.01.tar.gz',
         'FILES'        => q[dist/Module-CoreList],
     },
 
