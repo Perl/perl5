Perl/perl5

commit 2685dc2d9a9865ac92971890d053145da562c094

Make (??{$tied_ovrld}) see the right $1
I can return $1 from a regexp code block and it refers to the last
match *within* the block:

"aab" =~ /(a)((??{"b" =~ m|(.)|; $1}))/;
print "[$1 $2]\n";

Output:
[a b]

Even via a tied variable’s FETCH method:

sub ReEvalTieTest::TIESCALAR {bless[], "ReEvalTieTest"}
sub ReEvalTieTest::FETCH { "$1" }
tie my $t, "ReEvalTieTest";
"aab" =~ /(a)((??{"b" =~ m|(.)|; $t}))/;
print "[$1 $2]\n";

Output:
[a b]

But not if I assign a reference to an overloaded object to the tied
variable first:

sub ReEvalTieTest::TIESCALAR {bless[], "ReEvalTieTest"}
sub ReEvalTieTest::STORE{}
sub ReEvalTieTest::FETCH { "$1" }
tie my $t, "ReEvalTieTest";
{ package o; use overload '""'=>sub { "abc" } }
$t = bless [], "o";
"aab" =~ /(a)((??{"b" =~ m|(.)|; $t}))/;
print "[$1 $2]\n";

Output:
[a a]

$1 now refers to the outer pattern, not the inner pattern.

The code that handles the return value of code blocks was not check-
ing get-magic before overloading.

This commit fixes it to do that.

regexec.c
@@ -5103,6 +5103,9 @@ S_regmatch(pTHX_ regmatch_info *reginfo, char *startpos, regnode *prog)
 		else {                   /*  /(??{})  */
 		    /*  if its overloaded, let the regex compiler handle
 		     *  it; otherwise extract regex, or stringify  */
+		    const bool gmg = cBOOL(SvGMAGICAL(ret));
+		    if (gmg)
+			ret = sv_mortalcopy(ret);
 		    if (!SvAMAGIC(ret)) {
 			SV *sv = ret;
 			if (SvROK(sv))
@@ -5115,9 +5118,9 @@ S_regmatch(pTHX_ regmatch_info *reginfo, char *startpos, regnode *prog)
 				re_sv = (REGEXP *) mg->mg_obj;
 			}
 
-			/* force any magic, undef warnings here */
+			/* force any undef warnings here */
 			if (!re_sv) {
-			    ret = sv_mortalcopy(ret);
+			    if (!gmg) ret = sv_mortalcopy(ret);
 			    (void) SvPV_force_nolen(ret);
 			}
 		    }

t/re/pat_re_eval.t
@@ -22,7 +22,7 @@ BEGIN {
 }
 
 
-plan tests => 522;  # Update this when adding/deleting tests.
+plan tests => 523;  # Update this when adding/deleting tests.
 
 run_tests() unless caller;
 
@@ -1191,6 +1191,17 @@ sub run_tests {
 	   '(??{$x}) does not leak cached qr to (??{\$x}) (match)';
 	is scalar "abcabc" =~ /(??{$x})(??{$y})/, "",
 	   '(??{$x}) does not leak cached qr to (??{\$x}) (no match)';
+    }
+
+    {
+	sub ReEvalTieTest::TIESCALAR {bless[], "ReEvalTieTest"}
+	sub ReEvalTieTest::STORE{}
+	sub ReEvalTieTest::FETCH { "$1" }
+	tie my $t, "ReEvalTieTest";
+	$t = bless [], "o";
+	"aab" =~ /(a)((??{"b" =~ m|(.)|; $t}))/;
+	is "[$1 $2]", "[a b]",
+	   '(??{$tied_former_overload}) sees the right $1 in FETCH';
     }
 
 } # End of sub run_tests

commit 10beeb31a54ccb7c367cf6c53bbacc7ca2f4b2ba

Test that (??{$pvlvregexp}) does not recompile the regexp
2685dc2d9a accidentally fixed this.  Oops! :-)

t/re/recompile.t
@@ -22,7 +22,7 @@ BEGIN {
 }
 
 
-plan tests => 46;
+plan tests => 48;
 
 my $results = runperl(
 			switches => [ '-Dr' ],
@@ -209,4 +209,11 @@ CODE
 
 comp_n(2, <<'CODE', '(??{number})');
 "bb" =~ /(??{123})/;
+CODE
+
+comp_n(2, <<'CODE', '(??{$pvlv_regexp})');
+sub {
+   $_[0] = ${qr/abc/};
+  "bb" =~ /(??{$_[0]})/;
+}->($_[0]);
 CODE