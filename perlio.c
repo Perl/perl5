e4fb8e2ded39e7068dc433a1346c20813c8c8e5c
Nicholas Clark <nick@ccl4.org>
2013-12-08 19:15:25 +0100

    Fix sfio breakage in PerlIO_findFILE() caused by commit de009b76d60bdeb8.
    One hunk of commit de009b76d60bdeb8 (April 2005) makes a variable const,
    which is then assigned to. This is in sfio-specific code, so was not noticed.
    This change was merged to maint-5.8 as part of commit f1c3982b668c9e30 (Sep
    2005).

--- perlio.c
+++ perlio.c
@@ -416,7 +416,7 @@ FILE *
 PerlIO_findFILE(PerlIO *pio)
 {
     const int fd = PerlIO_fileno(pio);
-    FILE * const f = fdopen(fd, "r+");
+    FILE * f = fdopen(fd, "r+");
     PerlIO_flush(pio);
     if (!f && errno == EINVAL)
 	f = fdopen(fd, "w");
