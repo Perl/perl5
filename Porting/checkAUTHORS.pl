3eab96cadf1e1423d9c6e7051d14906c9d419edb
Karl Williamson <khw@cpan.org>
2014-01-04 19:25:22 +0100

    Change preferred email for Karl Williamson in AUTHORS

--- Porting/checkAUTHORS.pl
+++ Porting/checkAUTHORS.pl
@@ -799,7 +799,8 @@ pne\100cpan.org                         philip.newton\100gmx.net
 +                                       philip.newton\100datenrevision.de
 +                                       pnewton\100gmx.de
 pprymmer\100factset.com                 pvhp\100forte.com
-public\100khwilliamson.com              khw\100karl.(none)
+khw\100cpan.org                         khw\100karl.(none)
++                                       public\100khwilliamson.com
 +                                       khw\100khw-desktop.(none)
 
 radu\100netsoft.ro                      rgreab\100fx.ro
