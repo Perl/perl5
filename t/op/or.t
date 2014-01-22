Perl/perl5

commit 2e73d70e524ca7e5c7c5ae9b0017c31f6f6d4d9a

Propagate lvalue context to && and ||
I did this for the last statement in an lvalue sub (and the argument
to ‘return’ in an lvalue sub) in commit 2ec7f6f2428.

It turns out that it needs to apply in other lvaluish contexts, too:

$ ./perl -Ilib -le 'for($a||$b){$_++} print $b'
1
$ ./perl -Ilib -le 'for(pos $a || pos $b){$_++} print pos $b'
Modification of a read-only value attempted at -e line 1.

If I can assign to $b through this construct, then why not pos?

op.c
@@ -2298,10 +2298,8 @@ Perl_op_lvalue_flags(pTHX_ OP *o, I32 type, U32 flags)
 
     case OP_AND:
     case OP_OR:
-	if (type == OP_LEAVESUBLV) {
-	    op_lvalue(cLOGOPo->op_first,	     type);
-	    op_lvalue(cLOGOPo->op_first->op_sibling, type);
-	}
+	op_lvalue(cLOGOPo->op_first,		 type);
+	op_lvalue(cLOGOPo->op_first->op_sibling, type);
 	goto nomod;
     }
 

t/op/or.t
@@ -25,7 +25,7 @@ sub FETCH {
 package main;
 require './test.pl';
 
-plan( tests => 8 );
+plan( tests => 9 );
 
 
 my ($a, $b, $c);
@@ -66,3 +66,9 @@ $c = $a || $b;
     local $TODO = 'Double FETCH';
     is($c, 1,   '   $tied || $var');
 }
+
+$y = " ";
+for (pos $x || pos $y) {
+    eval { $_++ };
+}
+is(pos($y) || $@, 1, "|| propagates lvaluish context");

commit 1fc1f56336de4ce41549be6d04b6187983064f58

[perl #120374] Stop for($h{k}||'') from vivifying
Commit 2e73d70e52 broke this (made it vivify) by propagating lvalue
context to the branches of || and && (to fix another bug).  It broke
App::JobLog as a result.

Because foreach does not do defelem magic (i.e., it vivifies), this
ends up extending vivification to happen where it did not before.

Fixing foreach to do defelem magic (create ‘deferred element’ scalars,
the way sub calls do, to avoid vivifying immediately) would be another
way to fix this.  (And I plan to do it soon anyway.  That is the topic
of ticket #115194.)

However, to create a deferred element on the lhs of || would be a
waste, since the newly-created SV will evaluate to false, and will be
thrown away immediately.

So, when the lhs of || is helem or aelem, we can simply refram from
applying lvalue context, and the result is exactly the same as we
would get from applying defelem magic, but faster.

op.c
@@ -2323,9 +2323,13 @@ Perl_op_lvalue_flags(pTHX_ OP *o, I32 type, U32 flags)
     case OP_COREARGS:
 	return o;
 
-    case OP_AND:
     case OP_OR:
+	if (cLOGOPo->op_first->op_type == OP_AELEM
+	 || cLOGOPo->op_first->op_type == OP_HELEM)
+	    goto rhs;
+    case OP_AND:
 	op_lvalue(cLOGOPo->op_first,		 type);
+      rhs:
 	op_lvalue(cLOGOPo->op_first->op_sibling, type);
 	goto nomod;
     }

t/op/or.t
@@ -25,7 +25,7 @@ sub FETCH {
 package main;
 require './test.pl';
 
-plan( tests => 11 );
+plan( tests => 13 );
 
 
 my ($a, $b, $c);
@@ -71,7 +71,18 @@ $y = " ";
 for (pos $x || pos $y) {
     eval { $_++ };
 }
-is(pos($y) || $@, 1, "|| propagates lvaluish context");
+is(pos($y) || $@, 1, "|| propagates lvaluish context to its rhs");
+
+$x = "  ";
+pos $x = 1;
+for (pos $x || pos $y) {
+    eval { $_++ };
+}
+is(pos($x) || $@, 2, "|| propagates lvaluish context to its lhs");
+
+for ($h{k} || '') {}
+ok(!exists $h{k},
+  "|| does not propagate lvaluish cx to a subscript on its lhs");
 
 my $aa, $bb, $cc;
 $bb = 1;