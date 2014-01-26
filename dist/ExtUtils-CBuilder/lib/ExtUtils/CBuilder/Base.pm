73bacc4ae164fc3d2a86d821346c3c2e3f629f73
Brian Fraser <fraserbn@gmail.com>
2014-01-26 13:07:49 -0300

    Up the version of ExtUtils::CBuilder

--- dist/ExtUtils-CBuilder/lib/ExtUtils/CBuilder/Base.pm
+++ dist/ExtUtils-CBuilder/lib/ExtUtils/CBuilder/Base.pm
@@ -10,7 +10,7 @@ use IPC::Cmd qw(can_run);
 use File::Temp qw(tempfile);
 
 use vars qw($VERSION);
-$VERSION = '0.280213';
+$VERSION = '0.280214';
 
 # More details about C/C++ compilers:
 # http://developers.sun.com/sunstudio/documentation/product/compiler.jsp
--- dist/ExtUtils-CBuilder/lib/ExtUtils/CBuilder/Platform/Windows.pm
+++ dist/ExtUtils-CBuilder/lib/ExtUtils/CBuilder/Platform/Windows.pm
@@ -10,7 +10,7 @@ use ExtUtils::CBuilder::Base;
 use IO::File;
 
 use vars qw($VERSION @ISA);
-$VERSION = '0.280213';
+$VERSION = '0.280214';
 @ISA = qw(ExtUtils::CBuilder::Base);
 
 =begin comment
