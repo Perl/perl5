Perl/perl5

commit 31f05a37c4e9c37a7263491f2fc0237d836e1a80

Work properly under UTF-8 LC_CTYPE locales
This large (sorry, I couldn't figure out how to meaningfully split it
up) commit causes Perl to fully support LC_CTYPE operations (case
changing, character classification) in UTF-8 locales.

As a side effect it resolves [perl #56820].

The basics are easy, but there were a lot of details, and one
troublesome edge case discussed below.

What essentially happens is that when the locale is changed to a UTF-8
one, a global variable is set TRUE (FALSE when changed to a non-UTF-8
locale).  Within the scope of 'use locale', this variable is checked,
and if TRUE, the code that Perl uses for non-locale behavior is used
instead of the code for locale behavior.  Since Perl's internal
representation is UTF-8, we get UTF-8 behavior for a UTF-8 locale.

More work had to be done for regular expressions.  There are three
cases.

1) The character classes \w, [[:punct:]] needed no extra work, as
the changes fall out from the base work.

2) Strings that are to be matched case-insensitively.  These form
EXACTFL regops (nodes).  Notice that if such a string contains only
characters above-Latin1 that match only themselves, that the node can be
downgraded to an EXACT-only node, which presents better optimization
possibilities, as we now have a fixed string known at compile time to be
required to be in the target string to match.  Similarly if all
characters in the string match only other above-Latin1 characters
case-insensitively, the node can be downgraded to a regular EXACTFU node
(match, folding, using Unicode, not locale, rules).  The code changes
for this could be done without accepting UTF-8 locales fully, but there
were edge cases which needed to be handled differently if I stopped
there, so I continued on.

In an EXACTFL node, all such characters are now folded at compile time
(just as before this commit), while the other characters whose folds are
locale-dependent are left unfolded.  This means that they have to be

handy.h
@@ -523,12 +523,13 @@ Variant C<isFOO_utf8> is like C<isFOO_uni>, but the input is a pointer to a
 classification of just the first (possibly multi-byte) character in the string
 is tested.
 
-Variant C<isFOO_LC> is like the C<isFOO_A> and C<isFOO_L1> variants, but uses
-the C library function that gives the named classification instead of
-hard-coded rules.  For example, C<isDIGIT_LC()> returns the result of calling
-C<isdigit()>.  This means that the result is based on the current locale, which
-is what C<LC> in the name stands for.  FALSE is always returned if the input
-won't fit into an octet.
+Variant C<isFOO_LC> is like the C<isFOO_A> and C<isFOO_L1> variants, but the
+result is based on the current locale, which is what C<LC> in the name stands
+for.  If Perl can determine that the current locale is a UTF-8 locale, it uses
+the published Unicode rules; otherwise, it uses the C library function that
+gives the named classification.  For example, C<isDIGIT_LC()> when not in a
+UTF-8 locale returns the result of calling C<isdigit()>.  FALSE is always
+returned if the input won't fit into an octet.
 
 Variant C<isFOO_LC_uvchr> is like C<isFOO_LC>, but is defined on any UV.  It
 returns the same as C<isFOO_LC> for input code points less than 256, and
@@ -1241,18 +1242,24 @@ EXTCONST U32 PL_charclass[];
 #define toUPPER_LATIN1_MOD(c) ((! FITS_IN_8_BITS(c))                       \
                                ? (c)                                       \
                                : PL_mod_latin1_uc[ (U8) (c) ])
+#define IN_UTF8_CTYPE_LOCALE PL_in_utf8_CTYPE_locale
 
 /* Use foo_LC_uvchr() instead  of these for beyond the Latin1 range */
 
 /* For internal core Perl use only: the base macro for defining macros like
  * isALPHA_LC, which uses the current LC_CTYPE locale.  'c' is the code point
- * (0-255) to check.  'utf8_locale_classnum' is currently unused.  The code to
- * actually do the test this is passed in 'non_utf8'.  If 'c' is above 255, 0
- * is returned.  For accessing the full range of possible code points under
- * locale rules, use the macros based on _generic_LC_uvchr instead of this. */
+ * (0-255) to check.  In a UTF-8 locale, the result is the same as calling
+ * isFOO_L1(); the 'utf8_locale_classnum' parameter is something like
+ * _CC_UPPER, which gives the class number for doing this.  For non-UTF-8
+ * locales, the code to actually do the test this is passed in 'non_utf8'.  If
+ * 'c' is above 255, 0 is returned.  For accessing the full range of possible
+ * code points under locale rules, use the macros based on _generic_LC_uvchr
+ * instead of this. */
 #define _generic_LC_base(c, utf8_locale_classnum, non_utf8)                    \
            (! FITS_IN_8_BITS(c)                                                \
            ? 0                                                                 \
+           : IN_UTF8_CTYPE_LOCALE                                              \
+             ? cBOOL(PL_charclass[(U8) (c)] & _CC_mask(utf8_locale_classnum))  \
              : cBOOL(non_utf8))
 
 /* For internal core Perl use only: a helper macro for defining macros like
@@ -1275,15 +1282,41 @@ EXTCONST U32 PL_charclass[];
  * helper macros */
 #define _generic_toLOWER_LC(c, function, cast)  (! FITS_IN_8_BITS(c)           \
                                                 ? (c)                          \
+                                                : (IN_UTF8_CTYPE_LOCALE)       \
+                                                  ? PL_latin1_lc[ (U8) (c) ]   \
                                                 : function((cast)(c)))
 
+/* Note that the result can be larger than a byte in a UTF-8 locale.  It
+ * returns a single value, so can't adequately return the upper case of LATIN
+ * SMALL LETTER SHARP S in a UTF-8 locale (which should be a string of two
+ * values "SS");  instead it asserts against that under DEBUGGING, and
+ * otherwise returns its input */
 #define _generic_toUPPER_LC(c, function, cast)                                 \
                     (! FITS_IN_8_BITS(c)                                       \
                     ? (c)                                                      \
-                      : function((cast)(c)))
+                    : ((! IN_UTF8_CTYPE_LOCALE)                                \
+                      ? function((cast)(c))                                    \
+                      : ((((U8)(c)) == MICRO_SIGN)                             \
+                        ? GREEK_CAPITAL_LETTER_MU                              \
+                        : ((((U8)(c)) == LATIN_SMALL_LETTER_Y_WITH_DIAERESIS)  \
+                          ? LATIN_CAPITAL_LETTER_Y_WITH_DIAERESIS              \
+                          : ((((U8)(c)) == LATIN_SMALL_LETTER_SHARP_S)         \
+                            ? (__ASSERT_(0) (c))                               \
+                            : PL_mod_latin1_uc[ (U8) (c) ])))))
 
+/* Note that the result can be larger than a byte in a UTF-8 locale.  It
+ * returns a single value, so can't adequately return the fold case of LATIN
+ * SMALL LETTER SHARP S in a UTF-8 locale (which should be a string of two
+ * values "ss"); instead it asserts against that under DEBUGGING, and
+ * otherwise returns its input */
 #define _generic_toFOLD_LC(c, function, cast)                                  \
-                      _generic_toLOWER_LC(c, function, cast)
+                    (LIKELY((c) != MICRO_SIGN)                                 \
+                    ? (__ASSERT_(! IN_UTF8_CTYPE_LOCALE                        \
+                                 || (c) != LATIN_SMALL_LETTER_SHARP_S)         \
+                       _generic_toLOWER_LC(c, function, cast))                 \
+                    : (IN_UTF8_CTYPE_LOCALE)                                   \
+                      ? GREEK_SMALL_LETTER_MU                                  \
+                      : (c))
 
 /* Use the libc versions for these if available. */
 #if defined(HAS_ISASCII) && ! defined(USE_NEXT_CTYPE)

embed.fnc
@@ -650,7 +650,7 @@ p	|UV	|_to_upper_title_latin1|const U8 c|NN U8 *p|NN STRLEN *lenp|const char S_o
 #endif
 AMp	|UV	|to_uni_lower	|UV c|NN U8 *p|NN STRLEN *lenp
 AMmp	|UV	|to_uni_fold	|UV c|NN U8 *p|NN STRLEN *lenp
-AMp	|UV	|_to_uni_fold_flags|UV c|NN U8 *p|NN STRLEN *lenp|const U8 flags
+AMp	|UV	|_to_uni_fold_flags|UV c|NN U8 *p|NN STRLEN *lenp|U8 flags
 ADMpPR	|bool	|is_uni_alnum_lc|UV c
 ADMpPR	|bool	|is_uni_alnumc_lc|UV c
 ADMpPR	|bool	|is_uni_idfirst_lc|UV c
@@ -1513,13 +1513,13 @@ Apd	|UV	|to_utf8_case	|NN const U8 *p|NN U8* ustrp|NULLOK STRLEN *lenp \
 				|NN SV **swashp|NN const char *normal|NULLOK const char *special
 Abmd	|UV	|to_utf8_lower	|NN const U8 *p|NN U8* ustrp|NULLOK STRLEN *lenp
 AMp	|UV	|_to_utf8_lower_flags	|NN const U8 *p|NN U8* ustrp  \
-				|NULLOK STRLEN *lenp|const bool flags
+				|NULLOK STRLEN *lenp|bool flags
 Abmd	|UV	|to_utf8_upper	|NN const U8 *p|NN U8* ustrp|NULLOK STRLEN *lenp
 AMp	|UV	|_to_utf8_upper_flags	|NN const U8 *p|NN U8* ustrp   \
-				|NULLOK STRLEN *lenp|const bool flags
+				|NULLOK STRLEN *lenp|bool flags
 Abmd	|UV	|to_utf8_title	|NN const U8 *p|NN U8* ustrp|NULLOK STRLEN *lenp
 AMp	|UV	|_to_utf8_title_flags	|NN const U8 *p|NN U8* ustrp   \
-				|NULLOK STRLEN *lenp|const bool flags
+				|NULLOK STRLEN *lenp|bool flags
 Abmd	|UV	|to_utf8_fold	|NN const U8 *p|NN U8* ustrp|NULLOK STRLEN *lenp
 AMp	|UV	|_to_utf8_fold_flags|NN const U8 *p|NN U8* ustrp       \
 				|NULLOK STRLEN *lenp|U8 flags
@@ -2089,7 +2089,7 @@ Es	|void	|ssc_or		|NN const RExC_state_t *pRExC_state \
 				|NN const regnode_ssc *or_with
 Es	|SV*	|get_ANYOF_cp_list_for_ssc                                 \
 				|NN const RExC_state_t *pRExC_state \
-				|NN const regnode_charclass_posixl* const node
+				|NN const regnode_charclass_posixl_fold* const node
 Ei	|void	|ssc_intersection|NN regnode_ssc *ssc \
 				|NN SV* const invlist|const bool invert_2nd
 Ei	|void	|ssc_union	|NN regnode_ssc *ssc \

embedvar.h
@@ -161,6 +161,7 @@
 #define PL_in_clean_objs	(vTHX->Iin_clean_objs)
 #define PL_in_eval		(vTHX->Iin_eval)
 #define PL_in_load_module	(vTHX->Iin_load_module)
+#define PL_in_utf8_CTYPE_locale	(vTHX->Iin_utf8_CTYPE_locale)
 #define PL_incgv		(vTHX->Iincgv)
 #define PL_initav		(vTHX->Iinitav)
 #define PL_inplace		(vTHX->Iinplace)

ext/XS-APItest/t/handy.t
@@ -1,5 +1,9 @@
 #!perl -w
 
+BEGIN {
+    require 'loc_tools.pl';   # Contains find_utf8_locale()
+}
+
 use strict;
 use Test::More;
 use Config;
@@ -13,6 +17,7 @@ sub truth($) {  # Converts values so is() works
 }
 
 my $locale;
+my $utf8_locale;
 if($Config{d_setlocale}) {
     require POSIX;
     $locale = POSIX::setlocale( &POSIX::LC_ALL, "C");
@@ -31,6 +36,8 @@ if($Config{d_setlocale}) {
                 last;
             }
         }
+
+        $utf8_locale = find_utf8_locale();
     }
 }
 
@@ -149,13 +156,30 @@ foreach my $name (sort keys %properties) {
             if (defined $locale) {
                 require locale; import locale;
 
+                POSIX::setlocale( &POSIX::LC_ALL, "C");
                 $ret = truth eval "test_is${function}_LC($i)";
                 if ($@) {
                     fail($@);
                 }
                 else {
                     my $truth = truth($matches && $i < 128);
-                    is ($ret, $truth, "is${function}_LC( $display_name ) == $truth");
+                    is ($ret, $truth, "is${function}_LC( $display_name ) == $truth (C locale)");
+                }
+            }
+
+            if (defined $utf8_locale) {
+                use locale;
+
+                POSIX::setlocale( &POSIX::LC_ALL, $utf8_locale);
+                $ret = truth eval "test_is${function}_LC($i)";
+                if ($@) {
+                    fail($@);
+                }
+                else {
+
+                    # UTF-8 locale works on full range 0-255
+                    my $truth = truth($matches && $i < 256);
+                    is ($ret, $truth, "is${function}_LC( $display_name ) == $truth ($utf8_locale)");
                 }
             }
         }
@@ -171,13 +195,28 @@ foreach my $name (sort keys %properties) {
         if (defined $locale && $name ne 'vertws') {
             require locale; import locale;
 
+            POSIX::setlocale( &POSIX::LC_ALL, "C");
             $ret = truth eval "test_is${function}_LC_uvchr('$i')";
             if ($@) {
                 fail($@);
             }
             else {
                 my $truth = truth($matches && ($i < 128 || $i > 255));
-                is ($ret, $truth, "is${function}_LC_uvchr( $display_name ) == $truth");
+                is ($ret, $truth, "is${function}_LC_uvchr( $display_name ) == $truth (C locale)");
+            }
+        }
+
+        if (defined $utf8_locale && $name ne 'vertws') {
+            use locale;
+
+            POSIX::setlocale( &POSIX::LC_ALL, $utf8_locale);
+            $ret = truth eval "test_is${function}_LC_uvchr('$i')";
+            if ($@) {
+                fail($@);
+            }
+            else {
+                my $truth = truth($matches);
+                is ($ret, $truth, "is${function}_LC_uvchr( $display_name ) == $truth ($utf8_locale)");
             }
         }
 
@@ -195,13 +234,28 @@ foreach my $name (sort keys %properties) {
         if ($name ne 'vertws' && defined $locale) {
             require locale; import locale;
 
+            POSIX::setlocale( &POSIX::LC_ALL, "C");
             $ret = truth eval "test_is${function}_LC_utf8('$char')";
             if ($@) {
                 fail($@);
             }
             else {
                 my $truth = truth($matches && ($i < 128 || $i > 255));
-                is ($ret, $truth, "is${function}_LC_utf8( $display_name ) == $truth");
+                is ($ret, $truth, "is${function}_LC_utf8( $display_name ) == $truth (C locale)");
+            }
+        }
+
+        if ($name ne 'vertws' && defined $utf8_locale) {
+            use locale;
+
+            POSIX::setlocale( &POSIX::LC_ALL, $utf8_locale);
+            $ret = truth eval "test_is${function}_LC_utf8('$char')";
+            if ($@) {
+                fail($@);
+            }
+            else {
+                my $truth = truth($matches);
+                is ($ret, $truth, "is${function}_LC_utf8( $display_name ) == $truth ($utf8_locale)");
             }
         }
     }
@@ -292,10 +346,11 @@ foreach my $name (sort keys %to_properties) {
             }
         }
 
-        if ($name ne 'TITLE' && defined $locale) {
+        if ($name ne 'TITLE') { # Test _LC;  titlecase is not defined in locales.
+            if (defined $locale) {
             require locale; import locale;
 
-            # Test _LC;  titlecase is not defined in locales.
+                POSIX::setlocale( &POSIX::LC_ALL, "C");
             $ret = eval "test_to${function}_LC($j)";
             if ($@) {
                 fail($@);
@@ -304,7 +359,32 @@ foreach my $name (sort keys %to_properties) {
                 my $should_be = ($i < 128 && $map_ref->[$index] != $missing)
                                 ? $map_ref->[$index] + $j - $list_ref->[$index]
                                 : $j;
-                is ($ret, $should_be, sprintf("to${function}_LC( $display_name ) == 0x%02X", $should_be));
+                is ($ret, $should_be, sprintf("to${function}_LC( $display_name ) == 0x%02X (C locale)", $should_be));
+            }
+            }
+
+            if (defined $utf8_locale) {
+                use locale;
+
+                SKIP: {
+                    skip "to${property}_LC does not work for LATIN SMALL LETTER SHARP S", 1
+                        if $j == 0xDF && ($name eq 'FOLD' || $name eq 'UPPER');
+
+                    POSIX::setlocale( &POSIX::LC_ALL, $utf8_locale);
+                    $ret = eval "test_to${function}_LC($j)";
+                    if ($@) {
+                        fail($@);
+                    }
+                    else {
+                        my $should_be = ($i < 256
+                                         && ! ref $map_ref->[$index]
+                                         && $map_ref->[$index] != $missing
+                                        )
+                                        ? $map_ref->[$index] + $j - $list_ref->[$index]
+                                        : $j;
+                        is ($ret, $should_be, sprintf("to${function}_LC( $display_name ) == 0x%02X ($utf8_locale)", $should_be));
+                    }
+                }
             }
         }
 

globvar.sym
@@ -23,6 +23,7 @@ magic_data
 magic_vtable_names
 magic_vtables
 memory_wrap
+mod_latin1_uc
 no_aelem
 no_dir_func
 no_func

intrpvar.h
@@ -230,6 +230,7 @@ PERLVARI(I, dumpindent,	U16,	4)	/* number of blanks per dump
 PERLVAR(I, exit_flags,	U8)		/* was exit() unexpected, etc. */
 
 PERLVAR(I, utf8locale,	bool)		/* utf8 locale detected */
+PERLVAR(I, in_utf8_CTYPE_locale, bool)
 
 PERLVARA(I, colors,6,	char *)		/* values from PERL_RE_COLORS env var */
 

lib/locale.pm
@@ -27,24 +27,6 @@ expressions, LC_COLLATE for string comparison, and LC_NUMERIC for number
 formatting).  Each "use locale" or "no locale"
 affects statements to the end of the enclosing BLOCK.
 
-Starting in Perl 5.16, a hybrid mode for this pragma is available,
-
-    use locale ':not_characters';
-
-which enables only the portions of locales that don't affect the character
-set (that is, all except LC_COLLATE and LC_CTYPE).  This is useful when mixing
-Unicode and locales, including UTF-8 locales.
-
-    use locale ':not_characters';
-    use open ":locale";           # Convert I/O to/from Unicode
-    use POSIX qw(locale_h);       # Import the LC_ALL constant
-    setlocale(LC_ALL, "");        # Generally required for the next
-                                  # statement to take effect
-    printf "%.2f\n", 12345.67'    # Locale-defined formatting
-    @x = sort @y;                 # Unicode-defined sorting order.
-                                  # (Note that you will get better
-                                  # results using Unicode::Collate.)
-
 See L<perllocale> for more detailed information on how Perl supports
 locales.
 

lib/locale.t
@@ -602,10 +602,10 @@ foreach my $Locale (@Locale) {
 	next;
     }
 
-    # We test UTF-8 locales only under ':not_characters'; otherwise they have
-    # documented deficiencies.  Non- UTF-8 locales are tested only under plain
-    # 'use locale', as otherwise we would have to convert everything in them
-    # to Unicode.
+    # We test UTF-8 locales only under ':not_characters';  It is easier to
+    # test them in other test files than here.  Non- UTF-8 locales are tested
+    # only under plain 'use locale', as otherwise we would have to convert
+    # everything in them to Unicode.
 
     my %UPPER = ();     # All alpha X for which uc(X) == X and lc(X) != X
     my %lower = ();     # All alpha X for which lc(X) == X and uc(X) != X

commit 4d7db1b9da573ce828cd700a0f88a8e5cf401038

PATCH: [perl #121109] locales failing
This was due to a logic error in toFOLD_LC() introduced in
31f05a37c4e9c37a7263491f2fc0237d836e1a80.  It affected only the code
point at 0xB5 and shows up only in locales in which the character at that
code point is an uppercase letter.

handy.h
@@ -1310,13 +1310,11 @@ EXTCONST U32 PL_charclass[];
  * values "ss"); instead it asserts against that under DEBUGGING, and
  * otherwise returns its input */
 #define _generic_toFOLD_LC(c, function, cast)                                  \
-                    (LIKELY((c) != MICRO_SIGN)                                 \
-                    ? (__ASSERT_(! IN_UTF8_CTYPE_LOCALE                        \
-                                 || (c) != LATIN_SMALL_LETTER_SHARP_S)         \
-                       _generic_toLOWER_LC(c, function, cast))                 \
-                    : (IN_UTF8_CTYPE_LOCALE)                                   \
+                    ((UNLIKELY((c) == MICRO_SIGN) && IN_UTF8_CTYPE_LOCALE)     \
                       ? GREEK_SMALL_LETTER_MU                                  \
-                      : (c))
+                      : (__ASSERT_(! IN_UTF8_CTYPE_LOCALE                      \
+                                   || (c) != LATIN_SMALL_LETTER_SHARP_S)       \
+                         _generic_toLOWER_LC(c, function, cast)))
 
 /* Use the libc versions for these if available. */
 #if defined(HAS_ISASCII) && ! defined(USE_NEXT_CTYPE)