a5368aebf374b6cd78db2a9f2b316b21f409a8c0
Jerry D. Hedden <jdhedden@cpan.org>
2014-02-04 16:51:26 -0500

    Upgrade to threads::shared 1.46

--- Porting/Maintainers.pl
+++ Porting/Maintainers.pl
@@ -1199,7 +1199,7 @@ use File::Glob qw(:case);
     },
 
     'threads::shared' => {
-        'DISTRIBUTION' => 'JDHEDDEN/threads-shared-1.45.tar.gz',
+        'DISTRIBUTION' => 'JDHEDDEN/threads-shared-1.46.tar.gz',
         'FILES'        => q[dist/threads-shared],
         'EXCLUDED'     => [
             qw( examples/class.pl
