Perl/perl5

commit 5c85b638cb45ea2b8e5d70bfa899f2db6085e85c

SAVEt_CLEARSV: expand SvOK_off() macro
the next commit will change thinks that affect only part of the macro

scope.c
@@ -1090,7 +1090,11 @@ Perl_leave_scope(pTHX_ I32 base)
                         break;
                     }
                     default:
-                        SvOK_off(sv);
+                        assert_not_ROK(sv);
+                        assert_not_glob(sv);
+                        SvFLAGS(sv) &=~ (SVf_OK|SVf_IVisUV|SVf_UTF8);
+                        if (SvOOK(sv))
+                            sv_backoff(sv);
                         break;
                     }
                     SvPADSTALE_on(sv); /* mark as no longer live */

commit c79d007613fa174f6f5e1588ca5374f505fc44af

Commit 5c85b638cb45ea2b inadvertently broke the -DDEBUGGING build.
The macros assert_not_ROK(sv) and assert_not_glob(sv) are intended to be used
in expressions, and finish with a trailing comma, so they shouldn't have a ;
after them.

scope.c
@@ -1095,8 +1095,11 @@ Perl_leave_scope(pTHX_ I32 base)
                         break;
                     }
                     default:
-                        assert_not_ROK(sv);
-                        assert_not_glob(sv);
+                        /* This looks odd, but these two macros are for use in
+                           expressions and finish with a trailing comma, so
+                           adding a ; after them would be wrong. */
+                        assert_not_ROK(sv)
+                        assert_not_glob(sv)
                         SvFLAGS(sv) &=~ (SVf_OK|SVf_IVisUV|SVf_UTF8);
                         break;
                     }