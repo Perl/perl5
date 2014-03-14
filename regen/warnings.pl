0febf6c11783cfaf848994c4e17ba2e361a7caab
Ricardo Signes <rjbs@cpan.org>
2014-03-14 09:28:38 +0100

    bump the version of warnings.pm
    (and of regen/warnings.pl)

--- regen/warnings.pl
+++ regen/warnings.pl
@@ -16,7 +16,7 @@
 #
 # This script is normally invoked from regen.pl.
 
-$VERSION = '1.02_05';
+$VERSION = '1.03';
 
 BEGIN {
     require 'regen/regen_lib.pl';
@@ -470,7 +470,7 @@ read_only_bottom_close_and_rename($pm);
 __END__
 package warnings;
 
-our $VERSION = '1.22';
+our $VERSION = '1.23';
 
 # Verify that we're called correctly so that warnings will work.
 # see also strict.pm.
