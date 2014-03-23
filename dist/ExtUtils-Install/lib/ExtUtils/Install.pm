e5b7882f55674eccebed9b9ba58de3a21bd0efb1
Craig A. Berry <craigberry@mac.com>
2014-03-23 13:33:49 -0500

    Remove MM_TEST_ROOT feature from ExtUtils::Install tests.
    This feature depended on test directories created on the fly being
    in a predictable location, but as of 80af860fb39a3c6 they no longer
    are.  VMS systems released in the last 15 years or so don't even
    need the feature, so the best path forward is to get rid of it.
    
    A corresponding change was made in ExtUtils::MakeMaker 6.79_01 but
    ExtUtils::Install maintains its own fork of the same test libraries.

--- dist/ExtUtils-Install/lib/ExtUtils/Install.pm
+++ dist/ExtUtils-Install/lib/ExtUtils/Install.pm
@@ -38,11 +38,11 @@ ExtUtils::Install - install files from here to there
 
 =head1 VERSION
 
-1.63
+1.64
 
 =cut
 
-$VERSION = '1.63';  # <-- do not forget to update the POD section just above this line!
+$VERSION = '1.64';  # <-- do not forget to update the POD section just above this line!
 $VERSION = eval $VERSION;
 
 =pod
