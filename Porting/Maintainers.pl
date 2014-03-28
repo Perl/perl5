fb5a6c43a8bf77b89047f06b2d3932698843b7c9
Chris 'BinGOs' Williams <chris@bingosnet.co.uk>
2014-03-28 14:02:51 +0000

    Maintainers.pl synchronised for CPAN release of ExtUtils-Install

--- Porting/Maintainers.pl
+++ Porting/Maintainers.pl
@@ -459,7 +459,7 @@ use File::Glob qw(:case);
     },
 
     'ExtUtils::Install' => {
-        'DISTRIBUTION' => 'BINGOS/ExtUtils-Install-1.63.tar.gz',
+        'DISTRIBUTION' => 'BINGOS/ExtUtils-Install-1.64.tar.gz',
         'FILES'        => q[dist/ExtUtils-Install],
         'EXCLUDED'     => [
             qw( t/lib/Test/Builder.pm
