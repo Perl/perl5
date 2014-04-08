15a4bc856a24846a3bd2721aa4294ea77870cdb8
Zefram <zefram@fysh.org>
2014-04-08 19:03:59 +0100

    tighten Storable's recognition of tied SVs
    Since commit ff44333e5a9d9dca5272bb166df463607ebd3020, being RMAGICAL
    and having tie magic is not sufficient to recognise an SV as tied.
    When magic is turned off for mg_set(), the RMAGICAL flag is now left on,
    so that vstrings will be recognised as such.  So Storable needs to check
    whether the tie magic it sees is actually in effect, by also looking at
    the GMAGICAL and SMAGICAL flags.

--- Storable.pm
+++ Storable.pm
@@ -22,7 +22,7 @@ package Storable; @ISA = qw(Exporter);
 
 use vars qw($canonical $forgive_me $VERSION);
 
-$VERSION = '2.48';
+$VERSION = '2.49';
 
 BEGIN {
     if (eval { local $SIG{__DIE__}; require Log::Agent; 1 }) {
