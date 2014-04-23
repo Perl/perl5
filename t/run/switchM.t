Perl/perl5

commit 9fdd5a7ac74817cfaab65f7284f98ea66faca324

RT-121512 - Allow -I/dir/ with trailing slash to find .pmc files.
6b0bdd7f2041803dc3ec72b53d28052705861967 updated -I to not add a
trailing '/' if one was already present, but failed to update the
length of the resulting SV to account for the lack of another character.
This caused later checks based off of that length to fail (in this case,
seeing if the last 3 characters of the string are .pm).

t/run/switchM.t
@@ -8,7 +8,7 @@ use strict;
 
 require './test.pl';
 
-plan(2);
+plan(4);
 
 like(runperl(switches => ['-Irun/flib', '-Mbroken'], stderr => 1),
      qr/^Global symbol "\$x" requires explicit package name at run\/flib\/broken.pm line 6\./,
@@ -17,3 +17,11 @@ like(runperl(switches => ['-Irun/flib', '-Mbroken'], stderr => 1),
 like(runperl(switches => ['-Irun/flib/', '-Mbroken'], stderr => 1),
      qr/^Global symbol "\$x" requires explicit package name at run\/flib\/broken.pm line 6\./,
      "Ensure -Irun/flib/ produces correct filename in warnings");
+
+like(runperl(switches => ['-Irun/flib', '-Mt2', '-e "print t2::id()"'], stderr => 1),
+     qr/^t2pmc$/,
+     "Ensure -Irun/flib loads pmc");
+
+like(runperl(switches => ['-Irun/flib/', '-Mt2', '-e "print t2::id()"'], stderr => 1),
+     qr/^t2pmc$/,
+     "Ensure -Irun/flib/ loads pmc");

MANIFEST
@@ -5445,6 +5445,8 @@ t/run/dtrace.pl			For dtrace.t
 t/run/dtrace.t			Test for DTrace probes
 t/run/exit.t			Test perl's exit status.
 t/run/flib/broken.pm		Bad .pm file for switchM.t
+t/run/flib/t2.pm		Test for .pmcs with -I/dir/
+t/run/flib/t2.pmc		Test for .pmcs with -I/dir/
 t/run/fresh_perl.t		Tests that require a fresh perl.
 t/run/locale.t		Tests related to locale handling
 t/run/mad.t			Test vs MAD environment

pp_ctl.c
@@ -3999,6 +3999,9 @@ PP(pp_require)
 			/* Avoid '<dir>//<file>' */
 			if (!dirlen || *(tmp-1) != '/') {
 			    *tmp++ = '/';
+			} else {
+			    /* So SvCUR_set reports the correct length below */
+			    dirlen--;
 			}
 
 			/* name came from an SV, so it will have a '\0' at the

t/run/flib/t2.pm
@@ -1,0 +1,5 @@
+package t2;
+
+sub id { "t2pm" }
+
+1;

t/run/flib/t2.pmc
@@ -1,0 +1,5 @@
+package t2;
+
+sub id { "t2pmc" }
+
+1;

commit d9c544bf81d27bebeecc8eb1204005c1cc329f64

Make the test suite pass with -DPERL_DISABLE_PMC
Commit 9fdd5a7ac74817cfaab6 introduced new tests that fail when building
perl without PMC support.  In such cases, skip these new tests.

t/run/switchM.t
@@ -3,6 +3,9 @@
 BEGIN {
     chdir 't' if -d 't';
     @INC = '../lib';
+    require Config;
+    import Config;
+
 }
 use strict;
 
@@ -18,10 +21,16 @@ like(runperl(switches => ['-Irun/flib/', '-Mbroken'], stderr => 1),
      qr/^Global symbol "\$x" requires explicit package name at run\/flib\/broken.pm line 6\./,
      "Ensure -Irun/flib/ produces correct filename in warnings");
 
-like(runperl(switches => ['-Irun/flib', '-Mt2'], prog => 'print t2::id()', stderr => 1),
-     qr/^t2pmc$/,
-     "Ensure -Irun/flib loads pmc");
+SKIP: {
+    if ( $Config{ccflags} =~ /-DPERL_DISABLE_PMC/ ) {
+        skip('Tests fail without PMC support', 2);
+    }
 
-like(runperl(switches => ['-Irun/flib/', '-Mt2'], prog => 'print t2::id()', stderr => 1),
-     qr/^t2pmc$/,
-     "Ensure -Irun/flib/ loads pmc");
+    like(runperl(switches => ['-Irun/flib', '-Mt2'], prog => 'print t2::id()', stderr => 1),
+         qr/^t2pmc$/,
+         "Ensure -Irun/flib loads pmc");
+
+    like(runperl(switches => ['-Irun/flib/', '-Mt2'], prog => 'print t2::id()', stderr => 1),
+         qr/^t2pmc$/,
+         "Ensure -Irun/flib/ loads pmc");
+}