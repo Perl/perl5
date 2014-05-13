c68523cb697da4a97cd60f5c2bb390d24321c806
Ricardo Signes <rjbs@cpan.org>
2014-05-12 20:42:06 -0400

    bump version to 5.20.0, install 5.20 perldelta

--- uconfig.h
+++ uconfig.h
@@ -896,8 +896,8 @@
  *	This symbol contains the ~name expanded version of ARCHLIB, to be used
  *	in programs that are not prepared to deal with ~ expansion at run-time.
  */
-/*#define ARCHLIB "/usr/local/lib/perl5/5.19/unknown"		/ **/
-/*#define ARCHLIB_EXP "/usr/local/lib/perl5/5.19/unknown"		/ **/
+/*#define ARCHLIB "/usr/local/lib/perl5/5.20/unknown"		/ **/
+/*#define ARCHLIB_EXP "/usr/local/lib/perl5/5.20/unknown"		/ **/
 
 /* ARCHNAME:
  *	This symbol holds a string representing the architecture name.
@@ -3007,8 +3007,8 @@
  *	This symbol contains the ~name expanded version of PRIVLIB, to be used
  *	in programs that are not prepared to deal with ~ expansion at run-time.
  */
-#define PRIVLIB "/usr/local/lib/perl5/5.19"		/**/
-#define PRIVLIB_EXP "/usr/local/lib/perl5/5.19"		/**/
+#define PRIVLIB "/usr/local/lib/perl5/5.20"		/**/
+#define PRIVLIB_EXP "/usr/local/lib/perl5/5.20"		/**/
 
 /* CAN_PROTOTYPE:
  *	If defined, this macro indicates that the C compiler can handle
@@ -3151,8 +3151,8 @@
  *	This symbol contains the ~name expanded version of SITEARCH, to be used
  *	in programs that are not prepared to deal with ~ expansion at run-time.
  */
-/*#define SITEARCH "/usr/local/lib/perl5/5.19/unknown"		/ **/
-/*#define SITEARCH_EXP "/usr/local/lib/perl5/5.19/unknown"		/ **/
+/*#define SITEARCH "/usr/local/lib/perl5/5.20/unknown"		/ **/
+/*#define SITEARCH_EXP "/usr/local/lib/perl5/5.20/unknown"		/ **/
 
 /* SITELIB:
  *	This symbol contains the name of the private library for this package.
@@ -3174,8 +3174,8 @@
  *	removed.  The elements in inc_version_list (inc_version_list.U) can
  *	be tacked onto this variable to generate a list of directories to search.
  */
-#define SITELIB "/usr/local/lib/perl5/5.19"		/**/
-#define SITELIB_EXP "/usr/local/lib/perl5/5.19"		/**/
+#define SITELIB "/usr/local/lib/perl5/5.20"		/**/
+#define SITELIB_EXP "/usr/local/lib/perl5/5.20"		/**/
 #define SITELIB_STEM "/usr/local/lib/perl5"		/**/
 
 /* Size_t_size:
