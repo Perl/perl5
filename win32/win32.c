90674eaa88a3b2f52d8ac1b835c3505b25ff3f00
Steve Hay <steve.m.hay@googlemail.com>
2014-05-13 12:53:10 +0100

    [perl #121676] Fix memory leak in backticks and system
    Introduced by perl #113536. It doesn't actually leak on Windows XP due to
    differences in the implementation of GetEnvironmentStringsW() compared to
    later OSes, but the missing FreeEnvironmentStrings() was technically wrong
    anyway, and does now bite. Thanks for Daniel Dragan for finding this.

--- win32/win32.c
+++ win32/win32.c
@@ -1770,6 +1770,8 @@ win32_getenvironmentstrings(void)
     WideCharToMultiByte(CP_ACP, WC_NO_BEST_FIT_CHARS, lpWStr, wenvstrings_len, lpStr, 
                         aenvstrings_len, NULL, NULL);
 
+    FreeEnvironmentStrings(lpWStr);
+
     return(lpStr);
 }
 
