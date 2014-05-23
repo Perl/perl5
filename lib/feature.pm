09256113012d3dbec7c2d25e2fcb3feff356fb44
Steffen Mueller <smueller@cpan.org>
2014-05-23 07:47:15 +0200

    Regen!

--- lib/feature.pm
+++ lib/feature.pm
@@ -27,6 +27,7 @@ our %feature_bundle = (
     "5.10"    => [qw(array_base say state switch)],
     "5.11"    => [qw(array_base say state switch unicode_strings)],
     "5.15"    => [qw(current_sub evalbytes fc say state switch unicode_eval unicode_strings)],
+    "5.19"    => [qw(current_sub evalbytes fc say signatures state switch unicode_eval unicode_strings)],
     "all"     => [qw(array_base current_sub evalbytes fc lexical_subs postderef postderef_qq say signatures state switch unicode_eval unicode_strings)],
     "default" => [qw(array_base)],
 );
@@ -37,13 +38,12 @@ $feature_bundle{"5.14"} = $feature_bundle{"5.11"};
 $feature_bundle{"5.16"} = $feature_bundle{"5.15"};
 $feature_bundle{"5.17"} = $feature_bundle{"5.15"};
 $feature_bundle{"5.18"} = $feature_bundle{"5.15"};
-$feature_bundle{"5.19"} = $feature_bundle{"5.15"};
-$feature_bundle{"5.20"} = $feature_bundle{"5.15"};
+$feature_bundle{"5.20"} = $feature_bundle{"5.19"};
 $feature_bundle{"5.9.5"} = $feature_bundle{"5.10"};
 
 our $hint_shift   = 26;
 our $hint_mask    = 0x1c000000;
-our @hint_bundles = qw( default 5.10 5.11 5.15 );
+our @hint_bundles = qw( default 5.10 5.11 5.15 5.19 );
 
 # This gets set (for now) in $^H as well as in %^H,
 # for runtime speed of the uc/lc/ucfirst/lcfirst functions.
@@ -294,6 +294,7 @@ The following feature bundles are available:
 
   :5.20     say state switch unicode_strings
             unicode_eval evalbytes current_sub fc
+            signatures
 
 The C<:default> bundle represents the feature set that is enabled before
 any C<use feature> or C<no feature> declaration.
