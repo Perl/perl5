9795fb517b989e154d178f8a1278d1966c0c787d
Chris 'BinGOs' Williams <chris@bingosnet.co.uk>
2014-05-27 18:38:17 +0100

    Harmonise Module-CoreList version for CPAN release

--- Porting/Maintainers.pl
+++ Porting/Maintainers.pl
@@ -795,7 +795,7 @@ use File::Glob qw(:case);
     },
 
     'Module::CoreList' => {
-        'DISTRIBUTION' => 'BINGOS/Module-CoreList-3.10.tar.gz',
+        'DISTRIBUTION' => 'BINGOS/Module-CoreList-3.11.tar.gz',
         'FILES'        => q[dist/Module-CoreList],
     },
 
