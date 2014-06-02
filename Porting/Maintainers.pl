75bc7bf53a48f066ec16b264b61f2406366a1d21
Steve Hay <steve.m.hay@googlemail.com>
2014-06-02 18:42:01 +0100

    Update Porting/Maintainers.pl for Devel-PPPort upgrade in ea4b7f3223
    Also remove all the IGNORABLE files. These are not required in core for
    cpan/ distributions; we save space by leaving them out. And we don't want
    EXCLUDED files either.

--- Porting/Maintainers.pl
+++ Porting/Maintainers.pl
@@ -330,7 +330,7 @@ use File::Glob qw(:case);
     },
 
     'Devel::PPPort' => {
-        'DISTRIBUTION' => 'MHX/Devel-PPPort-3.21.tar.gz',
+        'DISTRIBUTION' => 'WOLFSAGE/Devel-PPPort-3.24.tar.gz',
         # RJBS has asked MHX to have UPSTREAM be 'blead'
         # (i.e. move this from cpan/ to dist/)
         'FILES'        => q[cpan/Devel-PPPort],
