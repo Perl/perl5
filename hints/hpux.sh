513433ab5e8823ec87ddbae400df376a166bd0d5
Jarkko Hietaniemi <jhi@iki.fi>
2014-06-19 11:13:00 -0400

    HP-UX cc in PA-RISC2.0 optimizer crashes with some files.

--- hints/hpux.sh
+++ hints/hpux.sh
@@ -377,6 +377,8 @@ EOM
 regexec_cflags=''
 doop_cflags=''
 op_cflags=''
+opmini_cflags=''
+perlmain_cflags=''
     fi
 
 case "$ccisgcc" in
@@ -427,6 +429,21 @@ case "$ccisgcc" in
 		    ;;
 	    esac
 	case "$archname" in
+	    PA-RISC2.0)
+		case "$ccversion" in
+		    B.11.11.*)
+			# opmini.c and op.c with +O2 makes the compiler die
+			# of internal error, for perlmain.c only +O0 (no opt)
+                        # works.
+			case "$optimize" in
+			*O2*)	opt=`echo "$optimize" | sed -e 's/O2/O1/'`
+				opmini_cflags="optimize=\"$opt\""
+				op_cflags="optimize=\"$opt\""
+				perlmain_cflags="optimize=\"\""
+				;;
+			esac
+		    esac
+		;;
 	    IA64*)
 		case "$ccversion" in
 		    B3910B*A.06.0[12345])
