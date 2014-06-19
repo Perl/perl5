4f893e616257d6f87d238c8d5d204072ad5a13b3
Jarkko Hietaniemi <jhi@iki.fi>
2014-06-19 15:08:25 -0400

    Note the open RT ticket.

--- cpan/Digest-SHA/hints/hpux.pl
+++ cpan/Digest-SHA/hints/hpux.pl
@@ -1,5 +1,8 @@
 # With +O2 this HP-UX cc compiler creates code which coredumps (Bus error)
 # when running t/woodbury.t, but dropping to +O1 seems to dodge that.
+#
+# This might turn out to be temporary, see:
+# https://rt.cpan.org/Ticket/Display.html?id=96498
 if ($Config{cc} eq 'cc' &&
     $Config{archname} eq 'PA-RISC2.0' &&
     $Config{ccversion} =~ /^B\.11\.11\./) {
