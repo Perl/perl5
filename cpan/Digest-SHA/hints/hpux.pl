960d7aa95fd85d333458d7fcada31d6575981c57
Jarkko Hietaniemi <jhi@iki.fi>
2014-06-19 11:33:32 -0400

    Lower the optimization on Digest-SHA for HP-UX cc.
    Upstream, but we are adding a platform specific hints file.

---
+++ cpan/Digest-SHA/hints/hpux.pl
@@ -0,0 +1,13 @@
+# With +O2 this HP-UX cc compiler creates code which coredumps (Bus error)
+# when running t/woodbury.t, but dropping to +O1 seems to dodge that.
+if ($Config{cc} eq 'cc' &&
+    $Config{archname} eq 'PA-RISC2.0' &&
+    $Config{ccversion} =~ /^B\.11\.11\./) {
+  if (defined $self->{OPTIMIZE}) {
+    $self->{OPTIMIZE} =~ s/\+O[2-9]/+O1/;
+    $self->{OPTIMIZE} =~ s/ \+Onolimit//;
+  } else {
+    $self->{OPTIMIZE} = '+O1';
+  }
+}
+
