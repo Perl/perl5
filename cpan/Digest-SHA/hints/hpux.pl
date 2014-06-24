55149fe1eba76b5f9b1db01ec2e8e47bf7c2abbc
Jarkko Hietaniemi <jhi@iki.fi>
2014-06-24 08:46:58 -0400

    HP-UX release wider match; also drop gcc opt level.

--- cpan/Digest-SHA/hints/hpux.pl
+++ cpan/Digest-SHA/hints/hpux.pl
@@ -1,15 +1,25 @@
 # With +O2 this HP-UX cc compiler creates code which coredumps (Bus error)
 # when running t/woodbury.t, but dropping to +O1 seems to dodge that.
 #
-# This might turn out to be temporary, see:
+# Also gcc seems to have similar issues, so drop the opt also there.
+#
+# This was reported also at:
 # https://rt.cpan.org/Ticket/Display.html?id=96498
-if ($Config{cc} eq 'cc' &&
-    $Config{archname} eq 'PA-RISC2.0' &&
-    $Config{ccversion} =~ /^B\.11\.11\./) {
-  if (defined $self->{OPTIMIZE}) {
-    $self->{OPTIMIZE} =~ s/\+O[2-9]/+O1/;
-    $self->{OPTIMIZE} =~ s/ \+Onolimit//;
-  } else {
-    $self->{OPTIMIZE} = '+O1';
+# but the ticket was rejected since MSHELOR thinks Digest::SHA
+# is doing nothing wrong.
+if ($Config{ccversion} =~ /^B\.11\.*\./) {
+  if ($Config{cc} eq 'cc') {
+    if (defined $self->{OPTIMIZE}) {
+      $self->{OPTIMIZE} =~ s/\+O[2-9]/+O1/;
+      $self->{OPTIMIZE} =~ s/ \+Onolimit//;
+    } else {
+      $self->{OPTIMIZE} = '+O1';
+    }
+  } elsif ($Config{cc} =~ /gcc/) {
+    if (defined $self->{OPTIMIZE}) {
+      $self->{OPTIMIZE} =~ s/\-O[2-9]*/-O1/;
+    } else {
+      $self->{OPTIMIZE} = '-O1';
+    }
   }
 }
