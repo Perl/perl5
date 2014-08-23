107020b50c3811d36f1327e28017fb6e26e79009
Chris 'BinGOs' Williams <chris@bingosnet.co.uk>
2014-08-23 21:35:09 +0100

    Update ExtUtils-CBuilder version for CPAN release

--- Porting/Maintainers.pl
+++ Porting/Maintainers.pl
@@ -427,7 +427,7 @@ use File::Glob qw(:case);
     },
 
     'ExtUtils::CBuilder' => {
-        'DISTRIBUTION' => 'AMBS/ExtUtils/ExtUtils-CBuilder-0.280216.tar.gz',
+        'DISTRIBUTION' => 'AMBS/ExtUtils/ExtUtils-CBuilder-0.280217.tar.gz',
         'FILES'        => q[dist/ExtUtils-CBuilder],
         'EXCLUDED'     => [
             qw(README.mkdn),
