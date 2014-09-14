37416c143ccd820a98793a867be458a61985ad25
Chris 'BinGOs' Williams <chris@bingosnet.co.uk>
2014-09-14 14:46:00 +0100

    Bump Module-CoreList versions to new date stamped version

--- dist/Module-CoreList/lib/Module/CoreList/TieHashDelta.pm
+++ dist/Module-CoreList/lib/Module/CoreList/TieHashDelta.pm
@@ -3,7 +3,7 @@ package Module::CoreList::TieHashDelta;
 use strict;
 use vars qw($VERSION);
 
-$VERSION = '5.021004';
+$VERSION = '5.201409014';
 
 sub TIEHASH {
     my ($class, $changed, $removed, $parent) = @_;
--- dist/Module-CoreList/lib/Module/CoreList/Utils.pm
+++ dist/Module-CoreList/lib/Module/CoreList/Utils.pm
@@ -6,7 +6,7 @@ use vars qw[$VERSION %utilities];
 use Module::CoreList;
 use Module::CoreList::TieHashDelta;
 
-$VERSION = '5.021004';
+$VERSION = '5.201409014';
 
 sub utilities {
     my $perl = shift;
