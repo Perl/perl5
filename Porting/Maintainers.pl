2287ae3497955cb33ac4984a463843378b7e651c
Chris 'BinGOs' Williams <chris@bingosnet.co.uk>
2014-09-16 10:01:26 +0100

    Synchronise Maintainers.pl for Module-CoreList

--- Porting/Maintainers.pl
+++ Porting/Maintainers.pl
@@ -793,7 +793,7 @@ use File::Glob qw(:case);
     },
 
     'Module::CoreList' => {
-        'DISTRIBUTION' => 'BINGOS/Module-CoreList-5.021003.tar.gz',
+        'DISTRIBUTION' => 'BINGOS/Module-CoreList-5.20140914.tar.gz',
         'FILES'        => q[dist/Module-CoreList],
     },
 
