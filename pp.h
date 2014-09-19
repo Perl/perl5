49d1dbaac672111bbe58149977f523e1fee67574
David Mitchell <davem@iabyn.com>
2014-09-19 17:52:59 +0100

    pp.h: remove spurious comment about OPpTARGET_MY
    OPpTARGET_MY is unrelated to MAXARG as far as I can tell.

--- pp.h
+++ pp.h
@@ -379,7 +379,6 @@ Does not use C<TARG>.  See also C<XPUSHu>, C<mPUSHu> and C<PUSHu>.
 
 #define ARGTARG		PL_op->op_targ
 
-    /* See OPpTARGET_MY: */
 #define MAXARG		(PL_op->op_private & OPpARG4_MASK)
 
 #define SWITCHSTACK(f,t) \
