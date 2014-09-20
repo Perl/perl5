50a0759e24381a2cb9a3f5a0b31cca366fdee204
Steve Hay <steve.m.hay@googlemail.com>
2014-09-20 10:28:18 +0100

    Blead is in sync with Data::Dumper 2.154

--- Porting/Maintainers.pl
+++ Porting/Maintainers.pl
@@ -321,7 +321,7 @@ use File::Glob qw(:case);
     },
 
     'Data::Dumper' => {
-        'DISTRIBUTION' => 'SMUELLER/Data-Dumper-2.151.tar.gz',
+        'DISTRIBUTION' => 'SMUELLER/Data-Dumper-2.154.tar.gz',
         'FILES'        => q[dist/Data-Dumper],
     },
 
