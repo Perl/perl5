9846cd95ba14e2441e8c1057547316e3a62ca855
Father Chrysostomos <sprout@cpan.org>
2014-09-27 14:15:50 -0700

    pp.c: Some branch prediction hints

--- pp.c
+++ pp.c
@@ -6248,7 +6248,7 @@ PP(pp_lvref)
     SV * const arg = PL_op->op_flags & OPf_STACKED ? POPs : NULL;
     sv_magic(ret, arg,
 	     PERL_MAGIC_lvref, (char *)elem, elem ? HEf_SVKEY : ARGTARG);
-    if (PL_op->op_private & OPpLVAL_INTRO) {
+    if (UNLIKELY(PL_op->op_private & OPpLVAL_INTRO)) {
       if (elem) {
 	MAGIC *mg;
 	HV *stash;
@@ -6277,7 +6277,7 @@ PP(pp_lvrefslice)
     const bool localizing = PL_op->op_private & OPpLVAL_INTRO;
     bool can_preserve = FALSE;
 
-    if (localizing) {
+    if (UNLIKELY(localizing)) {
 	MAGIC *mg;
 	HV *stash;
 	SV **svp;
