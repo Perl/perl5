Perl/perl5

commit 62a59291d781403a6b0e279db4433a5f486e3a61

regcomp.c: Avoid function call overhead
We don't need to call a function to convert from UTF-8 when the value is
invariant in and out of UTF-8.

regcomp.c
@@ -13491,7 +13491,7 @@ parseit:
         }
         else {
             /* Is a backslash; get the code point of the char after it */
-	    if (UTF) {
+	    if (UTF && ! UTF8_IS_INVARIANT(RExC_parse)) {
 		value = utf8n_to_uvchr((U8*)RExC_parse,
 				   RExC_end - RExC_parse,
 				   &numlen, UTF8_ALLOW_DEFAULT);

commit d56b1f57fd32d77a6c2b437d81c206ad3905e15a

regcomp.c: Properly dereference a ptr
UTF8_IS_INVARIANT takes a byte as its argument, not a ptr.  This bug was
introduced by 62a59291 in 5.21.4.  It doesn't probably cause bugs
because the ptr is always too big to be an invariant, but it slows
things down somewhat by not taking a shortcut it could.

It turns out that there is a discrepency between the APIs of the
UTF8-foo macros.  Some take a byte, and some a string ptr.

regcomp.c
@@ -13667,7 +13667,7 @@ S_regclass(pTHX_ RExC_state_t *pRExC_state, I32 *flagp, U32 depth,
         }
         else {
             /* Is a backslash; get the code point of the char after it */
-	    if (UTF && ! UTF8_IS_INVARIANT(RExC_parse)) {
+	    if (UTF && ! UTF8_IS_INVARIANT(UCHARAT(RExC_parse))) {
 		value = utf8n_to_uvchr((U8*)RExC_parse,
 				   RExC_end - RExC_parse,
 				   &numlen, UTF8_ALLOW_DEFAULT);