fda8e777fa2df245de209839b8b21252b2f12c47
Chris 'BinGOs' Williams <chris@bingosnet.co.uk>
2014-10-02 12:10:59 +0100

    The Module-CoreList on CPAN is now 5.20141002

--- Porting/Maintainers.pl
+++ Porting/Maintainers.pl
@@ -805,7 +805,7 @@ use File::Glob qw(:case);
     },
 
     'Module::CoreList' => {
-        'DISTRIBUTION' => 'BINGOS/Module-CoreList-5.20140920.tar.gz',
+        'DISTRIBUTION' => 'BINGOS/Module-CoreList-5.20141002.tar.gz',
         'FILES'        => q[dist/Module-CoreList],
     },
 
