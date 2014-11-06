a59aac591997973552d88b6602b36a979402061a
Father Chrysostomos <sprout@cpan.org>
2014-11-06 08:21:53 -0800

    Remove Mac Classic code from B::Concise

--- ext/B/B/Concise.pm
+++ ext/B/B/Concise.pm
@@ -1039,8 +1039,7 @@ sub tree {
 # to update the corresponding magic number in the next line.
 # Remember, this needs to stay the last things in the module.
 
-# Why is this different for MacOS?  Does it matter?
-my $cop_seq_mnum = $^O eq 'MacOS' ? 12 : 11;
+my $cop_seq_mnum = 11;
 $cop_seq_base = svref_2object(eval 'sub{0;}')->START->cop_seq + $cop_seq_mnum;
 
 1;
