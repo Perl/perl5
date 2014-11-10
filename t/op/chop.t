Perl/perl5

commit 2f9970be002ee78a22fc687876139eedc9eb3a65

Convert Perl_do_chomp() to the same prototype as Perl_do_chop().
Pass in an SV to hold the count, rather than returning the count.

embed.fnc
@@ -355,7 +355,7 @@ p	|bool	|do_print	|NULLOK SV* sv|NN PerlIO* fp
 : Used in pp_sys.c
 pR	|OP*	|do_readline
 : Used in pp.c
-p	|I32	|do_chomp	|NN SV* sv
+p	|void	|do_chomp	|NN SV *count|NN SV *sv
 : Defined in doio.c, used only in pp_sys.c
 p	|bool	|do_seek	|NULLOK GV* gv|Off_t pos|int whence
 Ap	|void	|do_sprintf	|NN SV* sv|I32 len|NN SV** sarg

pp.c
@@ -812,17 +812,19 @@ PP(pp_chop)
 PP(pp_schomp)
 {
     dVAR; dSP; dTARGET;
-    SETi(do_chomp(TOPs));
+    sv_setiv(TARG, 0);
+    do_chomp(TARG, TOPs);
+    SETs(TARG);
     RETURN;
 }
 
 PP(pp_chomp)
 {
     dVAR; dSP; dMARK; dTARGET; dORIGMARK;
-    register I32 count = 0;
 
+    sv_setiv(TARG, 0);
     while (MARK < SP)
-	count += do_chomp(*++MARK);
+	do_chomp(TARG, *++MARK);
     SP = ORIGMARK;
     XPUSHTARG;
     RETURN;

proto.h
@@ -707,10 +707,11 @@ PERL_CALLCONV int	Perl_do_binmode(pTHX_ PerlIO *fp, int iotype, int mode)
 #define PERL_ARGS_ASSERT_DO_BINMODE	\
 	assert(fp)
 
-PERL_CALLCONV I32	Perl_do_chomp(pTHX_ SV* sv)
-			__attribute__nonnull__(pTHX_1);
+PERL_CALLCONV void	Perl_do_chomp(pTHX_ SV *count, SV *sv)
+			__attribute__nonnull__(pTHX_1)
+			__attribute__nonnull__(pTHX_2);
 #define PERL_ARGS_ASSERT_DO_CHOMP	\
-	assert(sv)
+	assert(count); assert(sv)
 
 PERL_CALLCONV void	Perl_do_chop(pTHX_ SV *astr, SV *sv)
 			__attribute__nonnull__(pTHX_1)

doop.c
@@ -1060,11 +1060,10 @@ Perl_do_chop(pTHX_ register SV *astr, register SV *sv)
     SvSETMAGIC(sv);
 }
 
-I32
-Perl_do_chomp(pTHX_ register SV *sv)
+void
+Perl_do_chomp(pTHX_ SV *count, SV *sv)
 {
     dVAR;
-    register I32 count;
     STRLEN len;
     char *s;
     char *temp_buffer = NULL;
@@ -1073,10 +1072,9 @@ Perl_do_chomp(pTHX_ register SV *sv)
     PERL_ARGS_ASSERT_DO_CHOMP;
 
     if (RsSNARF(PL_rs))
-	return 0;
+	return;
     if (RsRECORD(PL_rs))
-      return 0;
-    count = 0;
+	return;
     if (SvTYPE(sv) == SVt_PVAV) {
 	register I32 i;
 	AV *const av = MUTABLE_AV(sv);
@@ -1085,17 +1083,17 @@ Perl_do_chomp(pTHX_ register SV *sv)
 	for (i = 0; i <= max; i++) {
 	    sv = MUTABLE_SV(av_fetch(av, i, FALSE));
 	    if (sv && ((sv = *(SV**)sv), sv != &PL_sv_undef))
-		count += do_chomp(sv);
+		do_chomp(count, sv);
 	}
-        return count;
+        return;
     }
     else if (SvTYPE(sv) == SVt_PVHV) {
 	HV* const hv = MUTABLE_HV(sv);
 	HE* entry;
         (void)hv_iterinit(hv);
         while ((entry = hv_iternext(hv)))
-            count += do_chomp(hv_iterval(hv,entry));
-        return count;
+            do_chomp(count, hv_iterval(hv,entry));
+	return;
     }
     else if (SvREADONLY(sv)) {
         if (SvFAKE(sv)) {
@@ -1123,11 +1121,11 @@ Perl_do_chomp(pTHX_ register SV *sv)
 	if (RsPARA(PL_rs)) {
 	    if (*s != '\n')
 		goto nope;
-	    ++count;
+	    ++SvIVX(count);
 	    while (len && s[-1] == '\n') {
 		--len;
 		--s;
-		++count;
+		++SvIVX(count);
 	    }
 	}
 	else {
@@ -1171,7 +1169,7 @@ Perl_do_chomp(pTHX_ register SV *sv)
 	    if (rslen == 1) {
 		if (*s != *rsptr)
 		    goto nope;
-		++count;
+		++SvIVX(count);
 	    }
 	    else {
 		if (len < rslen - 1)
@@ -1180,7 +1178,7 @@ Perl_do_chomp(pTHX_ register SV *sv)
 		s -= rslen - 1;
 		if (memNE(s, rsptr, rslen))
 		    goto nope;
-		count += rs_charlen;
+		SvIVX(count) += rs_charlen;
 	    }
 	}
 	s = SvPV_force_nolen(sv);
@@ -1194,7 +1192,6 @@ Perl_do_chomp(pTHX_ register SV *sv)
     SvREFCNT_dec(svrecode);
 
     Safefree(temp_buffer);
-    return count;
 }
 
 void

embed.h
@@ -974,7 +974,7 @@
 #define delete_eval_scope()	Perl_delete_eval_scope(aTHX)
 #define die_unwind(a)		Perl_die_unwind(aTHX_ a)
 #define do_aexec5(a,b,c,d,e)	Perl_do_aexec5(aTHX_ a,b,c,d,e)
-#define do_chomp(a)		Perl_do_chomp(aTHX_ a)
+#define do_chomp(a,b)		Perl_do_chomp(aTHX_ a,b)
 #define do_chop(a,b)		Perl_do_chop(aTHX_ a,b)
 #define do_dump_pad(a,b,c,d)	Perl_do_dump_pad(aTHX_ a,b,c,d)
 #define do_eof(a)		Perl_do_eof(aTHX_ a)

commit f595e19fc20e4a7fe60155dc88d9d801570d9abd

Fix $lex = chomp $lex regression from 5.12
Commit 2f9970be0 (5.13.9) caused the lhs to be written to with 0
before the chomp happens, and then incremented for each chomp on the
rhs argument.

That makes

    my $a = $/ = 7;
    $a = chomp $a;
    print $a;

print 0 instead of 1 and makes

    my $a = $/ = 0;
    $a = chomp $a;

assign the empty string to $a instead of 1.

embed.fnc
@@ -2004,7 +2004,7 @@ s	|SV *	|incpush_if_exists|NN AV *const av|NN SV *dir|NN SV *const stem
 #endif
 
 #if defined(PERL_IN_PP_C)
-s	|void	|do_chomp	|NN SV *retval|NN SV *sv|bool chomping
+s	|size_t	|do_chomp	|NN SV *retval|NN SV *sv|bool chomping
 s	|OP*	|do_delete_local
 sR	|SV*	|refto		|NN SV* sv
 #endif

pp.c
@@ -774,16 +774,17 @@ PP(pp_trans)
 
 /* Lvalue operators. */
 
-static void
+static size_t
 S_do_chomp(pTHX_ SV *retval, SV *sv, bool chomping)
 {
     STRLEN len;
     char *s;
+    size_t count = 0;
 
     PERL_ARGS_ASSERT_DO_CHOMP;
 
     if (chomping && (RsSNARF(PL_rs) || RsRECORD(PL_rs)))
-	return;
+	return 0;
     if (SvTYPE(sv) == SVt_PVAV) {
 	I32 i;
 	AV *const av = MUTABLE_AV(sv);
@@ -792,17 +793,17 @@ S_do_chomp(pTHX_ SV *retval, SV *sv, bool chomping)
 	for (i = 0; i <= max; i++) {
 	    sv = MUTABLE_SV(av_fetch(av, i, FALSE));
 	    if (sv && ((sv = *(SV**)sv), sv != &PL_sv_undef))
-		do_chomp(retval, sv, chomping);
+		count += do_chomp(retval, sv, chomping);
 	}
-        return;
+        return count;
     }
     else if (SvTYPE(sv) == SVt_PVHV) {
 	HV* const hv = MUTABLE_HV(sv);
 	HE* entry;
         (void)hv_iterinit(hv);
         while ((entry = hv_iternext(hv)))
-            do_chomp(retval, hv_iterval(hv,entry), chomping);
-	return;
+            count += do_chomp(retval, hv_iterval(hv,entry), chomping);
+	return count;
     }
     else if (SvREADONLY(sv)) {
             Perl_croak_no_modify();
@@ -832,11 +833,11 @@ S_do_chomp(pTHX_ SV *retval, SV *sv, bool chomping)
 	    if (RsPARA(PL_rs)) {
 		if (*s != '\n')
 		    goto nope;
-		++SvIVX(retval);
+		++count;
 		while (len && s[-1] == '\n') {
 		    --len;
 		    --s;
-		    ++SvIVX(retval);
+		    ++count;
 		}
 	    }
 	    else {
@@ -880,7 +881,7 @@ S_do_chomp(pTHX_ SV *retval, SV *sv, bool chomping)
 		if (rslen == 1) {
 		    if (*s != *rsptr)
 			goto nope;
-		    ++SvIVX(retval);
+		    ++count;
 		}
 		else {
 		    if (len < rslen - 1)
@@ -889,7 +890,7 @@ S_do_chomp(pTHX_ SV *retval, SV *sv, bool chomping)
 		    s -= rslen - 1;
 		    if (memNE(s, rsptr, rslen))
 			goto nope;
-		    SvIVX(retval) += rs_charlen;
+		    count += rs_charlen;
 		}
 	    }
 	    s = SvPV_force_nomg_nolen(sv);
@@ -936,6 +937,7 @@ S_do_chomp(pTHX_ SV *retval, SV *sv, bool chomping)
 	    sv_setpvs(retval, "");
 	SvSETMAGIC(sv);
     }
+    return count;
 }
 
 
@@ -946,9 +948,9 @@ PP(pp_schop)
     dSP; dTARGET;
     const bool chomping = PL_op->op_type == OP_SCHOMP;
 
+    const size_t count = do_chomp(TARG, TOPs, chomping);
     if (chomping)
-	sv_setiv(TARG, 0);
-    do_chomp(TARG, TOPs, chomping);
+	sv_setiv(TARG, count);
     SETTARG;
     RETURN;
 }
@@ -960,11 +962,12 @@ PP(pp_chop)
 {
     dSP; dMARK; dTARGET; dORIGMARK;
     const bool chomping = PL_op->op_type == OP_CHOMP;
+    size_t count = 0;
 
-    if (chomping)
-	sv_setiv(TARG, 0);
     while (MARK < SP)
-	do_chomp(TARG, *++MARK, chomping);
+	count += do_chomp(TARG, *++MARK, chomping);
+    if (chomping)
+	sv_setiv(TARG, count);
     SP = ORIGMARK;
     XPUSHTARG;
     RETURN;

proto.h
@@ -6471,7 +6471,7 @@ PERL_STATIC_NO_RET void	S_usage(pTHX)
 
 #endif
 #if defined(PERL_IN_PP_C)
-STATIC void	S_do_chomp(pTHX_ SV *retval, SV *sv, bool chomping)
+STATIC size_t	S_do_chomp(pTHX_ SV *retval, SV *sv, bool chomping)
 			__attribute__nonnull__(pTHX_1)
 			__attribute__nonnull__(pTHX_2);
 #define PERL_ARGS_ASSERT_DO_CHOMP	\

t/op/chop.t
@@ -6,7 +6,7 @@ BEGIN {
     require './test.pl'; require './charset_tools.pl';
 }
 
-plan tests => 144;
+plan tests => 148;
 
 $_ = 'abc';
 $c = foo();
@@ -273,4 +273,24 @@ $/ = "\n";
     $uid = $line[-1];
     is($uid, $expected,
         "RT #123057: chomp works as expected on split");
+}
+
+{
+    my $a = local $/ = 7;
+    $a = chomp $a;
+    is $a, 1, 'lexical $a = chomp $a when $a eq $/ eq 7';
+    $a = $/ = 0;
+    $a = chomp $a;
+    is $a, 1, 'lexical $a = chomp $a when $a eq $/ eq 0';
+    my @a = "7";
+    for my $b($a[0]) {
+        $/ = 7;
+        $b = chomp @a;
+        is $b, 1,
+          'lexical $b = chomp @a when $b eq $/ eq 7 and \$a[0] == \$b';
+        $b = $/ = 0;
+        $b = chomp @a;
+        is $b, 1,
+          'lexical $b = chomp @a when $b eq $/ eq 0 and \$a[0] == \$b';
+    }
 }