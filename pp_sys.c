Perl/perl5

commit 92f2ac5f91d4e93cd51cdb5d8e967e1a73c9d7dc

&CORE::setpgrp()
This commit allows &CORE::setpgrp to be called through references and
via ampersand syntax.  pp_setpgrp is modified to take into account the
nulls pushed on to the stack in pp_coreargs, which happens because
pp_coreargs has no other way to tell setpgrp how many arguments it’s
actually getting.  See commit 0163043a for details.

pp_sys.c
@@ -4342,15 +4342,11 @@ PP(pp_setpgrp)
     dVAR; dSP; dTARGET;
     Pid_t pgrp;
     Pid_t pid;
-    if (MAXARG == 1) pgrp = 0, pid = TOPi;
-    else if (MAXARG < 2) {
-	pgrp = 0;
+    pgrp = MAXARG == 2 && (TOPs||POPs) ? POPi : 0;
+    if (MAXARG > 0) pid = TOPs && TOPi;
+    else {
 	pid = 0;
 	XPUSHi(-1);
-    }
-    else {
-	pgrp = POPi;
-	pid = TOPi;
     }
 
     TAINT_PROPER("setpgrp");

gv.c
@@ -1358,7 +1358,7 @@ Perl_gv_fetchpvn_flags(pTHX_ const char *nambeg, STRLEN full_len, I32 flags,
 	    case KEY_lstat:
 	    case KEY_pop:
 	    case KEY_push:
-	    case KEY_setpgrp: case KEY_shift:
+	    case KEY_shift:
 	    case KEY_splice:
 	    case KEY_stat:
 	    case KEY_system:

t/op/coreamp.t
@@ -657,7 +657,18 @@ test_proto "sem$_" for qw "ctl get op";
 test_proto 'send';
 
 test_proto "set$_" for qw '
-  grent hostent netent priority protoent pwent servent sockopt
+  grent hostent netent
+';
+
+test_proto 'setpgrp';
+$tests +=2;
+eval { &mysetpgrp( 0) };
+pass "&setpgrp with one argument";
+eval { &mysetpgrp };
+pass "&setpgrp with no arguments";
+
+test_proto "set$_" for qw '
+  priority protoent pwent servent sockopt
 ';
 
 test_proto "shm$_" for qw "ctl get read write";

commit bae8cfc6926f9b04d8adac6a9ee6d563cc18263f

Don’t treat setpgrp($nonzero) as setpgrp(1)
This was broken inadvertently by 92f2ac5f (5.15.3).

I really have no idea how to test this.  I only confirmed the bug and
its fix via a temporary warn statement in pp_setpgrp (obviously not
included in this commit).

pp_sys.c
@@ -4449,7 +4449,7 @@ PP(pp_setpgrp)
     Pid_t pgrp;
     Pid_t pid;
     pgrp = MAXARG == 2 && (TOPs||POPs) ? POPi : 0;
-    if (MAXARG > 0) pid = TOPs && TOPi;
+    if (MAXARG > 0) pid = TOPs ? TOPi : 0;
     else {
 	pid = 0;
 	XPUSHi(-1);