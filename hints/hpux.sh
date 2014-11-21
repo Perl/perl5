ff265b04d5aca2feab3937e669b95fe0aefae8cd
Jarkko Hietaniemi <jhi@iki.fi>
2014-11-20 20:19:27 -0500

    Since HP cc is strict c89, use -std=c89 with gcc.
    Mitigates the downside of 0d55a45a a bit.

--- hints/hpux.sh
+++ hints/hpux.sh
@@ -76,6 +76,10 @@ case "$prefix" in
 case `$cc -v 2>&1`"" in
     *gcc*)  ccisgcc="$define"
 	    ccflags="$cc_cppflags"
+
+	    # Since the HP cc is strict C89, let's follow suit.
+	    ccflags="$ccflags -std=c89"
+
 	    if [ "X$gccversion" = "X" ]; then
 		# Done too late in Configure if hinted
 		gccversion=`$cc -dumpversion`
