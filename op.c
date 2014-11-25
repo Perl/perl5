Perl/perl5

commit e41e9865be5555602763ac0cf98065a8f3f38189

[perl #123062] & proto: only sub{} and \&sub
It was allowing \@array, \%hash, \($list, %of, @refs), undef, and
recently also \$scalar.  It should only allow sub{} and \&sub.

op.c
@@ -11153,8 +11153,11 @@ Perl_ck_entersub_args_proto(pTHX_ OP *entersubop, GV *namegv, SV *protosv)
 	    case '&':
 		proto++;
 		arg++;
-		if (o3->op_type != OP_REFGEN && o3->op_type != OP_SREFGEN
-		 && o3->op_type != OP_UNDEF)
+		if (o3->op_type != OP_SREFGEN
+		 || (  cUNOPx(cUNOPx(o3)->op_first)->op_first->op_type
+			!= OP_ANONCODE
+		    && cUNOPx(cUNOPx(o3)->op_first)->op_first->op_type
+			!= OP_RV2CV))
 		    bad_type_gv(arg,
 			    arg == 1 ? "block or sub {}" : "sub {}",
 			    namegv, 0, o3);

t/comp/proto.t
@@ -18,7 +18,7 @@ BEGIN {
 # strict
 use strict;
 
-print "1..201\n";
+print "1..206\n";
 
 my $i = 1;
 
@@ -289,6 +289,21 @@ a_sub \&tmp_sub_1;
 @array = ( \&tmp_sub_1 );
 eval 'a_sub @array';
 print "not " unless $@;
+printf "ok %d\n",$i++;
+eval 'a_sub \@array';
+print "not " unless $@ =~ /Type of arg/;
+printf "ok %d\n",$i++;
+eval 'a_sub \%hash';
+print "not " unless $@ =~ /Type of arg/;
+printf "ok %d\n",$i++;
+eval 'a_sub \$scalar';
+print "not " unless $@ =~ /Type of arg/;
+printf "ok %d\n",$i++;
+eval 'a_sub \($list, %of, @refs)';
+print "not " unless $@ =~ /Type of arg/;
+printf "ok %d\n",$i++;
+eval 'a_sub undef';
+print "not " unless $@ =~ /Type of arg/;
 printf "ok %d\n",$i++;
 
 ##

commit 60f71a82f635e2a8a70b36c2a82b27b78602408f

Allow \(&sub) for & proto
I accidentally broke this in e41e9865.  Prior to this commit,
\&foo was becoming srefgen and \(&foo) was becoming refgen.
Srefgen is a slightly faster version of refgen that only han-
dles one item.  The easiest fix was to change the logic
in op.c:ck_spair so that \(&sub) with parens becomes srefgen.

op.c
@@ -9358,10 +9358,11 @@ Perl_ck_spair(pTHX_ OP *o)
 	    const OPCODE type = newop->op_type;
 	    if (OP_HAS_SIBLING(newop))
 		return o;
-	    if (o->op_type == OP_REFGEN && !(newop->op_flags & OPf_PARENS)
-		&& (type == OP_RV2AV || type == OP_PADAV
-		 || type == OP_RV2HV || type == OP_PADHV
-		 || type == OP_RV2CV))
+	    if (o->op_type == OP_REFGEN
+	     && (  type == OP_RV2CV
+		|| (  !(newop->op_flags & OPf_PARENS)
+		   && (  type == OP_RV2AV || type == OP_PADAV
+		      || type == OP_RV2HV || type == OP_PADHV))))
 	    	NOOP; /* OK (allow srefgen for \@a and \%h) */
 	    else if (!(PL_opargs[type] & OA_RETSCALAR))
 		return o;

t/comp/proto.t
@@ -18,7 +18,7 @@ BEGIN {
 # strict
 use strict;
 
-print "1..206\n";
+print "1..207\n";
 
 my $i = 1;
 
@@ -285,6 +285,7 @@ sub tmp_sub_1 { printf "ok %d\n",$i++ }
 
 a_sub { printf "ok %d\n",$i++ };
 a_sub \&tmp_sub_1;
+a_sub \(&tmp_sub_1);
 
 @array = ( \&tmp_sub_1 );
 eval 'a_sub @array';