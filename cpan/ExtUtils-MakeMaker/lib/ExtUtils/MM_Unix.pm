7ff69a2dfa48adbf53463c1e2d00ff80e4fd34f5
Jarkko Hietaniemi <jhi@iki.fi>
2014-11-26 19:29:05 +0000

    [PATCH] Do not use ccstdflags with the core extensions.
    Signed-off-by: Chris 'BinGOs' Williams <chris@bingosnet.co.uk>

--- cpan/ExtUtils-MakeMaker/lib/ExtUtils/MM_Unix.pm
+++ cpan/ExtUtils-MakeMaker/lib/ExtUtils/MM_Unix.pm
@@ -192,9 +192,10 @@ sub cflags {
 	= @Config{qw(cc ccflags optimize shellflags)};
 
     # Perl 5.21.4 adds the (gcc) warning (-Wall ...) and std (-std=c89)
-    # flags to the %Config, and the modules in the core can be built
-    # with those.
-    my @ccextraflags = qw(ccwarnflags ccstdflags);
+    # flags to the %Config, and the modules in the core should be built
+    # with the warning flags, but NOT the -std=c89 flags (the latter
+    # would break using any system header files that are strict C99).
+    my @ccextraflags = qw(ccwarnflags);
     if ($ENV{PERL_CORE}) {
       for my $x (@ccextraflags) {
         if (exists $Config{$x}) {
