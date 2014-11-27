4cd7ec951b26fa1d08b01152a8c4c871561a2a62
Chris 'BinGOs' Williams <chris@bingosnet.co.uk>
2014-11-27 11:54:30 +0000

    Module-CoreList is at 5.20141120 on teh CPAN

--- Porting/Maintainers.pl
+++ Porting/Maintainers.pl
@@ -803,7 +803,7 @@ use File::Glob qw(:case);
     },
 
     'Module::CoreList' => {
-        'DISTRIBUTION' => 'BINGOS/Module-CoreList-5.20141020.tar.gz',
+        'DISTRIBUTION' => 'BINGOS/Module-CoreList-5.20141120.tar.gz',
         'FILES'        => q[dist/Module-CoreList],
     },
 
