93fa278758aa71f9c91078585c82898a9c7b8eb6
Father Chrysostomos <sprout@cpan.org>
2014-11-27 22:35:17 -0800

    Remove proto magic type
    It is no longer in use, as of two commits ago.

--- mg_raw.h
+++ mg_raw.h
@@ -12,8 +12,6 @@
       "/* arylen '#' Array length ($#ary) */" },
     { '%', "magic_vtable_max | PERL_MAGIC_VALUE_MAGIC",
       "/* rhash '%' Extra data for restricted hashes */" },
-    { '&', "magic_vtable_max",
-      "/* proto '&' my sub prototype CV */" },
     { '*', "want_vtbl_debugvar",
       "/* debugvar '*' $DB::single, signal, trace vars */" },
     { '.', "want_vtbl_pos | PERL_MAGIC_VALUE_MAGIC",
