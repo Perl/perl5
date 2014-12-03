Perl/perl5

commit 05a502dc1dc0cd21542a3e13fe0a9385fae70ec7

Skip padsv op in $lex =~ ...
m//, s/// and y/// already have logic to deal with implicit lexical
$_.  The pad offset of $_ is stored in the match op itself.  We can
take advantage of that and extend it to lexical variables in general.
That way we have fewer ops to execute, as $lex =~ // no longer calls
pp_padsv.  It also allows lexical variables’ names to be mentioned in
uninitialized warnings for y///.

lib/B/Deparse.pm
@@ -4753,7 +4753,7 @@ sub tr_decode_utf8 {
 
 sub pp_trans {
     my $self = shift;
-    my($op, $cx) = @_;
+    my($op, $cx, $morflags) = @_;
     my($from, $to);
     my $class = class($op);
     my $priv_flags = $op->private;
@@ -4770,10 +4770,16 @@ sub pp_trans {
     $flags .= "d" if $priv_flags & OPpTRANS_DELETE;
     $to = "" if $from eq $to and $flags eq "";
     $flags .= "s" if $priv_flags & OPpTRANS_SQUASH;
-    return $self->keyword("tr") . double_delim($from, $to) . $flags;
+    $flags .= $morflags if defined $morflags;
+    my $ret = $self->keyword("tr") . double_delim($from, $to) . $flags;
+    if (my $targ = $op->targ) {
+	return $self->maybe_parens($self->padname($targ) . " =~ $ret",
+				   $cx, 20);
+    }
+    return $ret;
 }
 
-sub pp_transr { &pp_trans . 'r' }
+sub pp_transr { push @_, 'r'; goto &pp_trans }
 
 sub re_dq_disambiguate {
     my ($first, $last) = @_;
@@ -4946,6 +4952,10 @@ sub matchop {
 	$var = $self->deparse($kid, 20);
 	$kid = $kid->sibling;
     }
+    elsif ($name eq 'match' and my $targ = $op->targ) {
+	$binop = 1;
+	$var = $self->padname($targ);
+    }
     my $quote = 1;
     my $pmflags = $op->pmflags;
     my $extended = ($pmflags & PMf_EXTENDED);
@@ -5084,6 +5094,10 @@ sub pp_subst {
 	$binop = 1;
 	$var = $self->deparse($kid, 20);
 	$kid = $kid->sibling;
+    }
+    elsif (my $targ = $op->targ) {
+	$binop = 1;
+	$var = $self->padname($targ);
     }
     my $flags = "";
     my $pmflags = $op->pmflags;

op.c
@@ -3577,22 +3577,31 @@ Perl_bind_match(pTHX_ I32 type, OP *left, OP *right)
 	right->op_targ = 0;
 	right->op_private &= ~OPpTARGET_MY;
     }
-    if (!(right->op_flags & OPf_STACKED) && ismatchop) {
+    if (!(right->op_flags & OPf_STACKED) && !right->op_targ && ismatchop) {
 	OP *newleft;
 
-	right->op_flags |= OPf_STACKED;
-	if (rtype != OP_MATCH && rtype != OP_TRANSR &&
+        if (left->op_type == OP_PADSV
+         && !(left->op_private & OPpLVAL_INTRO))
+        {
+            right->op_targ = left->op_targ;
+            op_free(left);
+            o = right;
+        }
+        else {
+            right->op_flags |= OPf_STACKED;
+            if (rtype != OP_MATCH && rtype != OP_TRANSR &&
             ! (rtype == OP_TRANS &&
                right->op_private & OPpTRANS_IDENTICAL) &&
 	    ! (rtype == OP_SUBST &&
 	       (cPMOPx(right)->op_pmflags & PMf_NONDESTRUCT)))
-	    newleft = op_lvalue(left, rtype);
-	else
-	    newleft = left;
-	if (right->op_type == OP_TRANS || right->op_type == OP_TRANSR)
-	    o = newBINOP(OP_NULL, OPf_STACKED, scalar(newleft), right);
-	else
-	    o = op_prepend_elem(rtype, scalar(newleft), right);
+		newleft = op_lvalue(left, rtype);
+	    else
+		newleft = left;
+	    if (right->op_type == OP_TRANS || right->op_type == OP_TRANSR)
+		o = newBINOP(OP_NULL, OPf_STACKED, scalar(newleft), right);
+	    else
+		o = op_prepend_elem(rtype, scalar(newleft), right);
+	}
 	if (type == OP_NOT)
 	    return newUNOP(OP_NOT, 0, scalar(o));
 	return o;

sv.c
@@ -15632,14 +15632,16 @@ S_find_uninit_var(pTHX_ const OP *const obase, const SV *const uninit_sv,
     case OP_SUBST:
     case OP_MATCH:
 	if ( !(obase->op_flags & OPf_STACKED)) {
-	    if (uninit_sv == ((obase->op_private & OPpTARGET_MY)
-				 ? PAD_SVl(obase->op_targ)
-				 : DEFSV))
+	    if (uninit_sv == DEFSV)
 	    {
 		sv = sv_newmortal();
 		sv_setpvs(sv, "$_");
 		return sv;
 	    }
+	    else if (obase->op_targ
+		  && uninit_sv == PAD_SVl(obase->op_targ))
+		return varname(NULL, '$', obase->op_targ, NULL, 0,
+			       FUV_SUBSCRIPT_NONE);
 	}
 	goto do_op;
 

t/lib/warnings/9uninit
@@ -818,6 +818,9 @@ $foo =~ s/./$m1/e;
 undef $g1;
 $m1 = '$g1';
 $foo =~ s//$m1/ee;
+undef $m1;
+$m1 =~ tr/x/y/;		undef $m1;
+$m1 =~ tr/x/y/r;
 EXPECT
 Use of my $_ is experimental at - line 16.
 Use of uninitialized value $_ in pattern match (m//) at - line 5.
@@ -882,6 +885,8 @@ Use of uninitialized value $m1 in regexp compilation at - line 43.
 Use of uninitialized value $g1 in substitution iterator at - line 43.
 Use of uninitialized value $m1 in substitution (s///) at - line 44.
 Use of uninitialized value in substitution iterator at - line 47.
+Use of uninitialized value $m1 in transliteration (tr///) at - line 49.
+Use of uninitialized value $m1 in transliteration (tr///) at - line 50.
 ########
 use warnings 'uninitialized';
 my ($m1);

commit 9e32885aec635971fc615833b86a648a39d78173

Fix deparsing of $lexical =~ //
I broke this in 05a502dc.

lib/B/Deparse.pm
@@ -5122,7 +5122,8 @@ sub matchop {
 	$var = $self->deparse($kid, 20);
 	$kid = $kid->sibling;
     }
-    elsif ($name eq 'match' and my $targ = $op->targ) {
+           # not $name; $name will be 'm' for both match and split
+    elsif ($op->name eq 'match' and my $targ = $op->targ) {
 	$binop = 1;
 	$var = $self->padname($targ);
     }

lib/B/Deparse.t
@@ -1098,6 +1098,10 @@ print /$s[1]/;
 # /$#a/
 print /$#main::a/;
 ####
+# $lexical =~ //
+my $x;
+$x =~ //;
+####
 # [perl #91318] /regexp/applaud
 print /a/a, s/b/c/a;
 print /a/aa, s/b/c/aa;