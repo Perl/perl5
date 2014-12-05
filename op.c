Perl/perl5

commit 987c96916e35a543383e96eebb4ef8b2c8ef66d0

Fold join to const or stringify where possible
Due to the exigencies of the implementation, "$_->$*" ends up with a
join op (join $", $$_), which is unnecessary.  This gave me the idea
of folding it where possible (instead of trying to tackle it in
toke.c), which would also make explicit joins benefit, too.

If the arguments are a simple scalar or constant followed by a
single-item list, then the join can become a stringify, and the sepa-
rator can simply disappear.

Further (and this is unrelated to "$_->$*"), if all of join’s argu-
ments are constant, the whole thing can be folded to a const op.

op.c
@@ -3872,6 +3872,7 @@ S_fold_constants(pTHX_ OP *o)
     OP * VOL curop;
     OP *newop;
     VOL I32 type = o->op_type;
+    bool folded;
     SV * VOL sv = NULL;
     int ret = 0;
     I32 oldscope;
@@ -4018,6 +4019,7 @@ S_fold_constants(pTHX_ OP *o)
     if (ret)
 	goto nope;
 
+    folded = o->op_folded;
     op_free(o);
     assert(sv);
     if (type == OP_STRINGIFY) SvPADTMP_off(sv);
@@ -4030,7 +4032,11 @@ S_fold_constants(pTHX_ OP *o)
     else
     {
 	newop = newSVOP(OP_CONST, 0, MUTABLE_SV(sv));
-	if (type != OP_STRINGIFY) newop->op_folded = 1;
+	/* OP_STRINGIFY and constant folding are used to implement qq.
+	   Here the constant folding is an implementation detail that we
+	   want to hide.  If the stringify op is itself already marked
+	   folded, however, then it is actually a folded join.  */
+	if (type != OP_STRINGIFY || folded) newop->op_folded = 1;
     }
     return newop;
 
@@ -10606,7 +10612,7 @@ Perl_ck_split(pTHX_ OP *o)
 OP *
 Perl_ck_join(pTHX_ OP *o)
 {
-    const OP * const kid = OP_SIBLING(cLISTOPo->op_first);
+    OP * const kid = OP_SIBLING(cLISTOPo->op_first);
 
     PERL_ARGS_ASSERT_CK_JOIN;
 
@@ -10622,6 +10628,23 @@ Perl_ck_join(pTHX_ OP *o)
 			SVfARG(msg), SVfARG(msg));
 	}
     }
+    if (kid->op_type == OP_CONST  /* an innocent, unsuspicious separator */
+     || (kid->op_type == OP_PADSV && !(kid->op_private & OPpLVAL_INTRO))
+     || (kid->op_type==OP_RV2SV && kUNOP->op_first->op_type == OP_GV
+	&& !(kid->op_private & (OPpLVAL_INTRO|OPpOUR_INTRO))))
+    {
+	const OP * const bairn = OP_SIBLING(kid); /* the list */
+	if (bairn && !OP_HAS_SIBLING(bairn) /* single-item list */
+	 && PL_opargs[bairn->op_type] & OA_RETSCALAR)
+	{
+	    OP * const ret = convert(OP_STRINGIFY, 0,
+				     op_sibling_splice(o, kid, 1, NULL));
+	    op_free(o);
+	    ret->op_folded = 1;
+	    return ret;
+	}
+    }
+
     return ck_fun(o);
 }
 

MANIFEST
@@ -5150,6 +5150,7 @@ t/op/negate.t			See if unary minus works
 t/op/not.t			See if not works
 t/op/numconvert.t		See if accessing fields does not change numeric values
 t/op/oct.t			See if oct and hex work
+t/op/opt.t			Test presence of some op optimisations
 t/op/ord.t			See if ord works
 t/op/or.t			See if || works in weird situations
 t/op/overload_integer.t		See if overload::constant for integer works after "use".

lib/B/Deparse-core.t
@@ -36,7 +36,7 @@ BEGIN {
 
 use strict;
 use Test::More;
-plan tests => 4018;
+plan tests => 4006;
 
 use feature (sprintf(":%vd", $^V)); # to avoid relying on the feature
                                     # logic to add CORE::
@@ -522,7 +522,7 @@ hex              01    $
 index            23    p
 int              01    $
 ioctl            3     p
-join             123   p
+join             13    p
 keys             1     - # also tested specially
 kill             123   p
 # last handled specially

opcode.h
@@ -1888,7 +1888,7 @@ EXTCONST U32 PL_opargs[] = {
 	0x00091480,	/* unpack */
 	0x0002140f,	/* pack */
 	0x00111408,	/* split */
-	0x0002140d,	/* join */
+	0x0002140f,	/* join */
 	0x00002401,	/* list */
 	0x00224200,	/* lslice */
 	0x00002405,	/* anonlist */

regen/opcodes
@@ -240,7 +240,7 @@ kvhslice	key/value hash slice	ck_null		m@	H L
 unpack		unpack			ck_fun		u@	S S?
 pack		pack			ck_fun		fmst@	S L
 split		split			ck_split	t@	S S S
-join		join or string		ck_join		mst@	S L
+join		join or string		ck_join		fmst@	S L
 
 # List operators.
 

sv.c
@@ -15743,17 +15743,21 @@ Perl_report_uninit(pTHX_ const SV *uninit_sv)
 {
     if (PL_op) {
 	SV* varname = NULL;
+	const char *desc;
 	if (uninit_sv && PL_curpad) {
 	    varname = find_uninit_var(PL_op, uninit_sv,0);
 	    if (varname)
 		sv_insert(varname, 0, 0, " ", 1);
 	}
+	desc = PL_op->op_type == OP_STRINGIFY && PL_op->op_folded
+		? "join or string"
+		: OP_DESC(PL_op);
         /* PL_warn_uninit_sv is constant */
         GCC_DIAG_IGNORE(-Wformat-nonliteral);
 	/* diag_listed_as: Use of uninitialized value%s */
 	Perl_warner(aTHX_ packWARN(WARN_UNINITIALIZED), PL_warn_uninit_sv,
 		SVfARG(varname ? varname : &PL_sv_no),
-		" in ", OP_DESC(PL_op));
+		" in ", desc);
         GCC_DIAG_RESTORE;
     }
     else {

t/lib/warnings/9uninit
@@ -1126,7 +1126,6 @@ Use of uninitialized value $m1 in regexp compilation at - line 8.
 Use of uninitialized value $g1 in split at - line 8.
 Use of uninitialized value $m2 in split at - line 8.
 Use of uninitialized value $m1 in join or string at - line 10.
-Use of uninitialized value $m1 in join or string at - line 11.
 Use of uninitialized value $m2 in join or string at - line 11.
 Use of uninitialized value $m1 in join or string at - line 12.
 Use of uninitialized value $m2 in join or string at - line 12.

t/lib/warnings/op
@@ -387,7 +387,7 @@ $a{0} ;			# OP_HELEM
 @a{0} ;			# OP_HSLICE
 unpack "a", "a" ;	# OP_UNPACK
 pack $a,"" ;		# OP_PACK
-join "" ;		# OP_JOIN
+join "", @_ ;		# OP_JOIN
 (@a)[0,1] ;		# OP_LSLICE
 			# OP_ANONLIST
 			# OP_ANONHASH

commit 6d4eed2136c73d9331ed2edb2e7650c90b974ea7

Return fresh scalar from join(const,const)
$ perl5.20.1 -Ilib  -le 'for(1,2) { push @_, \join "x", 1 } print for @_'
SCALAR(0x7fb131005438)
SCALAR(0x7fb131005648)
$ ./perl -Ilib  -le 'for(1,2) { push @_, \join "x", 1 } print for @_'
SCALAR(0x7fe612831b30)
SCALAR(0x7fe612831b30)

Notice how we now get two references to the same scalar.  I broke this
accidentally in 987c9691.  If join has two arguments, it gets con-
verted to a stringify op.  The stringify op might get constant-folded,
and folding of stringify is special, because the parser uses it
itself to implement qq().  So I had ck_join set op_folded to flag
the op as being a folded join.  Only that came too late, because
op_convert_list(OP_STRINGIFY,...) folds the op before it returns it.
Hence, the folded constant was flagged the wrong way, and stopped
being implicitly copied by refgen (\).

op.c
@@ -4225,7 +4225,8 @@ S_fold_constants(pTHX_ OP *o)
     folded = cBOOL(o->op_folded);
     op_free(o);
     assert(sv);
-    if (type == OP_STRINGIFY) SvPADTMP_off(sv);
+    if (type == OP_STRINGIFY && !folded)
+	SvPADTMP_off(sv);
     else if (!SvIMMORTAL(sv)) {
 	SvPADTMP_on(sv);
 	SvREADONLY_on(sv);
@@ -4450,6 +4451,8 @@ Perl_op_convert_list(pTHX_ I32 type, I32 flags, OP *o)
 
     CHANGE_TYPE(o, type);
     o->op_flags |= flags;
+    if (flags & OPf_FOLDED)
+	o->op_folded = 1;
 
     o = CHECKOP(type, o);
     if (o->op_type != (unsigned)type)
@@ -10973,10 +10976,9 @@ Perl_ck_join(pTHX_ OP *o)
 	if (bairn && !OP_HAS_SIBLING(bairn) /* single-item list */
 	 && PL_opargs[bairn->op_type] & OA_RETSCALAR)
 	{
-	    OP * const ret = op_convert_list(OP_STRINGIFY, 0,
+	    OP * const ret = op_convert_list(OP_STRINGIFY, OPf_FOLDED,
 				     op_sibling_splice(o, kid, 1, NULL));
 	    op_free(o);
-	    ret->op_folded = 1;
 	    return ret;
 	}
     }

op.h
@@ -139,6 +139,10 @@ Deprecated.  Use C<GIMME_V> instead.
                                 /*  On OP_PADRANGE, push @_ */
                                 /*  On OP_DUMP, has no label */
                                 /*  On OP_UNSTACK, in a C-style for loop */
+/* There is no room in op_flags for this one, so it has its own bit-
+   field member (op_folded) instead.  The flag is only used to tell
+   op_convert_list to set op_folded.  */
+#define OPf_FOLDED      1<<16
 
 /* old names; don't use in new code, but don't break them, either */
 #define OPf_LIST	OPf_WANT_LIST

t/op/join.t
@@ -6,7 +6,7 @@ BEGIN {
     require './test.pl';
 }
 
-plan tests => 28;
+plan tests => 29;
 
 @x = (1, 2, 3);
 is( join(':',@x), '1:2:3', 'join an array with character');
@@ -124,3 +124,7 @@ package o { use overload q|""| => sub { ${$_[0]}++ } }
   is $_, "1a2a3a4a5a6a7a8a9a10", 'join, $overloaded, LIST';
   is $$o, "b", 'overloading was called once on overloaded separator';
 }
+
+for(1,2) { push @_, \join "x", 1 }
+isnt $_[1], $_[0],
+    'join(const, const) still returns a new scalar each time';