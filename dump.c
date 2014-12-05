947a9e0f6c5ff845ed26cd24171e7dcddc3d0a60
Jarkko Hietaniemi <jhi@iki.fi>
2014-12-04 21:23:59 -0500

    printf type-matching continues. again.

--- dump.c
+++ dump.c
@@ -993,7 +993,7 @@ Perl_do_op_dump(pTHX_ I32 level, PerlIO *file, const OP *o)
                                       (label_flags & SVf_UTF8)));
    }
         Perl_dump_indent(aTHX_ level, file, "SEQ = %u\n",
-                                             cCOPo->cop_seq);
+                         (unsigned int)cCOPo->cop_seq);
 	break;
     case OP_ENTERLOOP:
 	Perl_dump_indent(aTHX_ level, file, "REDO ===> ");
