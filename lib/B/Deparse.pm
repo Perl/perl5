Perl/perl5

commit 34b54951568575920f2307bea918f5549bd5a82f

[perl #77452] Deparse { ...; BEGIN{} } correctly
8635e3c2 (5.21.6) changed the COP sequence numbers for nested blocks,
such that most BEGIN blocks (incl. ‘use’ statements) and sub declara-
tions end up in the right place.  However, it had the side effect of
causing declarations at the end of the enclosing scope to fall out of
it and appear below.

This commit fixes that by adding an extra nulled COP to the end of the
enclosing scope if that scope ends with a sub, so the final declara-
tion gets deparsed before it.

The frequency of sub declarations at the end of the enclosing scope is
sufficiently low (I’m guessing a bit here) that this slight increase
in run-time memory usage is probably acceptable.

I had to change B::Deparse to deparse nulled COPs the same way it does
live COPs, which means we get more extraneous semicolons than before.
I hope to fix that in a forthcoming commit.  I also ran into a B bug,
in that null ops are not presented to Perl code with the right op
class (see the blessing in the patch).  I plan to fix that in a separ-
ate commit, too.

lib/B/Deparse.pm
@@ -324,7 +324,7 @@ BEGIN {
 
 
 BEGIN { for (qw[ const stringify rv2sv list glob pushmark null aelem
-		 custom ]) {
+		 custom nextstate dbstate ]) {
     eval "sub OP_\U$_ () { " . opnumber($_) . "}"
 }}
 
@@ -1442,7 +1442,7 @@ sub walk_lineseq {
 	    next;
 	}
 	my $expr2 = $self->deparse($kids[$i], (@kids != 1)/2);
-	$expr2 =~ s/^sub :/+sub :/; # statement label otherwise
+	$expr2 =~ s/^sub :(?!:)/+sub :/; # statement label otherwise
 	$expr .= $expr2;
 	$expr =~ s/;\n?\z//;
 	$callback->($expr, $i);
@@ -3371,11 +3371,14 @@ sub _op_is_or_was {
 }
 
 sub pp_null {
-    my $self = shift;
-    my($op, $cx) = @_;
+    my($self, $op, $cx) = @_;
     if (class($op) eq "OP") {
 	# old value is lost
 	return $self->{'ex_const'} if $op->targ == OP_CONST;
+	if ($op->targ == OP_NEXTSTATE || $op->targ == OP_DBSTATE) {
+	    bless $op, "B::COP"; # XXX B bug
+	    return &pp_nextstate;
+	}
     } elsif ($op->first->name eq 'pushmark'
              or $op->first->name eq 'null'
                 && $op->first->targ == OP_PUSHMARK

lib/B/Deparse.t
@@ -13,7 +13,7 @@ BEGIN {
 use warnings;
 use strict;
 
-my $tests = 28; # not counting those in the __DATA__ section
+my $tests = 29; # not counting those in the __DATA__ section
 
 use B::Deparse;
 my $deparse = B::Deparse->new();
@@ -140,7 +140,7 @@ like($a, qr/$b/,
 
 $a = `$^X $path "-MO=Deparse" -e "use constant PI => 4" 2>&1`;
 $a =~ s/-e syntax OK\n//g;
-is($a, "use constant ('PI', 4);\n",
+is($a, "();\nuse constant ('PI', 4);\n",
    "Proxy Constant Subroutines must not show up as (incorrect) prototypes");
 
 #Re: perlbug #35857, patch #24505
@@ -188,12 +188,14 @@ EOFCODE
 $a = `$^X $path "-MO=Deparse" -e "sub ::::{}sub ::::::{}" 2>&1`;
 $a =~ s/-e syntax OK\n//g;
 is($a, <<'EOCODG', "sub :::: and sub ::::::");
+();
 sub :::: {
     
 }
 sub :::::: {
     
 }
+;
 EOCODG
 
 # [perl #117311]
@@ -224,12 +226,14 @@ $a =
   `$^X $path "-MO=Deparse" -e "BEGIN{*CORE::GLOBAL::require=sub{1}}" 2>&1`;
 $a =~ s/-e syntax OK\n//g;
 is($a, <<'EOCODF', "CORE::GLOBAL::require override causing panick");
+();
 sub BEGIN {
     *CORE::GLOBAL::require = sub {
         1;
     }
     ;
 }
+;
 EOCODF
 
 # [perl #91384]
@@ -317,7 +321,7 @@ like($a, qr/my sub use;\n\(\);\nCORE::use less;/,
 $a = readpipe qq`$^X $path "-MO=Deparse" -Xe `
              .qq`"use feature q|:all|; my sub __DATA__; `
              .qq`CORE::__DATA__" 2>&1`;
-like($a, qr/my sub __DATA__;\n\(\);\nCORE::__DATA__/,
+like($a, qr/my sub __DATA__;\n.*\nCORE::__DATA__/s,
     'CORE::__DATA__ after my sub __DATA__');
 
 # sub declarations
@@ -362,6 +366,34 @@ sub BEGIN {
 }
 EOCODJ
 }
+is runperl(stderr => 1, switches => [ '-MO=-qq,Deparse', $path ], prog => '
+      {
+        {
+          die;
+          BEGIN { pop }
+        }
+        BEGIN { pop }
+      }
+      BEGIN { pop }
+  '), <<'EOCODL', 'BEGIN blocks at the end of their enclosing blocks';
+{
+    {
+        die;
+        sub BEGIN {
+            pop @ARGV;
+        }
+        ;
+    }
+    sub BEGIN {
+        pop @ARGV;
+    }
+    ;
+}
+sub BEGIN {
+    pop @ARGV;
+}
+;
+EOCODL
 
 # [perl #115066]
 my $prog = 'use constant FOO => do { 1 }; no overloading; die';

ext/B/t/concise-xs.t
@@ -158,7 +158,7 @@ my $testpkgs = {
 
 	constant => [qw/ ASSIGN CVf_LVALUE
 		     CVf_METHOD LIST_CONTEXT OP_CONST OP_LIST OP_RV2SV
-		     OP_AELEM OP_CUSTOM
+		     OP_AELEM OP_CUSTOM OP_NEXTSTATE OP_DBSTATE
 		     OP_STRINGIFY OPf_KIDS OPf_MOD OPf_REF OPf_SPECIAL
 		     OPf_PARENS
 		     OPf_STACKED OPf_WANT OPf_WANT_LIST OPf_WANT_SCALAR

op.c
@@ -1547,7 +1547,10 @@ Perl_scalar(pTHX_ OP *o)
     do_kids:
 	while (kid) {
 	    OP *sib = OP_SIBLING(kid);
-	    if (sib && kid->op_type != OP_LEAVEWHEN)
+	    if (sib && kid->op_type != OP_LEAVEWHEN
+	     && (  OP_HAS_SIBLING(sib) || sib->op_type != OP_NULL
+		|| (  sib->op_targ != OP_NEXTSTATE
+		   && sib->op_targ != OP_DBSTATE  )))
 		scalarvoid(kid);
 	    else
 		scalar(kid);
@@ -3738,6 +3741,14 @@ Perl_block_end(pTHX_ I32 floor, OP *seq)
     const int needblockscope = PL_hints & HINT_BLOCK_SCOPE;
     OP* retval = scalarseq(seq);
     OP *o;
+
+    /* XXX Is the null PL_parser check necessary here? */
+    assert(PL_parser); /* Let’s find out under debugging builds.  */
+    if (PL_parser && PL_parser->parsed_sub) {
+	o = newSTATEOP(0, NULL, NULL);
+	op_null(o);
+	retval = op_append_elem(OP_LINESEQ, retval, o);
+    }
 
     CALL_BLOCK_HOOKS(bhk_pre_end, &retval);
 
@@ -6478,6 +6489,8 @@ Perl_newSTATEOP(pTHX_ I32 flags, char *label, OP *o)
     const U32 seq = intro_my();
     const U32 utf8 = flags & SVf_UTF8;
     COP *cop;
+
+    PL_parser->parsed_sub = 0;
 
     flags &= ~SVf_UTF8;
 

parser.h
@@ -111,6 +111,7 @@ typedef struct yy_parser {
     PERL_BITFIELD16	in_pod:1;      /* lexer is within a =pod section */
     PERL_BITFIELD16	filtered:1;    /* source filters in evalbytes */
     PERL_BITFIELD16	saw_infix_sigil:1; /* saw & or * or % operator */
+    PERL_BITFIELD16	parsed_sub:1;  /* last thing parsed was a sub */
 } yy_parser;
 
 /* flags for lexer API */

perly.h
@@ -256,6 +256,6 @@ typedef union YYSTYPE
 
 
 /* Generated from:
- * aa8cc2f0979e92f76ac0fbc21000e975a6a443beed009e907eddb57f3d8dbe6a perly.y
+ * 20fd9dcde08a4a26833ba7bf337260d78986b6542a0f55c00d7a9245af45faf5 perly.y
  * d1d4df7b8e30ac9dede664af9179e6e5e7ddc7f2ad9c4eff9e2e5b32c9e16a6e regen_perly.pl
  * ex: set ro: */

perly.tab
@@ -192,31 +192,31 @@ static const yytype_int16 yyrhs[] =
 static const yytype_uint16 yyrline[] =
 {
        0,   115,   115,   114,   125,   124,   134,   133,   146,   145,
-     158,   157,   170,   169,   181,   189,   197,   200,   208,   213,
-     214,   224,   225,   234,   238,   242,   246,   253,   255,   266,
-     265,   297,   305,   304,   312,   318,   324,   335,   337,   339,
-     346,   354,   356,   353,   372,   377,   384,   383,   398,   406,
-     412,   419,   418,   433,   437,   445,   463,   464,   469,   471,
-     473,   475,   477,   479,   481,   484,   490,   491,   496,   507,
-     508,   514,   520,   521,   526,   529,   533,   538,   542,   546,
-     550,   551,   555,   561,   566,   571,   572,   577,   578,   583,
-     584,   586,   591,   593,   598,   600,   599,   618,   628,   629,
-     633,   635,   637,   639,   643,   645,   650,   654,   658,   662,
-     668,   673,   679,   685,   687,   690,   689,   700,   701,   705,
-     709,   712,   717,   722,   725,   729,   733,   736,   741,   745,
-     748,   750,   752,   757,   759,   761,   766,   768,   770,   772,
-     774,   776,   778,   780,   782,   784,   786,   791,   793,   796,
-     798,   800,   803,   806,   817,   820,   827,   829,   831,   833,
-     835,   842,   844,   848,   849,   850,   851,   852,   854,   856,
-     858,   860,   862,   864,   866,   868,   870,   872,   874,   876,
-     878,   888,   898,   908,   918,   920,   922,   925,   930,   934,
-     936,   938,   940,   943,   945,   948,   950,   952,   954,   956,
-     958,   960,   962,   964,   967,   969,   971,   973,   975,   977,
-     981,   984,   983,   996,   997,   998,  1003,  1007,  1009,  1014,
-    1016,  1019,  1021,  1023,  1028,  1030,  1035,  1036,  1042,  1046,
-    1047,  1048,  1051,  1052,  1055,  1059,  1063,  1069,  1075,  1077,
-    1081,  1085,  1086,  1090,  1091,  1095,  1096,  1101,  1103,  1105,
-    1108
+     158,   157,   170,   169,   181,   189,   197,   201,   209,   215,
+     216,   226,   227,   236,   240,   244,   248,   255,   257,   268,
+     267,   300,   308,   307,   316,   322,   328,   339,   341,   343,
+     350,   358,   360,   357,   376,   381,   388,   387,   402,   410,
+     416,   423,   422,   437,   441,   449,   467,   468,   473,   475,
+     477,   479,   481,   483,   485,   488,   494,   495,   500,   511,
+     512,   518,   524,   525,   530,   533,   537,   542,   546,   550,
+     554,   555,   559,   565,   570,   575,   576,   581,   582,   587,
+     588,   590,   595,   597,   602,   604,   603,   622,   632,   633,
+     637,   639,   641,   643,   647,   649,   654,   658,   662,   666,
+     672,   677,   683,   689,   691,   694,   693,   704,   705,   709,
+     713,   716,   721,   726,   729,   733,   737,   740,   745,   749,
+     752,   754,   756,   761,   763,   765,   770,   772,   774,   776,
+     778,   780,   782,   784,   786,   788,   790,   795,   797,   800,
+     802,   804,   807,   810,   821,   824,   831,   833,   835,   837,
+     839,   846,   848,   852,   853,   854,   855,   856,   858,   860,
+     862,   864,   866,   868,   870,   872,   874,   876,   878,   880,
+     882,   892,   902,   912,   922,   924,   926,   929,   934,   938,
+     940,   942,   944,   947,   949,   952,   954,   956,   958,   960,
+     962,   964,   966,   968,   971,   973,   975,   977,   979,   981,
+     985,   988,   987,  1000,  1001,  1002,  1007,  1011,  1013,  1018,
+    1020,  1023,  1025,  1027,  1032,  1034,  1039,  1040,  1046,  1050,
+    1051,  1052,  1055,  1056,  1059,  1063,  1067,  1073,  1079,  1081,
+    1085,  1089,  1090,  1094,  1095,  1099,  1100,  1105,  1107,  1109,
+    1112
 };
 #endif
 
@@ -1140,6 +1140,6 @@ static const toketypes yy_type_tab[] =
 };
 
 /* Generated from:
- * aa8cc2f0979e92f76ac0fbc21000e975a6a443beed009e907eddb57f3d8dbe6a perly.y
+ * 20fd9dcde08a4a26833ba7bf337260d78986b6542a0f55c00d7a9245af45faf5 perly.y
  * d1d4df7b8e30ac9dede664af9179e6e5e7ddc7f2ad9c4eff9e2e5b32c9e16a6e regen_perly.pl
  * ex: set ro: */

commit 6436970cde5c8aac6df90c1710576d85946042e4

Avoid extraneous ‘();’ when deparsing just subs
34b54951 caused a program that is empty apart from sub definitions to
be deparsed with ‘();’ for the main sub.  Previously it was omitted.
This commit removes it, not only for the sake of backward-compatibil-
ity, but also because ‘();’ is not a particularly sane way of dumping
an empty program.

lib/B/Deparse.pm
@@ -890,8 +890,25 @@ sub compile {
 	local $B::overlay = {};
 	unless (null $root) {
 	    $self->pad_subs($self->{'curcv'});
-	    $self->pessimise($root, main_start);
-	    print $self->indent($self->deparse_root($root)), "\n";
+	    # Check for a stub-followed-by-ex-cop, resulting from a program
+	    # consisting solely of sub declarations.  For backward-compati-
+	    # bility (and sane output) we don’t want to emit the stub.
+	    #   leave
+	    #     enter
+	    #     stub
+	    #     ex-nextstate (or ex-dbstate)
+	    my $kid;
+	    if ( $root->name eq 'leave'
+	     and ($kid = $root->first)->name eq 'enter'
+	     and !null($kid = $kid->sibling) and $kid->name eq 'stub'
+	     and !null($kid = $kid->sibling) and $kid->name eq 'null'
+	     and class($kid) eq 'COP' and null $kid->sibling )
+	    {
+		# ignore
+	    } else {
+		$self->pessimise($root, main_start);
+		print $self->indent($self->deparse_root($root)), "\n";
+	    }
 	}
 	my @text;
 	while (scalar(@{$self->{'subs_todo'}})) {

lib/B/Deparse.t
@@ -140,7 +140,7 @@ like($a, qr/$b/,
 
 $a = `$^X $path "-MO=Deparse" -e "use constant PI => 4" 2>&1`;
 $a =~ s/-e syntax OK\n//g;
-is($a, "();\nuse constant ('PI', 4);\n",
+is($a, "use constant ('PI', 4);\n",
    "Proxy Constant Subroutines must not show up as (incorrect) prototypes");
 
 #Re: perlbug #35857, patch #24505
@@ -188,7 +188,6 @@ EOFCODE
 $a = `$^X $path "-MO=Deparse" -e "sub ::::{}sub ::::::{}" 2>&1`;
 $a =~ s/-e syntax OK\n//g;
 is($a, <<'EOCODG', "sub :::: and sub ::::::");
-();
 sub :::: {
     
 }
@@ -225,7 +224,6 @@ $a =
   `$^X $path "-MO=Deparse" -e "BEGIN{*CORE::GLOBAL::require=sub{1}}" 2>&1`;
 $a =~ s/-e syntax OK\n//g;
 is($a, <<'EOCODF', "CORE::GLOBAL::require override causing panick");
-();
 sub BEGIN {
     *CORE::GLOBAL::require = sub {
         1;