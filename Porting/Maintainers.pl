17aa418594b780abe2ae4b2dfdd5d03e1432bd6b
Chris 'BinGOs' Williams <chris@bingosnet.co.uk>
2014-12-09 15:24:29 +0000

    Update Compress-Raw-Bzip2 to CPAN version 2.067
      [DELTA]
    
      2.067 8 Dec 2014
    
          * Silence compiler warnings

--- Porting/Maintainers.pl
+++ Porting/Maintainers.pl
@@ -204,7 +204,7 @@ use File::Glob qw(:case);
     },
 
     'Compress::Raw::Bzip2' => {
-        'DISTRIBUTION' => 'PMQS/Compress-Raw-Bzip2-2.066.tar.gz',
+        'DISTRIBUTION' => 'PMQS/Compress-Raw-Bzip2-2.067.tar.gz',
         'FILES'        => q[cpan/Compress-Raw-Bzip2],
         'EXCLUDED'     => [
             qr{^t/Test/},
--- cpan/Compress-Raw-Bzip2/lib/Compress/Raw/Bzip2.pm
+++ cpan/Compress-Raw-Bzip2/lib/Compress/Raw/Bzip2.pm
@@ -11,7 +11,7 @@ use Carp ;
 use bytes ;
 our ($VERSION, $XS_VERSION, @ISA, @EXPORT, $AUTOLOAD);
 
-$VERSION = '2.066';
+$VERSION = '2.067';
 $XS_VERSION = $VERSION; 
 $VERSION = eval $VERSION;
 
