Perl/perl5

commit 354b74ae6f54fdd18bcf40fc3afcdaddc8b2ca9d

[perl #12285] Fix str vs num inf/nan treatment
sprintf, pack and chr were treating 0+"Inf" and "Inf" differently,
even though they have the same string and numeric values.

pack was also croaking for 0+"Inf" passed to a string format.

pp.c
@@ -3394,7 +3394,7 @@ PP(pp_chr)
     SV *top = POPs;
 
     SvGETMAGIC(top);
-    if (SvNOK(top) && UNLIKELY(Perl_isinfnan(SvNV(top))))
+    if (UNLIKELY(Perl_isinfnansv(top)))
         Perl_croak(aTHX_ "Cannot chr %"NVgf, SvNV(top));
     else {
         if (!IN_BYTES /* under bytes, chr(-1) eq chr(0xff), etc. */

embed.fnc
@@ -2681,6 +2681,7 @@ Apnod     |Size_t |my_strlcpy     |NULLOK char *dst|NULLOK const char *src|Size_
 #endif
 
 Apdn	|bool	|isinfnan	|NV nv
+p	|bool	|isinfnansv	|NN SV *sv
 
 #if !defined(HAS_SIGNBIT)
 AMdnoP	|int	|Perl_signbit	|NV f

embed.h
@@ -1178,6 +1178,7 @@
 #define intro_my()		Perl_intro_my(aTHX)
 #define invert(a)		Perl_invert(aTHX_ a)
 #define io_close(a,b)		Perl_io_close(aTHX_ a,b)
+#define isinfnansv(a)		Perl_isinfnansv(aTHX_ a)
 #define jmaybe(a)		Perl_jmaybe(aTHX_ a)
 #define keyword(a,b,c)		Perl_keyword(aTHX_ a,b,c)
 #define list(a)			Perl_list(aTHX_ a)

numeric.c
@@ -1386,6 +1386,33 @@ Perl_isinfnan(NV nv)
     return FALSE;
 }
 
+/*
+=for apidoc
+
+Checks whether the argument would be either an infinity or NaN when used
+as a number, but is careful not to trigger non-numeric or uninitialized
+warnings.  it assumes the caller has done SvGETMAGIC(sv) already.
+
+=cut
+*/
+
+bool
+Perl_isinfnansv(pTHX_ SV *sv)
+{
+    PERL_ARGS_ASSERT_ISINFNANSV;
+    if (!SvOK(sv))
+        return FALSE;
+    if (SvNOKp(sv))
+        return Perl_isinfnan(SvNVX(sv));
+    if (SvIOKp(sv))
+        return FALSE;
+    {
+        STRLEN len;
+        const char *s = SvPV_nomg_const(sv, len);
+        return cBOOL(grok_infnan(&s, s+len));
+    }
+}
+
 #ifndef HAS_MODFL
 /* C99 has truncl, pre-C99 Solaris had aintl.  We can use either with
  * copysignl to emulate modfl, which is in some platforms missing or

pp_pack.c
@@ -2082,6 +2082,23 @@ S_sv_exp_grow(pTHX_ SV *sv, STRLEN needed) {
     return SvGROW(sv, len+extend+1);
 }
 
+static void
+S_sv_check_inf(pTHX_ SV *sv, I32 datumtype)
+{
+    SvGETMAGIC(sv);
+    if (UNLIKELY(Perl_isinfnansv(sv))) {
+	const I32 c = TYPE_NO_MODIFIERS(datumtype);
+	const NV nv = SvNV_nomg(sv);
+	if (c == 'w')
+	    Perl_croak(aTHX_ "Cannot compress %"NVgf" in pack", nv);
+	else
+	    Perl_croak(aTHX_ "Cannot pack %"NVgf" with '%c'", nv, (int) c);
+    }
+}
+
+#define SvIV_no_inf(sv,d) (S_sv_check_inf(aTHX_ sv,d), SvIV_nomg(sv))
+#define SvUV_no_inf(sv,d) (S_sv_check_inf(aTHX_ sv,d), SvUV_nomg(sv))
+
 STATIC
 SV **
 S_pack_rec(pTHX_ SV *cat, tempsym_t* symptr, SV **beglist, SV **endlist )
@@ -2164,22 +2181,6 @@ S_pack_rec(pTHX_ SV *cat, tempsym_t* symptr, SV **beglist, SV **endlist )
 
         needs_swap = NEEDS_SWAP(datumtype);
 
-        fromstr = PEEKFROM;
-        if (SvNOK(fromstr)) {
-            const NV nv = SvNV(fromstr);
-            if (UNLIKELY(Perl_isinfnan(nv))) {
-                const I32 c = TYPE_NO_MODIFIERS(datumtype);
-                if (!strchr("fdFD", (char)c)) { /* floats are okay */
-                    if (c == 'w')
-                        Perl_croak(aTHX_
-                                   "Cannot compress %"NVgf" in pack", nv);
-                    else
-                        Perl_croak(aTHX_ "Cannot pack %"NVgf" with '%c'",
-                                   nv, (int) c);
-                }
-            }
-        }
-
 	/* Code inside the switch must take care to properly update
 	   cat (CUR length and '\0' termination) if it updated *cur and
 	   doesn't simply leave using break */
@@ -2201,7 +2202,7 @@ S_pack_rec(pTHX_ SV *cat, tempsym_t* symptr, SV **beglist, SV **endlist )
 		from = group ? start + group->strbeg : start;
 	    }
 	    fromstr = NEXTFROM;
-	    len = SvIV(fromstr);
+	    len = SvIV_no_inf(fromstr, datumtype);
 	    goto resize;
 	case '@' | TYPE_IS_SHRIEKING:
 	case '@':
@@ -2569,7 +2570,7 @@ S_pack_rec(pTHX_ SV *cat, tempsym_t* symptr, SV **beglist, SV **endlist )
 	    while (len-- > 0) {
 		IV aiv;
 		fromstr = NEXTFROM;
-                aiv = SvIV(fromstr);
+                aiv = SvIV_no_inf(fromstr, datumtype);
 		if ((-128 > aiv || aiv > 127))
 		    Perl_ck_warner(aTHX_ packWARN(WARN_PACK),
 				   "Character in 'c' format wrapped in pack");
@@ -2584,7 +2585,7 @@ S_pack_rec(pTHX_ SV *cat, tempsym_t* symptr, SV **beglist, SV **endlist )
 	    while (len-- > 0) {
 		IV aiv;
 		fromstr = NEXTFROM;
-                aiv = SvIV(fromstr);
+                aiv = SvIV_no_inf(fromstr, datumtype);
 		if ((0 > aiv || aiv > 0xff))
 		    Perl_ck_warner(aTHX_ packWARN(WARN_PACK),
 				   "Character in 'C' format wrapped in pack");
@@ -2600,7 +2601,7 @@ S_pack_rec(pTHX_ SV *cat, tempsym_t* symptr, SV **beglist, SV **endlist )
 	    while (len-- > 0) {
 		UV auv;
 		fromstr = NEXTFROM;
-		auv = SvUV(fromstr);
+		auv = SvUV_no_inf(fromstr, datumtype);
 		if (in_bytes) auv = auv % 0x100;
 		if (utf8) {
 		  W_utf8:
@@ -2662,7 +2663,7 @@ S_pack_rec(pTHX_ SV *cat, tempsym_t* symptr, SV **beglist, SV **endlist )
 	    while (len-- > 0) {
 		UV auv;
 		fromstr = NEXTFROM;
-		auv = SvUV(fromstr);
+		auv = SvUV_no_inf(fromstr, datumtype);
 		if (utf8) {
 		    U8 buffer[UTF8_MAXLEN], *endb;
 		    endb = uvchr_to_utf8_flags(buffer, auv,
@@ -2772,7 +2773,7 @@ S_pack_rec(pTHX_ SV *cat, tempsym_t* symptr, SV **beglist, SV **endlist )
 	    while (len-- > 0) {
 		I16 ai16;
 		fromstr = NEXTFROM;
-		ai16 = (I16)SvIV(fromstr);
+		ai16 = (I16)SvIV_no_inf(fromstr, datumtype);
 		ai16 = PerlSock_htons(ai16);
                 PUSH16(utf8, cur, &ai16, FALSE);
 	    }
@@ -2782,7 +2783,7 @@ S_pack_rec(pTHX_ SV *cat, tempsym_t* symptr, SV **beglist, SV **endlist )
 	    while (len-- > 0) {
 		I16 ai16;
 		fromstr = NEXTFROM;
-		ai16 = (I16)SvIV(fromstr);
+		ai16 = (I16)SvIV_no_inf(fromstr, datumtype);
 		ai16 = htovs(ai16);
                 PUSH16(utf8, cur, &ai16, FALSE);
 	    }
@@ -2792,7 +2793,7 @@ S_pack_rec(pTHX_ SV *cat, tempsym_t* symptr, SV **beglist, SV **endlist )
 	    while (len-- > 0) {
 		unsigned short aushort;
 		fromstr = NEXTFROM;
-		aushort = SvUV(fromstr);
+		aushort = SvUV_no_inf(fromstr, datumtype);
                 PUSH_VAR(utf8, cur, aushort, needs_swap);
 	    }
             break;
@@ -2803,7 +2804,7 @@ S_pack_rec(pTHX_ SV *cat, tempsym_t* symptr, SV **beglist, SV **endlist )
 	    while (len-- > 0) {
 		U16 au16;
 		fromstr = NEXTFROM;
-		au16 = (U16)SvUV(fromstr);
+		au16 = (U16)SvUV_no_inf(fromstr, datumtype);
                 PUSH16(utf8, cur, &au16, needs_swap);
 	    }
 	    break;
@@ -2812,7 +2813,7 @@ S_pack_rec(pTHX_ SV *cat, tempsym_t* symptr, SV **beglist, SV **endlist )
 	    while (len-- > 0) {
 		short ashort;
 		fromstr = NEXTFROM;
-		ashort = SvIV(fromstr);
+		ashort = SvIV_no_inf(fromstr, datumtype);
                 PUSH_VAR(utf8, cur, ashort, needs_swap);
 	    }
             break;
@@ -2823,7 +2824,7 @@ S_pack_rec(pTHX_ SV *cat, tempsym_t* symptr, SV **beglist, SV **endlist )
 	    while (len-- > 0) {
 		I16 ai16;
 		fromstr = NEXTFROM;
-		ai16 = (I16)SvIV(fromstr);
+		ai16 = (I16)SvIV_no_inf(fromstr, datumtype);
                 PUSH16(utf8, cur, &ai16, needs_swap);
 	    }
 	    break;
@@ -2832,7 +2833,7 @@ S_pack_rec(pTHX_ SV *cat, tempsym_t* symptr, SV **beglist, SV **endlist )
 	    while (len-- > 0) {
 		unsigned int auint;
 		fromstr = NEXTFROM;
-		auint = SvUV(fromstr);
+		auint = SvUV_no_inf(fromstr, datumtype);
                 PUSH_VAR(utf8, cur, auint, needs_swap);
 	    }
 	    break;
@@ -2840,7 +2841,7 @@ S_pack_rec(pTHX_ SV *cat, tempsym_t* symptr, SV **beglist, SV **endlist )
 	    while (len-- > 0) {
 		IV aiv;
 		fromstr = NEXTFROM;
-		aiv = SvIV(fromstr);
+		aiv = SvIV_no_inf(fromstr, datumtype);
                 PUSH_VAR(utf8, cur, aiv, needs_swap);
 	    }
 	    break;
@@ -2848,7 +2849,7 @@ S_pack_rec(pTHX_ SV *cat, tempsym_t* symptr, SV **beglist, SV **endlist )
 	    while (len-- > 0) {
 		UV auv;
 		fromstr = NEXTFROM;
-		auv = SvUV(fromstr);
+		auv = SvUV_no_inf(fromstr, datumtype);
                 PUSH_VAR(utf8, cur, auv, needs_swap);
 	    }
 	    break;
@@ -2856,7 +2857,8 @@ S_pack_rec(pTHX_ SV *cat, tempsym_t* symptr, SV **beglist, SV **endlist )
             while (len-- > 0) {
 		NV anv;
 		fromstr = NEXTFROM;
-		anv = SvNV(fromstr);
+		S_sv_check_inf(fromstr, datumtype);
+		anv = SvNV_nomg(fromstr);
 
 		if (anv < 0) {
 		    *cur = '\0';
@@ -2944,7 +2946,7 @@ S_pack_rec(pTHX_ SV *cat, tempsym_t* symptr, SV **beglist, SV **endlist )
 	    while (len-- > 0) {
 		int aint;
 		fromstr = NEXTFROM;
-		aint = SvIV(fromstr);
+		aint = SvIV_no_inf(fromstr, datumtype);
                 PUSH_VAR(utf8, cur, aint, needs_swap);
 	    }
 	    break;
@@ -2953,7 +2955,7 @@ S_pack_rec(pTHX_ SV *cat, tempsym_t* symptr, SV **beglist, SV **endlist )
 	    while (len-- > 0) {
 		U32 au32;
 		fromstr = NEXTFROM;
-		au32 = SvUV(fromstr);
+		au32 = SvUV_no_inf(fromstr, datumtype);
 		au32 = PerlSock_htonl(au32);
                 PUSH32(utf8, cur, &au32, FALSE);
 	    }
@@ -2963,7 +2965,7 @@ S_pack_rec(pTHX_ SV *cat, tempsym_t* symptr, SV **beglist, SV **endlist )
 	    while (len-- > 0) {
 		U32 au32;
 		fromstr = NEXTFROM;
-		au32 = SvUV(fromstr);
+		au32 = SvUV_no_inf(fromstr, datumtype);
 		au32 = htovl(au32);
                 PUSH32(utf8, cur, &au32, FALSE);
 	    }
@@ -2973,7 +2975,7 @@ S_pack_rec(pTHX_ SV *cat, tempsym_t* symptr, SV **beglist, SV **endlist )
 	    while (len-- > 0) {
 		unsigned long aulong;
 		fromstr = NEXTFROM;
-		aulong = SvUV(fromstr);
+		aulong = SvUV_no_inf(fromstr, datumtype);
                 PUSH_VAR(utf8, cur, aulong, needs_swap);
 	    }
 	    break;
@@ -2984,7 +2986,7 @@ S_pack_rec(pTHX_ SV *cat, tempsym_t* symptr, SV **beglist, SV **endlist )
 	    while (len-- > 0) {
 		U32 au32;
 		fromstr = NEXTFROM;
-		au32 = SvUV(fromstr);
+		au32 = SvUV_no_inf(fromstr, datumtype);
                 PUSH32(utf8, cur, &au32, needs_swap);
 	    }
 	    break;
@@ -2993,7 +2995,7 @@ S_pack_rec(pTHX_ SV *cat, tempsym_t* symptr, SV **beglist, SV **endlist )
 	    while (len-- > 0) {
 		long along;
 		fromstr = NEXTFROM;
-		along = SvIV(fromstr);
+		along = SvIV_no_inf(fromstr, datumtype);
                 PUSH_VAR(utf8, cur, along, needs_swap);
 	    }
 	    break;
@@ -3004,7 +3006,7 @@ S_pack_rec(pTHX_ SV *cat, tempsym_t* symptr, SV **beglist, SV **endlist )
             while (len-- > 0) {
 		I32 ai32;
 		fromstr = NEXTFROM;
-		ai32 = SvIV(fromstr);
+		ai32 = SvIV_no_inf(fromstr, datumtype);
                 PUSH32(utf8, cur, &ai32, needs_swap);
 	    }
 	    break;
@@ -3013,7 +3015,7 @@ S_pack_rec(pTHX_ SV *cat, tempsym_t* symptr, SV **beglist, SV **endlist )
 	    while (len-- > 0) {
 		Uquad_t auquad;
 		fromstr = NEXTFROM;
-		auquad = (Uquad_t) SvUV(fromstr);
+		auquad = (Uquad_t) SvUV_no_inf(fromstr, datumtype);
                 PUSH_VAR(utf8, cur, auquad, needs_swap);
 	    }
 	    break;
@@ -3021,7 +3023,7 @@ S_pack_rec(pTHX_ SV *cat, tempsym_t* symptr, SV **beglist, SV **endlist )
 	    while (len-- > 0) {
 		Quad_t aquad;
 		fromstr = NEXTFROM;
-		aquad = (Quad_t)SvIV(fromstr);
+		aquad = (Quad_t)SvIV_no_inf(fromstr, datumtype);
                 PUSH_VAR(utf8, cur, aquad, needs_swap);
 	    }
 	    break;

proto.h
@@ -2188,6 +2188,11 @@ PERL_CALLCONV bool	Perl_is_utf8_xidfirst(pTHX_ const U8 *p)
 	assert(p)
 
 PERL_CALLCONV bool	Perl_isinfnan(NV nv);
+PERL_CALLCONV bool	Perl_isinfnansv(pTHX_ SV *sv)
+			__attribute__nonnull__(pTHX_1);
+#define PERL_ARGS_ASSERT_ISINFNANSV	\
+	assert(sv)
+
 PERL_CALLCONV OP*	Perl_jmaybe(pTHX_ OP *o)
 			__attribute__nonnull__(pTHX_1);
 #define PERL_ARGS_ASSERT_JMAYBE	\

sv.c
@@ -11518,9 +11518,10 @@ Perl_sv_vcatpvfn_flags(pTHX_ SV *const sv, const char *const pat, const STRLEN p
 	    }
 	}
 
-        if (argsv && SvNOK(argsv)) {
+        if (argsv && strchr("BbcDdiOopuUXx",*q)) {
             /* XXX va_arg(*args) case? need peek, use va_copy? */
-            infnan = UNLIKELY(Perl_isinfnan(SvNV(argsv)));
+            SvGETMAGIC(argsv);
+            infnan = UNLIKELY(isinfnansv(argsv));
         }
 
 	switch (c = *q++) {

t/op/infnan.t
@@ -33,8 +33,9 @@ my @NaN = ("NAN", "nan", "qnan", "SNAN", "NanQ", "NANS",
            "nanonano"); # RIP, Robin Williams.
 
 my @printf_fmt = qw(e f g a d u o i b x p);
-my @packi_fmt = qw(a A Z b B h H c C s S l L i I n N v V j J w W p P u U);
+my @packi_fmt = qw(c C s S l L i I n N v V j J w W U);
 my @packf_fmt = qw(f d F);
+my @packs_fmt = qw(a4 A4 Z5 b20 B20 h10 H10 u);
 
 if ($Config{ivsize} == 8) {
     push @packi_fmt, qw(q Q);
@@ -80,23 +81,41 @@ for my $f (@printf_fmt) {
 
 ok(!defined eval { $a = sprintf("%c", $PInf)}, "sprintf %c +Inf undef");
 like($@, qr/Cannot printf/, "$PInf sprintf fails");
+ok(!defined eval { $a = sprintf("%c", "Inf")},
+  "stringy sprintf %c +Inf undef");
+like($@, qr/Cannot printf/, "stringy $PInf sprintf fails");
 
 ok(!defined eval { $a = chr($PInf) }, "chr(+Inf) undef");
 like($@, qr/Cannot chr/, "+Inf chr() fails");
+ok(!defined eval { $a = chr("Inf") }, "chr(stringy +Inf) undef");
+like($@, qr/Cannot chr/, "stringy +Inf chr() fails");
 
 ok(!defined eval { $a = sprintf("%c", $NInf)}, "sprintf %c -Inf undef");
 like($@, qr/Cannot printf/, "$NInf sprintf fails");
+ok(!defined eval { $a = sprintf("%c", "-Inf")},
+  "sprintf %c stringy -Inf undef");
+like($@, qr/Cannot printf/, "stringy $NInf sprintf fails");
 
 ok(!defined eval { $a = chr($NInf) }, "chr(-Inf) undef");
 like($@, qr/Cannot chr/, "-Inf chr() fails");
+ok(!defined eval { $a = chr("-Inf") }, "chr(stringy -Inf) undef");
+like($@, qr/Cannot chr/, "stringy -Inf chr() fails");
 
 for my $f (@packi_fmt) {
     ok(!defined eval { $a = pack($f, $PInf) }, "pack $f +Inf undef");
     like($@, $f eq 'w' ? qr/Cannot compress Inf/: qr/Cannot pack Inf/,
          "+Inf pack $f fails");
+    ok(!defined eval { $a = pack($f, "Inf") },
+      "pack $f stringy +Inf undef");
+    like($@, $f eq 'w' ? qr/Cannot compress Inf/: qr/Cannot pack Inf/,
+         "stringy +Inf pack $f fails");
     ok(!defined eval { $a = pack($f, $NInf) }, "pack $f -Inf undef");
     like($@, $f eq 'w' ? qr/Cannot compress -Inf/: qr/Cannot pack -Inf/,
          "-Inf pack $f fails");
+    ok(!defined eval { $a = pack($f, "-Inf") },
+      "pack $f stringy -Inf undef");
+    like($@, $f eq 'w' ? qr/Cannot compress -Inf/: qr/Cannot pack -Inf/,
+         "stringy -Inf pack $f fails");
 }
 
 for my $f (@packf_fmt) {
@@ -108,6 +127,19 @@ for my $f (@packf_fmt) {
     eval { $b = unpack($f, $a) };
     cmp_ok($b, '==', $NInf, "pack $f -Inf equals $NInf");
 }
+
+for my $f (@packs_fmt) {
+    ok(defined eval { $a = pack($f, $PInf) }, "pack $f +Inf defined");
+    is($a, pack($f, "Inf"), "pack $f +Inf same as 'Inf'");
+
+    ok(defined eval { $a = pack($f, $NInf) }, "pack $f -Inf defined");
+    is($a, pack($f, "-Inf"), "pack $f -Inf same as 'Inf'");
+}
+
+is eval { unpack "p", pack 'p', $PInf }, "Inf", "pack p +Inf";
+is eval { unpack "P3", pack 'P', $PInf }, "Inf", "pack P +Inf";
+is eval { unpack "p", pack 'p', $NInf }, "-Inf", "pack p -Inf";
+is eval { unpack "P4", pack 'P', $NInf }, "-Inf", "pack P -Inf";
 
 for my $i (@PInf) {
     cmp_ok($i + 0 , '==', $PInf, "$i is +Inf");
@@ -223,14 +255,23 @@ for my $f (@printf_fmt) {
 
 ok(!defined eval { $a = sprintf("%c", $NaN)}, "sprintf %c NaN undef");
 like($@, qr/Cannot printf/, "$NaN sprintf fails");
+ok(!defined eval { $a = sprintf("%c", "NaN")},
+  "sprintf %c stringy NaN undef");
+like($@, qr/Cannot printf/, "stringy $NaN sprintf fails");
 
 ok(!defined eval { $a = chr($NaN) }, "chr NaN undef");
 like($@, qr/Cannot chr/, "NaN chr() fails");
+ok(!defined eval { $a = chr("NaN") }, "chr stringy NaN undef");
+like($@, qr/Cannot chr/, "stringy NaN chr() fails");
 
 for my $f (@packi_fmt) {
     ok(!defined eval { $a = pack($f, $NaN) }, "pack $f NaN undef");
     like($@, $f eq 'w' ? qr/Cannot compress NaN/: qr/Cannot pack NaN/,
          "NaN pack $f fails");
+    ok(!defined eval { $a = pack($f, "NaN") },
+       "pack $f stringy NaN undef");
+    like($@, $f eq 'w' ? qr/Cannot compress NaN/: qr/Cannot pack NaN/,
+         "stringy NaN pack $f fails");
 }
 
 for my $f (@packf_fmt) {
@@ -238,6 +279,14 @@ for my $f (@packf_fmt) {
     eval { $b = unpack($f, $a) };
     cmp_ok($b, '!=', $b, "pack $f NaN not-equals $NaN");
 }
+
+for my $f (@packs_fmt) {
+    ok(defined eval { $a = pack($f, $NaN) }, "pack $f NaN defined");
+    is($a, pack($f, "NaN"), "pack $f NaN same as 'NaN'");
+}
+
+is eval { unpack "p", pack 'p', $NaN }, "NaN", "pack p +NaN";
+is eval { unpack "P3", pack 'P', $NaN }, "NaN", "pack P +NaN";
 
 for my $i (@NaN) {
     cmp_ok($i + 0, '!=', $i + 0, "$i is NaN numerically (by not being NaN)");

commit 9911fc4e591c446844059afc8e5ec87c56a7b33e

Don’t call string overloading in chr
See <20141130160250.GC31019@pjcj.net>.  Commit 354b74ae6f broke this.

pp.c
@@ -3390,6 +3390,8 @@ PP(pp_chr)
     SV *top = POPs;
 
     SvGETMAGIC(top);
+    if (UNLIKELY(SvAMAGIC(top)))
+	top = sv_2num(top);
     if (UNLIKELY(isinfnansv(top)))
         Perl_croak(aTHX_ "Cannot chr %"NVgf, SvNV(top));
     else {

t/op/chr.t
@@ -6,7 +6,7 @@ BEGIN {
     require "./test.pl";
 }
 
-plan tests => 42;
+plan tests => 45;
 
 # Note that t/op/ord.t already tests for chr() <-> ord() rountripping.
 
@@ -80,3 +80,11 @@ sub hexes {
     is(hexes(0x200000), "f8 88 80 80 80");
 }
 
+package o {
+    use overload
+        '""' => sub { ++$o::str; "42" },
+        '0+' => sub { ++$o::num; 42 };
+}
+is chr(bless [], "o"), chr(42), 'overloading called';
+is $o::str, undef, 'chr does not call string overloading';
+is $o::num, 1,     'chr does call num overloading';