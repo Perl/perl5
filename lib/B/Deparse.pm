Perl/perl5

commit 0175f038c506e9b29efdb14421ee3fd07adea401

Deparse chdir(bareword) without quotes

lib/B/Deparse.pm
@@ -2296,7 +2296,19 @@ sub pp_tell { unop(@_, "tell") }
 sub pp_getsockname { unop(@_, "getsockname") }
 sub pp_getpeername { unop(@_, "getpeername") }
 
-sub pp_chdir { maybe_targmy(@_, \&unop, "chdir") }
+sub pp_chdir {
+    my ($self, $op, $cx) = @_;
+    if ($op->flags & OPf_SPECIAL) {
+	my $kw = $self->keyword("chdir");
+	my $kid = $self->const_sv($op->first)->PV;
+	my $code = $kw
+		 . ($cx >= 16 || $self->{'parens'} ? "($kid)" : " $kid");
+	maybe_targmy(@_, sub { $_[3] }, $code);
+    } else {
+	maybe_targmy(@_, \&unop, "chdir")
+    }
+}
+
 sub pp_chroot { maybe_targmy(@_, \&unop, "chroot") }
 sub pp_readlink { unop(@_, "readlink") }
 sub pp_rmdir { maybe_targmy(@_, \&unop, "rmdir") }

lib/B/Deparse.t
@@ -2253,3 +2253,6 @@ my $x = ($r // [])->{'foo'}[0];
 # multideref with complex middle index
 my(%h, $i, $j, $k);
 my $x = $h{'foo'}[$i + $j]{$k};
+####
+# chdir bareword
+chdir FH;

commit 3c4a43a5724de1d11192fb70923d4fd22b0b689b

Fix deparsing of chdir without args
I just broke that in 0175f038c5.  I did not realise that OPf_SPECIAL
means two different things on chdir.  (See the previous commit.)

lib/B/Deparse.pm
@@ -2298,7 +2298,7 @@ sub pp_getpeername { unop(@_, "getpeername") }
 
 sub pp_chdir {
     my ($self, $op, $cx) = @_;
-    if ($op->flags & OPf_SPECIAL) {
+    if (($op->flags & (OPf_SPECIAL|OPf_KIDS)) == (OPf_SPECIAL|OPf_KIDS)) {
 	my $kw = $self->keyword("chdir");
 	my $kid = $self->const_sv($op->first)->PV;
 	my $code = $kw

lib/B/Deparse.t
@@ -2254,5 +2254,7 @@ my $x = ($r // [])->{'foo'}[0];
 my(%h, $i, $j, $k);
 my $x = $h{'foo'}[$i + $j]{$k};
 ####
-# chdir bareword
+# chdir
+chdir 'file';
 chdir FH;
+chdir;