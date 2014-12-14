19606b446b40d13c00d4dac8f89bd25bc6c48f5c
Chris 'BinGOs' Williams <chris@bingosnet.co.uk>
2014-12-14 20:19:54 +0000

    Update Archive-Tar to CPAN version 2.04
      [DELTA]
    
    2.04  14/12/2014
    - RT#100903 enable ptargrep.t to work on VMS

--- Porting/Maintainers.pl
+++ Porting/Maintainers.pl
@@ -119,7 +119,7 @@ use File::Glob qw(:case);
 %Modules = (
 
     'Archive::Tar' => {
-        'DISTRIBUTION' => 'BINGOS/Archive-Tar-2.02.tar.gz',
+        'DISTRIBUTION' => 'BINGOS/Archive-Tar-2.04.tar.gz',
         'FILES'        => q[cpan/Archive-Tar],
         'BUGS'         => 'bug-archive-tar@rt.cpan.org',
         'EXCLUDED'     => [
--- cpan/Archive-Tar/lib/Archive/Tar/Constant.pm
+++ cpan/Archive-Tar/lib/Archive/Tar/Constant.pm
@@ -3,7 +3,7 @@ package Archive::Tar::Constant;
 BEGIN {
     require Exporter;
 
-    $VERSION    = '2.02';
+    $VERSION    = '2.04';
     @ISA        = qw[Exporter];
 
     require Time::Local if $^O eq "MacOS";
