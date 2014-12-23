ac3483e438bb2771cce2d7be1e459ccb79c1d795
Father Chrysostomos <sprout@cpan.org>
2014-12-23 08:47:25 -0800

    Increase $B::VERSION to 1.55

--- ext/B/B.pm
+++ ext/B/B.pm
@@ -15,7 +15,7 @@ require Exporter;
 # walkoptree comes from B.xs
 
 BEGIN {
-    $B::VERSION = '1.54';
+    $B::VERSION = '1.55';
     @B::EXPORT_OK = ();
 
     # Our BOOT code needs $VERSION set, and will append to @EXPORT_OK.
