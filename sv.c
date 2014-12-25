1d43193d5d388c2cc55fe51bcb525be40cd80688
Yves Orton <demerphq@gmail.com>
2014-12-25 03:53:31 +0100

    add cast to make c++ happy

--- sv.c
+++ sv.c
@@ -5959,7 +5959,7 @@ Perl_sv_get_backrefs(pTHX_ SV *const sv)
     if (SvTYPE(sv) == SVt_PVHV) {
         if (SvOOK(sv)) {
             struct xpvhv_aux * const iter = HvAUX((HV *)sv);
-            backrefs = iter->xhv_backreferences;
+            backrefs = (SV *)iter->xhv_backreferences;
         }
     } else if (SvMAGICAL(sv)) {
         MAGIC *mg = mg_find(sv, PERL_MAGIC_backref);
