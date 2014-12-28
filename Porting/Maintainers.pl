9cff81c36694ed2ce5b74269ebef3e685b8a5e63
Steve Hay <steve.m.hay@googlemail.com>
2014-12-28 13:29:06 +0000

    Note that threads is currently customized in blead

--- Porting/Maintainers.pl
+++ Porting/Maintainers.pl
@@ -1169,6 +1169,11 @@ use File::Glob qw(:case);
     'threads' => {
         'DISTRIBUTION' => 'JDHEDDEN/threads-1.96.tar.gz',
         'FILES'        => q[dist/threads],
+        'CUSTOMIZED'   => [
+            # Waiting to be merged upstream: see CPAN RT#100755
+            qw( threads.xs
+                lib/threads.pm),
+        ],
         'EXCLUDED'     => [
             qr{^examples/},
             qw( t/pod.t
