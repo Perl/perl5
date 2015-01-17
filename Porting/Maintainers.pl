6bcde12e2f1e7cf9e52bae4dd4e3b833de47f42c
Matthew Horsfall (alh) <wolfsage@gmail.com>
2015-01-17 11:40:45 -0500

    Update Porting/Maintainers.pl for Devel::PPPort 3.27->3.28.
    Missed this in b127ff5

--- Porting/Maintainers.pl
+++ Porting/Maintainers.pl
@@ -334,7 +334,7 @@ use File::Glob qw(:case);
     },
 
     'Devel::PPPort' => {
-        'DISTRIBUTION' => 'WOLFSAGE/Devel-PPPort-3.27.tar.gz',
+        'DISTRIBUTION' => 'WOLFSAGE/Devel-PPPort-3.28.tar.gz',
         # RJBS has asked MHX to have UPSTREAM be 'blead'
         # (i.e. move this from cpan/ to dist/)
         'FILES'        => q[cpan/Devel-PPPort],
