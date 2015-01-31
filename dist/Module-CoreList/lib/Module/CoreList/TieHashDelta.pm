d428331bc0f5293721c2a4354712f34f26a75583
Steve Hay <steve.m.hay@googlemail.com>
2015-01-31 23:24:14 +0000

    Complete Module::CoreList update for 5.20.2
    This applies the $VERSION, %upstream and %bug_tracker changes for the
    5.20.2 update. These parts are not applicable to blead, so were not
    committed to and cherry-picked from there.

--- dist/Module-CoreList/lib/Module/CoreList/TieHashDelta.pm
+++ dist/Module-CoreList/lib/Module/CoreList/TieHashDelta.pm
@@ -3,7 +3,7 @@ package Module::CoreList::TieHashDelta;
 use strict;
 use vars qw($VERSION);
 
-$VERSION = '5.20150128';
+$VERSION = '5.20150131';
 
 sub TIEHASH {
     my ($class, $changed, $removed, $parent) = @_;
