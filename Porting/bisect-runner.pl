1e38156c8ea63eab098ce4ef5b3ddaff3f1dd36f
Karl Williamson <khw@cpan.org>
2015-05-16 09:49:22 -0600

    Porting/bisect-runner.pl: Generalize for EBCDIC
    This commit changes to not use hard-coded code point numbers so that it
    also works on EBCDIC platforms.  An unintended consequence is that the
    VT and \f spacing controls aren't considered a sign of binary.  That
    seems correct to me.

--- Porting/bisect-runner.pl
+++ Porting/bisect-runner.pl
@@ -1249,7 +1249,7 @@ sub match_and_exit {
         while (<$fh>) {
             if ($_ =~ $re) {
                 ++$matches;
-                if (tr/\t\r\n -~\200-\377//c) {
+                if (/[^[:^cntrl:]\h\v]/a) { # Matches non-spacing non-C1 controls
                     print "Binary file $file matches\n";
                 } else {
                     $_ .= "\n" unless /\n\z/;
