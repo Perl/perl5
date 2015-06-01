db7e9ac162f18610871bbd239c0919310c5892fd
Ricardo Signes <rjbs@cpan.org>
2015-06-01 14:28:47 -0400

    corelist: add stubs for v5.23.0
    (cherry picked from commit 19aba07308a6d7f9feb9fd2f9ea4cb504e2bbd7c)

--- dist/Module-CoreList/lib/Module/CoreList.pm
+++ dist/Module-CoreList/lib/Module/CoreList.pm
@@ -4,7 +4,7 @@ use vars qw/$VERSION %released %version %families %upstream
 	    %bug_tracker %deprecated %delta/;
 use Module::CoreList::TieHashDelta;
 use version;
-$VERSION = '5.20150520';
+$VERSION = '5.20150620';
 
 sub _released_order {   # Sort helper, to make '?' sort after everything else
     (substr($released{$a}, 0, 1) eq "?")
@@ -276,6 +276,7 @@ sub changes_between {
     5.021010 => '2015-03-20',
     5.021011 => '2015-04-20',
     5.022000 => '2015-06-01',
+    5.023000 => '????-??-??',
   );
 
 for my $version ( sort { $a <=> $b } keys %released ) {
@@ -11443,6 +11444,15 @@ for my $version ( sort { $a <=> $b } keys %released ) {
         removed => {
         }
     },
+    5.023000 => {
+        delta_from => 5.022000,
+        changed => {
+            'B::Op_private'         => '5.023000',
+            'Config'                => '5.023',
+        },
+        removed => {
+        }
+    },
 );
 
 sub is_core
@@ -11994,6 +12004,13 @@ for my $version (sort { $a <=> $b } keys %delta) {
     },
     5.022000 => {
         delta_from => 5.021011,
+        changed => {
+        },
+        removed => {
+        }
+    },
+    5.023000 => {
+        delta_from => 5.022000,
         changed => {
         },
         removed => {
