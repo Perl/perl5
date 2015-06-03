45d9e107cf94223a47b7a0eb69265c851039c08f
Chris 'BinGOs' Williams <chris@bingosnet.co.uk>
2015-06-03 16:00:32 +0100

    Module-CoreList-5.20150520 is on CPAN now

--- Porting/Maintainers.pl
+++ Porting/Maintainers.pl
@@ -821,7 +821,7 @@ use File::Glob qw(:case);
     },
 
     'Module::CoreList' => {
-        'DISTRIBUTION' => 'BINGOS/Module-CoreList-5.20150420.tar.gz',
+        'DISTRIBUTION' => 'BINGOS/Module-CoreList-5.20150520.tar.gz',
         'FILES'        => q[dist/Module-CoreList],
     },
 
