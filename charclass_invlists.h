5ad25bdcb978e712488e7c2656949d6efd14e0b5
Karl Williamson <khw@cpan.org>
2015-06-18 18:45:05 -0600

    mktables: Fix logic error
    When I wrote this some years ago, I committed a logic error, so that a
    constraint I thought applied was in fact somewhat different than what I
    thought.  The new comments give the proper constraint.
    
    The code is taking the data furnished by unicode.org and massaging it
    into tables usable by the perl interpreter.  This bug can cause wrong
    tables to be generated.  Fortunately, this bug does not arise with
    typical unicode.org data sets, so it hasn't shown up as a problem.
    Fixing it makes no difference in the current Unicode version.  I
    discovered it only in compiling Unicode 1.1.5, somewhat tweaked by me,
    so not the official data there either.

--- charclass_invlists.h
+++ charclass_invlists.h
@@ -99521,7 +99521,7 @@ static const UV XPosixXDigit_invlist[] = { /* for EBCDIC POSIX-BC */
  * 1a0687fb9c6c4567e853913549df0944fe40821279a3e9cdaa6ab8679bc286fd lib/unicore/extracted/DLineBreak.txt
  * 40bcfed3ca727c19e1331f6c33806231d5f7eeeabd2e6a9e06a3740c85d0c250 lib/unicore/extracted/DNumType.txt
  * a18d502bad39d527ac5586d7bc93e29f565859e3bcc24ada627eff606d6f5fed lib/unicore/extracted/DNumValues.txt
- * 00747a7c9a1449ace1906f89d4a4497b02dbd2cf2a7459e297c2dd023c30102a lib/unicore/mktables
+ * c9326eab8d7861c3543963e555d5b927348f4467c93071db23154dece7619654 lib/unicore/mktables
  * 462c9aaa608fb2014cd9649af1c5c009485c60b9c8b15b89401fdc10cf6161c6 lib/unicore/version
  * c6884f4d629f04d1316f3476cb1050b6a1b98ca30c903262955d4eae337c6b1e regen/charset_translations.pl
  * f199f92c0b5f87882b0198936ea8ef3dc43627b57a77ac3eb9250bd2664bbd88 regen/mk_invlists.pl
--- regcharclass.h
+++ regcharclass.h
@@ -2514,7 +2514,7 @@
  * 1a0687fb9c6c4567e853913549df0944fe40821279a3e9cdaa6ab8679bc286fd lib/unicore/extracted/DLineBreak.txt
  * 40bcfed3ca727c19e1331f6c33806231d5f7eeeabd2e6a9e06a3740c85d0c250 lib/unicore/extracted/DNumType.txt
  * a18d502bad39d527ac5586d7bc93e29f565859e3bcc24ada627eff606d6f5fed lib/unicore/extracted/DNumValues.txt
- * 00747a7c9a1449ace1906f89d4a4497b02dbd2cf2a7459e297c2dd023c30102a lib/unicore/mktables
+ * c9326eab8d7861c3543963e555d5b927348f4467c93071db23154dece7619654 lib/unicore/mktables
  * 462c9aaa608fb2014cd9649af1c5c009485c60b9c8b15b89401fdc10cf6161c6 lib/unicore/version
  * c6884f4d629f04d1316f3476cb1050b6a1b98ca30c903262955d4eae337c6b1e regen/charset_translations.pl
  * 1c951614c1cf87c2a801c615a65470d8f5e9388b62690e7498c7661eb7782a01 regen/regcharclass.pl
