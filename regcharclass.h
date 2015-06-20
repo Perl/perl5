3e9d75d4a471ae35adb39c1132e9d647cdbf52ed
Karl Williamson <khw@cpan.org>
2015-06-20 09:33:58 -0600

    mktables: Move code wrongly inside a block
    This would fail to be executed only for very early Unicode versions, so
    wasn't a visible bug.

--- regcharclass.h
+++ regcharclass.h
@@ -2514,7 +2514,7 @@
  * 1a0687fb9c6c4567e853913549df0944fe40821279a3e9cdaa6ab8679bc286fd lib/unicore/extracted/DLineBreak.txt
  * 40bcfed3ca727c19e1331f6c33806231d5f7eeeabd2e6a9e06a3740c85d0c250 lib/unicore/extracted/DNumType.txt
  * a18d502bad39d527ac5586d7bc93e29f565859e3bcc24ada627eff606d6f5fed lib/unicore/extracted/DNumValues.txt
- * de2d7c5ee30e5ce366684287c484d8640e4d47edeb92cc6542981a9ff160cc2c lib/unicore/mktables
+ * 2b4853eaf10099e0285b0ac90b9b68396c32dd4b4955abd486e55ee0372e9809 lib/unicore/mktables
  * 462c9aaa608fb2014cd9649af1c5c009485c60b9c8b15b89401fdc10cf6161c6 lib/unicore/version
  * c6884f4d629f04d1316f3476cb1050b6a1b98ca30c903262955d4eae337c6b1e regen/charset_translations.pl
  * 5e47f645eac3a918246254e19c06b604c8ea088cf62da5be84dcb953ef2bf16c regen/regcharclass.pl
