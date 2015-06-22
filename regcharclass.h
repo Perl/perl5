c1ed610affdd1278883f3d5723ee69ba0ac41253
Karl Williamson <khw@cpan.org>
2015-06-22 10:06:45 -0600

    mktables: Handle early Unicodes' script names
    These were all caps in early releases

--- regcharclass.h
+++ regcharclass.h
@@ -2514,7 +2514,7 @@
  * 1a0687fb9c6c4567e853913549df0944fe40821279a3e9cdaa6ab8679bc286fd lib/unicore/extracted/DLineBreak.txt
  * 40bcfed3ca727c19e1331f6c33806231d5f7eeeabd2e6a9e06a3740c85d0c250 lib/unicore/extracted/DNumType.txt
  * a18d502bad39d527ac5586d7bc93e29f565859e3bcc24ada627eff606d6f5fed lib/unicore/extracted/DNumValues.txt
- * 234024b47cb9fd57ae95b60cd4e9087feea586b70c1243b84141534d1bca2918 lib/unicore/mktables
+ * 255e5cd690d01db3f4797adb31fd7955a47e33a1848e4ff4700609a292129bba lib/unicore/mktables
  * 462c9aaa608fb2014cd9649af1c5c009485c60b9c8b15b89401fdc10cf6161c6 lib/unicore/version
  * c6884f4d629f04d1316f3476cb1050b6a1b98ca30c903262955d4eae337c6b1e regen/charset_translations.pl
  * 5e47f645eac3a918246254e19c06b604c8ea088cf62da5be84dcb953ef2bf16c regen/regcharclass.pl
