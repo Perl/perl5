9be22ca52f7cf6acfd9a0c389e87ed5f2ea5c859
Karl Williamson <khw@cpan.org>
2015-06-26 12:27:41 -0600

    mktables: White-space, comment, debugging changes only
    This adds comments, fixes up some white space and adds some things to
    trace (when enabled)

--- regcharclass.h
+++ regcharclass.h
@@ -2514,7 +2514,7 @@
  * 1a0687fb9c6c4567e853913549df0944fe40821279a3e9cdaa6ab8679bc286fd lib/unicore/extracted/DLineBreak.txt
  * 40bcfed3ca727c19e1331f6c33806231d5f7eeeabd2e6a9e06a3740c85d0c250 lib/unicore/extracted/DNumType.txt
  * a18d502bad39d527ac5586d7bc93e29f565859e3bcc24ada627eff606d6f5fed lib/unicore/extracted/DNumValues.txt
- * 7001c2771543a465fd9e712c24862a909674f59324b0f4cded4f6d7bccd99a1a lib/unicore/mktables
+ * ae367152a9b10ba533358a3fee6e874c1ee262120cd7180656fd521a0c4f9184 lib/unicore/mktables
  * 462c9aaa608fb2014cd9649af1c5c009485c60b9c8b15b89401fdc10cf6161c6 lib/unicore/version
  * c6884f4d629f04d1316f3476cb1050b6a1b98ca30c903262955d4eae337c6b1e regen/charset_translations.pl
  * 8b29da548b7ad90659de234b5061a8c9fb0f40322a256d60fc5e9385ae4ece0e regen/regcharclass.pl
