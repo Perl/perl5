346a20cf504cbd044f564c5edd05268c60167147
Karl Williamson <khw@cpan.org>
2015-07-01 22:21:49 -0600

    mktables: Don't hard-code number of code points
    Unicode 1 only had 2**16 code points.  It is simple to fix to have a
    varying number.

--- regcharclass.h
+++ regcharclass.h
@@ -2514,7 +2514,7 @@
  * 1a0687fb9c6c4567e853913549df0944fe40821279a3e9cdaa6ab8679bc286fd lib/unicore/extracted/DLineBreak.txt
  * 40bcfed3ca727c19e1331f6c33806231d5f7eeeabd2e6a9e06a3740c85d0c250 lib/unicore/extracted/DNumType.txt
  * a18d502bad39d527ac5586d7bc93e29f565859e3bcc24ada627eff606d6f5fed lib/unicore/extracted/DNumValues.txt
- * 81a7045c23470b452ba69f83ed581197689cdd7e2f00c2b07299d7aad05a7da9 lib/unicore/mktables
+ * d3050c52cb438fbb02fc5bb214b4481cc052e9e2a14d4bffaf2bdb600df2dc13 lib/unicore/mktables
  * 462c9aaa608fb2014cd9649af1c5c009485c60b9c8b15b89401fdc10cf6161c6 lib/unicore/version
  * c6884f4d629f04d1316f3476cb1050b6a1b98ca30c903262955d4eae337c6b1e regen/charset_translations.pl
  * 5e47f645eac3a918246254e19c06b604c8ea088cf62da5be84dcb953ef2bf16c regen/regcharclass.pl
