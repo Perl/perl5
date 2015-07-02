4db4b199a05fb83ac6f38720d690098cf862ed2c
Karl Williamson <khw@cpan.org>
2015-07-01 22:24:26 -0600

    mktables: Define ok(), needed by a required script.
    loc_tools.pl wants this function to be defined.

--- charclass_invlists.h
+++ charclass_invlists.h
@@ -99521,7 +99521,7 @@ static const UV XPosixXDigit_invlist[] = { /* for EBCDIC POSIX-BC */
  * 1a0687fb9c6c4567e853913549df0944fe40821279a3e9cdaa6ab8679bc286fd lib/unicore/extracted/DLineBreak.txt
  * 40bcfed3ca727c19e1331f6c33806231d5f7eeeabd2e6a9e06a3740c85d0c250 lib/unicore/extracted/DNumType.txt
  * a18d502bad39d527ac5586d7bc93e29f565859e3bcc24ada627eff606d6f5fed lib/unicore/extracted/DNumValues.txt
- * d3050c52cb438fbb02fc5bb214b4481cc052e9e2a14d4bffaf2bdb600df2dc13 lib/unicore/mktables
+ * f475076a560c319d50cd4d7c207db199c8fb0dfe652dd5cdab67f61a59149b60 lib/unicore/mktables
  * 462c9aaa608fb2014cd9649af1c5c009485c60b9c8b15b89401fdc10cf6161c6 lib/unicore/version
  * c6884f4d629f04d1316f3476cb1050b6a1b98ca30c903262955d4eae337c6b1e regen/charset_translations.pl
  * f199f92c0b5f87882b0198936ea8ef3dc43627b57a77ac3eb9250bd2664bbd88 regen/mk_invlists.pl
