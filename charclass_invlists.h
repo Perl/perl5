68ab5aec132b86e3bd4a93d911ed74296f30c5ef
Karl Williamson <khw@cpan.org>
2015-07-04 22:23:41 -0600

    mktables: Easier debugging
    This turns off some inconsequential annoying warnings during certain
    debugging situations

--- charclass_invlists.h
+++ charclass_invlists.h
@@ -99521,7 +99521,7 @@ static const UV XPosixXDigit_invlist[] = { /* for EBCDIC POSIX-BC */
  * 1a0687fb9c6c4567e853913549df0944fe40821279a3e9cdaa6ab8679bc286fd lib/unicore/extracted/DLineBreak.txt
  * 40bcfed3ca727c19e1331f6c33806231d5f7eeeabd2e6a9e06a3740c85d0c250 lib/unicore/extracted/DNumType.txt
  * a18d502bad39d527ac5586d7bc93e29f565859e3bcc24ada627eff606d6f5fed lib/unicore/extracted/DNumValues.txt
- * 95c4235c8cf21a734359671e0873f59927e0c84ceb09559d4903f88770f34ef6 lib/unicore/mktables
+ * df65d8d58f4049a424665df27529d9c1a1d9b1502a42170878ece23a941f6b6c lib/unicore/mktables
  * 462c9aaa608fb2014cd9649af1c5c009485c60b9c8b15b89401fdc10cf6161c6 lib/unicore/version
  * c6884f4d629f04d1316f3476cb1050b6a1b98ca30c903262955d4eae337c6b1e regen/charset_translations.pl
  * 7b6f61662df48e0cbfb234a926e02e5cb9468af052f7f9feb84285996f30df09 regen/mk_invlists.pl
