d8062573c60d6122099d03a5cee4ce337cc2e889
Karl Williamson <khw@cpan.org>
2015-07-25 22:35:36 -0600

    mktables: Unicode has withdrawn some files in the UCD
    This adds the capability to mktables to seamlessly handle the case where
    Unicode has removed a file from the Unicode Character Database that
    existed in prior versions.  The constructor for the object tells what
    release no longer has it.  The file is looked for only when compiling a
    Unicode version in the window that had it.

--- regcharclass.h
+++ regcharclass.h
@@ -2514,7 +2514,7 @@
  * 1a0687fb9c6c4567e853913549df0944fe40821279a3e9cdaa6ab8679bc286fd lib/unicore/extracted/DLineBreak.txt
  * 40bcfed3ca727c19e1331f6c33806231d5f7eeeabd2e6a9e06a3740c85d0c250 lib/unicore/extracted/DNumType.txt
  * a18d502bad39d527ac5586d7bc93e29f565859e3bcc24ada627eff606d6f5fed lib/unicore/extracted/DNumValues.txt
- * 4a10834235ba911687d3ca9e551ec3438ee1aebd4b15e88933033822d0ef698d lib/unicore/mktables
+ * 3e5d72e6b1f87e746fa1bc36c92cb1e29eaf458f1ff70a6cfdf840a0537cdaf1 lib/unicore/mktables
  * 462c9aaa608fb2014cd9649af1c5c009485c60b9c8b15b89401fdc10cf6161c6 lib/unicore/version
  * c6884f4d629f04d1316f3476cb1050b6a1b98ca30c903262955d4eae337c6b1e regen/charset_translations.pl
  * d9c04ac46bdd81bb3e26519f2b8eb6242cb12337205add3f7cf092b0c58dccc4 regen/regcharclass.pl
