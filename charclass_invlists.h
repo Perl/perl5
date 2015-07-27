18c36e43550d746a9c9fb692226798fd0bfa58d5
Karl Williamson <khw@cpan.org>
2015-07-26 22:40:39 -0600

    mktables: Convert PropertyAliases.txt to use new early infrastructure
    This file is crucial to compiling perl these days.  This commit converts
    to use the new infrastructure for dealing with compiling Unicode
    releases prior to when this file was made available.

--- charclass_invlists.h
+++ charclass_invlists.h
@@ -99521,7 +99521,7 @@ static const UV XPosixXDigit_invlist[] = { /* for EBCDIC POSIX-BC */
  * 1a0687fb9c6c4567e853913549df0944fe40821279a3e9cdaa6ab8679bc286fd lib/unicore/extracted/DLineBreak.txt
  * 40bcfed3ca727c19e1331f6c33806231d5f7eeeabd2e6a9e06a3740c85d0c250 lib/unicore/extracted/DNumType.txt
  * a18d502bad39d527ac5586d7bc93e29f565859e3bcc24ada627eff606d6f5fed lib/unicore/extracted/DNumValues.txt
- * 5722b164bb2fc4f9de21703d289cf59733811f2aeb40a42e0d111635dd9b5517 lib/unicore/mktables
+ * 3ea969db51a2eb31c393738fa54cb0bf96568fb5ac3f35abba1c81db3b916b76 lib/unicore/mktables
  * 462c9aaa608fb2014cd9649af1c5c009485c60b9c8b15b89401fdc10cf6161c6 lib/unicore/version
  * c6884f4d629f04d1316f3476cb1050b6a1b98ca30c903262955d4eae337c6b1e regen/charset_translations.pl
  * 7b6f61662df48e0cbfb234a926e02e5cb9468af052f7f9feb84285996f30df09 regen/mk_invlists.pl
