6410499c3576275d529c1c5db34622340e6531ef
Steve Hay <steve.m.hay@googlemail.com>
2015-08-10 08:41:13 +0100

    bump perl5db.pl's $VERSION
    (cherry picked from commit 67a38f64602c1e8bbf6b6e5d21455c3f5df90f01)
    
    [Altered for maint: We can't bump from 1.48 to 1.49 since we're on 1.44.
    Bumping to 1.45 wouldn't be sensible since it's presumably already been
    used, so we have to bump to an _01 version instead.]

--- lib/perl5db.pl
+++ lib/perl5db.pl
@@ -523,7 +523,7 @@ BEGIN {
 # Debugger for Perl 5.00x; perl5db.pl patch level:
 use vars qw($VERSION $header);
 
-$VERSION = '1.44';
+$VERSION = '1.44_01';
 
 $header = "perl5db.pl version $VERSION";
 
