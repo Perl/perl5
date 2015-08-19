bb92a51036a8a03bf8db31d2c1506656f65092f0
Sawyer X <xsawyerx@cpan.org>
2015-08-19 14:00:02 +0100

    tweak version number of Module::CoreList
    (cherry picked from commit 2f553ae1e126e9a08281bcc6d4e6b8ea2afe83a3)

--- dist/Module-CoreList/lib/Module/CoreList.pm
+++ dist/Module-CoreList/lib/Module/CoreList.pm
@@ -4,7 +4,7 @@ use vars qw/$VERSION %released %version %families %upstream
 	    %bug_tracker %deprecated %delta/;
 use Module::CoreList::TieHashDelta;
 use version;
-$VERSION = '5.20150214';
+$VERSION = '5.20150320';
 
 my $dumpinc = 0;
 sub import {
