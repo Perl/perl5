8f21889c2a0e574a419f6f6dbb2090be2bf6ff8f
Karl Williamson <khw@cpan.org>
2015-08-20 11:35:21 -0600

    mktables: Revamp the compare versions functionality
    This functionality is rarely used, but enables someone to see what
    Unicode has changed between releases X and Y, without the clutter of the
    things that are added after X came out.  In other words it compiles
    release X using Y's rules.  To use it, you must go in and edit mktables
    to specify to use this; so it is intended only for a developer who wants
    to look at Unicode history.  One use I've done is to look at the beta
    version of a new release to compare with the previous official one.
    This allows me to find typos, and unintentional changes and report them
    back to Unicode.
    
    This commit significantly overhauls this feature, giving better results
    than before.

--- regcharclass.h
+++ regcharclass.h
@@ -2514,7 +2514,7 @@
  * 1a0687fb9c6c4567e853913549df0944fe40821279a3e9cdaa6ab8679bc286fd lib/unicore/extracted/DLineBreak.txt
  * 40bcfed3ca727c19e1331f6c33806231d5f7eeeabd2e6a9e06a3740c85d0c250 lib/unicore/extracted/DNumType.txt
  * a18d502bad39d527ac5586d7bc93e29f565859e3bcc24ada627eff606d6f5fed lib/unicore/extracted/DNumValues.txt
- * 552944c6a848efa825d6683e49b8fda246226239bbb6b8d8f6111f5665c3b279 lib/unicore/mktables
+ * 8342a7b7a0fcf32673d96b5d4423cf4b70b0c20f3e20325d43651d22316fd2b1 lib/unicore/mktables
  * 462c9aaa608fb2014cd9649af1c5c009485c60b9c8b15b89401fdc10cf6161c6 lib/unicore/version
  * c6884f4d629f04d1316f3476cb1050b6a1b98ca30c903262955d4eae337c6b1e regen/charset_translations.pl
  * d9c04ac46bdd81bb3e26519f2b8eb6242cb12337205add3f7cf092b0c58dccc4 regen/regcharclass.pl
