bdc4e4b233f837e056756045c415e64dba78389c
Steffen Mueller <smueller@cpan.org>
2015-08-31 10:48:25 +0200

    Porting: Updated version of EU::ParseXS on CPAN

--- Porting/Maintainers.pl
+++ Porting/Maintainers.pl
@@ -523,7 +523,7 @@ use File::Glob qw(:case);
     },
 
     'ExtUtils::ParseXS' => {
-        'DISTRIBUTION' => 'SMUELLER/ExtUtils-ParseXS-3.24.tar.gz',
+        'DISTRIBUTION' => 'SMUELLER/ExtUtils-ParseXS-3.30.tar.gz',
         'FILES'        => q[dist/ExtUtils-ParseXS],
     },
 
