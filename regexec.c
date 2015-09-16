854d0921f9d0fb6c5ea15abeed3dd19d63bbabe1
Karl Williamson <khw@cpan.org>
2015-09-16 14:34:31 -0600

    PATCH [perl #123562] Regexp-matching "hangs"
    The regex engine got into an infinite loop because of the malformation.
    It is trying to back-up over a sequence of UTF-8 continuation bytes.
    But the character just before the sequence should be a start byte.  If
    not, there is a malformation.  I added a test to croak if that isn't the
    case so that it doesn't just infinitely loop.  I did this also in the
    similar areas of regexec.c.
    
    Comments long ago added to the code suggested that we check for
    malformations in the vicinity of the new tests.  But that was never
    done.  These new tests should be good enough to prevent looping, anyway.
    
    (cherry picked from commit 22b433eff9a1ffa2454e18405a56650f07b385b5)

--- regexec.c
+++ regexec.c
@@ -7830,6 +7830,10 @@ S_reghop3(U8 *s, SSize_t off, const U8* lim)
             if (UTF8_IS_CONTINUED(*s)) {
                 while (s > lim && UTF8_IS_CONTINUATION(*s))
                     s--;
+                if (! UTF8_IS_START(*s)) {
+                    dTHX;
+                    Perl_croak(aTHX_ "Malformed UTF-8 character (fatal)");
+                }
 	    }
             /* XXX could check well-formedness here */
 	}
@@ -7856,6 +7860,10 @@ S_reghop4(U8 *s, SSize_t off, const U8* llim, const U8* rlim)
             if (UTF8_IS_CONTINUED(*s)) {
                 while (s > llim && UTF8_IS_CONTINUATION(*s))
                     s--;
+                if (! UTF8_IS_START(*s)) {
+                    dTHX;
+                    Perl_croak(aTHX_ "Malformed UTF-8 character (fatal)");
+                }
             }
             /* XXX could check well-formedness here */
         }
@@ -7887,6 +7895,10 @@ S_reghopmaybe3(U8* s, SSize_t off, const U8* lim)
             if (UTF8_IS_CONTINUED(*s)) {
                 while (s > lim && UTF8_IS_CONTINUATION(*s))
                     s--;
+                if (! UTF8_IS_START(*s)) {
+                    dTHX;
+                    Perl_croak(aTHX_ "Malformed UTF-8 character (fatal)");
+                }
 	    }
             /* XXX could check well-formedness here */
 	}
