b2c3a90642ba76978ed44c10c9ff53eb8646c241
Steve Hay <steve.m.hay@googlemail.com>
2015-09-30 13:32:07 +0100

    Storable $VERSION++ for previous cherry-pick
    (Blead had already been bumped to 2.54 by commit 4b59afe630; we need a
    different number from that since we're lacking at least commit fb086a0950
    in maint-5.22. Blead will naturally get bumped to 2.55 at some point in
    the future so it wouldn't be wise to choose that, so use an _01 number as
    we often have to do for maint releases in these cases.)

--- dist/Storable/Storable.pm
+++ dist/Storable/Storable.pm
@@ -22,7 +22,7 @@ package Storable; @ISA = qw(Exporter);
 
 use vars qw($canonical $forgive_me $VERSION);
 
-$VERSION = '2.53';
+$VERSION = '2.53_01';
 
 BEGIN {
     if (eval { local $SIG{__DIE__}; require Log::Agent; 1 }) {
