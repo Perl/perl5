2aa28b861421a2e3084b5a6b4f179b54ea0d2faa
Craig A. Berry <craigberry@mac.com>
2015-10-15 09:12:21 -0500

    VMS-specific statbuf in Perl_find_script.
    In 97466d2cbf895b I added a declaration at function scope, but in
    some paths that was overridden at an inner scope, leaving the
    function-level one declared but not used.  So lets go back to the
    original intent of 45a23732c73c8 and have a separate declaration
    in each block that needs it.

--- util.c
+++ util.c
@@ -3310,6 +3310,7 @@ Perl_find_script(pTHX_ const char *scriptname, bool dosearch,
 	while (deftypes ||
 	       (!hasdir && my_trnlnm("DCL$PATH",tmpbuf,idx++)) )
 	{
+	    Stat_t statbuf;
 	    if (deftypes) {
 		deftypes = 0;
 		*tmpbuf = '\0';
