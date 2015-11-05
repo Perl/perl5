e1fc825d560ba7ba910aba84fe4eca5e5a1f2b66
Jarkko Hietaniemi <jhi@iki.fi>
2015-11-05 18:57:17 -0500

    annotate the acquire/release of PL_op_mutex
    op.c:1190:1: warning: mutex 'PL_op_mutex' is still held at the end of function
    }
    
    op.c:1199:5: warning: releasing mutex 'PL_op_mutex' that was not held
        OP_REFCNT_UNLOCK;

--- op.c
+++ op.c
@@ -1194,6 +1194,7 @@ Perl_op_null(pTHX_ OP *o)
 
 void
 Perl_op_refcnt_lock(pTHX)
+  PERL_TSA_ACQUIRE(PL_op_mutex)
 {
 #ifdef USE_ITHREADS
     dVAR;
@@ -1204,6 +1205,7 @@ Perl_op_refcnt_lock(pTHX)
 
 void
 Perl_op_refcnt_unlock(pTHX)
+  PERL_TSA_RELEASE(PL_op_mutex)
 {
 #ifdef USE_ITHREADS
     dVAR;
