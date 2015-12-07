a3914761bd81864e97c2c9093945a00fd3b2cf8f
Aaron Crane <arc@cpan.org>
2015-12-07 20:13:08 +0000

    Fix Emacs dir-local variables
    The setting for cperl-indent-level (which sets the default indentation step
    used by Emacs for Perl code) was missing a dot in the relevant cons pair.
    This meant that the value set was the single-element list (4) rather than
    the integer 4, so attempting to indent lines made Emacs produce an error
    "Wrong type argument: number-or-marker-p, (4)".

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,5 +1,5 @@
 ;; Default settings for all except cpan/
 ((nil . ((indent-tabs-mode . nil)))	; all modes
- (cperl-mode . ((cperl-indent-level 4)))
+ (cperl-mode . ((cperl-indent-level . 4)))
  (c-mode . ((c-indentation-style . bsd)
             (c-basic-offset . 4))))
