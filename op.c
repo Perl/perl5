44de5490cef8e95fb52b4a4e4747646f6550121e
David Mitchell <davem@iabyn.com>
2015-12-09 14:27:18 +0000

    rpeep: maintain chain when handling for(reverse..)
    There's code in rpeep() that eliminates the reverse op from
    for (reverse ....) {} and just flags the enteriter as needing to reverse
    its args.
    
    This code didn't leave oldoldop -> oldop -> o as a consistent chain of
    adjacent op_next ops.

--- op.c
+++ op.c
@@ -14154,6 +14154,11 @@ Perl_rpeep(pTHX_ OP *o)
 	    op_null(o);
 	    enter->op_private |= OPpITER_REVERSED;
 	    iter->op_private |= OPpITER_REVERSED;
+
+            oldoldop = NULL;
+            oldop    = ourlast;
+            o        = oldop->op_next;
+            goto redo;
 	    
 	    break;
 	}
