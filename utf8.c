2d212e86d115af0ec7bda9d374433535fb9b20ab
Karl Williamson <khw@cpan.org>
2015-12-15 17:47:27 -0700

    Change deprecation warning text
    The old text used the passive voice.  No 5.23 release has been made with
    the old text, so no perldelta changes are needed.

--- utf8.c
+++ utf8.c
@@ -36,7 +36,7 @@
 static const char unees[] =
     "Malformed UTF-8 character (unexpected end of string)";
 static const char cp_above_legal_max[] =
-    "It is deprecated to use code point 0x%"UVXf"; the permissible max is 0x%"UVXf"";
+ "Use of code point 0x%"UVXf" is deprecated; the permissible max is 0x%"UVXf"";
 
 #define MAX_NON_DEPRECATED_CP (IV_MAX)
 
