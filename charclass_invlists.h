b82bf1ab2e741cae81b915e189d77e2325bc6b80
Karl Williamson <khw@cpan.org>
2016-01-04 22:09:55 -0700

    mktables: Add info under -annotate option
    This adds some helpful text when this option is used, which is for examining the Unicode database in great detail

--- charclass_invlists.h
+++ charclass_invlists.h
@@ -87887,7 +87887,7 @@ static const U8 WB_table[19][19] = {
  * 1a0687fb9c6c4567e853913549df0944fe40821279a3e9cdaa6ab8679bc286fd lib/unicore/extracted/DLineBreak.txt
  * 40bcfed3ca727c19e1331f6c33806231d5f7eeeabd2e6a9e06a3740c85d0c250 lib/unicore/extracted/DNumType.txt
  * a18d502bad39d527ac5586d7bc93e29f565859e3bcc24ada627eff606d6f5fed lib/unicore/extracted/DNumValues.txt
- * 9d7fdc238d83baa868b4c24194beeb2e65250b2e308a14fdbee2b16f8c002e0d lib/unicore/mktables
+ * b2f25082bf2c632b3aa17e17fa480a068ad560868ddd51aebc281143b68ea1aa lib/unicore/mktables
  * 462c9aaa608fb2014cd9649af1c5c009485c60b9c8b15b89401fdc10cf6161c6 lib/unicore/version
  * 913d2f93f3cb6cdf1664db888bf840bc4eb074eef824e082fceda24a9445e60c regen/charset_translations.pl
  * 12bd58cb9d5a99f631ca95e269f7f9c90dacaf81020efa5d95a995f3cdc19200 regen/mk_invlists.pl
