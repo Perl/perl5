cbdbe9d466e0d26852ca1ace0825220c8ca7d215
Karl Williamson <khw@cpan.org>
2016-01-05 15:58:07 -0700

    mktables: Add constructor parameter
    This new parameter will be used in the next commit, adds a special case
    for handling tables that the perl interpreter relies on when compiling
    a Unicode version earlier than the property is defined by Unicode.  This
    will allow for tailoring the property to Perl's needs in the next commit

--- regcharclass.h
+++ regcharclass.h
@@ -2514,7 +2514,7 @@
  * 1a0687fb9c6c4567e853913549df0944fe40821279a3e9cdaa6ab8679bc286fd lib/unicore/extracted/DLineBreak.txt
  * 40bcfed3ca727c19e1331f6c33806231d5f7eeeabd2e6a9e06a3740c85d0c250 lib/unicore/extracted/DNumType.txt
  * a18d502bad39d527ac5586d7bc93e29f565859e3bcc24ada627eff606d6f5fed lib/unicore/extracted/DNumValues.txt
- * dc85c20e847c16da6e2ad5ec42fe0948952d09d858cce838648aff97215ceb73 lib/unicore/mktables
+ * 21f614a12bfde0478588228d46f1b594bf7e23c7d1f51492c70b13f7c9b8de09 lib/unicore/mktables
  * 462c9aaa608fb2014cd9649af1c5c009485c60b9c8b15b89401fdc10cf6161c6 lib/unicore/version
  * 996abda3c0fbc2bfd575092af09e3b9b0331e624eb2e969a268457f8fd31ecbb regen/charset_translations.pl
  * d9c04ac46bdd81bb3e26519f2b8eb6242cb12337205add3f7cf092b0c58dccc4 regen/regcharclass.pl
