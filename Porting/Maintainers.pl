7295ec19dc787d8f3490a4425e77653a56de2136
Steve Hay <steve.m.hay@googlemail.com>
2016-01-25 13:47:54 +0000

    Porting/Miantainers.pl update following changes in 273df2b189...538ad527dc
    (ExtUtils::CBuilder was also modified, but we probably don't need to keep
    tabs on the changes there since it's upstream => blead anyway:
    t/porting/customized.dat only lists changed files under cpan/, and EU::CB
    already had (unlisted) modified files.)

--- Porting/Maintainers.pl
+++ Porting/Maintainers.pl
@@ -485,6 +485,38 @@ use File::Glob qw(:case);
             qq[lib/ExtUtils/MM_VMS.pm],
         # Not yet submitted
             qq[t/lib/MakeMaker/Test/NoXS.pm],
+        # Backported commits from upstream
+            qw(lib/ExtUtils/Command/MM.pm
+               lib/ExtUtils/Liblist.pm
+               lib/ExtUtils/Liblist/Kid.pm
+               lib/ExtUtils/MM.pm
+               lib/ExtUtils/MM_AIX.pm
+               lib/ExtUtils/MM_Any.pm
+               lib/ExtUtils/MM_BeOS.pm
+               lib/ExtUtils/MM_Cygwin.pm
+               lib/ExtUtils/MM_DOS.pm
+               lib/ExtUtils/MM_Darwin.pm
+               lib/ExtUtils/MM_MacOS.pm
+               lib/ExtUtils/MM_NW5.pm
+               lib/ExtUtils/MM_OS2.pm
+               lib/ExtUtils/MM_QNX.pm
+               lib/ExtUtils/MM_UWIN.pm
+               lib/ExtUtils/MM_Unix.pm
+               lib/ExtUtils/MM_VOS.pm
+               lib/ExtUtils/MM_Win32.pm
+               lib/ExtUtils/MM_Win95.pm
+               lib/ExtUtils/MY.pm
+               lib/ExtUtils/MakeMaker/Config.pm
+               lib/ExtUtils/MakeMaker/FAQ.pod
+               lib/ExtUtils/MakeMaker/Tutorial.pod
+               lib/ExtUtils/MakeMaker/version.pm
+               lib/ExtUtils/MakeMaker/version/regex.pm
+               lib/ExtUtils/Mkbootstrap.pm
+               lib/ExtUtils/Mksymlists.pm
+               lib/ExtUtils/testlib.pm
+               t/cd.t
+               t/echo.t
+               ),
         ],
     },
 
