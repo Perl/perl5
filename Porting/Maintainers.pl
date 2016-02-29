50b6e5145b2207a553f4fb1fccd9902993794934
Jarkko Hietaniemi <jhi@iki.fi>
2016-02-29 08:47:54 -0500

    Scalar-List-Utils customization bump

--- Porting/Maintainers.pl
+++ Porting/Maintainers.pl
@@ -988,12 +988,14 @@ use File::Glob qw(:case);
         'FILES'        => q[cpan/Scalar-List-Utils],
         # Waiting to be merged upstream:
         # https://github.com/Scalar-List-Utils/Scalar-List-Utils/pull/24
+        # https://rt.cpan.org/Public/Bug/Display.html?id=105415
         'CUSTOMIZED'   => [
             qw( ListUtil.xs
                 lib/List/Util.pm
                 lib/List/Util/XS.pm
                 lib/Scalar/Util.pm
                 lib/Sub/Util.pm
+                t/product.t
                 )
         ],
     },
