33e9feea41a1ba8a58b32a5478d1dda0f7bcd052
Jarkko Hietaniemi <jhi@iki.fi>
2016-03-02 20:11:23 -0500

    perl5db.pl version bump

--- lib/perl5db.pl
+++ lib/perl5db.pl
@@ -528,7 +528,7 @@ BEGIN {
 # Debugger for Perl 5.00x; perl5db.pl patch level:
 use vars qw($VERSION $header);
 
-$VERSION = '1.49_02';
+$VERSION = '1.49_03';
 
 $header = "perl5db.pl version $VERSION";
 
