e2c60dbd4ac5372429de65a54f3138e5077197f3
Karl Williamson <khw@cpan.org>
2016-03-12 15:00:45 -0700

    t/re/uniprops.t: Remove wrong test cases
    mktables generates the file used in this test.  Unicode version 9
    introduces a numeric value that is an order of magnitude closer to 0
    than any previous version had.  This demonstrated a bug in mktables,
    where it didn't consider the possibility of floating point numbers being
    indistinguishably close to integers.  It did check for being too close
    to the rational numbers used in Unicode, but omitted checking for
    integers.  This adds that check, which in turn causes some wrong test
    cases to not be generated for this .t.
    
    This bug has not shown up in earlier Unicode versions, but is there
    nonetheless, so I'm pushing this now instead of waiting.

--- charclass_invlists.h
+++ charclass_invlists.h
@@ -87887,7 +87887,7 @@ static const U8 WB_table[19][19] = {
  * 1a0687fb9c6c4567e853913549df0944fe40821279a3e9cdaa6ab8679bc286fd lib/unicore/extracted/DLineBreak.txt
  * 40bcfed3ca727c19e1331f6c33806231d5f7eeeabd2e6a9e06a3740c85d0c250 lib/unicore/extracted/DNumType.txt
  * a18d502bad39d527ac5586d7bc93e29f565859e3bcc24ada627eff606d6f5fed lib/unicore/extracted/DNumValues.txt
- * 7baa3c79b0ac81279720b4871737ab448d7ddd1bfad31b981437ce49c1292535 lib/unicore/mktables
+ * fdff462ac2c512b5990e6276d8175d6a511c14654c31dc2fcfb2f802b9fa5c8e lib/unicore/mktables
  * 462c9aaa608fb2014cd9649af1c5c009485c60b9c8b15b89401fdc10cf6161c6 lib/unicore/version
  * 913d2f93f3cb6cdf1664db888bf840bc4eb074eef824e082fceda24a9445e60c regen/charset_translations.pl
  * 5774f77d07a81945b6a679ecce07ad90cdb334f3fb402ff63bdbecd2ec67da05 regen/mk_invlists.pl
