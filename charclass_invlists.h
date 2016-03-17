4ed2b786f0a44b01cfc49ae1a48bd9745aac2d6f
Karl Williamson <khw@cpan.org>
2016-03-17 14:27:34 -0600

    mktables: Fix bug with early Unicode versions
    An array had 2 optional elements at the end.  I got confused about
    handling them.  This change first deals with the final one, pops it and
    saves it separately if found.  Then only one optional element needs to
    be dealt with in the course of the code.
    
    This only gets executed for very early Unicode versions

--- charclass_invlists.h
+++ charclass_invlists.h
@@ -87887,7 +87887,7 @@ static const U8 WB_table[19][19] = {
  * 1a0687fb9c6c4567e853913549df0944fe40821279a3e9cdaa6ab8679bc286fd lib/unicore/extracted/DLineBreak.txt
  * 40bcfed3ca727c19e1331f6c33806231d5f7eeeabd2e6a9e06a3740c85d0c250 lib/unicore/extracted/DNumType.txt
  * a18d502bad39d527ac5586d7bc93e29f565859e3bcc24ada627eff606d6f5fed lib/unicore/extracted/DNumValues.txt
- * a72f8475755d75dd9ae8d8955051d61499542f157f1c65bc43c25a3537d69e62 lib/unicore/mktables
+ * 5dbee5ba64f6ba6da74242e1d45c9dff38c232cb813e5a38543508203182cf7a lib/unicore/mktables
  * 462c9aaa608fb2014cd9649af1c5c009485c60b9c8b15b89401fdc10cf6161c6 lib/unicore/version
  * 913d2f93f3cb6cdf1664db888bf840bc4eb074eef824e082fceda24a9445e60c regen/charset_translations.pl
  * 5774f77d07a81945b6a679ecce07ad90cdb334f3fb402ff63bdbecd2ec67da05 regen/mk_invlists.pl
