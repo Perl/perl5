489c35bbf500378513495f706a6ac5f45a8debec
Abigail <abigail@abigail.be>
2016-03-20 13:25:37 +0100

    Update Module::CoreList for 5.23.9

--- dist/Module-CoreList/lib/Module/CoreList/TieHashDelta.pm
+++ dist/Module-CoreList/lib/Module/CoreList/TieHashDelta.pm
@@ -3,7 +3,7 @@ package Module::CoreList::TieHashDelta;
 use strict;
 use vars qw($VERSION);
 
-$VERSION = '5.20160228';
+$VERSION = '5.20160320';
 
 sub TIEHASH {
     my ($class, $changed, $removed, $parent) = @_;
