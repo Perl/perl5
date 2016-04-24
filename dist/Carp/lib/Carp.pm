2f7a15bf0111b35a9af62de5c74e13855cb9cea3
Aaron Crane <arc@cpan.org>
2016-04-24 16:24:43 +0100

    [cpan#100183] Add missing "<FH> chunk #" phrase to Carp messages
    Commit 89988fbd2f7d8a44526a3cd9ab671b3102898bc9 added the relevant phrase for
    line-oriented filehandles; this extends that to also cover chunk-oriented
    ones.
    
    The patch was supplied by Chris R Donnelly <chris.donnelly@vauto.com> on the
    rt.cpan.org ticket; I have extended it to include a test.

--- dist/Carp/lib/Carp.pm
+++ dist/Carp/lib/Carp.pm
@@ -87,7 +87,7 @@ BEGIN {
     }
 }
 
-our $VERSION = '1.40';
+our $VERSION = '1.41';
 $VERSION =~ tr/_//d;
 
 our $MaxEvalLen = 0;
@@ -474,7 +474,7 @@ sub ret_backtrace {
         eval {
             CORE::die;
         };
-        if($@ =~ /^Died at .*(, <.*?> line \d+).$/ ) {
+        if($@ =~ /^Died at .*(, <.*?> (?:line|chunk) \d+).$/ ) {
             $mess .= $1;
         }
     }
