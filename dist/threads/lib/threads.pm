ca463a05f8c5a513ebcd09ee1fed6df729073c55
Ricardo Signes <rjbs@cpan.org>
2016-04-30 14:52:02 -0400

    threads: bump version (again) because content changed

--- dist/threads/lib/threads.pm
+++ dist/threads/lib/threads.pm
@@ -5,7 +5,7 @@ use 5.008;
 use strict;
 use warnings;
 
-our $VERSION = '2.06';
+our $VERSION = '2.07';
 my $XS_VERSION = $VERSION;
 $VERSION = eval $VERSION;
 
@@ -134,7 +134,7 @@ threads - Perl interpreter-based threads
 
 =head1 VERSION
 
-This document describes threads version 2.06
+This document describes threads version 2.07
 
 =head1 WARNING
 
