a3b4b767538d6cb0592a1428349ec55e219b81b3
Niko Tyni <ntyni@debian.org>
2016-05-01 22:53:11 +0300

    perlbug: Add unit tests
    Some of these tests have to mimic the interactive interface, which is
    probably rather fragile. However, as long as -F overrides any actual
    sending, no mail bombs will hopefully result.

--- Porting/Maintainers.pl
+++ Porting/Maintainers.pl
@@ -1449,6 +1449,7 @@ use File::Glob qw(:case);
                 lib/overload{.pm,.t,64.t}
                 lib/perl5db.{pl,t}
                 lib/perl5db/
+                lib/perlbug.t
                 lib/sigtrap.{pm,t}
                 lib/sort.{pm,t}
                 lib/strict.{pm,t}
