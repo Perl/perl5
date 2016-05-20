32e7f215b3ed0d58db0abe1375ecbf0b202ddc43
Father Chrysostomos <sprout@cpan.org>
2016-05-20 13:26:17 -0700

    Increase $feature::VERSION to 1.44

--- lib/feature.pm
+++ lib/feature.pm
@@ -5,7 +5,7 @@
 
 package feature;
 
-our $VERSION = '1.43';
+our $VERSION = '1.44';
 
 our %feature = (
     fc              => 'feature_fc',
--- regen/feature.pl
+++ regen/feature.pl
@@ -369,7 +369,7 @@ read_only_bottom_close_and_rename($h);
 __END__
 package feature;
 
-our $VERSION = '1.43';
+our $VERSION = '1.44';
 
 FEATURES
 
