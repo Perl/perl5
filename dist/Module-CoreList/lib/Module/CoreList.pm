f9d765f8c33817c67a6d8fdf6c644f45dae9efa1
James E Keenan <jkeenan@cpan.org>
2016-05-20 21:59:46 -0400

    Increment $VERSION to 5.YYYYMMDD for next scheduled monthly release date.
    (cherry picked from commit 47011d98d8138b193bac04ac39d25acbe9b579ba)

--- dist/Module-CoreList/lib/Module/CoreList.pm
+++ dist/Module-CoreList/lib/Module/CoreList.pm
@@ -4,7 +4,7 @@ use vars qw/$VERSION %released %version %families %upstream
 	    %bug_tracker %deprecated %delta/;
 use Module::CoreList::TieHashDelta;
 use version;
-$VERSION = '5.20160520';
+$VERSION = '5.20160620';
 
 sub _released_order {   # Sort helper, to make '?' sort after everything else
     (substr($released{$a}, 0, 1) eq "?")
