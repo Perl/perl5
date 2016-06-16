6295dc14882a54531ce4542f1d80fa8ae7b4f8f0
Karl Williamson <khw@cpan.org>
2016-06-16 11:48:28 -0600

    Tell mktables what Unicode version mk_invlist.pl handles
    A downside of supporting the Unicode break properties like \b{gcb},
    \b{lb} is that these aren't very mature in the Standard, and so code
    likely has to change when updating Perl to support a new version of the
    Standard.
    
    And the new rules may not be backwards compatible.  This commit creates
    a mechanism to tell mktables the Unicode version that the rules are
    written for.  If that is not the same version as being compiled, the
    test file marks any failing boundary tests as TODO, and outputs a
    warning if the compiled version is later than the code expects, to
    alert you to the fact that the code needs to be updated.

--- charclass_invlists.h
+++ charclass_invlists.h
@@ -87887,7 +87887,7 @@ static const U8 WB_table[19][19] = {
  * 1a0687fb9c6c4567e853913549df0944fe40821279a3e9cdaa6ab8679bc286fd lib/unicore/extracted/DLineBreak.txt
  * 40bcfed3ca727c19e1331f6c33806231d5f7eeeabd2e6a9e06a3740c85d0c250 lib/unicore/extracted/DNumType.txt
  * a18d502bad39d527ac5586d7bc93e29f565859e3bcc24ada627eff606d6f5fed lib/unicore/extracted/DNumValues.txt
- * 2e9c8c898fd78231c21ff0da9facb8d231bf419bde94dc63075dff904be4f5f7 lib/unicore/mktables
+ * 45321b549a605b65ead1e83cdb90fdd9c5a6c8731a537197f335bab251b4e778 lib/unicore/mktables
  * 462c9aaa608fb2014cd9649af1c5c009485c60b9c8b15b89401fdc10cf6161c6 lib/unicore/version
  * 913d2f93f3cb6cdf1664db888bf840bc4eb074eef824e082fceda24a9445e60c regen/charset_translations.pl
  * 12bd58cb9d5a99f631ca95e269f7f9c90dacaf81020efa5d95a995f3cdc19200 regen/mk_invlists.pl
