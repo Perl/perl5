4827ac7e56d329e2bcf028d5472ae4b3407a71d8
Chris 'BinGOs' Williams <chris@bingosnet.co.uk>
2016-06-21 14:57:44 +0100

    Fix Maintainers.pl after 94e22bd6

--- Porting/Maintainers.pl
+++ Porting/Maintainers.pl
@@ -340,7 +340,7 @@ use File::Glob qw(:case);
     },
 
     'Devel::PPPort' => {
-        'DISTRIBUTION' => 'WOLFSAGE/Devel-PPPort-3.32.tar.gz',
+        'DISTRIBUTION' => 'WOLFSAGE/Devel-PPPort-3.35.tar.gz',
         # RJBS has asked MHX to have UPSTREAM be 'blead'
         # (i.e. move this from cpan/ to dist/)
         'FILES'        => q[cpan/Devel-PPPort],
