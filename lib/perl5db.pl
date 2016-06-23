d34d6d1a95bc20c76c15f4e2dd7005cf6fcaf371
Tony Cook <tony@develop-help.com>
2016-06-23 16:09:24 +1000

    perl5db.pl: bump perldb $VERSION

--- lib/perl5db.pl
+++ lib/perl5db.pl
@@ -528,7 +528,7 @@ BEGIN {
 # Debugger for Perl 5.00x; perl5db.pl patch level:
 use vars qw($VERSION $header);
 
-$VERSION = '1.49_04';
+$VERSION = '1.49_05';
 
 $header = "perl5db.pl version $VERSION";
 
