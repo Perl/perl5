6320cdc08f146d42140872a5124e166084f2b766
Steve Hay <steve.m.hay@googlemail.com>
2016-06-28 08:54:25 +0100

    Upgrade Math::BigRat from version 0.260802 to 0.260804
    (This removes the blead customization, which is now incorporated with minor
    changes.)

--- Porting/Maintainers.pl
+++ Porting/Maintainers.pl
@@ -751,19 +751,15 @@ use File::Glob qw(:case);
     },
 
     'Math::BigRat' => {
-        'DISTRIBUTION' => 'PJACKLAM/Math-BigRat-0.260802.tar.gz',
+        'DISTRIBUTION' => 'PJACKLAM/Math-BigRat-0.260804.tar.gz',
         'FILES'        => q[cpan/Math-BigRat],
         'EXCLUDED'     => [
             qr{^inc/},
+            qr{^t/author-},
             qw( t/00sig.t
                 t/01load.t
                 t/02pod.t
                 t/03podcov.t
-                t/blog-mbr.t
-                ),
-        ],
-        'CUSTOMIZED'   => [
-            qw( lib/Math/BigRat.pm
                 ),
         ],
     },
