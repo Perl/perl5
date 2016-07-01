c4a535af41823cc84534bf8dbecfdcc3eb7daa86
Steve Hay <steve.m.hay@googlemail.com>
2016-07-01 08:27:18 +0100

    Upgrade Time::HiRes from version 1.9734 to 1.9739

--- Porting/Maintainers.pl
+++ Porting/Maintainers.pl
@@ -1173,7 +1173,7 @@ use File::Glob qw(:case);
     },
 
     'Time::HiRes' => {
-        'DISTRIBUTION' => 'JHI/Time-HiRes-1.9733.tar.gz',
+        'DISTRIBUTION' => 'JHI/Time-HiRes-1.9739.tar.gz',
         'FILES'        => q[dist/Time-HiRes],
     },
 
