b35c9daa7126b1fc7b23b213dc1c82fac49aca29
Steve Hay <steve.m.hay@googlemail.com>
2016-07-17 13:10:13 +0100

    Module::CoreList - use _24 suffix in $VERSIONs
    This is to distinguish the version to be shipped with 5.24.1 from that to
    be shipped with the concurrent 5.22.3, being released on the same day.

--- dist/Module-CoreList/lib/Module/CoreList/TieHashDelta.pm
+++ dist/Module-CoreList/lib/Module/CoreList/TieHashDelta.pm
@@ -3,7 +3,7 @@ package Module::CoreList::TieHashDelta;
 use strict;
 use vars qw($VERSION);
 
-$VERSION = '5.20160730';
+$VERSION = '5.20160730_24';
 
 sub TIEHASH {
     my ($class, $changed, $removed, $parent) = @_;
--- dist/Module-CoreList/lib/Module/CoreList/Utils.pm
+++ dist/Module-CoreList/lib/Module/CoreList/Utils.pm
@@ -6,7 +6,7 @@ use vars qw[$VERSION %utilities];
 use Module::CoreList;
 use Module::CoreList::TieHashDelta;
 
-$VERSION = '5.20160730';
+$VERSION = '5.20160730_24';
 
 sub utilities {
     my $perl = shift;
