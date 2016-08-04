e167a7177da799ca1757dc382b386abd3ae29491
Father Chrysostomos <sprout@cpan.org>
2016-08-04 14:09:57 -0700

    gv.c:require_tie_mod: Create namesv only when needed
    We only need it as an SV when we are going to load a module.  Also,
    there is no need for two separate SVs; we can use the same one for
    Perl_load_module as we use subsequently in gv_stashsv.

--- gv.c
+++ gv.c
@@ -1312,7 +1312,6 @@ S_require_tie_mod(pTHX_ GV *gv, const char *varpv, const char * name,
                                     load_module, so save it.  For the
                                     moment it’s always a single char.  */
     const SV * const target = varname == '[' ? GvSV(gv) : (SV *)GvHV(gv);
-    SV * const namesv = newSVpvn(name, len);
 
     PERL_ARGS_ASSERT_REQUIRE_TIE_MOD;
 
@@ -1326,19 +1325,20 @@ S_require_tie_mod(pTHX_ GV *gv, const char *varpv, const char * name,
       dSP;
 
       ENTER;
-      SAVEFREESV(namesv);
 
 #define HV_FETCH_TIE_FUNC (GV **)hv_fetch(stash, "_tie_it", 7, 0)
 
       /* Load the module if it is not loaded.  */
-      if (!(stash = gv_stashsv(namesv, 0))
+      if (!(stash = gv_stashpvn(name, len, 0))
        || !(gvp = HV_FETCH_TIE_FUNC) || !*gvp || !GvCV(*gvp))
       {
-	SV *module = newSVsv(namesv);
+        SV * const namesv = newSVpvn(name, len);
 	const char type = varname == '[' ? '$' : '%';
+	SAVEFREESV(namesv);
 	if ( flags & 1 )
 	    save_scalar(gv);
-	Perl_load_module(aTHX_ PERL_LOADMOD_NOIMPORT, module, NULL);
+	Perl_load_module(aTHX_ PERL_LOADMOD_NOIMPORT,
+			       SvREFCNT_inc_NN(namesv), NULL);
 	assert(sp == PL_stack_sp);
 	stash = gv_stashsv(namesv, 0);
 	if (!stash)
@@ -1356,7 +1356,6 @@ S_require_tie_mod(pTHX_ GV *gv, const char *varpv, const char * name,
       call_sv((SV *)*gvp, G_VOID|G_DISCARD);
       LEAVE;
     }
-    else SvREFCNT_dec_NN(namesv);
 }
 
 /*
