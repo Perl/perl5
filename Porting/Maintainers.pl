5275805d47bee27512db7283e558a3cb01927793
Chris 'BinGOs' Williams <chris@bingosnet.co.uk>
2016-08-08 12:58:53 +0100

    Fix M::CL version

--- Porting/Maintainers.pl
+++ Porting/Maintainers.pl
@@ -880,7 +880,7 @@ use File::Glob qw(:case);
     },
 
     'Module::CoreList' => {
-        'DISTRIBUTION' => 'BINGOS/Module-CoreList-5.20160620.tar.gz',
+        'DISTRIBUTION' => 'BINGOS/Module-CoreList-5.20160720.tar.gz',
         'FILES'        => q[dist/Module-CoreList],
     },
 
