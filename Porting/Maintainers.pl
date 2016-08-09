89309dcee30820578343533ad7e8beb3e25e18b9
Steve Hay <steve.m.hay@googlemail.com>
2016-08-09 08:29:33 +0100

    Upgrade IO-Socket-IP from version 0.37 to 0.38

--- Porting/Maintainers.pl
+++ Porting/Maintainers.pl
@@ -696,7 +696,7 @@ use File::Glob qw(:case);
     },
 
     'IO::Socket::IP' => {
-        'DISTRIBUTION' => 'PEVANS/IO-Socket-IP-0.37.tar.gz',
+        'DISTRIBUTION' => 'PEVANS/IO-Socket-IP-0.38.tar.gz',
         'FILES'        => q[cpan/IO-Socket-IP],
         'EXCLUDED'     => [
             qr{^examples/},
