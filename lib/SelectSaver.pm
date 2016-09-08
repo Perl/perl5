26d58bfed57736ec1e1f1dfd579484f8b6fcccd7
Nicolas R <atoomic@cpan.org>
2016-09-08 14:29:46 -0600

    Reduce SelectSaver memory footprint
    This is saving about 500k when using SelectSaver
    as most of the time Carp is not required.
    
    before> perl -I. -e 'require q{lib/SelectSaver.pm}; print qx{grep VmRSS /proc/$$/status}'
    VmRSS:      2920 kB
    
    after> perl -I. -e 'require q{lib/SelectSaver.pm}; print qx{grep VmRSS /proc/$$/status}'
    VmRSS:      2352 kB
    
    Committer:  Increment SelectSaver $VERSION.  Add perldelta entry for SelectSaver.
    
    For: RT # 129235

--- lib/SelectSaver.pm
+++ lib/SelectSaver.pm
@@ -1,6 +1,6 @@
 package SelectSaver;
 
-our $VERSION = '1.02';
+our $VERSION = '1.03';
 
 =head1 NAME
 
@@ -35,11 +35,10 @@ that was selected when it was created.
 =cut
 
 require 5.000;
-use Carp;
-use Symbol;
+use Symbol q{qualify};
 
 sub new {
-    @_ >= 1 && @_ <= 2 or croak 'usage: SelectSaver->new( [FILEHANDLE] )';
+    @_ >= 1 && @_ <= 2 or do { require Carp; Carp::croak('usage: SelectSaver->new( [FILEHANDLE] )') };
     my $fh = select;
     my $self = bless \$fh, $_[0];
     select qualify($_[1], caller) if @_ > 1;
