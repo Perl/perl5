96160768517a92e1ca0bc25a0f2e1c8bf1461aae
Yves Orton <demerphq@gmail.com>
2016-09-17 20:12:26 +0200

    doop.c: use sv_setpvn() instead of sv_setpvs()

--- doop.c
+++ doop.c
@@ -1008,7 +1008,7 @@ Perl_do_vop(pTHX_ I32 optype, SV *sv, SV *left, SV *right)
     PERL_ARGS_ASSERT_DO_VOP;
 
     if (sv != left || (optype != OP_BIT_AND && !SvOK(sv)))
-	sv_setpvs(sv, "");	/* avoid undef warning on |= and ^= */
+        sv_setpvn(sv, "", 0);        /* avoid undef warning on |= and ^= */
     if (sv == left) {
 	lsave = lc = SvPV_force_nomg(left, leftlen);
     }
