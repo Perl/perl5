3a57bd4d5e78d639b78eed9fcc27028720f8d326
James E Keenan <jkeenan@cpan.org>
2016-09-25 19:48:52 -0400

    Time-HiRes: bring up-to-date with CPAN.
    The ext3/ext2 filesystems do not have subsecond resolution, therefore skip the
    t/utime.t test. [rt.cpan.org #116127]

--- dist/Time-HiRes/HiRes.pm
+++ dist/Time-HiRes/HiRes.pm
@@ -28,7 +28,7 @@ our @EXPORT_OK = qw (usleep sleep ualarm alarm gettimeofday time tv_interval
 		 stat lstat utime
 		);
 
-our $VERSION = '1.9739';
+our $VERSION = '1.9740';
 our $XS_VERSION = $VERSION;
 $VERSION = eval $VERSION;
 
