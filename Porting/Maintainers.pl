94f73226be11b09ca74e0ba8b52b2b4121dbe173
Jarkko Hietaniemi <jhi@iki.fi>
2016-10-22 12:45:40 -0400

    Math-BigInt: Maintainers.pl customization

--- Porting/Maintainers.pl
+++ Porting/Maintainers.pl
@@ -771,6 +771,10 @@ use File::Glob qw(:case);
     'Math::BigInt' => {
         'DISTRIBUTION' => 'PJACKLAM/Math-BigInt-1.999726.tar.gz',
         'FILES'        => q[cpan/Math-BigInt],
+        'CUSTOMIZED'   => [
+            'lib/Math/BigInt.pm', # CPAN RT 118468
+            't/bigintpm.inc',     # CPAN RT 118468
+        ],
         'EXCLUDED'     => [
             qr{^inc/},
             qr{^examples/},
