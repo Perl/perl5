d1deb742f98169ce12c103478382902d83f6717a
Jarkko Hietaniemi <jhi@iki.fi>
2016-11-04 08:43:40 -0400

    netbsd-vax: also the 1E1000 is toxic (in compiletime).

--- cpan/Scalar-List-Utils/lib/List/Util/XS.pm
+++ cpan/Scalar-List-Utils/lib/List/Util/XS.pm
@@ -3,7 +3,7 @@ use strict;
 use warnings;
 use List::Util;
 
-our $VERSION = "1.46_01";    # FIXUP
+our $VERSION = "1.46_02";    # FIXUP
 $VERSION = eval $VERSION;    # FIXUP
 
 1;
--- cpan/Scalar-List-Utils/lib/Sub/Util.pm
+++ cpan/Scalar-List-Utils/lib/Sub/Util.pm
@@ -15,7 +15,7 @@ our @EXPORT_OK = qw(
   subname set_subname
 );
 
-our $VERSION    = "1.46_01";
+our $VERSION    = "1.46_02";
 $VERSION   = eval $VERSION;
 
 require List::Util; # as it has the XS
