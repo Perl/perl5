4d5cab080fb9aabea2532e76ad2a8692ccd30b9b
Yves Orton <demerphq@gmail.com>
2016-11-09 10:33:55 +0100

    gv.c: Do not use SvLEN() to set the length of a buffer, use SvLEN_set()

--- gv.c
+++ gv.c
@@ -1263,7 +1263,7 @@ Perl_gv_autoload_pvn(pTHX_ HV *stash, const char *name, STRLEN len, U32 flags)
 	    sv_setsv_nomg((SV *)cv, tmpsv);
 	    SvTEMP_off(tmpsv);
 	    SvREFCNT_dec_NN(tmpsv);
-	    SvLEN(cv) = SvCUR(cv) + 1;
+            SvLEN_set(cv, SvCUR(cv) + 1);
 	    SvCUR(cv) = ulen;
 	}
 	else {
