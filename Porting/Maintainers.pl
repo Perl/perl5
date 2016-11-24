fb882494267edb919d5a9a3fe0aca2dfeb30d012
Chris 'BinGOs' Williams <chris@bingosnet.co.uk>
2016-11-24 11:49:29 +0000

    This one goes up to 11

--- Porting/Maintainers.pl
+++ Porting/Maintainers.pl
@@ -852,7 +852,7 @@ use File::Glob qw(:case);
     },
 
     'Module::CoreList' => {
-        'DISTRIBUTION' => 'BINGOS/Module-CoreList-5.20161020.tar.gz',
+        'DISTRIBUTION' => 'BINGOS/Module-CoreList-5.20161120.tar.gz',
         'FILES'        => q[dist/Module-CoreList],
     },
 
