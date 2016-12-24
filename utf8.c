047c7c840ed49a138d2bb647049be888a2b8709c
Karl Williamson <khw@cpan.org>
2016-12-24 14:54:54 -0700

    PATCH: [perl #130397] Rmv useless code
    This code doesn't accomplish anything.
    
    Spotted by Andy Lester.

--- utf8.c
+++ utf8.c
@@ -4763,7 +4763,6 @@ Perl__swash_to_invlist(pTHX_ SV* const swash)
             invlist = _new_invlist(0);
         }
         else {
-            while (isSPACE(*l)) l++;
             l = (U8 *) after_atou;
 
             /* Get the 0th element, which is needed to setup the inversion list */
