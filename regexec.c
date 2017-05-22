f8d2fa4a0e346d086c97ad65a72be24c1bc06bd7
James E Keenan <jkeenan@cpan.org>
2017-05-21 22:16:23 -0400

    Patch suggested by Craig Berry for RT 131337.
    (cherry picked from commit 66c5e3f2ab554a89dfc00689602414ac21ea66f6)

--- regexec.c
+++ regexec.c
@@ -9755,6 +9755,8 @@ S_to_byte_substr(pTHX_ regexp *prog)
     return TRUE;
 }
 
+#ifndef PERL_IN_XSUB_RE
+
 bool
 Perl__is_grapheme(pTHX_ const U8 * strbeg, const U8 * s, const U8 * strend, const UV cp)
 {
@@ -9810,6 +9812,7 @@ Perl__is_grapheme(pTHX_ const U8 * strbeg, const U8 * s, const U8 * strend, cons
     return isGCB(cp_gcb_val, next_cp_gcb_val, strbeg, s, TRUE);
 }
 
+#endif
 
 
 
