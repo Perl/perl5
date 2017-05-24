754e15cfc175f0e2e1299d27fd387a0d868c2764
Karl Williamson <khw@cpan.org>
2017-05-23 20:51:56 -0600

    charnames: Remove obsolete pod about NBSP
    This is illegal since 5.26, and the text should have been removed then,
    but was overlooked.

--- lib/_charnames.pm
+++ lib/_charnames.pm
@@ -6,7 +6,7 @@
 package _charnames;
 use strict;
 use warnings;
-our $VERSION = '1.44';
+our $VERSION = '1.45';
 use unicore::Name;    # mktables-generated algorithmically-defined names
 
 use bytes ();          # for $bytes::hint_bits
