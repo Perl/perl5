32bc1f2052af1f841860bfab0fb7597bc98f4ba6
David Mitchell <davem@iabyn.com>
2017-06-22 12:30:03 +0100

    undo a Scalar-List-Utils customisation
    .. in preparation for installing new release
    
    Revert "Scalar-List-Utils: customized VERSION bump"
    
    This reverts commit cdeaf44d582bf74f4c686dc58b288d8d33f25b95.

--- cpan/Scalar-List-Utils/lib/List/Util.pm
+++ cpan/Scalar-List-Utils/lib/List/Util.pm
@@ -15,7 +15,7 @@ our @EXPORT_OK  = qw(
   all any first min max minstr maxstr none notall product reduce sum sum0 shuffle uniq uniqnum uniqstr
   pairs unpairs pairkeys pairvalues pairmap pairgrep pairfirst
 );
-our $VERSION    = "1.46_01";
+our $VERSION    = "1.46";
 our $XS_VERSION = $VERSION;
 $VERSION    = eval $VERSION;
 
