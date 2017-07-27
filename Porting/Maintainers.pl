0283ad960acb937e9387b3323f1f7852c2adb0fa
Dagfinn Ilmari Mannsåker <ilmari@ilmari.org>
2017-07-27 11:06:35 +0100

    [perl #130410] Import B-Debug 1.25 from CPAN
    This marks it as deprecated in core so it can be removed in 5.30.

--- Porting/Maintainers.pl
+++ Porting/Maintainers.pl
@@ -173,7 +173,7 @@ use File::Glob qw(:case);
     },
 
     'B::Debug' => {
-        'DISTRIBUTION' => 'RURBAN/B-Debug-1.24.tar.gz',
+        'DISTRIBUTION' => 'RURBAN/B-Debug-1.25.tar.gz',
         'FILES'        => q[cpan/B-Debug],
         'EXCLUDED'     => ['t/pod.t'],
     },
