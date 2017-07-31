b4bf9efcb45aefa039695c6ec86b9af593086308
Chris 'BinGOs' Williams <chris@bingosnet.co.uk>
2017-07-31 15:15:42 +0100

    ... And now for something completely different

--- Porting/Maintainers.pl
+++ Porting/Maintainers.pl
@@ -793,7 +793,7 @@ use File::Glob qw(:case);
     },
 
     'Module::CoreList' => {
-        'DISTRIBUTION' => 'BINGOS/Module-CoreList-5.20170715.tar.gz',
+        'DISTRIBUTION' => 'BINGOS/Module-CoreList-5.20170720.tar.gz',
         'FILES'        => q[dist/Module-CoreList],
     },
 
