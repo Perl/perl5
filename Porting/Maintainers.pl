ab49c48ac78b189e17cae8174dec1e9b155e3650
Chris 'BinGOs' Williams <chris@bingosnet.co.uk>
2017-09-22 12:41:19 +0100

    Update EUCB version from CPAN

--- Porting/Maintainers.pl
+++ Porting/Maintainers.pl
@@ -423,7 +423,7 @@ use File::Glob qw(:case);
     },
 
     'ExtUtils::CBuilder' => {
-        'DISTRIBUTION' => 'AMBS/ExtUtils-CBuilder-0.280224.tar.gz',
+        'DISTRIBUTION' => 'AMBS/ExtUtils-CBuilder-0.280226.tar.gz',
         'FILES'        => q[dist/ExtUtils-CBuilder],
         'EXCLUDED'     => [
             qw(README.mkdn),
