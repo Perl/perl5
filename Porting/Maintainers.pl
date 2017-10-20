940ab75c35c6e894453cfb821a9639a3bc4194f7
Steve Hay <steve.m.hay@googlemail.com>
2017-10-21 00:17:00 +0100

    Today's M::CL is up on CPAN already

--- Porting/Maintainers.pl
+++ Porting/Maintainers.pl
@@ -794,7 +794,7 @@ use File::Glob qw(:case);
     },
 
     'Module::CoreList' => {
-        'DISTRIBUTION' => 'BINGOS/Module-CoreList-5.20170923.tar.gz',
+        'DISTRIBUTION' => 'BINGOS/Module-CoreList-5.20171020.tar.gz',
         'FILES'        => q[dist/Module-CoreList],
     },
 
