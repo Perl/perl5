1222ab91f6663819e940d74716fd36e292cd58fb
Karl Williamson <khw@cpan.org>
2017-10-28 13:08:02 -0600

    Bump Socket version to 2.020_04
    Commit 0cdc775ef423ad6415e6f80b9244c17a52bf5149 made a small change in
    cpan/Socket/Socket.pm, causing a porting test failure, which is solved
    by a version bump, and changing customized.dat to account for that.

--- cpan/Socket/Socket.pm
+++ cpan/Socket/Socket.pm
@@ -3,7 +3,7 @@ package Socket;
 use strict;
 { use 5.006001; }
 
-our $VERSION = '2.020_03'; # patched in perl5.git
+our $VERSION = '2.020_04'; # patched in perl5.git
 $VERSION =~ tr/_//d;       # make $VERSION numeric
 
 =head1 NAME
