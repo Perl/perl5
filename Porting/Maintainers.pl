cc851c4b38a5e51a2c6b719e6d60e9a14fa0268e
Father Chrysostomos <sprout@cpan.org>
2017-10-31 22:12:42 -0700

    Porting/Maintainers.pl: vutil.c is no longer CUSTOMIZED

--- Porting/Maintainers.pl
+++ Porting/Maintainers.pl
@@ -1251,7 +1251,6 @@ use File::Glob qw(:case);
         # only necessary with the CPAN release.
         'CUSTOMIZED'   => [
             qw( lib/version.pm
-                vutil.c
                 vxs.inc
                 ),
         ],
