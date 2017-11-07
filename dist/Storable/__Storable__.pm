ad2ec6b54c9968999b7c70984b5a203fa72fd540
Nicolas R <atoomic@cpan.org>
2017-11-07 12:22:27 -0600

    Storable: remove Config dependency
    RT #132406
    
    Avoid loading Config/Config_heavy from Storable.
    
    Make Storable.pm a template file and check
    if the system can use flock at compile time.
    
    __Storable__.pm is the template file to edit,
    whereas Storable.pm.PL is the script generating
    Storable.pm from __Storable__.pm.
    
    Using a separate file for the template make it
    easier to edit.
    
    Also note that Storable.pm is now ignored by git.

--- dist/Storable/__Storable__.pm
+++ dist/Storable/__Storable__.pm
@@ -84,14 +84,7 @@ XSLoader::load('Storable', $Storable::VERSION);
 # Determine whether locking is possible, but only when needed.
 #
 
-sub CAN_FLOCK; my $CAN_FLOCK; sub CAN_FLOCK {
-	return $CAN_FLOCK if defined $CAN_FLOCK;
-	require Config; import Config;
-	return $CAN_FLOCK =
-		$Config{'d_flock'} ||
-		$Config{'d_fcntl_can_lock'} ||
-		$Config{'d_lockf'};
-}
+sub CAN_FLOCK; # TEMPLATE - replaced by Storable.pm.PL
 
 sub show_file_magic {
     print <<EOM;
