806161ab1a6a8eb99a7edf58de8f8d4ba36e7ac7
James E Keenan <jkeenan@cpan.org>
2017-11-18 15:33:32 -0500

    Upstream has released Test-Simple with new t/Legacy/Tester/tbt_09do.t.
    Since that now matches what we have in blead, we no longer need to have a
    CUSTOMIZED entry in Porting/Maintainers.pl.

--- Porting/Maintainers.pl
+++ Porting/Maintainers.pl
@@ -1082,9 +1082,6 @@ use File::Glob qw(:case);
                 t/zzz-check-breaks.t
                 ),
         ],
-        'CUSTOMIZED'   => [ qw[
-            t/Legacy/Tester/tbt_09do.t
-        ] ],
 
     },
 
