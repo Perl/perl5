f9d4150da0eac6b3cf69bea317ba63077bc60bf1
Steve Hay <steve.m.hay@googlemail.com>
2017-11-21 08:29:53 +0000

    Module::CoreList 5.20171120 is now on CPAN

--- Porting/Maintainers.pl
+++ Porting/Maintainers.pl
@@ -792,7 +792,7 @@ use File::Glob qw(:case);
     },
 
     'Module::CoreList' => {
-        'DISTRIBUTION' => 'BINGOS/Module-CoreList-5.20171020.tar.gz',
+        'DISTRIBUTION' => 'BINGOS/Module-CoreList-5.20171120.tar.gz',
         'FILES'        => q[dist/Module-CoreList],
     },
 
