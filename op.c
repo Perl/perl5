Perl/perl5

commit 19a8de486236b6004a51aa6c16eb9fcde47d86d1

fix lvalue context for 4-arg substr
4-arg substr uses its first arg as an lvalue, but wasn't lvaluifying
it properly.  [perl #115258]

op.c
@@ -13385,7 +13385,7 @@ Perl_ck_substr(pTHX_ OP *o)
 	if (kid->op_type == OP_NULL)
 	    kid = OpSIBLING(kid);
 	if (kid)
-	    kid->op_flags |= OPf_MOD;
+	    op_lvalue(kid, o->op_type);
 
     }
     return o;

t/op/substr.t
@@ -22,7 +22,7 @@ $SIG{__WARN__} = sub {
      }
 };
 
-plan(392);
+plan(399);
 
 run_tests() unless caller;
 
@@ -883,4 +883,30 @@ fresh_perl_is('$0 = "/usr/bin/perl"; substr($0, 0, 0, $0)', '', {}, "(perl #1293
     is $x, "\x{100}zzzz", "RT#130624: heap-use-after-free in 4-arg substr (targ)";
 }
 
+{
+    our @ta;
+    $#ta = -1;
+    substr($#ta, 0, 2) = 23;
+    is $#ta, 23;
+    $#ta = -1;
+    substr($#ta, 0, 2) =~ s/\A..\z/23/s;
+    is $#ta, 23;
+    $#ta = -1;
+    substr($#ta, 0, 2, 23);
+    is $#ta, 23;
+    sub ta_tindex :lvalue { $#ta }
+    $#ta = -1;
+    ta_tindex() = 23;
+    is $#ta, 23;
+    $#ta = -1;
+    substr(ta_tindex(), 0, 2) = 23;
+    is $#ta, 23;
+    $#ta = -1;
+    substr(ta_tindex(), 0, 2) =~ s/\A..\z/23/s;
+    is $#ta, 23;
+    $#ta = -1;
+    substr(ta_tindex(), 0, 2, 23);
+    is $#ta, 23;
+}
 
+1;

commit 833d07d41793fe678f93433fca9520ec1a22f750

[perl #132527] Allow 4-arg substr(delete ...)
Commit 19a8de4862 broke 4-arg substr with ‘delete’ (and various other
ops) for its first argument.  Since historically these have been
allowed, use a ‘loose’ lvalue context such as entersub when lvaluify-
ing the first argument.

op.c
@@ -13388,7 +13388,10 @@ Perl_ck_substr(pTHX_ OP *o)
 	if (kid->op_type == OP_NULL)
 	    kid = OpSIBLING(kid);
 	if (kid)
-	    op_lvalue(kid, o->op_type);
+	    /* Historically, substr(delete $foo{bar},...) has been allowed
+	       with 4-arg substr.  Keep it working by applying entersub
+	       lvalue context.  */
+	    op_lvalue(kid, OP_ENTERSUB);
 
     }
     return o;

t/op/substr.t
@@ -22,7 +22,7 @@ $SIG{__WARN__} = sub {
      }
 };
 
-plan(399);
+plan(400);
 
 run_tests() unless caller;
 
@@ -907,6 +907,15 @@ fresh_perl_is('$0 = "/usr/bin/perl"; substr($0, 0, 0, $0)', '', {}, "(perl #1293
     $#ta = -1;
     substr(ta_tindex(), 0, 2, 23);
     is $#ta, 23;
+}
+
+{ # [perl #132527]
+    use feature 'refaliasing';
+    no warnings 'experimental::refaliasing';
+    my $h;
+    \$h{foo} = \(my $bar = "baz");
+    substr delete $h{foo}, 1, 1, o=>;
+    is $bar, boz => 'first arg to 4-arg substr is loose lvalue context';
 }
 
 1;