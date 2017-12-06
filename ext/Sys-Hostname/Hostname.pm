0c9c439d08a65206d442724bcd9fb29fa5a7f937
Zefram <zefram@fysh.org>
2017-12-06 16:28:18 +0000

    document hostname(@args) deprecation
    Passing arguments to Sys::Hostname::hostname() is already deprecated.
    Note the deprecation's agreed removal version of Perl 5.32.  Also document
    it in perldeprecation.pod.  [perl #124349]

--- ext/Sys-Hostname/Hostname.pm
+++ ext/Sys-Hostname/Hostname.pm
@@ -16,7 +16,7 @@ use warnings ();
 our $host;
 
 BEGIN {
-    $VERSION = '1.21';
+    $VERSION = '1.22';
     {
 	local $SIG{__DIE__};
 	eval {
@@ -29,7 +29,7 @@ BEGIN {
 
 
 sub hostname {
-  @_ and warnings::warnif("deprecated", "hostname() doesn't accept any arguments");
+  @_ and warnings::warnif("deprecated", "hostname() doesn't accept any arguments. This will become fatal in Perl 5.32");
 
   # method 1 - we already know it
   return $host if defined $host;
