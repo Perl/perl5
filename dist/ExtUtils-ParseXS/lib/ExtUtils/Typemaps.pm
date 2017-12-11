76eb84800b51f32ecd4ab68d15ca549f2f67721b
Father Chrysostomos <sprout@cpan.org>
2017-12-10 16:54:44 -0800

    Increase $ExtUtils::Typemaps::VERSION to 3.37

--- dist/ExtUtils-ParseXS/lib/ExtUtils/Typemaps.pm
+++ dist/ExtUtils-ParseXS/lib/ExtUtils/Typemaps.pm
@@ -2,7 +2,7 @@ package ExtUtils::Typemaps;
 use 5.006001;
 use strict;
 use warnings;
-our $VERSION = '3.36';
+our $VERSION = '3.37';
 
 require ExtUtils::ParseXS;
 require ExtUtils::ParseXS::Constants;
