149b848a703c802fe948c79e4812c725aac8f013
Chris 'BinGOs' Williams <chris@bingosnet.co.uk>
2017-12-20 23:45:41 +0000

    Bump the perl version in various places for v5.27.8

--- lib/B/Op_private.pm
+++ lib/B/Op_private.pm
@@ -118,7 +118,7 @@ package B::Op_private;
 our %bits;
 
 
-our $VERSION = "5.027007";
+our $VERSION = "5.027008";
 
 $bits{$_}{3} = 'OPpENTERSUB_AMPER' for qw(entersub rv2cv);
 $bits{$_}{6} = 'OPpENTERSUB_DB' for qw(entersub rv2cv);
