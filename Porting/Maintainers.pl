3093cd0531d6e17e80fd45b92f37e51f523b16b2
Chris 'BinGOs' Williams <chris@bingosnet.co.uk>
2018-01-20 11:06:19 +0000

    Bibbidi-Bobbidi-Boo

--- Porting/Maintainers.pl
+++ Porting/Maintainers.pl
@@ -808,7 +808,7 @@ use File::Glob qw(:case);
     },
 
     'Module::CoreList' => {
-        'DISTRIBUTION' => 'BINGOS/Module-CoreList-5.20171220.tar.gz',
+        'DISTRIBUTION' => 'BINGOS/Module-CoreList-5.20180120.tar.gz',
         'FILES'        => q[dist/Module-CoreList],
     },
 
