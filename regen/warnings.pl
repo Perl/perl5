25ebbc22701a587d60124611ae3b072d2d47883a
Slaven Rezić <slaven@rezic.de>
2018-02-11 12:36:07 -0800

    warnings: Omit handle when $. is 0
    otherwise the new _at_level functions end up including
    ‘at <handle> line 0’.
    
    [Commit message by the committer.]

--- regen/warnings.pl
+++ regen/warnings.pl
@@ -765,7 +765,7 @@ sub __chk
     # If we have an explicit level, bypass Carp.
     if ($has_level and @callers_bitmask) {
 	my $stuff = " at " . join " line ", (caller $i)[1,2];
-	$stuff .= ", <" . *${^LAST_FH}{NAME} . "> line $." if ${^LAST_FH};
+	$stuff .= ", <" . *${^LAST_FH}{NAME} . "> line $." if $. && ${^LAST_FH};
 	die "$message$stuff.\n" if $results[0];
 	return warn "$message$stuff.\n";
     }
