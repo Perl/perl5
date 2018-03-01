7f93f0a02bc3646568448fc64578bf6f57276eb3
Father Chrysostomos <sprout@cpan.org>
2018-02-28 18:53:01 -0800

    Fold overload::numbers into overloading
    overload::numbers has always been undocumented and used only by
    overloading.pm.  Combining them into one at least theoretically
    reduces load time (one file to parse) and saves memory (one
    less stash).

--- regen/overload.pl
+++ regen/overload.pl
@@ -4,7 +4,7 @@
 #
 #    overload.h
 #    overload.inc
-#    lib/overload/numbers.pm
+#    lib/overloading.pm
 #
 # from information stored in the DATA section of this file.
 #
@@ -36,23 +36,121 @@ my ($c, $h) = map {
 	       copyright => [1997, 1998, 2000, 2001, 2005 .. 2007, 2011] });
 } 'overload.inc', 'overload.h';
 
-mkdir("lib/overload", 0777) unless -d 'lib/overload';
-my $p = open_new('lib/overload/numbers.pm', '>',
+my $p = open_new('lib/overloading.pm', '>',
 		 { by => 'regen/overload.pl',
-		   file => 'lib/overload/numbers.pm', copyright => [2008] });
+		   file => 'lib/overloading.pm',
+		   copyright => [2008, 2018] });
 
 {
 local $" = "\n    ";
 print $p <<"EOF";
-package overload::numbers;
+package overloading;
+use warnings;
+
+our \$VERSION = '0.02';
 
 our \@names = qw#
     @names
 #;
 
 { my \$i = 0; our %names = map { \$_ => \$i++ } \@names }
+
 EOF
 }
+print $p <<'EOF';
+my $HINT_NO_AMAGIC = 0x01000000; # see perl.h
+
+require 5.010001;
+
+sub _ops_to_nums {
+    map { exists $names{"($_"}
+	? $names{"($_"}
+	: do { require Carp; Carp::croak("'$_' is not a valid overload") }
+    } @_;
+}
+
+sub import {
+    my ( $class, @ops ) = @_;
+
+    if ( @ops ) {
+	if ( $^H{overloading} ) {
+	    vec($^H{overloading} , $_, 1) = 0 for _ops_to_nums(@ops);
+	}
+
+	if ( $^H{overloading} !~ /[^\0]/ ) {
+	    delete $^H{overloading};
+	    $^H &= ~$HINT_NO_AMAGIC;
+	}
+    } else {
+	delete $^H{overloading};
+	$^H &= ~$HINT_NO_AMAGIC;
+    }
+}
+
+sub unimport {
+    my ( $class, @ops ) = @_;
+
+    if ( exists $^H{overloading} or not $^H & $HINT_NO_AMAGIC ) {
+	if ( @ops ) {
+	    vec($^H{overloading} ||= '', $_, 1) = 1 for _ops_to_nums(@ops);
+	} else {
+	    delete $^H{overloading};
+	}
+    }
+
+    $^H |= $HINT_NO_AMAGIC;
+}
+
+1;
+__END__
+
+=head1 NAME
+
+overloading - perl pragma to lexically control overloading
+
+=head1 SYNOPSIS
+
+    {
+	no overloading;
+	my $str = "$object"; # doesn't call stringification overload
+    }
+
+    # it's lexical, so this stringifies:
+    warn "$object";
+
+    # it can be enabled per op
+    no overloading qw("");
+    warn "$object";
+
+    # and also reenabled
+    use overloading;
+
+=head1 DESCRIPTION
+
+This pragma allows you to lexically disable or enable overloading.
+
+=over 6
+
+=item C<no overloading>
+
+Disables overloading entirely in the current lexical scope.
+
+=item C<no overloading @ops>
+
+Disables only specific overloads in the current lexical scope.
+
+=item C<use overloading>
+
+Reenables overloading in the current lexical scope.
+
+=item C<use overloading @ops>
+
+Reenables overloading only for specific ops in the current lexical scope.
+
+=back
+
+=cut
+EOF
 
 print $h "enum {\n";
 
