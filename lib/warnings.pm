06afc688394f2ee3027d9ab81e7fa58256a91645
Father Chrysostomos <sprout@cpan.org>
2018-03-11 14:23:42 -0700

    warnings.pm: sprintf is faster than concat
    I benchmarked this line alone in a one-liner and found the sprintf
    variant to be roughly 10% faster than the concat version.  It’s also
    more readable (and maintainable).
    
    (Not significant enough to warrant a perldelta entry.)

--- lib/warnings.pm
+++ lib/warnings.pm
@@ -452,8 +452,9 @@ sub __chk
     if ($has_level and @callers_bitmask) {
 	# logic copied from util.c:mess_sv
 	my $stuff = " at " . join " line ", (caller $i)[1,2];
-	$stuff .= ", <" . *${^LAST_FH}{NAME} . "> "
-		 . ($/ eq "\n" ? "line" : "chunk") . " $."
+	$stuff .= sprintf ", <%s> %s %d",
+			   *${^LAST_FH}{NAME},
+			   ($/ eq "\n" ? "line" : "chunk"), $.
 	    if $. && ${^LAST_FH};
 	die "$message$stuff.\n" if $results[0];
 	return warn "$message$stuff.\n";
