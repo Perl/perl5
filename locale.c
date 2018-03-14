32a62865ef662fce2b2250a7e0eca15861e7fe20
Karl Williamson <khw@cpan.org>
2018-03-14 08:39:59 -0600

    locale.c: Silence Win32 compiler warning
    The return value is discarded here, and a few lines down calls this
    function again, retaining its return value.

--- locale.c
+++ locale.c
@@ -2578,8 +2578,8 @@ S_my_nl_langinfo(const int item, bool toggle)
 
                     /* Here everything past the dot is a digit.  Treat it as a
                      * code page */
-                    save_to_buffer("CP", &PL_langinfo_buf,
-                                         &PL_langinfo_bufsize, 0);
+                    (void) save_to_buffer("CP", &PL_langinfo_buf,
+                                                &PL_langinfo_bufsize, 0);
                     offset = STRLENs("CP");
 
                   has_nondigit:
