Perl/perl5

commit 9fe4122e6defd7e9204ed6f2370d926d4c3b261b

perl.c: Free some locale stuff on exit
This stops potential memory leaks when using POSIX 2008 locale handling,
by freeing the current locale object and two special ones.

perl.c
@@ -1154,20 +1154,33 @@ perl_destruct(pTHXx)
         PL_curlocales[i] = NULL;
     }
 #endif
+#ifdef HAS_POSIX_2008_LOCALE
+    {
+        /* This also makes sure we aren't using a locale object that gets freed
+         * below */
+        const locale_t old_locale = uselocale(LC_GLOBAL_LOCALE);
+        if (old_locale != LC_GLOBAL_LOCALE) {
+            freelocale(old_locale);
+        }
+    }
+#  ifdef USE_LOCALE_NUMERIC
+    if (PL_underlying_numeric_obj) {
+        freelocale(PL_underlying_numeric_obj);
+        PL_underlying_numeric_obj = (locale_t) NULL;
+    }
+#  endif
+#  ifdef USE_POSIX_2008_LOCALE
+    if (PL_C_locale_obj) {
+        freelocale(PL_C_locale_obj);
+        PL_C_locale_obj = NULL;
+    }
+#  endif
+#endif
 #ifdef USE_LOCALE_NUMERIC
     Safefree(PL_numeric_name);
     PL_numeric_name = NULL;
     SvREFCNT_dec(PL_numeric_radix_sv);
     PL_numeric_radix_sv = NULL;
-
-#  ifdef HAS_POSIX_2008_LOCALE
-    if (PL_underlying_numeric_obj) {
-        /* Make sure we aren't using the locale space we are about to free */
-        uselocale(LC_GLOBAL_LOCALE);
-        freelocale(PL_underlying_numeric_obj);
-        PL_underlying_numeric_obj = (locale_t) NULL;
-    }
-#  endif
 #endif
 
     if (PL_setlocale_buf) {

commit 8d8484540d380c09fb40616135be00825189f578

Fix locale failures introduced 5 hours ago
Commit 9fe4122e6defd7e9204ed6f2370d926d4c3b261b broke threaded builds
because it changed to free a global variable upon thread exit (I had
forgotten that it wasn't an interpreter variable).

I do not know why this passed before pushing; others have had trouble
reproducing it.  But the same tests were failing for me now.  The one
difference is that I had been using clang with address sanitizer
compiled in but turned off when I made that commit.  Now I'm using g++

Spotted by Dave Mitchell

perl.c
@@ -1169,12 +1169,6 @@ perl_destruct(pTHXx)
         PL_underlying_numeric_obj = (locale_t) NULL;
     }
 #  endif
-#  ifdef USE_POSIX_2008_LOCALE
-    if (PL_C_locale_obj) {
-        freelocale(PL_C_locale_obj);
-        PL_C_locale_obj = NULL;
-    }
-#  endif
 #endif
 #ifdef USE_LOCALE_NUMERIC
     Safefree(PL_numeric_name);