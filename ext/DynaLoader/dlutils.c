a0148659d0cc836103e3f40ebe395ac9257b38e4
Dagfinn Ilmari Mannsåker <ilmari@ilmari.org>
2018-03-27 13:22:57 +0100

    Mark unused dl_unload_all_files() argument

--- ext/DynaLoader/dlutils.c
+++ ext/DynaLoader/dlutils.c
@@ -75,6 +75,7 @@ static void
 dl_unload_all_files(pTHX_ void *unused)
 {
     CV *sub;
+    PERL_UNUSED_ARG(unused);
     if ((sub = get_cvs("DynaLoader::dl_unload_file", 0)) != NULL) {
         AV *dl_librefs = get_av("DynaLoader::dl_librefs", 0);
         SV *dl_libref;
