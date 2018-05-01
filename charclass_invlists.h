03774c76efee24550f9d9bd279df87fc9038add1
Karl Williamson <khw@cpan.org>
2018-05-01 15:47:11 -0600

    mktables: Add guard against Unicode breakage
    This adds a check that a new Unicode version doesn't create a rational
    number that is too close to a current rational for our existing
    floating point precision.  Should this happen, we can increase the
    precision we use.

--- charclass_invlists.h
+++ charclass_invlists.h
@@ -374323,7 +374323,7 @@ static const U8 WB_table[24][24] = {
  * be0f129691d479aa38646e4ca0ec1ee576ae7f75b0300a5624a7fa862fa8abba lib/unicore/extracted/DLineBreak.txt
  * 92449d354d9f6b6f2f97a292ebb59f6344ffdeb83d120d7d23e569c43ba67cd5 lib/unicore/extracted/DNumType.txt
  * e3a319527153b0c6c0c549b40fc6f3a01a7a0dcd6620784391db25901df3b154 lib/unicore/extracted/DNumValues.txt
- * d86a9893f6c106416be89b5f4668a212364638d9ae58b49ec1e05349f3e91c74 lib/unicore/mktables
+ * f364c5c093e0ea66c2f5059e05855559065dc850e89027730312185ab30169f2 lib/unicore/mktables
  * 21653d2744fdd071f9ef138c805393901bb9547cf3e777ebf50215a191f986ea lib/unicore/version
  * 4bb677187a1a64e39d48f2e341b5ecb6c99857e49d7a79cf503bd8a3c709999b regen/charset_translations.pl
  * 03e51b0f07beebd5da62ab943899aa4934eee1f792fa27c1fb638c33bf4ac6ea regen/mk_PL_charclass.pl
