2366eb49a9d126a5053639e620adb25a2fa9b524
Karl Williamson <khw@cpan.org>
2018-05-01 15:24:19 -0600

    Add tests for qr/\p{}/
    This adds tests for nv=integer, where 'integer' is expressed in %e.

--- uni_keywords.h
+++ uni_keywords.h
@@ -6834,7 +6834,7 @@ MPH_VALt match_uniprop( const unsigned char * const key, const U16 key_len ) {
  * be0f129691d479aa38646e4ca0ec1ee576ae7f75b0300a5624a7fa862fa8abba lib/unicore/extracted/DLineBreak.txt
  * 92449d354d9f6b6f2f97a292ebb59f6344ffdeb83d120d7d23e569c43ba67cd5 lib/unicore/extracted/DNumType.txt
  * e3a319527153b0c6c0c549b40fc6f3a01a7a0dcd6620784391db25901df3b154 lib/unicore/extracted/DNumValues.txt
- * 32b3fd3722ec94c467104bb1f24e7ac3a3f0929531b96b744aa2746ecca3595b lib/unicore/mktables
+ * d86a9893f6c106416be89b5f4668a212364638d9ae58b49ec1e05349f3e91c74 lib/unicore/mktables
  * 21653d2744fdd071f9ef138c805393901bb9547cf3e777ebf50215a191f986ea lib/unicore/version
  * 4bb677187a1a64e39d48f2e341b5ecb6c99857e49d7a79cf503bd8a3c709999b regen/charset_translations.pl
  * 03e51b0f07beebd5da62ab943899aa4934eee1f792fa27c1fb638c33bf4ac6ea regen/mk_PL_charclass.pl
