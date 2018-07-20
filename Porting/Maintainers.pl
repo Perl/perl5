37527a7ed8253652eecd27f1a4f6f1a3074193c1
Steve Hay <steve.m.hay@googlemail.com>
2018-07-20 16:56:18 +0100

    Today's Module::CoreList is now on CPAN
    BinGOs++!

--- Porting/Maintainers.pl
+++ Porting/Maintainers.pl
@@ -803,7 +803,7 @@ use File::Glob qw(:case);
     },
 
     'Module::CoreList' => {
-        'DISTRIBUTION' => 'BINGOS/Module-CoreList-5.20180626.tar.gz',
+        'DISTRIBUTION' => 'BINGOS/Module-CoreList-5.20180720.tar.gz',
         'FILES'        => q[dist/Module-CoreList],
     },
 
