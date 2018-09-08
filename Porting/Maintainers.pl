295a484ee22a430520edcf29ce6d2353844fd6c4
James E Keenan <jkeenan@cpan.org>
2018-09-08 19:51:26 -0400

    File::Path: sync with CPAN version 2.16

--- Porting/Maintainers.pl
+++ Porting/Maintainers.pl
@@ -504,7 +504,7 @@ use File::Glob qw(:case);
     },
 
     'File::Path' => {
-        'DISTRIBUTION' => 'JKEENAN/File-Path-2.15.tar.gz',
+        'DISTRIBUTION' => 'JKEENAN/File-Path-2.16.tar.gz',
         'FILES'        => q[cpan/File-Path],
         'EXCLUDED'     => [
             qw(t/Path-Class.t),
