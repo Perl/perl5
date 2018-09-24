a488d3333570d469109e4ac9251461c362e2f4c4
Karl Williamson <khw@cpan.org>
2018-09-24 11:16:14 -0600

    PATCH: [perl #133423]

--- regcomp.c
+++ regcomp.c
@@ -15591,7 +15591,6 @@ redo_curchar:
                     if (UCHARAT(RExC_parse) != ')')
                         vFAIL("Expecting close paren for wrapper for nested extended charclass");
 
-                    RExC_parse++;
                     RExC_flags = save_flags;
                     goto handle_operand;
                 }
