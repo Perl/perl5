df09255f565b73f060cd59e80498786e0f80d4a7
Svyatoslav <razmyslov@viva64.com>
2018-10-05 17:16:54 +0300

    PVS-Studio: fixed warning in ternary operator.
    (from https://github.com/Perl/perl5/pull/17)

--- toke.c
+++ toke.c
@@ -9559,9 +9559,9 @@ S_scan_ident(pTHX_ char *s, char *dest, STRLEN destlen, I32 ck_uni)
             s = skipspace(s);
         }
     }
-    if ((s <= PL_bufend - (is_utf8)
+    if ((s <= PL_bufend - ((is_utf8)
                           ? UTF8SKIP(s)
-                          : 1)
+                          : 1))
         && VALID_LEN_ONE_IDENT(s, PL_bufend, is_utf8))
     {
         if (is_utf8) {
