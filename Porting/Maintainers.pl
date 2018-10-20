aa130681407c1d5203e77f635a297101dd0f2aed
Aaron Crane <arc@cpan.org>
2018-10-20 16:07:16 +0100

    Bump version of Module::CoreList in Maintainers.pl

--- Porting/Maintainers.pl
+++ Porting/Maintainers.pl
@@ -783,7 +783,7 @@ use File::Glob qw(:case);
     },
 
     'Module::CoreList' => {
-        'DISTRIBUTION' => 'BINGOS/Module-CoreList-5.20180920.tar.gz',
+        'DISTRIBUTION' => 'BINGOS/Module-CoreList-5.20181020.tar.gz',
         'FILES'        => q[dist/Module-CoreList],
     },
 
