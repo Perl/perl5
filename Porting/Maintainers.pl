bdf921494cd66b6955501b5b1099ce6fc16f42e8
Chris 'BinGOs' Williams <chris@bingosnet.co.uk>
2018-12-21 09:50:55 +0000

    It's beginning to look a lot like Christmas

--- Porting/Maintainers.pl
+++ Porting/Maintainers.pl
@@ -785,7 +785,7 @@ use File::Glob qw(:case);
     },
 
     'Module::CoreList' => {
-        'DISTRIBUTION' => 'BINGOS/Module-CoreList-5.20181120.tar.gz',
+        'DISTRIBUTION' => 'BINGOS/Module-CoreList-5.20181218.tar.gz',
         'FILES'        => q[dist/Module-CoreList],
     },
 
