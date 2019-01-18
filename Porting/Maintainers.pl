0657084328c6c5c8225ddf2ce8238561011f6bff
Chris 'BinGOs' Williams <chris@bingosnet.co.uk>
2019-01-18 19:29:35 +0000

    Update Math-BigInt-FastCalc to CPAN version 0.5008
      [DELTA]
    
    0.5008 2018-10-09
    
     * Update MANIFEST.SKIP.
    
     * Sync test files with Math-BigInt.
    
     * Better Makefile.PL.

--- Porting/Maintainers.pl
+++ Porting/Maintainers.pl
@@ -719,7 +719,7 @@ use File::Glob qw(:case);
     },
 
     'Math::BigInt::FastCalc' => {
-        'DISTRIBUTION' => 'PJACKLAM/Math-BigInt-FastCalc-0.5007.tar.gz',
+        'DISTRIBUTION' => 'PJACKLAM/Math-BigInt-FastCalc-0.5008.tar.gz',
         'FILES'        => q[cpan/Math-BigInt-FastCalc],
         'EXCLUDED'     => [
             qr{^t/author-},
