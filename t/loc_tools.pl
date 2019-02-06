0c0c3518376d64d0be0faeacd283971b391b560a
Karl Williamson <khw@cpan.org>
2019-02-06 11:51:05 -0700

    t/loc_tools.pl: C.UTF-8 is a likely locale
    When looking for locales on a system, try this one which seems to be
    getting to be available widely.

--- t/loc_tools.pl
+++ t/loc_tools.pl
@@ -337,6 +337,7 @@ sub find_locales ($;$) {
     my @Locale;
     _trylocale("C", $categories, \@Locale, $allow_incompatible);
     _trylocale("POSIX", $categories, \@Locale, $allow_incompatible);
+    _trylocale("C.UTF-8", $categories, \@Locale, $allow_incompatible);
     foreach (1..16) {
         _trylocale("ISO8859-$_", $categories, \@Locale, $allow_incompatible);
         _trylocale("iso8859$_", $categories, \@Locale, $allow_incompatible);
