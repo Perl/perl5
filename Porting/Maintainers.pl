5eabe05513a5c4b2372fe96af04305ab920fa86a
Nicolas R <atoomic@cpan.org>
2019-02-14 10:30:34 -0600

    Update JSON-PP to CPAN version 4.00
    [DELTA]
    
    4.00 2018-12-07
    - production release
    
    3.99_01 2018-12-03
    - BACKWARD INCOMPATIBILITY:
    As JSON::XS 4.0 changed its policy and enabled allow_nonref
    by default, JSON::PP also enabled allow_nonref by default
    
    - implement allow_tags that was introduced by JSON::XS 3.0
    - add boolean_values that was introduced by JSON::XS 4.0
    - allow literal tags in strings in relaxed mode, as JSON::XS 3.02 does
    - allow PERL_JSON_PP_USE_B environmental variable to restore
    old number detection behavior for compatibility
    - various doc updates

--- Porting/Maintainers.pl
+++ Porting/Maintainers.pl
@@ -651,7 +651,7 @@ use File::Glob qw(:case);
     },
 
     'JSON::PP' => {
-        'DISTRIBUTION' => 'ISHIGAKI/JSON-PP-2.97001.tar.gz',
+        'DISTRIBUTION' => 'ISHIGAKI/JSON-PP-4.00.tar.gz',
         'FILES'        => q[cpan/JSON-PP],
     },
 
