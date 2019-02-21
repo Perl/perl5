60938bdc98cc202febdaf990b6841a9f0600a5f2
Chris 'BinGOs' Williams <chris@bingosnet.co.uk>
2019-02-21 11:11:58 +0000

    they seek him here, they seek him there

--- Porting/Maintainers.pl
+++ Porting/Maintainers.pl
@@ -781,7 +781,7 @@ use File::Glob qw(:case);
     },
 
     'Module::CoreList' => {
-        'DISTRIBUTION' => 'BINGOS/Module-CoreList-5.20190120.tar.gz',
+        'DISTRIBUTION' => 'BINGOS/Module-CoreList-5.20190220.tar.gz',
         'FILES'        => q[dist/Module-CoreList],
     },
 
