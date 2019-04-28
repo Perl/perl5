9daab0869a233e99f43a1a92302e5d3745b04b6f
H.Merijn Brand <h.m.brand@xs4all.nl>
2019-04-28 13:22:52 +0200

    Devel::PPPort order misc before uv because of UNLIKELY
    uv uses UNLIKELY before it gets undefined in misc, which will be the
    case fro perl-5.8.8 and below

--- Porting/Maintainers.pl
+++ Porting/Maintainers.pl
@@ -339,7 +339,7 @@ use File::Glob qw(:case);
     },
 
     'Devel::PPPort' => {
-        'DISTRIBUTION' => 'ETHER/Devel-PPPort-3.47.tar.gz',
+        'DISTRIBUTION' => 'ETHER/Devel-PPPort-3.48.tar.gz',
         'FILES'        => q[dist/Devel-PPPort],
         'EXCLUDED'     => [
             'PPPort.pm',    # we use PPPort_pm.PL instead
