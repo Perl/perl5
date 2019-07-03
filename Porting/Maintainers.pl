92050bba215a45f1b1af2343c99c36ab8bd642d7
Steve Hay <steve.m.hay@googlemail.com>
2019-07-03 13:55:33 +0100

    Update JSON::PP from version 4.02 to 4.04

--- Porting/Maintainers.pl
+++ Porting/Maintainers.pl
@@ -658,7 +658,7 @@ use File::Glob qw(:case);
     },
 
     'JSON::PP' => {
-        'DISTRIBUTION' => 'ISHIGAKI/JSON-PP-4.02.tar.gz',
+        'DISTRIBUTION' => 'ISHIGAKI/JSON-PP-4.04.tar.gz',
         'FILES'        => q[cpan/JSON-PP],
     },
 
