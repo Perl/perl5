3090e92d7668684af61aaf2d43317df609831017
Dagfinn Ilmari Mannsåker <ilmari@ilmari.org>
2019-07-22 10:37:30 +0100

    Add missing wchar.h include to Win32AP::File

--- Porting/Maintainers.pl
+++ Porting/Maintainers.pl
@@ -1269,6 +1269,12 @@ use File::Glob qw(:case);
         'EXCLUDED'     => [
             qr{^ex/},
         ],
+        # https://rt.cpan.org/Ticket/Display.html?id=127837
+        'CUSTOMIZED'   => [
+            qw( File.pm
+                File.xs
+                ),
+        ],
     },
 
     'XSLoader' => {
