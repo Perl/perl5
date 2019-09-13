c7fb625702e97df5bc916d4cbf32b73977676beb
Chris 'BinGOs' Williams <chris@bingosnet.co.uk>
2019-09-13 14:23:36 +0100

    Update Module-Metadata to CPAN version 1.000037
      [DELTA]
    
    1.000037  2019-09-07 18:32:44Z
      - add decode_pod option for automatic =encoding handling

--- Porting/Maintainers.pl
+++ Porting/Maintainers.pl
@@ -809,7 +809,7 @@ use File::Glob qw(:case);
     },
 
     'Module::Metadata' => {
-        'DISTRIBUTION' => 'ETHER/Module-Metadata-1.000036.tar.gz',
+        'DISTRIBUTION' => 'ETHER/Module-Metadata-1.000037.tar.gz',
         'FILES'        => q[cpan/Module-Metadata],
         'EXCLUDED'     => [
             qw(t/00-report-prereqs.t),
