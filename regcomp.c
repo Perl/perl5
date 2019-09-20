454a76a2f3e3e50928ae7f31e61d39f3518f61f6
Karl Williamson <khw@cpan.org>
2019-09-20 09:45:29 -0600

    regnext: Add some branch predictor hints

--- regcomp.c
+++ regcomp.c
@@ -21588,16 +21588,16 @@ Perl_regnext(pTHX_ regnode *p)
 {
     I32 offset;
 
-    if (!p)
+    if (UNLIKELY(!p))
 	return(NULL);
 
-    if (OP(p) > REGNODE_MAX) {		/* regnode.type is unsigned */
+    if (UNLIKELY(OP(p) > REGNODE_MAX)) {    /* regnode.type is unsigned */
 	Perl_croak(aTHX_ "Corrupted regexp opcode %d > %d",
                                                 (int)OP(p), (int)REGNODE_MAX);
     }
 
     offset = (reg_off_by_arg(p)) ? ARG(p) : NEXT_OFF(p);
-    if (offset == 0)
+    if (UNLIKELY(offset == 0))
 	return(NULL);
 
     return(p+offset);
