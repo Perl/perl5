669a1300d162433f684126609d77549fa2bb6d80
Steve Hay <steve.m.hay@googlemail.com>
2019-10-16 08:06:47 +0100

    Upgrade Math::BigInt from version 1.999816 to 1.999817

--- Porting/Maintainers.pl
+++ Porting/Maintainers.pl
@@ -714,7 +714,7 @@ use File::Glob qw(:case);
     },
 
     'Math::BigInt' => {
-        'DISTRIBUTION' => 'PJACKLAM/Math-BigInt-1.999816.tar.gz',
+        'DISTRIBUTION' => 'PJACKLAM/Math-BigInt-1.999817.tar.gz',
         'FILES'        => q[cpan/Math-BigInt],
         'EXCLUDED'     => [
             qr{^examples/},
