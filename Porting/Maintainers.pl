10934390253ed18c48f144735ad3c6a508caabc1
Chris 'BinGOs' Williams <chris@bingosnet.co.uk>
2019-10-22 01:08:30 +0100

    Update Sys-Syslog to CPAN version 0.36
      [DELTA]
    
    0.36 -- 2019-10-22 -- Sebastien Aperghis-Tramoni (SAPER)
    
      [Tests]
      - RT #127454 / GitHub PR #8: Intermittent failures on OpenBSD and other
        platforms (Jim Keenan).

--- Porting/Maintainers.pl
+++ Porting/Maintainers.pl
@@ -994,7 +994,7 @@ use File::Glob qw(:case);
     },
 
     'Sys::Syslog' => {
-        'DISTRIBUTION' => 'SAPER/Sys-Syslog-0.35.tar.gz',
+        'DISTRIBUTION' => 'SAPER/Sys-Syslog-0.36.tar.gz',
         'FILES'        => q[cpan/Sys-Syslog],
         'EXCLUDED'     => [
             qr{^eg/},
