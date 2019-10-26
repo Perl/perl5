eafff229ba7da21386c037ba98b8912239752a1f
Karl Williamson <khw@cpan.org>
2019-10-26 10:48:09 -0600

    Note that UTF8f and UTF8fARG are now documented

--- perl.h
+++ perl.h
@@ -3475,7 +3475,13 @@ EXTERN_C int perl_tsa_mutex_unlock(perl_mutex* mutex)
 
 #define HEKfARG(p) ((void*)(p))
 
-/* Takes three arguments: is_utf8, length, str */
+/*
+=for apidoc Amnh||UTF8f
+=for apidoc Amh||UTF8fARG|bool is_utf8|Size_t byte_len|char *str
+
+=cut
+ * %4p is a custom format
+ */
 #ifndef UTF8f
 #  define UTF8f "d%" UVuf "%4p"
 #endif
