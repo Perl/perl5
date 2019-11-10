042474943023d2327f5109d711e3cd6003dae210
Chris 'BinGOs' Williams <chris@bingosnet.co.uk>
2019-11-10 17:48:54 +0000

    Ten-ten let's do it again

--- Porting/Maintainers.pl
+++ Porting/Maintainers.pl
@@ -797,7 +797,7 @@ use File::Glob qw(:case);
     },
 
     'Module::CoreList' => {
-        'DISTRIBUTION' => 'BINGOS/Module-CoreList-5.20191020.tar.gz',
+        'DISTRIBUTION' => 'BINGOS/Module-CoreList-5.20191110.tar.gz',
         'FILES'        => q[dist/Module-CoreList],
     },
 
