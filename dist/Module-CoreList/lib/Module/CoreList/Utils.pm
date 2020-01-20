a5183bd1be43b0ca96d6a777290b066c8171b885
Matthew Horsfall <wolfsage@gmail.com>
2020-01-20 13:03:47 -0500

    Update Module::CoreList for 5.31.9

--- dist/Module-CoreList/lib/Module/CoreList/Utils.pm
+++ dist/Module-CoreList/lib/Module/CoreList/Utils.pm
@@ -4,7 +4,7 @@ use strict;
 use warnings;
 use Module::CoreList;
 
-our $VERSION = '5.20200120';
+our $VERSION = '5.20200220';
 our %utilities;
 
 sub utilities {
@@ -1568,6 +1568,13 @@ my %delta = (
     },
     5.031008 => {
         delta_from => 5.031007,
+        changed => {
+        },
+        removed => {
+        }
+    },
+    5.031009 => {
+        delta_from => 5.031008,
         changed => {
         },
         removed => {
