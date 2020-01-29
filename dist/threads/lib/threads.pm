92e3396ae225d3636973e1bff2a37a8e8f775fb5
Paul Johnson <paul@pjcj.net>
2020-01-29 18:02:02 +0100

    Fix minor grammar nit in threads docs
    Committer: Increment $VERSION.

--- dist/threads/lib/threads.pm
+++ dist/threads/lib/threads.pm
@@ -5,7 +5,7 @@ use 5.008;
 use strict;
 use warnings;
 
-our $VERSION = '2.23';      # remember to update version in POD!
+our $VERSION = '2.24';      # remember to update version in POD!
 my $XS_VERSION = $VERSION;
 $VERSION = eval $VERSION;
 
@@ -134,13 +134,13 @@ threads - Perl interpreter-based threads
 
 =head1 VERSION
 
-This document describes threads version 2.23
+This document describes threads version 2.24
 
 =head1 WARNING
 
 The "interpreter-based threads" provided by Perl are not the fast, lightweight
 system for multitasking that one might expect or hope for.  Threads are
-implemented in a way that make them easy to misuse.  Few people know how to
+implemented in a way that makes them easy to misuse.  Few people know how to
 use them correctly or will be able to provide help.
 
 The use of interpreter-based threads in perl is officially
