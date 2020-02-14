e041ae2ee2b832c4b0220e44abf414c126e1346f
Karl Williamson <khw@cpan.org>
2020-02-14 08:41:40 -0700

    regcomp.c: Remove WORST define
    The is not used and is actually 0, so does nothing as a flag, except
    when initializing.
    
    See the thread starting at
    http://nntp.perl.org/group/perl.perl5.porters/257101

--- regcomp.c
+++ regcomp.c
@@ -376,7 +376,6 @@ struct RExC_state_t {
 /*
  * Flags to be passed up and down.
  */
-#define	WORST		0	/* Worst case. */
 #define	HASWIDTH	0x01	/* Known to not match null strings, could match
                                    non-null ones. */
 
@@ -11183,7 +11182,7 @@ S_reg(pTHX_ RExC_state_t *pRExC_state, I32 paren, I32 *flagp, U32 depth)
         vFAIL("Too many nested open parens");
     }
 
-    *flagp = 0;				/* Tentatively. */
+    *flagp = 0;				/* Initialize. */
 
     if (RExC_in_lookbehind) {
 	RExC_in_lookbehind++;
@@ -12540,7 +12539,7 @@ S_regbranch(pTHX_ RExC_state_t *pRExC_state, I32 *flagp, I32 first, U32 depth)
         }
     }
 
-    *flagp = WORST;			/* Tentatively. */
+    *flagp = 0;			/* Initialize. */
 
     skip_to_be_ignored_text(pRExC_state, &RExC_parse,
                             FALSE /* Don't force to /x */ );
@@ -12763,7 +12762,7 @@ S_regpiece(pTHX_ RExC_state_t *pRExC_state, I32 *flagp, U32 depth)
 	    FLAGS(REGNODE_p(ret)) = 0;
 
 	    if (min > 0)
-		*flagp = WORST;
+		*flagp = 0;
 	    if (max > 0)
 		*flagp |= HASWIDTH;
             ARG1_SET(REGNODE_p(ret), (U16)min);
@@ -12801,7 +12800,7 @@ S_regpiece(pTHX_ RExC_state_t *pRExC_state, I32 *flagp, U32 depth)
 #endif
     nextchar(pRExC_state);
 
-    *flagp = (op != '+') ? (WORST|SPSTART|HASWIDTH) : (WORST|HASWIDTH);
+    *flagp = (op != '+') ? (SPSTART|HASWIDTH) : (HASWIDTH);
 
     if (op == '*') {
 	min = 0;
@@ -13470,7 +13469,7 @@ S_regatom(pTHX_ RExC_state_t *pRExC_state, I32 *flagp, U32 depth)
 
     DECLARE_AND_GET_RE_DEBUG_FLAGS;
 
-    *flagp = WORST;		/* Tentatively. */
+    *flagp = 0;		/* Initialize. */
 
     DEBUG_PARSE("atom");
 
