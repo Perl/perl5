f263169fd91a506af521aa215f0165d3eff2e459
Nicolas R <atoomic@cpan.org>
2020-02-27 15:26:41 -0700

    fixup make pod/perltoc.pod

--- Porting/new-perldelta.pl
+++ Porting/new-perldelta.pl
@@ -60,7 +60,7 @@ $olddelta =~ s{^(perl)(delta - what is new for perl v5.$old_major.$old_minor)$}
               {$1 . "5$old_major$old_minor" . $2}me
     or die "Can't find expected NAME contents in $olddelta";
 
-my $olddeltaname = "pod/perl7$old_major${old_minor}delta.pod";
+my $olddeltaname = "pod/perl5$old_major${old_minor}delta.pod";
 # in a built tree, $olddeltaname is a symlink to perldelta.pod, make sure
 # we don't write through it
 unlink($olddeltaname);
