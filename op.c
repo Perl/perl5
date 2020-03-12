6311900a664b5de7fc4b60c1935639bb1d0af7a8
Nicolas R <atoomic@cpan.org>
2020-03-12 15:14:26 -0600

    Revert "fixup to "avoid identical stack traces""
    This reverts commit fb8188b84d8a8f34f90aa9a8d9837892967f6b93.
    
    Unfortunately this is causing some errors during global destruction
    like:
    
    Unbalanced string table refcount: (1) for "open_IN" during global destruction
    
    It seems to be flapping, and recompiling Perl could see or not the
    issue.
    
    Upstream-URL: https://github.com/Perl/perl5/commit/fb8188b84d8a8f34f90aa9a8d9837892967f6b93

--- op.c
+++ op.c
@@ -11598,8 +11598,10 @@ S_process_special_blocks(pTHX_ I32 floor, const char *const fullname,
                  * to PL_compiling, IN_PERL_COMPILETIME/IN_PERL_RUNTIME
                  * will give the wrong answer.
                  */
-                PL_curcop = (COP*)newSTATEOP(PL_compiling.op_flags, NULL, NULL);
-                CopLINE_set(PL_curcop, CopLINE(&PL_compiling));
+                Newx(PL_curcop, 1, COP);
+                StructCopy(&PL_compiling, PL_curcop, COP);
+                PL_curcop->op_slabbed = 0;
+                SAVEFREEPV(PL_curcop);
             }
 
             PUSHSTACKi(PERLSI_REQUIRE);
