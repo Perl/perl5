eb43882a2e28f5ad3efa7f00d71c4b3d8fc8d70c
Nicolas R <atoomic@cpan.org>
2020-03-16 10:29:03 -0600

    silent no warnings experimental::signatures

--- lib/warnings.pm
+++ lib/warnings.pm
@@ -344,6 +344,11 @@ sub import
     ${^WARNING_BITS} = _bits($mask, @_);
 }
 
+my %removedWarningsFor = (
+    'FATAL'                    => 1,
+    'experimental::signatures' => 1,
+);
+
 sub unimport
 {
     shift;
@@ -356,7 +361,7 @@ sub unimport
 
     $mask = _expand_bits($mask);
     foreach my $word ( @_ ) {
-	if ($word eq 'FATAL') {
+	if ($removedWarningsFor{$word}) {
 	    next;
 	}
 	elsif ($catmask = $Bits{$word}) {
