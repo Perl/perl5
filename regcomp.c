7436a695ce7392c1cebf34ae9b05b77c9e5b86f9
Karl Williamson <khw@cpan.org>
2020-04-21 06:41:22 -0600

    regcomp.c: Combine common code
    This bit of code occurs in two places.  Move it so that it gets executed
    before the split, so it only has to be specified once.

--- regcomp.c
+++ regcomp.c
@@ -12633,12 +12633,13 @@ S_regpiece(pTHX_ RExC_state_t *pRExC_state, I32 *flagp, U32 depth)
 
     op = *RExC_parse;
 
+#ifdef RE_TRACK_PATTERN_OFFSETS
+    parse_start = RExC_parse;
+#endif
+
     if (op == '{') {
         const char* endptr;
 	maxpos = NULL;
-#ifdef RE_TRACK_PATTERN_OFFSETS
-        parse_start = RExC_parse; /* MJD */
-#endif
 	next = RExC_parse + 1;
 	while (isDIGIT(*next) || *next == ',') {
 	    if (*next == ',') {
@@ -12791,9 +12792,6 @@ S_regpiece(pTHX_ RExC_state_t *pRExC_state, I32 *flagp, U32 depth)
       vFAIL("Regexp *+ operand could be empty");
 #endif
 
-#ifdef RE_TRACK_PATTERN_OFFSETS
-    parse_start = RExC_parse;
-#endif
     nextchar(pRExC_state);
 
     *flagp = HASWIDTH;
