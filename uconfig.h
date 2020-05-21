Perl/perl5

commit e849841dca2a8b11119997585f795647c52cdcdf

Regenerate Configure and friends after metaconfig changes
A probe for wcrtomb() and one for attribute always inline have been
added, and the ones for:
1) checking if there is a C backtrace facility; and
2) character data alignedness
have been revised

config_h.SH
@@ -5,6 +5,13 @@
 #
 # See Porting/config_h.pl
 
+#!/bin/sh
+#
+# THIS IS A GENERATED FILE
+# DO NOT HAND-EDIT
+#
+# See Porting/config_h.pl
+
 : Set up for generating config_h.SH
 case "$CONFIG_SH" in
 '') CONFIG_SH=config.sh;;
@@ -28,6 +35,9 @@ esac
 case "$0" in
 */*) cd `expr X$0 : 'X\(.*\)/'` ;;
 esac
+case "$CONFIG_H" in
+already-done) echo "Not re-extracting config.h" ;;
+*)
 case "$CONFIG_H" in
 already-done) echo "Not re-extracting config.h" ;;
 *)
@@ -1500,6 +1510,7 @@ sed <<!GROK!THIS! >$CONFIG_H -e 's!^#undef\(.*/\)\*!/\*#define\1 \*!' -e 's!^#un
 #$d_attribute_pure HASATTRIBUTE_PURE	/**/
 #$d_attribute_unused HASATTRIBUTE_UNUSED	/**/
 #$d_attribute_warn_unused_result HASATTRIBUTE_WARN_UNUSED_RESULT	/**/
+#$d_attribute_always_inline HASATTRIBUTE_ALWAYS_INLINE	/**/
 
 /* HAS_BACKTRACE:
  *	This symbol, if defined, indicates that the backtrace() routine is
@@ -5122,6 +5133,12 @@ sed <<!GROK!THIS! >$CONFIG_H -e 's!^#undef\(.*/\)\*!/\*#define\1 \*!' -e 's!^#un
 #$d_ttyname_r HAS_TTYNAME_R	/**/
 #define TTYNAME_R_PROTO $ttyname_r_proto	/**/
 
+/* HAS_WCRTOMB:
+ *	This symbol, if defined, indicates that the wcrtomb routine is
+ *	available to convert a wide character into a multi-byte character.
+ */
+#$d_wcrtomb HAS_WCRTOMB	/**/
+
 /* I_MACH_CTHREADS:
  *	This symbol, if defined, indicates to the C program that it should
  *	include <mach/cthreads.h>.
@@ -5278,5 +5295,7 @@ sed <<!GROK!THIS! >$CONFIG_H -e 's!^#undef\(.*/\)\*!/\*#define\1 \*!' -e 's!^#un
 
 #endif
 !GROK!THIS!
+;;
+esac
 ;;
 esac

uconfig.h
@@ -1465,6 +1465,7 @@
 /*#define HASATTRIBUTE_PURE	/ **/
 /*#define HASATTRIBUTE_UNUSED	/ **/
 /*#define HASATTRIBUTE_WARN_UNUSED_RESULT	/ **/
+/*#define HASATTRIBUTE_ALWAYS_INLINE	/ **/
 
 /* HAS_BACKTRACE:
  *	This symbol, if defined, indicates that the backtrace() routine is
@@ -5087,6 +5088,12 @@
 /*#define HAS_TTYNAME_R	/ **/
 #define TTYNAME_R_PROTO 0	/**/
 
+/* HAS_WCRTOMB:
+ *	This symbol, if defined, indicates that the wcrtomb routine is
+ *	available to convert a wide character into a multi-byte character.
+ */
+/*#define HAS_WCRTOMB	/ **/
+
 /* I_MACH_CTHREADS:
  *	This symbol, if defined, indicates to the C program that it should
  *	include <mach/cthreads.h>.
@@ -5244,6 +5251,6 @@
 #endif
 
 /* Generated from:
- * e3dbc17ac938c9df0df81930ccebf94a0425dbff9d20c3edd18a84075eff4fc2 config_h.SH
- * a11d95f56af200907b9285ed0da243a4c20db34f5684af313d18195eaba2b18b uconfig.sh
+ * 0c6730559753fa1aa79fa3a10b50df81c20cae46d1fe3e3faef1c7d3aa13c92c config_h.SH
+ * 31c25f95118efbf99e358f81091058e6723b4cccb8474dfbc6d06c2b7d46ff17 uconfig.sh
  * ex: set ro: */

Configure
@@ -382,6 +382,7 @@ asctime_r_proto=''
 d_asctime_r=''
 d_asinh=''
 d_atanh=''
+d_attribute_always_inline=''
 d_attribute_deprecated=''
 d_attribute_format=''
 d_attribute_malloc=''
@@ -933,6 +934,7 @@ d_voidsig=''
 signal_t=''
 d_wait4=''
 d_waitpid=''
+d_wcrtomb=''
 d_wcscmp=''
 d_wcstombs=''
 d_wcsxfrm=''
@@ -4701,7 +4703,7 @@ else
 fi
 $rm -f try try.*
 case "$gccversion" in
-1.*) cpp=`./loc gcc-cpp $cpp $pth` ;;
+1*) cpp=`./loc gcc-cpp $cpp $pth` ;;
 esac
 case "$gccversion" in
 '') gccosandvers='' ;;
@@ -4741,7 +4743,7 @@ esac
 # gcc 3.* complain about adding -Idirectories that they already know about,
 # so we will take those off from locincpth.
 case "$gccversion" in
-3.*)
+3*)
     echo "main(){}">try.c
     for incdir in $locincpth; do
        warn=`$cc $ccflags -I$incdir -c try.c 2>&1 | \
@@ -5467,13 +5469,13 @@ fi
 case "$hint" in
 default|recommended)
 	case "$gccversion" in
-	1.*) dflt="$dflt -fpcc-struct-return" ;;
+	1*) dflt="$dflt -fpcc-struct-return" ;;
 	esac
 	case "$optimize:$DEBUGGING" in
 	*-g*:old) dflt="$dflt -DDEBUGGING";;
 	esac
 	case "$gccversion" in
-	2.*) if $test -d /etc/conf/kconfig.d &&
+	2*) if $test -d /etc/conf/kconfig.d &&
 			$contains _POSIX_VERSION $usrinc/sys/unistd.h >/dev/null 2>&1
 		then
 			# Interactive Systems (ISC) POSIX mode.
@@ -5482,7 +5484,7 @@ default|recommended)
 		;;
 	esac
 	case "$gccversion" in
-	1.*) ;;
+	1*) ;;
 	2.[0-8]*) ;;
 	?*)	set strict-aliasing -fno-strict-aliasing
 		eval $checkccflag
@@ -5600,7 +5602,7 @@ case "$cppflags" in
     ;;
 esac
 case "$gccversion" in
-1.*) cppflags="$cppflags -D__GNUC__"
+1*) cppflags="$cppflags -D__GNUC__"
 esac
 case "$mips_type" in
 '');;
@@ -9616,16 +9618,12 @@ prefixvar=sitescript
 set backtrace d_backtrace
 eval $inlibc
 
-: add flags if using c backtrace
+: Check if C backtrace is actually supported.
 case "$usecbacktrace" in
   "") usecbacktrace=$undef ;;
   [yY]*|true|$define)
     case "$d_backtrace" in
       [yY]*|true|$define)
-        case " $ccflags " in
-	  *" -DUSE_C_BACKTRACE "*) ;; # Already there.
-	  *) ccflags="$ccflags -DUSE_C_BACKTRACE -g" ;;
-	  esac
         ;;
       *)
         echo "This system does not support backtrace" >&4
@@ -11251,6 +11249,34 @@ fi
 *) val="$d_attribute_warn_unused_result" ;;
 esac
 set d_attribute_warn_unused_result
+eval $setvar
+$rm -f attrib*
+
+: Look for GCC-style attribute always_inline
+case "$d_attribute_always_inline" in
+'')
+echo " "
+echo "Checking whether your compiler can handle __attribute__((always_inline)) ..." >&4
+$cat >attrib.c <<'EOCP'
+#include <stdio.h>
+static __inline__ __attribute__((always_inline)) int I_will_always_be_inlined(void);
+EOCP
+if $cc $ccflags -c attrib.c >attrib.out 2>&1 ; then
+	if $contains 'warning' attrib.out >/dev/null 2>&1; then
+		echo "Your C compiler doesn't support __attribute__((always_inline))."
+		val="$undef"
+	else
+		echo "Your C compiler supports __attribute__((always_inline))."
+		val="$define"
+	fi
+else
+	echo "Your C compiler doesn't seem to understand __attribute__ at all."
+	val="$undef"
+fi
+;;
+*) val="$d_attribute_always_inline" ;;
+esac
+set d_attribute_always_inline
 eval $setvar
 $rm -f attrib*
 
@@ -15785,7 +15811,7 @@ eval $inlibc
 echo "Checking to see if you have isless..." >&4
 $cat >try.c <<EOCP
 #include <math.h>
-int main() { return isless(0.0); }
+int main() { return isless(2.0, 1.0); }
 EOCP
 set try
 if eval $compile; then
@@ -19705,16 +19731,108 @@ EOM
 	;;
 esac
 
+: Checking 32bit alignedness
 $cat <<EOM
 
 Checking to see whether you can access character data unalignedly...
-
-We assume only aligned access is permitted.
 EOM
-: Checking 32bit alignedness
-: We no longer check and assume it is required.
 case "$d_u32align" in
-'') d_u32align="$define" ;;
+'')   $cat >try.c <<EOCP
+#include <stdio.h>
+#$i_stdlib I_STDLIB
+#ifdef I_STDLIB
+#include <stdlib.h>
+#endif
+#define U32 $u32type
+#define BYTEORDER 0x$byteorder
+#define U8 $u8type
+#include <signal.h>
+#ifdef SIGBUS
+$signal_t bletch(int s) { exit(4); }
+#endif
+int main() {
+#if BYTEORDER == 0x1234 || BYTEORDER == 0x4321
+    volatile U8 buf[8];
+    volatile U32 *up;
+    int i;
+
+    if (sizeof(U32) != 4) {
+	printf("sizeof(U32) is not 4, but %d\n", sizeof(U32));
+	exit(1);
+    }
+
+    fflush(stdout);
+
+#ifdef SIGBUS
+    signal(SIGBUS, bletch);
+#endif
+
+    buf[0] = 0;
+    buf[1] = 0;
+    buf[2] = 0;
+    buf[3] = 1;
+    buf[4] = 0;
+    buf[5] = 0;
+    buf[6] = 0;
+    buf[7] = 1;
+
+    for (i = 0; i < 4; i++) {
+	up = (U32*)(buf + i);
+	if (! ((*up == 1 << (8*i)) ||   /* big-endian */
+	       (*up == 1 << (8*(3-i)))  /* little-endian */
+	      )
+	   )
+	{
+	    printf("read failed (%x)\n", *up);
+	    exit(2);
+	}
+    }
+
+    /* write test */
+    for (i = 0; i < 4; i++) {
+	up = (U32*)(buf + i);
+	*up = 0xBeef;
+	if (*up != 0xBeef) {
+	    printf("write failed (%x)\n", *up);
+	    exit(3);
+	}
+    }
+
+    exit(0);
+#else
+    printf("1\n");
+    exit(1);
+#endif
+    return 0;
+}
+EOCP
+set try
+if eval $compile_ok; then
+	echo "(Testing for character data alignment may crash the test.  That's okay.)" >&4
+	$run ./try 2>&1 >/dev/null
+	case "$?" in
+	0)	cat >&4 <<EOM
+You can access character data pretty unalignedly.
+EOM
+		d_u32align="$undef"
+		;;
+	*)	cat >&4 <<EOM
+It seems that you must access character data in an aligned manner.
+EOM
+		d_u32align="$define"
+		;;
+	esac
+else
+	rp='Can you access character data at unaligned addresses?'
+	dflt='n'
+	. ./myread
+	case "$ans" in
+	[yY]*)	d_u32align="$undef"  ;;
+	*)	d_u32align="$define" ;;
+	esac
+fi
+$rm_try
+;;
 esac
 
 : see if ualarm exists
@@ -19804,6 +19922,10 @@ eval $inlibc
 
 : see if waitpid exists
 set waitpid d_waitpid
+eval $inlibc
+
+: see if wcrtomb exists
+set wcrtomb d_wcrtomb
 eval $inlibc
 
 : look for wcscmp
@@ -23103,7 +23225,7 @@ fi
 
 : add -D_FORTIFY_SOURCE if feasible and not already there
 case "$gccversion" in
-[456789].*|[1-9][0-9]*)	case "$optimize$ccflags" in
+[456789].*)	case "$optimize$ccflags" in
 	*-O*)	case "$ccflags$cppsymbols" in
 		*_FORTIFY_SOURCE=*) # Don't add it again.
 			echo "You seem to have -D_FORTIFY_SOURCE already, not adding it." >&4
@@ -23956,6 +24078,7 @@ d_asinh='$d_asinh'
 d_atanh='$d_atanh'
 d_atolf='$d_atolf'
 d_atoll='$d_atoll'
+d_attribute_always_inline='$d_attribute_always_inline'
 d_attribute_deprecated='$d_attribute_deprecated'
 d_attribute_format='$d_attribute_format'
 d_attribute_malloc='$d_attribute_malloc'
@@ -24473,6 +24596,7 @@ d_voidtty='$d_voidtty'
 d_vsnprintf='$d_vsnprintf'
 d_wait4='$d_wait4'
 d_waitpid='$d_waitpid'
+d_wcrtomb='$d_wcrtomb'
 d_wcscmp='$d_wcscmp'
 d_wcstombs='$d_wcstombs'
 d_wcsxfrm='$d_wcsxfrm'

Cross/config.sh-arm-linux
@@ -116,6 +116,7 @@ d_asinh='undef'
 d_atanh='undef'
 d_atolf='undef'
 d_atoll='define'
+d_attribute_always_inline='undef'
 d_attribute_deprecated='undef'
 d_attribute_format='undef'
 d_attribute_malloc='undef'
@@ -629,6 +630,7 @@ d_voidtty=''
 d_vsnprintf='define'
 d_wait4='define'
 d_waitpid='define'
+d_wcrtomb='undef'
 d_wcscmp='undef'
 d_wcstombs='define'
 d_wcsxfrm='undef'

Cross/config.sh-arm-linux-n770
@@ -115,6 +115,7 @@ d_asinh='undef'
 d_atanh='undef'
 d_atolf='undef'
 d_atoll='define'
+d_attribute_always_inline='undef'
 d_attribute_deprecated='undef'
 d_attribute_format='undef'
 d_attribute_malloc='undef'
@@ -628,6 +629,7 @@ d_voidtty=''
 d_vsnprintf='define'
 d_wait4='define'
 d_waitpid='define'
+d_wcrtomb='undef'
 d_wcscmp='undef'
 d_wcstombs='define'
 d_wcsxfrm='undef'

NetWare/config.wc
@@ -103,6 +103,7 @@ d_asinh='undef'
 d_atanh='undef'
 d_atolf='undef'
 d_atoll='undef'
+d_attribute_always_inline='undef'
 d_attribute_deprecated='undef'
 d_attribute_format='undef'
 d_attribute_malloc='undef'
@@ -618,6 +619,7 @@ d_voidtty=''
 d_vsnprintf='undef'
 d_wait4='undef'
 d_waitpid='define'
+d_wcrtomb='undef'
 d_wcscmp='undef'
 d_wcstombs='define'
 d_wcsxfrm='undef'

Porting/config.sh
@@ -130,6 +130,7 @@ d_asinh='define'
 d_atanh='define'
 d_atolf='undef'
 d_atoll='define'
+d_attribute_always_inline='undef'
 d_attribute_deprecated='define'
 d_attribute_format='define'
 d_attribute_malloc='define'
@@ -645,6 +646,7 @@ d_voidtty=''
 d_vsnprintf='define'
 d_wait4='define'
 d_waitpid='define'
+d_wcrtomb='undef'
 d_wcscmp='define'
 d_wcstombs='define'
 d_wcsxfrm='define'

configure.com
@@ -5941,6 +5941,7 @@ $ WC "d_asinh='" + d_asinh + "'"
 $ WC "d_atanh='" + d_atanh + "'"
 $ WC "d_atolf='" + d_atolf + "'"
 $ WC "d_atoll='" + d_atoll + "'"
+$ WC "d_attribute_always_inline='undef'"
 $ WC "d_attribute_format='" + d_attribut + "'"
 $ WC "d_attribute_deprecated='undef'"
 $ WC "d_attribute_malloc='undef'"
@@ -6450,6 +6451,7 @@ $ WC "d_voidtty='" + "'"
 $ WC "d_vsnprintf='" + d_vsnprintf + "'"
 $ WC "d_wait4='" + d_wait4 + "'"
 $ WC "d_waitpid='define'"
+$ WC "d_wcrtomb='define'"
 $ WC "d_wcscmp='define'"
 $ WC "d_wcstombs='define'"
 $ WC "d_wcsxfrm='define'"

commit 4cf6a7be90b275623cdc867360b1cf1742f1ef27

Fix config_h.SH duplication due to re-running Porting/config_h.pl
Quoting Porting/config_h.pl :

  # This script is run just after metaconfig, and it
  # is run ONLY ONCE. Not to be used afterwards

It looks like e849841dca2a8b11119997585f795647c52cdcdf accidentally did
re-run it, resulting in some duplication in config_h.SH.

(Also includes regenerated uconfig.h by regen/uconfig_h.pl)

config_h.SH
@@ -5,13 +5,6 @@
 #
 # See Porting/config_h.pl
 
-#!/bin/sh
-#
-# THIS IS A GENERATED FILE
-# DO NOT HAND-EDIT
-#
-# See Porting/config_h.pl
-
 : Set up for generating config_h.SH
 case "$CONFIG_SH" in
 '') CONFIG_SH=config.sh;;
@@ -35,9 +28,6 @@ esac
 case "$0" in
 */*) cd `expr X$0 : 'X\(.*\)/'` ;;
 esac
-case "$CONFIG_H" in
-already-done) echo "Not re-extracting config.h" ;;
-*)
 case "$CONFIG_H" in
 already-done) echo "Not re-extracting config.h" ;;
 *)
@@ -5300,7 +5290,5 @@ sed <<!GROK!THIS! >$CONFIG_H -e 's!^#undef\(.*/\)\*!/\*#define\1 \*!' -e 's!^#un
 
 #endif
 !GROK!THIS!
-;;
-esac
 ;;
 esac

uconfig.h
@@ -5256,6 +5256,6 @@
 #endif
 
 /* Generated from:
- * 15acb59ff9e808ff9fbebc95510c0bed8b1de705034bf4b22482bfa431da15b6 config_h.SH
+ * cc910600600e832a5ad4789896a093ec8e001c4dec9ee3c7d7a3e6ba880651c7 config_h.SH
  * e598046e9da73796e21226371ce26e8f2144852e0f5da7f00e52e1dbe1eaeaa6 uconfig.sh
  * ex: set ro: */