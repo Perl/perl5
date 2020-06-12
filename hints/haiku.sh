6b6e83fce2074294ac11d547c8a75bc106bebab9
Thibault DUPONCHELLE <thibault.duponchelle@gmail.com>
2020-06-12 14:19:52 +0200

    Fix build on Haiku beta, set correct Haiku usrinc and locinc, add -fno-stack-protector to hints for amigaos.sh and haiku.sh (and disable accordingly -fstack-protector* in Configure)

--- hints/haiku.sh
+++ hints/haiku.sh
@@ -6,7 +6,6 @@ case "$prefix" in
 *) ;; # pass the user supplied value through
 esac
 
-
 libpth="$(finddir B_USER_DEVELOP_DIRECTORY)/lib $(finddir B_SYSTEM_DEVELOP_DIRECTORY)/lib $(finddir B_COMMON_DIRECTORY)/lib /system/lib"
 usrinc="$(finddir B_SYSTEM_DEVELOP_DIRECTORY)/headers/posix"
 locinc="$(finddir B_USER_CONFIG_DIRECTORY)/develop/headers $(finddir B_COMMON_DIRECTORY)/headers $(finddir B_SYSTEM_DEVELOP_DIRECTORY)/headers"
@@ -31,6 +30,8 @@ d_syserrlst='undef'
 # Haiku uses gcc.
 cc="gcc"
 ld='gcc'
+
+ccflags="$ccflags -fno-stack-protector"
 
 # The runtime loader library path variable is LIBRARY_PATH.
 case "$ldlibpthname" in
