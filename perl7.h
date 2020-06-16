e8fbb98738ca0d101cc153c46581a354630e7d11
Nicolas R <atoomic@cpan.org>
2020-06-16 11:59:52 -0600

    Attempt to setup P7 without loading any module

---
+++ perl7.h
@@ -0,0 +1,20 @@
+/*    perl7.h
+ *
+ *    Copyright (C) 2020 by Larry Wall and others
+ *
+ *    You may distribute under the terms of either the GNU General Public
+ *    License or the Artistic License, as specified in the README file.
+ *
+ */
+
+#ifndef H_PERL_7
+#define H_PERL_7 1
+
+/* this is used by toke.c to setup a Perl7 flavor */
+#define P7_TOKE_SETUP "use p7;"
+
+#endif /* Include guard */
+
+/*
+ * ex: set ts=8 sts=4 sw=4 et:
+ */
