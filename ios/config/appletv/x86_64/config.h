957c5f2cd7eebe3953c78f8284c54da40cdf1d2c
Jose Palao <jose@palao.com>
2020-06-23 02:58:30 +0200

    add TARGET_OS_IPHONE

--- ios/config/appletv/x86_64/config.h
+++ ios/config/appletv/x86_64/config.h
@@ -1417,9 +1417,9 @@
  *	This symbol is intended to be used along with CPPRUN in the same manner
  *	symbol CPPMINUS is used with CPPSTDIN. It contains either "-" or "".
  */
-#define CPPSTDIN "/usr/bin/clang -arch x86_64 -I/opt/local/include -I/Applications/Xcode.app/Contents/Developer/Platforms/AppleTVSimulator.platform/Developer/SDKs/AppleTVSimulator.sdk/usr/include -arch x86_64 -mappletvos-version-min=8.0 -isysroot /Applications/Xcode.app/Contents/Developer/Platforms/AppleTVSimulator.platform/Developer/SDKs/AppleTVSimulator.sdk -DPERL_APPLETV -DPERL_USE_SAFE_PUTENV -fno-common -DPERL_DARWIN -pipe -O0 -g -fno-strict-aliasing -fstack-protector-strong -E"
+#define CPPSTDIN "/usr/bin/clang -arch x86_64 -I/opt/local/include -I/Applications/Xcode.app/Contents/Developer/Platforms/AppleTVSimulator.platform/Developer/SDKs/AppleTVSimulator.sdk/usr/include -arch x86_64 -mappletvos-version-min=8.0 -isysroot /Applications/Xcode.app/Contents/Developer/Platforms/AppleTVSimulator.platform/Developer/SDKs/AppleTVSimulator.sdk -DPERL_APPLETV -DPERL_USE_SAFE_PUTENV -fno-common -DPERL_DARWIN -DTARGET_OS_IPHONE -pipe -O0 -g -fno-strict-aliasing -fstack-protector-strong -E"
 #define CPPMINUS "-"
-#define CPPRUN "/usr/bin/clang -arch x86_64 -I/opt/local/include -I/Applications/Xcode.app/Contents/Developer/Platforms/AppleTVSimulator.platform/Developer/SDKs/AppleTVSimulator.sdk/usr/include -arch x86_64 -mappletvos-version-min=8.0 -isysroot /Applications/Xcode.app/Contents/Developer/Platforms/AppleTVSimulator.platform/Developer/SDKs/AppleTVSimulator.sdk -DPERL_APPLETV -DPERL_USE_SAFE_PUTENV -fno-common -DPERL_DARWIN -pipe -O0 -g -fno-strict-aliasing -fstack-protector-strong -E"
+#define CPPRUN "/usr/bin/clang -arch x86_64 -I/opt/local/include -I/Applications/Xcode.app/Contents/Developer/Platforms/AppleTVSimulator.platform/Developer/SDKs/AppleTVSimulator.sdk/usr/include -arch x86_64 -mappletvos-version-min=8.0 -isysroot /Applications/Xcode.app/Contents/Developer/Platforms/AppleTVSimulator.platform/Developer/SDKs/AppleTVSimulator.sdk -DPERL_APPLETV -DPERL_USE_SAFE_PUTENV -fno-common -DPERL_DARWIN -DTARGET_OS_IPHONE -pipe -O0 -g -fno-strict-aliasing -fstack-protector-strong -E"
 #define CPPLAST "-"
 
 /* HAS_ACCESS:
