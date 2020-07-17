a9a7a872e8c9122ceab007873d7f401e2fc36546
Sawyer X <xsawyerx@cpan.org>
2020-07-18 00:32:05 +0300

    Bump the perl version in various places for 5.33.1

--- dist/Module-CoreList/lib/Module/CoreList.pm
+++ dist/Module-CoreList/lib/Module/CoreList.pm
@@ -4,7 +4,7 @@ use strict;
 our ( %released, %version, %families, %upstream, %bug_tracker, %deprecated, %delta );
 
 use version;
-our $VERSION = '5.20200717';
+our $VERSION = '5.20200820';
 
 sub PKG_PATTERN () { q#\A[a-zA-Z_][0-9a-zA-Z_]*(?:(::|')[0-9a-zA-Z_]+)*\z# }
 sub _looks_like_invocant ($) { local $@; !!eval { $_[0]->isa(__PACKAGE__) } }
@@ -367,6 +367,7 @@ sub changes_between {
     5.030003 => '2020-06-01',
     5.032000 => '2020-06-20',
     5.033000 => '2020-07-17',
+    5.033001 => '2020-08-20',
   );
 
 for my $version ( sort { $a <=> $b } keys %released ) {
@@ -17528,6 +17529,17 @@ for my $version ( sort { $a <=> $b } keys %released ) {
         removed => {
         }
     },
+    5.033001 => {
+        delta_from => 5.033000,
+        changed => {
+            'B::Op_private'         => '5.033001',
+            'Config'                => '5.033001',
+            'Module::CoreList'      => '5.20200820',
+            'Module::CoreList::Utils'=> '5.20200820',
+        },
+        removed => {
+        }
+    },
 );
 
 sub is_core
@@ -18651,6 +18663,13 @@ sub is_core
     },
     5.033000 => {
         delta_from => 5.032,
+        changed => {
+        },
+        removed => {
+        }
+    },
+    5.033001 => {
+        delta_from => 5.033,
         changed => {
         },
         removed => {
--- dist/Module-CoreList/lib/Module/CoreList/Utils.pm
+++ dist/Module-CoreList/lib/Module/CoreList/Utils.pm
@@ -4,7 +4,7 @@ use strict;
 use warnings;
 use Module::CoreList;
 
-our $VERSION = '5.20200717';
+our $VERSION = '5.20200820';
 our %utilities;
 
 sub utilities {
@@ -1624,6 +1624,13 @@ my %delta = (
     },
     5.033000 => {
         delta_from => 5.032,
+        changed => {
+        },
+        removed => {
+        }
+    },
+    5.033001 => {
+        delta_from => 5.033000,
         changed => {
         },
         removed => {
