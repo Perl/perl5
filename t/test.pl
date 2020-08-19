a1bd6395692dbab09d4c90af11a3d37972af9893
Nicolas R <nicolas@atoomic.org>
2020-08-19 15:57:09 -0600

    run_multiple_progs: advertise file and line number on failures
    This is helping identify test failures from run_multiple_progs
    by providing the source file and line number.

--- t/test.pl
+++ t/test.pl
@@ -1355,19 +1355,21 @@ sub run_multiple_progs {
 	local $::TODO = $reason{todo};
 
 	unless ($ok) {
-	    my $err_line = "PROG: $switch\n$prog\n" .
-			   "EXPECTED:\n$expected\n";
-	    $err_line   .= "EXIT STATUS: != 0\n" if $fatal;
-	    $err_line   .= "GOT:\n$results\n";
-	    $err_line   .= "EXIT STATUS: " . ($status >> 8) . "\n" if $fatal;
-	    if ($::TODO) {
-		$err_line =~ s/^/# /mg;
-		print $err_line;  # Harness can't filter it out from STDERR.
-	    }
-	    else {
-		print STDERR $err_line;
-	    }
-	}
+        my $err_line = '';
+        $err_line   .= "FILE: $file ; line $line\n" if defined $file;
+        $err_line   .= "PROG: $switch\n$prog\n" .
+			           "EXPECTED:\n$expected\n";
+        $err_line   .= "EXIT STATUS: != 0\n" if $fatal;
+        $err_line   .= "GOT:\n$results\n";
+        $err_line   .= "EXIT STATUS: " . ($status >> 8) . "\n" if $fatal;
+        if ($::TODO) {
+            $err_line =~ s/^/# /mg;
+            print $err_line;  # Harness can't filter it out from STDERR.
+        }
+        else {
+            print STDERR $err_line;
+        }
+    }
 
         if (defined $file) {
             _ok($ok, "at $file line $line", $name);
