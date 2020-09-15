8dcc7355faf658366e48227351b24a74e0e8be48
Todd Rinaldo <toddr@cpan.org>
2020-09-15 17:40:55 -0500

    Update IO-Socket-IP to CPAN version 0.41
    [DELTA]
    0.41 2020-09-15 16:59
     [CHANGES]
       * Updated to `=head2 barename` documentation style
    
     [BUGFIXES]
       * Remember to declare configure_requires Module::Build
    
    0.40 2020-09-15 16:41
      [CHANGES]
        * Don't ship a Makefile.PL
        * Updated docs about `-register` option
    
      [BUGFIXES]
        * Ensure safety under default-strict (RT133322)

--- Porting/Maintainers.pl
+++ Porting/Maintainers.pl
@@ -656,7 +656,7 @@ use File::Glob qw(:case);
     },
 
     'IO::Socket::IP' => {
-        'DISTRIBUTION' => 'PEVANS/IO-Socket-IP-0.39.tar.gz',
+        'DISTRIBUTION' => 'PEVANS/IO-Socket-IP-0.41.tar.gz',
         'FILES'        => q[cpan/IO-Socket-IP],
         'EXCLUDED'     => [
             qr{^examples/},
