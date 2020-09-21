6214834e70a87beb3851433e08f6ae20cc5327ba
Sawyer X <xsawyerx@cpan.org>
2020-09-21 19:58:12 +0200

    Bump Module::CoreList in Maintainers.pl

--- Porting/Maintainers.pl
+++ Porting/Maintainers.pl
@@ -817,7 +817,7 @@ use File::Glob qw(:case);
     },
 
     'Module::CoreList' => {
-        'DISTRIBUTION' => 'BINGOS/Module-CoreList-5.20200428.tar.gz',
+        'DISTRIBUTION' => 'BINGOS/Module-CoreList-5.20200920.tar.gz',
         'FILES'        => q[dist/Module-CoreList],
     },
 
