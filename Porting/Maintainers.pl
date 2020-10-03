0df722d69378fb3b3df5c8d7746122138fcdc5a9
Karen Etheridge <ether@cpan.org>
2020-10-02 21:07:27 -0700

    Update File-Temp to CPAN version 0.2311
      [DELTA]
    
    0.2311    2020-10-03 04:04:15Z
              - Fix new PERMS test so as to also pass on root (RT#133443)
    
    0.2310    2020-09-26 17:37:56Z
              - add AppVeyor CI
              - Add PERMS options to create temp file with given file permissions
              - remove remaining uses of indirect object syntax (#34, Nicolas R)

--- Porting/Maintainers.pl
+++ Porting/Maintainers.pl
@@ -535,7 +535,7 @@ use File::Glob qw(:case);
     },
 
     'File::Temp' => {
-        'DISTRIBUTION' => 'ETHER/File-Temp-0.2309.tar.gz',
+        'DISTRIBUTION' => 'ETHER/File-Temp-0.2311.tar.gz',
         'FILES'        => q[cpan/File-Temp],
         'EXCLUDED'     => [
             qw( README.mkdn
