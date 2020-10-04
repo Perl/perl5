d65eec809ef85dabc6484418ec2b227220003158
Karl Williamson <khw@cpan.org>
2020-10-04 17:20:31 -0600

    regcomp.c: regpiece(): Pass along that is a code block
    regpiece was failing to pass up to its caller that a piece of code
    contained a code block, if that code was quantified.
    
    I don't know what the consequences of this are

--- regcomp.c
+++ regcomp.c
@@ -12746,6 +12746,9 @@ S_regpiece(pTHX_ RExC_state_t *pRExC_state, I32 *flagp, U32 depth)
         }
     }
 
+    /* If this is a code block pass it up */
+    *flagp |= (flags & POSTPONED);
+
     if (max > 0)
         *flagp |= (flags & HASWIDTH);
 
