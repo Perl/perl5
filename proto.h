14948324ad8584d0e17ee975308fbb698e8f16a0
Karl Williamson <khw@cpan.org>
2020-12-16 13:54:35 -0700

    locale.c: Use a function table to simplify code
    Some locale categories require extra steps when they are changed.  This
    moves that logic to a table, which gets rid of some code

--- proto.h
+++ proto.h
@@ -5060,6 +5060,8 @@ STATIC const char*	S_category_name(const int category);
 #define PERL_ARGS_ASSERT_CATEGORY_NAME
 STATIC unsigned int	S_get_category_index(const int category, const char * locale);
 #define PERL_ARGS_ASSERT_GET_CATEGORY_INDEX
+STATIC void	S_new_LC_ALL(pTHX_ const char* unused);
+#define PERL_ARGS_ASSERT_NEW_LC_ALL
 STATIC void	S_new_collate(pTHX_ const char* newcoll);
 #define PERL_ARGS_ASSERT_NEW_COLLATE
 STATIC void	S_new_ctype(pTHX_ const char* newctype);
