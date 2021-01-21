fa08f8614adc5988a520a33b805aaf53f0153c46
Chris 'BinGOs' Williams <chris@bingosnet.co.uk>
2021-01-21 09:30:07 +0000

    Who wants to be the disco king?

--- Porting/Maintainers.pl
+++ Porting/Maintainers.pl
@@ -811,7 +811,7 @@ use File::Glob qw(:case);
     },
 
     'Module::CoreList' => {
-        'DISTRIBUTION' => 'BINGOS/Module-CoreList-5.20201120.tar.gz',
+        'DISTRIBUTION' => 'BINGOS/Module-CoreList-5.20210120.tar.gz',
         'FILES'        => q[dist/Module-CoreList],
     },
 
