b3a019b423725caa4eb6a8615a6c1367b85326b0
reneeb <info@perl-services.de>
2021-02-21 08:44:35 +0100

    update Module::CoreList distribution in Porting/Maintainers.pl

--- Porting/Maintainers.pl
+++ Porting/Maintainers.pl
@@ -808,7 +808,7 @@ use File::Glob qw(:case);
     },
 
     'Module::CoreList' => {
-        'DISTRIBUTION' => 'BINGOS/Module-CoreList-5.20210123.tar.gz',
+        'DISTRIBUTION' => 'BINGOS/Module-CoreList-5.20210220.tar.gz',
         'FILES'        => q[dist/Module-CoreList],
     },
 
