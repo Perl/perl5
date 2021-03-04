0494a7e2c2af12c9e359461b2c1d072f58e2f5bd
Karl Williamson <khw@cpan.org>
2021-03-03 18:50:42 -0700

    locale.c: Use DEBUG_(PRE|POST_)STMTS to save/restore errno
    Instead of explicitly saving the errno around debugging statements, the
    more general mechanism is used.

--- locale.c
+++ locale.c
@@ -101,6 +101,9 @@ static int debug_initialization = 0;
 #  define DEBUG_INITIALIZATION_set(v)
 #endif
 
+#define DEBUG_PRE_STMTS   dSAVE_ERRNO;
+#define DEBUG_POST_STMTS  RESTORE_ERRNO;
+
 #include "EXTERN.h"
 #define PERL_IN_LOCALE_C
 #include "perl_langinfo.h"
@@ -1065,13 +1068,10 @@ S_emulate_setlocale_i(pTHX_
              " %p\n", __FILE__, __LINE__, line, old_obj));
 
     if (! old_obj) {
-            dSAVE_ERRNO;
         DEBUG_L(PerlIO_printf(Perl_debug_log,
                               "%s: %" LINE_Tf " (%" LINE_Tf "):"
                                " emulate_setlocale_i switching to C failed:"
                                " %d\n", __FILE__, __LINE__, line, GET_ERRNO));
-            RESTORE_ERRNO;
-
         return NULL;
     }
 
@@ -1118,8 +1118,6 @@ S_emulate_setlocale_i(pTHX_
         new_obj = newlocale(mask, new_locale, old_obj);
 
         if (! new_obj) {
-            dSAVE_ERRNO;
-
             DEBUG_L(PerlIO_printf(Perl_debug_log,
                     "%s: %" LINE_Tf " (%" LINE_Tf "): emulate_setlocale_i"
                     " creating new object failed: errno=%d\n",
@@ -1154,7 +1152,6 @@ S_emulate_setlocale_i(pTHX_
             }
 #    endif
 
-            RESTORE_ERRNO;
             return NULL;
         }
 
@@ -1171,15 +1168,12 @@ S_emulate_setlocale_i(pTHX_
         /* Here, successfully created an object representing the desired
          * locale; now switch into it */
         if (! uselocale(new_obj)) {
-            dSAVE_ERRNO;
-
             DEBUG_L(PerlIO_printf(Perl_debug_log,
                     "%s: %" LINE_Tf " (%" LINE_Tf "): emulate_setlocale_i"
                     " switching to new object failed\n",
                     __FILE__, __LINE__, line));
 
             if (! uselocale(old_obj)) {
-
                 DEBUG_L(PerlIO_printf(Perl_debug_log,
                                       "%s: %" LINE_Tf " (%" LINE_Tf "):"
                                       " switching back to %p failed: %d\n",
@@ -1187,8 +1181,8 @@ S_emulate_setlocale_i(pTHX_
                                        GET_ERRNO));
 
             }
+
             freelocale(new_obj);
-            RESTORE_ERRNO;
             return NULL;
         }
     }
@@ -2419,10 +2413,8 @@ S_win32_setlocale(pTHX_ int category, const char* locale)
     result = setlocale(category, locale);
 #endif
     DEBUG_L(STMT_START {
-                dSAVE_ERRNO;
                 PerlIO_printf(Perl_debug_log, "%s:%d: %s\n", __FILE__, __LINE__,
                             setlocale_debug_string_r(category, locale, result));
-                RESTORE_ERRNO;
             } STMT_END);
 
     if (! override_LC_ALL)  {
@@ -2451,11 +2443,9 @@ S_win32_setlocale(pTHX_ int category, const char* locale)
 
     result = setlocale(LC_ALL, NULL);
     DEBUG_L(STMT_START {
-                dSAVE_ERRNO;
                 PerlIO_printf(Perl_debug_log, "%s:%d: %s\n",
                                __FILE__, __LINE__,
                                setlocale_debug_string_c(LC_ALL, NULL, result));
-                RESTORE_ERRNO;
             } STMT_END);
 
     return result;
