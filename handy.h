5bf5e40b412e024945b916ee4d0a0b8cac2decda
Karl Williamson <khw@cpan.org>
2021-03-27 13:15:10 -0600

    handy.h: Don't use char class if no LC_CTYPE
    It is possible to compile perl to not pay attention to LC_CTYPE.  This
    was testing for no locales at all; whereas the stricter requirement
    should be used.

--- handy.h
+++ handy.h
@@ -724,7 +724,7 @@ based on the underlying C library functions):
  * macros that contain _LC in their names
  */
 
-#ifdef USE_LOCALE
+#ifdef USE_LOCALE_CTYPE
 #  ifndef CTYPE256
 #    define CTYPE256
 #  endif
