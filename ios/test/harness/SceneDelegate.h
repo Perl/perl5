b918df8a58b2503eda8493e2d6a096961717434e
Jose Palao <dr.palao@gmail.com>
2021-04-11 21:33:45 +0200

    add back SceneDelegate, needed on simulator

---
+++ ios/test/harness/SceneDelegate.h
@@ -0,0 +1,16 @@
+//
+//  SceneDelegate.h
+//  test
+//
+//  Created by jose on 11/05/2020.
+//  Copyright © 2020 jose. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SceneDelegate : UIResponder <UIWindowSceneDelegate>
+
+@property (strong, nonatomic) UIWindow * window;
+
+@end
+
