79bd11b047da22520f8f3f2d09f9ec2df5bc54e2
Karl Williamson <khw@cpan.org>
2021-04-11 18:48:12 -0600

    Fix Add github email alias for Dominic Hamon
    This fixes a failing porting test, and was caused by github
    inappropriately changing the author's email in a Squash and Merge
    operation resulting from  commit 12c68733611a690880d3f4621d10c9110b8ac7e4

--- Porting/checkAUTHORS.pl
+++ Porting/checkAUTHORS.pl
@@ -884,6 +884,7 @@ dev-perl\100pimb.org                    knew-p5p\100pimb.org
 +                                       lists-p5p\100pimb.org
 djberg86\100attbi.com                   djberg96\100attbi.com
 dk\100tetsuo.karasik.eu.org             dmitry\100karasik.eu.org
+dma+github@stripysock.com               dominichamon@users.noreply.github.com
 dom\100earth.li                         dom\100semmle.com
 domo\100computer.org                    shouldbedomo\100mac.com
 +                                       domo\100slipper.ip.lu
