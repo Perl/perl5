9fb51e18ceb8802e9e187882c4fdcb3a7271405d
Nicholas Clark <nick@ccl4.org>
2021-05-22 09:36:21 +0000

    Bump Data::Dumper's $VERSION and update Changes, for a dev release.

--- dist/Data-Dumper/Dumper.pm
+++ dist/Data-Dumper/Dumper.pm
@@ -29,7 +29,7 @@ our ( $Indent, $Trailingcomma, $Purity, $Pad, $Varname, $Useqq, $Terse, $Freezer
 our ( @ISA, @EXPORT, @EXPORT_OK, $VERSION );
 
 BEGIN {
-    $VERSION = '2.180'; # Don't forget to set version and release
+    $VERSION = '2.180_50'; # Don't forget to set version and release
                         # date in POD below!
 
     @ISA = qw(Exporter);
@@ -1439,7 +1439,7 @@ modify it under the same terms as Perl itself.
 
 =head1 VERSION
 
-Version 2.180
+Version 2.180_50
 
 =head1 SEE ALSO
 
