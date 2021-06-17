9eb497a5312d129e899b37f35a0599a9e0922f8f
Thibault DUPONCHELLE <thibault.duponchelle@gmail.com>
2021-06-17 08:26:33 +0200

    Update UCD version. Remove changes to cpan Encode. Regen

--- lib/Unicode/UCD.pm
+++ lib/Unicode/UCD.pm
@@ -5,7 +5,7 @@ use warnings;
 no warnings 'surrogate';    # surrogates can be inputs to this
 use charnames ();
 
-our $VERSION = '0.75';
+our $VERSION = '0.76';
 
 sub DEBUG () { 0 }
 $|=1 if DEBUG;
