8cc4f46aab32f24119773a8bcb2e8dbfdb27fd3e
Nicholas Clark <nick@ccl4.org>
2021-07-04 19:23:05 +0000

    Copy values that are "written as IV, then read as PV" with the same flags.
    Previously Perl_sv_setsv_flags() would gleefully turn on SVf_POK for these
    values, which meant that any copy no longer propagated the (new) state
    that says "this value started as an integer".

--- sv.c
+++ sv.c
@@ -4892,6 +4892,12 @@ Perl_sv_setsv_flags(pTHX_ SV *dsv, SV* ssv, const I32 flags)
             SvIV_set(dsv, SvIVX(ssv));
             if (sflags & SVf_IVisUV)
                 SvIsUV_on(dsv);
+            if ((sflags & SVf_IOK) && !(sflags & SVf_POK)) {
+                /* Source was SVf_IOK|SVp_IOK|SVp_POK but not SVf_POK, meaning
+                   a value set as an integer and later stringified. So mark
+                   destination the same: */
+                SvFLAGS(dsv) &= ~SVf_POK;
+            }
         }
         SvFLAGS(dsv) |= sflags & (SVf_IOK|SVp_IOK|SVf_NOK|SVp_NOK|SVf_UTF8);
         {
