c2137cede9d6dffe9eb4da6dd749ff20b20d8257
Nicholas Clark <nick@ccl4.org>
2021-08-20 11:28:40 +0000

    Change S_new_body to static inline, and use it instead of a macro
    Rename the macro new_body_inline() to new_body_from_arena().
    
    The macro existed to provide an inline implementation for hot code paths,
    with a small function for the others. Now that we have proper inline
    functions, change the function to be inlined, and use it instead of the
    macro. There is no change to the code generated for the hot paths. This
    makes the source simpler.
    
    Yes, this will also inline the function in a couple of other places, but
    these days optimising compilers often do that anyway for small functions.
    Even if they aren't for this function (as was), the size of the function
    itself is comparable with the calling overheads, so it's not a massive
    penalty.
    
    Two if blocks now need to be placed inside #ifndef PURIFY/#endif guards
    because they now use the function S_new_body(), which is not defined with
    PURIFY. The code is actually unreachable - defining PURIFY causes all
    values for new_type_details->arena to be forced to 0 - hence the if was
    never true under PURIFY. Hence it could have been guarded the same way
    previously, but for (maybe false) improved readability was not. Previously
    there wasn't a compilation failure because the unreachable code was using
    a macro, not a static function, and the macro was always defined, even when
    not used.
    
    Really the macro's definition should have been inside #ifndef PURIFY, so
    fix this oversight - it is now only defined if it is needed.
    
    Retain the macro (with a new name), and split the parameters to the macro.
    This will make the next commit simpler.

--- sv.c
+++ sv.c
@@ -833,7 +833,7 @@ the start of the structure.  IV bodies, and also in some builds NV bodies,
 don't need it either, because they are no longer allocated.
 
 In turn, the new_body_* allocators call S_new_body(), which invokes
-new_body_inline macro, which takes a lock, and takes a body off the
+new_body_from_arena macro, which takes a lock, and takes a body off the
 linked list at PL_body_roots[sv_type], calling Perl_more_bodies() if
 necessary to refresh an empty list.  Then the lock is released, and
 the body is returned.
@@ -1162,27 +1162,24 @@ Perl_more_bodies (pTHX_ const svtype sv_type, const size_t body_size,
     }
 }
 
-/* grab a new thing from the free list, allocating more if necessary.
-   The inline version is used for speed in hot routines, and the
-   function using it serves the rest (unless PURIFY).
-*/
-#define new_body_inline(xpv, sv_type) \
+#ifndef PURIFY
+
+/* grab a new thing from the arena's free list, allocating more if necessary. */
+#define new_body_from_arena(xpv, root_index, type_meta) \
     STMT_START { \
-        void ** const r3wt = &PL_body_roots[sv_type]; \
+        void ** const r3wt = &PL_body_roots[root_index]; \
         xpv = (PTR_TBL_ENT_t*) (*((void **)(r3wt))      \
-          ? *((void **)(r3wt)) : Perl_more_bodies(aTHX_ sv_type, \
-                                             bodies_by_type[sv_type].body_size,\
-                                             bodies_by_type[sv_type].arena_size)); \
+          ? *((void **)(r3wt)) : Perl_more_bodies(aTHX_ root_index, \
+                                             type_meta.body_size,\
+                                             type_meta.arena_size)); \
         *(r3wt) = *(void**)(xpv); \
     } STMT_END
 
-#ifndef PURIFY
-
-STATIC void *
+PERL_STATIC_INLINE void *
 S_new_body(pTHX_ const svtype sv_type)
 {
     void *xpv;
-    new_body_inline(xpv, sv_type);
+    new_body_from_arena(xpv, sv_type, bodies_by_type[sv_type]);
     return xpv;
 }
 
@@ -1350,7 +1347,7 @@ Perl_sv_upgrade(pTHX_ SV *const sv, svtype new_type)
         assert(new_type_details->arena);
         assert(new_type_details->arena_size);
         /* This points to the start of the allocated area.  */
-        new_body_inline(new_body, new_type);
+        new_body = S_new_body(aTHX_ new_type);
         Zero(new_body, new_type_details->body_size, char);
         new_body = ((char *)new_body) - new_type_details->offset;
 #else
@@ -1418,12 +1415,15 @@ Perl_sv_upgrade(pTHX_ SV *const sv, svtype new_type)
         assert(new_type_details->body_size);
         /* We always allocated the full length item with PURIFY. To do this
            we fake things so that arena is false for all 16 types..  */
+#ifndef PURIFY
         if(new_type_details->arena) {
             /* This points to the start of the allocated area.  */
-            new_body_inline(new_body, new_type);
+            new_body = S_new_body(aTHX_ new_type);
             Zero(new_body, new_type_details->body_size, char);
             new_body = ((char *)new_body) - new_type_details->offset;
-        } else {
+        } else
+#endif
+        {
             new_body = new_NOARENAZ(new_type_details);
         }
         SvANY(sv) = new_body;
@@ -14331,11 +14331,14 @@ S_sv_dup_common(pTHX_ const SV *const ssv, CLONE_PARAMS *const param)
             case SVt_INVLIST:
             case SVt_PV:
                 assert(sv_type_details->body_size);
+#ifndef PURIFY
                 if (sv_type_details->arena) {
-                    new_body_inline(new_body, sv_type);
+                    new_body = S_new_body(aTHX_ sv_type);
                     new_body
                         = (void*)((char*)new_body - sv_type_details->offset);
-                } else {
+                } else
+#endif
+                {
                     new_body = new_NOARENA(sv_type_details);
                 }
             }
