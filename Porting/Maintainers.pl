f32d3523b3331dced464b9de736c0ed632295d70
Chris 'BinGOs' Williams <chris@bingosnet.co.uk>
2021-08-21 11:06:12 +0100

    Here be corelist

--- Porting/Maintainers.pl
+++ Porting/Maintainers.pl
@@ -816,7 +816,7 @@ use File::Glob qw(:case);
     },
 
     'Module::CoreList' => {
-        'DISTRIBUTION' => 'BINGOS/Module-CoreList-5.20210620.tar.gz',
+        'DISTRIBUTION' => 'BINGOS/Module-CoreList-5.20210820.tar.gz',
         'FILES'        => q[dist/Module-CoreList],
     },
 
