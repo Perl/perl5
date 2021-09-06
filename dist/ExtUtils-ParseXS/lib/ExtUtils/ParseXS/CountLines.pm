42510d5ab1dca0720ccf23ac6156d1d54aa971b5
Tony Cook <tony@develop-help.com>
2021-09-06 14:13:36 +1000

    bump $ExtUtils::ParseXS::VERSION

--- dist/ExtUtils-ParseXS/lib/ExtUtils/ParseXS/CountLines.pm
+++ dist/ExtUtils-ParseXS/lib/ExtUtils/ParseXS/CountLines.pm
@@ -1,7 +1,7 @@
 package ExtUtils::ParseXS::CountLines;
 use strict;
 
-our $VERSION = '3.43';
+our $VERSION = '3.44';
 
 our $SECTION_END_MARKER;
 
--- dist/ExtUtils-ParseXS/lib/ExtUtils/Typemaps/Cmd.pm
+++ dist/ExtUtils-ParseXS/lib/ExtUtils/Typemaps/Cmd.pm
@@ -2,7 +2,7 @@ package ExtUtils::Typemaps::Cmd;
 use 5.006001;
 use strict;
 use warnings;
-our $VERSION = '3.43';
+our $VERSION = '3.44';
 
 use ExtUtils::Typemaps;
 
