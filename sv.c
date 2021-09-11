Perl/perl5

commit 914bb57489325d34ddbb7c0557c53df7baa84d86

Define a third kind of COW state; STATIC
Previously, when IsCOW flag was set there were two cases:
  SvLEN()==0:
      PV is really a shared HEK

  SvLEN()!=0:
      PV is a COW structure with 1..256 refcount stored in its extra final byte

This change adds a third state:

  SvLEN()==0 && SvFLAGS() & SVppv_STATIC:
      PV is a shared static const pointer and must not be modified

sv_setsv_flags() and sv_setsv_cow() will preserve this state

sv_uncow() will copy it out to a regular string buffer

sv_dup() will preserve the static pointer into cloned threads

sv.c
@@ -4687,6 +4687,15 @@ Perl_sv_setsv_flags(pTHX_ SV *dsv, SV* ssv, const I32 flags)
             SvCUR_set(ssv, 0);
             SvTEMP_off(ssv);
         }
+        /* We must check for SvIsCOW_static() even without
+         * SV_COW_SHARED_HASH_KEYS being set or else we'll break SvIsBOOL()
+         */
+        else if (SvIsCOW_static(ssv)) {
+            SvPV_set(dsv, SvPVX(ssv));
+            SvLEN_set(dsv, 0);
+            SvCUR_set(dsv, cur);
+            SvFLAGS(dsv) |= (SVf_IsCOW|SVppv_STATIC);
+        }
         else if (flags & SV_COW_SHARED_HASH_KEYS
               &&
 #ifdef PERL_COPY_ON_WRITE
@@ -4873,6 +4882,7 @@ Perl_sv_setsv_cow(pTHX_ SV *dsv, SV *ssv)
     STRLEN cur = SvCUR(ssv);
     STRLEN len = SvLEN(ssv);
     char *new_pv;
+    U32 new_flags = (SVt_COW|SVf_POK|SVp_POK|SVf_IsCOW);
 #if defined(PERL_DEBUG_READONLY_COW) && defined(PERL_COPY_ON_WRITE)
     const bool already = cBOOL(SvIsCOW(ssv));
 #endif
@@ -4901,12 +4911,17 @@ Perl_sv_setsv_cow(pTHX_ SV *dsv, SV *ssv)
     assert (SvPOKp(ssv));
 
     if (SvIsCOW(ssv)) {
-
-        if (SvLEN(ssv) == 0) {
+        if (SvIsCOW_shared_hash(ssv)) {
             /* source is a COW shared hash key.  */
             DEBUG_C(PerlIO_printf(Perl_debug_log,
                                   "Fast copy on write: Sharing hash\n"));
             new_pv = HEK_KEY(share_hek_hek(SvSHARED_HEK_FROM_PV(SvPVX_const(ssv))));
+            goto common_exit;
+        }
+        else if (SvIsCOW_static(ssv)) {
+            /* source is static constant; preserve this */
+            new_pv = SvPVX(ssv);
+            new_flags |= SVppv_STATIC;
             goto common_exit;
         }
         assert(SvCUR(ssv)+1 < SvLEN(ssv));
@@ -4928,7 +4943,7 @@ Perl_sv_setsv_cow(pTHX_ SV *dsv, SV *ssv)
 
   common_exit:
     SvPV_set(dsv, new_pv);
-    SvFLAGS(dsv) = (SVt_COW|SVf_POK|SVp_POK|SVf_IsCOW);
+    SvFLAGS(dsv) = new_flags;
     if (SvUTF8(ssv))
         SvUTF8_on(dsv);
     SvLEN_set(dsv, len);
@@ -5220,6 +5235,7 @@ S_sv_uncow(pTHX_ SV * const sv, const U32 flags)
         const char * const pvx = SvPVX_const(sv);
         const STRLEN len = SvLEN(sv);
         const STRLEN cur = SvCUR(sv);
+        const bool was_shared_hek = SvIsCOW_shared_hash(sv);
 
 #ifdef DEBUGGING
         if (DEBUG_C_TEST) {
@@ -5264,7 +5280,7 @@ S_sv_uncow(pTHX_ SV * const sv, const U32 flags)
                 SvCUR_set(sv, cur);
                 *SvEND(sv) = '\0';
             }
-            if (! len) {
+            if (was_shared_hek) {
                         unshare_hek(SvSHARED_HEK_FROM_PV(pvx));
             }
 #ifdef DEBUGGING
@@ -6836,17 +6852,20 @@ Perl_sv_clear(pTHX_ SV *const orig_sv)
                         sv_dump(sv);
                     }
 #endif
-                    if (SvLEN(sv)) {
+                    if (SvIsCOW_static(sv)) {
+                        SvLEN_set(sv, 0);
+                    }
+                    else if (SvIsCOW_shared_hash(sv)) {
+                        unshare_hek(SvSHARED_HEK_FROM_PV(SvPVX_const(sv)));
+                    }
+                    else {
                         if (CowREFCNT(sv)) {
                             sv_buf_to_rw(sv);
                             CowREFCNT(sv)--;
                             sv_buf_to_ro(sv);
                             SvLEN_set(sv, 0);
                         }
-                    } else {
-                        unshare_hek(SvSHARED_HEK_FROM_PV(SvPVX_const(sv)));
                     }
-
                 }
                 if (SvLEN(sv)) {
                     Safefree(SvPVX_mutable(sv));
@@ -14100,7 +14119,7 @@ Perl_rvpv_dup(pTHX_ SV *const dsv, const SV *const ssv, CLONE_PARAMS *const para
             if (isGV_with_GP(ssv)) {
                 /* Don't need to do anything here.  */
             }
-            else if ((SvIsCOW(ssv))) {
+            else if ((SvIsCOW_shared_hash(ssv))) {
                 /* A "shared" PV - clone it as "shared" PV */
                 SvPV_set(dsv,
                          HEK_KEY(hek_dup(SvSHARED_HEK_FROM_PV(SvPVX_const(ssv)),
@@ -16027,13 +16046,13 @@ Perl_init_constants(pTHX)
     SvREFCNT(&PL_sv_no)		= SvREFCNT_IMMORTAL;
     SvFLAGS(&PL_sv_no)		= SVt_PVNV|SVf_READONLY|SVf_PROTECT
                                   |SVp_IOK|SVf_IOK|SVp_NOK|SVf_NOK
-                                  |SVp_POK|SVf_POK;
+                                  |SVp_POK|SVf_POK|SVf_IsCOW|SVppv_STATIC;
 
     SvANY(&PL_sv_yes)		= new_XPVNV();
     SvREFCNT(&PL_sv_yes)	= SvREFCNT_IMMORTAL;
     SvFLAGS(&PL_sv_yes)		= SVt_PVNV|SVf_READONLY|SVf_PROTECT
                                   |SVp_IOK|SVf_IOK|SVp_NOK|SVf_NOK
-                                  |SVp_POK|SVf_POK;
+                                  |SVp_POK|SVf_POK|SVf_IsCOW|SVppv_STATIC;
 
     SvANY(&PL_sv_zero)		= new_XPVNV();
     SvREFCNT(&PL_sv_zero)	= SvREFCNT_IMMORTAL;

MANIFEST
@@ -5830,6 +5830,7 @@ t/op/auto.t			See if autoincrement et all work
 t/op/avhv.t			See if pseudo-hashes work
 t/op/bless.t			See if bless works
 t/op/blocks.t			See if BEGIN and friends work
+t/op/bool.t			Check misc details of boolean values
 t/op/bop.t			See if bitops work
 t/op/caller.pl			Tests shared between caller.t and XS op.t
 t/op/caller.t			See if caller() works

sv.h
@@ -462,6 +462,8 @@ perform the upgrade if necessary.  See C<L</svtype>>.
 /* Some private flags. */
 
 
+/* scalar SVs with SVp_POK */
+#define SVppv_STATIC    0x40000000 /* PV is pointer to static const; must be set with SVf_IsCOW */
 /* PVAV */
 #define SVpav_REAL	0x40000000  /* free old entries */
 /* PVHV */
@@ -2008,10 +2010,11 @@ scalar.
     )								\
 )
 
-#define SvIsCOW(sv)		(SvFLAGS(sv) & SVf_IsCOW)
-#define SvIsCOW_on(sv)		(SvFLAGS(sv) |= SVf_IsCOW)
-#define SvIsCOW_off(sv)		(SvFLAGS(sv) &= ~SVf_IsCOW)
-#define SvIsCOW_shared_hash(sv)	(SvIsCOW(sv) && SvLEN(sv) == 0)
+#define SvIsCOW(sv)              (SvFLAGS(sv) & SVf_IsCOW)
+#define SvIsCOW_on(sv)           (SvFLAGS(sv) |= SVf_IsCOW)
+#define SvIsCOW_off(sv)          (SvFLAGS(sv) &= ~(SVf_IsCOW|SVppv_STATIC))
+#define SvIsCOW_shared_hash(sv)  ((SvFLAGS(sv) & (SVf_IsCOW|SVppv_STATIC)) == (SVf_IsCOW) && SvLEN(sv) == 0)
+#define SvIsCOW_static(sv)       ((SvFLAGS(sv) & (SVf_IsCOW|SVppv_STATIC)) == (SVf_IsCOW|SVppv_STATIC))
 
 #define SvSHARED_HEK_FROM_PV(pvx) \
         ((struct hek*)(pvx - STRUCT_OFFSET(struct hek, hek_key)))

t/op/bool.t
@@ -1,0 +1,37 @@
+#!./perl
+
+BEGIN {
+    chdir 't' if -d 't';
+    require './test.pl';
+    set_up_inc('../lib');
+}
+
+use strict;
+use warnings;
+
+my $truevar  = (5 == 5);
+my $falsevar = (5 == 6);
+
+cmp_ok($truevar, '==', 1);
+cmp_ok($truevar, 'eq', "1");
+
+cmp_ok($falsevar, '==', 0);
+cmp_ok($falsevar, 'eq', "");
+
+{
+    # Check that boolean COW string buffer is safe to copy into new SVs and
+    # doesn't get corrupted by inplace mutations
+    my $x = $truevar;
+    $x =~ s/1/t/;
+
+    cmp_ok($x, 'eq', "t");
+    cmp_ok($truevar, 'eq', "1");
+
+    my $y = $truevar;
+    substr($y, 0, 1, "T");
+
+    cmp_ok($y, 'eq', "T");
+    cmp_ok($truevar, 'eq', "1");
+}
+
+done_testing();

commit 883a9955139677c4ded5a6cc73a67ef12c1d3cb0

Fix a leak when copying a STATIC COW SV to a stringified SV
This is minor a a regression introduced by commit 914bb57489325d34:
    Define a third kind of COW state; STATIC

    ...

    sv_setsv_flags() and sv_setsv_cow() will preserve this state

There was a small omission in the copy code - it didn't handle the case where
the destination SV owned a string buffer already. This is actually
relatively rare - triggering it requires a scalar in code path that is
assigned both strings and booleans. Minimal test case is:

    my $got = 1;
    $got .= "";
    $got = ref "";

cut down from &is in t/comp/parser.t

sv.c
@@ -4691,6 +4691,9 @@ Perl_sv_setsv_flags(pTHX_ SV *dsv, SV* ssv, const I32 flags)
          * SV_COW_SHARED_HASH_KEYS being set or else we'll break SvIsBOOL()
          */
         else if (SvIsCOW_static(ssv)) {
+            if (SvPVX_const(dsv)) {     /* we know that dtype >= SVt_PV */
+                SvPV_free(dsv);
+            }
             SvPV_set(dsv, SvPVX(ssv));
             SvLEN_set(dsv, 0);
             SvCUR_set(dsv, cur);