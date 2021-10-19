079dcbe2d50dd72eeec8bf4b9c9e75bea54f938c
Leon Timmermans <fawaka@gmail.com>
2021-10-19 23:04:03 +0200

    Update Encode to 3.16

--- Porting/Maintainers.pl
+++ Porting/Maintainers.pl
@@ -387,7 +387,7 @@ use File::Glob qw(:case);
     },
 
     'Encode' => {
-        'DISTRIBUTION' => 'DANKOGAI/Encode-3.15.tar.gz',
+        'DISTRIBUTION' => 'DANKOGAI/Encode-3.16.tar.gz',
         'FILES'        => q[cpan/Encode],
         'EXCLUDED'     => [
             qw( t/whatwg-aliases.json
--- cpan/Encode/Encode.xs
+++ cpan/Encode/Encode.xs
@@ -1,5 +1,5 @@
 /*
- $Id: Encode.xs,v 2.51 2021/10/08 15:29:23 dankogai Exp dankogai $
+ $Id: Encode.xs,v 2.51 2021/10/08 15:29:23 dankogai Exp $
  */
 
 #define PERL_NO_GET_CONTEXT
