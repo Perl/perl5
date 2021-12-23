f3decbf11aa3b1ab0a4fcfe2f1140dc208829b39
Branislav Zahradník <barney@cpan.org>
2021-12-23 23:19:43 +0100

    Cleanup remnants after distinguising C- and perly- literals

--- toke.c
+++ toke.c
@@ -2128,7 +2128,6 @@ static int
 S_postderef(pTHX_ int const funny, char const next)
 {
     assert(funny == DOLSHARP
-        || memCHRs("$@%&*", funny)
         || funny == PERLY_DOLLAR
         || funny == PERLY_SNAIL
         || funny == PERLY_PERCENT_SIGN
