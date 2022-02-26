6291c55388b16b59b0ed003570cc6600a5abecd8
Neil Bowers <neilb@neilb.org>
2022-02-26 17:40:04 +0000

    Bumped Module-CoreList ready for 5.35.8
    (manually cherry picked from commit 3e9c99ef8d3b7e74008beac15a315388ce5117f4)

--- dist/Module-CoreList/lib/Module/CoreList/Utils.pm
+++ dist/Module-CoreList/lib/Module/CoreList/Utils.pm
@@ -4,7 +4,7 @@ use strict;
 use warnings;
 use Module::CoreList;
 
-our $VERSION = '5.20211220';
+our $VERSION = '5.20220120';
 our %utilities;
 
 sub utilities {
@@ -1757,6 +1757,13 @@ my %delta = (
     },
     5.035007 => {
         delta_from => 5.035006,
+        changed => {
+        },
+        removed => {
+        }
+    },
+    5.035008 => {
+        delta_from => 5.035007,
         changed => {
         },
         removed => {
