7198711574449e8459f29865017678de700d0d1c
James E Keenan <jkeenan@cpan.org>
2022-03-07 23:00:49 +0000

    DB_File: sync with CPAN version 1.857
    Silence gcc12 'empty-body' warning
    https://github.com/pmqs/DB_File/issues/10
    22c4850b70d0c32eb4acb9c987868274a61072ec

--- Porting/Maintainers.pl
+++ Porting/Maintainers.pl
@@ -330,7 +330,7 @@ use File::Glob qw(:case);
     },
 
     'DB_File' => {
-        'DISTRIBUTION' => 'PMQS/DB_File-1.856.tar.gz',
+        'DISTRIBUTION' => 'PMQS/DB_File-1.857.tar.gz',
         'FILES'        => q[cpan/DB_File],
         'EXCLUDED'     => [
             qr{^patches/},
