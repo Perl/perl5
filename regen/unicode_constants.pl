6763e244a01413dca6205be4f5d40f15a83bfff3
Karl Williamson <khw@cpan.org>
2022-03-17 06:14:46 -0600

    Add PRECEDES/SUCCEEDS to paired string delimiters
    This commit adds 15 pairs of symbols that are variants on PRECEDES.
    These look a lot like <>, so makes sense to make them paired delimiters.

--- regen/unicode_constants.pl
+++ regen/unicode_constants.pl
@@ -92,9 +92,13 @@ sub backslash_x_form($$;$) {
 
 # These all visually look like left and right delimiters
 my @bidi_strong_lefts = (  'LESS-THAN',
+                           'PRECEDE',
+                           'PRECEDES',
                            'SMALLER THAN',
                         );
 my @bidi_strong_rights = ( 'GREATER-THAN',
+                           'SUCCEED',
+                           'SUCCEEDS',
                            'LARGER THAN',
                          );
 
