Perl/perl5

commit d1e771d8c533168553df9b2a858d967f707fc9fe

Fix utf8_to_uvchr for non-core calls
Commit a460925186154b270b7a647a4f30b2f01fd97c4b broke calling this and
related functions from outside of core Perl, but only when the input is
empty (which is an error anyway, so was not caught except for deliberate
API edge case tests in Devel::PPPort).

inline.h
@@ -2468,7 +2468,12 @@ Perl_utf8n_to_uvchr_msgs(const U8 *s,
      * cases. */
 
     /* No calls from core pass in an empty string; non-core need a check */
-    PERL_NON_CORE_CHECK_EMPTY(s, send);
+#ifdef PERL_CORE
+    assert(curlen > 0);
+#else
+    if (curlen == 0) return _utf8n_to_uvchr_msgs_helper(s0, 0, retlen,
+                                                        flags, errors, msgs);
+#endif
 
     type = PL_strict_utf8_dfa_tab[*s];
 

commit b9f4995789b6b70a1f9acba99dcbca4da9b50b3c

Fix double encoding of UTF-8 on EBCDIC
Commit d1e771d8c533168553df9b2a858d967f707fc9fe broke EBCDIC builds by
doubly encoding some UTF-8 characters.

inline.h
@@ -2493,6 +2493,9 @@ Perl_utf8n_to_uvchr_msgs(const U8 *s,
             uv = UTF8_ACCUMULATE(uv, *s);
 
             if (state == 0) {
+#ifdef EBCDIC
+                uv = UNI_TO_NATIVE(uv);
+#endif
                 goto success;
             }
 
@@ -2517,7 +2520,7 @@ Perl_utf8n_to_uvchr_msgs(const U8 *s,
         *msgs = NULL;
     }
 
-    return UNI_TO_NATIVE(uv);
+    return uv;
 }
 
 PERL_STATIC_INLINE UV