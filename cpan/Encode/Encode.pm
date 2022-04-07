05e1b3e5fcf057b3f46649607e6555e6afb8bb8a
Yves Orton <demerphq@gmail.com>
2022-04-07 09:02:05 +0200

    Update Encode to 3.17
    This silences the build warnings reported in https://github.com/Perl/perl5/issues/19588
    and in https://github.com/Perl/perl5/issues/17014.
    
    It includes some test updates, but no functionality changes.

--- cpan/Encode/Encode.pm
+++ cpan/Encode/Encode.pm
@@ -1,5 +1,5 @@
 #
-# $Id: Encode.pm,v 3.16 2021/10/13 08:29:04 dankogai Exp $
+# $Id: Encode.pm,v 3.17 2022/04/07 03:05:51 dankogai Exp $
 #
 package Encode;
 use strict;
@@ -7,7 +7,7 @@ use warnings;
 use constant DEBUG => !!$ENV{PERL_ENCODE_DEBUG};
 our $VERSION;
 BEGIN {
-    $VERSION = sprintf "%d.%02d", q$Revision: 3.16 $ =~ /(\d+)/g;
+    $VERSION = sprintf "%d.%02d", q$Revision: 3.17 $ =~ /(\d+)/g;
     require XSLoader;
     XSLoader::load( __PACKAGE__, $VERSION );
 }
