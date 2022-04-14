d6945b2c7963b5f1e22f90b7aab16077aa2edd27
Yves Orton <demerphq@gmail.com>
2022-04-14 03:15:27 +0200

    regen/mk_invlists.pl - move token_name() sub closer to where it is used
    sub token_name() was injected into the middle of totally unrelated logic
    that does not use it. token_name() is a wrapper around sanitize_name()
    so move it next to that sub.
    
    Also includes the output from running regen/mk_invlists.pl to keep
    porting/regen.t happy.

--- lib/unicore/uni_keywords.pl
+++ lib/unicore/uni_keywords.pl
@@ -1323,5 +1323,5 @@
 # c72bbdeda99714db1c8024d3311da4aef3c0db3b9b9f11455a7cfe10d5e9aba3 lib/unicore/version
 # 0a6b5ab33bb1026531f816efe81aea1a8ffcd34a27cbea37dd6a70a63d73c844 regen/charset_translations.pl
 # 5f8520d3a17ade6317fc0c423f5091470924b1ef425bca0c41ce8e4a9f8460fe regen/mk_PL_charclass.pl
-# be6e055280d1dbb82673bc7532148f67dbd4e843deb3e0b39ae426c36ea7d77c regen/mk_invlists.pl
+# 7dae12c0180337d75f0b3e851c056573621ca68a83e23f55b0b1754131b2b73f regen/mk_invlists.pl
 # ex: set ro:
