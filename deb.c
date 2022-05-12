7aa7cc795ce2343186811f374b9bfa051092ffc4
Karl Williamson <khw@cpan.org>
2022-05-12 13:34:58 -0600

    perlapi: Document Perl_deb and kin

--- deb.c
+++ deb.c
@@ -41,6 +41,24 @@ Perl_deb_nocontext(const char *pat, ...)
 }
 #endif
 
+/*
+=for apidoc      deb
+=for apidoc_item deb_nocontext
+
+When perl is compiled with C<-DDEBUGGING>, this prints to STDERR the
+information given by the arguments, prefaced by the name of the file containing
+the script causing the call, and the line number within that file.
+
+If the C<v> (verbose) debugging option is in effect, the process id is also
+printed.
+
+The two forms differ only in that C<deb_nocontext> does not take a thread
+context (C<aTHX>) parameter, so is used in situations where the caller doesn't
+already have the thread context.
+
+=cut
+*/
+
 void
 Perl_deb(pTHX_ const char *pat, ...)
 {
@@ -54,6 +72,14 @@ Perl_deb(pTHX_ const char *pat, ...)
 #endif /* DEBUGGING */
     va_end(args);
 }
+
+/*
+=for apidoc vdeb
+
+This is like C<L</deb>>, but C<args> are an encapsulated argument list.
+
+=cut
+*/
 
 void
 Perl_vdeb(pTHX_ const char *pat, va_list *args)
