Perl/perl5

commit 996e6b9e17bebd710ff658cdb123da073ac0ee97

locale.c: Use low level macros at low level
Implementing Perl_setlocale, we can safely use the internal macros that
the public ones expand to call, without the overhead those public macros
impose (which they do to be more immune from improper calls from outside
code).

locale.c
@@ -2235,7 +2235,7 @@ Perl_setlocale(const int category, const char * locale)
 #  ifdef USE_LOCALE_NUMERIC
 #    ifdef LC_ALL
 
-    DECLARATION_FOR_LC_NUMERIC_MANIPULATION;
+        bool toggled = FALSE;
 
 #    endif
 
@@ -2248,25 +2248,29 @@ Perl_setlocale(const int category, const char * locale)
             return PL_numeric_name;
         }
 
-#  ifdef LC_ALL
+#  endif
+#  if defined(USE_LOCALE_NUMERIC) && defined(LC_ALL)
 
         /* For an LC_ALL query, switch back to the underlying numeric locale
          * (if we aren't there already) so as to get the correct results.  Our
          * records for all the other categories are valid without switching */
-        if (category == LC_ALL) {
-            STORE_LC_NUMERIC_FORCE_TO_UNDERLYING();
+        if (category == LC_ALL && ! PL_numeric_underlying) {
+            set_numeric_underlying();
+            toggled = TRUE;
         }
 
 #  endif
-#  endif    /* End of has LC_NUMERIC */
 
-        retval = save_to_buffer(querylocale_r(category),
-                                &PL_setlocale_buf, &PL_setlocale_bufsize, 0);
+        retval = querylocale_r(category);
 
-#  if defined(USE_LOCALE_NUMERIC) && defined(LC_ALL)
+#  ifdef LC_ALL
 
-        if (category == LC_ALL) {
-            RESTORE_LC_NUMERIC();
+        if (toggled) {
+
+            /* This toggling back could destroy 'retval' */
+            retval = save_to_buffer(retval,
+                                    &PL_setlocale_buf, &PL_setlocale_bufsize, 0);
+            set_numeric_standard();
     }
 
 #  endif

commit 6a7aca81ca553c1c29a8288d196211f1b7712c50

Fix querying locale without LC_NUMERIC on platform
This fixes GH #19890.

This cleans up querying the current locale, broken by 996e6b9e17 on
platforms without LC_NUMERIC(*).

That commit was written before we required C99, and being able to move
declarations to not be at the beginning of a block made me realize that
things could be simplified by a bit of refactoring, which this commit
does.

(*): Lack of LC_NUMERIC can be simulated by using

  './Configure -Accflags="-DNO_LOCALE_NUMERIC'

locale.c
@@ -2257,12 +2257,12 @@ Perl_setlocale(const int category, const char * locale)
     /* A NULL locale means only query what the current one is. */
     if (locale == NULL) {
 
-#  ifdef USE_LOCALE_NUMERIC
-#    ifdef LC_ALL
+#  ifndef USE_LOCALE_NUMERIC
 
-        bool toggled = FALSE;
+        /* Without LC_NUMERIC, it's trivial; we just return the value */
+        return querylocale_r(category);
 
-#    endif
+#  else
 
         /* We have the LC_NUMERIC name saved, because we are normally switched
          * into the C locale (or equivalent) for it. */
@@ -2273,22 +2273,32 @@ Perl_setlocale(const int category, const char * locale)
             return PL_numeric_name;
         }
 
-#  endif
-#  if defined(USE_LOCALE_NUMERIC) && defined(LC_ALL)
+#    ifndef LC_ALL
+
+        /* Without LC_ALL, just return the value */
+        return querylocale_r(category);
+
+#    else
+
+        /* Here, LC_ALL is available on this platform.  It's the one
+         * complicating category (because it can contain a toggled LC_NUMERIC
+         * value), for all the remaining ones (we took care of LC_NUMERIC
+         * above), just return the value */
+        if (category != LC_ALL) {
+            return querylocale_r(category);
+        }
+
+        bool toggled = FALSE;
 
         /* For an LC_ALL query, switch back to the underlying numeric locale
          * (if we aren't there already) so as to get the correct results.  Our
          * records for all the other categories are valid without switching */
-        if (category == LC_ALL && ! PL_numeric_underlying) {
+        if (! PL_numeric_underlying) {
             set_numeric_underlying();
             toggled = TRUE;
         }
 
-#  endif
-
-        retval = querylocale_r(category);
-
-#  ifdef LC_ALL
+        retval = querylocale_c(LC_ALL);
 
         if (toggled) {
 
@@ -2296,11 +2306,13 @@ Perl_setlocale(const int category, const char * locale)
             retval = save_to_buffer(retval,
                                     &PL_setlocale_buf, &PL_setlocale_bufsize, 0);
             set_numeric_standard();
-    }
-
-#  endif
+        }
 
         return retval;
+
+#    endif      /* Has LC_ALL */
+#  endif        /* Has LC_NUMERIC */
+
     } /* End of querying the current locale */
 
     cat_index = get_category_index(category, NULL);