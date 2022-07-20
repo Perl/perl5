ec60236c4e0ed022469b57d17902aadc9bcbb159
Nicolas R <atoomic@cpan.org>
2022-07-20 17:11:14 +0000

    Sync DB_File with CPAN 1.858
    From ChangeLog:
    
    Update to license wording

--- Porting/Maintainers.pl
+++ Porting/Maintainers.pl
@@ -330,7 +330,7 @@ use File::Glob qw(:case);
     },
 
     'DB_File' => {
-        'DISTRIBUTION' => 'PMQS/DB_File-1.857.tar.gz',
+        'DISTRIBUTION' => 'PMQS/DB_File-1.858.tar.gz',
         'FILES'        => q[cpan/DB_File],
         'EXCLUDED'     => [
             qr{^patches/},
