5c3e926b684e4ed8635799fb859ea9fc1bb64c2d
Nicolas R <atoomic@cpan.org>
2022-07-20 19:47:30 +0000

    Sync Test::Simple with CPAN 1.302191
    From ChangeLog:
    
    - avoid failing when printing diagnostic info comparing
    partial overload objects

--- Porting/Maintainers.pl
+++ Porting/Maintainers.pl
@@ -1126,7 +1126,7 @@ use File::Glob qw(:case);
     },
 
     'Test::Simple' => {
-        'DISTRIBUTION' => 'EXODIST/Test-Simple-1.302190.tar.gz',
+        'DISTRIBUTION' => 'EXODIST/Test-Simple-1.302191.tar.gz',
         'FILES'        => q[cpan/Test-Simple],
         'EXCLUDED'     => [
             qr{^examples/},
