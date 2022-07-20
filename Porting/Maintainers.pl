04ce75ea5d4462d0a877e2912d76ce95fe31251f
Nicolas R <atoomic@cpan.org>
2022-07-20 19:22:52 +0000

    Sync Math::BigInt with CPAN 1.999837
    From ChangeLog:
    
    1.999837 2022-07-02
    
     * Improve the interoperability between objects of different classes for
     * the
       methods that are still used as functions.
    
    1.999836 2022-06-24
    
     * Improve compatibility with older versions of the Math-BigRat
     * distribution.
       Math-BigInt version 1.999835 works with Math-BigRat version 0.2623
    and newer.
       Math-BigInt version 1.999836 works with Math-BigRat version 0.2618
    and newer.
    
     * Re-enable upgrading in Math::BigFloat->bdiv().
    
     * Fix an error in the enabling/disabling of upgrading/downgrading.
    
     * Fix typos and other formatting errors.
    
    1.999835 2022-05-24
    
     * Fix bug related to upgrading in Math::BigInt->brsft(). This bug
       only showed up in Perl versions 5.16 and older.
    
    1.999834 2022-05-23
    
     * Further improvements to upgrading and downgrading.
    
    1.999833 2022-05-23
    
     * Fix Math::BigFloat->bpi(). The previous attempt at correcting it
       wasn't sufficient. Added test to verify the fix.
    
     * Correct the formatting in the CHANGES file.
    
    1.999832 2022-05-21
    
     * Improve as_int(), as_float(), and as_rat() so that they return a
       Math::BigInt, Math::BigFloat, and Math::BigRat, respectivly,
       regardless of upgrading and downgrading.
    
     * Improve stringification method bsstr() so that it handles upgrading
       and downgrading better.
    
     * Fix Math::BigFloat->bpi(), which unfortunately didn't handle
       downgrading.
    
     * Avoid unnecessary downgrading/upgrading.
    
     * Add missed cases where downgrading/upgrading should be performed.
    
     * Avoid unnecessary stringification.
    
    1.999831 2022-05-16
    
     * Further improvements to upgrading, downgrading, and rounding.
    
     * New stringification method bfstr() for representing numbers as
     * fractions.
       E.g., Math::BigFloat -> new("1.25") -> bfstr() returns "5/4".
    
     * Miscellaneous bug fixes.
    
     * Fixed errors and typos in the documentation.

--- Porting/Maintainers.pl
+++ Porting/Maintainers.pl
@@ -751,7 +751,7 @@ use File::Glob qw(:case);
     },
 
     'Math::BigInt' => {
-        'DISTRIBUTION' => 'PJACKLAM/Math-BigInt-1.999830.tar.gz',
+        'DISTRIBUTION' => 'PJACKLAM/Math-BigInt-1.999837.tar.gz',
         'FILES'        => q[cpan/Math-BigInt],
         'EXCLUDED'     => [
             qr{^xt/},
