Perl/perl5

commit 3f1788e11f2685299067ac0f8d3e4fd141a5b5cd

make_patchnum.pl now works in -Dmksymlink target dirs
this fixes script to work when run from a target/build dir
created by Configure -Dmksymlinks..  It works properly when
building in the srcdir too.

Several git commands fail for me, I added $opt_v=1 to see,
and left them; they add ~15 lines of output to a build.

(A bit amended by rgs: made $opt_v dependent on a -v flag on
the command-line)

make_patchnum.pl
@@ -13,7 +13,7 @@ make_patchnum.pl - make patchnum
 
   perl make_patchnum.pl
 
-=head1 DESCRITPTION
+=head1 DESCRIPTION
 
 This program creates the files holding the information
 about locally applied patches to the source code. The created
@@ -23,7 +23,7 @@ files are  C<git_version.h> and C<lib/Config_git.pl>.
 
 Contains status information from git in a form meant to be processed
 by the tied hash logic of Config.pm. It is actually optional,
-although -V will look strange without it.
+although -V:git.\* will be uninformative without it.
 
 C<git_version.h> contains similar information in a C header file
 format, designed to be used by patchlevel.h. This file is obtained
@@ -40,8 +40,25 @@ Same terms as Perl itself.
 
 =cut
 
+# from a -Dmksymlink target dir, I need to cd to the git-src tree to
+# use git (like script does).  Presuming that's not unique, one fix is
+# to follow Configure's symlink-path to run git.  Maybe GIT_DIR or
+# path-args can solve it, if so we should advise here, I tried only
+# very briefly ('cd -' works too).
+
+my ($subcd, $srcdir);
+our $opt_v = scalar grep $_ eq '-v', @ARGV;
+
 BEGIN {
     my $root=".";
+    # test 1st to see if we're a -Dmksymlinks target dir
+    $subcd = '';
+    $srcdir = $root;
+    if (-l "./Configure") {
+	$srcdir = readlink("./Configure");
+	$srcdir =~ s/Configure//;
+	$subcd = "cd $srcdir &&"; # activate backtick fragment
+    }
     while (!-e "$root/perl.c" and length($root)<100) {
         if ($root eq '.') {
             $root="..";
@@ -71,14 +88,20 @@ sub write_file {
 }
 
 sub backtick {
+    # only for git.  If we're in a -Dmksymlinks build-dir, we need to
+    # cd to src so git will work .  Probably a better way.
     my $command = shift;
     if (wantarray) {
-        my @result= `$command`;
+        my @result= `$subcd $command`;
+        warn "$subcd $command: \$?=$?\n" if $?;
+        print "#> $subcd $command ->\n @result\n" if !$? and $opt_v;
         chomp @result;
         return @result;
     } else {
-        my $result= `$command`;
+        my $result= `$subcd $command`;
         $result="" if ! defined $result;
+        warn "$subcd $command: \$?=$?\n" if $?;
+        print "#> $subcd $command ->\n $result\n" if !$? and $opt_v;
         chomp $result;
         return $result;
     }
@@ -102,14 +125,15 @@ sub write_files {
 my $unpushed_commits = '/*no-op*/';
 my ($read, $branch, $snapshot_created, $commit_id, $describe)= ("") x 5;
 my ($changed, $extra_info, $commit_title, $new_patchnum, $status)= ("") x 5;
+
 if (my $patch_file= read_file(".patch")) {
     ($branch, $snapshot_created, $commit_id, $describe) = split /\s+/, $patch_file;
     $extra_info = "git_snapshot_date='$snapshot_created'";
     $commit_title = "Snapshot of:";
 }
-elsif (-d path_to('.git')) {
+elsif (-d "$srcdir/.git") {
     # git branch | awk 'BEGIN{ORS=""} /\*/ { print $2 }'
-    ($branch) = map { /\* ([^(]\S*)/ ? $1 : () } backtick('git branch');
+    ($branch) = map { /\* ([^(]\S*)/ ? $1 : () } backtick("git branch");
     my ($remote,$merge);
     if (length $branch) {
         $merge= backtick("git config branch.$branch.merge");
@@ -139,12 +163,12 @@ git_remote_branch='$remote/$merge'
 git_unpushed='$unpushed_commit_list'";
         }
     }
-    if ($changed) {
+    if ($changed) { # not touched since init'd. never true.
         $changed = 'true';
         $commit_title =  "Derived from:";
         $status='"uncommitted-changes"'
     } else {
-        $status='/*clean-working-directory*/'
+        $status='/*clean-working-directory-maybe*/'
     }
     $commit_title ||= "Commit id:";
 }

commit f3c54e9d7f11ef2892507b0e14e308d762e6b67e

Fix make_patchnum.pl when run from subdir
When running 'make_patchnum.pl' from a subdir then 'make_patchnum.pl'
failed to write a proper lib/Config_git.pl. That is: all values in it
were set to the empty string instead to the actual values.

Example:
	$ cd perl5
	$ mkdir foo && cd $foo
	$ ../miniperl -I../lib ../make_patchnum.pl
	Updating 'git_version.h' and 'lib/Config_git.pl'
	$ cat ../lib/Config_git.pl
	######################################################################
	# WARNING: 'lib/Config_git.pl' is generated by make_patchnum.pl
	#          DO NOT EDIT DIRECTLY - edit make_patchnum.pl instead
	######################################################################
	$Config::Git_Data=<<'ENDOFGIT';
	git_commit_id=''
	git_describe=''
	git_branch=''
	git_uncommitted_changes=''
	git_commit_id_title=''

This got broken in commit 3f1788e11f2685299067ac0f8d3e4fd141a5b5cd which
added support for -Dmksymlink.

That commit basically assumed that the stating point was always '.' and
that the './.git' directory exist. Neither are true when the command is
run from inside a subdir.

The proper thing to do is to first find the `$root` directory and then
check if it's in -Dmksymlinks target dir. (Luckily all that code already
exists, we just need to move the -Dmksymlinks logic a bit down.)

With this change applied:
	$ ../miniperl -I../lib ../make_patchnum.pl
	Updating 'git_version.h' and 'lib/Config_git.pl'
	$ cat lib/Config_git.pl
	######################################################################
	# WARNING: 'lib/Config_git.pl' is generated by make_patchnum.pl
	#          DO NOT EDIT DIRECTLY - edit make_patchnum.pl instead
	######################################################################
	$Config::Git_Data=<<'ENDOFGIT';
	git_commit_id='c589bf60082d119a14e11ef05a77fed6d36aba4b'
	git_describe='v5.37.2-170-gc589bf6'
	git_branch='bram/fix-make_patchnum-in-subdir'
	git_uncommitted_changes='0'
	git_commit_id_title='Commit id:'

make_patchnum.pl
@@ -51,14 +51,6 @@ our $opt_v = scalar grep $_ eq '-v', @ARGV;
 
 BEGIN {
     my $root=".";
-    # test 1st to see if we're a -Dmksymlinks target dir
-    $subcd = '';
-    $srcdir = $root;
-    if (-l "./Configure") {
-	$srcdir = readlink("./Configure");
-	$srcdir =~ s/Configure//;
-	$subcd = "cd $srcdir &&"; # activate backtick fragment
-    }
     while (!-e "$root/perl.c" and length($root)<100) {
         if ($root eq '.') {
             $root="..";
@@ -68,6 +60,15 @@ BEGIN {
     }
     die "Can't find toplevel" if !-e "$root/perl.c";
     sub path_to { "$root/$_[0]" } # use $_[0] if this'd be placed in toplevel.
+
+    # test to see if we're a -Dmksymlinks target dir
+    $subcd = '';
+    $srcdir = $root;
+    if (-l "$root/Configure") {
+        $srcdir = readlink("$root/Configure");
+        $srcdir =~ s/Configure//;
+        $subcd = "cd $srcdir &&"; # activate backtick fragment
+    }
 }
 
 sub read_file {