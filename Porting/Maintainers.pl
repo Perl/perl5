d92704829266f1206f84fc44c0621798940c2ceb
Neil Bowers <neilb@neilb.org>
2022-08-20 11:38:43 +0100

    version of Module-CoreList wasn't matching what's on CPAN
    BinGOs told me that this updated was all that was required,
    and I chose to trust him.

--- Porting/Maintainers.pl
+++ Porting/Maintainers.pl
@@ -833,7 +833,7 @@ use File::Glob qw(:case);
     },
 
     'Module::CoreList' => {
-        'DISTRIBUTION' => 'BINGOS/Module-CoreList-5.20220620.tar.gz',
+        'DISTRIBUTION' => 'BINGOS/Module-CoreList-5.20220720.tar.gz',
         'FILES'        => q[dist/Module-CoreList],
     },
 
