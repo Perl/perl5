105caebf5b2f71f0be0f3b85dc36179e4478fba5
Karl Williamson <khw@cpan.org>
2022-09-16 05:22:39 -0600

    mktables: More closely examine @missing lines
    These lines have all had the same range (all of Unicode).  But in
    Unicode 15.0, there will be some with different ranges.  This commit
    changes to save those values (which are currently still unused)

--- uni_keywords.h
+++ uni_keywords.h
@@ -7677,7 +7677,7 @@ match_uniprop( const unsigned char * const key, const U16 key_len ) {
  * 43f6df50e4878f501b417e366b0ee097ae5ccb2d4ce942026bed3d62d78e7887 lib/unicore/extracted/DLineBreak.txt
  * a04502ebb36a45d83cbe48a7d8132ea8143edb7b3d34d0aa6afe4a9685049741 lib/unicore/extracted/DNumType.txt
  * 11075771b112e8e7ccf6ffa637c4c91eadc3ef3db0517b24e605df8fd3624239 lib/unicore/extracted/DNumValues.txt
- * 0537fbc40ddced27a827e4b7a36155bf8489a66af02d49fdf42f93e6a94d768b lib/unicore/mktables
+ * 38749965b8af1a6b3a2569ae1f28e423836dc0bacdbefc99e6d5f794083abd6c lib/unicore/mktables
  * c72bbdeda99714db1c8024d3311da4aef3c0db3b9b9f11455a7cfe10d5e9aba3 lib/unicore/version
  * 0a6b5ab33bb1026531f816efe81aea1a8ffcd34a27cbea37dd6a70a63d73c844 regen/charset_translations.pl
  * c7ff8e0d207d3538c7feb4a1a152b159e5e902d20293b303569ea8323e84633e regen/mk_PL_charclass.pl
