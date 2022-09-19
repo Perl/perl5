90a0bd379d13766de59ee75d4d99b260cb3bfca9
Karl Williamson <khw@cpan.org>
2022-09-19 13:16:05 -0600

    mktables: Standardize value aliases
    As stated in the code comments added by this commit, Unicode has various
    spellings for the same property value.  For example in some places it
    uses 'W', and in others 'Wide'.  The legal spellings are listed in
    PropValueAliases.txt, which is processed early in the construction.  So
    we can standardize things on input, which makes it easier later.  This
    commit produces minimal changes in the generated tables, so that the
    algorithm can be verified by inspection of the results.  And no other
    code that has hard-coded in expected spellings needs to be changed.
    
    Prior to this commit, we standardized the default value for properties
    that have a default value,.

--- regcharclass.h
+++ regcharclass.h
@@ -3850,7 +3850,7 @@
  * 43f6df50e4878f501b417e366b0ee097ae5ccb2d4ce942026bed3d62d78e7887 lib/unicore/extracted/DLineBreak.txt
  * a04502ebb36a45d83cbe48a7d8132ea8143edb7b3d34d0aa6afe4a9685049741 lib/unicore/extracted/DNumType.txt
  * 11075771b112e8e7ccf6ffa637c4c91eadc3ef3db0517b24e605df8fd3624239 lib/unicore/extracted/DNumValues.txt
- * de1c26f14cc6ee44e5035e18fa66b732698393d3129693a499b984125b9df777 lib/unicore/mktables
+ * 0537fbc40ddced27a827e4b7a36155bf8489a66af02d49fdf42f93e6a94d768b lib/unicore/mktables
  * c72bbdeda99714db1c8024d3311da4aef3c0db3b9b9f11455a7cfe10d5e9aba3 lib/unicore/version
  * 0a6b5ab33bb1026531f816efe81aea1a8ffcd34a27cbea37dd6a70a63d73c844 regen/charset_translations.pl
  * acc94e4afc339fe2cf2ae74d6e1cbcf2c396328d78e56236ad314eadbfc84125 regen/regcharclass.pl
