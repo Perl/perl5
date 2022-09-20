Perl/perl5

commit a7ff7aca3826600e5832ffe161fe075182f19a82

locale.c: Use setlocale() for init, not P2008
We have found bugs in the POSIX 2008 libc implementations on various
platforms.  This code, which does the initialization of locale handling
has always been very conservative, expecting possible failures due to
bugs in it or the libc implementations, and backing out if necessary to
a crippled, but workable state, if something goes wrong.

I think we should use the oldest, most stable locale implementation in
these circumstances

locale.c
@@ -3604,7 +3604,7 @@ Perl_init_i18nl10n(pTHX_ int printwarn)
 
                 /* Note that this may change the locale, but we are going to do
                  * that anyway just below */
-                system_default_locale = setlocale_c(LC_ALL, "");
+                system_default_locale = stdized_setlocale(LC_ALL, "");
                 DEBUG_LOCALE_INIT(LC_ALL_INDEX_, "", system_default_locale);
 
                 /* Skip if invalid or if it's already on the list of locales to
@@ -3629,7 +3629,7 @@ Perl_init_i18nl10n(pTHX_ int printwarn)
 
 #  ifdef LC_ALL
 
-        sl_result[LC_ALL_INDEX_] = setlocale_c(LC_ALL, trial_locale);
+        sl_result[LC_ALL_INDEX_] = stdized_setlocale(LC_ALL, trial_locale);
         DEBUG_LOCALE_INIT(LC_ALL_INDEX_, trial_locale, sl_result[LC_ALL_INDEX_]);
         if (! sl_result[LC_ALL_INDEX_]) {
             setlocale_failure = TRUE;
@@ -3651,7 +3651,7 @@ Perl_init_i18nl10n(pTHX_ int printwarn)
             unsigned int j;
             for (j = 0; j < NOMINAL_LC_ALL_INDEX; j++) {
                 Safefree(curlocales[j]);
-                curlocales[j] = setlocale_i(j, trial_locale);
+                curlocales[j] = stdized_setlocale(categories[j], trial_locale);
                 if (! curlocales[j]) {
                     setlocale_failure = TRUE;
                 }
@@ -3835,7 +3835,7 @@ Perl_init_i18nl10n(pTHX_ int printwarn)
 
             for (j = 0; j < NOMINAL_LC_ALL_INDEX; j++) {
                 Safefree(curlocales[j]);
-                curlocales[j] = savepv(querylocale_i(j));
+                curlocales[j] = savepv(stdized_setlocale(categories[j], NULL));
                 DEBUG_LOCALE_INIT(j, NULL, curlocales[j]);
             }
         }
@@ -3873,6 +3873,16 @@ Perl_init_i18nl10n(pTHX_ int printwarn)
             }
         }
     } /* End of tried to fallback */
+
+#  ifdef USE_POSIX_2008_LOCALE
+
+    /* The stdized setlocales haven't affected the P2008 locales.  Initialize
+     * them now, */
+    for (i = 0; i < NOMINAL_LC_ALL_INDEX; i++) {
+        void_setlocale_i(i, curlocales[i]);
+    }
+
+#  endif
 
     /* Done with finding the locales; update our records */
     new_LC_ALL(NULL);

commit c511845f6226a6da07b3bd00f7c64f81323c8209

locale.c: Workaround for attributes.pm breakage
See https://github.com/Perl/perl5/issues/20155

The root cause of that problem is that under POSIX 2008, when a thread
terminates, it causes thread 0 (the controller) to change to the global
locale.  Commit a7ff7ac caused perl to pay attention to the environment
variables in effect at startup for setting the global locale when using
the POSIX 2008 locale API.  (Previously only the initial per-thread
locale was affected.)

This causes problems when the initial setting was for a locale that uses
a comma as the radix character, but the thread 0 is set to a locale that
is expecting a dot as a radix character.  Whenever another thread
terminates, thread 0 was silently changed to using the global locake,
and hence a comma.  This caused parse errors.

The real solution is to fix thread 0 to remain in its chosen locale.
But that fix is not ready in time for 5.37.4, and it is deemed important
to get something working for this monthly development release.

This commit changes the initial global LC_NUMERIC locale to always be C,
hence uses a dot radix.  The vast majority of code is expecting a dot.
This is not the ultimate fix, but it works around the immediate problem
at hand.

The test case is courtesy @bram-perl

locale.c
@@ -5011,6 +5011,15 @@ Perl_init_i18nl10n(pTHX_ int printwarn)
     /* Done with finding the locales; update the auxiliary records */
     new_LC_ALL(NULL);
 
+#  if defined(USE_POSIX_2008_LOCALE) && defined(USE_LOCALE_NUMERIC)
+
+    /* This is a temporary workaround for #20155, to avoid issues where the
+     * global locale wants a radix different from the per-thread one.  This
+     * restores behavior for LC_NUMERIC to what it was before a7ff7ac. */
+    posix_setlocale(LC_NUMERIC, "C");
+
+#  endif
+
     for (i = 0; i < NOMINAL_LC_ALL_INDEX; i++) {
         Safefree(curlocales[i]);
     }

t/run/locale.t
@@ -449,6 +449,31 @@ EOF
 EOF
                 "1,5\n2,5", { stderr => 'devnull' }, "Can do math when radix is a comma"); # [perl 115800]
 
+            SKIP: {
+                skip "Perl not compiled with 'useithreads'", 1 if ! $Config{'useithreads'};
+
+                local $ENV{LC_ALL} = undef;
+                local $ENV{LC_NUMERIC} = $comma;
+                fresh_perl_is(<<"EOF",
+                    use threads;
+
+                    my \$x = eval "1.25";
+                    print "\$x", "\n";  # number is ok before thread
+                    my \$str_x = "\$x";
+
+                    my \$thr = threads->create(sub {});
+                    \$thr->join();
+
+                    print "\$x\n";  # number stringifies the same after thread
+
+                    my \$y = eval "1.25";
+                    print "\$y\n";  # number is ok after threads
+                    print "\$y" eq "\$str_x" || 0;    # new number stringifies the same as old number
+EOF
+                "1.25\n1.25\n1.25\n1", { }, "Thread join doesn't disrupt calling thread"
+                ); # [GH 20155]
+            }
+
           SKIP: {
             unless ($have_strtod) {
                 skip("no strtod()", 1);