cc652ecdc386747e67afb9c4d5b1c5bed1973bbb
Karen Etheridge <ether@cpan.org>
2022-09-21 09:29:21 -0700

    Module::CoreList is now version 5.20220920

--- Porting/Maintainers.pl
+++ Porting/Maintainers.pl
@@ -833,7 +833,7 @@ use File::Glob qw(:case);
     },
 
     'Module::CoreList' => {
-        'DISTRIBUTION' => 'BINGOS/Module-CoreList-5.20220820.tar.gz',
+        'DISTRIBUTION' => 'BINGOS/Module-CoreList-5.20220920.tar.gz',
         'FILES'        => q[dist/Module-CoreList],
     },
 
