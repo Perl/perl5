5ea008209af93e27d0877f8577328803365901e6
Karl Williamson <khw@cpan.org>
2022-09-29 13:23:48 -0600

    version

--- dist/threads/lib/threads.pm
+++ dist/threads/lib/threads.pm
@@ -5,7 +5,7 @@ use 5.008;
 use strict;
 use warnings;
 
-our $VERSION = '2.29';      # remember to update version in POD!
+our $VERSION = '2.30';      # remember to update version in POD!
 my $XS_VERSION = $VERSION;
 $VERSION = eval $VERSION;
 
