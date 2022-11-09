Perl/perl5

commit 35458d36d3c9fcf47064e4379e15276057b0bca5

Compile anonymous subs as anoncode without srefgen.
Heretofore the compiler represented an anonymous subroutine as an
anoncode op then an srefgen op; the latter’s only purpose was to
return a reference to the anoncode’s returned CV.

This changeset slightly optimizes that by making pp_anoncode return
a reference if so bidden. The same optimization is applied to
pp_anonconst as well.

Deparse.pm is updated accordingly.

op.c
@@ -7220,7 +7220,6 @@ Perl_pmruntime(pTHX_ OP *o, OP *expr, OP *repl, UV flags, I32 floor)
              *                         pushmark (for regcomp)
              *                         pushmark (for entersub)
              *                         anoncode
-             *                         srefgen
              *                         entersub
              *     regcreset                  regcreset
              *     pushmark                   pushmark
@@ -7234,10 +7233,9 @@ Perl_pmruntime(pTHX_ OP *o, OP *expr, OP *repl, UV flags, I32 floor)
             SvREFCNT_inc_simple_void(PL_compcv);
             CvLVALUE_on(PL_compcv);
             /* these lines are just an unrolled newANONATTRSUB */
-            expr = newSVOP(OP_ANONCODE, 0,
+            expr = newSVOP(OP_ANONCODE, OPf_REF,
                     MUTABLE_SV(newATTRSUB(floor, 0, NULL, NULL, expr)));
             cv_targ = expr->op_targ;
-            expr = newUNOP(OP_REFGEN, 0, expr);
 
             expr = list(force_list(newUNOP(OP_ENTERSUB, 0, scalar(expr)), TRUE));
         }
@@ -11365,15 +11363,21 @@ OP *
 Perl_newANONATTRSUB(pTHX_ I32 floor, OP *proto, OP *attrs, OP *block)
 {
     SV * const cv = MUTABLE_SV(newATTRSUB(floor, 0, proto, attrs, block));
+
+    bool is_const = CvANONCONST(cv);
+
     OP * anoncode =
-        newSVOP(OP_ANONCODE, 0,
+        newSVOP(OP_ANONCODE, is_const ? 0 : OPf_REF,
                 cv);
-    if (CvANONCONST(cv))
-        anoncode = newUNOP(OP_ANONCONST, 0,
+
+    if (is_const) {
+        anoncode = newUNOP(OP_ANONCONST, OPf_REF,
                            op_convert_list(OP_ENTERSUB,
                                            OPf_STACKED|OPf_WANT_SCALAR,
                                            anoncode));
-    return newUNOP(OP_REFGEN, 0, anoncode);
+    }
+
+    return anoncode;
 }
 
 OP *
@@ -13745,6 +13749,7 @@ Perl_ck_entersub_args_proto(pTHX_ OP *entersubop, GV *namegv, SV *protosv)
                 proto++;
                 arg++;
                 if (    o3->op_type != OP_UNDEF
+                    && o3->op_type != OP_ANONCODE
                     && (o3->op_type != OP_SREFGEN
                         || (  cUNOPx(cUNOPx(o3)->op_first)->op_first->op_type
                                 != OP_ANONCODE

lib/B/Deparse.pm
@@ -2791,13 +2791,19 @@ sub pp_refgen {
     my $kid = $op->first;
     if ($kid->name eq "null") {
 	my $anoncode = $kid = $kid->first;
+
+	# Perl no longer generates this, but XS modules might:
 	if ($anoncode->name eq "anonconst") {
 	    $anoncode = $anoncode->first->first->sibling;
 	}
+
+	# Same as with `anonconst`:
 	if ($anoncode->name eq "anoncode"
 	 or !null($anoncode = $kid->sibling) and
 		 $anoncode->name eq "anoncode") {
             return $self->e_anoncode({ code => $self->padval($anoncode->targ) });
+
+	# Perl still generates this:
 	} elsif ($kid->name eq "pushmark") {
             my $sib_name = $kid->sibling->name;
             if ($sib_name eq 'entersub') {
@@ -2817,6 +2823,18 @@ sub e_anoncode {
     my ($self, $info) = @_;
     my $text = $self->deparse_sub($info->{code});
     return $self->keyword("sub") . " $text";
+}
+
+sub pp_anoncode {
+    my ($self, $anoncode) = @_;
+
+    return $self->e_anoncode( { code => $self->padval($anoncode->targ) } );
+}
+
+sub pp_anonconst {
+    my ($self, $anonconst) = @_;
+
+    return $self->pp_anoncode( $anonconst->first->first->sibling );
 }
 
 sub pp_srefgen { pp_refgen(@_) }
@@ -6351,9 +6369,7 @@ sub matchop {
 					     ->sibling #   entersub
 					     ->first   #     ex-list
 					     ->first   #       pushmark
-					     ->sibling #       srefgen
-					     ->first   #         ex-list
-					     ->first   #           anoncode
+					     ->sibling #       anoncode
 					     ->targ
 				     )
 				   : undef);

pp.c
@@ -405,7 +405,15 @@ PP(pp_anoncode)
     if (CvCLONE(cv))
         cv = MUTABLE_CV(sv_2mortal(MUTABLE_SV(cv_clone(cv))));
     EXTEND(SP,1);
-    PUSHs(MUTABLE_SV(cv));
+
+    SV* sv = MUTABLE_SV(cv);
+
+    if (LIKELY(PL_op->op_flags & OPf_REF)) {
+        sv = refto(sv);
+    }
+
+    PUSHs(sv);
+
     RETURN;
 }
 
@@ -6996,10 +7004,25 @@ PP(pp_anonconst)
 {
     dSP;
     dTOPss;
-    SETs(sv_2mortal((SV *)newCONSTSUB(SvTYPE(CopSTASH(PL_curcop))==SVt_PVHV
-                                        ? CopSTASH(PL_curcop)
-                                        : NULL,
-                                      NULL, SvREFCNT_inc_simple_NN(sv))));
+
+    CV* constsub = newCONSTSUB(
+        SvTYPE(CopSTASH(PL_curcop))==SVt_PVHV ? CopSTASH(PL_curcop) : NULL,
+        NULL,
+        SvREFCNT_inc_simple_NN(sv)
+    );
+
+    SV* ret_sv = sv_2mortal((SV *)constsub);
+
+    /* Prior to Perl 5.38 anonconst ops always fed into srefgen.
+       5.38 redefined anonconst to create the reference without srefgen.
+       OPf_REF was added to the op. In case some XS code out there creates
+       anonconst the old way, we accommodate OPf_REF's absence here.
+    */
+    if (LIKELY(PL_op->op_flags & OPf_REF)) {
+        ret_sv = refto(ret_sv);
+    }
+
+    SETs(ret_sv);
     RETURN;
 }
 

commit da2303ba956cf3a1bf1457fb3a1505fac3b2dfc5

Fix & test useless-anonymous-subroutine warnings.
Issue #20384: This fixes a breakage from
35458d36d3c9fcf47064e4379e15276057b0bca5 that Devel::WatchVars’s test suite
caught. A test is added to Perl’s own test suite to prevent regressions.

op.c
@@ -2124,6 +2124,7 @@ Perl_scalarvoid(pTHX_ OP *arg)
         case OP_REF:
         case OP_REFGEN:
         case OP_SREFGEN:
+        case OP_ANONCODE:
         case OP_DEFINED:
         case OP_HEX:
         case OP_OCT:

t/op/anonsub.t
@@ -86,6 +86,17 @@ print sub { return "ok 1\n" } -> ();
 EXPECT
 ok 1
 ########
+my @void_warnings;
+{
+    use warnings;
+    local $SIG{'__WARN__'} = sub { push @void_warnings, @_ };
+    sub { 1 };
+    1
+}
+"@void_warnings"
+EXPECT
+Useless use of anonymous subroutine in void context at - line 5.
+########
 # [perl #71154] undef &$code makes $code->() die with: Not a CODE reference
 sub __ANON__ { print "42\n" }
 undef &{$x=sub{}};