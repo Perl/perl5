Perl/perl5

commit 20d507b5869b116c3b1f8aeea48fbfbf68dfce60

Fail attempts to pass anonsub to \$ prototype.
Issue #20384

op.c
@@ -3453,7 +3453,7 @@ Perl_op_lvalue_flags(pTHX_ OP *o, I32 type, U32 flags)
          * set flag here.
          *   See also https://github.com/Perl/perl5/issues/20384
          */
-        goto do_next;
+        goto nomod;
     }
 
     /* [20011101.069 (#7861)] File test operators interpret OPf_REF to mean that

t/comp/proto.t
@@ -18,7 +18,7 @@ BEGIN {
 # strict
 use strict;
 
-print "1..215\n";
+print "1..216\n";
 
 my $i = 1;
 
@@ -892,3 +892,24 @@ $_ = sub ($$$$$$$) {};
 @_ = (1, 2, 3, prototype(), 4, 5, 6);
 print "not " unless "@_" eq '1 2 3 $$$$$$$ 4 5 6';
 print "ok ", $i++, " - [perl #123514] (got @_)\n";
+
+{
+    my $weird_failure = q<>;
+
+    if (eval 'sub scalarref (\$) { }; scalarref( sub {} ); 1') {
+        print "not ok $i: Unexpected scalarref success!\n";
+    }
+    else {
+        if ($@ !~ m/anonymous subroutine/ || $@ !~ m/scalarref/) {
+            $weird_failure = $@;
+
+            $weird_failure =~ s/^/# /m;
+
+            print "# Unexpected error (or none):$/$weird_failure";
+        }
+
+        print( ($weird_failure ? 'not ok' : 'ok') . " $i - anonsub passed to \\\$\n" );
+    }
+
+    $i++;
+}

commit 39f63679bd7d85789074bc729b0a38a917ffd64b

Fix XS modules (e.g., Function::Parameters) that create anonsubs.
Issue #20384: Commit 20d507b5869b116c3b1f8aeea48fbfbf68dfce60 broke
CPAN’s Function::Parameters. This updates that logic to (hopefully)
differentiate OP_ANONCODE from an XS module versus from Perl.

op.c
@@ -3454,7 +3454,16 @@ Perl_op_lvalue_flags(pTHX_ OP *o, I32 type, U32 flags)
          * set flag here.
          *   See also https://github.com/Perl/perl5/issues/20384
          */
-        goto nomod;
+
+        // Perl always sets OPf_REF as of 5.37.5.
+        //
+        if (LIKELY(o->op_flags & OPf_REF)) goto nomod;
+
+        // If we got here, then our op came from an XS module that predates
+        // 5.37.5’s change to the op tree, which we have to handle a bit
+        // diffrently to preserve backward compatibility.
+        //
+        goto do_next;
     }
 
     /* [20011101.069 (#7861)] File test operators interpret OPf_REF to mean that