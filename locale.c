dead3ccedb0e57c37846b3b30aa1d618ef1309f3
Karl Williamson <khw@cpan.org>
2022-11-28 10:50:14 -0700

    locale.c: Silence compiler warning
    This is only emitted on Windows Visual Studio, before 2015.

--- locale.c
+++ locale.c
@@ -1970,6 +1970,8 @@ S_new_numeric(pTHX_ const char *newnum, bool force)
                                                              NULL, NULL));
     Safefree(scratch_buffer);
 
+#    else
+    PERL_UNUSED_VAR(C_thousands_sep);
 #    endif
 
     PL_numeric_standard = PL_numeric_underlying_is_standard;
