81f4fdd02b22307b1419b00cdf59e23a678bc5d5
Chris 'BinGOs' Williams <chris@bingosnet.co.uk>
2022-12-21 10:41:14 +0000

    Bump Module-CoreList version

--- dist/Module-CoreList/lib/Module/CoreList.pm
+++ dist/Module-CoreList/lib/Module/CoreList.pm
@@ -4,7 +4,7 @@ use strict;
 our ( %released, %version, %families, %upstream, %bug_tracker, %deprecated, %delta );
 
 use version;
-our $VERSION = '5.20221220';
+our $VERSION = '5.20230120';
 
 sub PKG_PATTERN () { q#\A[a-zA-Z_][0-9a-zA-Z_]*(?:(::|')[0-9a-zA-Z_]+)*\z# }
 sub _looks_like_invocant ($) { local $@; !!eval { $_[0]->isa(__PACKAGE__) } }
@@ -19905,6 +19905,8 @@ for my $version ( sort { $a <=> $b } keys %released ) {
         changed => {
             'B::Op_private'         => '5.037008',
             'Config'                => '5.037008',
+            'Module::CoreList'      => '5.20230120',
+            'Module::CoreList::Utils'=> '5.20230120',
         },
         removed => {
         }
--- dist/Module-CoreList/lib/Module/CoreList/Utils.pm
+++ dist/Module-CoreList/lib/Module/CoreList/Utils.pm
@@ -4,7 +4,7 @@ use strict;
 use warnings;
 use Module::CoreList;
 
-our $VERSION = '5.20221220';
+our $VERSION = '5.20230120';
 our %utilities;
 
 sub utilities {
