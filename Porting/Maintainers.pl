f195468b0f1bf89b6b1dac4987efe3836cf3380f
reneeb <info@perl-services.de>
2023-01-21 00:42:50 +0100

    Prepare Module::CoreList for 5.37.9

--- Porting/Maintainers.pl
+++ Porting/Maintainers.pl
@@ -827,7 +827,7 @@ use File::Glob qw(:case);
     },
 
     'Module::CoreList' => {
-        'DISTRIBUTION' => 'BINGOS/Module-CoreList-5.20221220.tar.gz',
+        'DISTRIBUTION' => 'BINGOS/Module-CoreList-5.20230120.tar.gz',
         'FILES'        => q[dist/Module-CoreList],
     },
 
