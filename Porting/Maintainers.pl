6efc7c17371cfac23b398493777d6aa116ddb120
Yves Orton <demerphq@gmail.com>
2023-03-20 20:55:18 +0100

    Porting/Maintainers.pl - update Module-CoreList entry to reflect latest on CPAN
    Apparently this should have been updated some time ago.

--- Porting/Maintainers.pl
+++ Porting/Maintainers.pl
@@ -812,7 +812,7 @@ use File::Glob qw(:case);
     },
 
     'Module::CoreList' => {
-        'DISTRIBUTION' => 'BINGOS/Module-CoreList-5.20230120.tar.gz',
+        'DISTRIBUTION' => 'BINGOS/Module-CoreList-5.20230220.tar.gz',
         'FILES'        => q[dist/Module-CoreList],
     },
 
