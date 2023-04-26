492c28719682673e6c19378edf6824d79784b33e
Karl Williamson <khw@cpan.org>
2023-04-26 15:36:54 -0600

    Implement sv_strftime_tm and sv_strftime_ints
    These two functions are designed to free the caller from having to know
    anything about the intricacies of handling UTF-8 in using strftime(), as
    they take SV inputs and return an SV with the UTF-8 flag appropriately
    set.
    
    They differ only in that one takes a bunch of integer arguments that
    define the various components of the time; and the other takes a pointer
    to a struct tm.
    
    The POSIX implementation of strftime is converted to use these.

--- proto.h
+++ proto.h
@@ -2664,12 +2664,6 @@ Perl_my_strftime(pTHX_ const char *fmt, int sec, int min, int hour, int mday, in
 #define PERL_ARGS_ASSERT_MY_STRFTIME            \
         assert(fmt)
 
-PERL_CALLCONV char *
-Perl_my_strftime8_temp(pTHX_ const char *fmt, int sec, int min, int hour, int mday, int mon, int year, int wday, int yday, int isdst, utf8ness_t *utf8ness)
-        __attribute__format__(__strftime__,pTHX_1,0);
-#define PERL_ARGS_ASSERT_MY_STRFTIME8_TEMP      \
-        assert(fmt)
-
 PERL_CALLCONV NV
 Perl_my_strtod(const char * const s, char **e)
         __attribute__warn_unused_result__;
@@ -4874,6 +4868,16 @@ Perl_sv_streq_flags(pTHX_ SV *sv1, SV *sv2, const U32 flags);
 #define PERL_ARGS_ASSERT_SV_STREQ_FLAGS
 
 PERL_CALLCONV SV *
+Perl_sv_strftime_ints(pTHX_ SV *fmt, int sec, int min, int hour, int mday, int mon, int year, int wday, int yday, int isdst);
+#define PERL_ARGS_ASSERT_SV_STRFTIME_INTS       \
+        assert(fmt)
+
+PERL_CALLCONV SV *
+Perl_sv_strftime_tm(pTHX_ SV *fmt, const struct tm *mytm);
+#define PERL_ARGS_ASSERT_SV_STRFTIME_TM         \
+        assert(fmt); assert(mytm)
+
+PERL_CALLCONV SV *
 Perl_sv_string_from_errnum(pTHX_ int errnum, SV *tgtsv);
 #define PERL_ARGS_ASSERT_SV_STRING_FROM_ERRNUM
 
@@ -6974,6 +6978,11 @@ STATIC bool
 S_is_locale_utf8(pTHX_ const char *locale);
 # define PERL_ARGS_ASSERT_IS_LOCALE_UTF8        \
         assert(locale)
+
+STATIC char *
+S_strftime8(pTHX_ const char *fmt, const struct tm *mytm, const utf8ness_t fmt_utf8ness, utf8ness_t *result_utf8ness, const bool came_from_sv);
+# define PERL_ARGS_ASSERT_STRFTIME8             \
+        assert(fmt); assert(mytm); assert(result_utf8ness)
 
 STATIC char *
 S_strftime_tm(pTHX_ const char *fmt, const struct tm *mytm)
