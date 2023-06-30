e86c803ec47843aa629e93c16d138a7ebd72e685
Karl Williamson <khw@cpan.org>
2023-06-30 15:04:03 -0600

    Add parameter to S_parse_LC_ALL_string
    This will be used in a future commit to panic when the parsing fails,
    instead of returning an error.  This will be used only when the
    expectation is strong that the string being parsed is legal, and makes
    exceptions to that be caught without having to add recovery code to all
    the calls of this function.

--- proto.h
+++ proto.h
@@ -7089,7 +7089,7 @@ S_use_curlocale_scratch(pTHX);
 
 #     if defined(LC_ALL)
 STATIC parse_LC_ALL_string_return
-S_parse_LC_ALL_string(pTHX_ const char *string, const char **output, const line_t caller_line);
+S_parse_LC_ALL_string(pTHX_ const char *string, const char **output, const bool panic_on_error, const line_t caller_line);
 #       define PERL_ARGS_ASSERT_PARSE_LC_ALL_STRING \
         assert(string); assert(output)
 
