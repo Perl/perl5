dcfcd777eabb848596d2672749d4f1d7b1a96a86
Steve Hay <steve.m.hay@googlemail.com>
2023-07-19 21:38:49 +0100

    Update Pod::Simple from 3.43 to 3.45

--- Porting/Maintainers.pl
+++ Porting/Maintainers.pl
@@ -958,12 +958,8 @@ use File::Glob qw(:case);
     },
 
     'Pod::Simple' => {
-        'DISTRIBUTION' => 'KHW/Pod-Simple-3.43.tar.gz',
+        'DISTRIBUTION' => 'KHW/Pod-Simple-3.45.tar.gz',
         'FILES'        => q[cpan/Pod-Simple],
-        'EXCLUDED' => [
-            qw{.ChangeLog.swp},
-            qr{^\.github/}
-	],
     },
 
     'Pod::Usage' => {
