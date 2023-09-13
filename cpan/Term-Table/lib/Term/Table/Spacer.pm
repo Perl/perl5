f754204f7b4e611fdbcb6317184b4ab0557e1b16
Paul "LeoNerd" Evans <leonerd@leonerd.org.uk>
2023-09-13 18:43:33 +0100

    Add `Term-Table` as a dual-life CPAN module
    Required as a dependency of adding Test2-Suite

---
+++ cpan/Term-Table/lib/Term/Table/Spacer.pm
@@ -0,0 +1,15 @@
+package Term::Table::Spacer;
+use strict;
+use warnings;
+
+our $VERSION = '0.017';
+
+sub new { bless {}, $_[0] }
+
+sub width { 1 }
+
+sub sanitize  { }
+sub mark_tail { }
+sub reset     { }
+
+1;
