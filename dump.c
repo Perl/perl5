386907f061c1812ecaa5f3c88d9f729828408097
Tony Cook <tony@develop-help.com>
2023-09-14 10:37:42 +1000

    Revert "[perl #89544] Non-eval closures don’t need CvOUTSIDE"
    But they do need CvOUTSIDE() for eval-in-package-DB's scope
    magic to work correctly.
    
    This also incidentally fixes a TODO Deparse, since the CvOUTSIDE
    is now available
    
    Fixes #19370 aka rt89544
    
    This breaks #11286, but I don't think that's fixable without breaking
    eval-in-DB's special scope behaviour.
    
    This reverts (most of) commit a0d2bbd5c47035a4f7369e4fddd46b502764d86e.

--- dump.c
+++ dump.c
@@ -1774,7 +1774,6 @@ const struct flag_to_name cv_flags_names[] = {
     {CVf_CVGV_RC, "CVGV_RC,"},
     {CVf_DYNFILE, "DYNFILE,"},
     {CVf_AUTOLOAD, "AUTOLOAD,"},
-    {CVf_HASEVAL, "HASEVAL,"},
     {CVf_SLABBED, "SLABBED,"},
     {CVf_NAMED, "NAMED,"},
     {CVf_LEXICAL, "LEXICAL,"},
