78eede5f50919327b4740506bb9b954ce9bd1948
Paul "LeoNerd" Evans <leonerd@leonerd.org.uk>
2023-09-18 21:52:58 +0100

    bump version.pm to 0.9930

--- Porting/Maintainers.pl
+++ Porting/Maintainers.pl
@@ -1296,7 +1296,8 @@ our %Modules = (
     },
 
     'version' => {
-        'DISTRIBUTION' => 'LEONT/version-0.9929.tar.gz',
+        'DISTRIBUTION' => 'LEONT/version-0.9930.tar.gz',
+        'SYNCINFO'     => 'LeoNerd on Mon Sep 18 21:49:09 2023',
         'FILES'        => q[cpan/version vutil.c vutil.h vxs.inc],
         'EXCLUDED' => [
             qr{^vutil/lib/},
