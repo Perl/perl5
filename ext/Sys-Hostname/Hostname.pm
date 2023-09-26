1523e0a25a68b253956bcb49e4b46008b26537b5
Elvin Aslanov <rwp.primary@gmail.com>
2023-09-26 17:08:25 +0200

    `Sys-Hostname`: Few minor improvements
    - Correct indirect syntax in test
    - Correct indentation on BEGIN
    - Enrich documentation
    - Spelling and grammar
    - Capitalize comment sentences
    
    Bump version

--- ext/Sys-Hostname/Hostname.pm
+++ ext/Sys-Hostname/Hostname.pm
@@ -1,49 +1,45 @@
 package Sys::Hostname;
 
 use strict;
+use warnings ();
 
 use Carp;
-
 use Exporter 'import';
 
 our @EXPORT  = qw/ hostname /;
-
 our $VERSION;
-
-use warnings ();
-
 our $host;
 
 BEGIN {
-    $VERSION = '1.25';
-    {
-	local $SIG{__DIE__};
-	eval {
+  $VERSION = '1.26';
+  {
+	  local $SIG{__DIE__};
+	  eval {
 	    require XSLoader;
 	    XSLoader::load();
-	};
-	warn $@ if $@;
-    }
+	  };
+	  warn $@ if $@;
+  }
 }
 
 
 sub hostname {
-  @_ and croak("hostname() does not accepts arguments (it used to silently discard any provided)");
+  @_ and croak("hostname() does not accept arguments (it used to silently discard any provided)");
 
-  # method 1 - we already know it
+  # Method 1 - We already know it
   return $host if defined $host;
 
-  # method 1' - try to ask the system
+  # Method 1' - Try to ask the system
   $host = ghname() if defined &ghname;
   return $host if defined $host;
 
   if ($^O eq 'VMS') {
 
-    # method 2 - no sockets ==> return DECnet node name
+    # Method 2 - No sockets ==> return DECnet node name
     eval { local $SIG{__DIE__}; $host = (gethostbyname('me'))[0] };
     if ($@) { return $host = $ENV{'SYS$NODE'}; }
 
-    # method 3 - has someone else done the job already?  It's common for the
+    # Method 3 - Has someone else done the job already?  It's common for the
     #    TCP/IP stack to advertise the hostname via a logical name.  (Are
     #    there any other logicals which TCP/IP stacks use for the host name?)
     $host = $ENV{'ARPANET_HOST_NAME'}  || $ENV{'INTERNET_HOST_NAME'} ||
@@ -51,14 +47,14 @@ sub hostname {
             $ENV{'TCPWARE_DOMAINNAME'} || $ENV{'NEWS_ADDRESS'};
     return $host if $host;
 
-    # method 4 - does hostname happen to work?
+    # Method 4 - Does hostname happen to work?
     my($rslt) = `hostname`;
     if ($rslt !~ /IVVERB/) { ($host) = $rslt =~ /^(\S+)/; }
     return $host if $host;
 
-    # rats!
+    # Rats!
     $host = '';
-    croak "Cannot get host name of local machine";  
+    croak "Cannot get the hostname of the local machine";
 
   }
   elsif ($^O eq 'MSWin32') {
@@ -67,12 +63,12 @@ sub hostname {
     return $host;
   }
   else {  # Unix
-    # is anyone going to make it here?
+    # Is anyone going to make it here?
 
     local $ENV{PATH} = '/usr/bin:/bin:/usr/sbin:/sbin'; # Paranoia.
 
-    # method 2 - syscall is preferred since it avoids tainting problems
-    # XXX: is it such a good idea to return hostname untainted?
+    # Method 2 - syscall is preferred since it avoids tainting problems
+    # XXX: Is it such a good idea to return hostname untainted?
     eval {
 	local $SIG{__DIE__};
 	require "syscall.ph";
@@ -80,24 +76,24 @@ sub hostname {
 	syscall(&SYS_gethostname, $host, 65) == 0;
     }
 
-    # method 2a - syscall using systeminfo instead of gethostname
+    # Method 2a - Syscall using systeminfo instead of gethostname
     #           -- needed on systems like Solaris
     || eval {
 	local $SIG{__DIE__};
 	require "sys/syscall.ph";
 	require "sys/systeminfo.ph";
-	$host = "\0" x 65; ## preload scalar
+	$host = "\0" x 65; ## Preload scalar
 	syscall(&SYS_systeminfo, &SI_HOSTNAME, $host, 65) != -1;
     }
 
-    # method 3 - trusty old hostname command
+    # Method 3 - Trusty old hostname command
     || eval {
 	local $SIG{__DIE__};
 	local $SIG{CHLD};
 	$host = `(hostname) 2>/dev/null`; # BSDish
     }
 
-    # method 4 - use POSIX::uname(), which strictly can't be expected to be
+    # Method 4 - Use POSIX::uname(), which strictly can't be expected to be
     # correct
     || eval {
 	local $SIG{__DIE__};
@@ -105,16 +101,16 @@ sub hostname {
 	$host = (POSIX::uname())[1];
     }
 
-    # method 5 - sysV uname command (may truncate)
+    # Method 5 - SysV uname command (may truncate)
     || eval {
 	local $SIG{__DIE__};
 	$host = `uname -n 2>/dev/null`; ## sysVish
     }
 
-    # bummer
-    || croak "Cannot get host name of local machine";  
+    # Bummer
+    || croak "Cannot get the hostname of the local machine";
 
-    # remove garbage 
+    # Remove garbage
     $host =~ tr/\0\r\n//d;
     $host;
   }
@@ -137,7 +133,7 @@ Sys::Hostname - Try every conceivable way to get hostname
 
 Attempts several methods of getting the system hostname and
 then caches the result.  It tries the first available of the C
-library's gethostname(), C<`$Config{aphostname}`>, uname(2),
+library's C<gethostname(3)>, C<`$Config{aphostname}`>, C<uname(2)>,
 C<syscall(SYS_gethostname)>, C<`hostname`>, C<`uname -n`>,
 and the file F</com/host>.  If all that fails it C<croak>s.
 
