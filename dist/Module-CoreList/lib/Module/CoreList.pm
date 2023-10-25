48dbbb20cefe1033000467ee21df79399e156978
Graham Knop <haarg@haarg.org>
2023-10-25 16:23:24 +0200

    Update Module::CoreList version for 5.39.4

--- dist/Module-CoreList/lib/Module/CoreList.pm
+++ dist/Module-CoreList/lib/Module/CoreList.pm
@@ -4,7 +4,7 @@ use strict;
 our ( %released, %version, %families, %upstream, %bug_tracker, %deprecated, %delta );
 
 use version;
-our $VERSION = '5.20231020';
+our $VERSION = '5.20231025';
 
 sub PKG_PATTERN () { q#\A[a-zA-Z_][0-9a-zA-Z_]*(?:(::|')[0-9a-zA-Z_]+)*\z# }
 sub _looks_like_invocant ($) { local $@; !!eval { $_[0]->isa(__PACKAGE__) } }
@@ -409,7 +409,7 @@ sub changes_between {
     5.039001 => '2023-07-20',
     5.039002 => '2023-08-20',
     5.039003 => '2023-09-20',
-    5.039004 => '2023-10-20',
+    5.039004 => '2023-10-25',
   );
 
 for my $version ( sort { $a <=> $b } keys %released ) {
--- dist/Module-CoreList/lib/Module/CoreList/Utils.pm
+++ dist/Module-CoreList/lib/Module/CoreList/Utils.pm
@@ -4,7 +4,7 @@ use strict;
 use warnings;
 use Module::CoreList;
 
-our $VERSION = '5.20231020';
+our $VERSION = '5.20231025';
 our %utilities;
 
 sub utilities {
