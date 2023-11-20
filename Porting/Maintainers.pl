3d3715d9cdd99726132d3117adb282af723f7486
Karen Etheridge <ether@cpan.org>
2023-11-19 20:00:57 -0800

    record Module::CoreList release in praesumptio

--- Porting/Maintainers.pl
+++ Porting/Maintainers.pl
@@ -823,7 +823,7 @@ our %Modules = (
     },
 
     'Module::CoreList' => {
-        'DISTRIBUTION' => 'BINGOS/Module-CoreList-5.20230920.tar.gz',
+        'DISTRIBUTION' => 'BINGOS/Module-CoreList-5.20231120.tar.gz',
         'FILES'        => q[dist/Module-CoreList],
     },
 
