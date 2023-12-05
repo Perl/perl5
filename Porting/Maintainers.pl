2c0a9fe3caa8aa91db512f11b3d4e599d5cf03a7
James E Keenan <jkeenan@cpan.org>
2023-12-05 07:32:50 -0500

    cpan/Archive-Tar - Update to version 3.02
    3.02  13/04/2023 (Manfred Stock)
    - Test block sizes up to (2**31 - 1) bytes only (fix for 32bit perls)
    - Don't match on message from exception in symlink test
    - Improve formatting of $Archive::Tar::RESOLVE_SYMLINK documentation

--- Porting/Maintainers.pl
+++ Porting/Maintainers.pl
@@ -129,7 +129,8 @@ our @IGNORABLE = qw(
 our %Modules = (
 
     'Archive::Tar' => {
-        'DISTRIBUTION' => 'BINGOS/Archive-Tar-2.40.tar.gz',
+        'DISTRIBUTION' => 'BINGOS/Archive-Tar-3.02.tar.gz',
+        'SYNCINFO'     => 'jkeenan on Tue Dec  5 07:32:24 2023',
         'FILES'        => q[cpan/Archive-Tar],
         'BUGS'         => 'bug-archive-tar@rt.cpan.org',
         'EXCLUDED'     => [
