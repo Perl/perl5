54f925e1d05ec5ccff2d0d1c62e34c5c1d6a0897
James E Keenan <jkeenan@cpan.org>
2023-12-05 07:41:44 -0500

    Patching Archive-Tar 3.02
    To indicate that we are including a patched version of Archive-Tar-3.02
    in core, we increment $VERSION in its *.pm files and in %Modules within
    Porting/Maintainers.pl.
    
    We're excluding t/90_symlink.t from shipping with core until such time
    as (a) we know whether its unit tests are for "developers" only or for
    general automated testing; and (b) we get a better 'skip_all' condition.
    
    Fixes GH #21402

--- cpan/Archive-Tar/lib/Archive/Tar.pm
+++ cpan/Archive-Tar/lib/Archive/Tar.pm
@@ -32,7 +32,7 @@ use vars qw[$DEBUG $error $VERSION $WARN $FOLLOW_SYMLINK $CHOWN $CHMOD
 $DEBUG                  = 0;
 $WARN                   = 1;
 $FOLLOW_SYMLINK         = 0;
-$VERSION                = "3.02";
+$VERSION                = "3.02_001";
 $CHOWN                  = 1;
 $CHMOD                  = 1;
 $SAME_PERMISSIONS       = $> == 0 ? 1 : 0;
