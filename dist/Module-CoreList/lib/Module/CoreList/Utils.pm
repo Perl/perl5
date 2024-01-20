e9d88da85cd41ced39b593002825eac096ef48b0
Max Maischein <corion@corion.net>
2024-01-20 11:12:04 +0100

    Update Module::CoreList for 5.39.7

--- dist/Module-CoreList/lib/Module/CoreList/Utils.pm
+++ dist/Module-CoreList/lib/Module/CoreList/Utils.pm
@@ -1986,6 +1986,13 @@ my %delta = (
         removed => {
         }
     },
+    5.039007 => {
+        delta_from => 5.039006,
+        changed => {
+        },
+        removed => {
+        }
+    },
 );
 
 %utilities = Module::CoreList::_undelta(\%delta);
