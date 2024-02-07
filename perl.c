819c46dc1a9aa3dc49364b96096e67170ef1c947
Karl Williamson <khw@cpan.org>
2024-02-07 04:15:15 -0700

    locale.c,perl.c: Clean up MULTIPLICITY vs USE_THREADS
    I believe I finally understand the difference between these, and this
    commit is the result.
    
    Under MULTIPLICITY, for locale.c, it:
    
    1) Updates comments to reflect reality
    2) displays the aTHX in DEBUGGING statements.
    3) Uses reentrant mbrtowc vs non-reentrant mbtowc
    
    Under multiplicity, for perl.c, it avoids a leak.  PL_cur_locale_obj is
    created whenever there is MULTIPLICITY, not just when threaded, so needs
    to be destroyed whenever MULTIPLICITY.

--- perl.c
+++ perl.c
@@ -1127,7 +1127,7 @@ perl_destruct(pTHXx)
         PL_curlocales[i] = NULL;
     }
 #endif
-#if defined(USE_POSIX_2008_LOCALE) && defined(USE_THREADS)
+#if defined(USE_POSIX_2008_LOCALE) && defined(MULTIPLICITY)
     {
         /* This also makes sure we aren't using a locale object that gets freed
          * below */
