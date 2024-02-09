Perl/perl5

commit 1b0ae1279c041daa07b860f7bcbcfce62123bfd2

special-case shift/pop/splice in "uninitialized value" warnings
Before the preceding commit, this warning was halfway correct:

    $ perl -we 'my @x; $_ = length(shift @x) == 0;'
    Use of uninitialized value within @x in numeric eq (==) at -e line 1.

The "uninitialized value" did come from @x (sort of), but it was never
an element of @x, and no element of @x was used as an operand of (==).

After the preceding commit, we get:

    Use of uninitialized value length(within @x) in numeric eq (==) at -e line 1.

... which is invalid code. With this commit, we get:

    Use of uninitialized value length(shift(@x)) in numeric eq (==) at -e line 1.

(The same logic applies to pop().)

Also, splice() was overly optimistic in assigning blame:

    $ perl -we 'my @x = 42; $_ = splice(@x, 0, 0) == 0'
    Use of uninitialized value within @x in numeric eq (==) at -e line 1.

@x doesn't contain any undefs. The undef encountered by (==) was
returned from splice itself; it didn't come from any of splice's
arguments, so mentioning @x was just wrong.

With this commit we get:

    Use of uninitialized value in numeric eq (==) at -e line 1.

sv.c
@@ -16796,6 +16796,9 @@ Perl_varname(pTHX_ const GV *const gv, const char gvtype, PADOFFSET targ,
         /* We know that name has no magic, so can use 0 instead of SV_GMAGIC */
         Perl_sv_insert_flags(aTHX_ name, 0, 0,  STR_WITH_LEN("within "), 0);
     }
+    else {
+        assert(subscript_type == FUV_SUBSCRIPT_NONE);
+    }
 
     return name;
 }
@@ -17466,6 +17469,7 @@ S_find_uninit_var(pTHX_ const OP *const obase, const SV *const uninit_sv,
     case OP_UNPACK:
     case OP_SYSOPEN:
     case OP_SYSSEEK:
+    case OP_SPLICE: /* scalar splice(@x, $i, 0) ==> undef */
         match = 1;
         goto do_op;
 
@@ -17495,6 +17499,38 @@ S_find_uninit_var(pTHX_ const OP *const obase, const SV *const uninit_sv,
             sv_catpvs_nomg(sv, ")");
         }
         return sv;
+
+    case OP_SHIFT:
+    case OP_POP:
+        if (match) {
+            break;
+        }
+        o = cUNOPx(obase)->op_first;
+        if (o->op_type == OP_RV2AV) {
+            o2 = cUNOPx(o)->op_first;
+            if (o2->op_type != OP_GV) {
+                break;
+            }
+            gv = cGVOPx_gv(o2);
+            if (!gv) {
+                break;
+            }
+        }
+        else if (o->op_type == OP_PADAV) {
+            gv = NULL;
+        }
+        else {
+            break;
+        }
+        sv = varname(gv, '@', o->op_targ, NULL, 0, FUV_SUBSCRIPT_NONE);
+        if (sv) {
+            const char *name = OP_NAME(obase);
+            Perl_sv_insert_flags(aTHX_ sv, 0, 0, STR_WITH_LEN("("), 0);
+            Perl_sv_insert_flags(aTHX_ sv, 0, 0, name, strlen(name), 0);
+            sv_catpvs_nomg(sv, ")");
+            return sv;
+        }
+        break;
 
     case OP_POS:
         /* def-ness of rval pos() is independent of the def-ness of its arg */

commit 231916f14531a92d19a728b292867735affa02ab

fix "uninitialized value" warnings for shift()/pop()
This fixes an oversight introduced in 1b0ae12.

The Perl code "shift" (or "shift()") produces different optrees
depending on where it occurs: At file scope, it shifts @ARGV and the
corresponding OP has an explicit @ARGV child. But at subroutine scope,
it shifts @_, and @_ is not represented explicitly in the optree.
Instead the parser just generates an OP_SHIFT without any children.

This latter case was not handled in S_find_uninit_var (and blindly
accessing a non-existent child OP would crash perl). pop() was similarly
affected.

Fix: Explicitly check whether OPf_KIDS is set on the OP before accessing
the op_first field (plus some tests for this case).

Fixes #21960.

sv.c
@@ -17506,32 +17506,36 @@ S_find_uninit_var(pTHX_ const OP *const obase, const SV *const uninit_sv,
         if (match) {
             break;
         }
-        o = cUNOPx(obase)->op_first;
-        if (o->op_type == OP_RV2AV) {
-            o2 = cUNOPx(o)->op_first;
-            if (o2->op_type != OP_GV) {
-                break;
-            }
-            gv = cGVOPx_gv(o2);
-            if (!gv) {
-                break;
-            }
-        }
-        else if (o->op_type == OP_PADAV) {
-            gv = NULL;
+        if (!(obase->op_flags & OPf_KIDS)) {
+            sv = newSVpvn_flags("", 0, SVs_TEMP);
         }
         else {
-            break;
+            o = cUNOPx(obase)->op_first;
+            if (o->op_type == OP_RV2AV) {
+                o2 = cUNOPx(o)->op_first;
+                if (o2->op_type != OP_GV) {
+                    break;
+                }
+                gv = cGVOPx_gv(o2);
+                if (!gv) {
+                    break;
+                }
+            }
+            else if (o->op_type == OP_PADAV) {
+                gv = NULL;
+            }
+            else {
+                break;
+            }
+            sv = varname(gv, '@', o->op_targ, NULL, 0, FUV_SUBSCRIPT_NONE);
         }
-        sv = varname(gv, '@', o->op_targ, NULL, 0, FUV_SUBSCRIPT_NONE);
         if (sv) {
             const char *name = OP_NAME(obase);
             Perl_sv_insert_flags(aTHX_ sv, 0, 0, STR_WITH_LEN("("), 0);
             Perl_sv_insert_flags(aTHX_ sv, 0, 0, name, strlen(name), 0);
             sv_catpvs_nomg(sv, ")");
-            return sv;
         }
-        break;
+        return sv;
 
     case OP_POS:
         /* def-ness of rval pos() is independent of the def-ness of its arg */

t/lib/warnings/9uninit
@@ -2277,6 +2277,12 @@ $_ = length shift @$ref == 0;
 $_ = length pop @$ref == 0;
 
 $_ = splice(@arr, 0, 0) == 0;
+
+# GH #21960
+$_ = lc shift;
+$_ = sub { lc shift }->();
+$_ = lc pop;
+$_ = sub { lc pop }->();
 EXPECT
 Use of uninitialized value in numeric eq (==) at - line 6.
 Use of uninitialized value length($x) in numeric eq (==) at - line 7.
@@ -2292,3 +2298,7 @@ Use of uninitialized value length(pop(@arr)) in numeric eq (==) at - line 19.
 Use of uninitialized value in numeric eq (==) at - line 20.
 Use of uninitialized value in numeric eq (==) at - line 21.
 Use of uninitialized value in numeric eq (==) at - line 23.
+Use of uninitialized value shift(@ARGV) in lc at - line 26.
+Use of uninitialized value shift() in lc at - line 27.
+Use of uninitialized value pop(@ARGV) in lc at - line 28.
+Use of uninitialized value pop() in lc at - line 29.