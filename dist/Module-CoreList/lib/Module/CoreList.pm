581250508eccb6b73918d74d5cff855a7128822d
reneeb <info@perl-services.de>
2024-02-24 17:25:06 +0100

    prepare Module::CoreList for 5.39.9
    (cherry picked from commit ad4a6d926044374113dfb037dbbd4d8e53fe1572)

--- dist/Module-CoreList/lib/Module/CoreList.pm
+++ dist/Module-CoreList/lib/Module/CoreList.pm
@@ -4,7 +4,7 @@ use strict;
 our ( %released, %version, %families, %upstream, %bug_tracker, %deprecated, %delta );
 
 use version;
-our $VERSION = '5.20240223';
+our $VERSION = '5.20240320';
 
 sub PKG_PATTERN () { q#\A[a-zA-Z_][0-9a-zA-Z_]*(?:(::|')[0-9a-zA-Z_]+)*\z# }
 sub _looks_like_invocant ($) { local $@; !!eval { $_[0]->isa(__PACKAGE__) } }
@@ -420,6 +420,7 @@ sub changes_between {
     5.039006 => '2023-12-30',
     5.039007 => '2024-01-20',
     5.039008 => '2024-02-23',
+    5.039009 => '2024-03-20',
   );
 
 for my $version ( sort { $a <=> $b } keys %released ) {
@@ -21310,6 +21311,17 @@ for my $version ( sort { $a <=> $b } keys %released ) {
         removed => {
         }
     },
+    5.039009 => {
+        delta_from => 5.039008,
+        changed => {
+            'B::Op_private'         => '5.039009',
+            'Config'                => '5.039009',
+            'Module::CoreList'      => '5.20240320',
+            'Module::CoreList::Utils'=> '5.20240320',
+        },
+        removed => {
+        }
+    },
 );
 
 sub is_core
@@ -22804,6 +22816,13 @@ sub is_core
     },
     5.039008 => {
         delta_from => 5.039007,
+        changed => {
+        },
+        removed => {
+        }
+    },
+    5.039009 => {
+        delta_from => 5.039008,
         changed => {
         },
         removed => {
