c166f18122a64c5b778a97f71d5955963fdda512
Paul "LeoNerd" Evans <leonerd@leonerd.org.uk>
2024-04-28 12:47:42 +0100

    Prepare Module::CoreList for 5.39.11
    (cherry picked from commit d330c0a0139a79c865d936c34c044c036bf8c6a9)

--- dist/Module-CoreList/lib/Module/CoreList/Utils.pm
+++ dist/Module-CoreList/lib/Module/CoreList/Utils.pm
@@ -4,7 +4,7 @@ use strict;
 use warnings;
 use Module::CoreList;
 
-our $VERSION = '5.20240420';
+our $VERSION = '5.20240520';
 our %utilities;
 
 sub utilities {
@@ -2009,6 +2009,13 @@ my %delta = (
     },
     5.039010 => {
         delta_from => 5.039009,
+        changed => {
+        },
+        removed => {
+        }
+    },
+    5.039011 => {
+        delta_from => 5.039010,
         changed => {
         },
         removed => {
