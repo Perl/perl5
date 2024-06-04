8d22df394914cf78de50879cb5dfe8c1a2a43fa9
Graham Knop <haarg@haarg.org>
2024-06-04 12:03:30 +0200

    update Module::CoreList for perl 5.40.0-RC2

--- dist/Module-CoreList/lib/Module/CoreList.pm
+++ dist/Module-CoreList/lib/Module/CoreList.pm
@@ -4,7 +4,7 @@ use strict;
 our ( %released, %version, %families, %upstream, %bug_tracker, %deprecated, %delta );
 
 use version;
-our $VERSION = '5.20240524';
+our $VERSION = '5.20240604';
 
 sub PKG_PATTERN () { q#\A[a-zA-Z_][0-9a-zA-Z_]*(?:(::|')[0-9a-zA-Z_]+)*\z# }
 sub _looks_like_invocant ($) { local $@; !!eval { $_[0]->isa(__PACKAGE__) } }
@@ -422,7 +422,7 @@ sub changes_between {
     5.039008 => '2024-02-23',
     5.039009 => '2024-03-20',
     5.039010 => '2024-04-27',
-    5.040000 => '2024-05-24',
+    5.040000 => '2024-06-04',
   );
 
 for my $version ( sort { $a <=> $b } keys %released ) {
@@ -21616,8 +21616,8 @@ for my $version ( sort { $a <=> $b } keys %released ) {
             'B::Op_private'         => '5.040000',
             'Config'                => '5.04',
             'Fcntl'                 => '1.18',
-            'Module::CoreList'      => '5.20240524',
-            'Module::CoreList::Utils'=> '5.20240524',
+            'Module::CoreList'      => '5.20240604',
+            'Module::CoreList::Utils'=> '5.20240604',
             'POSIX'                 => '2.20',
             'Pod::Man'              => '5.01_02',
             'Pod::ParseLink'        => '5.01_02',
