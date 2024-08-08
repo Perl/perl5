9b8254ff8ce79a0638fd3a6e92925df817fae943
David Mitchell <davem@iabyn.com>
2024-08-08 11:50:29 +0100

    ExtUtils::ParseXS: partially eliminate cond field
    This object field was being used for two purposes: once to maintain
    state about CASE conditions, and once as a short-lived temporary value.
    Use a lexical var instead for the latter use. The new scope will be
    re-indented in the next commit.

--- dist/ExtUtils-ParseXS/lib/ExtUtils/ParseXS.pm
+++ dist/ExtUtils-ParseXS/lib/ExtUtils/ParseXS.pm
@@ -300,7 +300,6 @@ BEGIN {
                         # aren't being used for this XSUB.
   
   'cond',               # Most recent CASE string.
-                        # Also used temporarily elsewhere.
 
   'condnum',            # number of CASE keywords encountered.
                         # Zero indicates none encountered yet.
@@ -1170,16 +1169,20 @@ EOF
 #    dXSFUNCTION($self->{ret_type});
 EOF
 
-    $self->{cond} = set_cond($seen_ellipsis, $min_arg_count, $args_count);
+
+    {
+    # the code to emit to determine whether the correct number of argument
+    # have been passed
+    my $condition_code = set_cond($seen_ellipsis, $min_arg_count, $args_count);
 
     print Q(<<"EOF") if $self->{except}; # "-except" cmd line switch
 #    char errbuf[1024];
 #    *errbuf = '\\0';
 EOF
 
-    if($self->{cond}) {
+    if ($condition_code) {
       print Q(<<"EOF");
-#    if ($self->{cond})
+#    if ($condition_code)
 #       croak_xs_usage(cv,  "$report_args");
 EOF
     }
@@ -1189,6 +1192,7 @@ EOF
 #    PERL_UNUSED_VAR(cv); /* -W */
 #    PERL_UNUSED_VAR(items); /* -W */
 EOF
+    }
     }
 
     # gcc -Wall: if an XSUB has PPCODE, it is possible that none of ST,
