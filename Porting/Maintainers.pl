2b088f7d200786a56df29c170858b2aac11f6e2b
Thibault DUPONCHELLE <thibault.duponchelle@gmail.com>
2024-09-20 14:44:59 +0200

    CoreList is released to CPAN

--- Porting/Maintainers.pl
+++ Porting/Maintainers.pl
@@ -827,7 +827,7 @@ our %Modules = (
     },
 
     'Module::CoreList' => {
-        'DISTRIBUTION' => 'BINGOS/Module-CoreList-5.20240829.tar.gz',
+        'DISTRIBUTION' => 'BINGOS/Module-CoreList-5.20240920.tar.gz',
         'FILES'        => q[dist/Module-CoreList],
     },
 
