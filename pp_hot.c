4ce65f7680de02020b5c64062452fc6a2aad30f2
TAKAI Kousuke <62541129+t-a-k@users.noreply.github.com>
2024-12-13 23:55:05 +0900

    pp_add, pp_subtract: Unify TARGn(<result>, 1) calls into a single call
    Before this change, pp_add() and pp_subtract() had two (or three
    before the previous commit) calls of TARGn(<result>, 1) for each.
    Consolidating these calls into single call will (slightly) reduce
    the size of the compiled code.

--- pp_hot.c
+++ pp_hot.c
@@ -1811,6 +1811,7 @@ PP(pp_add)
     SV *targ = (PL_op->op_flags & OPf_STACKED)
                     ? PL_stack_sp[-1]
                     : PAD_SV(PL_op->op_targ);
+    NV nv;
 
     if (rpp_try_AMAGIC_2(add_amg, AMGf_assign|AMGf_numeric))
         return NORMAL;
@@ -1994,7 +1995,8 @@ PP(pp_add)
                         TARGi(NEGATE_2IV(result), 1);
                     else {
                         /* result valid, but out of range for IV.  */
-                        TARGn(-(NV)result, 1);
+                        nv = -(NV)result;
+                        goto ret_nv;
                     }
                 }
                 goto ret;
@@ -2006,12 +2008,13 @@ PP(pp_add)
     useleft = USE_LEFT(svl);
 #endif
 
-    {
-        /* If left operand is undef, treat as zero. */
-        NV value = useleft ? SvNV_nomg(svl) : 0.0;
-        value += SvNV_nomg(svr);
-        TARGn(value, 1);
-    }
+    /* If left operand is undef, treat as zero. */
+    nv = useleft ? SvNV_nomg(svl) : 0.0;
+    /* Separate statements here to ensure SvNV_nomg(svl) is evaluated
+       before SvNV_nomg(svr) */
+    nv += SvNV_nomg(svr);
+  ret_nv:
+    TARGn(nv, 1);
 
   ret:
     rpp_replace_2_1_NN(targ);
