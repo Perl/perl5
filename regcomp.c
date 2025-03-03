54a72ba5b73a159b2975b71e20795383a1cc4e48
Karl Williamson <khw@cpan.org>
2025-03-02 21:27:57 -0700

    regcomp.c: Use utf8_to-uv_or_die not utf8_to_uvchr_buf

--- regcomp.c
+++ regcomp.c
@@ -8178,7 +8178,9 @@ S_handle_possible_posix(pTHX_ RExC_state_t *pRExC_state,
                 p++;
             }
             else {
-                input_text[name_len++] = utf8_to_uvchr_buf((U8 *) p, e, NULL);
+                input_text[name_len++] = utf8_to_uv_or_die((const U8 *) p,
+                                                           (const U8 *) e,
+                                                           NULL);
                 p+= UTF8SKIP(p);
             }
 
