42d38273704232bb50d431ccf19776e9ca9d8797
Lukas Mai <lukasmai.403@gmail.com>
2025-03-21 14:55:13 +0100

    Bump the perl version in various places for 5.41.11

--- lib/B/Op_private.pm
+++ lib/B/Op_private.pm
@@ -118,7 +118,7 @@ package B::Op_private;
 our %bits;
 
 
-our $VERSION = "5.041010";
+our $VERSION = "5.041011";
 
 $bits{$_}{3} = 'OPpENTERSUB_AMPER' for qw(entersub rv2cv);
 $bits{$_}{6} = 'OPpENTERSUB_DB' for qw(entersub rv2cv);
