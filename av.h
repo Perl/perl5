45b391510009a94b610ebe6bf8dc8a1dcac60f0e
Karl Williamson <khw@cpan.org>
2025-04-19 17:00:26 -0600

    Revert "Move Perl_newAV() from mathoms to av.h"
    This reverts commit 6a3ea4b452fdfc6bab52ec7179d25af3c115f8ee.

--- av.h
+++ av.h
@@ -236,8 +236,7 @@ to fit one element without extending:
 
 */
 
-#define Perl_newAV(mTHX)  MUTABLE_AV(Perl_newSV_type(aTHX_ SVt_PVAV))
-
+#define newAV()	MUTABLE_AV(newSV_type(SVt_PVAV))
 #define newAV_mortal()	MUTABLE_AV(newSV_type_mortal(SVt_PVAV))
 #define newAV_alloc_x(size)  av_new_alloc(size,0)
 #define newAV_alloc_xz(size) av_new_alloc(size,1)
