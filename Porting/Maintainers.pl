549e6dc41380430374aa9c0c22148fa8249dba32
Karen Etheridge <ether@cpan.org>
2025-04-21 16:28:09 -0700

    update current release status for Module::CoreList

--- Porting/Maintainers.pl
+++ Porting/Maintainers.pl
@@ -832,7 +832,7 @@ our %Modules = (
     },
 
     'Module::CoreList' => {
-        'DISTRIBUTION' => 'BINGOS/Module-CoreList-5.20250420.tar.gz',
+        'DISTRIBUTION' => 'BINGOS/Module-CoreList-5.20250421.tar.gz',
         'FILES'        => q[dist/Module-CoreList],
     },
 
