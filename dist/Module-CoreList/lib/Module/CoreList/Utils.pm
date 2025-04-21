318c37bc22f2f29db7e97f49a948a8b63275f98a
Karen Etheridge <ether@cpan.org>
2025-04-20 20:23:54 -0700

    Update Module::CoreList for 5.41.12
    (cherry picked from commit 416f8181993ad7cea524cdd26db9025e8299ac88)

--- dist/Module-CoreList/lib/Module/CoreList/Utils.pm
+++ dist/Module-CoreList/lib/Module/CoreList/Utils.pm
@@ -4,7 +4,7 @@ use strict;
 use warnings;
 use Module::CoreList;
 
-our $VERSION = '5.20250420';
+our $VERSION = '5.20250422';
 our %utilities;
 
 sub utilities {
@@ -2128,6 +2128,13 @@ my %delta = (
     },
     5.041011 => {
         delta_from => 5.041010,
+        changed => {
+        },
+        removed => {
+        }
+    },
+    5.041012 => {
+        delta_from => 5.041011,
         changed => {
         },
         removed => {
