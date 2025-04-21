853e80a502702ed9f5456be195136c6e059c324c
Karen Etheridge <ether@cpan.org>
2025-04-21 08:42:25 -0700

    oops, extra paren

--- hints/darwin.sh
+++ hints/darwin.sh
@@ -373,7 +373,7 @@ ccflags="$ccflags -DHAS_BROKEN_LANGINFO_CODESET"
 
 # Get: "Assertion failed: (p->val == key), function lookup_substsearch, file
 # collate.c, line 596."
-if [ "$darwin_major" -ge 24 && "$perl_revision" -ge 5 -a ( "$perl_version" -ge 42 -o ( "$perl_version" -eq 41 -a "$perl_subversion" -ge 11 ) ) ]; then
+if [ "$darwin_major" -ge 24 && "$perl_revision" -ge 5 -a ( "$perl_version" -ge 42 -o ( "$perl_version" -eq 41 -a "$perl_subversion" -ge 11 ) ]; then
     ccflags="$ccflags -DNO_LOCALE_COLLATE"
 fi
 
