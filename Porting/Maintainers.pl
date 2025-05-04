a69510545966dc2cb3a77a38729409a50a9ae85e
James E Keenan <jkeenan@cpan.org>
2025-05-04 07:41:17 -0400

    ExtUtils::ParseXS: Update $VERSION in Maintainers.pl
    Blead version was updated in March.

--- Porting/Maintainers.pl
+++ Porting/Maintainers.pl
@@ -528,7 +528,7 @@ our %Modules = (
     },
 
     'ExtUtils::ParseXS' => {
-        'DISTRIBUTION' => 'LEONT/ExtUtils-ParseXS-3.51.tar.gz',
+        'DISTRIBUTION' => 'LEONT/ExtUtils-ParseXS-3.57.tar.gz',
         'FILES'        => q[dist/ExtUtils-ParseXS],
     },
 
