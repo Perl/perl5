8006e9296fb62c77c3d36bb4ae55f7773ce12fb8
Igor Todorovski <itodorov@ca.ibm.com>
2025-05-14 17:17:17 -0600

    Add os390.c
    z/OS has extra startup needs like a couple other platforms, e.g. cygwin.
    This adds a file that handles them.
    
    Principally, z/OS files have extra status fields regarding "tags".
    Files on this system may be encoded in different character sets, and
    these fields describe them.  Hence the OS has within it the knowledge of
    a file's encoding.  A file is "tagged" as being in a particular encoding
    (or encodings plural; I have no idea how that works).  This file adds a
    function to deal with them.

---
+++ os390/os390.c
@@ -0,0 +1,57 @@
+#include <string.h>
+#include <sys/stat.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/ps.h>
+#include <unistd.h>
+#include <stdarg.h>
+#include <varargs.h>
+#include <limits.h>
+#include <_Nascii.h>
+#include <fcntl.h>
+#include <libgen.h>
+#include <termios.h>
+#include <zos.h>
+#include "EXTERN.h"
+#include "perl.h"
+#include "XSUB.h"
+
+void
+zos_copytags_fd(pTHX_ CV *cv)
+{
+  dXSARGS;
+  int ret = 0;
+
+  if (items != 2)
+    Perl_croak(aTHX_ "Usage: ZOS::Filespec::copytags_fd(f1, f2])");
+
+  int from_fd = (int)SvIV(ST(0));
+  int to_fd = (int)SvIV(ST(1));
+
+  char path[_XOPEN_PATH_MAX] = {0};
+  int rc = w_ioctl(from_fd, _IOCC_GPN, _XOPEN_PATH_MAX, path);
+  if (rc == 0) {
+    __e2a_l(path, _XOPEN_PATH_MAX);
+  }
+
+  struct stat src_statsbuf;
+  if (stat(path, &src_statsbuf)) {
+    ret = -1;
+  }
+  if (ret != -1) {
+    ret = __setfdccsid(to_fd,  (src_statsbuf.st_tag.ft_txtflag << 16) | src_statsbuf.st_tag.ft_ccsid);
+  }
+
+  XSRETURN_IV(ret);
+}
+
+void
+init_os_extras(void)
+{
+  dTHX;
+  char* file = __FILE__;
+
+  newXSproto("ZOS::Filespec::copytags_fd",zos_copytags_fd,file,"$;$");
+
+  return;
+}
