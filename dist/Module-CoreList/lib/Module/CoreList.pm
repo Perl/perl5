dda57322812c142bde30a62c32203ebcd3dc8ec7
Philippe Bruhat (BooK) <book@cpan.org>
2025-07-03 22:44:41 +0200

    prepare Module::CoreList for perl 5.43.0
    (cherry picked from commit cc2aa78a3126358fba14810f2fd99fdec167840e)

--- dist/Module-CoreList/lib/Module/CoreList.pm
+++ dist/Module-CoreList/lib/Module/CoreList.pm
@@ -4,7 +4,7 @@ use strict;
 our ( %released, %version, %families, %upstream, %bug_tracker, %deprecated, %delta );
 
 use version;
-our $VERSION = '5.20250720';
+our $VERSION = '5.20250703';
 
 sub PKG_PATTERN () { q#\A[a-zA-Z_][0-9a-zA-Z_]*(?:(::|')[0-9a-zA-Z_]+)*\z# }
 sub _looks_like_invocant ($) { local $@; !!eval { $_[0]->isa(__PACKAGE__) } }
@@ -23453,6 +23453,9 @@ for my $version ( sort { $a <=> $b } keys %released ) {
         delta_from => 5.042000,
         changed => {
             'B::Op_private'         => '5.043000',
+            'Config'                => '5.043000',
+            'Module::CoreList'      => '5.20250703',
+            'Module::CoreList::Utils'=> '5.20250703',
             'feature'               => '1.98',
         },
         removed => {
--- dist/Module-CoreList/lib/Module/CoreList/Utils.pm
+++ dist/Module-CoreList/lib/Module/CoreList/Utils.pm
@@ -4,7 +4,7 @@ use strict;
 use warnings;
 use Module::CoreList;
 
-our $VERSION = '5.20250720';
+our $VERSION = '5.20250703';
 our %utilities;
 
 sub utilities {
