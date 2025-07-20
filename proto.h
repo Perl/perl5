0368807147af9f9d73dff18a39d5f288ee81335f
Karl Williamson <khw@cpan.org>
2025-07-20 11:05:48 -0600

    Convert Perl_uvoffuni_to_utf8_flags to a macro
    The function is hereby removed in favor of calling the plain
    uvoffuni_to_utf8_flags macro that already exists

--- proto.h
+++ proto.h
@@ -5483,10 +5483,8 @@ Perl_uvchr_to_utf8_flags(pTHX_ U8 *d, UV uv, UV flags); */
 /* PERL_CALLCONV U8 *
 Perl_uvchr_to_utf8_flags_msgs(pTHX_ U8 *d, UV uv, UV flags, HV **msgs); */
 
-PERL_CALLCONV U8 *
-Perl_uvoffuni_to_utf8_flags(pTHX_ U8 *d, UV uv, UV flags);
-#define PERL_ARGS_ASSERT_UVOFFUNI_TO_UTF8_FLAGS \
-        assert(d)
+/* PERL_CALLCONV U8 *
+Perl_uvoffuni_to_utf8_flags(pTHX_ U8 *d, UV uv, UV flags); */
 
 PERL_CALLCONV U8 *
 Perl_uvoffuni_to_utf8_flags_msgs(pTHX_ U8 *d, UV input_uv, const UV flags, HV **msgs);
