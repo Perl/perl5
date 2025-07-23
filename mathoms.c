40ce3d5f6ecefb541ac468ab5da37798907335ef
Karl Williamson <khw@cpan.org>
2025-07-23 08:10:29 -0600

    Note when functions were first deprecated
    We should eventually remove deprecated functions, after a suitable
    interval.  This commit notes when all the current deprecated functions
    were made so.  This saves time in the future in deciding if it is ok to
    actually remove them.

--- mathoms.c
+++ mathoms.c
@@ -791,6 +791,8 @@ next possible position in C<s> that could begin a non-malformed character.
 See L<perlapi/utf8n_to_uvchr> for details on when the REPLACEMENT CHARACTER is returned.
 
 =cut
+
+Deprecated since 5.38 
 */
 
 UV
@@ -860,7 +862,7 @@ convert to Unicode using L<perlapi/C<NATIVE_TO_UNI>>.
 UV
 Perl_utf8n_to_uvuni(pTHX_ const U8 *s, STRLEN curlen, STRLEN *retlen, U32 flags)
 {
-    PERL_ARGS_ASSERT_UTF8N_TO_UVUNI;
+    PERL_ARGS_ASSERT_UTF8N_TO_UVUNI; /* Deprecated since 5.38 */
 
     return NATIVE_TO_UNI(utf8n_to_uvchr(s, curlen, retlen, flags));
 }
@@ -868,7 +870,7 @@ Perl_utf8n_to_uvuni(pTHX_ const U8 *s, STRLEN curlen, STRLEN *retlen, U32 flags)
 UV
 Perl_utf8_to_uvchr(pTHX_ const U8 *s, STRLEN *retlen)
 {
-    PERL_ARGS_ASSERT_UTF8_TO_UVCHR;
+    PERL_ARGS_ASSERT_UTF8_TO_UVCHR; /* Deprecated since 5.38 */
 
     /* This function is unsafe if malformed UTF-8 input is given it, which is
      * why the function is deprecated.  If the first byte of the input
