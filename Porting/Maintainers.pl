eb4d72ff5f41eb2ac150c6914bd6c3a4ca3cf95d
Leon Timmermans <fawaka@gmail.com>
2025-08-02 17:07:01 +0200

    Update Maintainers.pl for CPAN release

--- Porting/Maintainers.pl
+++ Porting/Maintainers.pl
@@ -528,7 +528,7 @@ our %Modules = (
     },
 
     'ExtUtils::ParseXS' => {
-        'DISTRIBUTION' => 'LEONT/ExtUtils-ParseXS-3.57.tar.gz',
+        'DISTRIBUTION' => 'LEONT/ExtUtils-ParseXS-3.58.tar.gz',
         'FILES'        => q[dist/ExtUtils-ParseXS],
     },
 
