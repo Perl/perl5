89cb0b38b0733610b45af7b632fdec5061ec161d
Chris 'BinGOs' Williams <chris@bingosnet.co.uk>
2025-08-03 15:14:26 +0100

    Acta est fabula plaudite

--- Porting/Maintainers.pl
+++ Porting/Maintainers.pl
@@ -833,7 +833,7 @@ our %Modules = (
     },
 
     'Module::CoreList' => {
-        'DISTRIBUTION' => 'BINGOS/Module-CoreList-5.20250702.tar.gz',
+        'DISTRIBUTION' => 'BINGOS/Module-CoreList-5.20250803.tar.gz',
         'FILES'        => q[dist/Module-CoreList],
     },
 
