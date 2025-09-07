e90f1863833c355717dc64d7bc9a9433e3a2d977
David Mitchell <davem@iabyn.com>
2025-09-07 12:23:15 +0100

    ParseXS: standard_typemap_locations(): use push
    Currently this function builds a list of paths in reverse order, but
    *unshifts* each result one by one so that they end up in the right
    order.
    
    This commit switches the order of generation and *pushes* the result
    each time.
    
    The net result is the same, but its less cognitive load working out
    what's going on.

--- dist/ExtUtils-ParseXS/lib/ExtUtils/ParseXS/Utilities.pm
+++ dist/ExtUtils-ParseXS/lib/ExtUtils/ParseXS/Utilities.pm
@@ -110,23 +110,27 @@ A list of F<typemap> pathnames.
 sub standard_typemap_locations {
   my $include_ref = shift;
 
-  my @tm = qw(typemap);
+  my @tm;
+
+  # See function description above for why 'reverse' is used here.
+  foreach my $dir (reverse @{$include_ref}) {
+    my $file = File::Spec->catfile($dir, ExtUtils => 'typemap');
+    push @tm, $file;
+  }
 
   my $updir = File::Spec->updir();
   foreach my $dir (
-      File::Spec->catdir(($updir) x 1),
-      File::Spec->catdir(($updir) x 2),
-      File::Spec->catdir(($updir) x 3),
       File::Spec->catdir(($updir) x 4),
+      File::Spec->catdir(($updir) x 3),
+      File::Spec->catdir(($updir) x 2),
+      File::Spec->catdir(($updir) x 1),
   ) {
-    unshift @tm, File::Spec->catfile($dir, 'typemap');
-    unshift @tm, File::Spec->catfile($dir, lib => ExtUtils => 'typemap');
+    push @tm, File::Spec->catfile($dir, lib => ExtUtils => 'typemap');
+    push @tm, File::Spec->catfile($dir, 'typemap');
   }
 
-  foreach my $dir (@{ $include_ref}) {
-    my $file = File::Spec->catfile($dir, ExtUtils => 'typemap');
-    unshift @tm, $file;
-  }
+  push @tm, 'typemap';
+
   return @tm;
 }
 
