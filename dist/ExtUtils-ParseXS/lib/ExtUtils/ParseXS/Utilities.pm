518c6492e5a8518a361de484f857c015bf4818d9
David Mitchell <davem@iabyn.com>
2025-09-07 12:18:25 +0100

    ParseXS: standard_typemap_locations(): reindent
    Reindent after previous removed some scopes. White-space only.

--- dist/ExtUtils-ParseXS/lib/ExtUtils/ParseXS/Utilities.pm
+++ dist/ExtUtils-ParseXS/lib/ExtUtils/ParseXS/Utilities.pm
@@ -107,28 +107,28 @@ A list of F<typemap> pathnames.
 
 =cut
 
-  sub standard_typemap_locations {
-    my $include_ref = shift;
+sub standard_typemap_locations {
+  my $include_ref = shift;
 
-      my @tm = qw(typemap);
+  my @tm = qw(typemap);
 
-      my $updir = File::Spec->updir();
-      foreach my $dir (
-          File::Spec->catdir(($updir) x 1),
-          File::Spec->catdir(($updir) x 2),
-          File::Spec->catdir(($updir) x 3),
-          File::Spec->catdir(($updir) x 4),
-      ) {
-        unshift @tm, File::Spec->catfile($dir, 'typemap');
-        unshift @tm, File::Spec->catfile($dir, lib => ExtUtils => 'typemap');
-      }
-
-    foreach my $dir (@{ $include_ref}) {
-      my $file = File::Spec->catfile($dir, ExtUtils => 'typemap');
-      unshift @tm, $file;
-    }
-    return @tm;
+  my $updir = File::Spec->updir();
+  foreach my $dir (
+      File::Spec->catdir(($updir) x 1),
+      File::Spec->catdir(($updir) x 2),
+      File::Spec->catdir(($updir) x 3),
+      File::Spec->catdir(($updir) x 4),
+  ) {
+    unshift @tm, File::Spec->catfile($dir, 'typemap');
+    unshift @tm, File::Spec->catfile($dir, lib => ExtUtils => 'typemap');
   }
+
+  foreach my $dir (@{ $include_ref}) {
+    my $file = File::Spec->catfile($dir, ExtUtils => 'typemap');
+    unshift @tm, $file;
+  }
+  return @tm;
+}
 
 =head2 C<trim_whitespace()>
 
