08c5ab586107a0c9c261089caa2a1f805966f2d0
Karl Williamson <khw@cpan.org>
2025-09-08 09:43:01 -0600

    regcomp.c: White space only
    The previous commit removed a surrounding block; outdent correspondingly

--- regcomp.c
+++ regcomp.c
@@ -2527,11 +2527,11 @@ S_reg_scan_name(pTHX_ RExC_state_t *pRExC_state, U32 flags)
     else if ((advance = isIDFIRST_lazy_if_safe(RExC_parse, RExC_end, UTF))) {
          /* Note that the code here assumes well-formed UTF-8.  Skip IDFIRST by
           * using do...while */
-            do {
-                RExC_parse_advance(advance);
-            } while (   RExC_parse < RExC_end
-                     && (advance = isWORDCHAR_utf8_safe( (U8 *) RExC_parse,
-                                                         (U8 *) RExC_end)));
+        do {
+            RExC_parse_advance(advance);
+        } while (   RExC_parse < RExC_end
+                 && (advance = isWORDCHAR_utf8_safe( (U8 *) RExC_parse,
+                                                     (U8 *) RExC_end)));
     } else {
         RExC_parse_inc_by(1); /* so the <- from the vFAIL is after the offending
                          character */
