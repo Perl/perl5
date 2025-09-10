52c0e4a6a216056b3ba830df072a5e518cd83c0c
Karl Williamson <khw@cpan.org>
2025-09-10 04:52:21 -0600

    mktables: Clean up hieroglyphs database
    Some of the properties in this Unikemet d.b. are provisional, so Unicode
    doesn't furnish information about them.  So we have to do it ourselves.
    I didn't get it right previously.
    
    This also changes one property to an enum, which it should have been all
    along, and adds the current possible enum values.

--- regcharclass.h
+++ regcharclass.h
@@ -3851,7 +3851,7 @@
  * 4be1c18e7b121d951018065b453bb05083f624c8f905ce8be7c0f354c9097c95 lib/unicore/extracted/DLineBreak.txt
  * 786833e0a3f5ec0c0cd0940e4c15f730f3a92163f354ecd7dede28a70c0fa892 lib/unicore/extracted/DNumType.txt
  * 00b43cc5c9b86a834f82389c4537f103e652821387daa556f0bd220f6c23007e lib/unicore/extracted/DNumValues.txt
- * b96c9bfd2f20e7016a9036f9a2d7978b3453217d8379bdcbf86253fb27cbec7a lib/unicore/mktables
+ * 60d6468a78df93eaef78d39930a582a05871f050f7398d8bbdd33c1779a74649 lib/unicore/mktables
  * 63f771c327e92574fbd77919586079c38f669058a5e6b67ccec385ef8fcde882 lib/unicore/version
  * 0a6b5ab33bb1026531f816efe81aea1a8ffcd34a27cbea37dd6a70a63d73c844 regen/charset_translations.pl
  * 2a984ef37eb37e718ed25d472988745196816f9147d6ee6822a8efeafda340e5 regen/regcharclass.pl
