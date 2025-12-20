185d33206685b945c5cd16b7e87e7f7b2acb621c
Steve Hay <steve.m.hay@googlemail.com>
2025-12-20 18:43:14 +0000

    Prepare Module::CoreList for 5.43.7

--- dist/Module-CoreList/lib/Module/CoreList.pm
+++ dist/Module-CoreList/lib/Module/CoreList.pm
@@ -4,7 +4,7 @@ use strict;
 our ( %released, %version, %families, %upstream, %bug_tracker, %deprecated, %delta );
 
 use version;
-our $VERSION = '5.20251220';
+our $VERSION = '5.20260120';
 
 sub PKG_PATTERN () { q#\A[a-zA-Z_][0-9a-zA-Z_]*(?:(::|')[0-9a-zA-Z_]+)*\z# }
 sub _looks_like_invocant ($) { local $@; !!eval { $_[0]->isa(__PACKAGE__) } }
@@ -451,6 +451,7 @@ sub changes_between {
     5.043004 => '2025-10-22',
     5.043005 => '2025-11-20',
     5.043006 => '2025-12-20',
+    5.043007 => '2026-01-20',
   );
 
 for my $version ( sort { $a <=> $b } keys %released ) {
@@ -24380,6 +24381,17 @@ for my $version ( sort { $a <=> $b } keys %released ) {
         removed => {
         }
     },
+    5.043007 => {
+        delta_from => 5.043006,
+        changed => {
+            'B::Op_private'         => '5.043007',
+            'Config'                => '5.043007',
+            'Module::CoreList'      => '5.20260120',
+            'Module::CoreList::Utils'=> '5.20260120',
+        },
+        removed => {
+        }
+    },
 );
 
 sub is_core
@@ -26084,6 +26096,13 @@ sub is_core
     },
     5.043006 => {
         delta_from => 5.043005,
+        changed => {
+        },
+        removed => {
+        }
+    },
+    5.043007 => {
+        delta_from => 5.043006,
         changed => {
         },
         removed => {
