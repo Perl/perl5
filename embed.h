86cc3ae1e01c8d01bc3adcf499fdf9c24511c3e6
Karl Williamson <khw@cpan.org>
2026-01-04 13:22:59 -0700

    regen/embed.pl: Update copyright to 2026

--- embed.h
+++ embed.h
@@ -4,8 +4,8 @@
  *
  *    Copyright (C) 1993, 1994, 1995, 1996, 1997, 1998, 1999, 2000, 2001,
  *    2002, 2003, 2004, 2005, 2006, 2007, 2008, 2009, 2010, 2011, 2012, 2013,
- *    2014, 2015, 2016, 2017, 2018, 2019, 2020, 2021, 2022
- *    by Larry Wall and others
+ *    2014, 2015, 2016, 2017, 2018, 2019, 2020, 2021, 2022, 2023, 2024, 2025,
+ *    2026 by Larry Wall and others
  *
  *    You may distribute under the terms of either the GNU General Public
  *    License or the Artistic License, as specified in the README file.
