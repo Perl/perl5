99ce99b7ba73437a52c3d0935c458594a7d71799
Max Maischein <corion@corion.net>
2026-01-19 17:20:29 +0100

    Set Module::CoreList version to 20260119
    ... as we release one day before the 'usual' date

--- dist/Module-CoreList/lib/Module/CoreList.pm
+++ dist/Module-CoreList/lib/Module/CoreList.pm
@@ -4,7 +4,7 @@ use strict;
 our ( %released, %version, %families, %upstream, %bug_tracker, %deprecated, %delta );
 
 use version;
-our $VERSION = '5.20260120';
+our $VERSION = '5.20260119';
 
 sub PKG_PATTERN () { q#\A[a-zA-Z_][0-9a-zA-Z_]*(?:(::|')[0-9a-zA-Z_]+)*\z# }
 sub _looks_like_invocant ($) { local $@; !!eval { $_[0]->isa(__PACKAGE__) } }
@@ -24386,8 +24386,8 @@ for my $version ( sort { $a <=> $b } keys %released ) {
         changed => {
             'B::Op_private'         => '5.043007',
             'Config'                => '5.043007',
-            'Module::CoreList'      => '5.20260120',
-            'Module::CoreList::Utils'=> '5.20260120',
+            'Module::CoreList'      => '5.20260119',
+            'Module::CoreList::Utils'=> '5.20260119',
         },
         removed => {
         }
