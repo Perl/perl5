87206f4d8d10858c35063d70f5a6a27aba6f5865
Max Maischein <corion@corion.net>
2026-01-19 20:02:13 +0100

    Prepare Module::Corelist
    (cherry picked from commit 77fd20a5613302d30b603b4dc5823166ad45bfb1)

--- dist/Module-CoreList/lib/Module/CoreList/Utils.pm
+++ dist/Module-CoreList/lib/Module/CoreList/Utils.pm
@@ -4,7 +4,7 @@ use strict;
 use warnings;
 use Module::CoreList;
 
-our $VERSION = '5.20260119';
+our $VERSION = '5.20260220';
 our %utilities;
 
 sub utilities {
@@ -2219,6 +2219,13 @@ my %delta = (
     },
     5.043007 => {
         delta_from => 5.043006,
+        changed => {
+        },
+        removed => {
+        }
+    },
+    5.043008 => {
+        delta_from => 5.043007,
         changed => {
         },
         removed => {
