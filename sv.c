665189d7051d1a7aba937d97b910a436eed1d085
Karl Williamson <khw@cpan.org>
2026-02-11 09:20:19 -0700

    clone_using: Use correct interpreter for check
    Code shouldn't be trying to clone another interpreter while having
    mutexes locked.  The lines changed in this commit were checking the
    newly minted interpreter for this condition rather than the onde doing
    the cloning.
    
    Spotted by Matthew Horsfall

--- sv.c
+++ sv.c
@@ -17056,7 +17056,7 @@ perl_clone_using(PerlInterpreter *proto_perl, UV flags,
 #endif /* USE_LOCALE_COLLATE */
 
 #ifdef USE_LOCALE_THREADS
-    assert(PL_locale_mutex_depth <= 0);
+    assert(proto_perl->Ilocale_mutex_depth <= 0);
     PL_locale_mutex_depth = 0;
 #endif
 
@@ -17095,7 +17095,7 @@ perl_clone_using(PerlInterpreter *proto_perl, UV flags,
     PL_less_dicey_locale_bufsize = 0;
 #endif
 #ifdef USE_THREADS
-    assert(PL_env_mutex_depth <= 0);
+    assert(proto_perl->Ienv_mutex_depth <= 0);
     PL_env_mutex_depth = 0;
 #endif
 
