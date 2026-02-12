9cefb88fc5c290f28b29e8f7bfae50b3bd42f529
Paul "LeoNerd" Evans <leonerd@leonerd.org.uk>
2026-02-12 08:37:57 +0000

    Add a new currently-empty attributes.{ch} file pair

---
+++ attributes.c
@@ -0,0 +1,12 @@
+/*    attributes.c
+ *
+ *    Copyright (C) 2025 by Paul Evans and others
+ *
+ *    You may distribute under the terms of either the GNU General Public
+ *    License or the Artistic License, as specified in the README file.
+ *
+ */
+
+/*
+ * ex: set ts=8 sts=4 sw=4 et:
+ */
