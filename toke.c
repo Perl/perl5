df4ac0361a0c90a2b88d4939a402d7b33064976c
Karl Williamson <khw@cpan.org>
2026-02-20 11:24:31 -0700

    intuit_more: Avoid use of unreliable value
    As now stated in the comments, the 'send' variable may point to
    something earlier than it should.  This is a step towards fixing that.
    In this particular case, it is easy to rewrite this 'if' statement to
    avoid using 'send'

--- toke.c
+++ toke.c
@@ -4676,8 +4676,10 @@ S_intuit_more(pTHX_ char *s, char *e,
      * it's exactly two digits long, it would would be very unlikely for
      * someone to use octal to spell a number from 1-7, so would be a
      * character class */
-    if (isDIGIT(s[0]) && send - s <= 2 && (send - s == 1 || (isDIGIT(s[1])))) {
-        return TRUE;
+    if (isDIGIT(s[0]) && (   (e - s >= 2 &&                  s[1] == ']')
+                          || (e - s >= 3 && isDIGIT(s[1]) && s[2] == ']')))
+    {
+        return true;
     }
 
     /* this is terrifying, and it mostly works.  See GH #16478.
