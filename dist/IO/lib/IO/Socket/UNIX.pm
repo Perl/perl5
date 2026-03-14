554ea8d7a14780a13236483cc7430b4fb95f8ba0
Michael Banck <mbanck@debian.org>
2026-03-14 14:27:54 +0100

    dist/IO: Set io_socket_peername during configure() of a Unix socket
    Some systems (notably GNU/Hurd) do not support getpeername() on a local Unix
    socket, which breaks read() on them as it tries to set the peer if there is
    none already. In order to circumvent that, set the peer during configure(),
    where it is known.
    
    Fixes #24195

--- dist/IO/lib/IO/Socket/UNIX.pm
+++ dist/IO/lib/IO/Socket/UNIX.pm
@@ -11,7 +11,7 @@ use IO::Socket;
 use Carp;
 
 our @ISA = qw(IO::Socket);
-our $VERSION = "1.56";
+our $VERSION = "1.57";
 
 IO::Socket::UNIX->register_domain( AF_UNIX );
 
@@ -45,6 +45,7 @@ sub configure {
     }
     elsif(exists $arg->{Peer}) {
 	my $addr = sockaddr_un($arg->{Peer});
+	${*$sock}{'io_socket_peername'} = $arg->{Peer};
 	$sock->connect($addr) or
 	    return undef;
     }
