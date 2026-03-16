be7fd452232b3bd7ccc465019dfb5191968a49db
Paul "LeoNerd" Evans <leonerd@leonerd.org.uk>
2026-03-16 15:16:35 +0000

    Add padalloc_NO_SV flag for pad_alloc()
    This flag requests that the pad specifically doesn't have a new SV
    allocated into it, as the caller may wish to do something special with
    it

--- pad.h
+++ pad.h
@@ -127,6 +127,10 @@ struct padname_fieldinfo {
 #define PAD_FAKELEX_ANON   1 /* the lex is declared in an ANON, or ... */
 #define PAD_FAKELEX_MULTI  2 /* the lex can be instantiated multiple times */
 
+/* flags for pad_alloc */
+
+#define padalloc_NO_SV  0x0001  /* this needs to not collide with SVs_PADTMP or SVf_READONLY */
+
 /* flags for the pad_new() function */
 
 #define padnew_CLONE	1	/* this pad is for a cloned CV */
