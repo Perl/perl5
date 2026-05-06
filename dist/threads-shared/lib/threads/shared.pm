559c41609ffbdbfe5dfc7bd4dec1aef3ccaab936
David Mitchell <davem@iabyn.com>
2026-05-06 13:19:35 +0100

    threads::shared: bump version to 1.74

--- dist/threads-shared/lib/threads/shared.pm
+++ dist/threads-shared/lib/threads/shared.pm
@@ -8,7 +8,7 @@ use Config;
 
 use Scalar::Util qw(reftype refaddr blessed);
 
-our $VERSION = '1.73'; # Please update the pod, too.
+our $VERSION = '1.74'; # Please update the pod, too.
 my $XS_VERSION = $VERSION;
 $VERSION = eval $VERSION;
 
@@ -196,7 +196,7 @@ threads::shared - Perl extension for sharing data structures between threads
 
 =head1 VERSION
 
-This document describes threads::shared version 1.73
+This document describes threads::shared version 1.74
 
 =head1 SYNOPSIS
 
