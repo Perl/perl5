5e58fed8eb86892908fa73f2fd2a5b56939e80de
Leon Timmermans <fawaka@gmail.com>
2026-05-30 03:38:05 +0200

    Revert "util.c: Removed unused function S_ptr_hash. Mentioned in #24078"
    This reverts commit ccc335f545d75638e2a7551ae4660bbd434cbb0b.

--- util.c
+++ util.c
@@ -4650,6 +4650,38 @@ Perl_parse_unicode_opts(pTHX_ const char **popt)
 #  include <starlet.h>
 #endif
 
+/* hash a pointer and return a U32 */
+
+PERL_STATIC_INLINE U32 S_ptr_hash(PTRV u) {
+    PERL_ARGS_ASSERT_PTR_HASH;
+/* 64 bit input */
+#if PTRSIZE == 8
+    /*
+     * This is one of Thomas Wang's hash functions for 64-bit integers from:
+     * https://gist.github.com/scottchiefbaker/9e21877a0c6041fbf54bd583cd1c52b4
+     */
+    u = (~u) + (u << 18);
+    u = u ^ (u >> 31);
+    u = u * 21;
+    u = u ^ (u >> 11);
+    u = u + (u << 6);
+    u = u ^ (u >> 22);
+/* 32 bit input */
+#else
+    /*
+     * This is one of Bob Jenkins' hash functions for 32-bit integers
+     * from: https://burtleburtle.net/bob/hash/integer.html
+     */
+    u = (u + 0x7ed55d16) + (u << 12);
+    u = (u ^ 0xc761c23c) ^ (u >> 19);
+    u = (u + 0x165667b1) + (u << 5);
+    u = (u + 0xd3a2646c) ^ (u << 9);
+    u = (u + 0xfd7046c5) + (u << 3);
+    u = (u ^ 0xb55a4f09) ^ (u >> 16);
+#endif
+    return (U32)u;
+}
+
 /* Splitmix64 is a simple PRNG and integer hashing function. It was
  * introducted in 2015: https://gee.cs.oswego.edu/dl/papers/oopsla14.pdf
  * Examples: https://rosettacode.org/wiki/Pseudo-random_numbers/Splitmix64
