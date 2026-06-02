44fa43b1698e68e6e1c8aa83dbbd3b2563254409
Thibault DUPONCHELLE <thibault.duponchelle@gmail.com>
2026-06-02 10:35:51 +0200

    Update after Module::CoreList release to CPAN

--- Porting/Maintainers.pl
+++ Porting/Maintainers.pl
@@ -846,7 +846,7 @@ our %Modules = (
     },
 
     'Module::CoreList' => {
-        'DISTRIBUTION' => 'BINGOS/Module-CoreList-5.20260420.tar.gz',
+        'DISTRIBUTION' => 'BINGOS/Module-CoreList-5.20260601.tar.gz',
         'FILES'        => q[dist/Module-CoreList],
     },
 
