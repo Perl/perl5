904e4566f4c7f781f700c5fa4e4649047468f936
Steve Hay <steve.m.hay@googlemail.com>
2026-06-28 17:11:48 +0100

    Update Socket from 2.038 to 2.041

--- Porting/Maintainers.pl
+++ Porting/Maintainers.pl
@@ -1039,8 +1039,7 @@ our %Modules = (
     },
 
     'Socket' => {
-        'DISTRIBUTION' => 'PEVANS/Socket-2.038.tar.gz',
-        'SYNCINFO'     => 'LeoNerd on Sat Apr 27 09:57:02 2024',
+        'DISTRIBUTION' => 'PEVANS/Socket-2.041.tar.gz',
         'FILES'        => q[cpan/Socket],
         'EXCLUDED'     => ['.editorconfig'],
     },
