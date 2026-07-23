e807f1dcb31df88e3e0f2d7a8c348743a0f45f6e
Karl Williamson <khw@cpan.org>
2026-07-23 08:23:59 -0600

    perlintern: Mark djSP as back-compat

--- pp.h
+++ pp.h
@@ -79,7 +79,7 @@ L<perlcall>.
 Declares a local copy of perl's stack pointer for the XSUB, available via
 the C<SP> macro.  See C<L</SP>>.
 
-=for apidoc mn;||djSP
+=for apidoc Bmn;||djSP
 
 Declare Just C<SP>.  This is actually identical to C<dSP>, and declares
 a local copy of perl's stack pointer, available via the C<SP> macro.
