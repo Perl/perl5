Perl/perl5

commit aca7b5cadb252ed5eb4a57fe2a8980ce24725651

Create concept of SV-backed PADNAMEs
Currently entirely untested. Could do with some actual use, especially
during thread cloning, outer captures, and eventual reclaim afterwards.

pad.c
@@ -2924,8 +2924,37 @@ Perl_padname_free(pTHX_ PADNAME *pn)
                 Safefree(info);
             }
         }
+        if (PadnameIsFULLSV(pn))
+            SvREFCNT_dec(PadnameSV(pn));
         Safefree(pn);
     }
+}
+
+/*
+In-place upgrades a PADNAME so that PadnameIsFULLSV() is true. This stores an
+SV inside the padname, which is now always the one returned by PadnameSV().
+Code may use this SV as a place to attach magic.
+*/
+PADNAME *
+Perl_padname_upgrade_sv(pTHX_ PADNAME *pn)
+{
+    PERL_ARGS_ASSERT_PADNAME_UPGRADE_SV;
+
+    if(PadnameIsFULLSV(pn))
+        return pn;
+
+    SV *sv = newSV_type(SVt_PV);
+    SvLEN_set(sv, 0); /* sv does not own the buffer, it belongs to the underlying Padname */
+    SvCUR_set(sv, PadnameLEN(pn));
+    SvPVX(sv) = PadnamePV(pn);
+    SvPOK_on(sv);
+    SvUTF8_on(sv);
+    SvREADONLY_on(sv);
+
+    pn->xpadn_sv = sv;
+
+    PadnameFLAGS(pn) |= PADNAMEf_FULLSV;
+    return pn;
 }
 
 #if defined(USE_ITHREADS)
@@ -2956,9 +2985,20 @@ Perl_padname_dup(pTHX_ PADNAME *src, CLONE_PARAMS *param)
         return dst;
     }
 
-    dst = PadnameOUTER(src)
-     ? newPADNAMEouter(padname_dup(PADNAME_FROM_PV(PadnamePV(src)), param))
-     : newPADNAMEpvn(PadnamePV(src), PadnameLEN(src));
+    if(PadnameOUTER(src))
+        /* Cloning an outer padname just makes a capture of the outer part
+         * regardless of whether it is SV-backed
+         */
+        dst = newPADNAMEouter(padname_dup(PADNAME_FROM_PV(PadnamePV(src)), param));
+    else {
+        dst = newPADNAMEpvn(PadnamePV(src), PadnameLEN(src));
+
+        if(PadnameIsFULLSV(src)) {
+            char *pnpv = PadnamePV(dst);
+            dst->xpadn_sv = sv_dup_inc(src->xpadn_sv, param);
+            SvPVX(dst->xpadn_sv) = pnpv;
+        }
+    }
     ptr_table_store(PL_ptr_table, src, dst);
     PadnameFLAGS(dst) = PadnameFLAGS(src);
     PadnameREFCNT(dst) = 0; /* The caller will increment it.  */

pad.h
@@ -58,7 +58,10 @@ struct padnamelist {
 struct padname_fieldinfo;
 
 #define PADNAME_BASE_ \
-    char *	xpadn_pv;		\
+    union {                             \
+        char *	xpadn_pv;		\
+        SV *    xpadn_sv;               \
+    };                                  \
     HV *	xpadn_ourstash;		\
     union {				\
         HV *	xpadn_typestash;	\
@@ -247,8 +250,12 @@ The length of the name.
 Whether PadnamePV is in UTF-8.  Currently, this is always true.
 
 =for apidoc Amx|SV *|PadnameSV|PADNAME * pn
-Returns the pad name as a mortal SV. The returned SV is marked C<SvREADONLY>;
-the caller must not attempt to modify it.
+Returns the pad name as a maybe-mortal SV. The returned SV is marked
+C<SvREADONLY>; the caller must not attempt to modify it.
+
+The returned SV might be mortal, or it might be owned by the pad name itself.
+Either way, the caller should not attempt to take ownership of it or call
+C<SvREFCNT_dec> on it.
 
 =for apidoc m|bool|PadnameIsOUR|PADNAME * pn
 Whether this is an "our" variable.
@@ -338,11 +345,11 @@ Restore the old pad saved into the local variable C<opad> by C<PAD_SAVE_LOCAL()>
 #define PadARRAY(pad)		AvARRAY(pad)
 #define PadMAX(pad)		AvFILLp(pad)
 
-#define PadnamePV(pn)		((pn)->xpadn_pv + 0)
-#define PadnameLEN(pn)		((pn)->xpadn_len + 0)
+#define PadnamePV(pn)		(PadnameIsFULLSV(pn) ? SvPVX((pn)->xpadn_sv) : (pn)->xpadn_pv)
+#define PadnameLEN(pn)		(PadnameIsFULLSV(pn) ? SvCUR((pn)->xpadn_sv) : (pn)->xpadn_len)
 #define PadnameUTF8(pn)		1
 #define PadnameSV(pn) \
-        newSVpvn_flags(PadnamePV(pn), PadnameLEN(pn), SVs_TEMP|SVf_UTF8|SVf_READONLY|SVf_PROTECT)
+        (PadnameIsFULLSV(pn) ? (pn)->xpadn_sv : newSVpvn_flags(PadnamePV(pn), PadnameLEN(pn), SVs_TEMP|SVf_UTF8|SVf_READONLY|SVf_PROTECT))
 #define PadnameFLAGS(pn)	(pn)->xpadn_flags
 #define PadnameIsOUR(pn)	cBOOL((pn)->xpadn_ourstash)
 #define PadnameOURSTASH(pn)	(pn)->xpadn_ourstash
@@ -359,6 +366,7 @@ Restore the old pad saved into the local variable C<opad> by C<PAD_SAVE_LOCAL()>
 #define PadnameIsSTATE(pn)	(PadnameFLAGS(pn) & PADNAMEf_STATE)
 #define PadnameLVALUE(pn)	(PadnameFLAGS(pn) & PADNAMEf_LVALUE)
 #define PadnameIsFIELD(pn)	(PadnameFLAGS(pn) & PADNAMEf_FIELD)
+#define PadnameIsFULLSV(pn)     (PadnameFLAGS(pn) & PADNAMEf_FULLSV)
 
 #define PadnameLVALUE_on(pn)    (PadnameFLAGS(pn) |= PADNAMEf_LVALUE)
 #define PadnameIsSTATE_on(pn)   (PadnameFLAGS(pn) |= PADNAMEf_STATE)
@@ -369,10 +377,11 @@ Restore the old pad saved into the local variable C<opad> by C<PAD_SAVE_LOCAL()>
 #define PADNAMEf_TYPED      0x08    /* for B; unused by core */
 #define PADNAMEf_OUR        0x10    /* for B; unused by core */
 #define PADNAMEf_FIELD      0x20    /* field var */
+#define PADNAMEf_FULLSV     0x80    /* padname stored in SVt_PV in xpadn_sv */
 
 #ifdef PERL_CORE
-#  define PadnamePV_set(pn, pv)     ((pn)->xpadn_pv = (pv))
-#  define PadnameLEN_set(pn, len)   ((pn)->xpadn_len = (len))
+#  define PadnamePV_set(pn, pv)     (assert(!PadnameIsFULLSV(pn)), (pn)->xpadn_pv = (pv))
+#  define PadnameLEN_set(pn, len)   (assert(!PadnameIsFULLSV(pn)), (pn)->xpadn_len = (len))
 #endif
 
 /* backward compatibility */

embed.fnc
@@ -2738,6 +2738,8 @@ Adpx	|PADNAME **|padnamelist_store					\
 				|NN PADNAMELIST *pnl			\
 				|SSize_t key				\
 				|NULLOK PADNAME *val
+p	|PADNAME *|padname_upgrade_sv					\
+				|NN PADNAME *pn
 
 : pad API
 ARdp	|PADLIST *|pad_new	|int flags

embed.h
@@ -1383,6 +1383,7 @@
 #   define pad_push(a,b)                        Perl_pad_push(aTHX_ a,b)
 #   define pad_swipe(a,b)                       Perl_pad_swipe(aTHX_ a,b)
 #   define padlist_store(a,b,c)                 Perl_padlist_store(aTHX_ a,b,c)
+#   define padname_upgrade_sv(a)                Perl_padname_upgrade_sv(aTHX_ a)
 #   define parse_unicode_opts(a)                Perl_parse_unicode_opts(aTHX_ a)
 #   define parser_free(a)                       Perl_parser_free(aTHX_ a)
 #   define peep(a)                              Perl_peep(aTHX_ a)

pod/perldelta.pod
@@ -422,6 +422,12 @@ L<perlclib/Dealing with embedded perls and threads>.
 During compiletime of a named subroutine, the name of the new sub
 currently being compiled is now accessible via C<CvNAME_HEK(PL_compcv)>.
 
+=item *
+
+A new internal function, C<padname_upgrade_sv()> attaches an SV onto a
+PADNAME instance, which is used to store the name string of that padname.
+This SV can be used by code that wishes to attach magic onto the padname.
+
 =back
 
 =head1 Selected Bug Fixes

proto.h
@@ -5129,6 +5129,14 @@ Perl_padname_free(pTHX_ PADNAME *pn)
         Perl_assert_aTHX; assert(pn)
 
 PERL_CALLCONV PADNAME *
+Perl_padname_upgrade_sv(pTHX_ PADNAME *pn)
+        Perl_attribute_nonnull_aTHX
+        Perl_attribute_nonnull(pTHX_1)
+        __attribute__visibility__("hidden");
+#define PERL_ARGS_ASSERT_PADNAME_UPGRADE_SV     \
+        Perl_assert_aTHX; assert(pn)
+
+PERL_CALLCONV PADNAME *
 Perl_padnamelist_fetch(PADNAMELIST *pnl, SSize_t key)
         Perl_attribute_nonnull(1)
         __attribute__warn_unused_result__;

regen/embed.pl
@@ -3322,6 +3322,8 @@ my @undocumented_always_visible = qw(
     KEY_UNITCHECK
     MAX_UNICODE_UTF8_BYTES
     MGf_MGv2
+    PADNAMEf_FULLSV
+    PadnameIsFULLSV
 
     assert_scalar_or_IO_
     DEBUG__

commit 9f34f23a2ea941bcd8129515723a1b656c1fddb8

PADNAME: restore C99 compatibility
Commit aca7b5cadb25 inadvertently introduced a C11-only feature (unnamed
structure/union members) in struct padname. Give the union a name so we
can build cleanly on C99-only compilers.

Fixes #24740.

pad.c
@@ -2951,7 +2951,7 @@ Perl_padname_upgrade_sv(pTHX_ PADNAME *pn)
     SvUTF8_on(sv);
     SvREADONLY_on(sv);
 
-    pn->xpadn_sv = sv;
+    pn->xpadn_name_u.xpadn_sv = sv;
 
     PadnameFLAGS(pn) |= PADNAMEf_FULLSV;
     return pn;
@@ -2995,8 +2995,8 @@ Perl_padname_dup(pTHX_ PADNAME *src, CLONE_PARAMS *param)
 
         if(PadnameIsFULLSV(src)) {
             char *pnpv = PadnamePV(dst);
-            dst->xpadn_sv = sv_dup_inc(src->xpadn_sv, param);
-            SvPVX(dst->xpadn_sv) = pnpv;
+            dst->xpadn_name_u.xpadn_sv = sv_dup_inc(src->xpadn_name_u.xpadn_sv, param);
+            SvPVX(dst->xpadn_name_u.xpadn_sv) = pnpv;
         }
     }
     ptr_table_store(PL_ptr_table, src, dst);

pad.h
@@ -61,7 +61,7 @@ struct padname_fieldinfo;
     union {                             \
         char *	xpadn_pv;		\
         SV *    xpadn_sv;               \
-    };                                  \
+    } xpadn_name_u;                     \
     HV *	xpadn_ourstash;		\
     union {				\
         HV *	xpadn_typestash;	\
@@ -345,11 +345,11 @@ Restore the old pad saved into the local variable C<opad> by C<PAD_SAVE_LOCAL()>
 #define PadARRAY(pad)		AvARRAY(pad)
 #define PadMAX(pad)		AvFILLp(pad)
 
-#define PadnamePV(pn)		(PadnameIsFULLSV(pn) ? SvPVX((pn)->xpadn_sv) : (pn)->xpadn_pv)
-#define PadnameLEN(pn)		(PadnameIsFULLSV(pn) ? SvCUR((pn)->xpadn_sv) : (pn)->xpadn_len)
+#define PadnamePV(pn)		(PadnameIsFULLSV(pn) ? SvPVX((pn)->xpadn_name_u.xpadn_sv) : (pn)->xpadn_name_u.xpadn_pv)
+#define PadnameLEN(pn)		(PadnameIsFULLSV(pn) ? SvCUR((pn)->xpadn_name_u.xpadn_sv) : (pn)->xpadn_len)
 #define PadnameUTF8(pn)		1
 #define PadnameSV(pn) \
-        (PadnameIsFULLSV(pn) ? (pn)->xpadn_sv : newSVpvn_flags(PadnamePV(pn), PadnameLEN(pn), SVs_TEMP|SVf_UTF8|SVf_READONLY|SVf_PROTECT))
+        (PadnameIsFULLSV(pn) ? (pn)->xpadn_name_u.xpadn_sv : newSVpvn_flags(PadnamePV(pn), PadnameLEN(pn), SVs_TEMP|SVf_UTF8|SVf_READONLY|SVf_PROTECT))
 #define PadnameFLAGS(pn)	(pn)->xpadn_flags
 #define PadnameIsOUR(pn)	cBOOL((pn)->xpadn_ourstash)
 #define PadnameOURSTASH(pn)	(pn)->xpadn_ourstash
@@ -380,7 +380,7 @@ Restore the old pad saved into the local variable C<opad> by C<PAD_SAVE_LOCAL()>
 #define PADNAMEf_FULLSV     0x80    /* padname stored in SVt_PV in xpadn_sv */
 
 #ifdef PERL_CORE
-#  define PadnamePV_set(pn, pv)     (assert(!PadnameIsFULLSV(pn)), (pn)->xpadn_pv = (pv))
+#  define PadnamePV_set(pn, pv)     (assert(!PadnameIsFULLSV(pn)), (pn)->xpadn_name_u.xpadn_pv = (pv))
 #  define PadnameLEN_set(pn, len)   (assert(!PadnameIsFULLSV(pn)), (pn)->xpadn_len = (len))
 #endif
 