02c58b77b46d18e37fcdb8b57a26ca46b367ffb5
Karl Williamson <khw@cpan.org>
2026-09-07 04:29:07 -0600

    Allow declaring a parameter unused when not DEBUGGING
    This is a follow on to b35a6ea61ae020705a7132331e14646f84592da5 which
    adds the ability to decare a parameter to be unused.  This new commit
    makes it convenient to declare a parameter to be DEBUG_ONLY, allowing
    the removal of PERL_UNUSED_ARG statements in the code that the previous
    commit didn't.
    
    And this commit removes two related such statements, to show how it
    works.
    
    The purpose of PERL_UNUSED_ARG is to avoid expected warnings that are
    known harmless, without turning off all warnings of that kind.  It is
    possible to use that macro when there are actual uses of the parameter,
    thus defeating the macro's purpose to some extent.  This is typically
    done when some Configure options cause the parameter to be used, and
    some do not, and the author didn't want to add the extra #ifdef's
    required to get the macro to be in effect in just the precise
    conditions.  After all, there's added complexity in the code for both
    the author and future readers that can obscure the basic logic, with not
    that much gain.
    
    But, the most common case where the parameter is only used in some
    Configurations is in DEBUGGING builds.  This commit handles this common
    case.  It is a simple extension to
    b35a6ea61ae020705a7132331e14646f84592da5, to whose commit message you
    are referred to the details.
    
    This is enabled only for core perl and extensions.
    
    To use this capability, the symbol DEBUG_ONLY is added after the
    parameter name in *both* its embed.fnc entry and its definition.

--- proto.h
+++ proto.h
@@ -12931,10 +12931,11 @@ S_parse_LC_ALL_string(pTHX_ const char *string, const char **output, const parse
     } STMT_END
 
 static void
-S_restore_toggled_locale_i(pTHX_ const locale_category_index cat_index, const char *original_locale, const line_t caller_line)
+S_restore_toggled_locale_i(pTHX_ const locale_category_index cat_index, const char *original_locale, const line_t caller_line __attribute__unused_unless_debugging__)
         Perl_attribute_nonnull_aTHX;
 #   define PERL_ARGS_ASSERT_RESTORE_TOGGLED_LOCALE_I \
        STMT_START { Perl_assert_aTHX; PERL_UNUSED_CONTEXT_FOR_ARGS_ASSERT;  \
+                    PERL_DEBUG_ONLY_ARG_FOR_ARGS_ASSERT(caller_line);       \
     } STMT_END
 
 static const char *
@@ -12964,12 +12965,13 @@ S_setlocale_failure_panic_via_i(pTHX_ const locale_category_index cat_index, con
     } STMT_END
 
 static const char *
-S_toggle_locale_i(pTHX_ const locale_category_index cat_index, const char *new_locale, const line_t caller_line)
+S_toggle_locale_i(pTHX_ const locale_category_index cat_index, const char *new_locale, const line_t caller_line __attribute__unused_unless_debugging__)
         Perl_attribute_nonnull_aTHX
         Perl_attribute_nonnull(pTHX_2);
 #   define PERL_ARGS_ASSERT_TOGGLE_LOCALE_I     \
-       STMT_START { Perl_assert_aTHX; assert(new_locale);  \
-                    PERL_UNUSED_CONTEXT_FOR_ARGS_ASSERT;   \
+       STMT_START { Perl_assert_aTHX; assert(new_locale);              \
+                    PERL_UNUSED_CONTEXT_FOR_ARGS_ASSERT;               \
+                    PERL_DEBUG_ONLY_ARG_FOR_ARGS_ASSERT(caller_line);  \
     } STMT_END
 
 #   if defined(DEBUGGING)
