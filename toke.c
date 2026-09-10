e7253c8e866977adcdf2f3963435899b2bb28d4d
Richard Leach <richardleach@users.noreply.github.com>
2026-09-10 20:45:45 +0000

    Use newSV_type_generic when type is unknown or rare
    Avoid binary bloating when the target type is not known at compile time,
    or the call site/type is pretty cold.

--- toke.c
+++ toke.c
@@ -13668,7 +13668,7 @@ Perl_start_subparse(pTHX_ I32 is_format, U32 flags)
     save_item(PL_subname);
     SAVESPTR(PL_compcv);
 
-    PL_compcv = MUTABLE_CV(newSV_type(is_format ? SVt_PVFM : SVt_PVCV));
+    PL_compcv = MUTABLE_CV(newSV_type_generic(is_format ? SVt_PVFM : SVt_PVCV));
     CvFLAGS(PL_compcv) |= flags;
 
     PL_subline = CopLINE(PL_curcop);
