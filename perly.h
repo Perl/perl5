827da101793bd0d33d7712629ea758e174af27b1
Paul "LeoNerd" Evans <leonerd@leonerd.org.uk>
2026-09-15 21:08:11 +0100

    Expose class_method_parse_post_blockstart() as an API function
    The function previously existed under a different name, but was kept
    internal to be called by the parser.
    
    This is required as an API-visible function that XS parsing modules can
    call; specifically things like XS::Parse::Sublike, as demonstrated by
    
      https://github.com/Perl/perl5/issues/24773

--- perly.h
+++ perly.h
@@ -248,6 +248,6 @@ int yyparse (void);
 
 
 /* Generated from:
- * a0671909c03af7d45863e59a348167953070010923230d95305928773fe79aa9 perly.y
+ * 0540b64053bede9454d33873d5bc9a003be0e21cd244ab0ae3f0a386e00ffb9a perly.y
  * 1a4cfc5b32006b09aba17cc80cd31ade3503ab1e829681adfe337d9c26e19fe0 regen_perly.pl
  * ex: set ro ft=c: */
