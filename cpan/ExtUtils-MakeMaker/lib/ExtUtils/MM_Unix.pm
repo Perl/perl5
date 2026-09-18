b7e0525d8ab2abfccc5d02e21ea4e75a876db2cb
James E Keenan <jkeenan@cpan.org>
2026-09-18 07:23:29 -0400

    Smoke-testing version of GH #24839
    Committer: fixed two porting tests.  These will have to be fixed
    upstream on CPAN before synching into blead.

--- cpan/ExtUtils-MakeMaker/lib/ExtUtils/MM_Unix.pm
+++ cpan/ExtUtils-MakeMaker/lib/ExtUtils/MM_Unix.pm
@@ -15,7 +15,7 @@ use ExtUtils::MakeMaker qw($Verbose neatvalue _sprintf562);
 
 # If $VERSION is in scope, parse_version() breaks
 {
-our $VERSION = '7.78';
+our $VERSION = '7.78_01';
 $VERSION =~ tr/_//d;
 }
 
